0d88413727658b6c6fae70072cf60334d256473b
kutschke <>
2009-10-22 16:36:08 +0000

    Changes required by added and deleted data products.

--- ToyDP/src/classes.h
+++ ToyDP/src/classes.h
@@ -1,9 +1,9 @@
 //
 // Build a dictionary.
 //
-// $Id: classes.h,v 1.3 2009/10/09 13:31:32 kutschke Exp $
+// $Id: classes.h,v 1.4 2009/10/22 16:36:08 kutschke Exp $
 // $Author: kutschke $
-// $Date: 2009/10/09 13:31:32 $
+// $Date: 2009/10/22 16:36:08 $
 // 
 // Original author Rob Kutschke
 
@@ -16,7 +16,7 @@
 #include "ToyDP/inc/ToyHitCollection.hh"
 #include "ToyDP/inc/ToyGenParticleCollection.hh"
 #include "ToyDP/inc/StepPointMCCollection.hh"
-#include "ToyDP/inc/CrudeStrawHitCollection.hh"
+#include "ToyDP/inc/CrudeStrawHitPData.hh"
 
 //
 // Only include objects that we would like to be able to put into the event.
@@ -27,6 +27,6 @@ struct dictionary {
   edm::Wrapper<mu2e::ToyHitCollection>         dummy301;
   edm::Wrapper<mu2e::ToyGenParticleCollection> dummy302;
   edm::Wrapper<mu2e::StepPointMCCollection>    dummy303;
-  edm::Wrapper<mu2e::CrudeStrawHitCollection>  dummy304;
+  edm::Wrapper<mu2e::CrudeStrawHitPData>       dummy304;
 };
 }
