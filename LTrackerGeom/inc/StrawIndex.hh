98cee58ab9c704f6ffaad544afbd52827357981a
kutschke <>
2009-11-03 19:59:10 +0000

    Remove static fromInt; make c'tor public and explicit; maintenance on comparison operators.

--- LTrackerGeom/inc/StrawIndex.hh
+++ LTrackerGeom/inc/StrawIndex.hh
@@ -6,9 +6,9 @@
 // Has values 0...(N-1), where N is the number
 // of straws in the system.
 //
-// $Id: StrawIndex.hh,v 1.2 2009/10/22 16:21:05 kutschke Exp $
+// $Id: StrawIndex.hh,v 1.3 2009/11/03 19:59:10 kutschke Exp $
 // $Author: kutschke $ 
-// $Date: 2009/10/22 16:21:05 $
+// $Date: 2009/11/03 19:59:10 $
 //
 // Original author Rob Kutschke
 //
@@ -33,22 +33,21 @@ namespace mu2e {
   public:
 
     // No default c'tor by design.
+
+    // No automatic conversion of int to StrawIndex.
+    explicit StrawIndex(int idx):
+      _idx(idx){
+    }
     
     // Compiler generated versions are OK for:
     // copy c'tor, destructor, operator=
 
     // Return the value as an int.
+    // Do not want automatic conversion to an int.
     int asInt() const { return _idx;}
-
-    // Set value from an int.
-    static StrawIndex fromInt(int i) { return StrawIndex(i); }
 
     bool operator==( StrawIndex const& rhs) const{
       return (_idx == rhs._idx);
-    }
-
-    bool operator!=( StrawIndex const& rhs) const{
-      return !( *this == rhs);
     }
 
     bool operator<( StrawIndex const& rhs) const{
@@ -56,11 +55,6 @@ namespace mu2e {
     }
 
   private:
-
-    // 
-    StrawIndex(int idx):
-      _idx(idx){
-    }
 
     int _idx;
   };
@@ -70,6 +64,12 @@ namespace mu2e {
     ost << i.asInt();
     return ost;
   }
+
+  inline bool operator!=( StrawIndex const& lhs, 
+			  StrawIndex const& rhs) {
+      return !( lhs == rhs);
+  }
+
   
 
 }
