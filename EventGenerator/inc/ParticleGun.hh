00fe6b69ae122a4ab728c4ede80eca930ab8f906
kutschke <>
2010-03-29 16:20:57 +0000

    Replace dummy implementation with a real one.

--- EventGenerator/inc/ParticleGun.hh
+++ EventGenerator/inc/ParticleGun.hh
@@ -4,14 +4,29 @@
 // Shoots a single particle gun and puts its output
 // into a generated event.
 //
-// $Id: ParticleGun.hh,v 1.1 2009/09/30 22:57:47 kutschke Exp $
+// $Id: ParticleGun.hh,v 1.2 2010/03/29 16:20:57 kutschke Exp $
 // $Author: kutschke $ 
-// $Date: 2009/09/30 22:57:47 $
+// $Date: 2010/03/29 16:20:57 $
 //
 // Original author Rob Kutschke
+//
+// The adjustable properties are:
+// 1) 
 // 
 
+// Mu2e includes
 #include "EventGenerator/inc/GeneratorBase.hh"
+#include "Mu2eUtilities/inc/RandomUnitSphere.hh"
+#include "Mu2eUtilities/inc/PDGCode.hh"
+
+// External includes
+#include "CLHEP/Vector/ThreeVector.h"
+
+// Forward references.
+namespace edm{
+  class Run;
+}
+class TH1F;
 
 namespace mu2e {
 
@@ -21,18 +36,64 @@ namespace mu2e {
   class ParticleGun: public GeneratorBase{
 
   public:
-    ParticleGun( const SimpleConfig& config );
+    ParticleGun( edm::Run const& run, const SimpleConfig& config );
     virtual ~ParticleGun();
 
     virtual void generate( ToyGenParticleCollection&  );
 
   private:
 
+    // Information from the run time configuration.
+
+    // Number of particles per event.
+    int _n;
+
+    // PDG particle id code and the mass of the particle.
+    PDGCode::type _pdgId;
+
+    // Particles will be produced in a box, specified by 
+    // a point in the Tracker coordinate system and 
+    // the half lengths of the box.  Units are mm.
+    CLHEP::Hep3Vector _point;
+    CLHEP::Hep3Vector _halfLength;
+    
+    // Momentum range of the particle.  Units are MeV.
+    double _pmin;
+    double _pmax;
+
+    // Direction will be uniform over a restricted section of a unit sphere
+    RandomUnitSphere _randomUnitSphere;
+
+    // Time range over which the particle will be produced; units are ns.
+    double _tmin;
+    double _tmax;
+
+    // End of information from the run time configuration.
+
+    // Derived information.
+
+    // Mass of the particle to be generated.
+    double _mass;
+
+    // Ranges of momentum and time.
+    double _dp, _dt;
+
+    // Histogram information.
+    bool _doHistograms;
+
+    TH1F* _hMultiplicity;
+    TH1F* _hMomentum;
+    TH1F* _hCz;
+    TH1F* _hX0;
+    TH1F* _hY0;
+    TH1F* _hZ0;
+    TH1F* _hT0;
+
+    // Compute the default value of the point of origin.
+    double defaultZ0();
 
   };
 
 } // end namespace mu2e,
 
 #endif
-
-
--- EventGenerator/src/ParticleGun.cc
+++ EventGenerator/src/ParticleGun.cc
@@ -2,43 +2,187 @@
 // Shoots a single particle gun and puts its output
 // into a generated event.
 //
-// $Id: ParticleGun.cc,v 1.2 2009/11/13 23:29:19 kutschke Exp $
+// $Id: ParticleGun.cc,v 1.3 2010/03/29 16:20:57 kutschke Exp $
 // $Author: kutschke $ 
-// $Date: 2009/11/13 23:29:19 $
+// $Date: 2010/03/29 16:20:57 $
 //
 // Original author Rob Kutschke
 // 
 
 #include <iostream>
 
+// Framework includes
+#include "FWCore/Framework/interface/Run.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "FWCore/Services/interface/TFileService.h"
+#include "FWCore/Framework/interface/TFileDirectory.h"
+
+// Mu2e includes
 #include "EventGenerator/inc/ParticleGun.hh"
+#include "ConditionsService/inc/ConditionsHandle.hh"
+#include "ConditionsService/inc/ParticleDataTable.hh"
+#include "GeometryService/inc/GeomHandle.hh"
+#include "LTrackerGeom/inc/LTracker.hh"
+#include "ITrackerGeom/inc/ITracker.hh"
 #include "Mu2eUtilities/inc/PDGCode.hh"
 #include "Mu2eUtilities/inc/SimpleConfig.hh"
+
+// Root includes
+#include "TH1F.h"
+
+// Other external includes.
+#include "CLHEP/Random/RandFlat.h"
+#include "CLHEP/Units/PhysicalConstants.h"
 
 using namespace std;
 
 using CLHEP::Hep3Vector;
 using CLHEP::HepLorentzVector;
+using CLHEP::RandFlat;
+using CLHEP::twopi;
+
 
 namespace mu2e {
 
-  ParticleGun::ParticleGun( const SimpleConfig& config ):
-    GeneratorBase(){
+  ParticleGun::ParticleGun( edm::Run const& , const SimpleConfig& config ):
+    GeneratorBase(),
+    _n(1),
+    _point(),
+    _halfLength(),
+    _doHistograms(false),
+    _hMultiplicity(0),
+    _hMomentum(0){
+
+    // Finish setting up default values.
+    _point.setZ( defaultZ0() );
+
+    // Conversion energy for Al.  Should come from conditions.
+    static const double pEndPoint = 104.96;
+
+    // Process the run time configuration.
+    _n = config.getInt("particleGun.n", _n);
+
+    _pdgId = static_cast<PDGCode::type>(config.getInt("particleGun.id",  PDGCode::e_minus));
+
+    if ( config.hasName("particleGun.point") ){
+      _point  = config.getHep3Vector("particleGun.point");
+    }
+
+    if ( config.hasName("particleGun.halfLength") ){
+      _halfLength = config.getHep3Vector("particleGun.halfLength");
+    }
+
+    _pmin   = config.getDouble("particleGun.tmin", pEndPoint );
+    _pmax   = config.getDouble("particleGun.tmax", pEndPoint );
+
+    _randomUnitSphere.setczmin(  config.getDouble("particleGun.czmin",  0.5));
+    _randomUnitSphere.setczmax(  config.getDouble("particleGun.czmax",  0.7));
+    _randomUnitSphere.setphimin( config.getDouble("particleGun.czmin",   0.));
+    _randomUnitSphere.setphimax( config.getDouble("particleGun.czmax",  twopi));
+
+    _tmin   = config.getDouble("particleGun.tmin",  0. );
+    _tmax   = config.getDouble("particleGun.tmax",  0. );
+
+    _doHistograms = config.getBool("particleGun.doHistograms", _doHistograms);
+
+    // Compute derived information.
+    ConditionsHandle<ParticleDataTable> pdt("ignored");
+    _mass = pdt->particle(_pdgId).mass().value();
+
+    _dp  = ( _pmax - _pmin);
+    _dt  = ( _tmax - _tmin);
+
+    // Book histograms if enabled.
+    if ( !_doHistograms ) return;
+
+    edm::Service<edm::TFileService> tfs;
+
+    edm::TFileDirectory tfdir = tfs->mkdir( "ParticleGun" );
+    _hMultiplicity = tfs->make<TH1F>( "hMultiplicity", "Particle Gun Multiplicity",    10,  0.,  10.);
+    _hMomentum     = tfs->make<TH1F>( "hMomentum",     "Particle Gun Momentum (MeV)",  100, 0., 110.);
+    _hCz           = tfs->make<TH1F>( "hCz",           "Particle Gun cos(theta)",      100, -1.,  1.);
+    _hX0           = tfs->make<TH1F>( "hX0",           "Particle Gun X0",              100,   -20.,    20.);
+    _hY0           = tfs->make<TH1F>( "hY0",           "Particle Gun Y0",              100,   -20.,    20.);
+    _hZ0           = tfs->make<TH1F>( "hZ0",           "Particle Gun Z0",              100, -2000.,  2000.);
+    _hT0           = tfs->make<TH1F>( "hT0",           "Particle Gun Time",            100,     0.,  2000.);
+
   }
 
   ParticleGun::~ParticleGun(){
   }
 
-
   void ParticleGun::generate( ToyGenParticleCollection& genParts ){
 
-    Hep3Vector pos(0.,0.,0);
-    HepLorentzVector mom(0.,0.,0.,0.);
-    double time(0.);
+    if ( _doHistograms ){
+      _hMultiplicity->Fill(_n);
+    }
+
+    for ( int j =0; j<_n; ++j ){
+
+      // Random point inside of a box.
+      double x[3];
+      for ( int i=0; i<3; ++i ){
+        x[i] = _point[i] + RandFlat::shoot(-1.,1.)*_halfLength[i];
+      }
+      Hep3Vector pos(x[0], x[1], x[2]);
+
+      // Magnitude of momentum.
+      double p = _pmin + _dp * RandFlat::shoot();
+      
+      // 3 Momentum.
+      Hep3Vector mom = p*_randomUnitSphere.shoot();
+
+      // Energy
+      double e = sqrt(mom*mom + _mass*_mass);
     
+      // 4 Momentum.
+      HepLorentzVector p4( mom, e);
 
-    genParts.push_back( ToyGenParticle( PDGCode::e_minus, GenId::particleGun, pos, mom, time));
+      // Time
+      double time = _tmin + _dt*RandFlat::shoot();
 
+      genParts.push_back( ToyGenParticle( _pdgId, GenId::particleGun, pos, p4, time));
+
+      if ( _doHistograms ) {
+        _hMomentum->Fill(p);
+        _hCz->Fill(mom.cosTheta());
+        _hX0->Fill( pos.x() );
+        _hY0->Fill( pos.y() );
+        _hZ0->Fill( pos.z() );
+        _hT0->Fill( time );
+      }
+
+    }
+
+  } // end of generate
+
+
+  // Compute a default value of the z0 of the particle gun.
+  // In the tracker coordinate system, in mm.
+  double ParticleGun::defaultZ0(){
+
+    // Default z position is at the entrance to a nominal tracker, in mm.
+    double z0 = -1400.;
+
+    // Override this if an actual tracker is present.
+
+    edm::Service<GeometryService> geom;
+
+    if ( geom->hasElement<LTracker>() ){
+      GeomHandle<LTracker> ltracker;
+      z0 = -ltracker->zHalfLength();
+    } else if ( geom->hasElement<ITracker>() ) {
+      GeomHandle<ITracker> itracker;
+      z0 = -itracker->zHalfLength();
+    }
+
+    // Add a TTracker branch when ready.
+
+    // Tracker position in the Detecctor coordinate system.
+    static double trackerOffset = -1800.;
+
+    return z0+trackerOffset;
   }
 
 }
