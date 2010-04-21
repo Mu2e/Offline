2c52412883fc69ed10f8c4b72049a61600cac29e
rhbob <>
2010-04-21 17:50:29 +0000

    need to keep until done

---
+++ CalorimeterGeom/inc/DeviceId.hh
@@ -0,0 +1,20 @@
+#ifndef VANEID_HH
+#define VANEID_HH
+// $Id: DeviceId.hh,v 1.4 2010/04/21 17:50:29 rhbob Exp $
+// $Author: rhbob $
+// $Date: 2010/04/21 17:50:29 $
+
+// original authors Julie Managan and Robert Bernstein
+
+namespace mu2e{
+  namespace calorimeter{
+
+// Device identifier.
+// If set to -1, then the device is not defined.
+
+typedef int VaneId;
+
+  } //namespace calorimeter
+} //namespace mu2e
+
+#endif
