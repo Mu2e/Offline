02413f42d939750e4a066db9b2a1396fcca8e472
rhbob <>
2010-04-28 16:18:59 +0000

    adding Transport Solenoid

---
+++ Mu2eG4/inc/nestTorus.hh
@@ -0,0 +1,66 @@
+#ifndef NESTTORUS_HH
+#define NESTTORUS_HH
+//
+// Free function to create and place a new G4Torus, place inside a logical volume.
+// 
+// $Id: v 1.1.1.1
+// $Author: rhbob $ 
+// $Date: 2010/03/15
+//
+
+#include <string>
+#include <vector>
+
+#include "Mu2eG4/inc/VolumeInfo.hh"
+
+class G4Material;
+class G4LogicalVolume;
+class G4VPhysicalVolume;
+class G4CSGSolid;
+
+#include "G4ThreeVector.hh"
+#include "G4RotationMatrix.hh"
+#include "G4Colour.hh"
+
+
+namespace mu2e {
+
+  VolumeInfo nestTorus ( std::string const& name,
+			double halfDim[5],
+			G4Material* material,
+			G4RotationMatrix* rot,
+			G4ThreeVector const& offset,
+			G4LogicalVolume* parent,
+			int copyNo,
+			G4Colour color = G4Colour::Black(),
+			bool forceSolid = false
+			);
+  
+
+
+  // Alternate argument list, using a vector for the half dimensions.
+  //
+  inline VolumeInfo nestTorus ( std::string const& name,
+			       std::vector<double>&  halfDim,
+			       G4Material* material,
+			       G4RotationMatrix* rot,
+			       G4ThreeVector& offset,
+			       G4LogicalVolume* parent,
+			       int copyNo,
+			       G4Colour color = G4Colour::Black(),
+			       bool forceSolid = false
+			       ){
+    return nestTorus( name, 
+		     &halfDim[0],
+		     material,
+		     rot,
+		     offset,
+		     parent,
+		     copyNo,
+		     color,
+		     forceSolid
+		     );
+  }
+}
+
+#endif
---
+++ Mu2eG4/src/nestTorus.cc
@@ -0,0 +1,56 @@
+//
+// Free function to create and place a new G4Torus, place inside a logical volume.
+// 
+// $Id: nestTorus.cc,v 1.1.1.1
+// $Author: A Chandra
+// $Date: 2010/03/15
+//
+
+#include <string>
+
+#include "Mu2eG4/inc/nestTorus.hh"
+
+#include "G4Torus.hh"
+#include "G4LogicalVolume.hh"
+#include "G4Material.hh"
+#include "G4PVPlacement.hh"
+#include "G4ThreeVector.hh"
+#include "G4VisAttributes.hh"
+
+using namespace std;
+
+namespace mu2e {
+ 
+  //
+  // Create and place a G4Torus inside a logical volume.
+  // 
+  VolumeInfo nestTorus ( string const& name,
+			double param[5],
+			G4Material* material,
+			G4RotationMatrix* rot,
+			G4ThreeVector const& offset,
+			G4LogicalVolume* parent,
+			int copyNo,
+			G4Colour color,
+			bool forceSolid
+			){
+    
+    VolumeInfo info;
+    
+    info.solid   = new G4Torus( name, param[0], param[1], param[2], param[3], param[4]  );
+    
+    info.logical = new G4LogicalVolume( info.solid, material, name); 
+    
+    info.physical =  new G4PVPlacement( rot, offset, info.logical, name, parent, 0, copyNo);
+    
+    G4VisAttributes* visAtt = new G4VisAttributes(true, color);
+    visAtt->SetForceSolid(forceSolid);
+
+    visAtt->SetForceAuxEdgeVisible (false);
+
+    info.logical->SetVisAttributes(visAtt);
+
+    return info;
+  }
+
+}
