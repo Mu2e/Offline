c1db28580275276a9f70f8cee988c97d49b8fdb0
kutschke <>
2010-05-18 22:33:49 +0000

    Put into the mu2e namespace.

--- Mu2eG4/inc/DSField.hh
+++ Mu2eG4/inc/DSField.hh
@@ -1,5 +1,13 @@
-#ifndef DSFIELD_HH                              \
-  #define DSFIELD_HH
+#ifndef DSFIELD_HH
+#define DSFIELD_HH
+//
+// G4 interface to the Detector Solenoid full magnetic field.
+//
+// $Id: DSField.hh,v 1.3 2010/05/18 22:33:49 kutschke Exp $
+// $Author: kutschke $
+// $Date: 2010/05/18 22:33:49 $
+//
+// Original author Julie Managan and Bob Bernstein
 
 #include <memory>
 #include "G4MagneticField.hh"
@@ -7,26 +15,29 @@
 #include "BFwcont.hh"
 #include "G4ThreeVector.hh"
 
-class DSField: public G4MagneticField {
+namespace mu2e {
 
-public:
+  class DSField: public G4MagneticField {
 
-  DSField( std::string filename, G4ThreeVector const& origin, 
-           int const nx, int const ny, int const nz):
-    G4MagneticField(),
-    //_p(new BFBMvec(filename,origin,nx,ny,nz)){ 
-    _p(new BFwcont(filename,origin,nx,ny,nz)){
-  }
+  public:
 
-  virtual ~DSField(){};
-  virtual void GetFieldValue(const G4double Point[4],
-                             G4double *Bfield) const;
+    DSField( std::string filename, G4ThreeVector const& origin, 
+             int const nx, int const ny, int const nz):
+      G4MagneticField(),
+      //_p(new BFBMvec(filename,origin,nx,ny,nz)){ 
+      _p(new BFwcont(filename,origin,nx,ny,nz)){
+    }
 
-private:
+    virtual ~DSField(){};
+    virtual void GetFieldValue(const G4double Point[4],
+                               G4double *Bfield) const;
 
-  //std::auto_ptr<BFBMvec> _p;
-  std::auto_ptr<BFwcont> _p;
+  private:
 
-};
+    //std::auto_ptr<BFBMvec> _p;
+    std::auto_ptr<BFwcont> _p;
+
+  };
+}
 
 #endif
--- Mu2eG4/src/DSField.cc
+++ Mu2eG4/src/DSField.cc
@@ -1,8 +1,16 @@
+//
+// G4 interface to the Detector Solenoid full magnetic field.
+//
+// $Id: DSField.cc,v 1.3 2010/05/18 22:33:49 kutschke Exp $
+// $Author: kutschke $
+// $Date: 2010/05/18 22:33:49 $
+//
+// Original author Julie Managan and Bob Bernstein
+
 #include <cmath>
 #include <iostream>
 
 #include "Mu2eG4/inc/DSField.hh"
-//#include "../inc/BFBMvec.hh"
 #include "Mu2eG4/inc/BFwcont.hh"
 #include "G4MagneticField.hh"
 #include "G4Types.hh"
@@ -11,16 +19,19 @@
 using CLHEP::Hep3Vector;
 using namespace std;
 
-void DSField::GetFieldValue(const G4double Point[4],
-                             G4double *Bfield) const {
+namespace mu2e {
 
-  CLHEP::Hep3Vector point(Point[0],Point[1],Point[2]);
-  CLHEP::Hep3Vector BField = _p->GetBField(point);
-  Bfield[0] = BField.x()*tesla; 
-  Bfield[1] = BField.y()*tesla; 
-  Bfield[2] = BField.z()*tesla;
+  void DSField::GetFieldValue(const G4double Point[4],
+                              G4double *Bfield) const {
 
-  //  cout << "(" << BField.x() << "," << BField.y() << "," << BField.z() << ")" << endl; 
+    CLHEP::Hep3Vector point(Point[0],Point[1],Point[2]);
+    CLHEP::Hep3Vector BField = _p->GetBField(point);
+    Bfield[0] = BField.x()*tesla; 
+    Bfield[1] = BField.y()*tesla; 
+    Bfield[2] = BField.z()*tesla;
 
-}
+    //  cout << "(" << BField.x() << "," << BField.y() << "," << BField.z() << ")" << endl; 
 
+  }
+
+} // end namespace mu2e
