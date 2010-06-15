c9db8a9c0dcc69d8785638e2671bb837d19e37f4
kutschke <>
2010-06-15 16:50:07 +0000

    Use FRAMEWORK_DIR instead of MU2E_HOME.

--- bin/setup_mu2e_project.sh
+++ bin/setup_mu2e_project.sh
@@ -1,23 +1,26 @@
 #! /bin/sh
 #
-# $Id: setup_mu2e_project.sh,v 1.1 2009/09/30 22:57:47 kutschke Exp $
+# $Id: setup_mu2e_project.sh,v 1.2 2010/06/15 16:50:07 kutschke Exp $
 # $Author: kutschke $
-# $Date: 2009/09/30 22:57:47 $
+# $Date: 2010/06/15 16:50:07 $
 #
 # Original author Rob Kutschke
 #
-# Initialize the current directory tree as a Mu2e project.
+# Initialize the current directory tree as a the root of a framework based project.
 #  - add the local lib area to the LD_LIBRARY_PATH
 # 
 
-if [ "`basename $0 2>/dev/null`" = "setup_mu2e_user.sh" ];then
-    echo "you should be sourcing this file"; exit
+if [ "`basename $0 2>/dev/null`" = "setup_mu2e_project.sh" ];then
+    echo "You should be sourcing this file"; exit
 fi
-if [ "${MU2E_HOME-}" = '' ];then
-    echo "MU2E_HOME is not set; bin/setup_mu2e.sh (from a base release)"
-    echo "needs to be sourced"
-    exit
+
+if [ "${FRAMEWORK_DIR}" = '' ];then
+    echo "FRAMEWORK_DIR is not set; "
+    echo "You need to do setup the framework before sourcing this file."
+    return 21
 fi
+
+source ${FRAMEWORK_DIR}/bin/funcs.sh
 bin_dir=`dirname ${BASH_SOURCE}`   # assume file is in bin subdir
 bin_dir=`cd $bin_dir >/dev/null 2>&1 && echo $PWD`
 user_root=`dirname $bin_dir`
