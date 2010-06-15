3d9deb91e522ac4d4f6322df855bb3839557849e
kutschke <>
2010-06-15 16:49:10 +0000

    Complete rewrite.  This script now requires that a site specific file be sourced first.  On ilcsim* the site specific file is /prj/mu2e/setupmu2e.sh Change geant4 version to v4_9_3_p01.

--- setup.sh
+++ setup.sh
@@ -1,31 +1,31 @@
 #
-# $Id: setup.sh,v 1.5 2010/04/23 20:32:06 kutschke Exp $
+# $Id: setup.sh,v 1.6 2010/06/15 16:49:10 kutschke Exp $
 # $Author: kutschke $
-# $Date: 2010/04/23 20:32:06 $
+# $Date: 2010/06/15 16:49:10 $
 #
 # Original author Rob Kutschke
 #
-# The stuff in this first file should go into .profile.
-# Then remove this line:
-source setups.sh
+# Setup the environment to build a full release of the Mu2e software.
+# This presumes that you have already established the Mu2e environment
+# for this machine with something like:
+# source /prj/mu2e/
 
-# On ilcsim*
+if [ "`basename $0 2>/dev/null`" = "setup.sh" ];then
+    echo "You should be sourcing this file, not executing it."
+    exit
+fi
 
-source  /prj/mu2e/setup.sh
+if [ "${MU2E}" = '' ];then
+    echo "The environment variable MU2E is not set."
+    echo "You must setup the local Mu2e environment before sourcing this script."
+    exit
+fi
 
+# This will setup all products on which framework depends.
+setup framework v1_0_0
 
-# On fnalu, 32 bit sl4.
-#source /afs/fnal.gov/files/code/mu2e/d3/mu2e_v0.SL4.i686/bin/setup_mu2e.sh
-
-
-# Working area
-source  bin/setup_mu2e_project.sh
-
-# Geant is not yet in the externals package; get it from ups.
-setup geant4 v4_9_2_p01 -q g77-OpenGL
-
-# various G4 cross-section data files
-
+# Chose version of G4 and its cross-section files.
+setup geant4 v4_9_3_p01 -q g77-OpenGL
 setup g4neutron   v3_13a
 setup g4emlow     v6_2
 setup g4photon    v2_0
@@ -35,5 +35,9 @@ setup g4abla      v3_0
 # Other products
 setup heppdt      v3_04_01
 
-# Build the symlink director for the BaBar code.
+# Working area
+source bin/setup_mu2e_project.sh
+
+# Build the symlink directories for the BaBar code.
 ./BaBar/makeInclude.csh
+
