22ccc684bd1492a1955ad251836320ceb73b3260
kutschke <>
2010-08-22 18:42:13 +0000

    Change to use RandomNumberGeneratorService.  Add some features to match other generators.

--- EventGenerator/inc/PiCapture.hh
+++ EventGenerator/inc/PiCapture.hh
@@ -1,30 +1,31 @@
 #ifndef PiCapture_HH
 #define PiCapture_HH
-
 //
 //
+// Generate photons from pi- capture on Al nuclei.
 // Based on Ivano Sarra's work described in Mu2e doc 665-v2
 // 
-// $Id: PiCapture.hh,v 1.5 2010/08/19 19:10:36 rhbob Exp $
-// $Author: rhbob $ 
-// $Date: 2010/08/19 19:10:36 $
+// $Id: PiCapture.hh,v 1.6 2010/08/22 18:42:13 kutschke Exp $
+// $Author: kutschke $ 
+// $Date: 2010/08/22 18:42:13 $
 //
 // Original author Rob Kutschke, P. Shanahan
 // 
 
-
+// Mu2e includes
 #include "EventGenerator/inc/GeneratorBase.hh"
 #include "Mu2eUtilities/inc/RandomUnitSphere.hh"
+
+// CLHEP includes
+#include "CLHEP/Random/RandFlat.h"
+#include "CLHEP/Random/RandPoissonQ.h"
 #include "CLHEP/Random/RandGeneral.h"
 
-
-
+// Forward declarations outside of namespace mu2e.
 class TH1D;
-
 namespace edm{
   class Run;
 }
-
 
 namespace mu2e {
 
@@ -41,24 +42,39 @@ namespace mu2e {
 
   private:
 
-    const double EPhotFunc(const double x);
+    // Start: parameters from run time configuration/
 
-    TH1D* _piCaptureEPhot;
-    TH1D* _piCaptureEPhotZ;
+    double _mean;         //< mean per event
+    double _elow;         //< lower photon energy 
+    double _ehi;          //< upper photon energy 
+    int    _nbins;        //< number of bins in photon energy pdf
+    bool   _doHistograms; // Enable/disable histograms.
 
-    RandomUnitSphere _randomUnitSphere;
-    std::auto_ptr<CLHEP::RandGeneral> _funcGen;
+    // End: parameters from run time configuration/
 
-    double _mean; //< mean per event
-    double _elow; //< lower photon energy 
-    double _ehi; //< upper photon energy 
-    double _bindE; //< energy bin width for generator
-    int _nbins; //< number of bins in photon energy pdf
+    // Random number distributions
+    CLHEP::RandFlat     _randFlat;
+    CLHEP::RandPoissonQ _randPoissonQ;
+    RandomUnitSphere    _randomUnitSphere;
+    CLHEP::RandGeneral  _spectrum;
+
+    // Histograms.
+    TH1D* _hMultiplicity;
+    TH1D* _hEPhot;
+    TH1D* _hEPhotZ;
+    TH1D* _hzPos;
+    TH1D* _hcz;
+    TH1D* _hphi;
+    TH1D* _ht;
+
+    // Photon energy spectrum as a continuous function.
+    const double energySpectrum(const double e);
+
+    // Compute a binned representation of the photon energy spectrum.
+    std::vector<double> PiCapture::binnedEnergySpectrum();
 
   };
 
 } // end namespace mu2e,
 
 #endif
-
-
--- EventGenerator/src/PiCapture.cc
+++ EventGenerator/src/PiCapture.cc
@@ -1,45 +1,39 @@
-
+//
+// Generate photons from pi- capture on Al nuclei.
 // Based on Ivano Sarra's model described in mu2e Doc 665-v2
 //
-// $Id: PiCapture.cc,v 1.9 2010/08/19 19:10:29 rhbob Exp $
-// $Author: rhbob $ 
-// $Date: 2010/08/19 19:10:29 $
+// $Id: PiCapture.cc,v 1.10 2010/08/22 18:42:13 kutschke Exp $
+// $Author: kutschke $ 
+// $Date: 2010/08/22 18:42:13 $
 //
 // Original author Rob Kutschke/P. Shanahan
 // 
 
 #include <iostream>
 
-
+// Framework includes
 #include "FWCore/Framework/interface/Run.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/Services/interface/TFileService.h"
 #include "FWCore/Framework/interface/TFileDirectory.h"
 
+// Mu2e includes
 #include "EventGenerator/inc/PiCapture.hh"
 #include "Mu2eUtilities/inc/SimpleConfig.hh"
 #include "Mu2eUtilities/inc/PDGCode.hh"
-#include "Mu2eUtilities/inc/safeSqrt.hh"
-#include "Mu2eUtilities/inc/sqrtOrThrow.hh"
-#include "GeometryService/inc/GeometryService.hh"
+//#include "GeometryService/inc/GeometryService.hh"
 #include "GeometryService/inc/GeomHandle.hh"
 #include "TargetGeom/inc/Target.hh"
 
-// From CLHEP
+// CLHEP includes
 #include "CLHEP/Random/RandFlat.h"
 #include "CLHEP/Random/RandPoisson.h"
 
-// From ROOT
+// ROOT includes
 #include "TH1D.h"
 
 using namespace std;
-
-using CLHEP::Hep3Vector;
-using CLHEP::HepLorentzVector;
-using CLHEP::RandFlat;
-using CLHEP::RandPoisson;
-
 
 namespace mu2e {
 
@@ -49,111 +43,158 @@ namespace mu2e {
   static const double emax = 138.2; // 
 
   PiCapture::PiCapture( edm::Run& run, const SimpleConfig& config ):
-    GeneratorBase(),
-    _piCaptureEPhot(0),
-    _randomUnitSphere(),
-    _funcGen(){
-
-    edm::LogInfo log("GENCONFIG");
-    _mean = config.getDouble("picapture.mean",0.);
-    log << "Pi capture mean: " << _mean << "\n";
-    _elow = config.getDouble("picapture.elow",38.2);
-    log << "Pi capture E Low: " << _elow << "\n";
-    _ehi = config.getDouble("picapture.ehi",emax);
-    log << "Pi capture E High: " << _ehi << "\n";
-    _nbins = config.getInt("picapture.nbins",1000);
-    log << "Pi capture N Bins: " << _nbins << "\n";
-
-    // set up the generator function
-    if (_nbins>0) _bindE = (_ehi - _elow) / _nbins;
-    else {
-      // I'm sure this isn't the right way to do this...
-      throw cms::Exception("RANGE") <<"Nonsense picapture.nbins requested="<<
-	_nbins<<"\n";
-    }
-
     
-    double YFunc[_nbins];
-    for (int ib=0; ib<_nbins; ib++) {
+    // Base class
+    GeneratorBase(),
 
-      double x = _elow+(ib+0.5) * _bindE;
-      YFunc[ib] = EPhotFunc(x);
+    // Parameters from run time configuration
+    _mean(config.getDouble("picapture.mean", -1.)),
+    _elow(config.getDouble("picapture.elow", 38.2)),
+    _ehi(config.getDouble("picapture.ehi",   emax)),
+    _nbins(config.getInt("picapture.nbins",  1000)),
+    _doHistograms(config.getBool("picapture.doHistograms",true)),
+
+    // Histograms
+    _hEPhot(0),
+    _hEPhotZ(0),
+
+    // Random number distributions; getEngine is found in the base class.
+    _randFlat( getEngine() ),
+    _randPoissonQ( getEngine(), std::abs(_mean) ),
+    _randomUnitSphere( getEngine() ),
+    _spectrum( getEngine(), &(binnedEnergySpectrum()[0]),_nbins){
+
+    // Sanity checks
+    if ( std::abs(_mean) > 99999. ) {
+      throw cms::Exception("RANGE") 
+        << "PiCapture has been asked to produce a crazily large number of photons ."
+        << _mean
+        << "\n";
     }
-    _funcGen = auto_ptr<CLHEP::RandGeneral>(new CLHEP::RandGeneral(YFunc,_nbins));
+    if (_nbins <= 0) {
+      throw cms::Exception("RANGE") 
+        << "Nonsense picapture.nbins requested="
+        << _nbins
+        << "\n";
+    }
 
     // Book histograms.
-    edm::Service<edm::TFileService> tfs;
-    _piCaptureEPhot = tfs->make<TH1D>( "piCaptureEPhot", "Pi Capture Photon Energy", 200, 0, 200);
-    _piCaptureEPhotZ = tfs->make<TH1D>( "piCaptureEPhotZ", "Pi Capture Photon Energy (zoom)", 200, _elow, _ehi);
+    if ( _doHistograms ){
+      edm::Service<edm::TFileService> tfs;
+      edm::TFileDirectory tfdir = tfs->mkdir( "PiCapture" );
 
-  }
+      _hMultiplicity = tfdir.make<TH1D>( "hMultiplicity", "DIO Multiplicity",    20,     0.,      20. );
+
+      _hEPhot  = tfdir.make<TH1D>( "hEPhot",  "PiCapture E(Photon)",              200,     0.,   200. );
+      _hEPhotZ = tfdir.make<TH1D>( "hEPhotZ", "PiCapture E(Photon)(zoom)",        200,  _elow,   _ehi );
+      _hzPos   = tfdir.make<TH1D>( "hzPos",   "PiCap z Position (Tracker Coord)", 200, -6600., -5600. );
+      _hcz     = tfdir.make<TH1D>( "hcz",     "PiCap cos(theta)",                 100,    -1.,     1. );
+      _hphi    = tfdir.make<TH1D>( "hphi",    "PiCapture azimuth",                100,  -M_PI,   M_PI );
+      _ht      = tfdir.make<TH1D>( "ht",     "PiCapture time ",                  100,      0,  2000. );
+
+    }
+
+  } // end PiCapture::PiCapture
 
   PiCapture::~PiCapture(){
   }
 
-
   void PiCapture::generate( ToyGenParticleCollection& genParticles ){
 
-    // return for absurdly high fixed rate
-    if (_mean<-99999) return;
+    // Choose the number of photons to generate this event.
+    long n = _mean < 0 ? static_cast<long>(-_mean): _randPoissonQ.fire();
+    if ( _doHistograms ) { 
+      _hMultiplicity->Fill(n); 
+    }
 
     // Get access to the geometry system.
     GeomHandle<Target> target;
-
     int nFoils = target->nFoils();
 
+    for ( long i=0; i<n; ++i ){
 
+      // Pick a foil.
+      int ifoil = static_cast<int>(nFoils*_randFlat.fire());
+      TargetFoil const& foil = target->foil(ifoil);
 
-    // Pick a foil.
-    int ifoil = static_cast<int>(nFoils*CLHEP::RandFlat::shoot());
-    TargetFoil const& foil = target->foil(ifoil);
+      // Foil properties.
+      CLHEP::Hep3Vector const& center = foil.center();
+      const double r1 = foil.rIn();
+      const double dr = foil.rOut() - r1;
 
-    // Foil properties.
-    CLHEP::Hep3Vector const& center = foil.center();
-    const double r1 = foil.rIn();
-    const double dr = foil.rOut() - r1;
+      // A random point within the foil.
+      const double r   = r1 + dr*_randFlat.fire();
+      const double dz  = 2.*(0.5-_randFlat.fire())*foil.halfThickness();
+      const double phi = 2.*M_PI*_randFlat.fire();
+      CLHEP::Hep3Vector pos( center.x()+r*cos(phi), 
+                             center.y()+r*sin(phi), 
+                             center.z()+dz );
+
+      // Pick a random photon energy from the spectrum.
+      const double e = _elow + _spectrum.fire() * ( _ehi - _elow );
+
+      // This should reflect the model of the tails of the beam
+      // and the out-of-time protons.
+      const double time = 1694*_randFlat.fire();
+
+      // Make the 4 vector.
+      CLHEP::HepLorentzVector mom( _randomUnitSphere.fire(e), e);
       
-    // A random point within the foil.
-    const double r   = r1 + dr*CLHEP::RandFlat::shoot();
-    const double dz  = 2.*(0.5-RandFlat::shoot())*foil.halfThickness();
-    const double phi = 2.*M_PI*CLHEP::RandFlat::shoot();
-    CLHEP::Hep3Vector pos( center.x()+r*cos(phi), 
-			   center.y()+r*sin(phi), 
-			   center.z()+dz );
+      // Add the photon to the list of generated particles.
+      genParticles.push_back( ToyGenParticle( PDGCode::gamma, GenId::pionCapture, pos, mom, time));
 
-    // Pick a random energy.  
-    const double e = _elow + _funcGen->shoot() * ( _ehi - _elow );
-    _piCaptureEPhot->Fill(e);
-    _piCaptureEPhotZ->Fill(e);
+      if ( _doHistograms ){
+        _hEPhot->Fill(e);
+        _hEPhotZ->Fill(e);
+        _hzPos->Fill(pos.z());
+        _hcz->Fill(mom.vect().cosTheta());
+        _hphi->Fill(mom.vect().phi());
+        _ht->Fill(time);
+      }
 
-    // Momentum 3 vector: uniform on a unit sphere.
-    CLHEP::Hep3Vector p = e*_randomUnitSphere.shoot();
-
-    // This should reflect the model of the tails of the beam
-    // and the out-of-time protons.
-    const double time = 1694*CLHEP::RandFlat::shoot();
-
-    // Make the 4 vector.
-    CLHEP::HepLorentzVector mom( p.x(), p.y(), p.z(), e);
-      
-    // Add the electron to  the list.
-    genParticles.push_back( ToyGenParticle( PDGCode::gamma, GenId::pionCapture, pos, mom, time));
+    } // end loop over photons to generate
 
   }
 
-  const double PiCapture::EPhotFunc(const double x)
+  // Photon energy spectrum as a continuous function.
+  const double PiCapture::energySpectrum(const double x)
   {
     // Parameters from doc 665-v2
-    static const double emax=138.2;
-    static const double alpha = 2.691;
-    static const double gamma = 1.051;
-    static const double tau = 8.043;
-    static const double c0 = 2.741;
-    static const double c1 = -0.005;
+    static const double emax  = 138.2;
+    static const double alpha =   2.691;
+    static const double gamma =   1.051;
+    static const double tau   =   8.043;
+    static const double c0    =   2.741;
+    static const double c1    =  -0.005;
 
     return pow(emax-x,alpha) * exp(-(emax-gamma*x)/tau) * (c0 + c1*x);
 
+  } // PiCapture::energySpectrum
 
-  }// PiCapture::EPhotFunc
+  // Compute a binned representation of the photon energy spectrum.
+  std::vector<double> PiCapture::binnedEnergySpectrum(){
 
-}
+    // Sanity check.
+    if (_nbins <= 0) {
+      throw cms::Exception("RANGE") 
+        << "Nonsense PiCaptureGun.nbins requested="
+        << _nbins
+        << "\n";
+    }
+
+    // Bin width.
+    double dE = (_ehi - _elow) / _nbins;
+
+    // Vector to hold the binned representation of the energy spectrum.
+    std::vector<double> spectrum;
+    spectrum.reserve(_nbins);
+    
+    for (int ib=0; ib<_nbins; ib++) {
+      double x = _elow+(ib+0.5) * dE;
+      spectrum.push_back(energySpectrum(x));
+    }
+
+    return spectrum;
+  } // PiCapture::binnedEnergySpectrum
+
+} // namespace mu2e
