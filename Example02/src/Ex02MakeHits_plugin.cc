709d6839285b500003cc8dcf802f80396ae67c3d
kutschke <>
2010-08-26 16:35:42 +0000

    Use RandomNumberGeneratorService.

--- Example02/src/Ex02MakeHits_plugin.cc
+++ Example02/src/Ex02MakeHits_plugin.cc
@@ -3,9 +3,9 @@
 A producer module that makes a collection of overly simplified "hits"
 and adds them to the event.
 
-$Id: Ex02MakeHits_plugin.cc,v 1.9 2010/08/26 16:20:15 kutschke Exp $
+$Id: Ex02MakeHits_plugin.cc,v 1.10 2010/08/26 16:35:42 kutschke Exp $
 $Author: kutschke $
-$Date: 2010/08/26 16:20:15 $
+$Date: 2010/08/26 16:35:42 $
    
 Original author Rob Kutschke
 
@@ -51,13 +51,22 @@ using namespace std;
 
 namespace mu2e {
 
+  static double const czmax = 0.5;
+
   class Ex02MakeHits : public edm::EDProducer {
     
   public:
     explicit Ex02MakeHits(edm::ParameterSet const& pSet) : 
-      minPulseHeight_(pSet.getParameter<double>("minPulseHeight"))
+      
+      // Information from the parameter set.
+      minPulseHeight_(pSet.getParameter<double>("minPulseHeight")),
+
+      // Random number distributions share one engine.
+      _randGauss( createEngine( get_seed_value(pSet)) ),
+      _angularDistribution( _randGauss.engine(), -czmax, czmax )
     {
       produces<ToyHitCollection>();
+
     }
     virtual ~Ex02MakeHits() { }
 
@@ -67,6 +76,9 @@ namespace mu2e {
 
     // Parameters from run time configuration.
     double minPulseHeight_;
+
+    CLHEP::RandGauss _randGauss;
+    RandomUnitSphere _angularDistribution;
 
   };
 
@@ -82,15 +94,10 @@ namespace mu2e {
     // Make the collection to hold the hits that we will make.
     auto_ptr<ToyHitCollection> p(new ToyHitCollection);
 
-    // Instantiate an object that can generate random vectors uniformly
-    // over a unit sphere, with constraints on cos(theta) and azimuth.
-    // These are static so it is only done once, on the first call to
-    // the function and is retained for all future calls.
-    static double const czmax = 0.5;
-    static RandomUnitSphere ranunit(*CLHEP::HepRandom::getTheEngine(), -czmax, czmax);
-
-    // Generate a unit vector in the direction of one track from the origin.
-    CLHEP::Hep3Vector vunit(ranunit.fire());
+    // Generate a unit vector, random over the unit sphere ( over a 
+    // restricted range of cos(theta).  This will be used as the 
+    // direction of the generated particle.
+    CLHEP::Hep3Vector vunit(_angularDistribution.fire());
 
     // Radius of each layer of the tracker.
     // Access by reference so there is no copying.
@@ -107,7 +114,7 @@ namespace mu2e {
       CLHEP::Hep3Vector intersectionPoint = s*vunit;
 
       // Generate a gaussian distributed pulse height.
-      double ph = CLHEP::RandGauss::shoot(10.,2.);
+      double ph = _randGauss.fire(10.,2.);
       
       // Keep only hits with large enough pulse height.
       if ( ph > minPulseHeight_ ){
