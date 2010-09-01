aace7a9fa7bea851ab201e20f96706f80a8c7216
genser <>
2010-09-01 20:25:33 +0000

    added specialization for the bool case

--- BFieldGeom/inc/Container3D.hh
+++ BFieldGeom/inc/Container3D.hh
@@ -5,9 +5,9 @@
 // A templated class to hold a collection of objects defined on a
 // 3D grid.
 //
-// $Id: Container3D.hh,v 1.3 2010/08/12 19:25:27 genser Exp $
+// $Id: Container3D.hh,v 1.4 2010/09/01 20:25:33 genser Exp $
 // $Author: genser $
-// $Date: 2010/08/12 19:25:27 $
+// $Date: 2010/09/01 20:25:33 $
 //
 
 #include <vector>
@@ -86,7 +86,7 @@ namespace mu2e {
     }
 
     // Check for a valid index
-    bool isValid(unsigned int ix, unsigned int iy, unsigned int iz){
+    bool isValid(unsigned int ix, unsigned int iy, unsigned int iz) const{
       if ( ix >= 0 && ix < _nx &&
            iy >= 0 && iy < _ny &&
            iz >= 0 && iz < _nz ) return true;
@@ -94,7 +94,7 @@ namespace mu2e {
     }
 
     // Throw if the point is not valid.
-    void isValidOrThrow( unsigned int ix, unsigned int iy, unsigned int iz){
+    void isValidOrThrow( unsigned int ix, unsigned int iy, unsigned int iz) const {
       if ( !isValid(ix, iy, iz) ){
         std::ostringstream os;
         os << "Invalid index into Container3D: " 
@@ -122,6 +122,122 @@ namespace mu2e {
     }
 
   };
+
+  template <>
+  class Container3D<bool>
+  {
+  public:
+
+    // Default constructor.
+    // Need this for ROOT IO.  But normally should never use it.
+    Container3D():
+      _nx(),
+      _ny(),
+      _nz(),
+      _vec(){
+    }
+
+    // Normal constructor.
+    Container3D( unsigned int nx, unsigned int ny, unsigned int nz):
+      _nx(nx),
+      _ny(ny),
+      _nz(nz),
+      _vec(_nx*_ny*_nz,false){
+    }
+
+    // "Normal" constructor for bool
+    Container3D( unsigned int nx, unsigned int ny, unsigned int nz, bool bv):
+      _nx(nx),
+      _ny(ny),
+      _nz(nz),
+      _vec(_nx*_ny*_nz,bv){
+    }
+
+    // Copy c'tor.
+    Container3D(const Container3D& rhs):
+      _nx(rhs._nx),
+      _ny(rhs._ny),
+      _nz(rhs._nz),
+      _vec(rhs._vec){
+    }
+
+    // Assignment operator.
+    Container3D& operator=(const Container3D& rhs){
+      _nx = rhs._nx;
+      _ny = rhs._ny;
+      _nz = rhs._nz;
+      _vec  = rhs._vec;
+    }
+
+    ~Container3D() { }
+
+    // Set element, without safety features.  Use if the calller has 
+    // already ensured the validity of the arguments.
+    void set(unsigned int ix, unsigned int iy, unsigned int iz, bool obj ){
+      _vec[index(ix,iy,iz)] = obj;
+    }
+
+    // Get element, without safety features. Use if the calller has 
+    // already ensured the validity of the arguments.
+    bool get( unsigned int ix, unsigned int iy, unsigned int iz) const {
+      return _vec[index(ix,iy,iz)];
+    }
+
+    // Synonym for get, without safety features.
+    bool operator()( unsigned int ix, unsigned int iy, unsigned int iz) const {
+      return _vec[index(ix,iy,iz)];
+    }
+
+    // Set, with safety features.
+    void setSafe(unsigned int ix, unsigned int iy, unsigned int iz, bool obj ){
+      isValidOrThrow(ix,iy,iz);
+      _vec.at(index(ix,iy,iz)) = obj;
+    }
+
+    // Get, with safety features.
+    bool getSafe( unsigned int ix, unsigned int iy, unsigned int iz) const {
+      isValidOrThrow(ix,iy,iz);
+      return _vec.at(index(ix,iy,iz));
+    }
+
+    // Check for a valid index
+    bool isValid(unsigned int ix, unsigned int iy, unsigned int iz) const {
+      if ( ix >= 0 && ix < _nx &&
+           iy >= 0 && iy < _ny &&
+           iz >= 0 && iz < _nz ) return true;
+      return false;
+    }
+
+    // Throw if the point is not valid.
+    void isValidOrThrow( unsigned int ix, unsigned int iy, unsigned int iz) const {
+      if ( !isValid(ix, iy, iz) ){
+        std::ostringstream os;
+        os << "Invalid index into Container3D: " 
+           << ix << " "
+           << iy << " "
+           << iz << " | Limits are: "
+           << _nx << " "
+           << _ny << " "
+           << _nz;          
+        throw std::domain_error(os.str());
+      }
+    }
+
+  private:
+
+    // Dimensions of the grid.
+    unsigned int _nx, _ny, _nz;
+
+    // Container to hold everything.
+    std::vector<bool> _vec;
+
+    // Compute the index into the array.
+    std::vector<bool>::size_type index(unsigned int ix, unsigned int iy, unsigned int iz) const {
+      return ix*_ny*_nz + iy*_nz + iz;
+    }
+
+  };
 }
+
 
 #endif
