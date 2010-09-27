f879507b7c0993cb2e95100ce232e86ce7bc2a97
kutschke <>
2010-09-27 19:43:08 +0000

    Add subdirectory Config/ to PYTHONPATH.

--- bin/setup_mu2e_project.sh
+++ bin/setup_mu2e_project.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 #
-# $Id: setup_mu2e_project.sh,v 1.2 2010/06/15 16:50:07 kutschke Exp $
+# $Id: setup_mu2e_project.sh,v 1.3 2010/09/27 19:43:08 kutschke Exp $
 # $Author: kutschke $
-# $Date: 2010/06/15 16:50:07 $
+# $Date: 2010/09/27 19:43:08 $
 #
 # Original author Rob Kutschke
 #
 # Initialize the current directory tree as a the root of a framework based project.
-#  - add the local lib area to the LD_LIBRARY_PATH
-# 
+#  - add the local lib subdirectory to the LD_LIBRARY_PATH
+#  - add the local Config subdirectory to the PYTHONPATH
 
 if [ "`basename $0 2>/dev/null`" = "setup_mu2e_project.sh" ];then
     echo "You should be sourcing this file"; exit
@@ -25,5 +25,6 @@ bin_dir=`dirname ${BASH_SOURCE}`   # assume file is in bin subdir
 bin_dir=`cd $bin_dir >/dev/null 2>&1 && echo $PWD`
 user_root=`dirname $bin_dir`
 add_to_var $user_root/lib  LD_LIBRARY_PATH
+add_to_var $user_root/Config PYTHONPATH
 
 unset bin_dir user_root
