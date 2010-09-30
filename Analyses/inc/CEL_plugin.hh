19442963661ef2aec2defde5d172887e73cd9b3d
rhbob <>
2010-09-30 16:40:48 +0000

    conversion electron primitive analysis to go with RPCs

---
+++ Analyses/inc/CEL_plugin.hh
@@ -0,0 +1,10 @@
+//
+// An EDProducer Module that checks conversion energy loss
+//
+// $Id: CEL_plugin.hh,v 1.1 2010/09/30 16:40:48 rhbob Exp $
+// $Author: rhbob $ 
+// $Date: 2010/09/30 16:40:48 $
+//
+// Original author R. Bernstein
+//
+// a dummy file just to make sure the /inc directory is created by CVS
