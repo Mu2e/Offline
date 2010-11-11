176ea2924939c14e6dc9106f93e697c7f5fb1979
genser <>
2010-11-11 21:16:53 +0000

    Id->id; remove RO related info and functions

--- ToyDP/inc/CaloCrystalHitMCTruth.hh
+++ ToyDP/inc/CaloCrystalHitMCTruth.hh
@@ -1,9 +1,9 @@
 #ifndef CaloCrystalHitMCTruth_H
 #define CaloCrystalHitMCTruth_H
 
-// $Id: CaloCrystalHitMCTruth.hh,v 1.2 2010/11/02 03:24:23 genser Exp $
+// $Id: CaloCrystalHitMCTruth.hh,v 1.3 2010/11/11 21:16:53 genser Exp $
 // $Author: genser $
-// $Date: 2010/11/02 03:24:23 $
+// $Date: 2010/11/11 21:16:53 $
 //
 // Original author KLG
 
@@ -27,45 +27,31 @@ namespace mu2e {
     CaloCrystalHitMCTruth():
       _crystalId(-1),
       _time(0.),
-      _energyDep(0.),
-      _energyDepTotal(0.),
-      _numberOfROIdsUsed(0),
-      _charge(0)
+      _energyDep(0.)
     {}
 
-    CaloCrystalHitMCTruth(int crystalId, edm::ProductID const & caloHitCollId, CaloHitMCTruth const & hit);
+    CaloCrystalHitMCTruth(int crystalId,double time, double energy):
+      _crystalId(crystalId),
+      _time(time),
+      _energyDep(energy)
+    {}
+
     // Accessors
 
-    int              Id() const { return _crystalId; }
+    int              id() const { return _crystalId; }
     float            time()      const { return _time; }
     float            energyDep() const { return _energyDep; }
-    float            energyDepTotal() const { return _energyDepTotal; }
-    bool             isCharged() const { return _charge>0; }
-    int              charge()    const { return _charge; }
-    int              numberOfROIdsUsed() const { return _numberOfROIdsUsed; }
-    std::vector<DPIndex> const & roIds() const { return _roIds; }
 
     // Accept compiler generated versions of d'tor, copy c'tor, assignment operator.
     
     // Print contents of the object.
     void print( std::ostream& ost = std::cout, bool doEndl = true ) const;
 
-    // almost operator += CaloHitMCTruth
-    CaloCrystalHitMCTruth& add(edm::ProductID const & caloHitCollId, CaloHitMCTruth const & hit);
-
-    // almost like one of the constructors, plays a role of a two
-    // argument assignment operator
-    void assign(int crystalId, edm::ProductID const & caloHitCollId, CaloHitMCTruth const & hit);
-
    private:
 
     int              _crystalId;
     float            _time;             // (ns)
     float            _energyDep;        // (MeV)
-    float            _energyDepTotal;     // (MeV)
-    int              _numberOfROIdsUsed;  // roIds used to calculate _energyDep
-    std::vector<DPIndex> _roIds;
-    int              _charge;
 
   };
 
--- ToyDP/src/CaloCrystalHitMCTruth.cc
+++ ToyDP/src/CaloCrystalHitMCTruth.cc
@@ -1,10 +1,9 @@
 // 
-// This is a place to put additional information produced by HitMaker,
-// like true drift distance, signal propagation time, etc.
+// Crystal hit info plus possible additional information produced by HitMaker
 //
-// $Id: CaloCrystalHitMCTruth.cc,v 1.2 2010/11/02 03:24:39 genser Exp $
+// $Id: CaloCrystalHitMCTruth.cc,v 1.3 2010/11/11 21:16:53 genser Exp $
 // $Author: genser $
-// $Date: 2010/11/02 03:24:39 $
+// $Date: 2010/11/11 21:16:53 $
 //
 
 // C++ includes
@@ -22,55 +21,13 @@ using namespace std;
 
 namespace mu2e {
 
-  CaloCrystalHitMCTruth::CaloCrystalHitMCTruth(int crystalId, edm::ProductID const & caloHitCollId, CaloHitMCTruth const & hit) :
-    _crystalId(crystalId),
-    _time(hit.time()),
-    _energyDep(hit.energyDep()),
-    _energyDepTotal(hit.energyDep()),
-    _numberOfROIdsUsed(1),
-    _charge(hit.charge())
-  {
-    _roIds.push_back(DPIndex(caloHitCollId,hit.roId()));
-  }
-
-  // operator += CaloHitMCTruth
-  CaloCrystalHitMCTruth& CaloCrystalHitMCTruth::add(edm::ProductID const & caloHitCollId, CaloHitMCTruth const & hit) {
-    _roIds.push_back(DPIndex(caloHitCollId,hit.roId()));
-    _energyDep += hit.energyDep();
-    _energyDepTotal += hit.energyDep();
-    ++_numberOfROIdsUsed;
-    _charge += hit.charge();
-    return *this;
-  }
-
-  // almost like one of the constructors, plays a role of a two
-  // argument assignment operator
-  void CaloCrystalHitMCTruth::assign(int crystalId, edm::ProductID const & caloHitCollId, CaloHitMCTruth const & hit) {
-    _crystalId = crystalId;
-    _time = hit.time();
-    _energyDep = hit.energyDep();
-    _energyDepTotal = hit.energyDep();
-    _roIds.clear();
-    _roIds.push_back(DPIndex(caloHitCollId,hit.roId()));
-    _numberOfROIdsUsed = 1;
-    _charge = hit.charge();
-    return;
-  }
-
   // Print the information found in this hit.
   void CaloCrystalHitMCTruth::print( ostream& ost, bool doEndl ) const {
 
     ost << "Calorimeter Crystal Hit MC:"
         << " crystal id: "  << _crystalId
         << " time "         << _time
-        << " energyDep: "  << _energyDep
-        << " energyDepT: "  << _energyDepTotal
-        << " used roids: "  << _numberOfROIdsUsed
-        << " crystal roids:";
-    for (size_t i=0; i!=_roIds.size(); ++i) {
-      ost  << " " << i << ": " <<_roIds[i];
-    }
-    ost << " charge: "      << _charge;
+        << " energyDep: "  << _energyDep;
  
     if ( doEndl ){
       ost << endl;
