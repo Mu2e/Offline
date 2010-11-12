f5bb809522b656100c0eb456aad8f4720154b86f
genser <>
2010-11-12 21:49:26 +0000

    introduced sort_functors.hh; rewrote creation of CaloCrystalHitMCTruthCollection

--- Mu2eG4/src/addCalorimeterHits.cc
+++ Mu2eG4/src/addCalorimeterHits.cc
@@ -1,9 +1,9 @@
 //
 // Populate output collections for calorimeter
 //
-// $Id: addCalorimeterHits.cc,v 1.3 2010/11/11 21:19:19 genser Exp $
+// $Id: addCalorimeterHits.cc,v 1.4 2010/11/12 21:49:26 genser Exp $
 // $Author: genser $
-// $Date: 2010/11/11 21:19:19 $
+// $Date: 2010/11/12 21:49:26 $
 //
 // Original author Ivan Logashenko
 //
@@ -16,6 +16,7 @@
 #include "GeometryService/inc/GeomHandle.hh"
 #include "CalorimeterGeom/inc/Calorimeter.hh"
 #include "Mu2eG4/inc/addCalorimeterHits.hh"
+#include "Mu2eUtilities/inc/sort_functors.hh"
 
 // G4 includes
 #include "G4Event.hh"
@@ -25,7 +26,7 @@ using namespace std;
 
 namespace mu2e{
 
-  // Utility class (structure) to hold calculated drift time for G4 hits
+  // Utility class (structure) to hold RO info for G4 hits
 
   class ROHit {
   public:
@@ -148,13 +149,6 @@ namespace mu2e{
 	  // Save current hit
 	  caloHits.push_back(       CaloHit(       roid,h_time,h_edepc+h_charged*addEdep));
 	  caloHitsMCTruth.push_back(CaloHitMCTruth(roid,h_time,h_edep,h_charged));
-
-          if (roid%nro==0 && h_charged==0) {
-            caloCrystalHitsMCTruth.push_back(CaloCrystalHitMCTruth(cg->getCrystalByRO(roid),
-                                                                   h_time,
-                                                                   h_edep*nro));
-          }
-
 	  // ...and create new hit	  
 	  h_time    = ro_hits[i]._time;
 	  h_edep    = ro_hits[i]._edep;
@@ -179,13 +173,72 @@ namespace mu2e{
 
       caloHits.push_back(       CaloHit(roid,h_time,h_edepc+h_charged*addEdep));
       caloHitsMCTruth.push_back(CaloHitMCTruth(roid,h_time,h_edep,h_charged));
-
-      if (roid%nro==0 && h_charged==0) {
-        caloCrystalHitsMCTruth.push_back(CaloCrystalHitMCTruth(cg->getCrystalByRO(roid),
-                                                               h_time,
-                                                               h_edep*nro));
-      }
       
+    }
+
+    // now CaloCrystalHitMCTruth
+    // Organize hits by crystal id; reject all but first RO; reject all RO hit directly
+
+    hitmap.clear();
+
+    for ( int i=0; i<nHits; ++i){
+      StepPointG4* h = (*hits)[i];
+      // reject all "non" first RO
+      if (h->volumeId()%nro==0) continue;
+      // reject all RO hit directly
+      if (h->eDep()<0.0) continue;
+      if (h->eDep()==0.0 && h->position()==CLHEP::Hep3Vector(0.0,0.0,0.0)) continue;
+      hitmap[cg->getCrystalByRO(h->volumeId())].push_back(i);
+    }
+
+    CaloCrystalHitMCTruthCollection cr_hits;
+
+    for(HitMap::const_iterator cr = hitmap.begin(); cr != hitmap.end(); ++cr) {
+
+      // crystal id
+      int cid = cr->first;
+
+      // Prepare info for hit creation before the next iteration
+      cr_hits.clear();
+
+      // Loop over all hits found for this crystal
+      vector<int> const& ihits = cr->second;
+
+      for( size_t i=0; i<ihits.size(); i++ ) {
+	StepPointG4* h = (*hits)[ihits[i]];
+	cr_hits.push_back(CaloCrystalHitMCTruth(cid,h->time(),h->eDep()));        
+      }
+
+      sort(cr_hits.begin(), cr_hits.end(), lessByTime<CaloCrystalHitMCTruthCollection::value_type>());
+
+      // now form final hits if they are close enough in time
+
+      CaloCrystalHitMCTruthCollection::value_type cHitMCTruth  = cr_hits[0];
+
+      for ( size_t i=1; i<cr_hits.size(); ++i ) {
+
+	if ( (cr_hits[i].time()-cr_hits[i-1].time()) > timeGap ) {
+
+	  // Save current hit
+
+          caloCrystalHitsMCTruth.push_back(cHitMCTruth);
+
+	  // ...and create new hit	  
+
+          cHitMCTruth  = cr_hits[i];
+
+	} else {
+
+	  // Add energy to the old hit (keep the "earlier" time)
+
+          cHitMCTruth.setEnergyDep(cHitMCTruth.energyDep()+cr_hits[i].energyDep());
+
+	}
+
+      }
+
+      caloCrystalHitsMCTruth.push_back(cHitMCTruth);
+
     }
 
   }
