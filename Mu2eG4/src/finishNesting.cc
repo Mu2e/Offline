483bed9ebdaf7ac9ae34a9417b5f662eee60e818
genser <>
2010-12-06 22:30:41 +0000

    added optional printout depending on the verbose flag.

--- Mu2eG4/src/finishNesting.cc
+++ Mu2eG4/src/finishNesting.cc
@@ -1,12 +1,16 @@
 //
 // Free function to be used by the nest... functions
 // 
-// $Id: finishNesting.cc,v 1.1 2010/12/02 17:44:32 genser Exp $
+// $Id: finishNesting.cc,v 1.2 2010/12/06 22:30:41 genser Exp $
 // $Author: genser $ 
-// $Date: 2010/12/02 17:44:32 $
+// $Date: 2010/12/06 22:30:41 $
 //
-// Original author KLG
+// Original author KLG based on nest... functions
 //
+
+// C++ includes; if using cout << *info.solid etc...
+#include <iostream>
+#include <iomanip>
 
 // Mu2e includes
 #include "Mu2eG4/inc/finishNesting.hh"
@@ -18,6 +22,12 @@
 #include "G4PVPlacement.hh"
 #include "G4VisAttributes.hh"
 
+// if using cout << *info.solid etc...
+#include "G4VSolid.hh"
+#include "G4RotationMatrix.hh"
+#include "G4ThreeVector.hh"
+
+using namespace std;
 
 namespace mu2e {
 
@@ -32,11 +42,78 @@ namespace mu2e {
                      bool const forceSolid,
                      bool const forceAuxEdgeVisible,
                      bool const placePV,
-                     bool const doSurfCheck
+                     bool const doSurfaceCheck,
+                     bool const verbose
                      ) {
 
     G4Helper    & _helper = *(edm::Service<G4Helper>());
     AntiLeakRegistry & reg = _helper.antiLeakRegistry();
+
+    // the code below if activated prints the parameters of the solid
+    // being placed 
+    if (verbose) {
+      ios::fmtflags oldfl = cout.flags();
+      int const oldpr = cout.precision();
+      int const oldwdth = cout.width();
+      static int const newpr   = 18;
+      static int const newwdth = 28;
+
+      cout.setf(ios::right,ios::adjustfield);
+      cout.precision(newpr);
+      cout.width(newwdth);
+
+      cout << "Placing " << info.name;
+      if (parent!=0) {
+        cout << " inside " << parent->GetName()<< endl;
+      } else {
+        cout << endl;
+      }
+      cout << scientific << setprecision(newpr) << setw(newwdth) << right << *info.solid << endl;
+      cout << "Offset " << offset << endl;
+      cout << "Rotation ";
+      if (rot != 0) {
+        rot->print(cout);
+        cout << endl;
+        cout << "Tolerance " << rot->getTolerance() << endl;
+        cout << "Accessing the rotation matrix " << endl;
+        int const vsize = rot->colX().SIZE;
+        G4ThreeVector colX(rot->colX());
+        G4ThreeVector colY(rot->colY());
+        G4ThreeVector colZ(rot->colZ());
+        for (int i=0;i!=vsize;++i) {
+          cout << " " << setprecision(newpr) << setw(newwdth) << right << colX[i] <<
+            " "<< setprecision(newpr) << setw(newwdth) << right << colY[i] <<
+            " "<< setprecision(newpr) << setw(newwdth) << right << colZ[i] << endl;
+        }
+
+//         // adjusting very small matrix elements
+//         static double const smallValue = 1.e-9;
+//         bool adjustedr = false;
+//         for (int i=0;i!=vsize;++i) {
+//           if(abs(colX[i]) < smallValue ) {
+//             colX[i] = 0.0;
+//             adjustedr = true;
+//           }
+//           if(abs(colY[i]) < smallValue ) {
+//             colY[i] = 0.0;
+//             adjustedr = true;
+//           }
+//           if(abs(colZ[i]) < smallValue ) {
+//             colZ[i] = 0.0;
+//             adjustedr = true;
+//           }
+//         }
+//         if (adjustedr) {
+//           rot->set(colX,colY,colZ);
+//           rot->print(cout);
+//         }
+      } else { 
+        cout << endl << 0 << endl;
+      }
+      cout << setprecision(oldpr) << setw(oldwdth);
+      cout.flags(oldfl);
+
+    }
 
     info.logical  = new G4LogicalVolume( info.solid, material, info.name); 
 
@@ -48,9 +125,12 @@ namespace mu2e {
                                          parent, 
                                          0, 
                                          copyNo, 
-                                         doSurfCheck)
+                                         doSurfaceCheck)
       :
       0;
+
+    // uncomment for a more thorrow overlap check
+    // doSurfaceCheck && info.physical!=0 && info.physical->CheckOverlaps(100000,0.0,false);
 
 
     if (!isVisible) {
