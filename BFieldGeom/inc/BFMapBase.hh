c8af01cd3cf5b0476ba022199152dce2b7ea1820
kutschke <>
2011-01-28 23:51:59 +0000

    Make d'tor virtual.

--- BFieldGeom/inc/BFMapBase.hh
+++ BFieldGeom/inc/BFMapBase.hh
@@ -3,6 +3,11 @@
 //
 // Interface to the magnetic field maps. Used by BFMap and BFMapSet.
 //
+// $Id: BFMapBase.hh,v 1.2 2011/01/28 23:51:59 kutschke Exp $
+// $Author: kutschke $
+// $Date: 2011/01/28 23:51:59 $
+//
+
 #include <string>
 #include "CLHEP/Vector/ThreeVector.h"
 
@@ -11,6 +16,7 @@ namespace mu2e {
   class BFMapBase {
 
   public:
+    virtual ~BFMapBase();
 
     virtual CLHEP::Hep3Vector getBField(CLHEP::Hep3Vector const&) const = 0;
     virtual const std::string& getKey() const = 0;
