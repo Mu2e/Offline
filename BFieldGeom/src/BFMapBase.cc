e36774c8667529e641a68e2d088854508f2591f7
kutschke <>
2011-01-28 23:52:01 +0000

    First release.

---
+++ BFieldGeom/src/BFMapBase.cc
@@ -0,0 +1,15 @@
+//
+// Interface to the magnetic field maps. Used by BFMap and BFMapSet.
+//
+// $Id: BFMapBase.cc,v 1.1 2011/01/28 23:52:01 kutschke Exp $
+// $Author: kutschke $
+// $Date: 2011/01/28 23:52:01 $
+//
+
+#include "BFieldGeom/inc/BFMapBase.hh"
+
+namespace mu2e { 
+
+  BFMapBase::~BFMapBase(){}
+
+} // end namespace mu2e
