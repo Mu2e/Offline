c5567b67cc06314b7f529f169f36862026d8d375
ehrlich <>
2011-02-21 23:11:43 +0000

    simplified the test script

--- EventDisplay/test/EventDisplay.py
+++ EventDisplay/test/EventDisplay.py
@@ -2,8 +2,6 @@ import FWCore.ParameterSet.python.Config as mu2e
 
 process = mu2e.Process("EventDisplay")
 
-#process.maxEvents = mu2e.untracked.PSet(input = mu2e.untracked.int32(100))
-
 process.load("Config/MessageLogger_cfi")
 
 process.GeometryService = mu2e.Service("GeometryService",
@@ -12,7 +10,7 @@ process.GeometryService = mu2e.Service("GeometryService",
 process.source = mu2e.Source("PoolSource",
                  fileNames = mu2e.untracked.vstring("/mu2e/data/outstage/wasiko/56545/56545_8/data_cosmic.root"))
 
-process.root1 = mu2e.EDAnalyzer("EventDisplay", g4ModuleLabel = mu2e.string("g4run"))
+process.eventdisplay = mu2e.EDAnalyzer("EventDisplay")
 
-process.output = mu2e.EndPath(process.root1);
+process.output = mu2e.EndPath(process.eventdisplay);
 
