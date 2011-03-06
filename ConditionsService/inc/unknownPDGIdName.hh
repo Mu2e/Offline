72e88e4cc41d27a6de5e07c38c78d2f20d89e816
kutschke <>
2011-03-06 00:31:43 +0000

    First release.

---
+++ ConditionsService/inc/unknownPDGIdName.hh
@@ -0,0 +1,21 @@
+#ifndef unknownPDGIdName_HH
+#define unknownPDGIdName_HH
+//
+// Return a string identifying the name of a particle with an unknown PDGID.
+//
+// $Id: unknownPDGIdName.hh,v 1.1 2011/03/06 00:31:43 kutschke Exp $
+// $Author: kutschke $
+// $Date: 2011/03/06 00:31:43 $
+//
+// Original author Rob Kutschke
+//
+
+#include <string>
+
+namespace mu2e {
+
+  std::string unknownPDGIdName( unsigned int id);
+
+}
+
+#endif
