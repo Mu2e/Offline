1d3132af098f97efeb09e84cf4f0678a12de8035
ayarritu <>
2011-03-08 14:12:51 +0000

    Handles info about SimParticle

---
+++ Mu2eUtilities/src/SimParticleInfo.cc
@@ -0,0 +1,106 @@
+//
+// Integrated access to all information about a StrawHit that was 
+// created by a SimParticle.   This class is a building block of 
+// the SimParticlesWithHits class. If a StrawHit contains contributions
+// from two SimParticles, then there will usually be one two StrawHitMCInfo
+// objects, one attached to each SimParticle. 
+//
+// $Id: SimParticleInfo.cc,v 1.1 2011/03/08 14:12:51 ayarritu Exp $
+// $Author: ayarritu $
+// $Date: 2011/03/08 14:12:51 $
+//
+// See the notes in the header file for the meaning of the member datum _time.
+// 
+
+
+// Framework includes
+#include "FWCore/Framework/interface/Event.h"
+
+// Mu2e includes
+#include "Mu2eUtilities/inc/StrawHitMCInfo.hh"
+#include "Mu2eUtilities/inc/resolveDPIndices.hh"
+#include "ToyDP/inc/StepPointMCCollection.hh"
+#include "ToyDP/inc/DPIndexVector.hh"
+#include "ToyDP/inc/StepPointMC.hh"
+#include "Mu2eUtilities/inc/SimParticleInfo.hh"
+
+// C++ includes.
+#include <cfloat>
+
+namespace mu2e {
+
+    SimParticleInfo::SimParticleInfo( 
+            key_type simId,
+            SimParticle const& simParticle,
+            const edm::Event& evt):
+     _simId(simId),
+     _simParticle(&simParticle),
+     _event(&evt),
+     _hitInfos(),
+     _firstInTracker(0),
+     _lastInTracker(0){}
+
+      
+    // calculate the first StepPointMC in track 
+    StepPointMC const& SimParticleInfo::firstStepPointMCinTracker() const {
+
+        if ( _firstInTracker )  return *_firstInTracker;  
+        else if ( _hitInfos.size() == 0 )  {
+          throw cms::Exception("HITS") 
+            << "_hitInfos is empty at " << __func__ << "\n";
+        }; 
+
+        double currentFirstTime = DBL_MAX;
+        StepPointMC const* currentStepPoint = 0;
+        
+        // Straws are already ordered by earliest time of StepMCPoint 
+        std::vector<StepPointMC const *> const& steps = _hitInfos.front().steps();
+                
+         // loop over all StepPointMC objects
+         for ( std::vector<StepPointMC const *>::const_iterator stepsit = steps.begin(); stepsit!=steps.end(); ++stepsit){
+             
+             if ( (*stepsit)->trackId() == _simId ) {             
+                    double time = (*stepsit)->time();
+                    if ( time < currentFirstTime ) { 
+                       currentFirstTime = time; 
+                       currentStepPoint = *stepsit;
+                } 
+             }       
+         }      
+         _firstInTracker =  currentStepPoint; 
+         return *_firstInTracker; 
+    }
+
+    // calculate the last StepPointMC in track 
+    StepPointMC const& SimParticleInfo::lastStepPointMCinTracker() const {
+
+        if ( _lastInTracker )  return *_lastInTracker; 
+        else if ( _hitInfos.size() == 0 ) {
+          throw cms::Exception("HITS") 
+            << "_hitInfos is empty at " << __func__ << "\n";
+        }
+
+        double currentLastTime = -1;
+        StepPointMC const* currentStepPoint = 0;
+        
+            // loop over all straws 
+            for ( std::vector<StrawHitMCInfo>::const_iterator strawsit = _hitInfos.begin(); strawsit!= _hitInfos.end(); ++strawsit ) {
+                 std::vector<StepPointMC const *> const& steps = strawsit->steps();
+                 
+                 // loop over all StepPointMC objects
+                 for ( std::vector<StepPointMC const *>::const_iterator stepsit = steps.begin(); stepsit!=steps.end(); ++stepsit){
+                    
+                     if ( (*stepsit)->trackId() == _simId ) {             
+                         double time = (*stepsit)->time();
+                         if ( time > currentLastTime ) { 
+                           currentLastTime = time; 
+                           currentStepPoint = *stepsit;
+                         } 
+                     }       
+                  }      
+              } 
+       _lastInTracker = currentStepPoint; 
+       return *_lastInTracker; 
+    }
+
+} // end of namespace
