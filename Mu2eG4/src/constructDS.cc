27f574c91726767a1e3061ec75f6cd982614fa0f
genser <>
2011-04-29 17:43:06 +0000

    reimplemented DS3Vacuum as a subtraction solid to limit the magnetic field to the MBS area only and to be able to make the MBS steel opening smaller and place the elements external to MBS in the air; added conditional diagnostic printout

--- Mu2eG4/src/constructDS.cc
+++ Mu2eG4/src/constructDS.cc
@@ -1,9 +1,9 @@
 //
 // Free function to create  DS. (Detector Solenoid)
 //
-// $Id: constructDS.cc,v 1.1 2011/01/05 21:04:47 genser Exp $
+// $Id: constructDS.cc,v 1.2 2011/04/29 17:43:06 genser Exp $
 // $Author: genser $
-// $Date: 2011/01/05 21:04:47 $
+// $Date: 2011/04/29 17:43:06 $
 //
 // Original author KLG based on Mu2eWorld constructDS
 //
@@ -24,12 +24,14 @@
 #include "Mu2eG4/inc/MaterialFinder.hh"
 #include "Mu2eG4/inc/constructDS.hh"
 #include "Mu2eG4/inc/nestTubs.hh"
+#include "Mu2eG4/inc/finishNesting.hh"
 
 // G4 includes
 #include "G4ThreeVector.hh"
 #include "G4Material.hh"
 #include "G4Color.hh"
-
+#include "G4Tubs.hh"
+#include "G4SubtractionSolid.hh"
 
 using namespace std;
 
@@ -38,6 +40,8 @@ namespace mu2e {
   void constructDS( const VolumeInfo& parent, 
                     SimpleConfig const * const _config
                     ){
+
+    int const verbosityLevel = _config->getInt("toyDS.verbosityLevel",0);
 
     // Extract information from the config file.
     TubsParams detSolCoilParams( _config->getDouble("toyDS.rIn"),
@@ -71,20 +75,72 @@ namespace mu2e {
                              detSolCoilParams.innerRadius,
                              ds2HalfLength);
 
-    TubsParams ds3VacParams( 0.,
-                             detSolCoilParams.innerRadius,
-                             ds3HalfLength);
-
     // Compute positions of objects in Mu2e coordinates.
     double dsFrontZ0 = rTorus + 2.*ts5HalfLength - 2.*ds1HalfLength - dsFrontHalfLength;
     double ds1Z0     = rTorus + 2.*ts5HalfLength - ds1HalfLength;
     double ds2Z0     = rTorus + 2.*ts5HalfLength + ds2HalfLength;
-    double ds3Z0     = ds2Z0  + ds2HalfLength    + ds3HalfLength;
+
+    // MBS
+
+    // if MBS is there the vacuum is longer, we we add a minimal
+    // length even when MBS is not there to have a legal subtraction
+    // volume since other components (e.g. neutron absorber) depend on
+    // it now
+
+    double ds3OriginalHalfLength = ds3HalfLength;
+    double ds3OriginalZ0         = ds2Z0  + ds2HalfLength  + ds3HalfLength;
+
+    double BSTSHLength = ds3HalfLength + 1.;
+    double BSTSZ       = ds3OriginalZ0 + 1.;
+    if ( _config->getBool("hasMBS",false) ) {
+      BSTSHLength = _config->getDouble("mbs.BSTSHLength");
+      BSTSZ       = _config->getDouble("mbs.BSTSZ");
+    }
+
+    ds3HalfLength = ( -ds2Z0 - ds2HalfLength + BSTSZ + BSTSHLength)*0.5;
+
+    double ds3Z0         = ds2Z0  + ds2HalfLength    + ds3HalfLength;
+
+    // ds3 is a subtraction solid accommodating the MBS
+
+    double ds3SubtrZ0         = ( ds3OriginalZ0  + ds3OriginalHalfLength + ds3Z0 + ds3HalfLength)*0.5;
+    double ds3SubtrHalfLength = (-ds3OriginalZ0  - ds3OriginalHalfLength + ds3Z0 + ds3HalfLength)*0.5;
+
+    TubsParams ds3VacParams( 0.,
+                             detSolCoilParams.innerRadius,
+                             ds3HalfLength);
+
+    double SPBSOuterRadius   = _config->getBool("hasMBS",false) ? 
+      _config->getDouble("mbs.SPBSOuterRadius") : 0.;
+    TubsParams ds3VacSubtrParams( SPBSOuterRadius,
+                                  detSolCoilParams.innerRadius,
+                                  ds3SubtrHalfLength);
+
     G4ThreeVector detSolCoilPosition(-solenoidOffset, 0., dsCoilZ0);
     G4ThreeVector    dsFrontPosition(-solenoidOffset, 0., dsFrontZ0);
     G4ThreeVector        ds1Position(-solenoidOffset, 0., ds1Z0);
     G4ThreeVector        ds2Position(-solenoidOffset, 0., ds2Z0);
     G4ThreeVector        ds3Position(-solenoidOffset, 0., ds3Z0);
+    G4ThreeVector   ds3SubtrPosition(-solenoidOffset, 0., ds3SubtrZ0);
+
+
+    if ( verbosityLevel > 0) {
+      double zhl  =  ds3HalfLength;
+      cout << __func__ << " ds3Vac Original Z Offset in Mu2e  : " << ds3OriginalZ0 << endl;
+      cout << __func__ << " ds3OriginalHalfLength             : " << ds3OriginalHalfLength << endl;
+      cout << __func__ << " ds3Vac Z Offset in Mu2e           : " << ds3Z0 << endl;
+      cout << __func__ << " ds3HalfLength                     : " << zhl << endl;
+      cout << __func__ << " ds3Vac Z extent in Mu2e           : " << 
+        ds3Z0 - zhl << ", " << ds3Z0 + zhl << endl;
+    }
+
+    if ( verbosityLevel > 0) {
+      double zhl  =  ds3SubtrHalfLength;
+      cout << __func__ << " ds3Subtr Z Offset in Mu2e         : " << ds3SubtrZ0 << endl;
+      cout << __func__ << " ds3SubtrHalfLength                : " << zhl << endl;
+      cout << __func__ << " ds3Subtr Z extent in Mu2e         : " << 
+        ds3SubtrZ0 - zhl << ", " << ds3SubtrZ0 + zhl << endl;
+    }
 
     MaterialFinder materialFinder(*_config);
     G4Material* detSolCoilMaterial = materialFinder.get("toyDS.materialName");
@@ -162,20 +218,46 @@ namespace mu2e {
                                           doSurfaceCheck
                                           );
 
-    VolumeInfo ds3VacInfo     = nestTubs( "ToyDS3Vacuum",
-                                          ds3VacParams,
-                                          vacuumMaterial,
-                                          0,
-                                          ds3Position-_hallOriginInMu2e,
-                                          parent,
-                                          0,
-                                          toyDSVisible,
-                                          G4Color::Blue(),
-                                          toyDSSolid,
-                                          forceAuxEdgeVisible,
-                                          placePV,
-                                          doSurfaceCheck
-                                          );
+    // downstream vacuum part also enclosing the Muon Beam Stop
+    // it is a subtraction solid with the "subtraction tub" placed downstream
+
+    VolumeInfo ds3VacInfo;
+    ds3VacInfo.name = "ToyDS3Vacuum";
+
+    G4Tubs* ds3VacSolid = new G4Tubs(ds3VacInfo.name+"Full",
+                                     ds3VacParams.innerRadius,
+                                     ds3VacParams.outerRadius,
+                                     ds3VacParams.zHalfLength,
+                                     ds3VacParams.phi0, 
+                                     ds3VacParams.phiMax);
+
+    G4Tubs* ds3VacSubtrSolid = new G4Tubs(ds3VacInfo.name+"Subtr",
+                                          ds3VacSubtrParams.innerRadius,
+                                          ds3VacSubtrParams.outerRadius,
+                                          ds3VacSubtrParams.zHalfLength,
+                                          ds3VacSubtrParams.phi0, 
+                                          ds3VacSubtrParams.phiMax);
+
+    ds3VacInfo.solid = new G4SubtractionSolid(ds3VacInfo.name, 
+                                              ds3VacSolid, ds3VacSubtrSolid,0,ds3SubtrPosition-ds3Position);
+    
+    ds3VacInfo.centerInParent = ds3Position-_hallOriginInMu2e;
+    ds3VacInfo.centerInWorld  = ds3VacInfo.centerInParent + parent.centerInWorld;
+    // the above should probably be done by finishNesting
+
+    finishNesting(ds3VacInfo,
+                  vacuumMaterial,
+                  0,
+                  ds3VacInfo.centerInParent,
+                  parent.logical,
+                  0,
+                  toyDSVisible,
+                  G4Color::Blue(),
+                  toyDSSolid,
+                  forceAuxEdgeVisible,
+                  placePV,
+                  doSurfaceCheck
+                  );
 
   } // end of Mu2eWorld::constructDS;
 
