637991b46076d5a1455c60201ae0457f66ec7827
ignatov <>
2011-05-12 16:17:12 +0000

    comment checking of consistence of itracker with mother volume dimensions: DS volume was changed from simple tube to more complicated boolean solid.

--- Mu2eG4/src/ITrackerBuilder.cc
+++ Mu2eG4/src/ITrackerBuilder.cc
@@ -52,9 +52,11 @@ VolumeInfo ITrackerBuilder::constructTracker( G4LogicalVolume* mother, double zO
         double z0    = CLHEP::mm * itracker->z0();
         G4ThreeVector trackerOffset(0.,0.,z0-zOff);
 
-        if ( itracker->rOut() >= ((G4Tubs *)mother->GetSolid())->GetOuterRadius() )
-                throw cms::Exception("GEOM") <<"The ITracker doesn't fit inside the DS, please check the external radius of the ITracker\n";
-
+	/*	if ( itracker->rOut() >= ((G4Tubs *)mother->GetSolid())->GetOuterRadius() )
+	  throw cms::Exception("GEOM") <<"The ITracker doesn't fit inside the DS, please check the external radius of the ITracker\n"
+				       <<"rout ITracker="<<itracker->rOut()<<" DS outer radius="<<((G4Tubs *)mother->GetSolid())->GetOuterRadius()<<"\n"
+				       <<(*(G4Tubs *)mother->GetSolid());
+	*/
         if (itracker->isExternal()) {
                 throw cms::Exception("GEOM") <<"This GDML file option is temporarily disabled\n";
                 //            G4GDMLParser parser;
