2071bf0744e8cce0e6beaa81a6acb0d64a7df13a
kutschke <>
2011-05-18 04:37:17 +0000

    Use the Mu2e version of FileInPath; fix EDMException to Exception in header file name.

--- Mu2eUtilities/src/SimpleConfig.cc
+++ Mu2eUtilities/src/SimpleConfig.cc
@@ -2,9 +2,9 @@
  *
  * Main class in a primitive runtime parameter utility.
  *
- * $Id: SimpleConfig.cc,v 1.12 2011/05/18 02:27:19 wb Exp $
- * $Author: wb $
- * $Date: 2011/05/18 02:27:19 $
+ * $Id: SimpleConfig.cc,v 1.13 2011/05/18 04:37:17 kutschke Exp $
+ * $Author: kutschke $
+ * $Date: 2011/05/18 04:37:17 $
  *
  * Original author Rob Kutschke
  *
@@ -26,15 +26,14 @@
 
 // Framework includes
 #include "messagefacility/MessageLogger/MessageLogger.h"
-#include "art/ParameterSet/FileInPath.h"
-#include "art/Utilities/EDMException.h"
+#include "art/Utilities/Exception.h"
 #include "cetlib/exception.h"
-#include "cetlib/search_path.h"
 
 // Mu2e includes
 #include "Mu2eUtilities/inc/SimpleConfig.hh"
 #include "Mu2eUtilities/inc/TrimInPlace.hh"
 #include "Mu2eUtilities/src/SimpleConfigRecord.hh"
+#include "Mu2eUtilities/inc/FileInPath.hh"
 
 using namespace std;
 
@@ -55,10 +54,9 @@ namespace mu2e {
     _allowReplacement(allowReplacement),
     _messageOnReplacement(messageOnReplacement){
 
-    cet::search_path sp("MU2E_SEARCH_PATH");
-    if( ! sp.find_file(filename, _inputfile) )
-      throw "SimpleConfig c'tor: find_file failure!";  // TODO: improve exception
-
+    // FileInPath throws if the file cannot be found in the path.
+    FileInPath fip(filename);
+    _inputfile = fip.fullPath();
     ReadFile();
   }
 
