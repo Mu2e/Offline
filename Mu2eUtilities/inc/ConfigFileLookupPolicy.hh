a409a3c65d089138dbe0060cc02cdef374335737
greenc <>
2011-05-19 16:33:34 +0000

    ConfigFileLookupPolicy is ready for use.

--- Mu2eUtilities/inc/ConfigFileLookupPolicy.hh
+++ Mu2eUtilities/inc/ConfigFileLookupPolicy.hh
@@ -12,19 +12,30 @@ namespace mu2e {
 class mu2e::ConfigFileLookupPolicy :
   public cet::filepath_maker {
 public:
-  ConfigFileLookupPolicy();
   virtual std::string operator() (std::string const &filename);
   virtual ~ConfigFileLookupPolicy() {}
 
 private:
-  cet::search_path path_;
+  inline cet::search_path const &path() const;
 };
+
+inline
+cet::search_path const &
+mu2e::ConfigFileLookupPolicy::
+path() const {
+  // path_s is function-local-static rather than class-scope so that all
+  // instances within an execution have the same search path even if
+  // MU2E_SEARCH_PATH gets tweaked within a job -- unlikely, but
+  // possible.
+  static cet::search_path const path_s("MU2E_SEARCH_PATH");
+  return path_s;
+}
 
 inline
 std::string
 mu2e::ConfigFileLookupPolicy::
 operator()(std::string const &filename) {
-  return path_.find_file(filename);
+  return path().find_file(filename);
 }
 
 #endif /* Mu2eUtilities_ConfigFileLookupPolicy_hh */
--- Mu2eUtilities/src/ConfigFileLookupPolicy.cc
+++
@@ -1,7 +0,0 @@
-#include "Mu2eUtilities/inc/ConfigFileLookupPolicy.hh"
-
-mu2e::ConfigFileLookupPolicy::
-ConfigFileLookupPolicy()
-  :
-  path_("") // FIXME: expand environmane variables into here.
-{}
