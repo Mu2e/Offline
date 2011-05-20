162a75f461a12f546e361f9e052657ba407d1347
greenc <>
2011-05-20 17:58:00 +0000

    Consolidate service implementations into one file per service.

--- ConditionsService/src/ConditionsService_service.cc
+++ ConditionsService/src/ConditionsService_service.cc
@@ -1,15 +1,93 @@
 //
-// Plugin for the Conditions Service.
+// Primitive conditions data service.
+// It does not yet do validty checking.
 //
-// $Id: ConditionsService_service.cc,v 1.2 2011/05/18 02:27:15 wb Exp $
-// $Author: wb $
-// $Date: 2011/05/18 02:27:15 $
+// $Id: ConditionsService_service.cc,v 1.3 2011/05/20 17:58:00 greenc Exp $
+// $Author: greenc $
+// $Date: 2011/05/20 17:58:00 $
 //
 // Original author Rob Kutschke
 //
 
+// C++ include files
+#include <iostream>
+#include <typeinfo>
+
+// Framework include files
+//#include "art/Persistency/Provenance/ModuleDescription.h"
+//#include "art/Persistency/Provenance/EventID.h"
+//#include "art/Persistency/Provenance/Timestamp.h"
+//#include "art/Persistency/Provenance/SubRunID.h"
+//#include "art/Persistency/Provenance/RunID.h"
 #include "art/Framework/Services/Registry/ServiceMacros.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+
+// Mu2e include files
 #include "ConditionsService/inc/ConditionsService.hh"
 
-using mu2e::ConditionsService;
-DEFINE_ART_SERVICE(ConditionsService);
+// Calibration entities.
+// Would like to break the coupling to these.
+#include "ConditionsService/inc/AcceleratorParams.hh"
+#include "ConditionsService/inc/DAQParams.hh"
+#include "ConditionsService/inc/ParticleDataTable.hh"
+#include "ConditionsService/inc/PhysicsParams.hh"
+#include "ConditionsService/inc/TrackerCalibrations.hh"
+
+using namespace std;
+
+namespace mu2e {
+
+  ConditionsService::ConditionsService(fhicl::ParameterSet const& iPS,
+                                       art::ActivityRegistry&iRegistry) :
+    _conditionsFile(iPS.get<std::string>("conditionsfile","conditions.txt")),
+    _config(_conditionsFile),
+    _entities(),
+    _run_count()
+  {
+    iRegistry.watchPreBeginRun(this, &ConditionsService::preBeginRun);
+  }
+
+
+  ConditionsService::~ConditionsService(){
+  }
+
+  void
+  ConditionsService::preBeginRun(art::Run const &) {
+
+    if(++_run_count > 1) {
+      mf::LogWarning("CONDITIONS") << "This test version does not change geometry on run boundaries.";
+      return;
+    }
+
+    mf::LogInfo log("CONDITIONS");
+    log << "Conditions input file is: " << _conditionsFile << "\n";
+
+    if ( _config.getBool("printConfig",false) ){
+      log << "\n" << _config;
+    }
+
+    if ( _config.getBool("printConfigStats",false) ){
+      // Work around absence of << operator for this print method.
+      ostringstream os;
+      _config.printStatistics(os);
+      log << os.str();
+    }
+
+    checkConsistency();
+
+    // Can we break the coupling to the entities?
+    addEntity( std::auto_ptr<ParticleDataTable>  ( new ParticleDataTable  (_config)) );
+    addEntity( std::auto_ptr<AcceleratorParams>  ( new AcceleratorParams  (_config)) );
+    addEntity( std::auto_ptr<DAQParams>          ( new DAQParams          (_config)) );
+    addEntity( std::auto_ptr<PhysicsParams>      ( new PhysicsParams      (_config)) );
+    addEntity( std::auto_ptr<TrackerCalibrations>( new TrackerCalibrations(_config)) );
+  }
+
+  // Check that the configuration is self consistent.
+  void ConditionsService::checkConsistency(){
+  }
+
+} // end namespace mu2e
+
+DEFINE_ART_SERVICE(mu2e::ConditionsService);
