8e4951ba322532cf9a0ec9c4409da22b3dc60014
genser <>
2011-05-26 22:09:53 +0000

    added detectors 11 & 12 in the middle of the ttracker; added config file controlled debug printout

--- Mu2eG4/src/constructVirtualDetectors.cc
+++ Mu2eG4/src/constructVirtualDetectors.cc
@@ -1,9 +1,9 @@
 //
 // Free function to create the virtual detectors
 //
-// $Id: constructVirtualDetectors.cc,v 1.5 2011/05/18 14:21:44 greenc Exp $
-// $Author: greenc $
-// $Date: 2011/05/18 14:21:44 $
+// $Id: constructVirtualDetectors.cc,v 1.6 2011/05/26 22:09:53 genser Exp $
+// $Author: genser $
+// $Date: 2011/05/26 22:09:53 $
 //
 // Original author KLG based on Mu2eWorld constructVirtualDetectors
 //
@@ -25,6 +25,7 @@
 #include "Mu2eG4/inc/VirtualDetectorSD.hh"
 #include "Mu2eG4/inc/nestTubs.hh"
 #include "VirtualDetectorGeom/inc/VirtualDetector.hh"
+#include "TTrackerGeom/inc/TTracker.hh"
 
 // G4 includes
 
@@ -43,6 +44,8 @@ namespace mu2e {
   void constructVirtualDetectors( SimpleConfig const * const _config ){
 
     // Place virtual detectors
+
+    int static const verbosityLevel = _config->getInt("vd.verbosityLevel",0);
 
     bool vdVisible           = _config->getBool("vd.visible",true);
     bool vdSolid             = _config->getBool("vd.solid",true);
@@ -70,14 +73,14 @@ namespace mu2e {
 
     G4Helper* _helper = &(*(art::ServiceHandle<G4Helper>()));
 
-    for( int id=1; id<=2; ++id) if( vdg->exist(id) ) {
+    for( int vdid=1; vdid<=2; ++vdid) if( vdg->exist(vdid) ) {
       VolumeInfo const & parent = _helper->locateVolInfo("ToyTS1Vacuum");
       ostringstream name;
-      name << "VirtualDetector" << id;
+      name << "VirtualDetector" << vdid;
       VolumeInfo vd = nestTubs( name.str(), vdParams, vacuumMaterial, 0,
-                                vdg->getLocal(id),
+                                vdg->getLocal(vdid),
                                 parent,
-                                id, vdVisible, G4Color::Red(), vdSolid,
+                                vdid, vdVisible, G4Color::Red(), vdSolid,
                                 forceAuxEdgeVisible,
                                 placePV,
                                 doSurfaceCheck );
@@ -86,14 +89,14 @@ namespace mu2e {
 
     // Virtual Detectors 3-6 are placed inside TS3
 
-    for( int id=3; id<=6; ++id) if( vdg->exist(id) ) {
+    for( int vdid=3; vdid<=6; ++vdid) if( vdg->exist(vdid) ) {
       VolumeInfo const & parent = _helper->locateVolInfo("ToyTS3Vacuum");
       ostringstream name;
-      name << "VirtualDetector" << id;
+      name << "VirtualDetector" << vdid;
       VolumeInfo vd = nestTubs( name.str(), vdParams, vacuumMaterial, 0,
-                                vdg->getLocal(id),
+                                vdg->getLocal(vdid),
                                 parent,
-                                id, vdVisible, G4Color::Red(), vdSolid,
+                                vdid, vdVisible, G4Color::Red(), vdSolid,
                                 forceAuxEdgeVisible,
                                 placePV,
                                 doSurfaceCheck );
@@ -102,14 +105,14 @@ namespace mu2e {
 
     // Virtual Detectors 7-8 are placed inside TS5
 
-    for( int id=7; id<=8; ++id) if( vdg->exist(id) ) {
+    for( int vdid=7; vdid<=8; ++vdid) if( vdg->exist(vdid) ) {
       VolumeInfo const & parent = _helper->locateVolInfo("ToyTS5Vacuum");
       ostringstream name;
-      name << "VirtualDetector" << id;
+      name << "VirtualDetector" << vdid;
       VolumeInfo vd = nestTubs( name.str(), vdParams, vacuumMaterial, 0,
-                                vdg->getLocal(id),
+                                vdg->getLocal(vdid),
                                 parent,
-                                id, vdVisible, G4Color::Red(), vdSolid,
+                                vdid, vdVisible, G4Color::Red(), vdSolid,
                                 forceAuxEdgeVisible,
                                 placePV,
                                 doSurfaceCheck );
@@ -136,26 +139,94 @@ namespace mu2e {
       deltaZ = 2.0 *_config->getDouble("neutronabsorber.internalHalfLengthZ01");
     }
 
-    for( int id=9; id<=10; ++id) if( vdg->exist(id) ) {
-
-      double zvd = vdg->getGlobal(id).z();
+    for( int vdid=9; vdid<=10; ++vdid) if( vdg->exist(vdid) ) {
+      
+      double zvd = vdg->getGlobal(vdid).z();
       double rvd = Ravr + deltaR/deltaZ*(zvd-Z0) - 5.0;
 
-      cout << "VD " << id << " : " << zvd << " " << rvd << endl;
+      if ( verbosityLevel > 0) {
+        cout << "VD " << vdid << " : " << zvd << " " << rvd << endl;
+      }
 
-      TubsParams vdParamsTarget(0,rvd,vdHalfLength);
+      TubsParams vdParamsTarget(0.,rvd,vdHalfLength);
 
       VolumeInfo const & parent = _helper->locateVolInfo("ToyDS2Vacuum");
       ostringstream name;
-      name << "VirtualDetector" << id;
+      name << "VirtualDetector" << vdid;
       VolumeInfo vd = nestTubs( name.str(), vdParamsTarget, vacuumMaterial, 0,
-                                vdg->getLocal(id),
+                                vdg->getLocal(vdid),
                                 parent,
-                                id, vdVisible, G4Color::Red(), vdSolid,
+                                vdid, vdVisible, G4Color::Red(), vdSolid,
                                 forceAuxEdgeVisible,
                                 placePV,
                                 doSurfaceCheck );
       vd.logical->SetSensitiveDetector(vdSD);
+    }
+
+    // placing virtual detectors in the middle of the ttracker
+
+    // check if ttracker exists and if the number of devices
+    // ttracker.numDevices is even is done in VirtualDetectorMaker
+
+    int vdid = 11;
+    if( vdg->exist(vdid) ) {
+      
+      // the radius of tracker mother
+      TTracker const & ttracker = *(GeomHandle<TTracker>());
+      double orvd = ttracker.getTrackerEnvelopeParams().outerRadius();
+      double irvd = ttracker.getTrackerEnvelopeParams().innerRadius();
+
+      if ( verbosityLevel > 0) {
+        double zvd = vdg->getGlobal(vdid).z();
+        cout << "VD " << vdid << " z, r : " << zvd << " " << irvd << " " << orvd << endl;
+      }
+
+      TubsParams vdParamsTTracker(irvd,orvd,vdHalfLength);
+
+      VolumeInfo const & parent = _helper->locateVolInfo("TrackerMother");
+      ostringstream name;
+      name << "VirtualDetector" << vdid;
+      VolumeInfo vd = nestTubs( name.str(), vdParamsTTracker, vacuumMaterial, 0,
+                                vdg->getLocal(vdid),
+                                parent,
+                                vdid, vdVisible, G4Color::Red(), vdSolid,
+                                forceAuxEdgeVisible,
+                                placePV,
+                                doSurfaceCheck );
+      vd.logical->SetSensitiveDetector(vdSD);
+
+      vdid = 12;
+      if( vdg->exist(vdid) ) {
+
+        TubsParams vdParamsTTrackerInner(0.,irvd,vdHalfLength);
+
+      // VD 12 is placed inside the ttracker at the same z position as
+      // VD 11 but from radius 0 to the inner radius of the ttracker
+      // mother volume. However, its mother volume is ToyDS3Vacuum
+      // which has a different offset. We will use the global offset
+      // here (!) as DS is not in the geometry service yet
+
+        VolumeInfo const & parent = _helper->locateVolInfo("ToyDS3Vacuum");
+
+        G4ThreeVector vdLocalOffset = vdg->getGlobal(vdid) - parent.centerInMu2e();
+
+        if ( verbosityLevel > 0) {
+          double zvd = vdg->getGlobal(vdid).z();
+          cout << "VD " << vdid << " z, r : " << zvd  << " " << irvd << endl;
+        }
+
+        ostringstream name;
+        name << "VirtualDetector" << vdid;
+        VolumeInfo vd = nestTubs( name.str(), vdParamsTTrackerInner, vacuumMaterial, 0,
+                                  vdLocalOffset,
+                                  parent,
+                                  vdid, vdVisible, G4Color::Red(), vdSolid,
+                                  forceAuxEdgeVisible,
+                                  placePV,
+                                  doSurfaceCheck );
+        vd.logical->SetSensitiveDetector(vdSD);
+      }
+
     }
 
   }
