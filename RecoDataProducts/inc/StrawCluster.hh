4ae710ac640b77525b294c07ab5126323403c54a
wenzel <>
2011-06-02 22:50:54 +0000

    complete StrawCluster class

--- RecoDataProducts/inc/StrawCluster.hh
+++ RecoDataProducts/inc/StrawCluster.hh
@@ -3,9 +3,9 @@
 //
 // First version of a Cluster.
 //
-// $Id: StrawCluster.hh,v 1.2 2011/06/01 21:12:23 wenzel Exp $
+// $Id: StrawCluster.hh,v 1.3 2011/06/02 22:50:54 wenzel Exp $
 // $Author: wenzel $
-// $Date: 2011/06/01 21:12:23 $
+// $Date: 2011/06/02 22:50:54 $
 //
 // Original author Hans Wenzel
 //
@@ -13,9 +13,25 @@
 // C++ includes
 #include <vector>
 
-// Mu2e includes
-#include "DataProducts/inc/DPIndex.hh"
 
+
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Core/Event.h"
+// Mu2e includes.
+#include "GeometryService/inc/GeometryService.hh"
+#include "GeometryService/inc/GeomHandle.hh"
+#include "GeometryService/inc/getTrackerOrThrow.hh"
+#include "TrackerGeom/inc/Tracker.hh"
+#include "RecoDataProducts/inc/StrawHitCollection.hh"
+#include "DataProducts/inc/DPIndexVectorCollection.hh"
+#include "MCDataProducts/inc/StepPointMCCollection.hh"
+#include "Mu2eUtilities/inc/TwoLinePCA.hh"
+#include "Mu2eUtilities/inc/resolveTransients.hh"
+#include "Mu2eUtilities/inc/resolveDPIndices.hh"
+#include "Mu2eUtilities/inc/LineSegmentPCA.hh"
+// CLHEP includes
+#include "CLHEP/Vector/ThreeVector.h"
+#include "CLHEP/Vector/TwoVector.h"
 namespace art {
   class ProductID;
 }
@@ -31,10 +47,18 @@ namespace mu2e {
 
     // Accessors
     std::vector<DPIndex> const & StrawHitIndices() const { return _StrawHitIndices; }
-
+    CLHEP::Hep3Vector X(art::Event const& event) const;
+    double Energy(art::Event const& event) const;
+    double Halflength(art::Event const& event) const;
+    double averageT(art::Event const& event) const;
+    double averagedT(art::Event const & event) const;
+    DeviceId did(art::Event const & event) const;
+    SectorId secid(art::Event const & event) const;
+    CLHEP::Hep3Vector dirX(art::Event const& event) const; 
+    LineSegmentPCA linesegment(art::Event const& event) const;
     //    StrawCluster& add(art::ProductID const & CollId, CaloHit const & hit);
   private:
-
+    //   const Tracker& tracker;
     std::vector<DPIndex> _StrawHitIndices;
   };
 } // namespace mu2e
--- RecoDataProducts/src/StrawCluster.cc
+++ RecoDataProducts/src/StrawCluster.cc
@@ -1,16 +1,15 @@
 //
-// StrawCluster to be created based on CaloHit's
+// StrawCluster 
 //
-// $Id: StrawCluster.cc,v 1.1 2011/06/01 21:13:31 wenzel Exp $
+// $Id: StrawCluster.cc,v 1.2 2011/06/02 22:50:54 wenzel Exp $
 // $Author: wenzel $
-// $Date: 2011/06/01 21:13:31 $
+// $Date: 2011/06/02 22:50:54 $
 //
 
 // C++ includes
 #include <ostream>
 
 // Framework includes.
-//#include "art/Utilities/Exception.h"
 #include "art/Persistency/Provenance/ProductID.h"
 
 // Mu2e includes
@@ -21,7 +20,127 @@ using namespace std;
 namespace mu2e {
 
   StrawCluster::StrawCluster(std::vector<DPIndex>&  hitIndices)
-  {
-     _StrawHitIndices=hitIndices;
+  {   // Geometry info for the TTracker.
+    // Get a reference to one of the T trackers.
+    // Throw exception if not successful.
+    _StrawHitIndices=hitIndices;
   }
+
+  CLHEP::Hep3Vector StrawCluster::X(art::Event const & event) const
+  {  
+    const Tracker& tracker = getTrackerOrThrow();    
+    CLHEP::Hep3Vector pvec = CLHEP::Hep3Vector(0.,0.,0.);
+    for (size_t index =0;index<_StrawHitIndices.size();++index)
+      {
+	DPIndex const& junkie = _StrawHitIndices[index];
+	StrawHit const& strawhit = *resolveDPIndex<StrawHitCollection>(event,junkie);
+	Straw str = tracker.getStraw(strawhit.strawIndex());
+	const CLHEP::Hep3Vector mpvec  = str.getMidPoint();
+	pvec = pvec + mpvec;
+      }
+    return pvec;
+  }
+
+  double StrawCluster::Halflength(art::Event const & event) const
+  {  
+    double hlen = 0.0;
+    const Tracker& tracker = getTrackerOrThrow();    
+    for (size_t index =0;index<_StrawHitIndices.size();++index)
+      {
+	DPIndex const& junkie = _StrawHitIndices[index];
+	StrawHit const& strawhit = *resolveDPIndex<StrawHitCollection>(event,junkie);
+	Straw str = tracker.getStraw(strawhit.strawIndex());
+	if (str.getHalfLength()>hlen)
+	  {
+	    hlen=str.getHalfLength();
+	  }
+      }
+    return hlen;
+  }
+
+  double StrawCluster::Energy(art::Event const & event) const
+  {  
+    double e =0.0;
+    for (size_t index =0;index<_StrawHitIndices.size();++index)
+      {
+	DPIndex const& junkie = _StrawHitIndices[index];
+	StrawHit const& strawhit = *resolveDPIndex<StrawHitCollection>(event,junkie);
+	e = e+strawhit.energyDep();
+      }
+    return e;
+  }
+  double StrawCluster::averageT(art::Event const & event) const
+  {  
+    double T =0.0;
+    for (size_t index =0;index<_StrawHitIndices.size();++index)
+      {
+	DPIndex const& junkie = _StrawHitIndices[index];
+	StrawHit const& strawhit = *resolveDPIndex<StrawHitCollection>(event,junkie);
+	T = T+strawhit.time();
+      }
+    T=T/double(_StrawHitIndices.size());
+    return T;
+  }
+  double StrawCluster::averagedT(art::Event const & event) const
+  {  
+    double dT =0.0;
+    for (size_t index =0;index<_StrawHitIndices.size();++index)
+      {
+	DPIndex const& junkie = _StrawHitIndices[index];
+	StrawHit const& strawhit = *resolveDPIndex<StrawHitCollection>(event,junkie);
+	dT = dT+strawhit.dt();
+      }
+    dT=dT/double(_StrawHitIndices.size());
+    return dT;
+  }
+  DeviceId StrawCluster::did(art::Event const & event) const
+  {   
+
+    const Tracker& tracker = getTrackerOrThrow();    
+    DPIndex const& junkie = _StrawHitIndices[0];
+    StrawHit const& strawhit = *resolveDPIndex<StrawHitCollection>(event,junkie);
+    Straw    str = tracker.getStraw(strawhit.strawIndex());
+    StrawId  sid = str.id();
+    DeviceId did = sid.getDeviceId();
+    return did;
+
+  }
+  SectorId StrawCluster::secid(art::Event const & event) const
+  {   
+
+    const Tracker& tracker = getTrackerOrThrow();    
+    DPIndex const& junkie = _StrawHitIndices[0];
+    StrawHit const& strawhit = *resolveDPIndex<StrawHitCollection>(event,junkie);
+    Straw    str = tracker.getStraw(strawhit.strawIndex());
+    StrawId  sid = str.id();
+    SectorId secid = sid.getSectorId();
+    return secid;
+
+  }
+  CLHEP::Hep3Vector StrawCluster::dirX(art::Event const & event) const
+  {  
+    const Tracker& tracker = getTrackerOrThrow();    
+    DPIndex const& junkie = _StrawHitIndices[0];
+    StrawHit const& strawhit = *resolveDPIndex<StrawHitCollection>(event,junkie);
+    Straw str = tracker.getStraw(strawhit.strawIndex());
+    const CLHEP::Hep3Vector dirvec = str.getDirection();
+    return dirvec;
+  }
+ 
+  LineSegmentPCA StrawCluster::linesegment(art::Event const& event) const
+  {  
+    CLHEP::Hep3Vector direction = dirX(event);
+    CLHEP::Hep3Vector position  = X(event);
+    double hlen  = Halflength(event);
+    const CLHEP::Hep2Vector p0 =      
+      CLHEP::Hep2Vector(position.getX()-hlen*direction.getX(),
+			position.getY()-hlen*direction.getY());
+    const CLHEP::Hep2Vector p1 = 
+      CLHEP::Hep2Vector(position.getX()+hlen*direction.getX(),
+			position.getY()+hlen*direction.getY());
+    LineSegmentPCA linesegment(p0, p1);
+    return linesegment;
+  }
+  
+
 } // namespace mu2e
