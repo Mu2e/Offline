dc5d0e0f2fc6ff65351aa15aeab70b7a9257f300
kutschke <>
2011-06-05 17:29:11 +0000

    Add std::vector<mu2e::TracerProduct>

--- Sandbox/src/classes.h
+++ Sandbox/src/classes.h
@@ -1,9 +1,9 @@
 //
 // Build a dictionary.
 //
-// $Id: classes.h,v 1.2 2011/06/05 16:40:11 kutschke Exp $
+// $Id: classes.h,v 1.3 2011/06/05 17:29:11 kutschke Exp $
 // $Author: kutschke $
-// $Date: 2011/06/05 16:40:11 $
+// $Date: 2011/06/05 17:29:11 $
 //
 // Original author Rob Kutschke
 //
@@ -17,3 +17,4 @@
 
 template class art::Wrapper<mu2e::TransientProduct00Collection>;
 template class art::Wrapper<mu2e::TracerProduct>;
+template class art::Wrapper<std::vector<mu2e::TracerProduct> >;
