5cbe3fabf9084b58e807e78b0b7435867474ac5d
mf <>
2011-06-05 23:11:35 +0000

    A SubEvent (previously called an Incident) is a set of hits all of which can sensibly be ascribed a time-of-first-pulse-on-the-wire, corrected for the time for the particle to get to Z=0 based on some average cos theta, tying in some time window.  That is, this producer and these classes implement a split of the entire event into a series of sliding time windows.

--- RecoDataProducts/inc/StrawHitSynopsis.hh
+++ RecoDataProducts/inc/StrawHitSynopsis.hh
@@ -6,9 +6,9 @@
 // A StawHitSynopsis provides qick information about the hit, plus a pointer
 // to the actual straw hit.
 //
-// $Id: StrawHitSynopsis.hh,v 1.1 2011/06/01 23:29:47 mf Exp $
+// $Id: StrawHitSynopsis.hh,v 1.1 2011/06/05 23:11:35 mf Exp $
 // $Author: mf $
-// $Date: 2011/06/01 23:29:47 $
+// $Date: 2011/06/05 23:11:35 $
 //
 // Original author: Mark Fischler
 //
@@ -21,9 +21,6 @@
 // Framework includes.
 
 // Mu2e includes.
-#include "TrackerGeom/inc/Tracker.hh"
-#include "GeometryService/inc/getTrackerOrThrow.hh"
-
 
 namespace mu2e {
 
@@ -31,28 +28,36 @@ namespace mu2e {
   {
   public:
     // ctor -- provide the TTracker geometry, the hit, and time estimate
-    StrawHitSynopsis(Tracker const & tracker, 
-             StrawHit const& h,
-             double t0_star)
-      : _tt(&tracker)
-      , _strawHitPtr (&h)
+    StrawHitSynopsis(StrawHit const& h, double t0_star)
+      : _strawHitPtr (&h)
       , _t0_star (t0_star) {}
       
-    // TODO - There will be more content to this class; also accessors
-    //        (which is why _tt will be needed) 
-
-   StrawHitSynopsis() 
-   : _tt(0)
-   , _strawHitPtr (0) 
-   , _t0_star (0) {}
+    StrawHitSynopsis() 
+    : _strawHitPtr (0) 
+    , _t0_star (0) {}
    
+    // TODO - There will be more content to this class; also more accessors
+ 
+    // accessors
+    
+    double t0_star() const {return _t0_star;}
+    StrawHit const * strawHitPtr() const {return _strawHitPtr;}
+        
   private:
    // private data
-    Tracker const * _tt;
     StrawHit const * _strawHitPtr;
     double _t0_star;
     
   }; // StrawHitSynopsis class definition
+
+  inline
+  std::ostream & 
+  operator << ( std::ostream & os, const StrawHitSynopsis & s )
+  {
+    os << "StrawHitSynopsis: straw " <<  s.strawHitPtr()->strawIndex().asInt()
+       << " at t0_star = " << s.t0_star() << "\n";
+    return os;
+  }
 
 } // end of namespace mu2e
 
--- RecoDataProducts/src/classes.h
+++ RecoDataProducts/src/classes.h
@@ -1,9 +1,9 @@
 //
 // Build a dictionary.
 //
-// $Id: classes.h,v 1.1 2011/05/24 17:16:43 kutschke Exp $
-// $Author: kutschke $
-// $Date: 2011/05/24 17:16:43 $
+// $Id: classes.h,v 1.2 2011/06/05 23:11:35 mf Exp $
+// $Author: mf $
+// $Date: 2011/06/05 23:11:35 $
 //
 // Original author Rob Kutschke
 //
@@ -12,14 +12,16 @@
 
 #include "art/Persistency/Common/Wrapper.h"
 
-#include "RecoDataProducts/inc/CaloCrystalHitCollection.hh"
-#include "RecoDataProducts/inc/CaloHitCollection.hh"
-#include "RecoDataProducts/inc/HoughCircleCollection.hh"
-#include "RecoDataProducts/inc/StrawClusterCollection.hh"
 #include "RecoDataProducts/inc/StrawHitCollection.hh"
+#include "RecoDataProducts/inc/StrawClusterCollection.hh"
+#include "RecoDataProducts/inc/CaloHitCollection.hh"
+#include "RecoDataProducts/inc/CaloCrystalHitCollection.hh"
+#include "RecoDataProducts/inc/HoughCircleCollection.hh"
+#include "RecoDataProducts/inc/SubEventCollection.hh"
 
-template class art::Wrapper<mu2e::HoughCircleCollection>;
 template class art::Wrapper<mu2e::StrawHitCollection>;
 template class art::Wrapper<mu2e::StrawClusterCollection>;
 template class art::Wrapper<mu2e::CaloHitCollection>;
 template class art::Wrapper<mu2e::CaloCrystalHitCollection>;
+template class art::Wrapper<mu2e::HoughCircleCollection>;
+template class art::Wrapper<mu2e::SubEventCollection>;
