974d07a16d3e4cf2c4515b3e4c27edb2e76a539d
kutschke <>
2011-06-07 22:01:30 +0000

    First release.

---
+++ MCDataProducts/inc/PtrStepPointMCVectorCollection.hh
@@ -0,0 +1,18 @@
+#ifndef MCDataProducts_PtrStepPointMCVectorCollection_hh
+#define MCDataProducts_PtrStepPointMCVectorCollection_hh
+//
+// Define a type for a collection of art::Ptr<StepPointMC> objects.
+//
+// Original author Ivan Logashenko
+//
+
+#include "MCDataProducts/inc/PtrStepPointMCVector.hh"
+#include "art/Persistency/Common/Ptr.h"
+
+#include <vector>
+
+namespace mu2e {
+  typedef std::vector<PtrStepPointMCVector> PtrStepPointMCVectorCollection;
+}
+
+#endif /* MCDataProducts_PtrStepPointMCVectorCollection_hh */
