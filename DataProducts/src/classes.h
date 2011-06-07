497b48c2c7029da6514d80b5ad08d096003efc21
kutschke <>
2011-06-07 21:28:24 +0000

    Remove DPInex and related items.

--- DataProducts/src/classes.h
+++ DataProducts/src/classes.h
@@ -1,24 +1,17 @@
 //
 // Build a dictionary.
 //
-// $Id: classes.h,v 1.4 2011/06/01 14:57:48 greenc Exp $
-// $Author: greenc $
-// $Date: 2011/06/01 14:57:48 $
+// $Id: classes.h,v 1.5 2011/06/07 21:28:24 kutschke Exp $
+// $Author: kutschke $
+// $Date: 2011/06/07 21:28:24 $
 //
 // Original author Rob Kutschke
 //
 
-#include "DataProducts/inc/DPIndexVectorCollection.hh"
 #include "Mu2eUtilities/inc/PDGCode.hh"
 #include "TrackerGeom/inc/StrawId.hh"
 #include "TrackerGeom/inc/StrawIndex.hh"
-#include "art/Persistency/Common/Wrapper.h"
-#include "art/Persistency/Provenance/ProductID.h"
 #include "cetlib/map_vector.h"
 #include <vector>
 
-template class art::Wrapper<mu2e::DPIndexVectorCollection>;
-
 template class std::vector<cet::map_vector_key>;
-template class std::vector<mu2e::DPIndex>;
-template class std::vector<mu2e::DPIndexVector>;
