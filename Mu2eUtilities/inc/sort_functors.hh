9cf76d1e81985af27a52bd521550b953907435de
kutschke <>
2011-06-15 21:05:20 +0000

    Provide additional functor for the fix to MakeCaloCrystalHits_module.cc.

--- Mu2eUtilities/inc/sort_functors.hh
+++ Mu2eUtilities/inc/sort_functors.hh
@@ -1,9 +1,9 @@
 #ifndef Mu2eUtilities_sort_functors_hh
 #define Mu2eUtilities_sort_functors_hh
 //
-// $Id: sort_functors.hh,v 1.4 2011/05/18 02:27:18 wb Exp $
-// $Author: wb $
-// $Date: 2011/05/18 02:27:18 $
+// $Id: sort_functors.hh,v 1.5 2011/06/15 21:05:20 kutschke Exp $
+// $Author: kutschke $
+// $Date: 2011/06/15 21:05:20 $
 //
 // Original author KLG
 //
@@ -59,6 +59,29 @@ namespace mu2e {
                 a.time() < b.time()
                 )
                );
+    }
+
+  private:
+
+    Calorimeter const & _cal;
+
+  };
+
+  template <typename HitT>
+  class lessByCIdAndTimeByPointer {
+
+  public:
+
+
+    explicit lessByCIdAndTimeByPointer( Calorimeter const & cal): _cal(cal) {}
+
+    bool operator() (HitT const * a, HitT const * b) const {
+      
+      return (  _cal.getCrystalByRO(a->id()) <  _cal.getCrystalByRO(b->id()) ||
+              ( _cal.getCrystalByRO(a->id()) == _cal.getCrystalByRO(b->id()) &&
+                a->time() < b->time()
+              )
+             );
     }
 
   private:
