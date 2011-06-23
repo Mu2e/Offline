0e9203bb01531aef920e7990c4566882fe4ec691
onoratog <>
2011-06-23 05:54:13 +0000

    *** empty log message ***

---
+++ Analyses/src/BkgNorm_module.cc
@@ -0,0 +1,326 @@
+//
+// A module to evaluate the normalization of background to simulate
+//
+// $Id: BkgNorm_module.cc,v 1.1 2011/06/23 05:54:13 onoratog Exp $
+// $Author: onoratog $
+// $Date: 2011/06/23 05:54:13 $
+//
+// Original author Gianni Onorato
+//
+
+#include "CLHEP/Units/PhysicalConstants.h"
+#include "MCDataProducts/inc/PtrStepPointMCVectorCollection.hh"
+#include "GeometryService/inc/GeomHandle.hh"
+#include "GeometryService/inc/GeometryService.hh"
+#include "GeometryService/inc/getTrackerOrThrow.hh"
+#include "MCDataProducts/inc/GenParticleCollection.hh"
+#include "MCDataProducts/inc/PhysicalVolumeInfoCollection.hh"
+#include "MCDataProducts/inc/SimParticleCollection.hh"
+#include "MCDataProducts/inc/StepPointMCCollection.hh"
+#include "MCDataProducts/inc/StrawHitMCTruthCollection.hh"
+#include "RecoDataProducts/inc/StrawHitCollection.hh"
+#include "Mu2eUtilities/inc/LinePointPCA.hh"
+#include "TFile.h"
+#include "TNtuple.h"
+#include "TTrackerGeom/inc/TTracker.hh"
+#include "TrackerGeom/inc/Straw.hh"
+#include "TrackerGeom/inc/Tracker.hh"
+#include "art/Framework/Core/EDAnalyzer.h"
+#include "art/Framework/Core/Event.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Core/TFileDirectory.h"
+#include "art/Framework/Services/Optional/TFileService.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "art/Persistency/Common/Handle.h"
+#include "art/Persistency/Provenance/Provenance.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+#include <cmath>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <string>
+
+using namespace std;
+
+namespace mu2e {
+
+
+  class BkgNorm : public art::EDAnalyzer {
+  public:
+    explicit BkgNorm(fhicl::ParameterSet const& pset):
+      _diagLevel(pset.get<int>("diagLevel",0)),
+      _trackerStepPoints(pset.get<string>("trackerStepPoints","tracker")),
+      _makerModuleLabel(pset.get<std::string>("makerModuleLabel")),
+      _generatorModuleLabel(pset.get<std::string>("generatorModuleLabel", "generate")),
+      _g4ModuleLabel(pset.get<std::string>("g4ModuleLabel", "g4run")),
+      _minimumEnergy(pset.get<double>("minimumEnergy",0.0001)), // MeV
+      _tNtup(0),
+      _nDevices(36),
+      _nSectors(6),
+      _nLayers(2),
+      _nStrawsPerLay(50)
+    {
+    }
+    virtual ~BkgNorm() {
+    }
+    virtual void beginJob();
+
+    void analyze(art::Event const& e );
+
+  private:
+
+    void doTracker(art::Event const& evt);
+
+
+    // Diagnostic level
+    int _diagLevel;
+
+    // Name of the tracker StepPoint collection
+    std::string _trackerStepPoints;
+
+    // Label of the module that made the hits.
+    std::string _makerModuleLabel;
+
+    // Label of the generator.
+    std::string _generatorModuleLabel;
+
+    // Label of the G4 module
+    std::string _g4ModuleLabel;
+
+    double _minimumEnergy; //minimum energy deposition of hits
+
+    TNtuple* _tNtup;
+    const int _nDevices, _nSectors, _nLayers, _nStrawsPerLay;
+
+  };
+
+
+  void BkgNorm::beginJob( ) {
+  }
+
+  void BkgNorm::analyze(art::Event const& evt ) {
+
+    static int ncalls(0);
+    ++ncalls;
+
+    art::ServiceHandle<GeometryService> geom;
+
+    if (ncalls == 1) {
+
+      art::ServiceHandle<art::TFileService> tfs;
+
+      // "evt:run:"; //Event info (2 entries)
+      // "time:dt:eDep:lay:dev:sec:strawId:strawX:strawY:strawZ:"; //StrawHit info (10 entries)
+      // "trkPdgId:trkP:trkIsGen:trkStartVolume:trkStepFromEva:EvaIsGen"; //Track making hits info (6 entries)
+      // "genPdgId:genId:genP:genE:genX:genY:genZ:genT:genPhi:genCosth:"; //Generated particle info (10 entries)
+      // "dau1PdgId:dau1P:dau1StartVolume:"; //First Daughter of generated particle info (3 entries)
+
+      _tNtup        = tfs->make<TNtuple>( "StrawHits", "Straw Ntuple", "evt:run:time:dt:eDep:lay:dev:sec:strawId:strawX:strawY:strawZ:trkPdgId:trkP:trkIsGen:trkStartVolume:trkStepFromEva:EvaIsGen:genPdgId:genId:genP:genE:genX:genY:genZ:genT:genPhi:genCosth:dau1PdgId:dau1P:dau1StartVolume"); 
+   }
+
+    doTracker(evt);
+    
+  } // end of analyze
+
+  void BkgNorm::doTracker(art::Event const& evt) {
+
+    const Tracker& tracker = getTrackerOrThrow();
+
+    art::Handle<StrawHitCollection> pdataHandle;
+    evt.getByLabel(_makerModuleLabel,pdataHandle);
+    StrawHitCollection const* hits = pdataHandle.product();
+
+    art::Handle<StrawHitMCTruthCollection> truthHandle;
+    evt.getByLabel(_makerModuleLabel,truthHandle);
+    StrawHitMCTruthCollection const* hits_truth = truthHandle.product();
+
+    art::Handle<PtrStepPointMCVectorCollection> mcptrHandle;
+    evt.getByLabel(_makerModuleLabel,"StrawHitMCPtr",mcptrHandle);
+    PtrStepPointMCVectorCollection const* hits_mcptr = mcptrHandle.product();
+
+    if (!(hits->size() == hits_truth->size() &&
+          hits_mcptr->size() == hits->size() ) ) {
+      throw cet::exception("RANGE")
+        << "Strawhits: " << hits->size()
+        << " MCTruthStrawHits: " << hits_truth->size()
+        << " MCPtr: " << hits_mcptr->size();
+    }
+
+    art::Handle<GenParticleCollection> genParticles;
+    evt.getByLabel(_generatorModuleLabel, genParticles);
+
+    art::Handle<SimParticleCollection> simParticles;
+    evt.getByLabel(_g4ModuleLabel, simParticles);
+
+    art::Handle<PhysicalVolumeInfoCollection> volumes;
+    evt.getRun().getByLabel(_g4ModuleLabel, volumes);
+
+    bool haveSimPart = ( simParticles.isValid() && volumes.isValid() );
+    if ( haveSimPart ){
+      haveSimPart = !(simParticles->empty() || volumes->empty());
+    }
+
+    size_t nStrawPerEvent = hits->size();
+
+    for (size_t i=0; i<nStrawPerEvent; ++i) {
+
+      // Access data
+      StrawHit             const&      hit(hits->at(i));
+      StrawHitMCTruth      const&    truth(hits_truth->at(i));
+      PtrStepPointMCVector const&    mcptr(hits_mcptr->at(i));
+
+      double hitEnergy = hit.energyDep();
+
+      //Skip the straw if the energy of the hit is smaller than the minimum required
+      if (hitEnergy < _minimumEnergy) continue;
+
+      //Get hit straw
+      StrawIndex si = hit.strawIndex();
+      Straw str = tracker.getStraw(si);
+      StrawId sid = str.id();
+      LayerId lid = sid.getLayerId();
+      DeviceId did = sid.getDeviceId();
+      SectorId secid = sid.getSectorId();
+
+      //Get coordinates of the hit:
+
+      //X, Y and Z coordinate of the straw middle point
+
+      const CLHEP::Hep3Vector stMidPoint3 = str.getMidPoint();
+      double xc = stMidPoint3.getX();
+      double yc = stMidPoint3.getY();
+      double z = stMidPoint3.getZ();
+
+      //time of the hit
+      double hitTime = hit.time();
+
+      size_t nHitsPerStraw = mcptr.size();
+
+      float tntpArray[29];
+      int idx(0);
+      tntpArray[idx++] = evt.id().event();
+      tntpArray[idx++] = evt.run();
+      tntpArray[idx++] = hitTime;
+      tntpArray[idx++] = hit.dt();
+      tntpArray[idx++] = hitEnergy;
+      tntpArray[idx++] = lid.getLayer();
+      tntpArray[idx++] = did;
+      tntpArray[idx++] = secid.getSector();
+      tntpArray[idx++] = sid.getStraw();
+      tntpArray[idx++] = xc;
+      tntpArray[idx++] = yc;
+      tntpArray[idx++] = z;
+
+      bool notFirstTrack = true;
+
+      size_t j = 0;
+
+      CLHEP::Hep3Vector const& strDir = str.direction();
+
+      double strRadius = str.getRadius();
+
+      while (notFirstTrack && j<mcptr.size()) {
+	if (j==mcptr.size()-1) {
+	  j=0;
+	  break;
+	}
+	StepPointMC const& mchit = *mcptr[j];
+	SimParticle const& sim = simParticles->at(mchit.trackId());
+	CLHEP::Hep3Vector const& StartPos = sim.startPosition();
+	LinePointPCA lppca(stMidPoint3, strDir, StartPos);
+	double insideDistance = lppca.dca();
+	if (insideDistance >= strRadius) {
+	  notFirstTrack = false;
+	  break;
+	} else {
+	  ++j;
+	}
+      }
+      
+      StepPointMC const& mchit = *mcptr[j];
+      SimParticle const& sim = simParticles->at(mchit.trackId());
+      tntpArray[idx++] = sim.pdgId();
+      tntpArray[idx++] = sim.startMomentum().vect().mag();
+      tntpArray[idx++] = sim.fromGenerator();
+      tntpArray[idx++] = sim.startVolumeIndex();
+      int nEvolutionSteps = 0;
+      SimParticleCollection::key_type Dau1Idx = SimParticleCollection::key_type(0); 
+      if (!sim.fromGenerator()) {
+	bool notEva = true;
+	SimParticle& baby = const_cast<SimParticle&>(sim);
+	while (notEva) {
+	  if (!baby.hasParent()) {
+	    tntpArray[idx++] = nEvolutionSteps;
+	    tntpArray[idx++] = 0;
+	    notEva = false;
+	    break;
+	  }
+	  SimParticle & mommy = const_cast<SimParticle&>(*baby.parent());
+	  nEvolutionSteps++;
+	  if (mommy.fromGenerator()) {
+	    tntpArray[idx++] = nEvolutionSteps;
+	    tntpArray[idx++] = 1;
+	    notEva = false;
+	    Dau1Idx = baby.id();
+	    break;
+	  } else {
+	    baby = mommy;
+	  }
+	}
+      } else {
+	tntpArray[idx++] = nEvolutionSteps;
+	tntpArray[idx++] = 1;
+      }
+
+
+      SimParticleCollection::key_type idxInSim = SimParticleCollection::key_type(1);
+      SimParticle const& geninSim = simParticles->at(idxInSim);
+      /*      bool notFound = true;
+      while (notFound) {
+	if (geninSim.fromGenerator()) {
+	  notFound = false;
+	  break;
+	} else {
+	  idxInSim++;
+	}
+      }
+      */      
+
+      GenParticle const& gen = genParticles->at(geninSim.generatorIndex());
+      tntpArray[idx++] = gen.pdgId();
+      tntpArray[idx++] = gen.generatorId().id();      
+      tntpArray[idx++] = gen.momentum().vect().mag();
+      tntpArray[idx++] = gen.momentum().e();
+      tntpArray[idx++] = gen.position().x();
+      tntpArray[idx++] = gen.position().y();
+      tntpArray[idx++] = gen.position().z();
+      tntpArray[idx++] = gen.time();
+      tntpArray[idx++] = gen.momentum().cosTheta();
+      tntpArray[idx++] = gen.momentum().phi();
+
+      if (Dau1Idx != SimParticleCollection::key_type(0)) {
+	SimParticle const& Dau1 = simParticles->at(Dau1Idx);
+	tntpArray[idx++] = Dau1.pdgId();
+	tntpArray[idx++] = Dau1.startMomentum().vect().mag();
+	tntpArray[idx++] = Dau1.startVolumeIndex();      
+      } else {
+	tntpArray[idx++] = 0;
+	tntpArray[idx++] = 0;
+	tntpArray[idx++] = 0;
+      }
+
+      _tNtup->Fill(tntpArray);
+
+
+    } //end of Strawhits loop
+    
+  } // end of doTracker
+  
+}
+
+using mu2e::BkgNorm;
+DEFINE_ART_MODULE(BkgNorm);
+
