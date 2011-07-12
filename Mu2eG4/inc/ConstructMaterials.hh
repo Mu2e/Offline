094b1d3dbe964fb0a1e62090b0e252326f6fb4bc
kutschke <>
2011-07-12 04:09:53 +0000

    Remove constructG4Materials method that is not really needed.

--- Mu2eG4/inc/ConstructMaterials.hh
+++ Mu2eG4/inc/ConstructMaterials.hh
@@ -3,9 +3,9 @@
 //
 // Construct materials requested by the run-time configuration system.
 //
-// $Id: ConstructMaterials.hh,v 1.4 2011/05/18 02:27:17 wb Exp $
-// $Author: wb $
-// $Date: 2011/05/18 02:27:17 $
+// $Id: ConstructMaterials.hh,v 1.5 2011/07/12 04:09:53 kutschke Exp $
+// $Author: kutschke $
+// $Date: 2011/07/12 04:09:53 $
 //
 // Original author Rob Kutschke
 //
@@ -52,11 +52,7 @@ namespace mu2e {
 
   private:
 
-
-    // Construct predefined G4 materials.
-    void constructG4Materials( SimpleConfig const& config );
-
-    // Construct some additional materials specific to Mu2e.
+    // Construct materials specific to Mu2e.
     void constructMu2eMaterials( SimpleConfig const& config );
 
     // Wrapper around FindOrBuildElement.
@@ -68,7 +64,7 @@ namespace mu2e {
     // Check that a material name is not already in use.
     void uniqueMaterialOrThrow( G4String const& name);
 
-    // Check to see if a string appear within a vector of strings.
+    // Check to see if a string appears within a vector of strings.
     bool isRequested( std::vector<std::string> const& V, std::string const& name);
 
   };
