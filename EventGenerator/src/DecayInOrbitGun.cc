25de911248872815108b38a127e96c231cf0ac21
brownd <>
2012-01-27 20:30:27 +0000

    bug fix

--- EventGenerator/src/DecayInOrbitGun.cc
+++ EventGenerator/src/DecayInOrbitGun.cc
@@ -1,9 +1,9 @@
 //
 // Generate some number of DIO electrons.
 //
-// $Id: DecayInOrbitGun.cc,v 1.38 2012/01/27 20:15:07 brownd Exp $
+// $Id: DecayInOrbitGun.cc,v 1.39 2012/01/27 20:30:27 brownd Exp $
 // $Author: brownd $
-// $Date: 2012/01/27 20:15:07 $
+// $Date: 2012/01/27 20:30:27 $
 //
 // Original author Rob Kutschke
 //
@@ -63,7 +63,7 @@ namespace mu2e {
 
     // Random number distributions; getEngine comes from the base class.
     _randSimpleEnergy(getEngine(), &(binnedEnergySpectrum()[0]), _nbins ),
-    _randFlatEnergy(getEngine(),_elow,ehi),
+    _randFlatEnergy(getEngine(),_elow,_ehi),
     _randPoissonQ( getEngine(), std::abs(_mean) ),
     _randomUnitSphere ( getEngine(), _czmin, _czmax, _phimin, _phimax ),
     _STfname(config.getString("FoilParticleGenerator.STfilename","ExampleDataFiles/StoppedMuons/stoppedMuons_02.txt")),
@@ -164,7 +164,7 @@ namespace mu2e {
         e = _elow + _randSimpleEnergy.fire() * (_ehi - _elow);
       } else if (_useFlatSpectrum) {
 	e = _randFlatEnergy.fire();
-      } else
+      } else {
         e = _randEnergy->fire();
       }
 
