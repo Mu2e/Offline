682438b6a409a03cf9ecb43edabe61ed744ed789
gandr <>
2012-02-11 00:43:23 +0000

    Allow to visualize field map regions.  Tweak definitions of some of visregions.boxes parameters for better usability.

--- Mu2eG4/src/constructVisualizationRegions.cc
+++ Mu2eG4/src/constructVisualizationRegions.cc
@@ -22,6 +22,7 @@
 
 #include "GeometryService/inc/GeomHandle.hh"
 #include "GeometryService/inc/WorldG4.hh"
+#include "BFieldGeom/inc/BFieldManager.hh"
 
 #include "G4Helper/inc/G4Helper.hh"
 #include "G4Helper/inc/VolumeInfo.hh"
@@ -32,8 +33,8 @@
 #include "Mu2eG4/inc/findMaterialOrThrow.hh"
 
 
-//#define AGDEBUG(stuff) std::cerr<<"AG: "<<__FILE__<<", line "<<__LINE__<<": "<<stuff<<std::endl;
-#define AGDEBUG(stuff)
+#define AGDEBUG(stuff) std::cerr<<"AG: "<<__FILE__<<", line "<<__LINE__<<": "<<stuff<<std::endl;
+//#define AGDEBUG(stuff)
 
 namespace mu2e {
 
@@ -46,21 +47,84 @@ namespace mu2e {
     const bool doSurfaceCheck      = false; // overlaps are OK
     const bool placePV             = true;
 
-    std::vector<int> visible;
-    config.getVectorInt("visregions.boxes.visible", visible, visible);
-    if(!visible.empty()) {
+    //----------------------------------------------------------------
+    // Field maps
+    if(config.getBool("visregions.fieldMaps.visible", false)) {
+      const bool solid = config.getBool("visregions.fieldMaps.solid");
 
-      const int nboxes = visible.size();
+      std::vector<double> red;   config.getVectorDouble("visregions.fieldMaps.color.red", red);
+      if(red.empty()) {
+        throw cet::exception("GEOM")<<"constructVisualizationRegions(): visregions.fieldMaps.color.red is empty";
+      }
+      std::vector<double> green; config.getVectorDouble("visregions.fieldMaps.color.green", green, red.size());
+      std::vector<double> blue;  config.getVectorDouble("visregions.fieldMaps.color.blue", blue, red.size());
 
-      std::vector<int> solid; config.getVectorInt("visregions.boxes.solid", solid, nboxes);
-      std::vector<std::string> material; config.getVectorString("visregions.boxes.material", material, nboxes);
+      G4Material *material = findMaterialOrThrow(config.getString("visregions.fieldMaps.material"));
 
-      std::vector<double> red; config.getVectorDouble("visregions.boxes.color.red", red, nboxes);
-      std::vector<double> green; config.getVectorDouble("visregions.boxes.color.green", green, nboxes);
-      std::vector<double> blue; config.getVectorDouble("visregions.boxes.color.blue", blue, nboxes);
+      std::vector<double> squashY;
+      config.getVectorDouble("visregions.fieldMaps.squashY", squashY, squashY);
 
 
-      std::vector<double> xmin; config.getVectorDouble("visregions.boxes.xmin", xmin, nboxes);
+      GeomHandle<BFieldManager> fieldMgr;
+
+      const BFieldManager::MapContainerType& maps = fieldMgr->getMapContainer();
+      int mapNumber(0);
+      for(BFieldManager::MapContainerType::const_iterator i=maps.begin(); i!=maps.end(); ++i, ++mapNumber) {
+        const BFMap& field = i->second;
+
+        CLHEP::Hep3Vector mapCenterInMu2e(field.xmin() + field.dx()*(field.nx()-1)/2,
+
+                                          squashY.empty()?
+                                          field.ymin() + field.dy()*(field.ny()-1)/2
+                                          : (squashY[1]+squashY[0])/2.,
+
+                                          field.zmin() + field.dz()*(field.nz()-1)/2
+                                          );
+
+        std::vector<double> mapHalfSize(3);
+        mapHalfSize[0] =  field.dx()*(field.nx()-1)/2.;
+        mapHalfSize[1] =  squashY.empty() ? field.dy()*(field.ny()-1)/2. : (squashY[1]-squashY[0])/2.;
+        mapHalfSize[2] =  field.dz()*(field.nz()-1)/2.;
+
+        int icolor = mapNumber % red.size();
+        G4Colour color(red[icolor], green[icolor], blue[icolor]);
+
+        std::ostringstream boxname;
+        boxname<<"VisualizationMapBox_"<<i->first;
+        nestBox(boxname.str(),
+                mapHalfSize,
+                material,
+                0,
+                mapCenterInMu2e + worldGeom->mu2eOriginInWorld(),
+                worldVolume, 0,
+                true/*visible*/,
+                color,
+                solid,
+                forceAuxEdgeVisible,
+                placePV,
+                doSurfaceCheck
+                );
+
+      }
+    }
+
+    //----------------------------------------------------------------
+    // Add the arbitrary boxes
+
+    if(config.getBool("visregions.boxes.visible")) {
+
+      const bool solid(config.getBool("visregions.boxes.solid"));
+      G4Material *material = findMaterialOrThrow(config.getString("visregions.boxes.material"));
+
+      std::vector<double> red; config.getVectorDouble("visregions.boxes.color.red", red);
+      if(red.empty()) {
+        throw cet::exception("GEOM")<<"constructVisualizationRegions(): visregions.boxes.color.red is empty";
+      }
+      std::vector<double> green; config.getVectorDouble("visregions.boxes.color.green", green, red.size());
+      std::vector<double> blue; config.getVectorDouble("visregions.boxes.color.blue", blue, red.size());
+
+      std::vector<double> xmin; config.getVectorDouble("visregions.boxes.xmin", xmin);
+      const int nboxes = xmin.size();
       std::vector<double> ymin; config.getVectorDouble("visregions.boxes.ymin", ymin, nboxes);
       std::vector<double> zmin; config.getVectorDouble("visregions.boxes.zmin", zmin, nboxes);
 
@@ -80,18 +144,21 @@ namespace mu2e {
         boxHalfSize[1] = (ymax[ibox]-ymin[ibox])/2;
         boxHalfSize[2] = (zmax[ibox]-zmin[ibox])/2;
 
+        int icolor = ibox % red.size();
+        G4Colour color(red[icolor], green[icolor], blue[icolor]);
+
         std::ostringstream boxname;
         boxname<<"VisualizationBox"<<std::setw(3)<<std::setfill('0')<<ibox;
 
         nestBox(boxname.str(),
                 boxHalfSize,
-                findMaterialOrThrow(material[ibox]),
+                material,
                 0,
                 boxCenterInMu2e + worldGeom->mu2eOriginInWorld(),
                 worldVolume, 0,
-                visible[ibox],
-                G4Colour(red[ibox], green[ibox], blue[ibox]),
-                solid[ibox],
+                true/*visible*/,
+                color,
+                solid,
                 forceAuxEdgeVisible,
                 placePV,
                 doSurfaceCheck
