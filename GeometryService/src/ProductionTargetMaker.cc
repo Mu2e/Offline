cff3355b9af2264e11e4f39e4d60d58eb151df68
gandr <>
2012-02-16 05:02:11 +0000

    The nominal position of the production target is fully specified by its Z coordinate; it is at the PS center in XY.

--- GeometryService/src/ProductionTargetMaker.cc
+++ GeometryService/src/ProductionTargetMaker.cc
@@ -8,16 +8,22 @@
 
 #include "GeometryService/inc/ProductionTarget.hh"
 
+#include "GeometryService/inc/GeomHandle.hh"
+#include "BeamlineGeom/inc/Beamline.hh"
+
 namespace mu2e {
 
-  ProductionTargetMaker::ProductionTargetMaker(const SimpleConfig& c) 
+  ProductionTargetMaker::ProductionTargetMaker(const SimpleConfig& c)
     : m_det(new ProductionTarget(c.getDouble("targetPS_rOut"),
-				 c.getDouble("targetPS_halfLength"),
-				 c.getDouble("targetPS_rotX") * CLHEP::degree,
-				 c.getDouble("targetPS_rotY") * CLHEP::degree,
-				 c.getHep3Vector("productionTarget.position")
-				 )
-	    )
+                                 c.getDouble("targetPS_halfLength"),
+                                 c.getDouble("targetPS_rotX") * CLHEP::degree,
+                                 c.getDouble("targetPS_rotY") * CLHEP::degree,
+                                 CLHEP::Hep3Vector(GeomHandle<Beamline>()->solenoidOffset(),
+                                                   0,
+                                                   c.getDouble("productionTarget.zNominal")
+                                                   )
+                                 )
+            )
   {}
 
 }
