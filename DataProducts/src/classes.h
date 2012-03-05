951666eaaf5b33fa5fa816d4701144a5a208f9c4
gandr <>
2012-03-05 20:14:15 +0000

    New persistent class FilterEfficiency

--- DataProducts/src/classes.h
+++ DataProducts/src/classes.h
@@ -1,15 +1,19 @@
 //
 // Build a dictionary.
 //
-// $Id: classes.h,v 1.7 2012/03/01 19:30:35 kutschke Exp $
-// $Author: kutschke $
-// $Date: 2012/03/01 19:30:35 $
+// $Id: classes.h,v 1.8 2012/03/05 20:14:15 gandr Exp $
+// $Author: gandr $
+// $Date: 2012/03/05 20:14:15 $
 //
 // Original author Rob Kutschke
 //
 
 #include "DataProducts/inc/StrawIndex.hh"
+#include "DataProducts/inc/FilterEfficiency.hh"
+
+#include "art/Persistency/Common/Wrapper.h"
 #include "cetlib/map_vector.h"
 #include <vector>
 
 template class std::vector<cet::map_vector_key>;
+template class art::Wrapper<mu2e::FilterEfficiency>;
