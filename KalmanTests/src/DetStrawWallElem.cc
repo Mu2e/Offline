099ccce8ee373d35a2b563486d106d91643ef892
brownd <>
2012-03-22 22:28:54 +0000

    Allow material from inactive hits to stay active if the DOCA to the track still goes through the straw

--- KalmanTests/src/DetStrawWallElem.cc
+++ KalmanTests/src/DetStrawWallElem.cc
@@ -10,6 +10,7 @@
 #include "KalmanTests/inc/TrkStrawHit.hh"
 #include "MatEnv/MatDBInfo.hh"
 #include "TrkBase/TrkDifTraj.hh"
+#include "TrkBase/TrkPoca.hh"
 #include "DetectorModel/DetIntersection.hh"
 #include "TrkBase/TrkErrCode.hh"
 #include "TrkBase/TrkDifTraj.hh"
@@ -24,23 +25,42 @@ namespace mu2e {
 
   bool
   DetStrawWallElem::reIntersect(const Trajectory* traj,DetIntersection& dinter) const {
+    bool retval(false);
+    double pdist(0.0);
+    double pflt(0.0);
 // check if the associated hot is active; if not, return false
-    if(!_strawhit->isActive())return false;
-// update the POCA on the hot if necessary; must cast the trajectory, FIXME!!!!
-    const TrkDifTraj* dtraj = dynamic_cast<const TrkDifTraj*>(traj);
-    assert(dtraj != 0);
-    _strawhit->updatePoca(dtraj);
-// call the base class function
-    DetElem::reIntersect(traj,dinter);
-// Use the straw hit to define how much material has been traversed.  As the pathlen is
-// used to place the material on a Kalman fit, make sure it's not at the identical place as the
-// hit, as that causes confusion
-    dinter.pathlen = _strawhit->fltLen() + 0.01*_strawhit->straw().getRadius();
-// find the trajectory direction at this pathlen
-    Hep3Vector tdir = dtraj->direction(_strawhit->fltLen());
-    double dpath = _strawhit->wallPath(tdir);
-    dinter.pathrange[0] = dinter.pathlen-dpath;
-    dinter.pathrange[1] = dinter.pathlen+dpath;
-    return true;
+    if(_strawhit->isActive()){
+      // update the POCA on the hot if necessary; must cast the trajectory, FIXME!!!!
+      const TrkDifTraj* dtraj = dynamic_cast<const TrkDifTraj*>(traj);
+      assert(dtraj != 0);
+      _strawhit->updatePoca(dtraj);
+      if(_strawhit->pocaStatus().success()){
+	pflt = _strawhit->fltLen();
+	pdist = fabs(_strawhit->driftRadius());
+	retval =  true;
+      }
+    } else {
+// use POCA to determine if the track went through the straw, even if it's inactive
+      TrkPoca poca(*traj,_strawhit->fltLen(),*_strawhit->hitTraj(),_strawhit->hitLen());
+      if(poca.status().success() && fabs(poca.doca()) < _strawhit->straw().getRadius()){
+	pflt = poca.flt1();
+	pdist = fabs(poca.doca());
+	retval = true;
+      }
+    }
+    if(retval){
+      // call the base class function
+      // As the pathlen is
+      // used to place the material on a Kalman fit, make sure it's not at the identical place as the
+      // hit, as that causes confusion
+      DetElem::reIntersect(traj,dinter);
+      dinter.pathlen = pflt + 0.01*_strawhit->straw().getRadius();
+      Hep3Vector tdir = traj->direction(dinter.pathlen);
+      double dpath = _strawhit->wallPath(pdist,tdir);
+      dinter.pathrange[0] = dinter.pathlen-dpath;
+      dinter.pathrange[1] = dinter.pathlen+dpath;
+    }
+    return retval;
   }
 }
+
