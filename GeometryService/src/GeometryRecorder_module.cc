0572445810111f783d6b3d3bd16e1aa68a5f2a45
gandr <>
2012-04-05 21:15:41 +0000

    Recorder module.

---
+++ GeometryService/src/GeometryRecorder_module.cc
@@ -0,0 +1,70 @@
+// Attach select GeometryService data to the run object.
+//
+// Andrei Gaponenko, 2012
+
+#include <memory>
+
+#include "GeometryService/inc/GeometryService.hh"
+#include "GeometryService/inc/GeomHandle.hh"
+#include "GeometryService/inc/WorldG4.hh"
+#include "Mu2eBuildingGeom/inc/Mu2eBuilding.hh"
+#include "ProductionTargetGeom/inc/ProductionTarget.hh"
+#include "ProtonBeamDumpGeom/inc/ProtonBeamDump.hh"
+#include "ExtinctionMonitorFNAL/inc/ExtMonFNAL.hh"
+#include "ProductionSolenoidGeom/inc/PSEnclosure.hh"
+#include "ProductionSolenoidGeom/inc/PSShield.hh"
+
+// art includes.
+#include "fhiclcpp/ParameterSet.h"
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Principal/Run.h"
+#include "art/Framework/Core/ModuleMacros.h"
+
+namespace mu2e {
+
+  //================================================================
+  class GeometryRecorder : public art::EDProducer {
+    template<class DET> void put(art::Run& run) {
+      run.put(std::auto_ptr<DET>(new DET(*GeomHandle<DET>())));
+    }
+
+  public:
+    explicit GeometryRecorder(const fhicl::ParameterSet& pset);
+    virtual void produce(art::Event&) {/*No per-event actions*/}
+    virtual void endRun(art::Run&);
+  };
+
+  //================================================================
+  GeometryRecorder::GeometryRecorder(const fhicl::ParameterSet&) {
+    produces<Mu2eBuilding, art::InRun>();
+    produces<ProductionTarget, art::InRun>();
+    produces<ProtonBeamDump, art::InRun>();
+    produces<ExtMonFNAL::ExtMon, art::InRun>();
+    produces<PSEnclosure, art::InRun>();
+    produces<PSShield, art::InRun>();
+
+    produces<WorldG4, art::InRun>();
+  }
+
+  //================================================================
+  // Better do the work in endRun() than beginRun(), otherwise
+  // the WorldG4 object may not yet be available.
+  void GeometryRecorder::endRun(art::Run& run) {
+    put<Mu2eBuilding>(run);
+    put<ProductionTarget>(run);
+    put<ProtonBeamDump>(run);
+    put<ExtMonFNAL::ExtMon>(run);
+    put<PSEnclosure>(run);
+    put<PSShield>(run);
+
+    art::ServiceHandle<GeometryService> geom;
+    if(geom->hasElement<WorldG4>()) {
+      put<WorldG4>(run);
+    }
+  }
+
+  //================================================================
+
+} // namespace mu2e
+
+DEFINE_ART_MODULE(mu2e::GeometryRecorder);
