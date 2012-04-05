4dc7c015d1a48718807077d1753143e7db765339
gandr <>
2012-04-05 18:16:17 +0000

    Bild genreflex dictionaries for GeomPrimitives objects.

---
+++ GeomPrimitives/src/classes.h
@@ -0,0 +1,10 @@
+// $Id: classes.h,v 1.1 2012/04/05 18:16:17 gandr Exp $
+// $Author: gandr $
+// $Date: 2012/04/05 18:16:17 $
+//
+// Original author Andrei Gaponenko
+//
+
+#include "GeomPrimitives/inc/Polycone.hh"
+#include "GeomPrimitives/inc/Tube.hh"
+#include "GeomPrimitives/inc/TubsParams.hh"
