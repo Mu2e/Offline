d4aea23fabd5eb97560e730fb3326cdf97a8e75c
kutschke <>
2012-06-09 20:03:25 +0000

    First release.

---
+++ Mu2eG4/inc/SensitiveDetectorHelper.hh
@@ -0,0 +1,96 @@
+#ifndef Mu2eG4_SensitiveDetectorHelper_hh
+#define Mu2eG4_SensitiveDetectorHelper_hh
+//
+// Some helper functions to manage repeated tasks related to sensitive detectors.
+//
+// $Id: SensitiveDetectorHelper.hh,v 1.1 2012/06/09 20:03:25 kutschke Exp $
+// $Author: kutschke $
+// $Date: 2012/06/09 20:03:25 $
+//
+// Original author Rob Kutschke
+//
+
+// From Mu2e
+#include "Mu2eG4/inc/Mu2eSensitiveDetector.hh"
+#include "MCDataProducts/inc/StepPointMCCollection.hh"
+#include "MCDataProducts/inc/StepInstanceName.hh"
+
+// From the art tool chain
+#include "fhiclcpp/ParameterSet.h"
+#include "cetlib/maybe_ref.h"
+
+// From C++ and STL
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+// Forward references.
+namespace art   { class Event; }
+namespace fhicl { class ParameterSet; }
+
+namespace mu2e {
+
+  class SensitiveDetectorHelper{
+
+  public:
+
+    SensitiveDetectorHelper( fhicl::ParameterSet const& pset );
+    // Accept compiler generator d'tor, copy c'tor and assignment operator.
+
+    // Register the sensitive detector with this class; to be called after G4 Initialize.
+    void registerSensitiveDetectors();
+
+    // Create empty data products; to be called at the start of each event.
+    void createProducts();
+
+    // Attach the new per-event data products to the corresponding sensitive detector objects.
+    void updateSensitiveDetectors( PhysicsProcessInfo&   info,
+                                   art::ProductID const& simsId,
+                                   art::Event&           event );
+
+    // Put the data products into the event.
+    void put( art::Event& event);
+
+    // Return all of the instances names of the data products to be produced.
+    std::vector<std::string> stepInstanceNamesToBeProduced() const;
+
+    // Return one of the StepPointMCCollections.
+    cet::maybe_ref<StepPointMCCollection> steps( StepInstanceName::enum_type id );
+
+  private:
+
+    // A helper class to hold information about each sensitive detector object.
+    struct StepInstance {
+      explicit StepInstance(StepInstanceName::enum_type astepName ):
+        p(),
+        stepName(StepInstanceName(astepName)),
+        sensitiveDetector(0){
+      }
+
+      explicit StepInstance():
+        p(),
+        stepName(),
+        sensitiveDetector(0){
+      }
+
+      // Accept compiler written d'tor, copy c'tor and assignment operator.
+
+      // See note 1 in the .cc file for why the collection is not held by pointer.
+      // For historical reasons the two names are different; maybe some day we will synchronize them.
+      StepPointMCCollection    p;
+      StepInstanceName         stepName;
+      Mu2eSensitiveDetector *  sensitiveDetector;
+
+    };
+
+    // All of the StepPointMC collections, except the timevd.
+    //std::vector<StepInstance> stepInstances_;
+    typedef std::map<StepInstanceName::enum_type,StepInstance> InstanceMap;
+    InstanceMap stepInstances_;
+
+  };
+
+
+} // end namespace mu2e
+#endif /* Mu2eG4_SensitiveDetectorHelper_hh */
---
+++ Mu2eG4/src/SensitiveDetectorHelper.cc
@@ -0,0 +1,134 @@
+//
+// A helper class manage repeated tasks related to sensitive detectors.
+//
+// $Id: SensitiveDetectorHelper.cc,v 1.1 2012/06/09 20:03:25 kutschke Exp $
+// $Author: kutschke $
+// $Date: 2012/06/09 20:03:25 $
+//
+// Original author Rob Kutschke
+//
+//
+// Notes.
+//
+// 1) The timeVD virtual detector does not use the G4 sensitve detector methodology.
+//    It is, instead, implemented inside SteppingAction.  So it is not managed
+//    by this class.
+//
+// 2) The embedded class StepIntance needs to be copyable so that it can be put into
+//    the std::vector.  Therefore it cannot contain an auto_ptr to its StepPointMC
+//    collection.  The work around is to hold the collection by value and to use swap
+//    to transfer it into the auto_ptr that will be given to the event.  This is
+//    a very small CPU time penalty but it saves us from doing any explicit memory management.
+//
+// 3) Todo:
+//    Add a grammar to the pset to select which sensitive detectors to do.
+//    Perhaps implement a keep/drop model as in the IO configuration.
+//    Should also connect this to the code that creates the sensitive detectors.
+//
+
+// From Mu2e.
+#include "Mu2eG4/inc/SensitiveDetectorHelper.hh"
+#include "MCDataProducts/inc/StatusG4.hh"
+#include "Mu2eG4/inc/SensitiveDetectorName.hh"
+
+// From art and its tool chain.
+#include "art/Framework/Principal/Event.h"
+#include "fhiclcpp/ParameterSet.h"
+
+// From G4.
+#include "G4VSensitiveDetector.hh"
+#include "G4SDManager.hh"
+
+using namespace std;
+
+namespace mu2e {
+
+  // pset argument reserved for future use; see note 3.
+  SensitiveDetectorHelper::SensitiveDetectorHelper( fhicl::ParameterSet const& pset ):
+    stepInstances_(){
+
+    // Build list of StepInstances to look after.  See note 1.
+    std::vector<StepInstanceName> const& allSD(StepInstanceName::allValues());
+    for ( std::vector<StepInstanceName>::const_iterator i=allSD.begin();
+          i != allSD.end(); ++i ){
+      StepInstanceName::enum_type id(*i);
+      if ( id != StepInstanceName::unknown && id !=StepInstanceName::timeVD ){
+        stepInstances_[id] = StepInstance(id);
+      }
+    }
+
+  }
+
+  // Find the sensitive detector objects and attach them to each StepInstance object.
+  // Must not be called until G4 has been initialized.
+  void SensitiveDetectorHelper::registerSensitiveDetectors(){
+    G4SDManager* sdManager = G4SDManager::GetSDMpointer();
+    for ( InstanceMap::iterator i=stepInstances_.begin();
+            i != stepInstances_.end(); ++i ){
+      StepInstance& step(i->second);
+      step.sensitiveDetector = static_cast<Mu2eSensitiveDetector*>(sdManager->FindSensitiveDetector(step.stepName.name().c_str()));
+    }
+  }
+
+  // Create new data products.  To be called at start of each event.
+  void SensitiveDetectorHelper::createProducts(){
+
+    for ( InstanceMap::iterator i=stepInstances_.begin();
+            i != stepInstances_.end(); ++i ){
+      i->second.p.clear();
+    }
+
+  }
+
+  // Hand each sensitive detector object the StepPointMC collection that it will fill.
+  // Also hand it the information needed to create art::Ptr objects.
+  void SensitiveDetectorHelper::updateSensitiveDetectors( PhysicsProcessInfo&   info,
+                                                          art::ProductID const& simsId,
+                                                          art::Event&           event  ){
+
+    for ( InstanceMap::iterator i=stepInstances_.begin();
+          i != stepInstances_.end(); ++i ){
+      StepInstance& instance(i->second);
+      if ( instance.sensitiveDetector ){
+        instance.sensitiveDetector->beforeG4Event(instance.p, info, simsId, event );
+      }
+    }
+
+  }
+
+  // Put all of the data products into the event.
+  // To be called at the end of each event.
+  void SensitiveDetectorHelper::put( art::Event& event ){
+
+    for ( InstanceMap::iterator i=stepInstances_.begin();
+            i != stepInstances_.end(); ++i ){
+      auto_ptr<StepPointMCCollection> p(new StepPointMCCollection);
+      StepInstance& instance(i->second);
+      std::swap( instance.p, *p);
+      event.put(p, instance.stepName.name() );
+    }
+
+  }
+
+  // Return all of the instances names of the data products to be produced.
+  vector<string> SensitiveDetectorHelper::stepInstanceNamesToBeProduced() const{
+    vector<string> names;
+    for ( InstanceMap::const_iterator i=stepInstances_.begin();
+            i != stepInstances_.end(); ++i ){
+      names.push_back( i->second.stepName.name() );
+    }
+    return names;
+  }
+
+  // Return one of the StepPointMCCollections.
+  cet::maybe_ref<StepPointMCCollection> SensitiveDetectorHelper::steps( StepInstanceName::enum_type id ){
+    InstanceMap::iterator i = stepInstances_.find(id);
+    if ( i != stepInstances_.end() ) {
+      return cet::maybe_ref<StepPointMCCollection>(i->second.p);
+    }
+
+    // Cannot find the requested id; return an empty maybe_ref.
+    return cet::maybe_ref<StepPointMCCollection>();
+  }
+
+}  // end namespace mu2e
