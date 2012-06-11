aea0de3820fdfef16eda859b2e41243d9d4923f7
kutschke <>
2012-06-11 05:22:06 +0000

    Fix for compatibility with art::Ptr. See item 34 in release notes v1_1_9.

--- GeneralUtilities/inc/OwningPointerCollection.hh
+++ GeneralUtilities/inc/OwningPointerCollection.hh
@@ -1,24 +1,24 @@
 #ifndef GeneralUtilities_OwningPointerCollection_hh
 #define GeneralUtilities_OwningPointerCollection_hh
 //
-// A class template to take ownership of a collection of bare pointers to 
+// A class template to take ownership of a collection of bare pointers to
 // objects, to provide access to those objects and to delete them when the
-// container object goes out of scope.  
+// container object goes out of scope.
 //
 // This is designed to allow complex objects made on the heap to be used
 // as transient-only data products.
 //
 // The original use is for TrkRecoTrk.
 //
-// $Id: OwningPointerCollection.hh,v 1.2 2011/06/15 20:59:55 kutschke Exp $
+// $Id: OwningPointerCollection.hh,v 1.3 2012/06/11 05:22:06 kutschke Exp $
 // $Author: kutschke $
-// $Date: 2011/06/15 20:59:55 $
+// $Date: 2012/06/11 05:22:06 $
 //
 // Original author Rob Kutschke
 //
 // Notes:
 // 1) I believe it would be wise to make the underlying container:
-//      std::vector<boost::right_kind_of_smart_pointer>.
+//      std::vector<boost::right_kind_of_smart_pointer<T>>.
 //    scoped_ptr will not work since they are not copyable ( needed for vector ).
 //    unique_ptr seems mostly concerned with threads.
 //    I tried shared_ptr but I could not make it play nice with genreflex.
@@ -33,15 +33,18 @@ namespace mu2e {
 
   public:
 
+    typedef typename std::vector<const T*>::const_iterator const_iterator;
+    typedef T const* value_type;
+
     OwningPointerCollection():v_(){}
 
     // Caller transfers ownership of the pointees to us.
-    explicit OwningPointerCollection( std::vector<T*> v ):
+    explicit OwningPointerCollection( std::vector<T*>& v ):
       v_(v.begin(), v.end() ){
     }
 
     // Caller transfers ownership of the pointees to us.
-    explicit OwningPointerCollection( std::vector<T const*> v ):
+    explicit OwningPointerCollection( std::vector<T const*>& v ):
       v_(v){
     }
 
@@ -62,6 +65,14 @@ namespace mu2e {
       v_.push_back(t);
     }
 
+    typename std::vector<const T*>::const_iterator begin() const{
+      return v_.begin();
+    }
+
+    typename std::vector<const T*>::const_iterator end() const{
+      return v_.end();
+    }
+
     // Possibly needed by producer modules?
     /*
     void pop_back( ){
@@ -77,8 +88,8 @@ namespace mu2e {
 
     // Accessors: this container retains ownership of the pointees.
     size_t   size()                    const { return  v_.size(); }
-    T const& operator[](std::size_t i) const { return *v_.at(i);  }
-    T const& at        (std::size_t i) const { return *v_.at(i);  }
+    T const* operator[](std::size_t i) const { return  v_.at(i);  }
+    T const* at        (std::size_t i) const { return  v_.at(i);  }
     T const* get       (std::size_t i) const { return  v_.at(i);  }
 
     // const access to the underlying container.
