ee69db1ad628537496a0c8fa600d6bd954fab93f
kutschke <>
2012-07-02 15:50:50 +0000

    Added printout of package names inside the loop.

--- BaBar/update.sh
+++ BaBar/update.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#
+# Check the status of packages of interest
+#
+# $Id: update.sh,v 1.3 2012/07/02 15:50:50 kutschke Exp $
+# $Date: 2012/07/02 15:50:50 $
+# $Author: kutschke $
+#
+# Contact person Rob Kutschke
+#
+
 for PKG in BField BaBar BbrGeom CLHEP DetectorModel KalmanTrack MatEnv ProbTools TrajGeom TrkBase difAlgebra
-do svn update BaBar/$PKG
+do
+  echo "Checking for updates in " $PKG
+  svn update BaBar/$PKG
 done
