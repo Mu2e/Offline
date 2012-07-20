6d80e9ce692dc367a1471f0dac4fbd8f53864d74
kutschke <>
2012-07-20 22:38:12 +0000

    Add printout and more histograms.

--- KalmanTests/src/ReadKalFits_module.cc
+++ KalmanTests/src/ReadKalFits_module.cc
@@ -1,9 +1,9 @@
 //
 // Read the tracks added to the event by KalFitTest_module.
 //
-// $Id: ReadKalFits_module.cc,v 1.8 2012/06/12 21:06:20 brownd Exp $
-// $Author: brownd $
-// $Date: 2012/06/12 21:06:20 $
+// $Id: ReadKalFits_module.cc,v 1.9 2012/07/20 22:38:12 kutschke Exp $
+// $Author: kutschke $
+// $Date: 2012/07/20 22:38:12 $
 //
 // Original author Rob Kutschke
 //
@@ -50,7 +50,7 @@ namespace mu2e {
     void beginJob();
     void analyze(const art::Event& e);
 
-// DIO spectrum
+    // DIO spectrum
     static double DIOspectrum(double eenergy);
 
   private:
@@ -59,15 +59,24 @@ namespace mu2e {
     std::string _fitterModuleLabel;
     // whether to weight the DIO or not
     bool _weight;
-  // diagnostic of Kalman fit
+    // diagnostic of Kalman fit
     KalFitMC _kfitmc;
- 
+
+    // Control level of printout.
+    int _verbosity;
+    int _maxPrint;
+
     // Histograms
     TH1F* _hNTracks;
     TH1F* _hfitCL;
     TH1F* _hChisq;
+    TH1F* _hmomentum0;
+    TH1F* _hdp;
+    TH1F* _hz0;
 
     TTree* _trkdiag;
+
+
     Int_t _trkid,_eventid;
     Float_t _diowt;
 
@@ -77,18 +86,26 @@ namespace mu2e {
     _fitterModuleLabel(pset.get<string>("fitterModuleLabel")),
     _weight(pset.get<bool>("WeightEvents",true)),
     _kfitmc(pset.get<fhicl::ParameterSet>("KalFitMC")),
+    _verbosity(pset.get<int>("verbosity",0)),
+    _maxPrint(pset.get<int>("maxPrint",0)),
     _hNTracks(0),
     _hfitCL(0),
     _hChisq(0),
+    _hmomentum0(0),
+    _hdp(0),
+    _hz0(0),
     _trkdiag(0){
   }
 
   void ReadKalFits::beginJob( ){
     art::ServiceHandle<art::TFileService> tfs;
-    _hNTracks = tfs->make<TH1F>( "hNTracks", "Number of tracks per event.",         10, 0.,   10. );
-    _hfitCL   = tfs->make<TH1F>( "hfitCL",   "Confidence Level of the Track fit.",  50, 0.,    1. );
-    _hChisq   = tfs->make<TH1F>( "hChisq",   "Chisquared of the Track fit.",       100, 0.,  500. );
-    _trkdiag = _kfitmc.createTrkDiag();
+    _hNTracks   = tfs->make<TH1F>( "hNTracks",  "Number of tracks per event.",         10,     0.,   10. );
+    _hfitCL     = tfs->make<TH1F>( "hfitCL",    "Confidence Level of the Track fit.",  50,     0.,    1. );
+    _hChisq     = tfs->make<TH1F>( "hChisq",    "Chisquared of the Track fit.",       100,     0.,  500. );
+    _hmomentum0 = tfs->make<TH1F>( "hmomentum", "Reco Momentum at front face",        100,    70.,  110. );
+    _hdp        = tfs->make<TH1F>( "hdp",       "Momentum Change across Tracker",     100,   -10.,    0. );
+    _hz0        = tfs->make<TH1F>( "hz0",       "z of start valid region (cm)",       100, -1500., -800. );
+    _trkdiag    = _kfitmc.createTrkDiag();
 // add local branches
     _trkdiag->Branch("eventid",&_eventid,"eventid/I");
     _trkdiag->Branch("trkid",&_trkid,"trkid/I");
@@ -98,6 +115,8 @@ namespace mu2e {
 
   // For each event, look at tracker hits and calorimeter hits.
   void ReadKalFits::analyze(const art::Event& event) {
+    cout << "Enter ReadKalFits:: analyze: " << _verbosity << endl;
+
     _eventid++;
     _kfitmc.findMCData(event);
     // DIO spectrum weight; use the generated momenum magnitude
@@ -110,6 +129,10 @@ namespace mu2e {
     event.getByLabel(_fitterModuleLabel,trksHandle);
     TrkRecoTrkCollection const& trks = *trksHandle;
 
+    if ( _verbosity > 0 && _eventid <= _maxPrint ){
+      cout << "ReadKalmanFits  for event: " << event.id() << "  Number of fitted tracks: " << trks.size() << endl;
+    }
+
     _hNTracks->Fill( trks.size() );
     _trkid = -1;
     for ( size_t i=0; i< trks.size(); ++i ){
@@ -119,15 +142,43 @@ namespace mu2e {
       if ( !trep ) continue;
 
       _kfitmc.trkDiag(trk);
- 
+
       // For some quantities you require the concrete representation, not
       // just the base class.
       KalRep const* krep = dynamic_cast<KalRep const*>(trep);
       if ( !krep ) continue;
 
       // Fill a histogram.
-      _hfitCL->Fill(trep->chisqConsistency().likelihood() );
-      _hChisq->Fill(trep->chisqConsistency().chisqValue() );
+      _hfitCL->Fill(krep->chisqConsistency().likelihood() );
+      _hChisq->Fill(krep->chisqConsistency().chisqValue() );
+
+      double s0   = krep->startValidRange();
+      double sEnd = krep->endValidRange();
+
+      Hep3Vector momentum0   = krep->momentum(s0);
+      Hep3Vector momentumEnd = krep->momentum(sEnd);
+      HepPoint   pos0        = krep->position(s0);
+      double dp              = momentumEnd.mag()-momentum0.mag();
+
+      _hmomentum0->Fill(momentum0.mag());
+      _hdp->Fill(dp);
+
+      _hz0->Fill(pos0.z());
+
+      if ( _verbosity > 1 && _eventid <= _maxPrint ){
+        cout << "   Fitted track: "
+             << i            << " "
+             << krep->nDof() << " "
+             << krep->chisqConsistency().likelihood()  << " | "
+             << s0                        << " "
+             << krep->startFoundRange()   << " | "
+             << sEnd                      << " "
+             << krep->endFoundRange()     << " | "
+             << momentum0.mag()           << " "
+             << momentumEnd.mag()         << " | "
+             << pos0.z()
+             << endl;
+      }
 
     }
 // if there are no tracks, enter dummies
