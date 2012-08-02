44776e934e5e828540747592f705d8154813de42
greenc <>
2012-08-02 15:10:14 +0000

    Ensure we're in the correct directory as assumed by the update / status code.

--- BaBar/status.sh
+++ BaBar/status.sh
@@ -2,12 +2,17 @@
 #
 # Check the status of packages of interest
 #
-# $Id: status.sh,v 1.2 2012/07/02 15:50:50 kutschke Exp $
-# $Date: 2012/07/02 15:50:50 $
-# $Author: kutschke $
+# $Id: status.sh,v 1.3 2012/08/02 15:10:14 greenc Exp $
+# $Date: 2012/08/02 15:10:14 $
+# $Author: greenc $
 #
 # Contact person Rob Kutschke
 #
+
+if ! [[ -f "SConstruct" ]]; then
+  echo "ERROR: must be executed from top level of offline code." 1>&2
+  exit 1
+fi
 
 list="BaBar BbrGeom BField CLHEP DetectorModel KalmanTrack MatEnv ProbTools TrajGeom TrkBase difAlgebra"
 for package in ${list}
--- BaBar/update.sh
+++ BaBar/update.sh
@@ -2,15 +2,20 @@
 #
 # Check the status of packages of interest
 #
-# $Id: update.sh,v 1.3 2012/07/02 15:50:50 kutschke Exp $
-# $Date: 2012/07/02 15:50:50 $
-# $Author: kutschke $
+# $Id: update.sh,v 1.4 2012/08/02 15:10:14 greenc Exp $
+# $Date: 2012/08/02 15:10:14 $
+# $Author: greenc $
 #
 # Contact person Rob Kutschke
 #
 
+if ! [[ -f "SConstruct" ]]; then
+  echo "ERROR: must be executed from top level of offline code." 1>&2
+  exit 1
+fi
+
 for PKG in BField BaBar BbrGeom CLHEP DetectorModel KalmanTrack MatEnv ProbTools TrajGeom TrkBase difAlgebra
 do
-  echo "Checking for updates in " $PKG
-  svn update BaBar/$PKG
+  echo "Checking for updates in $PKG"
+  svn update "BaBar/$PKG"
 done
