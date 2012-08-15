93ae4396d85cdd45b0fd0388312db4193a61c184
ehrlich <>
2012-08-15 04:03:28 +0000

    added the option of heavy concrete for the hall ceiling

--- Mu2eG4/src/constructHall.cc
+++ Mu2eG4/src/constructHall.cc
@@ -1,9 +1,9 @@
 //
 // Free function to create the hall walls and hall interior inside the earthen overburden.
 //
-// $Id: constructHall.cc,v 1.13 2012/04/17 19:56:56 gandr Exp $
-// $Author: gandr $
-// $Date: 2012/04/17 19:56:56 $
+// $Id: constructHall.cc,v 1.14 2012/08/15 04:03:28 ehrlich Exp $
+// $Author: ehrlich $
+// $Date: 2012/08/15 04:03:28 $
 //
 // Original author KLG based on Mu2eWorld constructHall
 //
@@ -27,6 +27,7 @@
 #include "G4Orb.hh"
 #include "G4TwoVector.hh"
 #include "CLHEP/Vector/Rotation.h"
+#include "G4NistManager.hh"
 
 #include <vector>
 #include <algorithm>
@@ -45,6 +46,33 @@ namespace mu2e {
 
     // Materials for the hall walls, floor, and ceiling
     G4Material* wallMaterial = materialFinder.get("hall.wallMaterialName");
+
+    G4Material* ceilingMaterial = wallMaterial;
+    if(config.getBool("hall.useHeavyConcreteCeiling",false))
+    {
+      G4Material* heavyConcrete = new G4Material("HeavyConcrete",3.295e3*kg/m3,17);
+      G4NistManager* man = G4NistManager::Instance();
+      heavyConcrete->AddElement(man->FindOrBuildElement("H"), 0.01048482);  //Hydrogen
+      heavyConcrete->AddElement(man->FindOrBuildElement("B"),  0.00943758); //Boron
+      heavyConcrete->AddElement(man->FindOrBuildElement("C"),  0.0129742);  //Carbon
+      heavyConcrete->AddElement(man->FindOrBuildElement("O"),  0.27953541); //Oxygen
+      heavyConcrete->AddElement(man->FindOrBuildElement("F"),  1.5175E-4);  //Fluorine
+      heavyConcrete->AddElement(man->FindOrBuildElement("Na"), 3.7014E-4);  //Sodium
+      heavyConcrete->AddElement(man->FindOrBuildElement("Mg"), 0.08298213); //Magnesium
+      heavyConcrete->AddElement(man->FindOrBuildElement("Al"), 0.02769028); //Aluminum
+      heavyConcrete->AddElement(man->FindOrBuildElement("Si"), 0.06317253); //Silicon
+      heavyConcrete->AddElement(man->FindOrBuildElement("P"), 0.00176963);  //Phosphorus
+      heavyConcrete->AddElement(man->FindOrBuildElement("S"), 5.8275E-4);   //Sulfur
+      heavyConcrete->AddElement(man->FindOrBuildElement("K"), 4.2024E-4);   //Potassium
+      heavyConcrete->AddElement(man->FindOrBuildElement("Ca"), 0.03227609); //Calcium
+      heavyConcrete->AddElement(man->FindOrBuildElement("Ti"), 5.457E-5);   //Titanium
+      heavyConcrete->AddElement(man->FindOrBuildElement("Mn"), 0.00321757); //Manganese
+      heavyConcrete->AddElement(man->FindOrBuildElement("Fe"), 0.47423935); //Iron
+      heavyConcrete->AddElement(man->FindOrBuildElement("Sr"), 6.4097E-4);  //Strontium
+      ceilingMaterial = heavyConcrete;
+      std::cout<<"Heavy Concrete used for the Hall Ceiling"<<std::endl;
+      std::cout<<ceilingMaterial<<std::endl;
+    }
 
     GeomHandle<WorldG4> world;
     GeomHandle<Mu2eBuilding> building;
@@ -121,7 +149,7 @@ namespace mu2e {
                                             G4TwoVector(0,0), 1., G4TwoVector(0,0), 1.);
 
     finishNesting(hallCeiling,
-                  wallMaterial,
+                  ceilingMaterial,
                   &horizontalConcreteRotation,
                   hallCeiling.centerInParent,
                   hallInfo.logical,
