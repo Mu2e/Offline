3e2cb31ed2714edc78a0a1f09041f36a8e203f81
genser <>
2012-08-23 23:17:19 +0000

    Root c++ function to test MakeStrawHit_module (and ReadStrawHits_module)

---
+++ Mu2eG4/test/strawHits.C
@@ -0,0 +1,170 @@
+//
+// Root c++ function to test MakeStrawHit_module
+// 
+// $Id: strawHits.C,v 1.1 2012/08/23 23:17:19 genser Exp $
+// $Author: genser $
+// $Date: 2012/08/23 23:17:19 $
+// 
+// Original author KLG based on Rob Kutschke's example
+//
+// 1) Retrieve histograms and ntuples from the file that was created
+//    by mixExample01.fcl
+//
+// 2) Draw the histograms to the screen (called a canvas).
+//
+// 3) Split a canvas into multiple pads and draw a different histogram
+//    in each pad.
+//
+// 4) Save the canvas in a format suitable for printing ( postscript )
+//    or in a format suitable for inclusion in other documents
+//    ( png, jpg, gif ).
+//
+
+// run it in root e.g. like .x strawHits.C++ (or .x strawHits.C++g)
+
+#include <TROOT.h>
+#include <TSystem.h>
+#include <TFile.h>
+
+#include <TH1.h>
+#include <TStyle.h>
+#include <TString.h>
+#include <TPad.h>
+#include <TCanvas.h>
+#include <TTree.h>
+
+#include <sstream>
+#include <iostream>
+#include <vector>
+
+void strawHits()
+{
+
+  // With this you can reinvoke the script without exiting root.
+  gROOT->Reset();
+
+  // Get rid of grey background (ugly for print out).
+  gROOT->SetStyle("Plain");
+
+  // Statistics box for histograms should include all of:
+  // number of Entries, Mean, Rms, Underflows, Overflows
+  gStyle->SetOptStat("emruo");
+
+  // Base name of input file and of all plot files.
+  TString basename("strawHits_01");
+
+  // Open the input file that contains histograms and ntuples
+  // made by ReadStrawHits_module
+
+  std::vector<TFile*> file;
+
+  file.push_back(new TFile("mixExample01_2000_before_xtalk_off.root"));
+  file.push_back(new TFile("mixExample01_2000_after_xtalk_off_e2a_off.root"));
+  file.push_back(new TFile("mixExample01_2000_after_xtalk_on2p_e2a_off.root"));
+  file.push_back(new TFile("mixExample01_2000_after_xtalk_off_e2a_on.root"));
+  file.push_back(new TFile("mixExample01_2000_after_xtalk_on2p_e2a_on.root"));
+  file.push_back(new TFile("mixExample01_2000_after_xtalk_on1p_e2a_on.root"));
+
+  const int nfiles = file.size();
+
+  // Name of the output pdf file.
+  // Postscript is the only graphics format for which root supports multi-page output files.
+  TString psfile( basename + ".pdf");
+
+  std::vector<TH1F*> _hHitTime(nfiles);      
+  std::vector<TH1F*> _hHitDeltaTime(nfiles); 
+  std::vector<TH1F*> _hHitEnergy(nfiles);    
+  std::vector<TH1F*> _hNHits(nfiles);        
+  std::vector<TH1F*> _hNHitsPerWire(nfiles); 
+  std::vector<TH1F*> _hDriftTime(nfiles);    
+  std::vector<TH1F*> _hDriftDistance(nfiles);
+  std::vector<TH1F*> _hDistanceToMid(nfiles);
+  std::vector<TH1F*> _hNG4Steps(nfiles);     
+  std::vector<TH1F*> _hG4StepLength(nfiles); 
+  std::vector<TH1F*> _hG4StepEdep(nfiles);   
+  TH1F* _tmp;
+
+  for (int ii=0; ii!=nfiles; ++ii) {
+    file[ii]->GetObject("readStrawHits/hHitTime",       _tmp); assert(_tmp!=0); _hHitTime[ii]=_tmp;
+    file[ii]->GetObject("readStrawHits/hHitDeltaTime",  _tmp); assert(_tmp!=0); _hHitDeltaTime[ii]=_tmp;
+    file[ii]->GetObject("readStrawHits/hHitEnergy",     _tmp); assert(_tmp!=0); _hHitEnergy[ii]=_tmp;
+    file[ii]->GetObject("readStrawHits/hNHits",         _tmp); assert(_tmp!=0); _hNHits[ii]=_tmp;
+    file[ii]->GetObject("readStrawHits/hNHitsPerWire",  _tmp); assert(_tmp!=0); _hNHitsPerWire[ii]=_tmp;
+    file[ii]->GetObject("readStrawHits/hDriftTime",     _tmp); assert(_tmp!=0); _hDriftTime[ii]=_tmp;
+    file[ii]->GetObject("readStrawHits/hDriftDistance", _tmp); assert(_tmp!=0); _hDriftDistance[ii]=_tmp;
+    file[ii]->GetObject("readStrawHits/hDistanceToMid", _tmp); assert(_tmp!=0); _hDistanceToMid[ii]=_tmp;
+    file[ii]->GetObject("readStrawHits/hNG4Steps",      _tmp); assert(_tmp!=0); _hNG4Steps[ii]=_tmp;
+    file[ii]->GetObject("readStrawHits/hG4StepLength",  _tmp); assert(_tmp!=0); _hG4StepLength[ii]=_tmp;
+    file[ii]->GetObject("readStrawHits/hG4StepEdep",    _tmp); assert(_tmp!=0); _hG4StepEdep[ii]=_tmp;
+  }
+
+  const int nhistograms = 11;
+  std::vector<std::vector<TH1F*>*> _histograms(nhistograms);
+
+  //  TH1F** _histograms[11];
+
+  _histograms[ 0] = &_hHitTime;      // *_histograms[ 0] is _hHitTime;
+  _histograms[ 1] = &_hHitDeltaTime;
+  _histograms[ 2] = &_hHitEnergy;
+  _histograms[ 3] = &_hNHits;
+  _histograms[ 4] = &_hNHitsPerWire;
+  _histograms[ 5] = &_hDriftTime;
+  _histograms[ 6] = &_hDriftDistance;
+  _histograms[ 7] = &_hDistanceToMid;
+  _histograms[ 8] = &_hNG4Steps;
+  _histograms[ 9] = &_hG4StepLength;
+  _histograms[10] = &_hG4StepEdep;
+
+  // Get a pointer to the ntuple.
+  // TNtuple* nt; file]->GetObject("readStrawHit/ntup",nt);
+
+  // Open a new canvas on the screen.
+  // The last two arguments are the size of the window.
+  TCanvas *canvas = new TCanvas("c", "Plots from " + basename, 900, 900 );
+
+  // Open a multi-page output pdf file .
+  canvas->Print( psfile+"[");
+
+  for (int jj=0; jj!=nhistograms; ++jj) {
+
+    // Clear canvas in preparation for next page.
+    // Split the canvas into 6 pads.
+    canvas->cd(0);
+    canvas->Clear();
+    canvas->Divide(2,3);
+
+    // Draw some histograms, one per pad.
+    // cd(n): move to graphics pad number "n".
+    // "H9": draw outline histogram ("H") in high resolution mode (9)
+
+    for (int ii=0; ii!=nfiles; ++ii) {
+      canvas->cd(ii+1);
+      cout << jj << " " << ii << " Drawing " << ((*_histograms[jj])[ii])->GetTitle() <<endl;
+      ((*_histograms[jj])[ii])->Draw("H9");
+    }
+
+    // Flush page to screen
+    canvas->Update();
+
+    // Add this canvas to the pdf file.
+    canvas->Print(psfile);
+
+    // Prompt and wait for response before continuing.
+    cerr << "Double click in the last active pad to continue: " ;
+    gPad->WaitPrimitive();
+    cerr << endl;
+
+  }
+
+  // Uncomment this line to save this canvas as a png file (slow)
+  //canvas->Print( basename + "_1.png" );
+
+  // Close the pdf file.
+  canvas->Print(psfile+"]");
+
+}
+
+// This tells emacs to view this file in c++ mode.
+// Local Variables:
+// mode:c++
+// End:
