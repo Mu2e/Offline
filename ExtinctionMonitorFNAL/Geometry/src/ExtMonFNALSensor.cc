c4617fab985a144538f7069ac14bc0ad52c36f2e
gandr <>
2012-08-31 18:48:08 +0000

    ExtMonFNALSensor: use the offline pixel numbering convention to be consistent with the rest of the code.

--- ExtinctionMonitorFNAL/Geometry/src/ExtMonFNALSensor.cc
+++ ExtinctionMonitorFNAL/Geometry/src/ExtMonFNALSensor.cc
@@ -19,20 +19,20 @@ namespace mu2e {
                                                 double ySensor) const
   {
     // We assume there are 2x2 chips per sensor, one per coordinate quadrant.
-    const int icx = xSensor < 0 ? 0 : 1;
-    const int icy = ySensor < 0 ? 0 : 1;
+    const unsigned icx = xSensor < 0 ? 0 : 1;
+    const unsigned icy = ySensor < 0 ? 0 : 1;
     ExtMonFNALChipId cid(sid, icx, icy);
 
     // Assume no gaps between chips
     const double chipx0 = (icx - 1)*chip_.nColumns()*chip_.xPitch();
     const double chipy0 = (icy - 1)*chip_.nRows()*chip_.yPitch();
 
-    // Pixel column and row numbers are 1-based for FE-I4
-    const int ix = 1 + std::floor((xSensor - chipx0)/chip_.xPitch());
-    const int iy = 1 + std::floor((ySensor - chipy0)/chip_.yPitch());
+    // Zero based pixel column and row numbers for the offline identifier
+    const int ix = std::floor((xSensor - chipx0)/chip_.xPitch());
+    const int iy = std::floor((ySensor - chipy0)/chip_.yPitch());
 
     ExtMonFNALPixelId res =
-      (0 < ix)&&(ix <= chip_.nColumns())&&(0 < iy)&&(iy <= chip_.nRows()) ?
+      (0 <= ix)&&(unsigned(ix) < chip_.nColumns())&&(0 <= iy)&&(unsigned(iy) < chip_.nRows()) ?
       ExtMonFNALPixelId(cid, ix, iy) :
       ExtMonFNALPixelId();
 
@@ -43,12 +43,12 @@ namespace mu2e {
   CLHEP::Hep2Vector ExtMonFNALSensor::sensorCoordinates(const ExtMonFNALPixelId& id) const
   {
     // Assume no gaps between chips
-    double chipx0 = (id.chip().chipCol() - 1)*chip_.nColumns()*chip_.xPitch();
-    double chipy0 = (id.chip().chipRow() - 1)*chip_.nRows()*chip_.yPitch();
+    double chipx0 = id.chip().chipCol()*chip_.nColumns()*chip_.xPitch();
+    double chipy0 = id.chip().chipRow()*chip_.nRows()*chip_.yPitch();
 
-    // Subtract 0.5 to get pixel center in the 1-based numbering convention
-    const double xSensor = chipx0 + chip_.xPitch()*(id.col() - 0.5);
-    const double ySensor = chipy0 + chip_.yPitch()*(id.row() - 0.5);
+    // Add 0.5 to get pixel center in the 0-based numbering convention
+    const double xSensor = chipx0 + chip_.xPitch()*(id.col() + 0.5);
+    const double ySensor = chipy0 + chip_.yPitch()*(id.row() + 0.5);
 
     return CLHEP::Hep2Vector(xSensor, ySensor);
   }
