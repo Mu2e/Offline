049aa9b8c146a7fc4bdfce9a4681b2716dcfe0a6
brownd <>
2012-09-26 18:12:50 +0000

    Checkout a specific revision for production

--- BaBar/checkout.sh
+++ BaBar/checkout.sh
@@ -2,9 +2,9 @@
 #
 # Checkout the packages of interest
 #
-# $Id: checkout.sh,v 1.1 2011/10/13 22:20:42 kutschke Exp $
-# $Date: 2011/10/13 22:20:42 $
-# $Author: kutschke $
+# $Id: checkout.sh,v 1.2 2012/09/26 18:12:50 brownd Exp $
+# $Date: 2012/09/26 18:12:50 $
+# $Author: brownd $
 #
 # Contact person Rob Kutschke
 #
@@ -14,7 +14,7 @@ cd BaBar
 list="BaBar BbrGeom BField CLHEP DetectorModel KalmanTrack MatEnv ProbTools TrajGeom TrkBase difAlgebra"
 for package in ${list}
 do
-  svn co https://opteron05.lbl.gov/svn/MU2E/${package}/trunk ${package}
+  svn co -r 564 https://opteron05.lbl.gov/svn/MU2E/${package}/trunk ${package}
 done
 
 cd ..
