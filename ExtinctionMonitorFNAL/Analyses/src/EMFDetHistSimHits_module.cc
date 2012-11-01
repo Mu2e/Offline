c407f23cc8ae04dbf05130642709a67eea0d174b
gandr <>
2012-11-01 23:42:56 +0000

    Histogram SimHits in digitization jobs (could be better done in g4s2).

--- ExtinctionMonitorFNAL/Analyses/src/EMFDetHistSimHits_module.cc
+++ ExtinctionMonitorFNAL/Analyses/src/EMFDetHistSimHits_module.cc
@@ -11,6 +11,7 @@
 #include "fhiclcpp/ParameterSet.h"
 #include "art/Framework/Core/EDAnalyzer.h"
 #include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Run.h"
 #include "art/Framework/Core/ModuleMacros.h"
 
 #include "MCDataProducts/inc/ExtMonFNALSimHitCollection.hh"
@@ -20,47 +21,60 @@
 #include "GeometryService/inc/GeomHandle.hh"
 
 namespace mu2e {
+  namespace ExtMonFNAL {
 
-  //================================================================
-  class EMFDetHistSimHits : public art::EDAnalyzer {
-    std::string _inModuleLabel;
-    std::string _inInstanceName;
+    //================================================================
+    class EMFDetHistSimHits : public art::EDAnalyzer {
+      std::string _inModuleLabel;
+      std::string _inInstanceName;
+      std::string geomModuleLabel_;
+      std::string geomInstanceName_;
 
-    // This is a workaround for geometry not being available at beginJob()
-    bool booked_;
-    EMFSimHitHistograms ch_;
+      // This is a workaround for geometry not being available at beginJob()
+      bool booked_;
+      EMFSimHitHistograms ch_;
 
-  public:
-    explicit EMFDetHistSimHits(const fhicl::ParameterSet& pset);
-    virtual void beginRun(const art::Run& run);
-    virtual void analyze(const art::Event& event);
-  };
+    public:
+      explicit EMFDetHistSimHits(const fhicl::ParameterSet& pset);
+      virtual void beginRun(const art::Run& run);
+      virtual void analyze(const art::Event& event);
+    };
 
-  //================================================================
-  EMFDetHistSimHits::EMFDetHistSimHits(const fhicl::ParameterSet& pset)
-    : _inModuleLabel(pset.get<std::string>("inputModuleLabel"))
-    , _inInstanceName(pset.get<std::string>("inputInstanceName"))
-    , booked_(false)
-  {}
+    //================================================================
+    EMFDetHistSimHits::EMFDetHistSimHits(const fhicl::ParameterSet& pset)
+      : _inModuleLabel(pset.get<std::string>("inputModuleLabel"))
+      , _inInstanceName(pset.get<std::string>("inputInstanceName", ""))
+      , geomModuleLabel_(pset.get<std::string>("geomModuleLabel"))
+      , geomInstanceName_(pset.get<std::string>("geomInstanceName", ""))
+      , booked_(false)
+    {}
 
-  //================================================================
-  void EMFDetHistSimHits::beginRun(const art::Run&) {
-    // This is a workaround for geometry not being available at beginJob()
-    if(!booked_) {
-      booked_ = true;
-      GeomHandle<ExtMonFNAL::ExtMon> extmon;
-      ch_.book(*extmon);
+    //================================================================
+    void EMFDetHistSimHits::beginRun(const art::Run& run) {
+      // This is a workaround for geometry not being available at beginJob()
+      if(!booked_) {
+        booked_ = true;
+        if(!geomModuleLabel_.empty()) {
+          art::Handle<ExtMonFNAL::ExtMon> extmon;
+          run.getByLabel(geomModuleLabel_, geomInstanceName_, extmon);
+          ch_.book(*extmon);
+        }
+        else {
+          GeomHandle<ExtMonFNAL::ExtMon> extmon;
+          ch_.book(*extmon);
+        }
+      }
     }
-  }
 
-  //================================================================
-  void EMFDetHistSimHits::analyze(const art::Event& event) {
-    art::Handle<ExtMonFNALSimHitCollection> ih;
-    event.getByLabel(_inModuleLabel, _inInstanceName, ih);
-    ch_.fill(*ih);
-  }
+    //================================================================
+    void EMFDetHistSimHits::analyze(const art::Event& event) {
+      art::Handle<ExtMonFNALSimHitCollection> ih;
+      event.getByLabel(_inModuleLabel, _inInstanceName, ih);
+      ch_.fill(*ih);
+    }
 
-  //================================================================
+    //================================================================
+  } // namespace ExtMonFNAL
 } // namespace mu2e
 
-DEFINE_ART_MODULE(mu2e::EMFDetHistSimHits);
+DEFINE_ART_MODULE(mu2e::ExtMonFNAL::EMFDetHistSimHits);
