b65e04a03fd9977cd6c910055008d29dc9bf0acb
kutschke <>
2012-11-15 23:11:51 +0000

    Selected svn revision 579

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #
-# $Id: setup.sh,v 1.44 2012/09/24 18:56:48 gandr Exp $
-# $Author: gandr $
-# $Date: 2012/09/24 18:56:48 $
+# $Id: setup.sh,v 1.45 2012/11/15 23:11:51 kutschke Exp $
+# $Author: kutschke $
+# $Date: 2012/11/15 23:11:51 $
 #
 # Original author Rob Kutschke
 #
@@ -75,7 +75,7 @@ if [  -f "./BaBar/makeInclude.sh" ]; then
   ./BaBar/makeInclude.sh
   if [ ! -f "BaBar/BaBar/include/BaBar.hh" ]; then
    echo "Checking out the BaBar Kalman Filter code."
-   ./BaBar/checkout.sh
+   ./BaBar/checkout.sh "-r 579"
   else
    echo "BaBar Kalman filter code already present. Not checking it out."
   fi
