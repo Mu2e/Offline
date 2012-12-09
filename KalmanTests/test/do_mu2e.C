ebc2ed60fc48c4d50d43689bcb33dc6dc95a36ba
brownd <>
2012-12-09 00:38:24 +0000

    update script

--- KalmanTests/test/do_mu2e.C
+++ KalmanTests/test/do_mu2e.C
@@ -1,16 +1,12 @@
 {  
   gROOT->LoadMacro("Scripts/FillChain.C");
   gROOT->LoadMacro("KalmanTests/test/mu2e.C+");
-  gROOT->LoadMacro("KalmanTests/test/KalFit.C+");
   TChain* con(0);
   TChain* dio(0);
-  char condir[50] = {"/data/mixc_4.5441689"};
-  char diodir[50] = {"/data/mixDIO4.5473031"};
-  FillChain(con,condir,"mixConversion.root",10);
-  FillChain(dio,diodir,"mixDIO.root",40);
-  mu2e(dio,con,10.0,1000000,100000,103.4,104.8,true,"_4sig.png");
-  KalCuts();
-  KalFitAcc(con);
-  acan->SaveAs("acc_4sig.png");
+  char condir[50] = {"/data/mixConversion.6418952"};
+  char diodir[50] = {"/data/mixDIO.6430701"};
+  FillChain(con,condir,"mixConversion.root",10,"ReadKalFits/trkdiag");
+  FillChain(dio,diodir,"mixDIO.root",40,"ReadKalFits/trkdiag");
+  mu2e(dio,con,10.0,1000000,100000,103.4,104.8,true);
 }
 
