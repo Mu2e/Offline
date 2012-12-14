c0aafdad79eee49f6c52af3d6759b082bd51a58a
genser <>
2012-12-14 00:44:12 +0000

    added AntiTritonInelasticProcess

--- MCDataProducts/inc/ProcessCode.hh
+++ MCDataProducts/inc/ProcessCode.hh
@@ -7,9 +7,9 @@
 // that the particle is a primary particle and other enum entries to
 // indicate that a particle was killed in one of the user actions written by G4.
 //
-// $Id: ProcessCode.hh,v 1.10 2012/12/13 23:59:10 genser Exp $
+// $Id: ProcessCode.hh,v 1.11 2012/12/14 00:44:12 genser Exp $
 // $Author: genser $
-// $Date: 2012/12/13 23:59:10 $
+// $Date: 2012/12/14 00:44:12 $
 //
 // Original author Rob Kutschke
 //
@@ -62,7 +62,7 @@ namespace mu2e {
       MuAtomDecay,            Rayl,                    ionInelastic,        He3Inelastic,
       alphaInelastic,         AntiHe3InelasticProcess, AntiAlphaInelasticProcess, AntiDeuteronInelastic,
       dInelastic,             tInelastic,              RadioactiveDecay,    CHIPS_Inelastic,
-      NotSpecified,           hFritiofCaptureAtRest,    hBertiniCaptureAtRest,
+      NotSpecified,           hFritiofCaptureAtRest,    hBertiniCaptureAtRest, AntiTritonInelasticProcess,
       lastEnum,
 
       // An alias for backward compatibility
@@ -91,7 +91,7 @@ namespace mu2e {
     "MuAtomDecay",            "Rayl",                    "ionInelastic",        "He3Inelastic",              \
     "alphaInelastic",         "AntiHe3InelasticProcess", "AntiAlphaInelasticProcess", "AntiDeuteronInelastic", \
     "dInelastic",             "tInelastic",              "RadioactiveDecay",    "CHIPS_Inelastic",           \
-    "NotSpecified",           "hFritiofCaptureAtRest",   "hBertiniCaptureAtRest"
+    "NotSpecified",           "hFritiofCaptureAtRest",   "hBertiniCaptureAtRest", "AntiTritonInelasticProcess"
 
   public:
 
