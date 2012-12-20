725120e8e356fe149c95875364993160590ad97f
genser <>
2012-12-20 17:27:42 +0000

    added comments etc...

--- Mu2eG4/inc/Mu2eG4UserHelpers.hh
+++ Mu2eG4/inc/Mu2eG4UserHelpers.hh
@@ -3,9 +3,9 @@
 //
 // A collection of Geant4 user helper functions
 // initially extracted from the TrackingAction
-// $Id: Mu2eG4UserHelpers.hh,v 1.1 2012/12/19 23:36:57 genser Exp $
+// $Id: Mu2eG4UserHelpers.hh,v 1.2 2012/12/20 17:27:42 genser Exp $
 // $Author: genser $
-// $Date: 2012/12/19 23:36:57 $
+// $Date: 2012/12/20 17:27:42 $
 //
 // Original author KLG based on Rob's TrackingAction
 //
@@ -38,24 +38,27 @@ namespace mu2e {
     typedef SimParticleCollection::mapped_type mapped_type;
     typedef std::map<key_type,mapped_type>     map_type;
 
-    G4String findStoppingProcessName(G4Track const* trk);
-
+    // Check consistency of mother-daughter pointers.
     bool checkCrossReferences( bool doPrint, bool doThrow, map_type const& _transientMap);
 
+    // Debug printout.
     void printTrackInfo(G4Track const* trk, std::string const& text,
                         map_type const& _transientMap,
                         art::CPUTimer const& _timer,
                         CLHEP::Hep3Vector const& _mu2eOrigin,
                         bool isEnd=false);
 
+    G4String findStoppingProcessName(G4Track const* trk);
     ProcessCode findCreationCode(G4Track const* trk);
 
     double getPreLastStepKE(G4Track const* trk);
 
     int  getNSteps(G4Track const* trk);  
 
+    // Control the saving of trajectories.
+    // The first method does the big picture bookkeeping.
+    // The second method decides yes/no for storing the trajectory of one track.
     void controlTrajectorySaving(G4Track const* trk, int _sizeLimit, int _currentSize);
-
     bool saveThisTrajectory( const G4Track* trk );
 
   }
