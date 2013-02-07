6f59f64ac1ee6ab8efc5eff0d028b5a61b41e5b0
brownd <>
2013-02-07 15:39:03 +0000

    Add script to scan integral boundary

--- KalmanTests/test/do_mu2e.C
+++ KalmanTests/test/do_mu2e.C
@@ -3,10 +3,10 @@
   gROOT->LoadMacro("KalmanTests/test/mu2e.C+");
   TChain* con(0);
   TChain* dio(0);
-  char condir[50] = {"/data/mixConversion.6418952"};
-  char diodir[50] = {"/data/mixDIO.6430701"};
+  char condir[50] = {"/data/mixConversion.6499779"};
+  char diodir[50] = {"/data/mixDIO.6499810"};
   FillChain(con,condir,"mixConversion.root",10,"ReadKalFits/trkdiag");
   FillChain(dio,diodir,"mixDIO.root",40,"ReadKalFits/trkdiag");
-  mu2e(dio,con,10.0,1000000,100000,103.4,104.8,true);
+  mu2e(dio,con,10.0,1000000,100000,103.38,104.8,true);
 }
 
