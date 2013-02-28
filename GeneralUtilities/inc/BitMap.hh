0aac2e9a1a6fd66904fb592b64e2e6417bfb53c2
kutschke <>
2013-02-28 18:07:34 +0000

    Enum values are now bit numbers, not bit masks.

--- GeneralUtilities/inc/BitMap.hh
+++ GeneralUtilities/inc/BitMap.hh
@@ -3,46 +3,47 @@
 //
 // Template used to instantiate the bit map classes.
 //
-//   $Id: BitMap.hh,v 1.1 2013/02/22 00:36:06 kutschke Exp $
+//   $Id: BitMap.hh,v 1.2 2013/02/28 18:07:34 kutschke Exp $
 //   $Author: kutschke $
-//   $Date: 2013/02/22 00:36:06 $
+//   $Date: 2013/02/28 18:07:34 $
 //
 // The user must supply a detail class with the following requirements:
 //
-//   1) The class must contain an enum named mask_type.
-//   2) The class must contain two static functions:
+//   1) The detail class must contain an enum named bit_type.
+//      The values of the enums are bit numbers on the domain [0,31].
+//
+//   2) The detail class must contain two static functions:
 //       static std::string const& typeName();
-//       static std::map<mask_type,std::string> const& bitNames();
+//       static std::map<mu2e::BitMap_mask_type,std::string> const& bitNames();
 //
 //      The first function returns one string that holds the name of the type, usually the same name as the .hh file.
 //      The template uses it decorating some printed output.
 //
 //      The second function returns an std::map that implements the cross reference between each mask_type value and
-//      its string representation.
+//      its string representation. Note that this is NOT a cross-reference between bit names and their string representations.
 //
 // Notes:
 //
-// 0) Do we want to remove c'tor from an int?  It breaks the type safety and the validity check will not catch it.
-//    The downside is that there are valid reasons for to have it. Mabye a static conversion function makes more sense
-//    since it needs to be used more consciously?
+// 0) There is no c'tor from any primitive integral type; this is to enforce type safety so that you cannot cross-stitch
+//    detail classes.
 //
-// 1) Do we want the c'tor from mask_type to be explicit or not? Consider the code:
-//        TrackBits v =  TrackBits::passedStep2;
+// 1) Do we want the c'tor from bit_type to be explicit or not? Consider the code:
+//        Color v =  Color::red;
 //    Do we want this to compile?  If yes, the the c'tor must be not explicit.  If we do not want it to compile
 //    then it must be explicit.  A related question is illustrated by this code fragment:
-//        TrackBits v;
-//        v = TrackBits::passedStep2;
-//    If we want this to work, then we need to additional assignment operator; if do not want it to compile,
-//    we do not want the additional assignment operator.
+//        Color v;
+//        v = Color::Red;
+//    If we want this to work, then we need and additional assignment operator; if do not want it to compile,
+//    we do not want the additional assignment operator. In the present release, both examples work; we believe this
+//    is safe because there are no types that can automatically convert to bit_type.
 //
 // 2) By design there is no operator<( BitMap const& ) since that would be ambiguous for comparison
 //    by id or by string representation. Instead there are two free functions, lessByValue and lessByStringRep.
 //
 // 3) By design there are no overloads of merge or hasProperties that take an argument that is of built-in
-//    integral type.  This is needed to ensure type saftey: if I have an object of one bitmap type and use
-//    enum values from a different bitmask type, I will get a compiler error.
-//
-//    The class template does provide a c'tor from an int; but it is slow because it checks that only known bits are set.
+//    integral type.  This is needed for as a safety feature - see note 4 for an example.
+//    The are also no overloads that take an argument of mask_type, to prevent people from hand constructing
+//    illegal masks and using them.
 //
 // 4) Consider the following:
 //       BitMap m;
@@ -50,17 +51,14 @@
 //    or
 //       m.hasProperties(DETAIL::property1|DETAIL::property2);
 //
-//    In both cases, the expression in the argument returns a integral type; but, by note 3), there are no
-//    overloads of merge or hasProperties that takes an integral type.  The solution is to provide an overload
-//    of operator| that will evaluate the expression and return a value of type mask_type.
+//    These will give erroneous results since it is meaningful to OR objects of mask_type but not of bit_type.
+//    These examples will not compile because result of the OR operation is a object of type int and there are
+//    no overloads of merge or hasProperties that take an int.
 //
 // 5) The code enforces the following notion of validity.  A value is valid if all of bits that are set in the
 //    value correspond to bits that are defined by the bitNames method of the Detail class.  It is the responsibility
 //    of the writer of the detail class to ensure that the enum declaration and the bitNames method are consistent.
-//    It is possible to create an invalid value by casting an arbitrary int into a mask_type.
-//
-// 6) Do we want a second template parameter for the type of the value?  So what we can use short int or char when
-//    that would cover the anticipated need for bits?  Maybe this becomes moot in C+11 if we use enum classes.
+//    It is possible to create an invalid value by casting an arbitrary int into a bit_type.
 //
 
 
@@ -74,26 +72,26 @@
 
 namespace mu2e {
 
-  // Helper function.  See note 4.
-  template <class T>
-  T operator|(T a, T b)
-  {
-    return static_cast<T>(static_cast<int>(a) | static_cast<int>(b));
-  }
+  // This type should be used by the Detail classes as the first template parameter of
+  // the type returned by their bitNames method.
+  typedef unsigned BitMap_mask_type;
 
   template < class DETAIL > class BitMap : public DETAIL {
+
   public:
 
-    typedef typename DETAIL::mask_type      mask_type;
+
+    typedef typename DETAIL::bit_type  bit_type;
+    typedef          BitMap_mask_type  mask_type;
     typedef std::map<mask_type,std::string> map_type;
 
-    explicit BitMap():_value(static_cast<mask_type>(0)){}
+    explicit BitMap():_value(empty_value()){}
 
     // Explicit or not? See note 1
-    BitMap( mask_type value): _value(value) {}
+    BitMap( bit_type value): _value(1<<value) {}
 
     // Constructor from a vector of bit names; must match the names used in the detail class.
-    explicit BitMap( std::vector<std::string> const& names ):_value(static_cast<mask_type>(0)){
+    explicit BitMap( std::vector<std::string> const& names ):_value(empty_value()){
 
       for ( std::vector<std::string>::const_iterator i=names.begin();
             i != names.end(); ++i ){
@@ -108,23 +106,30 @@ namespace mu2e {
 
     // Accept compiler generated d'tor, copy c'tor and copy assignment.
 
+    // Construct an object with all known bits set.
+    static BitMap allBits(){
+      BitMap b;
+      b._value = legalBits();
+      return b;
+    }
+
     // Additional assignment operator. See note 1.
-    BitMap& operator=( mask_type c){
-      _value = c;
+    BitMap& operator=( bit_type c){
+      _value = 1<<c;
       return *this;
     }
 
     // Two merge methods; see notes 3 and 4.
-    void merge( mask_type mask){
-      _value = static_cast<mask_type>(_value | mask);
+    void merge( bit_type bitNumber){
+      _value = static_cast<mask_type>( _value | (1<<bitNumber) );
     }
 
-    void merge( BitMap mask){
-      merge( mask.value() );
+    void merge( BitMap arg){
+      _value = static_cast<mask_type>( _value | arg._value);
     }
 
     void reset(){
-      _value = static_cast<mask_type>(0);
+      _value = empty_value();
     }
 
     // By design there is no merge method taking an argument of built-in integral type.
@@ -133,7 +138,7 @@ namespace mu2e {
     mask_type value() const { return _value;}
 
     bool empty() const{
-      return (_value == static_cast<mask_type>(0));
+      return (_value == static_cast<mask_type>(0) );
     }
 
     // For convenience, forward the static function to a member function.
@@ -141,19 +146,18 @@ namespace mu2e {
       return isValid(_value);
     }
 
-    // Two hasProperties methods; see notes 3 and 4.
-    bool hasProperties( mask_type mask) const {
-      return (_value & mask ) == mask;
+    bool hasProperties( bit_type arg) const {
+      return hasProperties(BitMap(arg));
     }
 
-    bool hasProperties( BitMap mask) const {
-      return hasProperties( mask.value());
+    bool hasProperties( BitMap arg) const {
+      return (_value & arg._value ) == arg._value;
     }
 
     // Form a string representation of the bitmap.
     std::string stringRep() const {
 
-      if ( _value == static_cast<mask_type>(0) ){
+      if ( empty() ){
         return "noBitsSet";
       }
 
@@ -162,7 +166,7 @@ namespace mu2e {
       map_type const& bitNames = DETAIL::bitNames();
       for ( typename map_type::const_iterator i=bitNames.begin(), e=bitNames.end();
             i != e; ++i ){
-        if ( hasProperties(i->first) ) {
+        if ( hasPropertiesByMask(i->first) ) {
           if ( properties.size() > 0 ) properties += " ";
           properties += i->second;
         }
@@ -180,18 +184,19 @@ namespace mu2e {
       return stringRep();
     }
 
-    // Comparisons.  See note 2.
-    bool operator==( BitMap g) const{
-      return ( _value == g._value );
+    // Comparisons. About operator< , see note 2.
+    bool operator==( bit_type b) const{
+      BitMap tmp(b);
+      return ( _value == tmp._value );
     }
 
-    bool operator==( mask_type g) const{
-      return ( _value == g );
+    bool operator==( BitMap arg) const{
+      return ( _value == arg._value );
     }
 
     // A value is invalid if any bits not defined in the detail class are set.
     static bool isValid( int value ){
-      static int illegalBits = ~legalBits();
+      static mask_type illegalBits = ~legalBits();
       return ( (value & illegalBits) == 0);
     }
 
@@ -219,7 +224,22 @@ namespace mu2e {
       }
     }
 
-    // Helper method for c'tor
+
+  private:
+
+    mask_type _value;
+
+    // Helper methods that expose mask_type.  Keep them private to force people to use
+    // type safe(r) public interface.
+
+    mask_type empty_value(){
+      return static_cast<mask_type>(0);
+    }
+
+    bool hasPropertiesByMask( mask_type mask) const {
+      return (_value & mask ) == mask;
+    }
+
     mask_type findMaskByNameOrThrow( std::string const& name ){
 
       typename map_type::const_iterator j = findMaskByName(name);
@@ -232,7 +252,6 @@ namespace mu2e {
 
     }
 
-    // Helper method for c'tor
     typename map_type::const_iterator findMaskByName( std::string const& name ){
 
       for ( typename map_type::const_iterator j=bitNames().begin();
@@ -243,9 +262,9 @@ namespace mu2e {
     }
 
     // Compute a mask in which all bits defined in the detail class are set.
-    static int legalBits(){
+    static mask_type legalBits(){
 
-      int tmp(0);
+      mask_type tmp(0);
       map_type const& bitNames = DETAIL::bitNames();
       for ( typename map_type::const_iterator i=bitNames.begin(), e=bitNames.end();
             i != e; ++i ){
@@ -254,10 +273,6 @@ namespace mu2e {
 
       return tmp;
     }
-
-  private:
-
-    mask_type _value;
 
   };
 
