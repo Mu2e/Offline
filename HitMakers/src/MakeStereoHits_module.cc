7f85b8610b5475e40d2d87bd7301e359df6b3c27
brownd <>
2013-03-08 19:13:50 +0000

    Improve algorithm efficiency (but it's still slow)

--- HitMakers/src/MakeStereoHits_module.cc
+++ HitMakers/src/MakeStereoHits_module.cc
@@ -2,9 +2,9 @@
 // A module to create simple stereo hits out of StrawHits.  This can work
 // with either tracker.  StrawHit selection is done by flagging in an upstream module
 //
-// $Id: MakeStereoHits_module.cc,v 1.2 2013/03/08 04:30:23 brownd Exp $
+// $Id: MakeStereoHits_module.cc,v 1.3 2013/03/08 19:13:50 brownd Exp $
 // $Author: brownd $
-// $Date: 2013/03/08 04:30:23 $
+// $Date: 2013/03/08 19:13:50 $
 // 
 //  Original Author: David Brown, LBNL
 //  
@@ -33,6 +33,7 @@
 
 // C++ includes.
 #include <iostream>
+#include <float.h>
 
 using namespace std;
 
@@ -51,15 +52,14 @@ namespace mu2e {
     // Diagnostics level.
     int _diagLevel;
     // Name of the StrawHit collection
-    std::string _shLabel;
+    string _shLabel;
   // Parameters
-    double _maxDz; // maximum z sepration
     double _maxDt; // maximum time separation between hits
     double _maxDE; // maximum deposited energy deference: this excludes inconsistent hits
-    double _maxDot; // maximum dot product magnitude between wire directions
-    bool _allPairs; // whether to make all possible stereo pairs, or only use each hit once
+    double _maxDPerp; // maximum transverse separation
     double _maxTDNsig; // maximum # of TimeDivision sigmas past the active edge of a wire to allow making stereo hits
     double _maxChisq; // maximum # of TimeDivision consistency chisquared to allow making stereo hits
+    bool _writepairs; // write out the stereo pairs
   };
 
   MakeStereoHits::MakeStereoHits(fhicl::ParameterSet const& pset) :
@@ -67,15 +67,14 @@ namespace mu2e {
     // Parameters
     _diagLevel(pset.get<int>("diagLevel",0)),
     _shLabel(pset.get<string>("StrawHitCollectionLabel","makeSH")),
-    _maxDz(pset.get<double>("maxDz",75.0)), // mm
     _maxDt(pset.get<double>("maxDt",60.0)), // nsec
-    _maxDE(pset.get<double>("maxDE",0.95)), // dimensionless, from 0 to 1
-    _maxDot(pset.get<double>("maxDot",0.98)), // must be < 1.0
-    _allPairs(pset.get<bool>("allPairs",true)), // make all possible pairs
-    _maxTDNsig(pset.get<double>("maxTDNsig",4.0)),
-    _maxChisq(pset.get<double>("maxChisquared",9.0)){
+    _maxDE(pset.get<double>("maxDE",0.99)), // dimensionless, from 0 to 1
+    _maxDPerp(pset.get<double>("maxDPerp",100)), // mm, maximum perpendicular distance between time-division points
+    _maxTDNsig(pset.get<double>("maxTDNsig",5.0)),
+    _maxChisq(pset.get<double>("maxChisquared",12.0)),
+    _writepairs(pset.get<bool>("WriteStereoPairs",false)) {
     // Tell the framework what we make.
-    produces<StereoHitCollection>();
+    if(_writepairs)produces<StereoHitCollection>();
     produces<StrawHitPositionCollection>();
   }
 
@@ -107,61 +106,66 @@ namespace mu2e {
       shpos->push_back(StrawHitPosition(strawhits->at(ish),tracker,tcal));
     }
     // create the stereo hits
-    auto_ptr<StereoHitCollection> stereohits(new StereoHitCollection);
-    stereohits->reserve(1.5*nsh);
+    StereoHitCollection stereohits;
+    stereohits.reserve(3*nsh);
+    vector<double> minchisq(nsh,FLT_MAX);
+    vector<int> minsep(nsh,SectorId::apart);
+    vector<size_t> ibest(nsh);
     // double loop over selected straw hits
     for(size_t ish=0;ish<nsh;++ish){
       StrawHit const& sh1 = strawhits->at(ish);
-      const Straw& straw1 = tracker.getStraw(sh1.strawIndex());
+      Straw const& straw1 = tracker.getStraw(sh1.strawIndex());
+      StrawHitPosition const& shp1 = (*shpos)[ish];
       for(size_t jsh=ish+1;jsh<nsh;++jsh){
 	StrawHit const& sh2 = strawhits->at(jsh);
-	const Straw& straw2 = tracker.getStraw(sh2.strawIndex());
-	if( fabs(sh1.time()-sh2.time()) < _maxDt // hits are close in time
-	    && fabs(straw1.getMidPoint().z()-straw2.getMidPoint().z()) < _maxDz // z separation
-	    && fabs(sh1.energyDep() - sh2.energyDep())/(sh1.energyDep()+sh2.energyDep()) < _maxDE // deposited energy is roughly consistent (should compare dE/dx but have no path yet!)
-	    && fabs(straw1.getDirection().dot(straw2.getDirection())) < _maxDot) // wires aren't paralell
-	{
+	Straw const& straw2 = tracker.getStraw(sh2.strawIndex());
+	StrawHitPosition const& shp2 = (*shpos)[jsh];
+	SectorId::isep sep = straw1.id().getSectorId().separation(straw2.id().getSectorId());
+	if( sep != SectorId::same && sep < SectorId::apart // hits are in the same station but not the same sector
+	    && fabs(sh1.time()-sh2.time()) < _maxDt // hits are close in time
+	    && fabs(sh1.energyDep() - sh2.energyDep())/(sh1.energyDep()+sh2.energyDep()) < _maxDE   // deposited energy is roughly consistent (should compare dE/dx but have no path yet!)
+	    && (shp1.pos()-shp2.pos()).perp() < _maxDPerp) { // transverse separation isn't too big
   // tentative stereo hit: this solves for the POCA
 	  StereoHit sth(*strawhits,tracker,ish,jsh);
 	  if( straw1.getDetail().activeHalfLength()-fabs(sth.wdist1()) > -straw1.getDetail().outerRadius() // stereo point is inside the active length
 	      && straw2.getDetail().activeHalfLength()-fabs(sth.wdist2()) > -straw2.getDetail().outerRadius()) { // stereo point is inside the active length
 	    // compute difference between stereo points and TD prediction
-	    double chi1 = ((*shpos)[ish].pos()-sth.pos()).dot(straw1.getDirection())/(*shpos)[ish].posRes(StrawHitPosition::phi);	      
-	    double chi2 = ((*shpos)[jsh].pos()-sth.pos()).dot(straw2.getDirection())/(*shpos)[jsh].posRes(StrawHitPosition::phi);
+	    double chi1 = (shp1.wireDist()-sth.wdist1())/shp1.posRes(StrawHitPosition::phi);	      
+	    double chi2 = (shp2.wireDist()-sth.wdist2())/shp2.posRes(StrawHitPosition::phi);
 	    if(fabs(chi1) <_maxTDNsig && fabs(chi2) < _maxTDNsig)
 	    {
 	      // compute chisquared
 	      double chisq = chi1*chi1+chi2*chi2; 
 	      if(chisq < _maxChisq){
 		sth.setChisquared(chisq);
-		stereohits->push_back(sth);
+		stereohits.push_back(sth);
+// choose the best pair as:
+// 1) take the pair with the minimum plane separation
+// 2) otherwise, take the pair with the minimum chisquared
+		if(sep < minsep[ish] || (sep == minsep[ish] && chisq < minchisq[ish])){
+		  minsep[ish] = sep;
+		  minchisq[ish] = chisq;
+		  ibest[ish] = stereohits.size()-1;
+		}
+		if(sep < minsep[jsh] || (sep == minsep[jsh] && chisq < minchisq[jsh])){
+		  minsep[jsh] = sep;
+		  minchisq[jsh] = chisq;
+		  ibest[jsh] = stereohits.size()-1;
+		}
 	      }
 	    }
 	  }
 	}
       }
     }
-// now, resolve the stereo hits to find the best position for each hit that particpates.  The algorithm is:
-// 1) if a hit pairs with another hit in the same station, take the pair with the minimum plane separation
-// 2) if a hit pairs with another hit in the same plane, take the pair with the minimum chisquared
-    size_t nst = stereohits->size();
+// now, overwrite the positions for those hits which have stereosresolve the stereo hits to find the best position for each hit that particpates.  The algorithm is:
     for(size_t ish=0; ish<nsh;++ish){
-      StrawHitPosition& shp = shpos->at(ish);
-      double minchisq(100.0);
-      SectorId::isep minsep(SectorId::apart);
-      for(size_t ist=0;ist<nst;++ist){
-	StereoHit const& sth = stereohits->at(ist);
-	if(sth.hitIndex1() == ish || sth.hitIndex2() == ish) {
-	  if(sth.sectorSeparation() < minsep ||
-	      (sth.sectorSeparation() == minsep && sth.chisq() < minchisq)){
-	    minsep = sth.sectorSeparation();
-	    minchisq = sth.chisq();
-	    shp = StrawHitPosition(sth);
-	  }
-	}
-      }
+      if(minsep[ish] < SectorId::apart)shpos->at(ish) = StrawHitPosition(stereohits[ibest[ish]],ish);
     }
-    event.put(stereohits);
+    if(_writepairs){
+      auto_ptr<StereoHitCollection> sthits(new StereoHitCollection(stereohits));
+      event.put(sthits);
+    }
     event.put(shpos);
   } // end MakeStereoHits::produce.
 
