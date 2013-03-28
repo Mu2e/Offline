c42e94070260e3036f4d58e6fc8fb801bf9c5adb
knoepfel <>
2013-03-28 17:21:11 +0000

    Commit new proton pulse shape; adjust ProtonPulseRandPDF.cc accordingly.

--- Mu2eUtilities/src/ProtonPulseRandPDF.cc
+++ Mu2eUtilities/src/ProtonPulseRandPDF.cc
@@ -1,9 +1,9 @@
 //
 // Constructor of a PDF to extract random times to describe the proton pulse
 //
-// $Id: ProtonPulseRandPDF.cc,v 1.6 2012/07/15 22:06:17 kutschke Exp $
-// $Author: kutschke $
-// $Date: 2012/07/15 22:06:17 $
+// $Id: ProtonPulseRandPDF.cc,v 1.7 2013/03/28 17:21:11 knoepfel Exp $
+// $Author: knoepfel $
+// $Date: 2013/03/28 17:21:11 $
 //
 // Original author Gianni Onorato
 //
@@ -19,10 +19,22 @@
 //CLHEP includes
 #include "CLHEP/Random/RandFlat.h"
 
+// The following defines the proton pulse shape parameters (pdf width,
+// pdf step and differential distribution).  Please note that it is
+// preferred to assume a delta function distribution for the proton
+// pulse during framework jobs, and then to convolute the obtained
+// timing distributions with the desired shape after the fact.
+//
+// The shape below supersedes "ProtonPulseSpectrum.txt" and is current
+// as of Mar. 2013.  Also note that in any framework jobs that have
+// the proton pulse shape enabled, the center of the pulse will be
+// shifted 200 ns wrt 0.
+
 using namespace std;
 
-static const double _PDFwidth = 260; //time in nanosecond 
-static const double _PDFstep  = 0.5; 
+static const double _PDFwidth = 400; //time in nanosecond
+static const double _PDFstep  = 2.; 
+static const string _FILEname = "ConditionsService/data/ProtonPulseSpectrum_01.txt";
 
 namespace mu2e{
   
@@ -53,7 +65,7 @@ namespace mu2e{
     
     vector<double> spectrum;
     ConfigFileLookupPolicy findConfig;
-    const string infile = findConfig("ConditionsService/data/ProtonPulseSpectrum.txt");
+    const string infile = findConfig( _FILEname );
     fstream inst(infile.c_str(), ios::in);
     if (!inst.is_open()) {
       throw cet::exception("ProductNotFound")
