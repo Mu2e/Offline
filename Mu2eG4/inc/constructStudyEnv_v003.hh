8c04048ff274e230058eaabc3b2614b3a19c5f92
genser <>
2013-04-09 23:18:20 +0000

    added constructDoubleLayerdModule

--- Mu2eG4/inc/constructStudyEnv_v003.hh
+++ Mu2eG4/inc/constructStudyEnv_v003.hh
@@ -1,14 +1,18 @@
 #ifndef Mu2eG4_constructStudyEnv_v003_hh
 #define Mu2eG4_constructStudyEnv_v003_hh
 //
-// Free function to create one of the study environment geometries
+// Free function to create calorimetric study environment geometry plus a helper function
 //
-// $Id: constructStudyEnv_v003.hh,v 1.1 2013/04/05 19:52:40 genser Exp $
+// $Id: constructStudyEnv_v003.hh,v 1.2 2013/04/09 23:18:20 genser Exp $
 // $Author: genser $
-// $Date: 2013/04/05 19:52:40 $
+// $Date: 2013/04/09 23:18:20 $
 //
 // Original author KLG
 //
+
+#include <vector>
+#include <string>
+#include "G4Colour.hh"
 
 namespace mu2e {
 
@@ -18,6 +22,26 @@ namespace mu2e {
   void constructStudyEnv_v003(VolumeInfo   const & parentVInfo,
                               SimpleConfig const & _config
                               );
+
+  void constructDoubleLayerdModule(std::string const & mNamePrefix,
+                                   VolumeInfo  const & parentVInfo,
+                                   std::vector<double> const & tHL,
+                                   std::vector<double> const & tCInParent,
+                                   std::vector<double> const & hHL,
+                                   std::vector<std::string> const & mMat,
+                                   G4int    mNumberOfLayers,
+                                   G4int    verbosityLevel,
+                                   G4double mLCenterInParent,
+                                   G4int passiveVolumeStartingCopyNumber,
+                                   G4int  activeVolumeStartingCopyNumber,
+                                   bool const isVisible,
+                                   G4Colour const & passiveVolumeColour,
+                                   G4Colour const &  activeVolumeColour,
+                                   bool const forceSolid,
+                                   bool const forceAuxEdgeVisible,
+                                   bool const placePV,
+                                   bool const doSurfaceCheck
+                                   );
 
 }
 
