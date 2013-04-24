17f196d7c954431b9dc2ffd69e2e69529b50b481
kutschke <>
2013-04-24 19:01:19 +0000

    Test all packages; switch to a more robust test using svn diff.

--- BaBar/checkVersion.sh
+++ BaBar/checkVersion.sh
@@ -8,9 +8,9 @@
 #
 # This code only prints warnings; it does not do an update.
 #
-# $Id: checkVersion.sh,v 1.2 2013/04/16 23:28:19 gandr Exp $
-# $Date: 2013/04/16 23:28:19 $
-# $Author: gandr $
+# $Id: checkVersion.sh,v 1.3 2013/04/24 19:01:19 kutschke Exp $
+# $Date: 2013/04/24 19:01:19 $
+# $Author: kutschke $
 #
 # Contact person Rob Kutschke
 #
@@ -23,53 +23,38 @@ if ! [[ -f "SConstruct" ]]; then
   exit 1
 fi
 
-# Holds the output of an svn status command.
-tmpFile="svntmp"
+list="BaBar BbrGeom BField CLHEP DetectorModel KalmanTrack MatEnv ProbTools TrajGeom TrkBase difAlgebra"
+badBaBarPackage=""
 
-if [ -e $tmpFile ]; then
-  /bin/rm -f $tmpFile
-  echo  "remove it"
+echo "Checking for correct versions of BaBar packages. May take 30 seconds"
+
+for package in ${list}
+do
+
+  # Holds the output of an svn status command.
+  tmpFile="svntmp_"${package}
+  if [ -e $tmpFile ]; then
+    /bin/rm -f $tmpFile
+  fi
+
+  #echo "Checking status of: " ${package}
+  svn diff -r ${requiredVersion} BaBar/${package} >& ${tmpFile}
+
+  # Any output is an error
+  nLines=`wc $tmpFile | awk '{print $1}'`
+
+  if [ $nLines != 0 ]; then
+    echo " "
+    echo "ERROR " ${package} " does not match required version " ${requiredVersion}
+    svn status -u -q BaBar/${package}
+    echo " "
+    badBaBarPackage=${package}" "${badBaBarPackage}
+  else
+    /bin/rm -f ${tmpFile}
+  fi
+
+done
+
+if [ -z "${badBaBarPackage}" ]; then
+  echo "All BaBar packages pass the version check."
 fi
-
-cmd="svn status -u -q BaBar/BaBar"
-$cmd >& $tmpFile
-
-# Check that the output of the svn command looks correct.
-nLines=`wc $tmpFile | awk '{print $1}'`
-
-if [ $nLines != 1 ]; then
-  echo "ERROR: Expected exactly one line from the command " $cmd
-  echo "Number of lines is: " $nLines
-  echo "File contents: "
-  echo " "
-  cat $tmpFile
-  echo " "
-  echo "Please remove the file " $tmpFile " when you are done checking this."
-  exit 1
-fi
-
-nLines2=`cat $tmpFile | grep "Status against revision:" | wc | awk '{print $1}'`
-if [ $nLines != 1 ]; then
-  echo "ERROR: Did not find the expected line in the output of the command " $cmd
-  echo "File contents: "
-  echo " "
-  cat $tmpFile
-  echo " "
-  echo "Please remove the file " $tmpFile " when you are done checking this."
-  exit 1
-fi
-
-checkedOutVersion=`cat $tmpFile | grep "Status against revision:" | awk '{print $4}' `
-
-if [ "$checkedOutVersion" != "$requiredVersion" ]; then
-  echo "The checked out version of the BaBar code does not match the required version."
-  echo "Checked out version: " $checkedOutVersion
-  echo "Required version:    " $requiredVersion
-  echo "If you need to update the BaBaR code you should: source BaBar/update.sh"
-fi
-
-if [ "$checkedOutVersion" == "$requiredVersion" ]; then
-  echo "The checked out BaBar code matches the required version: " $requiredVersion
-fi
-
-/bin/rm -f $tmpFile
