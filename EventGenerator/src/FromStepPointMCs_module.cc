0e81ad3a834b1bd93b7540a94b74eab96f985cff
gandr <>
2013-05-30 15:15:11 +0000

    Support more then one input collection for FromStepPointMCs.

--- EventGenerator/src/FromStepPointMCs_module.cc
+++ EventGenerator/src/FromStepPointMCs_module.cc
@@ -39,29 +39,56 @@ namespace mu2e {
         <<")";
       return os.str();
     }
+
+
+    // In art v1_00_06 the ValidHandle::id() method needed by an
+    // art::Ptr constructor was missing.  A workaround:
+    template<class PROD> struct MyHandle : public art::ValidHandle<PROD> {
+      MyHandle(const art::ValidHandle<PROD>& h) : art::ValidHandle<PROD>(h) {}
+      art::ProductID id( ) const { return this->provenance()->productID(); }
+    };
+    template<class PROD> MyHandle<PROD> makeMyHandle(const art::ValidHandle<PROD>& h) {
+      return MyHandle<PROD>(h);
+    }
   }
 
   class FromStepPointMCs : public art::EDProducer {
   public:
     explicit FromStepPointMCs(fhicl::ParameterSet const& pset);
-    void produce(art::Event& event);
+    void produce(art::Event& event) override;
 
   private:
-    std::string inModuleLabel_;
-    std::string  inInstanceName_;
+    typedef std::vector<art::InputTag> InputTags;
+    InputTags inputs_;
     GlobalConstantsHandle<ParticleDataTable> pdt_;
     int logLevel_;
     bool allowDuplicates_;  // easily get a wrong answer by double counting particles
   };
 
   FromStepPointMCs::FromStepPointMCs(fhicl::ParameterSet const& pset)
-    : inModuleLabel_(pset.get<std::string>("inputModuleLabel"))
-    , inInstanceName_(pset.get<std::string>("inputInstanceName"))
-    , logLevel_(pset.get<int>("logLevel", 0))
+    : logLevel_(pset.get<int>("logLevel", 0))
     , allowDuplicates_(pset.get<bool>("allowDuplicates", false))
   {
     produces<GenParticleCollection>();
     produces<GenParticleSPMHistory>();
+
+    typedef std::vector<std::string> Strings;
+    Strings tagstr(pset.get<Strings>("inputTags", Strings()));
+
+    if(tagstr.empty()) { // legacy support
+      mf::LogWarning warn("FromStepPointMCs");
+      warn<<"WARNING: FromStepPointMCs: the inputTags list is not set or empty. "
+          <<"Will try to use inputModuleLabel and inputInstanceName instead.\n";
+
+      std::string label(pset.get<std::string>("inputModuleLabel"));
+      std::string instance(pset.get<std::string>("inputInstanceName"));
+      inputs_.emplace_back(label, instance);
+    }
+    else {
+      for(const auto& s : tagstr) {
+        inputs_.emplace_back(s);
+      }
+    }
   }
 
   void FromStepPointMCs::produce(art::Event& event) {
@@ -71,58 +98,56 @@ namespace mu2e {
 
     art::ProductID gpc_pid = getProductID<GenParticleCollection>(event);
 
-    // The input collection
-    art::Handle<mu2e::StepPointMCCollection> ih;
-    event.getByLabel(inModuleLabel_, inInstanceName_, ih);
-    const StepPointMCCollection& inhits(*ih);
+    for(const auto& tag : inputs_) {
+      auto ih = event.getValidHandle<mu2e::StepPointMCCollection>(tag);
 
-    // The purpose of this module is to continue simulation of an
-    // event from saved hits (normally in Virtual Detectors).  If our
-    // inputs contain multiple hits made by the same particle
-    // then creation of new GenParticle from each hit would be wrong.
-    // Make sure each SimParticle occurs only once.
+      // The purpose of this module is to continue simulation of an
+      // event from saved hits (normally in Virtual Detectors).  If our
+      // inputs contain multiple hits made by the same particle
+      // then creation of new GenParticle from each hit would be wrong.
+      // Make sure each SimParticle occurs only once.
+      typedef std::set<const SimParticle*> SPSet;
+      SPSet seenParticles;
+      for(StepPointMCCollection::const_iterator i=ih->begin(); i!=ih->end(); ++i) {
+        const art::Ptr<SimParticle>& particle = i->simParticle();
+        if(!seenParticles.insert(&*particle).second) {
+          std::ostringstream os;
+          os <<"FromStepPointMCs: duplicate SimParticle "<<toStr(*particle)<<" in input hits collection. Hit: "<<*i<<" in event "<<event.id();
+          if(allowDuplicates_) {
+            mf::LogWarning warn("FromStepPointMCs");
+            warn<<"WARNING: "<<os.str()<<"\n";
+          }
+          else {
+            throw cet::exception("BADINPUTS")<<os.str();
+          }
+        }
 
-    typedef std::set<const SimParticle*> SPSet;
-    SPSet seenParticles;
-    for(StepPointMCCollection::const_iterator i=inhits.begin(); i!=inhits.end(); ++i) {
-      const art::Ptr<SimParticle>& particle = i->simParticle();
-      if(!seenParticles.insert(&*particle).second) {
-        std::ostringstream os;
-        os <<"FromStepPointMCs: duplicate SimParticle "<<toStr(*particle)<<" in input hits collection. Hit: "<<*i<<" in event "<<event.id();
-        if(allowDuplicates_) {
-          mf::LogWarning warn("FromStepPointMCs");
-          warn<<"WARNING: "<<os.str()<<"\n";
+        if(logLevel_ > 1) {
+          std::cout<<"AG: creating new GenParticle from hit "<<*i<<std::endl;
+        }
+
+        if(pdt_->particle(particle->pdgId())) {
+          const double mass = pdt_->particle(particle->pdgId()).ref().mass().value();
+          const CLHEP::HepLorentzVector fourMom(i->momentum(), sqrt(mass*mass + i->momentum().mag2()));
+
+          output->push_back(GenParticle(particle->pdgId(),
+                                        GenId::fromStepPointMCs,
+                                        i->position(),
+                                        fourMom,
+                                        i->time()
+                                        ));
+
+          history->addSingle(art::Ptr<GenParticle>(gpc_pid, output->size()-1, event.productGetter(gpc_pid)),
+                             art::Ptr<StepPointMC>(makeMyHandle(ih), std::distance(ih->begin(), i))
+                             );
         }
         else {
-          throw cet::exception("BADINPUTS")<<os.str();
+          mf::LogWarning warn("FromStepPointMCs");
+          warn<<"WARNING: No particle data for pdgId = "<<particle->pdgId()<<"\n";
         }
-      }
 
-      if(logLevel_ > 1) {
-        std::cout<<"AG: creating new GenParticle from hit "<<*i<<std::endl;
-      }
-
-      if(pdt_->particle(particle->pdgId())) {
-        const double mass = pdt_->particle(particle->pdgId()).ref().mass().value();
-        const CLHEP::HepLorentzVector fourMom(i->momentum(), sqrt(mass*mass + i->momentum().mag2()));
-
-        output->push_back(GenParticle(particle->pdgId(),
-                                      GenId::fromStepPointMCs,
-                                      i->position(),
-                                      fourMom,
-                                      i->time()
-                                      ));
-
-        history->addSingle(art::Ptr<GenParticle>(gpc_pid, output->size()-1, event.productGetter(gpc_pid)),
-                           art::Ptr<StepPointMC>(ih, std::distance(inhits.begin(), i))
-                           );
-      }
-      else {
-        mf::LogWarning warn("FromStepPointMCs");
-        warn<<"WARNING: No particle data for pdgId = "<<particle->pdgId()<<"\n";
-      }
-
-    }
+      } // for (StepPointMCCollection entries)
+    } // for(inputs_)
 
     event.put(std::move(output));
     event.put(std::move(history));
