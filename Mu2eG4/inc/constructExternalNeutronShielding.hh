219e0e214f4f035acddb1b33125eb7b01c4873cc
knoepfel <>
2013-09-27 13:09:56 +0000

    Add constructExternalNeutronShielding Mu2eG4 functions

---
+++ Mu2eG4/inc/constructExternalNeutronShielding.hh
@@ -0,0 +1,14 @@
+// Original author David Norvil Brown
+
+#ifndef Mu2eG4_constructExternalNeutronShielding_hh
+#define Mu2eG4_constructExternalNeutronShielding_hh
+
+namespace mu2e {
+
+  class VolumeInfo;
+  class SimpleConfig;
+
+  void constructExternalNeutronShielding(const VolumeInfo& parent, const SimpleConfig& config);
+}
+
+#endif/*Mu2eG4_constructExternalNeutronShielding_hh */
