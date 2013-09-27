0b47012bd14febaea742abc5a8e18fd689ae04c0
knoepfel <>
2013-09-27 13:27:59 +0000

    Remove deprecated ExternalNeutronAborber class

--- ExternalNeutronAbsorberGeom/src/ExternalNeutronAbsorber.cc
+++
@@ -1,7 +0,0 @@
-#include "ExternalNeutronAbsorberGeom/inc/ExternalNeutronAbsorber.hh"
-
-namespace mu2e {
-  ExternalNeutronAbsorber::ExternalNeutronAbsorber()
-    : _halfLengthZ(0.), _halfLengthXY(0.), _halfThickness(0.)
-  {}
-}
