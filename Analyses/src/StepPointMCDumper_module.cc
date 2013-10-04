9eb7828bf4b0feb0e50f83d958fe427755ca1be0
gandr <>
2013-10-04 22:29:12 +0000

    New module to dump StepPointMCs into a ROOT tree.

---
+++ Analyses/src/StepPointMCDumper_module.cc
@@ -0,0 +1,172 @@
+// Ntuple dumper for StepPointMCs.
+//
+// Andrei Gaponenko, 2013
+
+#include <string>
+#include <vector>
+#include <limits>
+#include <cmath>
+
+#include "cetlib/exception.h"
+#include "CLHEP/Vector/ThreeVector.h"
+
+#include "TDirectory.h"
+#include "TH1.h"
+#include "TTree.h"
+
+#include "art/Utilities/InputTag.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "art/Framework/Core/EDAnalyzer.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Run.h"
+#include "art/Framework/Principal/Provenance.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Services/Optional/TFileService.h"
+
+#include "MCDataProducts/inc/StepPointMC.hh"
+#include "MCDataProducts/inc/StepPointMCCollection.hh"
+
+#include "ConditionsService/inc/GlobalConstantsHandle.hh"
+#include "ConditionsService/inc/ParticleDataTable.hh"
+
+namespace mu2e {
+
+  class Mu2eCoordinates {};
+
+  //================================================================
+  double getCharge(PDGCode::type pdgId) {
+    // unlike generic conditions, MC particle data
+    // should not change run-to-run, so static is safe
+    // use static for efficiency
+    static GlobalConstantsHandle<ParticleDataTable> pdt;
+
+    ParticleDataTable::maybe_ref info = pdt->particle(pdgId);
+
+    if(!info.isValid()) {
+      throw cet::exception("MISSINGINFO")<<"No valid PDG info for pdgId = "<<pdgId<<"\n";
+    }
+
+    return info.ref().charge();
+  }
+
+  //================================================================
+  double getKineticEnergy(const StepPointMC& hit) {
+    // unlike generic conditions, MC particle data
+    // should not change run-to-run, so static is safe
+    // use static for efficiency
+    static GlobalConstantsHandle<ParticleDataTable> pdt;
+
+    ParticleDataTable::maybe_ref info = pdt->particle(hit.simParticle()->pdgId());
+
+    if(!info.isValid()) {
+      throw cet::exception("MISSINGINFO")<<"No valid PDG info for hit = "<<hit<<"\n";
+    }
+
+    const double mass = info.ref().mass();
+    return sqrt(hit.momentum().mag2() + std::pow(mass, 2)) - mass;
+  }
+
+  //================================================================
+  struct VDHit {
+    float x;
+    float y;
+    float z;
+    float time;
+
+    float px;
+    float py;
+    float pz;
+    float pmag;
+    float ek;
+
+    float charge;
+    int   pdgId;
+    unsigned particleId;
+    unsigned volumeCopyNumber;
+
+    VDHit() : x(std::numeric_limits<double>::quiet_NaN())
+            , y(std::numeric_limits<double>::quiet_NaN())
+            , z(std::numeric_limits<double>::quiet_NaN())
+
+            , time(std::numeric_limits<double>::quiet_NaN())
+
+            , px(std::numeric_limits<double>::quiet_NaN())
+            , py(std::numeric_limits<double>::quiet_NaN())
+            , pz(std::numeric_limits<double>::quiet_NaN())
+            , pmag(std::numeric_limits<double>::quiet_NaN())
+            , ek(std::numeric_limits<double>::quiet_NaN())
+
+      , charge(std::numeric_limits<double>::quiet_NaN())
+      , pdgId(0)
+      , particleId(-1U)
+      , volumeCopyNumber(-1U)
+    {}
+
+    //----------------------------------------------------------------
+    // Constructor using mu2e coords
+    VDHit(const Mu2eCoordinates& , const StepPointMC& hit)
+      : x(hit.position().x())
+      , y(hit.position().y())
+      , z(hit.position().z())
+
+      , time(hit.time())
+
+      , px(hit.momentum().x())
+      , py(hit.momentum().y())
+      , pz(hit.momentum().z())
+
+      , pmag(hit.momentum().mag())
+      , ek(getKineticEnergy(hit))
+
+      , charge(getCharge(hit.simParticle()->pdgId()))
+
+      , pdgId(hit.simParticle()->pdgId())
+      , particleId(hit.simParticle()->id().asUint())
+      , volumeCopyNumber(hit.volumeId())
+    {}
+
+  }; // struct VDHit
+
+  //================================================================
+  class StepPointMCDumper : public art::EDAnalyzer {
+    art::InputTag hitsInputTag_;
+
+    // Members needed to write the ntuple
+    TTree *nt_;
+    VDHit hit_;
+
+  public:
+    explicit StepPointMCDumper(const fhicl::ParameterSet& pset);
+    virtual void beginJob();
+    virtual void analyze(const art::Event& event);
+  };
+
+  //================================================================
+  StepPointMCDumper::StepPointMCDumper(const fhicl::ParameterSet& pset)
+    : hitsInputTag_(pset.get<std::string>("hitsInputTag"))
+    , nt_(0)
+  {}
+
+  //================================================================
+  void StepPointMCDumper::beginJob() {
+    art::ServiceHandle<art::TFileService> tfs;
+    static const char branchDesc[] = "x/F:y/F:z/F:time/F:px/F:py/F:pz/F:pmag/F:ek/F:charge/F:pdgId/I:particleId/i:volumeCopy/i";
+    nt_ = tfs->make<TTree>( "nt", "StepPointMCDumper ntuple");
+    nt_->Branch("hits", &hit_, branchDesc);
+  }
+
+  //================================================================
+  void StepPointMCDumper::analyze(const art::Event& event) {
+    const auto& ih = event.getValidHandle<StepPointMCCollection>(hitsInputTag_);
+    for(const auto& i : *ih) {
+      hit_ = VDHit(Mu2eCoordinates(), i);
+      nt_->Fill();
+    }
+
+  } // analyze(event)
+
+    //================================================================
+
+} // namespace mu2e
+
+DEFINE_ART_MODULE(mu2e::StepPointMCDumper);
