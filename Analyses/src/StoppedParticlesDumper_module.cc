5da817c5ca32eea136a223ce7089709d6b976491
knoepfel <>
2013-12-20 21:43:23 +0000

    Update StoppedParticlesDumper_module.c

--- Analyses/src/StoppedParticlesDumper_module.cc
+++ Analyses/src/StoppedParticlesDumper_module.cc
@@ -15,12 +15,55 @@
 
 #include "MCDataProducts/inc/SimParticleCollection.hh"
 #include "MCDataProducts/inc/SimParticlePtrCollection.hh"
+#include "MCDataProducts/inc/StepPointMC.hh"
+#include "MCDataProducts/inc/StepPointMCCollection.hh"
 
 #include "TTree.h"
+
+#include <algorithm>
+#include <iterator>
 
 namespace mu2e {
 
   namespace {
+
+    typedef std::vector<std::string> VS;
+    typedef std::vector<StepPointMCCollection> VspMC;
+
+    float getTau( const art::Ptr<SimParticle>& p, 
+                  const VspMC& hitColls ){
+
+      float tau = p->endProperTime();
+
+      // Check if mother PID equals daughter PID
+      art::Ptr<SimParticle> part (p);
+
+      while ( part->parent().isNonnull() && (part->parent()->pdgId() == part->pdgId()) ) {
+        part = part->parent();
+        
+        // Find matches in hit collections
+        unsigned counter (0);
+        const StepPointMC* spMC(nullptr);
+        for ( const auto& hitColl : hitColls ) {
+          std::for_each( hitColl.begin(), hitColl.end(), 
+                         [&](const StepPointMC& sp){ 
+                           if ( sp.simParticle().key() == part.key() ) {
+                             spMC = &sp;
+                             counter++;                             
+                           }
+                         } );
+          
+        }
+
+        if      ( counter == 0 ) throw cet::exception("StepPointMC") << " Non-existent StepPointMC-SimParticle assignment! " ;
+        else if ( counter  > 1 ) throw cet::exception("StepPointMC") << " Ambiguous StepPointMC-SimParticle assignment! " ;
+        else  tau += spMC->properTime();
+
+      }
+
+      return tau;
+    }
+                    
     // This should be minimal info, we'll want to load this
     // in memory in consumer jobs.  This is NOT an analysis ntuple!
     struct StopInfo {
@@ -32,19 +75,20 @@ namespace mu2e {
 
       StopInfo() : x(), y(), z(), t(), tau() {}
 
-      StopInfo(const SimParticle& p)
-        : x(p.endPosition().x())
-        , y(p.endPosition().y())
-        , z(p.endPosition().z())
-        , t(p.endGlobalTime())
-        , tau(p.endProperTime())
+      StopInfo(const art::Ptr<SimParticle>& p, const VspMC& spMCcolls, const bool writeTau )
+        : x(p->endPosition().x())
+        , y(p->endPosition().y())
+        , z(p->endPosition().z())
+        , t(p->endGlobalTime())
+        , tau( writeTau ? getTau(p,spMCcolls) : -1 )
       {
-        if(!p.endDefined()) {
+        if(!p->endDefined()) {
           throw cet::exception("BADINPUTS")
             <<"StoppedParticlesDumper: input SimParticle does not have end defined!\n";
         }
       }
     };
+
   }// namespace
 
   //================================================================
@@ -56,6 +100,7 @@ namespace mu2e {
   private:
     art::InputTag input_;
     bool writeProperTime_;
+    VS hitColls_;
 
     TTree *nt_;
     StopInfo data_;
@@ -66,6 +111,7 @@ namespace mu2e {
     art::EDAnalyzer(pset),
     input_(pset.get<std::string>("inputCollection")),
     writeProperTime_(pset.get<bool>("writeProperTime")),
+    hitColls_( writeProperTime_ ? pset.get<VS>("hitCollections") : VS() ),
     nt_()
   {}
 
@@ -82,11 +128,20 @@ namespace mu2e {
 
   //================================================================
   void StoppedParticlesDumper::analyze(const art::Event& event) {
+
+    VspMC spMCColls;
+
+    for ( const auto& iColl : hitColls_ ){
+      auto spColl = event.getValidHandle<StepPointMCCollection>(iColl);
+      spMCColls.push_back( *spColl );
+    }
+
     auto ih = event.getValidHandle<SimParticlePtrCollection>(input_);
     for(const auto& p : *ih) {
-      data_ = StopInfo(*p);
+      data_ = StopInfo( p, spMCColls, writeProperTime_ );
       nt_->Fill();
     }
+
   }
 
   //================================================================
