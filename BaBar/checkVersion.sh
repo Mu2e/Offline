389303d77cf30b280e5fed4e4155f065bf603faf
kutschke <>
2014-01-06 21:06:55 +0000

    Do checks without needing network access.

--- BaBar/checkVersion.sh
+++ BaBar/checkVersion.sh
@@ -3,13 +3,17 @@
 # Check the version of the checked out BaBar svn code and compare
 # it to the required version, which is passed in as an argument.
 #
-# The check is done on a single directory.  I believe that this is enough
-# but I can't be certain that it won't change in the future.
+# The check is done for each top level directory, not for each file.
+# I believe that this is good enough.
+#
+# The check is done in two steps:
+#  1) Check that the version of the checked out code matches the required version.
+#  2) Check that there have been no changes since checkout.
 #
 # This code only prints warnings; it does not do an update.
 #
-# $Id: checkVersion.sh,v 1.4 2013/10/22 23:54:13 kutschke Exp $
-# $Date: 2013/10/22 23:54:13 $
+# $Id: checkVersion.sh,v 1.5 2014/01/06 21:06:55 kutschke Exp $
+# $Date: 2014/01/06 21:06:55 $
 # $Author: kutschke $
 #
 # Contact person Rob Kutschke
@@ -18,6 +22,12 @@
 # The required version number is given as an input argument.
 requiredVersion=$1
 
+# If any second argument is present it enables verbose mode.
+verbose=""
+if [ ${#} = 2 ]; then
+   verbose="verbose"
+fi
+
 if ! [[ -f "SConstruct" ]]; then
   echo "ERROR: must be executed from top level of offline code." 1>&2
   exit 1
@@ -25,43 +35,60 @@ fi
 
 list="BaBar BbrGeom BField CLHEP DetectorModel KalmanTrack MatEnv ProbTools TrajGeom TrkBase difAlgebra"
 badBaBarPackage=""
+modifiedPackages=""
 
-echo "Checking for correct versions of BaBar packages. May take 30 seconds"
+if [ "${verbose}" != ''  ]; then
+  echo "Checking for correct versions of BaBar packages."
+fi
 
 for package in ${list}
 do
 
-  # Holds the output of an svn status command.
-  tmpFile="svntmp_"${package}
-  if [ -e $tmpFile ]; then
-    /bin/rm -f $tmpFile
+  # Step 1: checked out version matches the required version
+  version=`svn info BaBar/${package} | awk '$1 == "Revision:" {print $2}'`
+
+  if [ ${version} != ${requiredVersion} ]; then
+    echo " "
+    echo "ERROR " ${package} " does not match required version:"
+    echo "   Required version: " ${requiredVersion}
+    echo "   Actual version:   " ${version}
+    echo " "
+    badBaBarPackage=${package} "  "  ${badBaBarPackage}
   fi
 
-  #echo "Checking status of: " ${package}
-  svn diff -r ${requiredVersion} BaBar/${package} >& ${tmpFile}
+  # Step 2: no changes since checkout.
+  count=`svn diff BaBar/${package} | wc | awk '{print $1}'`
 
-  # Any output is an error
-  nLines=`wc $tmpFile | awk '{print $1}'`
-
-  if [ $nLines != 0 ]; then
-    echo " "
-    echo "ERROR " ${package} " does not match required version " ${requiredVersion}
-    svn status -u -q BaBar/${package}
-    echo " "
-    badBaBarPackage=${package}" "${badBaBarPackage}
-  else
-    /bin/rm -f ${tmpFile}
+  if [ "${count}" != "0" ]; then
+    modifiedPackages=${modifiedPackages}"_"${package}
   fi
 
 done
 
+# Summary of step 1
 if [ -z "${badBaBarPackage}" ]; then
-  echo "All BaBar packages pass the version check."
+  if [ "${verbose}" != ''  ]; then
+    echo "All BaBar packages have the correct checked out version."
+  fi
 else
   echo " "
-  echo "Some BaBar packages have an incorrect version."
+  echo "ERROR: Some BaBar packages have an incorrect version."
+  echo "You must fix this before continuuing."
   echo "To update to the correct version issue the following command: "
   echo " "
   echo "BaBar/update.sh " ${requiredVersion}
+  echo " "
+fi
+
+# Summary of step 2
+if [ -z "${modifiedPackages}" ]; then
+  if [ "${verbose}" != ''  ]; then
+    echo "There have been no changes to the BaBar code since checkout."
+  fi
+else
+  echo " "
+  echo "WARNING:  Some BaBar packages have had changes since checkout."
+  echo "          The packages are: " `echo ${modifiedPackages} | sed 's/\_/ /g'`
+  echo "          If this is expected, carry on.  If not, contact an expert."
   echo " "
 fi
