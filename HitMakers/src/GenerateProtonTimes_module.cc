35b37a199c513d3d6ee8fa3b1e13a2e1dddf1e65
gandr <>
2014-01-17 01:09:59 +0000

    Remove a workaround that is not needed any more.

--- HitMakers/src/GenerateProtonTimes_module.cc
+++ HitMakers/src/GenerateProtonTimes_module.cc
@@ -27,19 +27,6 @@
 
 namespace mu2e {
 
-  namespace {
-    // In art v1_00_06 the ValidHandle::id() method needed by an
-    // art::Ptr constructor is missing.  A workaround:
-    template<class PROD> struct MyHandle : public art::ValidHandle<PROD> {
-      MyHandle(const art::ValidHandle<PROD>& h) : art::ValidHandle<PROD>(h) {}
-      art::ProductID id( ) const { return this->provenance()->productID(); }
-    };
-    template<class PROD> MyHandle<PROD> makeMyHandle(const art::ValidHandle<PROD>& h) {
-      return MyHandle<PROD>(h);
-    }
-
-  } // end of anonymous namespace
-
   class GenerateProtonTimes : public art::EDProducer {
 
   public:
@@ -74,7 +61,7 @@ namespace mu2e {
     // Generate and record offsets for all primaries
     for(const auto& iter : *ih) {
       if(iter.second.isPrimary()) {
-        art::Ptr<SimParticle> part(makeMyHandle(ih), iter.first.asUint());
+        art::Ptr<SimParticle> part(ih, iter.first.asUint());
         (*res)[part] = protonPulse_.fire();
       }
     }
@@ -83,7 +70,7 @@ namespace mu2e {
       // Associate the offsets to the daughters as well
       for(const auto& iter : *ih) {
         if(!iter.second.isPrimary()) {
-          art::Ptr<SimParticle> daughter(makeMyHandle(ih), iter.first.asUint());
+          art::Ptr<SimParticle> daughter(ih, iter.first.asUint());
 
           art::Ptr<SimParticle> mother = daughter->parent();
           while(mother && !mother->isPrimary()) {
