065bed2c81604c8502f6d1ad1bf8205691865454
brownd <>
2014-01-18 17:31:23 +0000

    Add new package for tracker conditions objects

---
+++ TrackerConditions/inc/StrawPhysics.hh
@@ -0,0 +1,60 @@
+#ifndef TrackerConditions_StrawPhysics_hh
+#define TrackerConditions_StrawPhysics_hh
+//
+// StrawPhysics collects the electronics response behavior of a Mu2e straw in
+// several functions and parameters
+//
+// $Id: StrawPhysics.hh,v 1.1 2014/01/18 17:31:23 brownd Exp $
+// $Author: brownd $
+// $Date: 2014/01/18 17:31:23 $
+//
+// Original author David Brown, LBNL
+//
+
+// C++ includes
+#include <iostream>
+#include <array>
+#include <vector>
+#include <utility>
+
+// Mu2e includes
+#include "Mu2eInterfaces/inc/ConditionsEntity.hh"
+#include "fhiclcpp/ParameterSet.h"
+#include <array>
+#include <vector>
+
+namespace mu2e {
+  class StrawPhysics : virtual public ConditionsEntity {
+    public:
+      // construct from parameters
+      StrawPhysics(fhicl::ParameterSet const& pset);
+      virtual ~StrawPhysics();
+    // models.  Note these are different from the corresponding
+    // functions used in reconstruction, as those can be wire-
+    // dependent and have emergent properties
+      double ionizationCharge(double ionizationEnergy) const;
+      double ionizationEnergy(double ionizationCharge) const;
+      unsigned nIonization(double charge) const; 
+      double strawGain(double ddist, double phi) const;
+      double driftDistanceToTime(double ddist, double phi) const;
+      double driftTimeSpread(double ddist, double phi) const;
+      double propagationAttenuation(double wdist) const;
+      double propagationTime(double wdist) const;
+      double velocityDispersion() const { return _vdisp; } 
+
+    private:
+      double _EIonize; // energy of each ionization (MeV)
+      double _QIonize; // charge of a single ionization (=e, pC)
+      double _gasgain; // avalanche gain
+      // attenuation length of charge down the wire; note
+      // there is a short and a long component, each with it's own amplitude
+      double _attlen[2];
+      double _longfrac;
+      double _vdrift; // electron drift velocity
+      double _drifterr; // drift time error
+      double _vprop; // (average) propagation velocity
+      double _vdisp; // dispersion of propagation velocity (dv/dl)
+  };
+}
+#endif
+
---
+++ TrackerConditions/src/StrawPhysics.cc
@@ -0,0 +1,65 @@
+//
+// StrawPhysics collects the electronics response behavior of a Mu2e straw in
+// several functions.
+//
+// $Id: StrawPhysics.cc,v 1.1 2014/01/18 17:31:23 brownd Exp $
+// $Author: brownd $
+// $Date: 2014/01/18 17:31:23 $
+//
+// Original author David Brown, LBNL
+//
+#include "TrackerConditions/inc/StrawPhysics.hh"
+#include "cetlib/exception.h"
+#include <math.h>
+#include <algorithm>
+
+using namespace std;
+namespace mu2e {
+  StrawPhysics::StrawPhysics(fhicl::ParameterSet const& pset) :
+    _EIonize(pset.get<double>("EnergyPerIonization",100.0e-6)), // 100% Ar is between 27 ev/ionization and 100 ev/ionization, not sure what model G4 uses, also should use Ar/CO2 FIXME!!
+    _QIonize(pset.get<double>("ChargePerIonization",1.6e-7)), // e, pC
+    _gasgain(pset.get<double>("GasGain",3.0e4)),
+    _attlen{pset.get<double>("ShortAttentuationLength",50.0),pset.get<double>("LongAttentuationLength",27000.0)}, // from ATLAS TRT measurement
+    _longfrac(pset.get<double>("LongAttenuationFraction",0.92)),
+    _vdrift(pset.get<double>("DriftVelocity",0.05)), // mm/nsec
+    _drifterr(pset.get<double>("DriftTimeError",1.5)), // nsec
+    _vprop(pset.get<double>("PropagationVelocity",231.0)), //mm/nsec
+    _vdisp(pset.get<double>("PropagationVelocityDispersion",0.01)) //1/nsec
+  {}
+
+  StrawPhysics::~StrawPhysics() {}
+// for now, simple linear models of the effects.  In future, we want to
+// include non-linear effects and angular dependence (ie Lorentz effects)
+  double StrawPhysics::strawGain(double ddist, double phi) const { 
+    return _gasgain;
+  }
+
+  double StrawPhysics::driftDistanceToTime(double ddist, double phi) const {
+    return ddist/_vdrift;
+  }
+
+  double StrawPhysics::driftTimeSpread(double ddist, double phi) const {
+    return _drifterr;
+  }
+  
+  double StrawPhysics::propagationAttenuation(double wdist) const {
+    return  (1.0-_longfrac)*exp(-wdist/_attlen[0]) +
+      _longfrac*exp(-wdist/_attlen[1]);
+  }
+
+  double StrawPhysics::propagationTime(double wdist) const {
+    return  wdist/_vprop;
+  }
+
+  double StrawPhysics::ionizationCharge(double ionizationEnergy) const {
+    return _QIonize*ionizationEnergy/_EIonize; }
+
+  double StrawPhysics::ionizationEnergy(double ionizationCharge) const {
+    return _EIonize*ionizationCharge/_QIonize; }
+
+  unsigned StrawPhysics::nIonization(double charge) const {
+    return static_cast<unsigned>(std::max(1,static_cast<int>(ceil(charge/_QIonize))));
+  }
+
+}
+
