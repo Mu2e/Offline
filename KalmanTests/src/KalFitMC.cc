596109757ef83ea4a7bb6d32d99597402635483b
brownd <>
2014-04-06 13:11:40 +0000

    Small fix to relation testing

--- KalmanTests/src/KalFitMC.cc
+++ KalmanTests/src/KalFitMC.cc
@@ -1,8 +1,8 @@
 //
 // MC functions associated with KalFit
-// $Id: KalFitMC.cc,v 1.56 2014/04/02 14:18:00 brownd Exp $
+// $Id: KalFitMC.cc,v 1.57 2014/04/06 13:11:40 brownd Exp $
 // $Author: brownd $ 
-// $Date: 2014/04/02 14:18:00 $
+// $Date: 2014/04/06 13:11:40 $
 //
 //geometry
 #include "GeometryService/inc/GeometryService.hh"
@@ -983,28 +983,32 @@ namespace mu2e
     art::Ptr<SimParticle> pj = sppj->parent();
     if(pi.isNonnull() && pi == sppj)return daughter;
     if(pj.isNonnull() && pj == sppi)return mother;
-    if(pi.isNonnull() && pj.isNonnull() && pi == pj)return sibling;
-    std::vector<art::Ptr<SimParticle> > pvi, pvj;
-    while(pi.isNonnull()){
-      pvi.push_back(pi);
-      pi = pi->parent();
-    }
-    while(pj.isNonnull()){
-      pvj.push_back(pj);
-      pj = pj->parent();
-    }
-    std::vector<art::Ptr<SimParticle> >::iterator ifnd;
-    ifnd = std::find(pvi.begin(),pvi.end(),sppj);
-    if(ifnd != pvi.end())return udaughter;
-    ifnd = std::find(pvj.begin(),pvj.end(),sppi);
-    if(ifnd != pvj.end())return umother;
-    for(size_t ii=0;ii<pvj.size();++ii){
-      ifnd = std::find(pvi.begin(),pvi.end(),pvj[ii]);
-      if(ifnd != pvi.end())return usibling;
-    }
-    for(size_t ii=0;ii<pvi.size();++ii){
-      ifnd = std::find(pvj.begin(),pvj.end(),pvi[ii]);
-      if(ifnd != pvj.end())return usibling;
+    if(pi.isNonnull() && pj.isNonnull()){
+      if( pi == pj)return sibling;
+      std::vector<art::Ptr<SimParticle> > pvi, pvj;
+      pvi.push_back(sppi);
+      pvj.push_back(sppj);
+      while(pi.isNonnull()){
+	pvi.push_back(pi);
+	pi = pi->parent();
+      }
+      while(pj.isNonnull()){
+	pvj.push_back(pj);
+	pj = pj->parent();
+      }
+      std::vector<art::Ptr<SimParticle> >::iterator ifnd;
+      ifnd = std::find(pvi.begin(),pvi.end(),sppj);
+      if(ifnd != pvi.end())return udaughter;
+      ifnd = std::find(pvj.begin(),pvj.end(),sppi);
+      if(ifnd != pvj.end())return umother;
+      for(size_t ii=0;ii<pvj.size();++ii){
+	ifnd = std::find(pvi.begin(),pvi.end(),pvj[ii]);
+	if(ifnd != pvi.end())return usibling;
+      }
+      for(size_t ii=0;ii<pvi.size();++ii){
+	ifnd = std::find(pvj.begin(),pvj.end(),pvi[ii]);
+	if(ifnd != pvj.end())return usibling;
+      }
     }
     return none;
   }
