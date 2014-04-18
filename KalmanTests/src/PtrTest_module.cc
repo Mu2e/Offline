718bcc88bf44d5dd08e6cde6b6f5529e581ff98d
kutschke <>
2014-04-18 16:55:36 +0000

    First release.

---
+++ KalmanTests/src/PtrTest_module.cc
@@ -0,0 +1,96 @@
+//
+// Example of how to read a KalRepCollection and create a matching KalRepPtrCollection.
+// This can be used as a model for writing a module that merges two KalRepCollections.
+//
+// $Id: PtrTest_module.cc,v 1.1 2014/04/18 16:55:36 kutschke Exp $
+// $Author: kutschke $
+// $Date: 2014/04/18 16:55:36 $
+//
+// Original author Rob Kutschke
+//
+
+// Framework includes.
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Utilities/InputTag.h"
+
+// Need this for the BaBar headers.
+using namespace CLHEP;
+
+// BaBar includes
+#include "BaBar/BaBar.hh"
+#include "KalmanTrack/KalRep.hh"
+#include "BaBar/TrkBase/include/TrkParticle.hh"
+
+// mu2e tracking
+#include "KalmanTests/inc/TrkFitDirection.hh"
+
+// C++ includes.
+#include <iostream>
+#include <string>
+
+// This is fragile and needs to be last until CLHEP is
+// properly qualified and included in the BaBar classes.
+#include "KalmanTests/inc/KalRepCollection.hh"
+#include "KalmanTests/inc/KalRepPtrCollection.hh"
+
+using namespace std;
+
+namespace mu2e {
+
+  class PtrTest : public art::EDProducer {
+  public:
+
+    explicit PtrTest(fhicl::ParameterSet const& pset);
+    void produce( art::Event& e) override;
+
+  private:
+
+    // Information about the data product that contains the fitted tracks.
+    // The instance name is also used as the instance name of the output data product.
+    TrkParticle     _tpart;
+    TrkFitDirection _fdir;
+    std::string     _instanceName;
+    art::InputTag   _inputTag;
+
+  };
+
+}  // end namespace mu2e
+
+mu2e::PtrTest::PtrTest(fhicl::ParameterSet const& pset):
+  _tpart((TrkParticle::type)(pset.get<int>("fitparticle",TrkParticle::e_minus))),
+  _fdir((TrkFitDirection::FitDirection)(pset.get<int>("fitdirection",TrkFitDirection::downstream))),
+  _instanceName( _fdir.name() + _tpart.name()),
+  _inputTag( pset.get<string>("fitterModuleLabel"), _instanceName)
+{
+  produces<KalRepPtrCollection>(_instanceName);
+}
+
+void mu2e::PtrTest::produce(art::Event& event)
+{
+
+  // Access the input data product.
+  auto trksHandle  = event.getValidHandle<KalRepCollection>(_inputTag);
+  auto const& inputTracks = *trksHandle;
+
+  // Create an empty output data product.
+  unique_ptr<KalRepPtrCollection> outputTracks(new KalRepPtrCollection );
+  outputTracks->reserve(inputTracks.size());
+
+  // Populate the output data product.
+  for ( size_t i=0; i<inputTracks.size(); ++i ){
+
+    KalRep const* krep = inputTracks.get(i);
+    if ( !krep ) continue;
+
+    outputTracks->emplace_back(trksHandle,i);
+  }
+
+  // Add the output data product to the event.
+  event.put(std::move(outputTracks),_instanceName);
+
+}
+
+DEFINE_ART_MODULE(mu2e::PtrTest);
---
+++ KalmanTests/src/ReadPtr_module.cc
@@ -0,0 +1,109 @@
+//
+// Example of accessing fitted tracks via a KalRepPtrCollection.
+//
+// $Id: ReadPtr_module.cc,v 1.1 2014/04/18 16:55:36 kutschke Exp $
+// $Author: kutschke $
+// $Date: 2014/04/18 16:55:36 $
+//
+// Original author Rob Kutschke
+//
+
+// Framework includes.
+#include "art/Framework/Core/EDAnalyzer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Services/Optional/TFileService.h"
+#include "art/Utilities/InputTag.h"
+
+// Need this for the BaBar headers.
+using namespace CLHEP;
+
+// BaBar includes
+#include "BaBar/BaBar.hh"
+#include "KalmanTrack/KalRep.hh"
+#include "BaBar/TrkBase/include/TrkParticle.hh"
+
+// mu2e tracking
+#include "KalmanTests/inc/TrkFitDirection.hh"
+
+// This is fragile and needs to be last until CLHEP is
+// properly qualified and included in the BaBar classes.
+//#include "KalmanTests/inc/KalRepCollection.hh"
+#include "KalmanTests/inc/KalRepCollection.hh"
+#include "KalmanTests/inc/KalRepPtrCollection.hh"
+
+#include "TH1D.h"
+
+// C++ includes.
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+namespace mu2e {
+
+  class ReadPtr : public art::EDAnalyzer {
+  public:
+
+    explicit ReadPtr(fhicl::ParameterSet const& pset);
+
+    void analyze( art::Event const& e) override;
+    void beginJob() override;
+
+  private:
+
+    // Information about the data product that contains the fitted tracks.
+    TrkParticle     _tpart;
+    TrkFitDirection _fdir;
+    std::string     _instanceName;
+    art::InputTag   _inputTag;
+
+    TH1D* _hNTracks;
+    TH1D* _hFitStatus;
+    TH1D* _hP;
+
+  };
+}   // end namespace mu2e
+
+mu2e::ReadPtr::ReadPtr(fhicl::ParameterSet const& pset):
+  EDAnalyzer(pset),
+  _tpart((TrkParticle::type)(pset.get<int>("fitparticle",TrkParticle::e_minus))),
+  _fdir((TrkFitDirection::FitDirection)(pset.get<int>("fitdirection",TrkFitDirection::downstream))),
+  _instanceName( _fdir.name() + _tpart.name()),
+  _inputTag( pset.get<string>("fitterModuleLabel"), _instanceName)
+{}
+
+void mu2e::ReadPtr::beginJob(){
+  art::ServiceHandle<art::TFileService> tfs;
+  _hNTracks   = tfs->make<TH1D>( "hNTracks",   "Number of tracks per Event",  5,  0.,   5.);
+  _hFitStatus = tfs->make<TH1D>( "hFitStatus", "Fit Status",                  10, 0.,  10.);
+  _hP         = tfs->make<TH1D>( "hP",         "Reconstructed Momentum",     30, 80., 110.);
+}
+
+void mu2e::ReadPtr::analyze(art::Event const& event) {
+
+  auto trksHandle    = event.getValidHandle<KalRepPtrCollection>(_inputTag);
+  auto const& tracks = *trksHandle;
+
+  _hNTracks->Fill(tracks.size());
+
+  for ( auto const& trk : tracks ){
+
+    KalRep const& krep = *trk;
+
+    int stat = krep.fitStatus().success();
+    _hFitStatus->Fill(stat);
+
+    if ( stat != 1 ) continue;
+
+    double s0   = krep.startValidRange();
+    _hP->Fill( krep.momentum(s0).mag() ) ;
+
+
+  }
+
+}
+
+
+DEFINE_ART_MODULE(mu2e::ReadPtr);
