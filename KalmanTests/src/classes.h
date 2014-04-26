626d15fd2fe5c79f9d64c9e32ee237282b36be0e
murat <>
2014-04-26 18:13:38 +0000

    - add a template for art::Ptr<KalRep>, which apparently was missing

--- KalmanTests/src/classes.h
+++ KalmanTests/src/classes.h
@@ -1,9 +1,9 @@
 //
 // Build a dictionary.
 //
-// $Id: classes.h,v 1.6 2014/04/18 16:40:18 kutschke Exp $
-// $Author: kutschke $
-// $Date: 2014/04/18 16:40:18 $
+// $Id: classes.h,v 1.7 2014/04/26 18:13:38 murat Exp $
+// $Author: murat $
+// $Date: 2014/04/26 18:13:38 $
 //
 // Original author Rob Kutschke
 //
@@ -19,6 +19,7 @@ using namespace CLHEP;
 #include "KalmanTests/inc/KalRepPtrCollection.hh"
 #include "KalmanTests/inc/KalFitMC.hh"
 
+template class art::Ptr<KalRep>;
 template class art::Wrapper<mu2e::KalRepCollection>;
 template class art::Wrapper<mu2e::KalRepPtrCollection>;
 template class std::vector<mu2e::TrkStrawHitInfo>;
