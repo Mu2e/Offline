06bcf81af3b2262181723825a703116aa24dcbd5
gandr <>
2014-05-27 20:09:50 +0000

    The TrackSummaryRecoMap transient class provides bookkeeping to separate reco and truth handling paths for TrackSummary.

--- KalmanTests/src/classes.h
+++ KalmanTests/src/classes.h
@@ -1,9 +1,9 @@
 //
 // Build a dictionary.
 //
-// $Id: classes.h,v 1.7 2014/04/26 18:13:38 murat Exp $
-// $Author: murat $
-// $Date: 2014/04/26 18:13:38 $
+// $Id: classes.h,v 1.8 2014/05/27 20:09:50 gandr Exp $
+// $Author: gandr $
+// $Date: 2014/05/27 20:09:50 $
 //
 // Original author Rob Kutschke
 //
@@ -18,8 +18,16 @@ using namespace CLHEP;
 #include "KalmanTests/inc/KalRepCollection.hh"
 #include "KalmanTests/inc/KalRepPtrCollection.hh"
 #include "KalmanTests/inc/KalFitMC.hh"
+#include "KalmanTests/inc/TrackSummaryRecoMap.hh"
 
 template class art::Ptr<KalRep>;
 template class art::Wrapper<mu2e::KalRepCollection>;
 template class art::Wrapper<mu2e::KalRepPtrCollection>;
 template class std::vector<mu2e::TrkStrawHitInfo>;
+
+template class std::pair<art::Ptr<art::Ptr<KalRep> >, art::Ptr<mu2e::TrackSummary> >;
+template class std::pair<art::Ptr<mu2e::TrackSummary>, art::Ptr<art::Ptr<KalRep> > >;
+template class art::Assns<art::Ptr<KalRep>, mu2e::TrackSummary>;
+template class art::Assns<mu2e::TrackSummary, art::Ptr<KalRep> >;
+template class art::Wrapper<art::Assns<art::Ptr<KalRep>, mu2e::TrackSummary> >;
+template class art::Wrapper<art::Assns<mu2e::TrackSummary, art::Ptr<KalRep> > >;
