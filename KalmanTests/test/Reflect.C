257c9e88fe396c1c5903c994eb704c4745aa851e
brownd <>
2014-09-10 18:52:12 +0000

    Update diagnostic

--- KalmanTests/test/Reflect.C
+++ KalmanTests/test/Reflect.C
@@ -58,8 +58,8 @@ void init() {
   snprintf(ctext,80,"dtd>%4.3f&&dtd<%4.3f",tdlow,tdhigh);
   dtd = TCut(ctext);
 
-  double momlow(95);
-  double momhigh(115);
+  double momlow(80);
+  double momhigh(140);
   snprintf(ctext,80,"umom>%4.3f&&umom<%4.3f",momlow,momhigh);
   umom = TCut(ctext);
   snprintf(ctext,80,"dmom>%4.3f&&dmom<%4.3f",momlow,momhigh);
@@ -84,7 +84,7 @@ void init() {
   dgood = dreco+dnact+dmomerr+dt0err+dmom+dtd+dfitc;
 
   goodpair = ugood+dgood;
-  goodmc = TCut("umcmom>0.0&&dmcmom>0.0");
+  goodmc = TCut("umcmom>0.0&&dmcmom>0.0&&abs(umcpdgid)==11&&umcpdgid==dmcpdgid");
 
   ud0low = TCut("ud0<100");
   dd0low = TCut("dd0<100");
@@ -128,66 +128,49 @@ void momres(TTree* ref) {
   if(!isinit)init();
   TCanvas* rcan = new TCanvas("rcan","Momentum Resolution",1200,800);
   rcan->Clear();
-  rcan->Divide(2,2);
+  rcan->Divide(2,1);
   gStyle->SetOptFit(111111);
   gStyle->SetOptStat("oumr");
 
   rcan->cd(1);
   gPad->SetLogy();
-  TH1F* umomres = new TH1F("umomres","Upstream momentum resolution;MeV",251,momlo,momhi);
-  ref->Project("umomres","umcmom-umom",ugood+goodmc+ud0low);
-  double upint = 2*umomres->GetEntries()*umomres->GetBinWidth(1);
+  TH1F* momres = new TH1F("momres","Single Track Momentum Resolution;MeV/c",201,momlo,momhi);
+  ref->Project("momres","umcmom-umom",ugood+goodmc);
+  cout << momres->GetEntries() <<endl;
+  ref->Project("+momres","dmom-dmcmom",dgood+goodmc);
+  cout << momres->GetEntries() <<endl;
+  double upint = 2*momres->GetEntries()*momres->GetBinWidth(1);
   cball->SetParameters(upint,0.0,0.1,3.0,0.8,0.02,0.3);
   cball->SetParLimits(5,0.001,0.4);
-  cball->SetParLimits(6,0.1,umomres->GetRMS());
-  umomres->Fit("cball","LIR");
+  cball->SetParLimits(6,0.1,momres->GetRMS());
+  momres->Fit("cball","LIR");
 
   rcan->cd(2);
   gPad->SetLogy();
-  TH1F* dmomres = new TH1F("dmomres","Downstream momentum resolution;MeV",251,momlo,momhi);
-  ref->Project("dmomres","dmom-dmcmom",dgood+goodmc+dd0low);
-  double dint = 2*dmomres->GetEntries()*dmomres->GetBinWidth(1);
-  cball->SetParameters(dint,0.0,0.1,3.0,0.8,0.02,0.3);
-  cball->SetParLimits(5,0.001,0.4);
-  cball->SetParLimits(6,0.1,dmomres->GetRMS());
-  dmomres->Fit("cball","LIR");
-
-  rcan->cd(3);
-  gPad->SetLogy();
-  TH1F* umomresd = new TH1F("umomresd","Upstream momentum resolution, d0 cut;MeV",251,momlo,momhi);
-  ref->Project("umomresd","umcmom-umom",ugood+goodmc+ud0hi);
-  double upintd = 2*umomresd->GetEntries()*umomresd->GetBinWidth(1);
+  TH1F* momresd = new TH1F("momresd","Single Track Momentum Resolution, d0 cut;MeV/c",201,momlo,momhi);
+  ref->Project("momresd","umcmom-umom",ugood+goodmc+ud0low);
+  ref->Project("+dmomresd","dmom-dmcmom",dgood+goodmc+dd0low);
+  double upintd = 2*momresd->GetEntries()*momresd->GetBinWidth(1);
   cball->SetParameters(upintd,0.0,0.1,3.0,0.8,0.02,0.3);
   cball->SetParLimits(5,0.001,0.4);
-  cball->SetParLimits(6,0.1,umomresd->GetRMS());
-  umomresd->Fit("cball","LIR");
-
-  rcan->cd(4);
-  gPad->SetLogy();
-  TH1F* dmomresd = new TH1F("dmomresd","Downstream momentum resolution, d0 cut;MeV",251,momlo,momhi);
-  ref->Project("dmomresd","dmom-dmcmom",dgood+goodmc+dd0hi);
-  double dintd = 2*dmomresd->GetEntries()*dmomresd->GetBinWidth(1);
-  cball->SetParameters(dintd,0.0,0.1,3.0,0.8,0.02,0.3);
-  cball->SetParLimits(5,0.001,0.4);
-  cball->SetParLimits(6,0.1,dmomresd->GetRMS());
-  dmomresd->Fit("cball","LIR");
-
+  cball->SetParLimits(6,0.1,momresd->GetRMS());
+  momresd->Fit("cball","LIR");
 }
 
 void diffres(TTree* ref) {
   if(!isinit)init();
-  TH1F* momdiff = new TH1F("momdiff","Reco Downstream - Upstream momentum",251,difflo,diffhi);
-  TH1F* mcmomdiff = new TH1F("mcmomdiff","True Downstream - Upstream momentum",251,difflo,diffhi);
-  TH1F* dmomdiff = new TH1F("dmomdiff","Reco Downstream - Upstream momentum, d0 cut",251,difflo,diffhi);
-  TH1F* dmcmomdiff = new TH1F("dmcmomdiff","True Downstream - Upstream momentum, d0 cut",251,difflo,diffhi);
+  TH1F* momdiff = new TH1F("momdiff","Reco Downstream - Upstream momentum",201,difflo,diffhi);
+  TH1F* mcmomdiff = new TH1F("mcmomdiff","True Downstream - Upstream momentum",201,difflo,diffhi);
+  TH1F* dmomdiff = new TH1F("dmomdiff","Reco Downstream - Upstream momentum, d0 cut",201,difflo,diffhi);
+  TH1F* dmcmomdiff = new TH1F("dmcmomdiff","True Downstream - Upstream momentum, d0 cut",201,difflo,diffhi);
 
   TCut notarget("abs(ud0>100)||(abs(uz0>200)&&utd>-.7)");
 
-  ref->Project("momdiff","dmom-umom",ugood+dgood+goodmc+ud0low+dd0low);
-  ref->Project("mcmomdiff","dmcmom-umcmom",ugood+dgood+goodmc+ud0low+dd0low);
+  ref->Project("momdiff","dmom-umom",ugood+dgood+goodmc);
+  ref->Project("mcmomdiff","dmcmom-umcmom",ugood+dgood+goodmc);
 
-  ref->Project("dmomdiff","dmom-umom",ugood+dgood+goodmc+notarget);
-  ref->Project("dmcmomdiff","dmcmom-umcmom",ugood+dgood+goodmc+notarget);
+  ref->Project("dmomdiff","dmom-umom",ugood+dgood+goodmc+dd0low+ud0low);
+  ref->Project("dmcmomdiff","dmcmom-umcmom",ugood+dgood+goodmc+dd0low+ud0low);
 
   TCanvas* dcan = new TCanvas("dcan","Momentum Difference",1200,800);
   dcan->Clear();
