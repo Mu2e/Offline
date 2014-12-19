dc6475513ee6a18f8c8c5f6f7cbcb5aae86c3659
Ray Culbertson <rlc@fnal.gov>
2014-12-19 16:22:08 -0600

    a file to be sourced to create bash functions for easy reference to scripts

---
+++ functions.sh
@@ -0,0 +1,28 @@
+#
+# bash functions to provide easy reference to scripts in Offline
+#
+
+jsonMaker() {
+  $MU2E_BASE_RELEASE/Tools/DH/jsonMaker.py "$*"
+}
+export jsonMaker
+
+samRm() {
+  $MU2E_BASE_RELEASE/Tools/DH/samRm.sh "$*"
+}
+export samRm
+
+samGet() {
+  $MU2E_BASE_RELEASE/Tools/DH/samGet.sh "$*"
+}
+export samGet
+
+samPrestage() {
+  $MU2E_BASE_RELEASE/Tools/DH/samPrestage.sh "$*"
+}
+export samPrestage
+
+samDatasets() {
+  $MU2E_BASE_RELEASE/Tools/DH/samDatasets.sh "$*"
+}
+export samDatasets
