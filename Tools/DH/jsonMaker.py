f6a8122eca6c30ce4af4cf3e015ff1fa89b14848
Ray Culbertson <rlc@fnal.gov>
2014-12-22 10:26:34 -0600

    allow root files in phy-sim

--- Tools/DH/jsonMaker.py
+++ Tools/DH/jsonMaker.py
@@ -611,8 +611,9 @@ def buildJsonOther(par, file, jp):
         if par.verbose>4:
             print "ERROR - file family type \"phy-\" but owner not mu2e"
     # only expect art files in sim ff
+    # and occasionally a root file like mustops
     if par.file_family[4:7]=="sim" and \
-       jp['file_format'] != "art":
+           (jp['file_format'] != "art" and jp['file_format'] != "root"):
         file.state = file.state | file.WRONGFILEFAMILY
         if par.verbose>4:
             print "ERROR - file family type \"-sim\" but file type not art"
