cd49e8eb093599695af775d1ba8e4dcc92fd3fc3
David Nathan Brown <dave_brown@lbl.gov>
2015-03-16 17:55:52 -0700

    update BaBar version

--- setup.sh
+++ setup.sh
@@ -75,7 +75,7 @@ source ${MU2E_BASE_RELEASE}/bin/setup_mu2e_project.sh
 
 # Check out the BaBar code.
 # First build the symlink directory.  Then checkout the code.
-babarversion=615
+babarversion=616
 if [  -f "./BaBar/makeInclude.sh" ]; then
   source ./BaBar/makeInclude.sh
   if [ ! -f "BaBar/BaBar/include/BaBar.hh" ]; then
