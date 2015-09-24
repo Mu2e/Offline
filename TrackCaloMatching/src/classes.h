510b66ae126000e3de9793beda1c6192bcc6f8c1
Pavel Murat <murat@fnal.gov>
2015-09-23 23:58:33 -0500

    cleanup: remove obsolete classes

--- TrackCaloMatching/src/classes.h
+++ TrackCaloMatching/src/classes.h
@@ -14,21 +14,12 @@
 #include "art/Persistency/Common/Wrapper.h"
 
 #include "TrackCaloMatching/inc/TrkToCaloExtrapol.hh"
-// #include "KalmanTrack/KalRep.hh"
 #include "TrackCaloMatching/inc/TrkToCaloExtrapolCollection.hh"
 #include "TrackCaloMatching/inc/TrackClusterMatch.hh"
-#include "TrackCaloMatching/inc/TrackClusterMatchOld.hh"
 #include "RecoDataProducts/inc/CaloCluster.hh"
-
-/* template class art::Ptr<mu2e::CaloCluster>; */
-
-/* template class art::Ptr<KalRep>; */
 
 template class std::vector <mu2e::TrackClusterMatch>;
 template class art::Wrapper<mu2e::TrackClusterMatchCollection>;
-
-template class std::vector <mu2e::TrackClusterMatchOld>;
-template class art::Wrapper<mu2e::TrackClusterMatchOldCollection>;
 
 template class art::Ptr<mu2e::TrkToCaloExtrapol>;
 template class std::vector<art::Ptr<mu2e::TrkToCaloExtrapol> >;
