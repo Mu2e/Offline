d5318bca18235f941522c25ff54b166d5ec22cd2
Rob Kutschke <kutschke@fnal.gov>
2015-11-13 15:12:58 -0600

    Move Maker to the GeometryService.

--- GeometryService/src/DetectorSolenoidShieldingMaker.cc
+++ GeometryService/src/DetectorSolenoidShieldingMaker.cc
@@ -1,7 +1,7 @@
 // Mu2e includes
 #include "ConfigTools/inc/SimpleConfig.hh"
 #include "DetectorSolenoidGeom/inc/DetectorSolenoid.hh"
-#include "DetectorSolenoidGeom/inc/DetectorSolenoidShieldingMaker.hh"
+#include "GeometryService/inc/DetectorSolenoidShieldingMaker.hh"
 #include "DetectorSolenoidGeom/inc/DetectorSolenoidShielding.hh"
 #include "GeomPrimitives/inc/Tube.hh"
 
@@ -26,83 +26,83 @@ namespace mu2e {
     const CLHEP::Hep3Vector& dsPos = ds.position();
 
     // Vacuum pump spool piece
-    dss->_vpspCryoSeal.reset( new Tube( c.getString("vpsp.material"), 
-                                        CLHEP::Hep3Vector( dsPos.x(), 
+    dss->_vpspCryoSeal.reset( new Tube( c.getString("vpsp.material"),
+                                        CLHEP::Hep3Vector( dsPos.x(),
                                                            dsPos.y(),
                                                            ds.cryoZMax()+c.getDouble("vpsp.cryoseal.z") ),
                                         c.getDouble("vpsp.cryoseal.rIn"),
                                         c.getDouble("vpsp.cryoseal.rOut"),
                                         c.getDouble("vpsp.cryoseal.halfLength") ) );
-    
+
     dss->_dssTubes.push_back( dss->getVPSPCryoSeal() );
 
-    dss->_vpspMain.reset( new Tube( c.getString("vpsp.material"), 
-                                    CLHEP::Hep3Vector( dsPos.x(), 
+    dss->_vpspMain.reset( new Tube( c.getString("vpsp.material"),
+                                    CLHEP::Hep3Vector( dsPos.x(),
                                                        dsPos.y(),
-                                                       ds.cryoZMax()+c.getDouble("vpsp.main.z") ),                   
-                                    c.getDouble("vpsp.main.rIn" ,ds.rIn1() ),      
-                                    c.getDouble("vpsp.main.rOut",ds.rIn2()),     
+                                                       ds.cryoZMax()+c.getDouble("vpsp.main.z") ),
+                                    c.getDouble("vpsp.main.rIn" ,ds.rIn1() ),
+                                    c.getDouble("vpsp.main.rOut",ds.rIn2()),
                                     c.getDouble("vpsp.main.halfLength") ) );
 
     dss->_dssTubes.push_back( dss->getVPSPmain() );
 
-    dss->_vpspEndSeal.reset( new Tube( c.getString("vpsp.material"), 
-                                       CLHEP::Hep3Vector( dsPos.x(), 
+    dss->_vpspEndSeal.reset( new Tube( c.getString("vpsp.material"),
+                                       CLHEP::Hep3Vector( dsPos.x(),
                                                           dsPos.y(),
-                                                          ds.cryoZMax()+c.getDouble("vpsp.endseal.z") ),                   
-                                       c.getDouble("vpsp.endseal.rIn"),      
-                                       c.getDouble("vpsp.endseal.rOut"),     
+                                                          ds.cryoZMax()+c.getDouble("vpsp.endseal.z") ),
+                                       c.getDouble("vpsp.endseal.rIn"),
+                                       c.getDouble("vpsp.endseal.rOut"),
                                        c.getDouble("vpsp.endseal.halfLength") ) );
 
     dss->_dssTubes.push_back( dss->getVPSPendSeal() );
-    
-    dss->_vpspEndFlange.reset( new Tube( c.getString("vpsp.material"), 
-                                         CLHEP::Hep3Vector( dsPos.x(), 
+
+    dss->_vpspEndFlange.reset( new Tube( c.getString("vpsp.material"),
+                                         CLHEP::Hep3Vector( dsPos.x(),
                                                             dsPos.y(),
-                                                            ds.cryoZMax()+c.getDouble("vpsp.endflange.z") ),                   
-                                         c.getDouble("vpsp.endflange.rIn"),      
-                                         c.getDouble("vpsp.endflange.rOut"),     
+                                                            ds.cryoZMax()+c.getDouble("vpsp.endflange.z") ),
+                                         c.getDouble("vpsp.endflange.rIn"),
+                                         c.getDouble("vpsp.endflange.rOut"),
                                          c.getDouble("vpsp.endflange.halfLength") ) );
 
     dss->_dssTubes.push_back( dss->getVPSPendFlange() );
 
     // Instrumentation bulkhead feedthrough
-    dss->_ifbMain.reset( new Tube( c.getString("ifb.material"), 
-                                   CLHEP::Hep3Vector( dsPos.x(), 
+    dss->_ifbMain.reset( new Tube( c.getString("ifb.material"),
+                                   CLHEP::Hep3Vector( dsPos.x(),
                                                       dsPos.y(),
                                                       ds.cryoZMax()+c.getDouble("ifb.main.z") ),
-                                   c.getDouble("ifb.main.rIn"),      
-                                   c.getDouble("ifb.main.rOut"),     
+                                   c.getDouble("ifb.main.rIn"),
+                                   c.getDouble("ifb.main.rOut"),
                                    c.getDouble("ifb.main.halfLength") ) );
 
     dss->_dssTubes.push_back( dss->getIFBmain() );
-    
-    dss->_ifbEndSeal.reset( new Tube( c.getString("ifb.material"), 
-                                      CLHEP::Hep3Vector( dsPos.x(), 
+
+    dss->_ifbEndSeal.reset( new Tube( c.getString("ifb.material"),
+                                      CLHEP::Hep3Vector( dsPos.x(),
                                                          dsPos.y(),
-                                                         ds.cryoZMax()+c.getDouble("ifb.endseal.z") ),                   
-                                      c.getDouble("ifb.endseal.rIn"),      
-                                      c.getDouble("ifb.endseal.rOut"),     
+                                                         ds.cryoZMax()+c.getDouble("ifb.endseal.z") ),
+                                      c.getDouble("ifb.endseal.rIn"),
+                                      c.getDouble("ifb.endseal.rOut"),
                                       c.getDouble("ifb.endseal.halfLength") ) );
 
     dss->_dssTubes.push_back( dss->getIFBendSeal() );
 
-    dss->_ifbEndPlug.reset( new Tube( c.getString("ifb.material"), 
-                                      CLHEP::Hep3Vector( dsPos.x(), 
+    dss->_ifbEndPlug.reset( new Tube( c.getString("ifb.material"),
+                                      CLHEP::Hep3Vector( dsPos.x(),
                                                          dsPos.y(),
-                                                         ds.cryoZMax()+c.getDouble("ifb.endplug.z") ),                   
-                                      c.getDouble("ifb.endplug.rIn"),      
-                                      c.getDouble("ifb.endplug.rOut"),     
+                                                         ds.cryoZMax()+c.getDouble("ifb.endplug.z") ),
+                                      c.getDouble("ifb.endplug.rIn"),
+                                      c.getDouble("ifb.endplug.rOut"),
                                       c.getDouble("ifb.endplug.halfLength") ) );
 
     dss->_dssTubes.push_back( dss->getIFBendPlug() );
 
-    dss->_ifbEndWindow.reset( new Tube( c.getString("ifb.endwindow.material", c.getString("ifb.material") ), 
-                                        CLHEP::Hep3Vector( dsPos.x(), 
+    dss->_ifbEndWindow.reset( new Tube( c.getString("ifb.endwindow.material", c.getString("ifb.material") ),
+                                        CLHEP::Hep3Vector( dsPos.x(),
                                                            dsPos.y(),
-                                                           ds.cryoZMax()+c.getDouble("ifb.endwindow.z") ),                   
-                                        c.getDouble("ifb.endwindow.rIn"),      
-                                        c.getDouble("ifb.endwindow.rOut"),     
+                                                           ds.cryoZMax()+c.getDouble("ifb.endwindow.z") ),
+                                        c.getDouble("ifb.endwindow.rIn"),
+                                        c.getDouble("ifb.endwindow.rOut"),
                                         c.getDouble("ifb.endwindow.halfLength") ) );
 
     dss->_dssTubes.push_back( dss->getIFBendWindow() );
