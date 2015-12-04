f083224f7abbbad1a4e0a9d7f59507c870b73a2b
Andrew Edmonds <aedmonds@lbl.gov>
2015-12-04 10:33:42 -0800

    Use the new CD3 number for the number of flash particles per POT

--- Projects/edmonds-ipa-and-beam-test-sims/scripts/BeamFlashRun.h
+++ Projects/edmonds-ipa-and-beam-test-sims/scripts/BeamFlashRun.h
@@ -15,7 +15,8 @@ BeamFlashRun::BeamFlashRun(std::string filename) : BaseRun(filename, "Flash") {
 
   std::cout << "Mean EDep = " << GetSHEDepPlot()->GetMean() << std::endl;
   std::cout << "Flash Charge Deposit per Hit = " << fChargeDepositPerHit << std::endl;
-  double n_flashes_per_POT = 0.0022923; // from Doc-DB 3774: Table 1
+  double n_flashes_per_POT = 0.0022923; // from Doc-DB 3774: Table 1 (TDR number)
+  n_flashes_per_POT = 0.00202; //from Doc-DB 6273: Table 2 (CD3 number)
   fNParticlesPerMicrobunch = n_POT_per_microbunch * n_flashes_per_POT;
 }
 
