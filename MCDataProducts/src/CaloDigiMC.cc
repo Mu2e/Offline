7ca84d53fdb7d88271b896db62d9374b813b0e09
gianipez <gianipez@fnal.gov>
2015-12-18 17:47:44 -0600

    MC truth for new calorimeter-digitization

---
+++ MCDataProducts/src/CaloDigiMC.cc
@@ -0,0 +1,67 @@
+//
+// This is a place to put additional information produced by HitMaker,
+//
+
+// Original author Stefano Roberto Soleti
+//
+
+// C++ includes
+#include <ostream>
+
+// Framework includes.
+#include "cetlib/exception.h"
+
+// Mu2e includes
+#include "MCDataProducts/inc/CaloDigiMC.hh"
+
+using namespace std;
+
+namespace mu2e {
+  void          CaloDigiMC::init(){
+    _nParticles = 0;
+    _totalEDep  = 0.;
+    _meanTime   = 0.;
+  }
+
+  
+  void          CaloDigiMC::addCaloShower(const CaloShowerStepMC* CaloShower)  {
+    
+    double    csEdep  = CaloShower->energy();
+    double    csTime  = CaloShower->time();
+    
+    int       csSimId = CaloShower->simParticle()->id().asInt();
+    
+    
+      //first: search if the particle is already present
+    int      found(0), index(-1);
+      
+      for (int i=0; i<_nParticles; ++i){
+	int   simId = _simParticle.at(i)->id().asInt();
+	if (simId == csSimId){
+	  found = 1;
+	  index = i;
+	  break;
+	}
+      }
+      
+      if (found == 1){//update the information
+	_time.at(index) = (_time.at(index)*eDep(index) + csEdep*csTime) / (eDep(index) + csEdep);
+	_eDep.at(index) += csEdep; 
+      }else{   //add particle
+	
+	_eDep. push_back(csEdep);
+	_time. push_back(csTime);
+	_pdgId.push_back(CaloShower->simParticle()->pdgId());
+	
+	_simParticle.push_back(CaloShower->simParticle());
+	
+	++_nParticles;
+      }
+      
+      _meanTime     =  (_meanTime * _totalEDep + csEdep*csTime) / (_totalEDep    + csEdep);
+      _totalEDep    += csEdep;
+
+
+  }
+
+} // namespace mu2e
