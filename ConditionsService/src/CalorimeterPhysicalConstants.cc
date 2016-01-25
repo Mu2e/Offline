204982c8466b3b48c3f41ceee7e17bcf074d87f8
Robert Bernstein <rhbob@fnal.gov>
2016-01-25 10:23:41 -0600

    fixing use of conditions service

--- ConditionsService/src/CalorimeterPhysicalConstants.cc
+++ ConditionsService/src/CalorimeterPhysicalConstants.cc
@@ -16,19 +16,10 @@
 namespace mu2e {
 
   CalorimeterPhysicalConstants::CalorimeterPhysicalConstants( SimpleConfig const& config )
-  {
-    //
-    // initialize with absurd values
-	_radiationLength = 0 ;	
-	_criticalEnergyPos = 0;
-	_criticalEnergyNeg = 0;
-        _density = 0;
+    :_radiationLength(config.getDouble("calorimeter.radiationLengthCsI")*CLHEP::cm)
+    ,_criticalEnergyPos(config.getDouble("calorimeter.criticalEnergyPosCsI")*CLHEP::MeV) // e^-
+    ,_criticalEnergyNeg(config.getDouble("calorimeter.criticalEnergyNegCsI")*CLHEP::MeV) // e^-
+    ,_density(config.getDouble("calorimeter.densityCsI")*CLHEP::g/CLHEP::cm3) // g/cc 
 
-	_radiationLength   = config.getDouble("calorimeter.radiationLengthCsI")*CLHEP::cm ; //cm
-	_criticalEnergyPos = config.getDouble("calorimeter.criticalEnergyPosCsI")*CLHEP::MeV; // e^-
-	_criticalEnergyNeg = config.getDouble("calorimeter.criticalEnergyNegCsI")*CLHEP::MeV; // e^-
-	_density           = config.getDouble("calorimeter.densityCsI")*CLHEP::g/CLHEP::cm3 ; // g/cc 
-
-  }
-
+  {}
 }
