bf86c87493a6943d0339e9932342d47db0a41384
Andrei Gaponenko <gandr@fnal.gov>
2016-03-01 09:52:37 -0600

    Add EventWeightHelper to Mu2eUtilities.

---
+++ Mu2eUtilities/inc/EventWeightHelper.hh
@@ -0,0 +1,45 @@
+// Compute the final event weight by multiplying several EventWeight
+// inputs, and produce a diagnostic histogram.
+//
+// The object is configured at construction time.
+// The update() method must be called once at the beginning of
+// a new event and before using the weight() call.
+//
+// Andrei Gaponenko, 2016
+
+#ifndef Mu2eUtilities_inc_EventWeightHelper_hh
+#define Mu2eUtilities_inc_EventWeightHelper_hh
+
+#include <string>
+
+#include "art/Utilities/InputTag.h"
+#include "art/Framework/Services/Optional/TFileDirectory.h"
+
+namespace fhicl { class ParameterSet; }
+namespace art { class Event; }
+class TH1;
+
+namespace mu2e {
+
+  class EventWeightHelper {
+  public:
+    // must be called once per event, before any calls to the weight() method
+    void update(const art::Event& evt);
+
+    double weight() const { return weight_; }
+
+    // histograms will be placed in topdir/subdir
+    EventWeightHelper(const fhicl::ParameterSet& pset,
+                      art::TFileDirectory topdir,
+                      const std::string& subdir);
+
+  private:
+    typedef std::vector<art::InputTag> InputTags;
+    InputTags inputs_;
+    double weight_;
+
+    TH1 *hfinal_;
+  };
+}
+
+#endif/*Mu2eUtilities_inc_EventWeightHelper_hh*/
---
+++ Mu2eUtilities/src/EventWeightHelper.cc
@@ -0,0 +1,41 @@
+// Andrei Gaponenko, 2016
+
+#include "Mu2eUtilities/inc/EventWeightHelper.hh"
+
+#include "fhiclcpp/ParameterSet.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Services/Optional/TFileDirectory.h"
+
+#include "MCDataProducts/inc/EventWeight.hh"
+
+#include "TH1.h"
+
+namespace mu2e {
+
+  EventWeightHelper::EventWeightHelper(const fhicl::ParameterSet& pset,
+                                       art::TFileDirectory topdir,
+                                       const std::string& subdir)
+    : inputs_{pset.is_empty() ? InputTags() : pset.get<InputTags>("inputs")}
+    , weight_{-1.}
+    , hfinal_{nullptr}
+  {
+    if(!pset.is_empty()) {
+      art::TFileDirectory tfdir = subdir.empty() ? topdir : topdir.mkdir(subdir.c_str());
+      const int nbins = pset.get<unsigned>("hist.nbins", 100);
+      const int xmin = pset.get<unsigned>("hist.xmin", 0.);
+      const int xmax = pset.get<unsigned>("hist.xmax", 2.);
+      hfinal_ = tfdir.make<TH1D>("finalWeight", "Final event weight", nbins, xmin, xmax);
+    }
+  }
+
+  void EventWeightHelper::update(const art::Event& evt) {
+    weight_ = 1.;
+    for(const auto& in: inputs_) {
+      const auto& wh = evt.getValidHandle<EventWeight>(in);
+      weight_ *= wh->weight();
+    }
+    if(hfinal_) {
+      hfinal_->Fill(weight_);
+    }
+  }
+}
