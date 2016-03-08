937853dbeeb9cc245d04aa0991dc4a205ca82558
Ralf Ehrlich <ehrlich@mu2egpvm01.fnal.gov>
2016-03-07 23:01:10 -0600

    added CRV efficiency scripts

---
+++ CRVResponse/efficiencyCheck/CRV_Efficiency_check_count_5cm_verticalPlanes.sh
@@ -0,0 +1,47 @@
+layerOffset=42
+moduleGap=5
+timeWindow=10
+sides=2
+#  for moduleGap in {2..5}
+#  do
+#    for layerOffset in {0..62..2}
+#    do
+
+       for PEthreshold in {6..16..2}
+       do
+
+         for photonYield in {3000,3500,4000,4500,5000,5500,6000,6500,7000}  # 20,??,26,??,33,??,40,??,?? PE/SiPM for 5cm wide / 5.6m long counter
+         do
+
+              files=`ls /pnfs/mu2e/scratch/outstage/ehrlich/CRV_efficiency5cm_side_moduleGap$moduleGap'_'layerOffset$layerOffset'_'photonYield$photonYield.*/*/mu2e.log`
+
+              events=0
+              eventsCoincidence=0
+
+              for file in $files;
+              do
+
+                searchString="SUMMARY CrvCoincidencePE$PEthreshold""T$timeWindow""C$sides""s"
+                searchCommand="grep '$searchString' $file"
+#                searchCommand="tail -n 70 $file | grep '$searchString'"
+                searchResult=`eval $searchCommand`
+                if [ $? -ne 0 ]; then
+                  continue
+                fi
+
+                s=($searchResult)
+                eventsTmp=${s[4]}
+                eventsCoincidenceTmp=${s[2]}
+
+                events=$((events+eventsTmp))
+                eventsCoincidence=$((eventsCoincidence+eventsCoincidenceTmp))
+
+              done
+
+              inefficiency=`echo "$eventsCoincidence/$events" | bc -l`
+              echo modulegap:$moduleGap layerOffset:$layerOffset photonYield:$photonYield PEThreshold:$PEthreshold timeWindow:$timeWindow sidesChecked:$sides eventsCoincidence:$eventsCoincidence eventsTotal:$events inefficiency:$inefficiency
+
+         done
+       done
+#    done
+#  done
---
+++ CRVResponse/efficiencyCheck/CRV_Efficiency_check_submit_5cm_6600.sh
@@ -0,0 +1,60 @@
+i=0
+layerOffset=42
+moduleGap=5
+#  for moduleGap in {2..5}
+#  do
+
+     dz=$((822+$moduleGap))
+
+#    for layerOffset in {0..62..2}
+#    do
+      for photonYield in {3000,3500,4000,4500,5000,5500,6000,6500,7000}  # 20,??,26,??,33,??,40,??,?? PE/SiPM for 5cm wide / 5.6m long counter
+      do
+
+        ((i++));
+
+        scintTolerance="0"
+#        backgroundParam1="2.9e-4"
+        backgroundParam1="0"
+        backgroundParam3=$(echo "scale=3; 5000/$photonYield" | bc)
+
+        genconfigfile=CRVResponse/efficiencyCheck/submit/genconfig_5cm_6600'_'$i.txt
+        echo "#include \"CRVResponse/efficiencyCheck/genconfig_5cm_6600.txt\"" >| $genconfigfile
+        echo "double cosmicDYB.dz = $dz;" >> $genconfigfile
+
+        geomfile=CRVResponse/efficiencyCheck/submit/geom_5cm_6600'_'$i.txt
+        echo "#include \"CRVResponse/efficiencyCheck/geom_5cm_6600.txt\"" >| $geomfile
+        echo "double crs.gapBetweenModules = $moduleGap;" >> $geomfile
+        echo "double crs.layerOffset       = $layerOffset;" >> $geomfile
+
+#### for adjacentPulseTimeDifference = 5ns
+
+        fclfile=CRVResponse/efficiencyCheck/submit/CRV_Efficiency_check_5cm_6600'_'$i.fcl
+        echo "#include \"CRVResponse/efficiencyCheck/CRV_Efficiency_check_5cm_6600.fcl\"" >| $fclfile
+        echo "services.user.GeometryService.inputFile                 : \"$geomfile\"" >> $fclfile
+        echo "physics.producers.generate.inputfile                    : \"$genconfigfile\"" >> $fclfile
+        echo "physics.producers.CrvPhotonArrivals.scintillationYield  : $photonYield" >> $fclfile
+        echo "physics.producers.CrvPhotonArrivals.scintillationYieldTolerance  : $scintTolerance" >> $fclfile
+        echo "physics.producers.CrvSiPMResponses.ThermalProb          : 0" >> $fclfile
+        echo "physics.producers.CrvSiPMResponses.backgroundParam1     : $backgroundParam1" >> $fclfile
+        echo "physics.producers.CrvSiPMResponses.backgroundParam3     : $backgroundParam3" >> $fclfile
+
+        mu2eart --setup=./setup.sh --fcl=$fclfile --njobs=50 --events-per-job=10000 --jobname=CRV_efficiency5cm_top6600_moduleGap$moduleGap'_'layerOffset$layerOffset'_'photonYield$photonYield --outstage=/pnfs/mu2e/scratch/outstage
+
+#### for adjacentPulseTimeDifference = 10ns
+
+        fclfile=CRVResponse/efficiencyCheck/submit/CRV_Efficiency_check_5cm10_6600'_'$i.fcl
+        echo "#include \"CRVResponse/efficiencyCheck/CRV_Efficiency_check_5cm10_6600.fcl\"" >| $fclfile
+        echo "services.user.GeometryService.inputFile                 : \"$geomfile\"" >> $fclfile
+        echo "physics.producers.generate.inputfile                    : \"$genconfigfile\"" >> $fclfile
+        echo "physics.producers.CrvPhotonArrivals.scintillationYield  : $photonYield" >> $fclfile
+        echo "physics.producers.CrvPhotonArrivals.scintillationYieldTolerance  : $scintTolerance" >> $fclfile
+        echo "physics.producers.CrvSiPMResponses.ThermalProb          : 0" >> $fclfile
+        echo "physics.producers.CrvSiPMResponses.backgroundParam1     : $backgroundParam1" >> $fclfile
+        echo "physics.producers.CrvSiPMResponses.backgroundParam3     : $backgroundParam3" >> $fclfile
+
+        mu2eart --setup=./setup.sh --fcl=$fclfile --njobs=50 --events-per-job=10000 --jobname=CRV_efficiency5cm10_top6600_moduleGap$moduleGap'_'layerOffset$layerOffset'_'photonYield$photonYield --outstage=/pnfs/mu2e/scratch/outstage
+
+      done
+#    done
+#  done
