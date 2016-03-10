d810e3e1ed3d2f3c105036b3931be54e59a8caac
David Nathan Brown <dave_brown@lbl.gov>
2016-03-09 17:58:11 -0800

    Update diagnostics

--- TrkPatRec/test/EPSeparation.C
+++ TrkPatRec/test/EPSeparation.C
@@ -19,10 +19,10 @@ struct EffStruct {
 
 void EPSeparation(TTree* sh, EffStruct& eff,const char* fname=0) {
   double escale(1.0);
-  TCut conv("mcgen==2");
-  TCut prot("mcgen==28&&mcpdg==2212");
-  TH1F* ee = new TH1F("ee","Energy Deposition;Straw energy (KeV)",100,0,20.0);
-  TH1F* pe = new TH1F("pe","Energy Deposition;Straw energy (KeV)",100,0,20.0);
+  TCut conv("mcproc==56&&mcpdg==11&&mcoe>100");
+  TCut prot("mcpdg==2212");
+  TH1F* ee = new TH1F("ee","Energy Deposition;Straw energy (KeV)",100,0,22.0);
+  TH1F* pe = new TH1F("pe","Energy Deposition;Straw energy (KeV)",100,0,22.0);
   ee->SetLineColor(kRed);
   pe->SetLineColor(kBlack);
   ee->SetStats(0);
@@ -32,39 +32,8 @@ void EPSeparation(TTree* sh, EffStruct& eff,const char* fname=0) {
   sh->Project("pe","1000.0*edep",prot);
   ee->Scale(escale);
 
-  TH1F* edist = new TH1F("edist","MC true DOCA; Distance to wire (mm)",100,0,2.51);
-  TH1F* pdist = new TH1F("pdist","MC true DOCA; Distance to wire (mm)",100,0,2.51);
-  TH1F* epath = new TH1F("epath","MC true gas path; gas path (mm)",100,0,5.01);
-  TH1F* ppath = new TH1F("ppath","MC true gas path; gas path (mm)",100,0,5.01);
-  edist->SetLineColor(kRed);
-  pdist->SetLineColor(kBlack);
-  epath->SetLineColor(kRed);
-  ppath->SetLineColor(kBlack);
-  epath->SetStats(0);
-  ppath->SetStats(0);
-//  edist->Sumw2();
-//  epath->Sumw2();
-  sh->Project("edist","mcshd",conv);
-  sh->Project("pdist","mcshd",prot);
-  sh->Project("epath","2*sqrt(2.5^2-mcshd^2)",conv);
-  sh->Project("ppath","2*sqrt(2.5^2-mcshd^2)",prot);
-  edist->Scale(escale);
-  epath->Scale(escale);
-  edist->SetStats(0);
-  pdist->SetStats(0);
-
-  TProfile *ed = new TProfile("ed","Average energy vs pathlength",100,0,5.01);
-  TProfile *pd = new TProfile("pd","Average energy vs pathlength",100,0,5.011);
-  ed->SetLineColor(kRed);
-  pd->SetLineColor(kBlack);
-  sh->Project("ed","edep:2*sqrt(2.5^2-mcshd^2)",conv);
-  sh->Project("pd","edep:2*sqrt(2.5^2-mcshd^2)",prot);
-  ed->Scale(escale);
-  ed->SetStats(0);
-  pd->SetStats(0);
-
-  TH1F* ededx = new TH1F("ededx","dE/dx;Straw energy/pathlength (KeV/mm)",100,0,5);
-  TH1F* pdedx = new TH1F("pdedx","dE/dx;Straw energy/pathlength (KeV/mm)",100,0,5);
+  TH1F* ededx = new TH1F("ededx","dE/dx;Straw energy/pathlength (KeV/mm)",100,0,6);
+  TH1F* pdedx = new TH1F("pdedx","dE/dx;Straw energy/pathlength (KeV/mm)",100,0,6);
   ededx->SetLineColor(kRed);
   pdedx->SetLineColor(kBlack);
 //  ededx->Sumw2();
@@ -111,8 +80,8 @@ void EPSeparation(TTree* sh, EffStruct& eff,const char* fname=0) {
   deleg->AddEntry(ededx,elab,"l");
   deleg->AddEntry(pdedx,plab,"l");
 
-  TCanvas* ecan = new TCanvas("ecan","Energy",1200,800);
-  ecan->Divide(3,2);
+  TCanvas* ecan = new TCanvas("ecan","Energy",800,400);
+  ecan->Divide(2,1);
   ecan->cd(1);
   pe->Draw();
   ee->Draw("same");
@@ -121,22 +90,25 @@ void EPSeparation(TTree* sh, EffStruct& eff,const char* fname=0) {
   lecut->SetLineStyle(3);
   lecut->Draw();
   ecan->cd(2);
-  pdist->Draw();
-  edist->Draw("same");
-  ecan->cd(3);
-  ppath->Draw();
-  epath->Draw("same");
-  ecan->cd(4);
-  pd->Draw();
-  ed->Draw("same");
-  ecan->cd(5);
   pdedx->Draw();
   ededx->Draw("same");
   deleg->Draw();
   TLine *ldecut = new TLine(eff._decut,0.0,eff._decut,pdedx->GetMaximum());
   ldecut->SetLineStyle(3);
   ldecut->Draw();
+  
+  TCanvas* e2can = new TCanvas("e2can","e2can",700,700);
+  pe->Draw();
+  ee->Draw("same");
+  eleg->Draw();
+  lecut->SetLineStyle(3);
+  lecut->Draw();
+
+  
   if(fname !=0)
-    ecan->SaveAs(fname);
+    e2can->SaveAs(fname);
+
+  
+
 //  delete ecan;
 }
