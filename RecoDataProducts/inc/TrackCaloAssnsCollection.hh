88f51310eaf86f40f1bfff55599861a17d1c8afd
Robert Bernstein <rhbob@fnal.gov>
2016-04-28 11:04:30 -0500

    putting things in right directories

---
+++ RecoDataProducts/inc/TrackCaloAssnsCollection.hh
@@ -0,0 +1,22 @@
+#ifndef TrackCaloAssns_TrackCaloAssnsCollection_hh
+#define TrackCaloAssns_TrackCaloAssnsCollection_hh
+
+//
+// Define a type for a collection of TrackCaloAssns objects.
+//
+// $Id: 
+// $Author: 
+// $Date: 
+//
+// Original author Vadim Rusu
+//
+
+#include <vector>
+
+#include "RecoDataProducts/inc/TrackCaloAssns.hh"
+
+namespace mu2e {
+   typedef std::vector<mu2e::TrackCaloMatchInfo> TrackCaloAssnsCollection;
+}
+
+#endif 
