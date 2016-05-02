1fcdf6c89067ee2aee4cff96f3e6e2847c1148cc
David Nathan Brown <dave_brown@lbl.gov>
2016-05-02 09:42:41 -0700

    Add script for generating dead straws

---
+++ TrackerMC/test/RandomDeadStraws.C
@@ -0,0 +1,93 @@
+#include "TRandom3.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+void RandomDeadStraws(unsigned ndeadplane, unsigned ndeadpanel, unsigned ndeadstraw, const char* outfile) {
+  unsigned NStraws(48);
+  unsigned NLayers(2);
+  unsigned NPlanes(40);
+  unsigned NPanels(6);
+  vector<string> deadstraws;
+  vector<unsigned> inactiveplanes;
+  vector<string> inactivepanels;
+  vector<string> inactivestraws;
+// 20-slot tracker with 18 stations:w
+  inactiveplanes.push_back(12);
+  inactiveplanes.push_back(13);
+  inactiveplanes.push_back(26);
+  inactiveplanes.push_back(27);
+  TRandom3 myrand(3499803);
+
+  filebuf fb;
+  fb.open (outfile,ios::out);
+  ostream os(&fb);
+  // write header
+  os << "physics.producers.makeSD.deadStrawList : {" << endl;
+
+  if(ndeadplane > 0){
+    os  << "deadPlane : [ " << endl;
+    unsigned ndplane(0); 
+    while(ndplane < ndeadplane){
+      int plane = myrand.Integer(NPlanes);
+      if(find(inactiveplanes.begin(), inactiveplanes.end(), plane) == inactiveplanes.end()){
+	++ndplane;
+	os  << plane;
+	if(ndplane < ndeadplane) os << "," << endl;
+	inactiveplanes.push_back(plane);
+      }
+    }
+    os  << "]" << endl;
+  }
+
+
+  if(ndeadpanel > 0){
+    os  << "deadPanels : [ " << endl;
+    unsigned ndpanel(0); 
+    while(ndpanel < ndeadpanel){
+      int plane = myrand.Integer(NPlanes);
+      if(find(inactiveplanes.begin(), inactiveplanes.end(), plane) == inactiveplanes.end()){
+	unsigned panel = myrand.Integer(NPanels);
+	char panelid[80];
+	snprintf(panelid,80,"\"%u_%u\"",plane,panel);
+	if(find(inactivepanels.begin(),inactivepanels.end(),panelid) == inactivepanels.end()){
+	  ++ndpanel;
+	  os  << panelid;
+	  if(ndpanel < ndeadpanel) os << "," << endl;
+	  inactivepanels.push_back(panelid);
+	}
+      }
+    }
+    os  << "]" << endl;
+  }
+
+  if(ndeadstraw > 0) {
+    os  << "deadStraws : [ " << endl;
+    unsigned ndstraw(0);
+    while (ndstraw < ndeadstraw){ 
+      int plane = myrand.Integer(NPlanes);
+      if(find(inactiveplanes.begin(), inactiveplanes.end(), plane) == inactiveplanes.end()){
+     	unsigned panel = myrand.Integer(NPanels);
+	char panelid[80];
+	snprintf(panelid,80,"\"%u_%u\"",plane,panel);
+	if(find(inactivepanels.begin(),inactivepanels.end(),panelid) == inactivepanels.end()){
+	  unsigned layer = myrand.Integer(NLayers);
+	  unsigned straw = myrand.Integer(NStraws);
+	  char strawid[80];
+	  snprintf(strawid,80,"\"%u_%u_%u_%u\"",plane,panel,layer,straw);
+	  if(find(deadstraws.begin(), deadstraws.end(), strawid) == deadstraws.end()){
+	    ++ndstraw;
+	    os << strawid;
+	    if(ndstraw < ndeadstraw) os << "," << endl;
+	    deadstraws.push_back(strawid);
+	  }
+	}
+      }
+    }
+    os  << "]" << endl;;
+  }
+}
+
