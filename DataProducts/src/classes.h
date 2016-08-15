8817a4e1196b99373a89789e46b5e6dd4f1fd3f0
Rob Kutschke <kutschke@fnal.gov>
2016-08-14 19:46:43 -0500

    Restore the 5 include files removed in the previous commit. I am not sure which are actually needed. Added note the the Infrastructure work doc to understand this and remove include files that can safely be removed.

--- DataProducts/src/classes.h
+++ DataProducts/src/classes.h
@@ -13,9 +13,15 @@
 #include "boost/array.hpp"
 #include <vector>
 
+#include "CLHEP/Vector/TwoVector.h"
+#include "CLHEP/Vector/ThreeVector.h"
 #include "CLHEP/Vector/Rotation.h"
 #include "CLHEP/Vector/RotationX.h"
 #include "CLHEP/Vector/RotationY.h"
 #include "CLHEP/Vector/RotationZ.h"
 #include "CLHEP/Vector/EulerAngles.h"
 #include <CLHEP/Geometry/Transform3D.h>
+#include "CLHEP/Matrix/Vector.h"
+#include "CLHEP/Matrix/Matrix.h"
+#include "CLHEP/Matrix/SymMatrix.h"
+
