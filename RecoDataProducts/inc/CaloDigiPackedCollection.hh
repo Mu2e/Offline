7271daa80053ae7c2e95005aeec09e33664e827f
echenard <echenard@hep.caltech.edu>
2016-08-30 18:35:55 -0700

    Added missing file

---
+++ RecoDataProducts/inc/CaloDigiPackedCollection.hh
@@ -0,0 +1,12 @@
+#ifndef DataProducts_CaloDigiPackedCollection_hh
+#define DataProducts_CaloDigiPackedCollection_hh
+
+#include <vector>
+#include "RecoDataProducts/inc/CaloDigiPacked.hh"
+
+namespace mu2e 
+{
+   typedef std::vector<mu2e::CaloDigiPacked> CaloDigiPackedCollection;
+}
+
+#endif 
