18217c9eda3f6efaf6c453d3091fdac6cef804d8
David Nathan Brown <dave_brown@lbl.gov>
2016-09-08 07:23:17 -0700

    Add the actual modules

---
+++ TrkPatRec/src/KalFinalFit_module.cc
@@ -0,0 +1,256 @@
+//
+// Final Kalman fit
+//
+// Original author D. Brown and G. Tassielli
+//
+
+// framework
+#include "art/Framework/Principal/Event.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Services/Optional/TFileService.h"
+// conditions
+#include "ConditionsService/inc/ConditionsHandle.hh"
+#include "GeometryService/inc/getTrackerOrThrow.hh"
+#include "TTrackerGeom/inc/TTracker.hh"
+#include "GeometryService/inc/GeometryService.hh"
+// utiliites
+#include "GeneralUtilities/inc/Angles.hh"
+#include "TrkReco/inc/TrkUtilities.hh"
+// data
+#include "DataProducts/inc/Helicity.hh"
+#include "RecoDataProducts/inc/StrawHitCollection.hh"
+#include "RecoDataProducts/inc/StrawHitFlagCollection.hh"
+#include "RecoDataProducts/inc/KalSeedCollection.hh"
+// BaBar
+#include "BTrk/BaBar/BaBar.hh"
+#include "BTrk/TrkBase/TrkPoca.hh"
+// Mu2e BaBar
+#include "BTrkData/inc/TrkStrawHit.hh"
+#include "TrkReco/inc/KalFit.hh"
+#include "RecoDataProducts/inc/KalRepCollection.hh"
+#include "RecoDataProducts/inc/KalRepPtrCollection.hh"
+#include "RecoDataProducts/inc/KalRepPayloadCollection.hh"
+#include "TrkPatRec/inc/PayloadSaver.hh"
+//CLHEP
+#include "CLHEP/Units/PhysicalConstants.h"
+#include "CLHEP/Matrix/Vector.h"
+// root 
+#include "TH1F.h"
+#include "TTree.h"
+// C++
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <functional>
+#include <float.h>
+#include <vector>
+using namespace std; 
+using CLHEP::Hep3Vector;
+using CLHEP::HepVector;
+
+namespace mu2e 
+{
+  class KalFinalFit : public art::EDProducer
+  {
+    public:
+      explicit KalFinalFit(fhicl::ParameterSet const&);
+      virtual ~KalFinalFit();
+      virtual void produce(art::Event& event ); 
+    private:
+      unsigned _iev;
+      // configuration parameters
+      int _debug;
+      int _printfreq;
+      bool _addhits; 
+      art::Handle<mu2e::StrawHitCollection> _strawhitsH;
+      art::Handle<KalSeedCollection> _trksSeedH;
+      // event object tags
+      art::InputTag _shTag;
+      art::InputTag _shfTag;
+      art::InputTag _ksTag; 
+      // flags
+      StrawHitFlag _addsel;
+      StrawHitFlag _addbkg;
+      TrkFitFlag _goodseed;
+      double _maxdtmiss;
+      // outlier cuts
+      double _maxadddoca, _maxaddchi;
+      TrkParticle _tpart; // particle type being searched for
+      TrkFitDirection _fdir;  // fit direction in search
+      // event objects
+      const StrawHitCollection* _shcol;
+      const StrawHitFlagCollection* _shfcol;
+      const KalSeedCollection * _kscol;
+      // Kalman fitter
+      KalFit _kfit;
+      PayloadSaver _payloadSaver;
+      // helper functions
+      bool findData(const art::Event& e);
+      void findMissingHits(KalRep* krep, vector<hitIndex>& indices);
+
+      // flow diagnostic
+  };
+
+  KalFinalFit::KalFinalFit(fhicl::ParameterSet const& pset) :
+    _debug(pset.get<int>("debugLevel",0)),
+    _printfreq(pset.get<int>("printFrequency",101)),
+    _addhits(pset.get<bool>("addhits",true)),
+    _shTag(pset.get<art::InputTag>("StrawHitCollectionTag","makeSH")),
+    _shfTag(pset.get<art::InputTag>("StrawHitFlagCollectionTag","FlagBkgHits")),
+    _ksTag(pset.get<art::InputTag>("SeedCollectionTag","KalSeedFit")),
+    _addsel(pset.get<vector<string> >("AddHitSelectionBits",vector<string>{} )),
+    _addbkg(pset.get<vector<string> >("AddHitBackgroundBits",vector<string>{})),
+    _goodseed(pset.get<vector<string> >("GoodKalSeedFitBits",vector<string>{})),
+    _maxdtmiss(pset.get<double>("DtMaxMiss",40.0)),
+    _maxadddoca(pset.get<double>("MaxAddDoca",2.75)),
+    _maxaddchi(pset.get<double>("MaxAddChi",4.0)),
+    _tpart((TrkParticle::type)(pset.get<int>("fitparticle",TrkParticle::e_minus))),
+    _fdir((TrkFitDirection::FitDirection)(pset.get<int>("fitdirection",TrkFitDirection::downstream))),
+    _kfit(pset.get<fhicl::ParameterSet>("KalFit",fhicl::ParameterSet())),
+    _payloadSaver(pset)
+  {
+    produces<KalRepCollection>();
+    produces<KalRepPtrCollection>();
+    produces<KalRepPayloadCollection>();
+    produces<StrawHitFlagCollection>();
+    produces<KalSeedCollection>();
+  }
+
+  KalFinalFit::~KalFinalFit(){}
+
+  void KalFinalFit::produce(art::Event& event ) {
+    // event printout
+    _iev=event.id().event();
+    if(_debug > 0 && (_iev%_printfreq)==0)cout<<"KalFinalFit: event="<<_iev<<endl;
+    // find the data
+    if(!findData(event)){
+      throw cet::exception("RECO")<<"mu2e::KalFinalFit: data missing or incomplete"<< endl;
+    }
+    // create output
+    unique_ptr<KalRepCollection>    krcol(new KalRepCollection );
+    unique_ptr<KalRepPtrCollection> krPtrcol(new KalRepPtrCollection );
+    unique_ptr<KalSeedCollection> kscol(new KalSeedCollection());
+    // copy in the existing flags
+    unique_ptr<StrawHitFlagCollection> shfcol(new StrawHitFlagCollection(*_shfcol));
+    // lookup productID for payload saver
+    art::ProductID kalRepsID(getProductID<KalRepCollection>(event));
+    // loop over the seed fits
+    unsigned iseed(0);
+    for (auto kseed : *_kscol) {
+    // only process fits which meet the requirements
+      if(kseed.status().hasAllProperties(_goodseed)) {
+	// check the seed has the same basic parameters as this module expects
+	if(kseed.particle() != _tpart || kseed.fitDirection() != _fdir ) {
+	  throw cet::exception("RECO")<<"mu2e::KalFinalFit: wrong particle or direction"<< endl;
+	}
+	// seed should have at least segment
+	if(kseed.segments().size() < 1){
+	  throw cet::exception("RECO")<<"mu2e::KalFinalFit: no segments"<< endl;
+	}
+	// build a Kalman rep around this seed
+	KalRep *krep(0);
+	_kfit.makeTrack(_shcol,kseed,krep);
+	// if successfull, try to add missing hits
+	if(_addhits && krep != 0 && krep->fitStatus().success()){
+	    // first, add back the hits on this track
+	  _kfit.unweedHits(krep,_maxaddchi);
+	  vector<hitIndex> misshits;
+	  findMissingHits(krep,misshits);
+	  if(misshits.size() > 0){
+	    _kfit.addHits(krep,_shcol,misshits,_maxaddchi);
+	  }
+	}
+	// put successful fits into the event
+	if(krep != 0 && krep->fitStatus().success()){
+	  // warning about 'fit current': this is not an error
+	  if(!krep->fitCurrent()){
+	    cout << "Fit not current! " << endl;
+	  }
+	  // save successful kalman fits in the event
+	  krcol->push_back(krep);
+	  int index = krcol->size()-1;
+	  krPtrcol->emplace_back(kalRepsID, index, event.productGetter(kalRepsID));
+	  // convert successful fits into 'seeds' for persistence
+	  // FIXME!
+	  // flag the hits used in this track. 
+	  if(iseed<StrawHitFlag::_maxTrkId){
+	    for(auto ihit=krep->hitVector().begin();ihit != krep->hitVector().end();++ihit){
+	      if((*ihit)->isActive())shfcol->at(static_cast<TrkStrawHit*>(*ihit)->index()).merge(StrawHitFlag::trackBit(iseed));
+	    }
+	  }
+	} else // fit failure
+	  delete krep;
+      }
+      ++iseed;
+    }
+    // put the output products into the event
+    art::ProductID krcolID(getProductID<KalRepPayloadCollection>(event));
+    _payloadSaver.put(*krcol, krcolID, event);
+    event.put(move(krcol));
+    event.put(move(krPtrcol));
+    event.put(move(kscol));
+    event.put(move(shfcol));
+  }
+
+  // find the input data objects 
+  bool KalFinalFit::findData(const art::Event& evt){
+    _shcol = 0;
+    _shfcol = 0;
+    _kscol = 0;
+
+    auto shH = evt.getValidHandle<StrawHitCollection>(_shTag);
+    _shcol = shH.product();
+    auto shfH = evt.getValidHandle<StrawHitFlagCollection>(_shfTag);
+    _shfcol = shfH.product();
+    auto ksH = evt.getValidHandle<KalSeedCollection>(_ksTag);
+    _kscol = ksH.product();
+
+    return _shcol != 0 && _shfcol != 0 && _kscol != 0;
+  }
+
+  void KalFinalFit::findMissingHits(KalRep* krep,vector<hitIndex>& misshits) {
+    const Tracker& tracker = getTrackerOrThrow();
+    //  Trajectory info
+    Hep3Vector tdir;
+    HepPoint tpos;
+    krep->pieceTraj().getInfo(krep->flt0(),tpos,tdir);
+    unsigned nstrs = _shcol->size();
+    TrkStrawHitVector tshv;
+    convert(krep->hitVector(),tshv);
+    for(unsigned istr=0; istr<nstrs;++istr){
+      if(_shfcol->at(istr).hasAllProperties(_addsel)&& !_shfcol->at(istr).hasAnyProperty(_addbkg)){
+	StrawHit const& sh = _shcol->at(istr);
+	if(fabs(_shcol->at(istr).time()-krep->t0()._t0) < _maxdtmiss) {
+	  // make sure we haven't already used this hit
+	  vector<TrkStrawHit*>::iterator ifnd = find_if(tshv.begin(),tshv.end(),FindTrkStrawHit(sh));
+	  if(ifnd == tshv.end()){
+	    // good in-time hit.  Compute DOCA of the wire to the trajectory
+	    Straw const& straw = tracker.getStraw(sh.strawIndex());
+	    CLHEP::Hep3Vector hpos = straw.getMidPoint();
+	    CLHEP::Hep3Vector hdir = straw.getDirection();
+	    // convert to HepPoint to satisfy antique BaBar interface: FIXME!!!
+	    HepPoint spt(hpos.x(),hpos.y(),hpos.z());
+	    TrkLineTraj htraj(spt,hdir,-straw.getHalfLength(),straw.getHalfLength());
+	    // estimate flightlength along track.  This assumes a constant BField!!!
+	    double fltlen = (hpos.z()-tpos.z())/tdir.z();
+	    // estimate hit length
+	    HepPoint tp = krep->pieceTraj().position(fltlen);
+	    Hep3Vector tpos(tp.x(),tp.y(),tp.z()); // ugly conversion FIXME!
+	    double hitlen = hdir.dot(tpos - hpos);
+	    TrkPoca hitpoca(krep->pieceTraj(),fltlen,htraj,hitlen);
+	    // flag hits with small residuals
+	    if(fabs(hitpoca.doca()) < _maxadddoca){
+	      misshits.push_back(istr);
+	    }
+	  }
+	}
+      }
+    }
+  }
+
+}// mu2e
+using mu2e::KalFinalFit;
+DEFINE_ART_MODULE(KalFinalFit);
---
+++ TrkPatRec/src/KalSeedFit_module.cc
@@ -0,0 +1,246 @@
+//
+// Starting with a Helix fit, perform a Least-squares fit (using the BTrk
+// Kalman fit, appropriately configured) to produce an initial estimate of the parmeters and
+// covariance for the final Kalman fit.  This fit uses wire positions only,
+// not drift.
+//
+// Original author Dave Brown (LBNL) 31 Aug 2016
+//
+
+// framework
+#include "art/Framework/Principal/Event.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Core/EDProducer.h"
+#include "GeometryService/inc/DetectorSystem.hh"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Services/Optional/TFileService.h"
+// conditions
+#include "ConditionsService/inc/ConditionsHandle.hh"
+#include "GeometryService/inc/getTrackerOrThrow.hh"
+#include "TTrackerGeom/inc/TTracker.hh"
+#include "GeometryService/inc/GeometryService.hh"
+#include "GeometryService/inc/GeomHandle.hh"
+#include "BFieldGeom/inc/BFieldManager.hh"
+#include "GeometryService/inc/DetectorSystem.hh"
+// utiliites
+#include "GeneralUtilities/inc/Angles.hh"
+#include "TrkReco/inc/TrkUtilities.hh"
+// data
+#include "DataProducts/inc/Helicity.hh"
+#include "RecoDataProducts/inc/StrawHitCollection.hh"
+#include "RecoDataProducts/inc/StrawHitFlagCollection.hh"
+#include "RecoDataProducts/inc/HelixSeedCollection.hh"
+#include "RecoDataProducts/inc/KalSeedCollection.hh"
+// BaBar
+#include "BTrk/BbrGeom/BbrVectorErr.hh"
+#include "BTrk/TrkBase/TrkPoca.hh"
+// Mu2e BaBar
+#include "BTrkData/inc/TrkStrawHit.hh"
+#include "TrkReco/inc/KalFit.hh"
+#include "RecoDataProducts/inc/KalRepCollection.hh"
+#include "RecoDataProducts/inc/KalRepPtrCollection.hh"
+#include "RecoDataProducts/inc/KalRepPayloadCollection.hh"
+#include "TrkPatRec/inc/PayloadSaver.hh"
+//CLHEP
+#include "CLHEP/Units/PhysicalConstants.h"
+#include "CLHEP/Matrix/Vector.h"
+// root 
+#include "TH1F.h"
+#include "TTree.h"
+// C++
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <functional>
+#include <float.h>
+#include <vector>
+using namespace std; 
+using CLHEP::Hep3Vector;
+using CLHEP::HepVector;
+
+namespace mu2e 
+{
+  class KalSeedFit : public art::EDProducer
+  {
+    public:
+      explicit KalSeedFit(fhicl::ParameterSet const&);
+      virtual ~KalSeedFit();
+      virtual void beginRun(art::Run&);
+      virtual void produce(art::Event& event ); 
+    private:
+      unsigned _iev;
+      // configuration parameters
+      int _debug;
+      int _printfreq;
+      // event object tags
+      art::InputTag _shTag;
+      art::InputTag _shfTag;
+      art::InputTag _hsTag;
+      unsigned _minnhits; // minimum # of hits
+      double _maxdoca;      // outlier cut
+      TrkParticle _tpart; // particle type being searched for
+      TrkFitDirection _fdir;  // fit direction in search
+      Helicity _helicity; // cached value of helicity expected for this fit
+      double _amsign; // cached sign of angular momentum WRT the z axis 
+      // cache of event objects
+      const StrawHitCollection* _shcol;
+      const StrawHitFlagCollection* _shfcol;
+      const HelixSeedCollection * _hscol;
+      // ouptut collections
+      // Kalman fitter.  This will be configured for a least-squares fit (no material or BField corrections).
+      KalFit _seedfit;
+      // helper functions
+      bool findData(const art::Event& e);
+      void filterOutliers(TrkDef& trkdef);
+  };
+
+  KalSeedFit::KalSeedFit(fhicl::ParameterSet const& pset) :
+    _debug(pset.get<int>("debugLevel",0)),
+    _printfreq(pset.get<int>("printFrequency",101)),
+    _shTag(pset.get<art::InputTag>("StrawHitCollectionTag","makeSH")),
+    _shfTag(pset.get<art::InputTag>("StrawHitFlagCollectionTag","FlagBkgHits")),
+    _hsTag(pset.get<art::InputTag>("SeedCollectionTag","RobustHelixFinder")),
+    _minnhits(pset.get<unsigned>("MinNHits",10)),
+    _maxdoca(pset.get<double>("MaxDoca",40.0)),
+    _tpart((TrkParticle::type)(pset.get<int>("fitparticle",TrkParticle::e_minus))),
+    _fdir((TrkFitDirection::FitDirection)(pset.get<int>("fitdirection",TrkFitDirection::downstream))),
+    _seedfit(pset.get<fhicl::ParameterSet>("SeedFit",fhicl::ParameterSet()))
+  {
+    produces<KalSeedCollection>();
+  }
+
+  KalSeedFit::~KalSeedFit(){}
+
+  void KalSeedFit::beginRun(art::Run& ){
+ // calculate the helicity
+    GeomHandle<BFieldManager> bfmgr;
+    GeomHandle<DetectorSystem> det;
+    // change coordinates to mu2e
+    CLHEP::Hep3Vector vpoint(0.0,0.0,0.0);
+    CLHEP::Hep3Vector vpoint_mu2e = det->toMu2e(vpoint);
+    CLHEP::Hep3Vector field = bfmgr->getBField(vpoint_mu2e);
+    // helicity is a purely geometric quantity, however it's easiest
+    // to determine it from the kinematics (angular momentum and Z momentum)
+    _amsign = copysign(1.0,-_tpart.charge()*field.z());
+    _helicity = Helicity(static_cast<float>(_fdir.dzdt()*_amsign));
+  }
+
+  void KalSeedFit::produce(art::Event& event ) {
+    // create output collection
+    unique_ptr<KalSeedCollection> seedfits(new KalSeedCollection());
+    // event printout
+    _iev=event.id().event();
+    if(_debug > 0 && (_iev%_printfreq)==0)cout<<"KalSeedFit: event="<<_iev<<endl;
+    // find the data
+    if(!findData(event)){
+      throw cet::exception("RECO")<<"mu2e::KalSeedFit: data missing or incomplete"<< endl;
+    }
+
+    // loop over the Helices
+    for (size_t iseed=0; iseed<_hscol->size(); ++iseed) {
+    // convert the HelixSeed to a TrkDef
+      HelixSeed const& hseed(_hscol->at(iseed));
+      HelixTraj hstraj(TrkParams(HelixTraj::NHLXPRM));
+      // verify the helicity.  This could be wrong due to FP effects, so don't treat it as an exception
+      if(_helicity == hseed._helix.helicity() &&
+      // convert the helix to a fit trajectory.  This accounts for the physical particle direction
+	  TrkUtilities::RobustHelix2Traj(hseed._helix,hstraj,_amsign)){
+	if(_debug > 1) cout << "Using HelixTraj with parameters " << hstraj.parameters()->parameter() << endl;
+  // create the track definition
+	TrkDef seeddef(hseed._timeCluster,hstraj,_tpart,_fdir);
+  // filter outliers; this doesn't use drift information, just straw positions
+  // This function should move to the helix fit FIXME!
+	filterOutliers(seeddef);
+      // now, fit the seed helix from the filtered hits
+	KalRep *seedrep(0);
+	_seedfit.makeTrack(_shcol,seeddef,seedrep);
+	if(seedrep != 0 && seedrep->fitStatus().success()){
+	// convert the status into a FitFlag
+	  TrkFitFlag fitstat(TrkFitFlag::fitOK);
+	  // create a KalSeed object from this fit, recording the particle and fit direction
+	  KalSeed kseed(_tpart,_fdir,seedrep->t0(),seedrep->flt0(),fitstat);
+	  // extract the hits from the rep and put the hitseeds into the KalSeed
+	  TrkUtilities::fillHitSeeds(seedrep,kseed._hits);
+	  if(kseed._hits.size() >= _minnhits)kseed._status.merge(TrkFitFlag::hitsOK);
+	  // extract the helix trajectory from the fit (there is just 1)
+	  double locflt;
+	  const HelixTraj* htraj = dynamic_cast<const HelixTraj*>(seedrep->localTrajectory(seedrep->flt0(),locflt));
+	  // use this to create segment.  This will be the only segment in this track
+	  if(htraj != 0){
+	    KalSegment kseg;
+	    // sample the momentum at this point
+	    BbrVectorErr momerr = seedrep->momentumErr(seedrep->flt0());
+	    TrkUtilities::fillSegment(*htraj,momerr,kseg);
+	    kseed._segments.push_back(kseg);
+	    // push this seed into the collection
+	    seedfits->push_back(kseed); 
+	  } else {
+	    throw cet::exception("RECO")<<"mu2e::KalSeedFit: Can't extract helix traj from seed fit" << endl;
+	  }
+	}
+      // cleanup the seed fit KalRep.  Optimally the seedrep should be a data member of this module
+      // and get reused to avoid thrashing memory, but the BTrk code doesn't support that, FIXME!
+	delete seedrep;
+      } else {
+	throw cet::exception("RECO")<<"mu2e::KalSeedFit: HelixSeed with wrong helicity found ! " << endl;
+      }
+    }
+    // put the tracks into the event
+    event.put(move(seedfits));
+  }
+
+
+  // find the input data objects 
+  bool KalSeedFit::findData(const art::Event& evt){
+    _shcol = 0;
+    _shfcol = 0;
+    _hscol = 0;
+
+    auto shH = evt.getValidHandle<StrawHitCollection>(_shTag);
+    _shcol = shH.product();
+    auto shfH = evt.getValidHandle<StrawHitFlagCollection>(_shfTag);
+    _shfcol = shfH.product();
+    auto hsH = evt.getValidHandle<HelixSeedCollection>(_hsTag);
+    _hscol = hsH.product();
+
+    return _shcol != 0 && _shfcol != 0 && _hscol != 0;
+  }
+
+  void KalSeedFit::filterOutliers(TrkDef& mydef){
+  // for now filter on DOCA.  In future this shoudl be an MVA using time and position FIXME!
+    //  Trajectory info
+    Hep3Vector tdir;
+    HepPoint tposp;
+    double flt0 = mydef.helix().zFlight(0.0);
+    mydef.helix().getInfo(flt0,tposp,tdir);
+    // tracker and conditions
+    const Tracker& tracker = getTrackerOrThrow();
+    const vector<hitIndex>& indices = mydef.strawHitIndices();
+    vector<hitIndex> goodhits;
+    for(unsigned ihit=0;ihit<indices.size();++ihit){
+      StrawHit const& sh = _shcol->at(indices[ihit]);
+      Straw const& straw = tracker.getStraw(sh.strawIndex());
+      CLHEP::Hep3Vector hpos = straw.getMidPoint();
+      CLHEP::Hep3Vector hdir = straw.getDirection();
+      // convert to HepPoint to satisfy antique BaBar interface: FIXME!!!
+      HepPoint spt(hpos.x(),hpos.y(),hpos.z());
+      TrkLineTraj htraj(spt,hdir,-straw.getHalfLength(),straw.getHalfLength());
+      // estimate flightlength along track.  This assumes a constant BField!!!
+      double fltlen = (hpos.z()-tposp.z())/tdir.z();
+      HepPoint tp = mydef.helix().position(fltlen);
+      Hep3Vector tpos(tp.x(),tp.y(),tp.z()); // ugly conversion FIXME!
+      double hitlen = hdir.dot(tpos - hpos);
+      TrkPoca hitpoca(mydef.helix(),fltlen,htraj,hitlen);
+      // keep hits with small residuals
+      if(fabs(hitpoca.doca()) < _maxdoca){
+        goodhits.push_back(indices[ihit]);
+      }
+    }
+    // update track
+    mydef.strawHitIndices() = goodhits;
+  }
+
+}// mu2e
+using mu2e::KalSeedFit;
+DEFINE_ART_MODULE(KalSeedFit);
