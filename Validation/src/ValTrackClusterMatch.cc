7f330d2c549984d042645f719782d595428f51c0
Ray Culbertson <rlc@fnal.gov>
2017-01-06 15:05:38 -0600

    make du and dv symmetric

--- Validation/src/ValTrackClusterMatch.cc
+++ Validation/src/ValTrackClusterMatch.cc
@@ -10,8 +10,8 @@ int mu2e::ValTrackClusterMatch::declare(art::TFileDirectory tfs) {
   _hVer = tfs.make<TH1D>( "Ver", "Version Number", 101, -0.5, 100.0);
 
   _hNMatch = tfs.make<TH1D>( "NMatch", "N Matches", 11, -0.5, 10.5);
-  _hdu = tfs.make<TH1D>( "du", "du", 100, 0., 1200.);
-  _hdv = tfs.make<TH1D>( "dv", "dv", 100, 0., 1200.);
+  _hdu = tfs.make<TH1D>( "du", "du", 100, -1000., 1000.);
+  _hdv = tfs.make<TH1D>( "dv", "dv", 100, -1000., 1000.);
   _hep = tfs.make<TH1D>( "ep", "E/p", 100, 0., 1.6);
   _hchi2 = tfs.make<TH1D>( "Chi2", "Chi2", 100, 0.0, 10.0);
   _hchi2t = tfs.make<TH1D>( "Chi2t", "Chi2 time", 100, 0.0, 10.0);
@@ -26,7 +26,7 @@ int mu2e::ValTrackClusterMatch::fill(
 
   // increment this by 1 any time the defnitions of the histograms or the 
   // histogram contents change, and will not match previous versions
-  _hVer->Fill(0.0);
+  _hVer->Fill(1.0);
 
   _hNMatch->Fill(coll.size());
   for(auto match : coll) {
