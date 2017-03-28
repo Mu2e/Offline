5df15b4bf8cde6b3928ff2b4101f336697f711f4
Rob Kutschke <kutschke@fnal.gov>
2017-03-28 18:45:19 -0500

    Switch to art v2_06_02 and BTrk v1_01_08a for use with geant4 v4_10_2_p02.

--- setup.sh
+++ setup.sh
@@ -68,10 +68,7 @@ if [[ $($MU2E_BASE_RELEASE/buildopts --g4vis) == qt ]]; then
 fi
 
 # Setup the framework and its dependent products
-setup -B art v2_06_02a -q${MU2E_UPS_QUALIFIERS}
-
-# root6 needs a path to include files to prevent some runtime warnings
-export ROOT_INCLUDE_PATH=`dropit -s -p$ROOT_INCLUDE_PATH $MU2E_BASE_RELEASE`
+setup -B art v2_06_02 -q${MU2E_UPS_QUALIFIERS}
 
 # root6 needs a path to include files to prevent some runtime warnings
 export ROOT_INCLUDE_PATH=`dropit -s -p$ROOT_INCLUDE_PATH $MU2E_BASE_RELEASE`
@@ -91,8 +88,9 @@ if [[ $($MU2E_BASE_RELEASE/buildopts --trigger) == "on" ]]; then
 fi
 
 # Other libraries we need.
-setup -B heppdt v3_04_01f -q${MU2E_UPS_QUALIFIERS}
-setup -B BTrk   v1_01_08  -q${MU2E_UPS_QUALIFIERS}
+setup -B heppdt   v3_04_01f -q${MU2E_UPS_QUALIFIERS}
+setup -B BTrk     v1_01_08a -q${MU2E_UPS_QUALIFIERS}
+setup -B xerces_c v3_1_4a   -q${MU2E_UPS_QUALIFIERS}
 
 # The build system.
 setup -B scons v2_5_1 -q p2713b
