3bc1d0f9996882379262a1d16df3eb61015e5f07
David Nathan Brown <dave_brown@lbl.gov>
2017-04-14 13:40:15 -0700

    Working diagnostics for module timing

---
+++ TrkFilters/test/PlotTimes.C
@@ -0,0 +1,48 @@
+#include "TTree.h"
+#include "TH1F.h"
+#include "TCanvas.h"
+#include "TSystem.h"
+//#include <sys/types.h>
+//#include <dirent.h>
+//#include <libgen.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+void PlotTime(const char* dirname, const char* name, TH1F* timeplot) {
+  TTree* rdtree = new TTree(name,name);
+  string fullname = std::string(dirname) + std::string(name)+std::string(".csv");
+  rdtree->ReadFile(fullname.c_str(),"evt/I:time/F:mod/C",',');
+  rdtree->Project(timeplot->GetName(),"time");
+}
+
+
+void PlotAllTimes(const char* dirname, double maxtime) {
+  // find all the csv files in this directory
+  static const string csv(".csv");
+  vector<TH1F*> plots;
+//  auto dir`p = opendir(dirname);
+  void* dirp = gSystem->OpenDirectory(dirname);
+  if(dirp != 0){
+    const char* dent(0);
+    while ( (dent = gSystem->GetDirEntry(dirp)) != 0){
+      string fname(dent);
+      // compare the suffix
+      if(fname.size() >= csv.size() && 0 == fname.compare (fname.length() - csv.length(), csv.length(), csv)){
+	cout << "found entry " << fname << endl;
+	string name = fname.substr(0,fname.find_last_of("."));
+	string title = fname.substr(0,fname.find_last_of(".")) + std::string(" Execution Time;Time (sec)");
+	TH1F* plot = new TH1F(name.c_str(),title.c_str(),500,0.0,maxtime);
+	PlotTime(dirname,name.c_str(),plot);
+	plots.push_back(plot);
+      }
+    }
+    TCanvas* can = new TCanvas("tcan","times",1200,1200);
+    int ncel = (int)ceil(sqrt(plots.size()));
+    can->Divide(ncel,ncel);
+    for(unsigned iplot=0;iplot<plots.size();++iplot){
+      can->cd(iplot+1);
+      plots[iplot]->Draw();
+    }
+  }
+}
