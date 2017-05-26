9b22db85e498411c3782e59a200d9501910af8d0
Andrew Edmonds <aedmonds@lbl.gov>
2017-05-26 14:25:27 -0500

    Adding FilterKalSeed module, KalSeedToTrkQualTree module, IndexMap data product and example FCL files

---
+++ DataProducts/inc/IndexMap.hh
@@ -0,0 +1,56 @@
+#ifndef IndexMap_hh
+#define IndexMap_hh
+
+//
+// Maps an index from a full collection onto the index in a condensed collection
+//
+// Original author Andrew Edmonds
+
+#include <ostream>
+#include <map>
+
+namespace mu2e {
+
+  typedef size_t FullIndex;
+  typedef size_t CondensedIndex;
+
+  class IndexMap{
+    
+  public:
+    IndexMap() { };
+
+    void addElement(FullIndex full, CondensedIndex condensed) {
+      std::pair<FullIndex, CondensedIndex> newPair(full, condensed);
+      _theMap.insert(newPair);
+    }
+
+    mu2e::CondensedIndex getCondensedIndex(const FullIndex& full) const {
+      return _theMap.at(full);
+    }
+
+    // Print the information found in this hit.
+    void print( std::ostream& ost, bool doEndl ) const {
+
+      ost << "IndexMap: " << std::endl;
+      for (const auto& element : _theMap) {
+	ost << element.first << " --> " << element.second << std::endl;
+      }      
+      if ( doEndl ){
+	ost << std::endl;
+      }
+    }
+
+  private:
+    std::map<FullIndex, CondensedIndex> _theMap; // FullIndex is first, CondensedIndex is second
+  };
+
+
+
+  inline std::ostream& operator<<( std::ostream& ost,
+				     IndexMap const& map){
+    map.print(ost,false);
+    return ost;
+  }
+
+}
+#endif /* IndexMap_hh */
---
+++ TrkDiag/src/KalSeedToTrkQualTree_module.cc
@@ -0,0 +1,262 @@
+////////////////////////////////////////////////////////////////////////
+// Class:       KalSeedToTrkQualTree
+// Plugin Type: analyzer (art v2_06_02)
+// File:        KalSeedToTrkQualTree_module.cc
+//
+// Generated at Fri Apr 21 14:56:02 2017 by Andrew Edmonds using cetskelgen
+// from cetlib version v2_02_00.
+////////////////////////////////////////////////////////////////////////
+
+#include "art/Framework/Core/EDAnalyzer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Principal/Run.h"
+#include "art/Framework/Principal/SubRun.h"
+#include "canvas/Utilities/InputTag.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+#include "TTree.h"
+
+#include "art/Framework/Services/Optional/TFileService.h"
+
+#include "BTrk/TrkBase/TrkHelixUtils.hh"
+
+#include "DataProducts/inc/IndexMap.hh"
+
+#include "MCDataProducts/inc/StrawDigiMCCollection.hh"
+#include "MCDataProducts/inc/StepPointMCCollection.hh"
+#include "MCDataProducts/inc/EventWeight.hh"
+
+#include "RecoDataProducts/inc/StrawDigiCollection.hh"
+#include "RecoDataProducts/inc/StrawHitCollection.hh"
+#include "RecoDataProducts/inc/TrkQual.hh"
+#include "RecoDataProducts/inc/KalSeed.hh"
+#include "RecoDataProducts/inc/TrkStrawHitSeed.hh"
+
+#include "TrkDiag/inc/helixpar.hh"
+#include "TrkDiag/inc/TrkMCTools.hh"
+
+#include "BTrk/BbrGeom/HepPoint.h"
+#include "GlobalConstantsService/inc/GlobalConstantsHandle.hh"
+#include "GlobalConstantsService/inc/ParticleDataTable.hh"
+#include "GeometryService/inc/GeomHandle.hh"
+#include "GeometryService/inc/DetectorSystem.hh"
+#include "BFieldGeom/inc/BFieldManager.hh"
+
+#include "CLHEP/Units/PhysicalConstants.h"
+
+namespace mu2e {
+  class KalSeedToTrkQualTree;
+}
+
+
+class mu2e::KalSeedToTrkQualTree : public art::EDAnalyzer {
+public:
+  explicit KalSeedToTrkQualTree(fhicl::ParameterSet const & p);
+  // The compiler-generated destructor is fine for non-base
+  // classes without bare pointers or other resource use.
+
+  // Plugins should not be copied or assigned.
+  KalSeedToTrkQualTree(KalSeedToTrkQualTree const &) = delete;
+  KalSeedToTrkQualTree(KalSeedToTrkQualTree &&) = delete;
+  KalSeedToTrkQualTree & operator = (KalSeedToTrkQualTree const &) = delete;
+  KalSeedToTrkQualTree & operator = (KalSeedToTrkQualTree &&) = delete;
+
+  // Required functions.
+  void analyze(art::Event const & e) override;
+
+private:
+
+  // art input tags for collections
+  art::InputTag _kalFinalTag;
+  art::InputTag _strawDigiMCTag;
+  art::InputTag _stepPointMCTag;
+  art::InputTag _trkQualTag;
+  art::InputTag _beamWeightTag;
+  art::InputTag _genWeightTag;
+
+  // For the TTree
+  TTree* _trkQualTree; // the tree we will fill
+
+  // event weight info
+  double _eventWeight; // total event weight (product of weights below)
+  double _beamWeight; // proton beam intensity weight
+  double _genWeight; // weight from the generator (e.g. DIO spectrum)
+
+  // variables needed for cutting but not used in the TrkQualMVA itself
+  TrkFitFlag _fitStatus; // the status of the fit
+  double _fitMom; // the fit momentum at the front of the tracker
+  double _mcMom; // true momentum at the front of the tracker
+  double _t0; // t0 of the track
+  double _tandip; // tandip of the track
+
+  // the current TrkQual information
+  TrkQual _trkQual; // the current TrkQual MVAStruct
+  double _trkQualValue;
+
+  // variables that we will add will go in here
+  //
+  //
+  //
+
+
+  // For condensed collections
+  bool _usingCondensedCollections; // true if we are using condensed collection and so should expect an IndexMap
+  art::InputTag _indexMapTag;
+  const mu2e::IndexMap* _indexMap;
+
+
+  // Functions that we use to fill variables
+  void fillEventInfo(const art::Event& event) {
+    _genWeight = _beamWeight = _eventWeight = 1;
+
+    art::Handle<EventWeight> beamWeightHandle;
+    event.getByLabel(_beamWeightTag, beamWeightHandle);
+    if(beamWeightHandle.isValid()){
+      _beamWeight = beamWeightHandle->weight();
+      _eventWeight *= _beamWeight;
+    }
+
+    art::Handle<EventWeight> genWeightHandle;
+    event.getByLabel(_genWeightTag, genWeightHandle);
+    if(genWeightHandle.isValid()){
+      _genWeight = genWeightHandle->weight();
+      _eventWeight *= _genWeight;
+    }
+  }
+
+  void fillKalSeedInfo(const KalSeed& kal_seed) {
+    _fitStatus = kal_seed.status();
+    _t0 = kal_seed.t0().t0();
+  }
+
+  void fillKalSegmentInfo(const KalSegment& kal_segment) {
+    _fitMom = kal_segment.mom();
+  }
+
+  void fillStepPointMCInfo(const StepPointMC& step_point) {
+    GeomHandle<BFieldManager> bfmgr;
+    GeomHandle<DetectorSystem> det;
+    GlobalConstantsHandle<ParticleDataTable> pdt;
+
+    _mcMom = step_point.momentum().mag();
+
+    double charge = pdt->particle(step_point.simParticle()->pdgId()).ref().charge();
+    double hflt(0.0);
+    CLHEP::HepVector parvec(5,0);
+    static CLHEP::Hep3Vector vpoint_mu2e = det->toMu2e(CLHEP::Hep3Vector(0.0,0.0,0.0));
+    static double bz = bfmgr->getBField(vpoint_mu2e).z();
+    CLHEP::Hep3Vector pos(step_point.position());
+    HepPoint ppos(pos.x(),pos.y(),pos.z());
+    TrkHelixUtils::helixFromMom( parvec, hflt,ppos, step_point.momentum(),charge,bz);
+    helixpar hpar = helixpar(parvec);
+    _tandip = hpar._td;
+  }
+
+  void fillTrkQualInfo(const TrkQual& trk_qual) {
+    _trkQual = trk_qual;
+    _trkQualValue = trk_qual.MVAOutput();
+  }
+};
+
+
+mu2e::KalSeedToTrkQualTree::KalSeedToTrkQualTree(fhicl::ParameterSet const & pset)
+  :
+  EDAnalyzer(pset),
+  _kalFinalTag(pset.get<art::InputTag>("kalFinalTag")),
+  _strawDigiMCTag(pset.get<art::InputTag>("strawDigiMCTag")),
+  _stepPointMCTag(pset.get<art::InputTag>("stepPointMCTag")),
+  _trkQualTag(pset.get<art::InputTag>("trkQualTag")),
+  _beamWeightTag(pset.get<art::InputTag>("beamWeightTag", "")),
+  _genWeightTag(pset.get<art::InputTag>("genWeightTag", "")),
+  _usingCondensedCollections(pset.get<bool>("usingCondensedCollections", "false")),
+  _indexMapTag(pset.get<art::InputTag>("indexMapTag", ""))
+ // More initializers here.
+{
+  art::ServiceHandle<art::TFileService> tfs;
+  _trkQualTree=tfs->make<TTree>("trkQualTree","Variables for Testing and Training TrkQual");
+
+  _trkQualTree->Branch("EventWeight", &_eventWeight);
+  _trkQualTree->Branch("BeamWeight", &_beamWeight);
+  _trkQualTree->Branch("GenWeight", &_genWeight);
+  _trkQualTree->Branch("FitStatus", &_fitStatus);
+  _trkQualTree->Branch("FitMom", &_fitMom);
+  _trkQualTree->Branch("MCMom", &_mcMom);
+  _trkQualTree->Branch("T0", &_t0);
+  _trkQualTree->Branch("TanDip", &_tandip);
+
+  int n_trkqual_vars = TrkQual::n_vars;
+  for (int i_trkqual_var = 0; i_trkqual_var < n_trkqual_vars; ++i_trkqual_var) {
+    TrkQual::MVA_varindex i_index =TrkQual::MVA_varindex(i_trkqual_var);
+    std::string varname = TrkQual::varName(i_index);
+    _trkQualTree->Branch(varname.c_str(), &_trkQual[i_index]);
+  }
+  //  _trkQualTree->Branch("trkQual", &_trkQual);
+  _trkQualTree->Branch("OriginalTrkQualVal", &_trkQualValue);
+
+  // new variables will go here
+}
+
+void mu2e::KalSeedToTrkQualTree::analyze(art::Event const & event)
+{
+  // The collections we'll need
+  const auto& finalKals = event.getValidHandle<KalSeedCollection>(_kalFinalTag);
+  const auto& strawDigiMCs = event.getValidHandle<StrawDigiMCCollection>(_strawDigiMCTag);
+  const auto& vdStepPointMCs = event.getValidHandle<StepPointMCCollection>(_stepPointMCTag);
+
+  if (_usingCondensedCollections) {
+    _indexMap = event.getValidHandle<IndexMap>(_indexMapTag);
+  }
+
+  fillEventInfo(event);
+
+  // Loop through the final KalSeed fits
+  for (const auto& finalKal : *finalKals) {
+
+    fillKalSeedInfo(finalKal);
+
+    // Get the segment of the fit at the front of the tracker
+    KalSegment const& frontSegment = finalKal.segments().front(); 
+    fillKalSegmentInfo(frontSegment);
+
+    // Find the SimParticle that this track corresponds to
+    const auto& hits = finalKal.hits();
+    art::Ptr<mu2e::SimParticle> primary; // will be filled
+
+    std::vector<StrawHitIndex> hit_indices;
+    for ( const auto& trk_straw_hit_seed : hits) {
+      // might want the condensed hit indices
+      StrawHitIndex index = trk_straw_hit_seed.index();
+      if (_usingCondensedCollections) {
+	index = _indexMap->getCondensedIndex(index);
+      }
+
+      hit_indices.push_back(index);
+    }
+    TrkMCTools::primaryParticle(primary, hit_indices, strawDigiMCs);
+
+    // Find the StepPointMC at the front of the tracker
+    const auto& trackid = primary->id();
+    for ( const auto& i_step_point : *vdStepPointMCs) {
+      if ( i_step_point.volumeId() == VirtualDetectorId::TT_FrontHollow 
+	   && i_step_point.trackId() == trackid) {
+	
+	fillStepPointMCInfo(i_step_point);
+	break;
+      }
+    }
+  }
+
+  // Get the TrkQual information that was used originally
+  const auto& trkQuals =  event.getValidHandle<TrkQualCollection>(_trkQualTag);
+  for (const auto& trkQual : *trkQuals) {
+    fillTrkQualInfo(trkQual);
+  }
+
+  // Fill the tree
+  _trkQualTree->Fill();
+}
+
+DEFINE_ART_MODULE(mu2e::KalSeedToTrkQualTree)
