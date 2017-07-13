4c48f9db0b0a01000bb4842456d130555bdf9b5d
Rob Kutschke <kutschke@fnal.gov>
2017-07-13 15:54:22 -0500

    Add missing header.

--- TrkReco/inc/AmbigResolver.hh
+++ TrkReco/inc/AmbigResolver.hh
@@ -5,25 +5,26 @@
 // Original author: David Brown (LBNL), 2012
 //
 // $Id: AmbigResolver.hh,v 1.4 2014/08/01 18:56:10 gandr Exp $
-// $Author: gandr $ 
+// $Author: gandr $
 // $Date: 2014/08/01 18:56:10 $
 //
 #ifndef AmbigResolver_HH
 #define AmbigResolver_HH
 #include "BTrk/BaBar/BaBar.hh"
 
+#include <vector>
+
 class KalRep;
 class TrkSimpTraj;
 namespace mu2e {
   class TrkStrawHit;
-
   class AmbigResolver {
     public:
     explicit AmbigResolver(double extErr);
     virtual ~AmbigResolver() = 0;
 
 // resolve a track.  Depending on the configuration, this might
-// update the hit state and the t0 value. 
+// update the hit state and the t0 value.
 
     virtual bool resolveTrk(KalRep* kfit) const = 0;
 
