8db6cd9d56808e05d300100ca06852c02528e3fc
David No. Brown <david.brown@louisville.edu>
2017-09-29 13:29:35 -0500

    Misc updates to geometry

---
+++ Mu2eG4/inc/nestPolyhedra.hh
@@ -0,0 +1,63 @@
+#ifndef Mu2eG4_nestPolyhedra_hh
+#define Mu2eG4_nestPolyhedra_hh
+//
+// Free function to create and place a new G4Polyhedra, place inside a logical volume.
+//
+// $Id: nestPolyhedra.hh,v 1.3 2014/09/19 19:14:55 knoepfel Exp $
+// $Author: knoepfel $
+// $Date: 2014/09/19 19:14:55 $
+//
+// Original author Rob Kutschke
+//
+
+#include <string>
+#include <vector>
+
+#include "G4Helper/inc/VolumeInfo.hh"
+
+class G4CSGSolid;
+class G4LogicalVolume;
+class G4Material;
+class G4VPhysicalVolume;
+
+// G4 includes
+#include "G4Polyhedra.hh"
+#include "G4Colour.hh"
+#include "G4RotationMatrix.hh"
+#include "G4ThreeVector.hh"
+
+
+namespace mu2e {
+
+  class Polyhedra;
+  class PolyhedraParams;
+
+  VolumeInfo nestPolyhedra ( std::string const& name,
+                            PolyhedraParams const & polyObj,
+                            G4Material* material,
+                            G4RotationMatrix const* rot,
+                            G4ThreeVector const & offset,
+                            VolumeInfo const & parent,
+                            int copyNo,
+                            bool const isVisible,
+                            G4Colour const color,
+                            bool const forceSolid,
+                            bool const forceAuxEdgeVisible,
+                            bool const placePV,
+                            bool const doSurfaceCheck
+                            );
+
+  VolumeInfo nestPolyhedra ( std::string const& name,
+                            PolyhedraParams const & polyObj,
+                            G4Material* material,
+                            G4RotationMatrix const* rot,
+                            G4ThreeVector const & offset,
+                            VolumeInfo const & parent,
+                            int copyNo,
+                            G4Colour const color,
+			    std::string const& lookupToken
+                            );
+
+}
+
+#endif /* Mu2eG4_nestPolyhedra_hh */
