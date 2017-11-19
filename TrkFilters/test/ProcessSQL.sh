ee1b2ac4111fa079f723e8388fff9cceaac40e65
David Nathan Brown <dave_brown@lbl.gov>
2017-11-19 15:44:01 -0600

    update script

--- TrkFilters/test/ProcessSQL.sh
+++ TrkFilters/test/ProcessSQL.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for mod in "makeSH" "FSHPreStereo" "MakeStrawHitPositions" "MakeStereoHits" "FlagStrawHits" "FlagBkgHits" "TimeClusterFinder" "TCFilter" "PosHelixFinder" "PosHelixFilter" "NegHelixFinder" "NegHelixFilter" "KSFDeM" "DeMSeedFilter" "KSFUeMi" "UeMSeedFilter"; do
+for mod in "makeSH" "MakeStereoHits" "FlagBkgHits" "TimeClusterFinder" "TCFilter"  "HelixFinder" "PosHelixFilter" "NegHelixFilter" "KSFDeM" "DeMSeedFilter" "KSFUeMi" "UeMSeedFilter"; do
 echo Processing module $mod
 sqlite3  -separator "," $1 "SELECT Event, Time, ModuleLabel FROM TimeModule WHERE ModuleLabel=\"${mod}\";" > $mod.csv
 done
