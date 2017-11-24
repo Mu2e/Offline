79e47e08a252dd2db7df5c7b370be2b1ec79f3c8
David Nathan Brown <dave_brown@lbl.gov>
2017-11-24 07:44:36 -0800

    update diagnostics

--- TrkFilters/test/PlotTimes.C
+++ TrkFilters/test/PlotTimes.C
@@ -7,13 +7,14 @@
 //#include <libgen.h>
 #include <string>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 void PlotTime(const char* dirname, const char* name, TH1F* timeplot) {
   TTree* rdtree = new TTree(name,name);
-  string fullname = std::string(dirname) + std::string(name)+std::string(".csv");
+  string fullname = std::string(dirname) + std::string(name);
   rdtree->ReadFile(fullname.c_str(),"evt/I:time/F:mod/C",',');
-  rdtree->Project(timeplot->GetName(),"time");
+  rdtree->Project(timeplot->GetName(),"1000.0*time");
 }
 
 
@@ -23,6 +24,7 @@ void PlotAllTimes(const char* dirname, double maxtime) {
   vector<TH1F*> plots;
 //  auto dir`p = opendir(dirname);
   void* dirp = gSystem->OpenDirectory(dirname);
+  std::vector<string> fnames;
   if(dirp != 0){
     const char* dent(0);
     while ( (dent = gSystem->GetDirEntry(dirp)) != 0){
@@ -30,12 +32,16 @@ void PlotAllTimes(const char* dirname, double maxtime) {
       // compare the suffix
       if(fname.size() >= csv.size() && 0 == fname.compare (fname.length() - csv.length(), csv.length(), csv)){
 	cout << "found entry " << fname << endl;
-	string name = fname.substr(0,fname.find_last_of("."));
-	string title = fname.substr(0,fname.find_last_of(".")) + std::string(" Execution Time;Time (sec)");
-	TH1F* plot = new TH1F(name.c_str(),title.c_str(),500,0.0,maxtime);
-	PlotTime(dirname,name.c_str(),plot);
-	plots.push_back(plot);
+	fnames.push_back(fname);
       }
+    }
+    std::sort(fnames.begin(),fnames.end());
+    for(auto const& fname : fnames ){
+      string name = fname.substr(1,fname.find_last_of(".")-1);
+      string title = fname.substr(1,fname.find_last_of(".")-1) + std::string(" Execution Time;Time (msec)");
+      TH1F* plot = new TH1F(name.c_str(),title.c_str(),500,0.0,maxtime);
+      PlotTime(dirname,fname.c_str(),plot);
+      plots.push_back(plot);
     }
     TCanvas* acan = new TCanvas("alltimes","times",1200,1200);
     int ncel = (int)ceil(sqrt(plots.size()));
@@ -43,13 +49,13 @@ void PlotAllTimes(const char* dirname, double maxtime) {
     for(unsigned iplot=0;iplot<plots.size();++iplot){
       acan->cd(iplot+1);
       plots[iplot]->Draw();
-      TCanvas* can = new TCanvas(plots[iplot]->GetName(),plots[iplot]->GetName(),400,400);
-      can->Divide(1,1);
-      can->cd(1);
-      plots[iplot]->Draw();
-      char rname[100];
-      snprintf(rname,100,"%s%s",plots[iplot]->GetName(),".root");
-      can->SaveAs(rname);
+//      TCanvas* can = new TCanvas(plots[iplot]->GetName(),plots[iplot]->GetName(),400,400);
+//      can->Divide(1,1);
+//      can->cd(1);
+//      plots[iplot]->Draw();
+//      char rname[100];
+//      snprintf(rname,100,"%s%s",plots[iplot]->GetName(),".root");
+//      can->SaveAs(rname);
     }
   }
 }
