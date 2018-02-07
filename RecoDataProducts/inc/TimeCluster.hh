c9cee32eab9ee1453088a12008064f88c1114b34
David Nathan Brown <dave_brown@lbl.gov>
2018-02-06 21:54:29 -0800

    Continue XYZVec migration

--- RecoDataProducts/inc/TimeCluster.hh
+++ RecoDataProducts/inc/TimeCluster.hh
@@ -16,7 +16,7 @@
 #include "canvas/Persistency/Common/Ptr.h"
 // Mu2e includes
 #include "RecoDataProducts/inc/StrawHitIndex.hh"
-#include "CLHEP/Vector/ThreeVector.h"
+#include "RecoDataProducts/inc/XYZVec.hh"
 // BTrk includes
 #include "BTrk/TrkBase/TrkT0.hh"
 // c++
@@ -28,13 +28,13 @@ namespace mu2e {
     int                               nhits      () const { return _strawHitIdxs.size(); }
     const std::vector<StrawHitIndex>& hits       () const { return _strawHitIdxs; }
     const TrkT0&                      t0         () const { return _t0; }
-    const CLHEP::Hep3Vector&          position   () const { return _pos; }
+    const XYZVec&          position   () const { return _pos; }
     const art::Ptr<CaloCluster>&      caloCluster() const { return _caloCluster; }
     int                               caloFastIdx() const { return _caloFastIdx; }
 
     std::vector<StrawHitIndex> _strawHitIdxs; // associated straw hits: can be empty
     TrkT0		       _t0;           // t0 and associated error
-    CLHEP::Hep3Vector          _pos;          // position of the time cluster   
+    XYZVec          _pos;          // position of the time cluster   
     art::Ptr<CaloCluster>      _caloCluster;  // associated calorimeter cluster: can be null
     int                        _caloFastIdx;
   };
