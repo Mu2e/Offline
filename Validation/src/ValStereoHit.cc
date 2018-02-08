c7f2e20119aa4f766557096604eea1423582ca32
David Nathan Brown <dave_brown@lbl.gov>
2018-02-07 22:17:03 -0800

    Replace StereoHit with ComboHit

--- Validation/src/ValStereoHit.cc
+++ Validation/src/ValStereoHit.cc
@@ -9,7 +9,6 @@ int mu2e::ValStereoHit::declare(art::TFileDirectory tfs) {
   _ht = tfs.make<TH1D>( "t", "time", 100, 0.0, 2000.0);
   _hE = tfs.make<TH1D>( "E", "Energy",50, 0.0, 0.02);
   _hchi2 = tfs.make<TH1D>( "chi2", "Chi2",50, 0.0, 50.0);
-  _hmva = tfs.make<TH1D>( "mva", "MVA",50, 0.0, 1.00);
 
   return 0;
 }
@@ -24,11 +23,10 @@ int mu2e::ValStereoHit::fill(const mu2e::StereoHitCollection & coll,
   _hN->Fill(coll.size()); 
   _hN2->Fill(coll.size()); 
   for(auto sh : coll) {
-    _pos.fill(sh.pos());
+    _pos.fill(sh.posCLHEP());
     _ht->Fill(sh.time());
-    _hE->Fill(sh.energy());
-    _hchi2->Fill(sh.chisq());
-    _hmva->Fill(sh.mvaout());
+    _hE->Fill(sh.energyDep());
+    _hchi2->Fill(sh.qual());
   }
   return 0;
 }
