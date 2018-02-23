d29013b288e71c47a9acc1b242bdba9fbbacea07
David Nathan Brown <dave_brown@lbl.gov>
2018-02-22 21:51:55 -0600

    Update scripts

--- TrkFilters/test/ProcessSQL.sh
+++ TrkFilters/test/ProcessSQL.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 FCOUNT=0
-for mod in "TTmakeSH" "TTmakePH" "TTflagBkgHits" "TTtimeClusterFinder"  "TThelixFinder" "TTKSFDeM" ; do
+for mod in "RSD" "TTmakeSH" "TTmakePH" "TTflagBkgHits" "TTtimeClusterFinder"  "TThelixFinder" "TTKSFDeM" ; do
 echo Processing module $mod
 FCOUNT=`expr $FCOUNT + 1`
 sqlite3  -separator "," $1 "SELECT Event, Time, ModuleLabel FROM TimeModule WHERE ModuleLabel=\"${mod}\";" > $FCOUNT$mod.csv
