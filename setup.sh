c44a2c8e7a6d52a1fed28a18b1c89c2b55cb883b
Krzysztof Genser <genser@fnal.gov>
2018-02-27 17:06:23 -0600

    Removed explicit setup of xerces_c again

--- setup.sh
+++ setup.sh
@@ -78,7 +78,6 @@ setup -B mu2e_artdaq_core v1_02_01e -q${MU2E_UPS_QUALIFIERS}:+${MU2E_ART_SQUALIF
 # Other libraries we need.
 
 setup -B heppdt   v3_04_01g -q${MU2E_UPS_QUALIFIERS}
-setup -B xerces_c v3_1_4b   -q${MU2E_UPS_QUALIFIERS}
 setup -B BTrk   v1_02_11  -q${MU2E_UPS_QUALIFIERS}
 setup -B cry   v1_7i  -q${MU2E_UPS_QUALIFIERS}
 # setup -B gsl v2_4  -q${build}
