1cf7ad51bd36b82096b30b4009b6b63856d3424c
Andrei Gaponenko <gandr@fnal.gov>
2018-07-05 14:40:38 -0500

    PBI code: separate the analyzer from the producer, add fhicl validation.  Just a "flat" producer at the moment.

---
+++ EventGenerator/src/ProtonBunchIntensityFlat_module.cc
@@ -0,0 +1,73 @@
+// Module to simulate the microbunch-to-microbunch intensity fluctuations of the
+// protons hitting the primary target.  This module does not describe the time
+// structure of the proton bunches, which is assumed to be independent of the
+// intensity.  The data product of this module is used by downstream simulation
+// modules, in particular, all generators which start from stopped muons need to
+// use the same (coherent) intensity fluctuations for the rates of signal and
+// background processes to be consistent.
+//
+// Original author: David Brown (LBNL) 19 May 2015
+// Revamp: Andrei Gaponenko, 2018
+
+#include "fhiclcpp/types/Atom.h"
+#include "fhiclcpp/types/Table.h"
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+
+#include "SeedService/inc/SeedService.hh"
+#include "MCDataProducts/inc/ProtonBunchIntensity.hh"
+#include "Mu2eUtilities/inc/artURBG.hh"
+
+#include <random>
+
+namespace mu2e {
+
+  class ProtonBunchIntensityFlat : public art::EDProducer {
+  public:
+
+    struct Config {
+      using Name=fhicl::Name;
+      using Comment=fhicl::Comment;
+      fhicl::Atom<double> mean{Name("mean"), Comment("Mean number of protons per microbunch") };
+      fhicl::Atom<double> halfWidth{Name("halfWidth"), Comment("Fractional half width of the flat distribution, 0 <= halfWidth <= 1.")};
+    };
+
+    typedef art::EDProducer::Table<Config> Parameters;
+
+    explicit ProtonBunchIntensityFlat(const Parameters& conf);
+    void produce(art::Event& evt) override;
+
+  private:
+    artURBG urbg_;
+    std::uniform_real_distribution<double> flat_;
+    double mean_;
+  };
+
+  ProtonBunchIntensityFlat::ProtonBunchIntensityFlat(const Parameters& conf)
+    : urbg_(createEngine(art::ServiceHandle<SeedService>()->getSeed()))
+    , flat_(-conf().halfWidth(), +conf().halfWidth())
+    , mean_(conf().mean())
+  {
+    produces<mu2e::ProtonBunchIntensity>();
+
+    if(conf().halfWidth() < 0.) {
+      throw cet::exception("BADCONFIG")<<"ProtonBunchIntensityFlat: illegal halfWidth = "
+                                       <<conf().halfWidth()<<" < 0.\n";
+    }
+
+    if(1. < conf().halfWidth()) {
+      throw cet::exception("BADCONFIG")<<"ProtonBunchIntensityFlat: illegal halfWidth = "
+                                       <<conf().halfWidth()<<" > 1.\n";
+    }
+  }
+
+  void ProtonBunchIntensityFlat::produce(art::Event& event) {
+    double res = mean_ * (1. + flat_(urbg_));
+
+    // convert to nearest ingeger and write out
+    event.put(std::make_unique<ProtonBunchIntensity>(unsigned(rint(res)), mean_));
+  }
+}
+
+DEFINE_ART_MODULE(mu2e::ProtonBunchIntensityFlat);
