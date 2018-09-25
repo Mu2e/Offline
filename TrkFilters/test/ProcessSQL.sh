4f00ddf5eb75ea237c0d0e95bcfc92a0112c57f0
gianipez <gianipez@fnal.gov>
2018-09-25 12:53:09 -0500

    udpates

--- TrkFilters/test/ProcessSQL.sh
+++ TrkFilters/test/ProcessSQL.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 FCOUNT=0
-for mod in "RSD" "TTmakeSH" "TTmakePH" "TTflagBkgHits" "TTtimeClusterFinder"  "TThelixFinder" "TTKSFDeM" "TTKSFDeP" ; do
+#for mod in "RSD" "TTmakeSH" "TTmakePH" "TTflagBkgHits" "TTtimeClusterFinder"  "TThelixFinder" "TTKSFDeM" "TTKSFDeP" "CalHelixFinderDem" "CalHelixFinderDep" "CalSeedFitDem" "CalSeedFitDep"; do
+for mod in "RSD" "makeSH" "makePH" "FlagBkgHits" "TimeClusterFinder"  "HelixFinder" "KSFDeM" "KSFDeP" "CaloCluserFast" "CalTimePeakFinder" "CalHelixFinderDem" "CalHelixFinderDep" "CalSeedFitDem" "CalSeedFitDep"; do
 echo Processing module $mod
 FCOUNT=`expr $FCOUNT + 1`
 sqlite3  -separator "," $1 "SELECT Event, Time, ModuleLabel FROM TimeModule WHERE ModuleLabel=\"${mod}\";" > $FCOUNT$mod.csv
