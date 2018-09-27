34d5cbc91ec5188e3104de6f2e667286be9eb15f
David Nathan Brown <dave_brown@lbl.gov>
2018-09-27 12:01:10 -0500

    minor fix

--- JobConfig/examples/generate_flatmugamma-calo.sh
+++ JobConfig/examples/generate_flatmugamma-calo.sh
@@ -1,3 +1,3 @@
-generate_fcl --description flatmugamma-calo --dsconf MDC2018a --dsowner mu2e --include JobConfig/primary/flatmugamma-calo.fcl --run-number 1002 --events-per-job 500000 --njobs 10
+generate_fcl --description flatmugamma-calo --dsconf MDC2018a --dsowner mu2e --include JobConfig/primary/flatmugamma-calo.fcl --run-number 1002 --events-per-job 250000 --njobs 400
 rm -rf flatmugamma-calo
 mv 000 flatmugamma-calo
