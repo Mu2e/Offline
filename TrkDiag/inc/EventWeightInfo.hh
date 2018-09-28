60319a918327d8ef2a60371c61aeb3c46b5c75fa
Andrew Edmonds <aedmonds@lbl.gov>
2018-09-28 15:04:22 -0500

    Adding event weight branch to TrkAna that will get all EventWeight objects and create leaves for each one

---
+++ TrkDiag/inc/EventWeightInfo.hh
@@ -0,0 +1,39 @@
+//
+// struct to record event weight information
+// Andy Edmonds, September 2019
+//
+#ifndef EventWeightInfo_HH
+#define EventWeightInfo_HH
+#include "Rtypes.h"
+#include <string>
+namespace mu2e
+{
+  struct EventWeightInfo {
+    static const int MAX_WEIGHTS = 50;
+    const std::string leafnames(std::vector<std::string> labels) {
+      std::string leaves = "";
+      for (std::vector<std::string>::const_iterator i_label = labels.begin(); i_label != labels.end(); ++i_label) {
+	leaves += *i_label + "/F";
+	if (i_label != labels.end()-1) {
+	  leaves += ":";
+	}
+      }
+      return leaves;
+    }
+
+    void setWeights(const std::vector<Float_t>& weights) { 
+      for (unsigned int i_weight = 0; i_weight < weights.size(); ++i_weight) {
+	_weights[i_weight] = weights.at(i_weight);
+      }
+    }
+
+    void reset() {
+      for (auto& i_weight : _weights) {
+	i_weight = -1.0;
+      }
+    }
+
+    Float_t _weights[MAX_WEIGHTS];
+  };
+}
+#endif
