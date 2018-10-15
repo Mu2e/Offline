de79a5a74edb3ed2100dc14f2b7a9908b142e3ef
Eleonora Diociaiuti <ediociat@mu2egpvm04.fnal.gov>
2018-10-15 17:30:38 -0500

    Added a new constructor to be used with the Radiatively corrected generator

--- EventGenerator/src/StoppedParticleReactionGun_module.cc
+++ EventGenerator/src/StoppedParticleReactionGun_module.cc
@@ -15,7 +15,7 @@
 #include "CLHEP/Units/PhysicalConstants.h"
 
 #include "art/Framework/Core/EDProducer.h"
-#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Core/ModuleMacros.h"  
 #include "art/Framework/Principal/Event.h"
 #include "art/Framework/Principal/Run.h"
 #include "art/Framework/Principal/Handle.h"
@@ -31,12 +31,20 @@
 #include "MCDataProducts/inc/GenParticleCollection.hh"
 #include "Mu2eUtilities/inc/RandomUnitSphere.hh"
 #include "Mu2eUtilities/inc/CzarneckiSpectrum.hh"
+#include "Mu2eUtilities/inc/ConversionSpectrum.hh"
 #include "Mu2eUtilities/inc/SimpleSpectrum.hh"
 #include "Mu2eUtilities/inc/EjectedProtonSpectrum.hh"
 #include "Mu2eUtilities/inc/BinnedSpectrum.hh"
 #include "Mu2eUtilities/inc/Table.hh"
 #include "Mu2eUtilities/inc/RootTreeSampler.hh"
 #include "GeneralUtilities/inc/RSNTIO.hh"
+
+#include "TTree.h"
+#include "TFile.h"
+#include "TH1F.h"
+#include "TH2F.h"  
+#include "TMath.h"
+
 
 namespace mu2e {
 
@@ -53,26 +61,34 @@ namespace mu2e {
 
     double elow_; // BinnedSpectrum does not store emin and emax reliably
     double ehi_;
+    double eMax_;
     BinnedSpectrum spectrum_;
-    static BinnedSpectrum parseSpectrumShape(const fhicl::ParameterSet& psphys,
-                                             PDGCode::type pdgId,
-                                             double *elow,
-                                             double *ehi);
 
+    void parseSpectrumShape(BinnedSpectrum&            spectrum,
+			    const fhicl::ParameterSet& psphys,
+			    PDGCode::type              pdgId,
+			    double                     *elow,
+			    double                     *ehi,
+			    double                     *eMax);
     GenId genId_;
-
-    int verbosityLevel_;
+    int   verbosityLevel_;
 
     art::RandomNumberGenerator::base_engine_t& eng_;
-    CLHEP::RandGeneral randSpectrum_;
-    RandomUnitSphere randomUnitSphere_;
+    CLHEP::RandGeneral*  randSpectrum_;
+    RandomUnitSphere     randomUnitSphere_;
+    CLHEP::RandFlat      randomFlat_;
 
     RootTreeSampler<IO::StoppedParticleF> stops_;
 
     double generateEnergy();
 
+
+    bool   doHistograms_;
+
+    TH1F* _hEnergyPositron;
   public:
     explicit StoppedParticleReactionGun(const fhicl::ParameterSet& pset);
+    ~StoppedParticleReactionGun();
     virtual void produce(art::Event& event);
   };
 
@@ -84,13 +100,15 @@ namespace mu2e {
     , spectrumVariable_(parseSpectrumVar(psphys_.get<std::string>("spectrumVariable")))
     , elow_()
     , ehi_()
-    , spectrum_(parseSpectrumShape(psphys_, pdgId_, &elow_, &ehi_))
+    , eMax_()
+      //    , spectrum_(parseSpectrumShape(psphys_, pdgId_, &elow_, &ehi_))
     , genId_(GenId::findByName(psphys_.get<std::string>("genId", "StoppedParticleReactionGun")))
     , verbosityLevel_(pset.get<int>("verbosityLevel", 0))
     , eng_(createEngine(art::ServiceHandle<SeedService>()->getSeed()))
-    , randSpectrum_(eng_, spectrum_.getPDF(), spectrum_.getNbins())
     , randomUnitSphere_(eng_)
+    , randomFlat_      (eng_)
     , stops_(eng_, pset.get<fhicl::ParameterSet>("muonStops"))
+    , doHistograms_       (pset.get<bool>("doHistograms",true ) )
   {
     produces<mu2e::GenParticleCollection>();
 
@@ -99,6 +117,9 @@ namespace mu2e {
                                        <<psphys_.get<std::string>("genId", "StoppedParticleReactionGun")
                                        <<"\n";
     }
+
+    parseSpectrumShape(spectrum_,psphys_, pdgId_, &elow_, &ehi_,&eMax_);
+    randSpectrum_ = new CLHEP::RandGeneral(eng_, spectrum_.getPDF(), spectrum_.getNbins());
 
     if(verbosityLevel_ > 0) {
       std::cout<<"StoppedParticleReactionGun: using = "
@@ -116,16 +137,28 @@ namespace mu2e {
                <<std::endl;
 
       std::cout <<"StoppedParticleReactionGun: spectrum shape = "
-          <<psphys_.get<std::string>("spectrumShape") << std::endl;
+		<<psphys_.get<std::string>("spectrumShape") << std::endl;
       if (psphys_.get<std::string>("spectrumShape")  == "tabulated")
-          std::cout << " Spectrum file = "
-          << psphys_.get<std::string>("spectrumFileName")
-          << std::endl;
+	std::cout << " Spectrum file = "
+		  << psphys_.get<std::string>("spectrumFileName")
+		  << std::endl;
     }
     if(verbosityLevel_ > 1){
       std::cout <<"StoppedParticleReactionGun: spectrum: " << std::endl;
       spectrum_.print();
     }
+
+    if ( doHistograms_ ) {
+      art::ServiceHandle<art::TFileService> tfs;
+      art::TFileDirectory tfdir = tfs->mkdir( "StoppedParticleReactionGun" );
+
+      _hEnergyPositron = tfdir.make<TH1F>("hEnergyPositron", "Energy of the positron from #mu", 1200,  0.02,  120.02  );
+    }
+  }
+
+
+  StoppedParticleReactionGun::~StoppedParticleReactionGun() {
+    delete randSpectrum_;
   }
 
   //================================================================
@@ -138,45 +171,64 @@ namespace mu2e {
   }
 
   //================================================================
-  BinnedSpectrum
-  StoppedParticleReactionGun::parseSpectrumShape(const fhicl::ParameterSet& psphys,
-                                                 PDGCode::type pdgId,
-                                                 double *elow,
-                                                 double *ehi)
+  void StoppedParticleReactionGun::parseSpectrumShape(BinnedSpectrum&            Spectrum,
+						      const fhicl::ParameterSet& psphys,
+						      PDGCode::type              pdgId,
+						      double                     *elow,
+						      double                     *ehi,
+						      double                     *eMax )
   {
-    BinnedSpectrum res;
-
     const std::string spectrumShape(psphys.get<std::string>("spectrumShape"));
     if (spectrumShape == "Czarnecki") {
       const double mass(GlobalConstantsHandle<ParticleDataTable>()->particle(pdgId).ref().mass().value());
       *elow = psphys.get<double>("elow", mass);
       *ehi = GlobalConstantsHandle<PhysicsParams>()->getEndpointEnergy();
-      res.initialize< CzarneckiSpectrum >(*elow, *ehi, psphys.get<double>("spectrumResolution"));
+      // *eMax = psphys.get<double>("eMax");
+      Spectrum.initialize< CzarneckiSpectrum >(*elow, *ehi, psphys.get<double>("spectrumResolution"));
     }
     else if (spectrumShape == "flat") {
       *elow = psphys.get<double>("elow");
       *ehi = psphys.get<double>("ehi");
-      res.initialize<SimpleSpectrum>(*elow, *ehi, *ehi-*elow, SimpleSpectrum::Spectrum::Flat );
+      // *eMax = psphys.get<double>("eMax");
+      Spectrum.initialize<SimpleSpectrum>( *elow, *ehi, *ehi-*elow, SimpleSpectrum::Spectrum::Flat );
     }
     else if (spectrumShape == "CeEndpoint") {
       // A simple kludge: ignore the random distribution by setting elow=ehi=eConversion
-      res.initialize<SimpleSpectrum>(0., 1., 1., SimpleSpectrum::Spectrum::Flat );
-      *elow = *ehi = GlobalConstantsHandle<PhysicsParams>()->getEndpointEnergy();
+      *elow = *ehi = 92.32;//GlobalConstantsHandle<PhysicsParams>()->getEndpointEnergy();
+      //   *eMax = psphys.get<double>("eMax");
+      Spectrum.initialize<SimpleSpectrum>( 0., 1., 1., SimpleSpectrum::Spectrum::Flat );
+
+
+    }
+    else if (spectrumShape == "ConversionSpectrum") {
+      std::cout<<"using the right spectrum"<<std::endl;
+       *elow = psphys.get<double>("elow");
+       *ehi  = psphys.get<double>("ehi" );
+       *eMax = psphys.get<double>("eMax");
+      // for radiatively corrected spectrum, elow and ehi are derivatives 
+      double bin = psphys.get<double>("spectrumResolution");
+      /// looks crazy, but it will work
+      Spectrum.initialize<ConversionSpectrum>(*eMax,*elow,*ehi,bin,*eMax,bin);
     }
     else if (spectrumShape == "ejectedProtons") {
       *elow = 0.;
       *ehi = 105.; // cut off at muon mass
-      double spectrumRes = (*ehi - *elow)/psphys.get<unsigned>("nbins");
-      res.initialize<EjectedProtonSpectrum>(*elow, *ehi, spectrumRes);
+      *eMax = psphys.get<double>("eMax");
+
+      int nbins  = psphys.get<unsigned>("nbins");
+      double bin = (*ehi - *elow)/nbins;
+      Spectrum.initialize<EjectedProtonSpectrum>(*elow, *ehi, bin);
     }
     else if (spectrumShape == "tabulated") {
       // assume that tabulated are the bin centers
-      res.initialize(loadTable<2>( ConfigFileLookupPolicy()( psphys.get<std::string>("spectrumFileName"))) );
-      *elow = res.getAbscissa(0);
-      *ehi  = res.getAbscissa(res.getNbins()-1) + res.getBinWidth();
+      Spectrum.initialize(loadTable<2>( ConfigFileLookupPolicy()( psphys.get<std::string>("spectrumFileName"))) );
+      *elow = Spectrum.getAbscissa(0);
+      *ehi  = Spectrum.getAbscissa(Spectrum.getNbins()-1) + Spectrum.getBinWidth();
+      *eMax = psphys.get<double>("eMax");
+
       if(psphys.get<bool>("BinCenter", false)){
-        *elow -= res.getBinWidth()/2;
-        *ehi  -= res.getBinWidth()/2;
+        *elow -= Spectrum.getBinWidth()/2;
+        *ehi  -= Spectrum.getBinWidth()/2;
       }
       if(*elow < 0.0) throw cet::exception("BADCONFIG")
         << "StoppedParticleReactionGun: negative energy endpoint "<< *elow <<"\n";
@@ -185,8 +237,6 @@ namespace mu2e {
       throw cet::exception("BADCONFIG")
         << "StoppedParticleReactionGun: unknown spectrum shape "<<spectrumShape<<"\n";
     }
-
-    return res;
   }
 
   //================================================================
@@ -211,11 +261,16 @@ namespace mu2e {
                          stop.t);
 
     event.put(std::move(output));
+
+    if ( doHistograms_ ){
+      _hEnergyPositron->Fill(energy);
+    }
+
   }
 
   //================================================================
   double StoppedParticleReactionGun::generateEnergy() {
-    double res = elow_ + (ehi_ - elow_)*randSpectrum_.fire();
+    double res = elow_ + (ehi_ - elow_)*randSpectrum_->fire();
     if(res < 0.0)
        throw cet::exception("BADE")<<"StoppedParticleReactionGun: negative energy "<< res <<"\n";
     switch(spectrumVariable_) {
