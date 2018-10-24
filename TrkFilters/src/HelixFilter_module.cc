d5f7e1ae34708f2eca5d2a9d57718d150e9ea0a0
mwhalen <m.whalen@yale.edu>
2018-10-24 18:11:17 -0500

    Updated Filter for Helix Selection

--- TrkFilters/src/HelixFilter_module.cc
+++ TrkFilters/src/HelixFilter_module.cc
@@ -10,9 +10,13 @@
 #include "RecoDataProducts/inc/TrkFitFlag.hh"
 #include "RecoDataProducts/inc/TriggerInfo.hh"
 #include "fhiclcpp/ParameterSet.h"
-// mu2e
+#include "BFieldGeom/inc/BFieldManager.hh"
+#include "GeometryService/inc/GeomHandle.hh"
+#include "GeometryService/inc/DetectorSystem.hh"
 // data
 #include "RecoDataProducts/inc/HelixSeed.hh"
+// mu2e
+#include "Mu2eUtilities/inc/HelixTool.hh"
 using namespace CLHEP;
 // c++
 #include <string>
@@ -26,28 +30,47 @@ namespace mu2e
 {
   class HelixFilter : public art::EDFilter
   {
-    public:
-      explicit HelixFilter(fhicl::ParameterSet const& pset);
-      virtual bool filter(art::Event& event) override;
-      virtual bool endRun( art::Run& run ) override;
+  public:
+    explicit HelixFilter(fhicl::ParameterSet const& pset);
+    virtual bool filter(art::Event& event) override;
+    virtual bool beginRun(art::Run&   run   );
+    virtual bool endRun( art::Run& run ) override;
 
-    private:
-      art::InputTag _hsTag;
-      bool _hascc; // Calo Cluster
-      unsigned _minnhits;
-      double _minmom, _maxmom;
-      TrkFitFlag _goodh; // helix fit flag
-      int _debug;
-      // counters
-      unsigned _nevt, _npass;
+  private:
+    art::InputTag _hsTag;
+    bool _hascc; // Calo Cluster
+    unsigned _minnhits;
+    int _minnstrawhits;
+    double _minmom, _maxmom;
+    double _maxpT;
+    double _minpT;
+    double _maxchi2XY;
+    double _maxchi2PhiZ;
+    double _maxd0;
+    double _mind0;
+    double _maxlambda;
+    double _minlambda;
+    double _bz0;
+    TrkFitFlag _goodh; // helix fit flag
+    int _debug;
+    // counters
+    unsigned _nevt, _npass;
   };
 
   HelixFilter::HelixFilter(fhicl::ParameterSet const& pset) :
     _hsTag(pset.get<art::InputTag>("HelixSeedCollection","PosHelixFinder")),
     _hascc(pset.get<bool>("RequireCaloCluster",false)),
     _minnhits(pset.get<unsigned>("MinNHits",11)),
-    _minmom(pset.get<double>("MinMomentum",280.0)),
-    _maxmom(pset.get<double>("MaxMomentum",380.0)) ,
+    _minnstrawhits(pset.get<int>("MinNStrawHits",15)),
+    _minmom(pset.get<double>("MinMomentum",70.0)),
+    _maxmom(pset.get<double>("MaxMomentum",120.0)),
+    _minpT(pset.get<double>("MinpT", 0.)),
+    _maxchi2XY(pset.get<double>("MaxChi2XY", 5.)),
+    _maxchi2PhiZ(pset.get<double>("MaxChi2PhiZ", 5.)),
+    _maxd0(pset.get<double>("MaxD0", 200.)),
+    _mind0(pset.get<double>("MinD0", -200.)),
+    _maxlambda(pset.get<double>("MaxAbsLambda",350.)),
+    _minlambda(pset.get<double>("MinAbsLambda",150.)),
     _goodh(pset.get<vector<string> >("HelixFitFlag",vector<string>{"HelixOK"})),
     _debug(pset.get<int>("debugLevel",1)),
     _nevt(0), _npass(0)
@@ -55,25 +78,52 @@ namespace mu2e
     produces<TriggerInfo>();
   }
 
+  bool HelixFilter::beginRun(art::Run & run){
+    // get bfield
+    GeomHandle<BFieldManager> bfmgr;
+    GeomHandle<DetectorSystem> det;
+    Hep3Vector vpoint_mu2e = det->toMu2e(Hep3Vector(0.0,0.0,0.0));
+    _bz0 = bfmgr->getBField(vpoint_mu2e).z();
+    return true;
+  }
+
   bool HelixFilter::filter(art::Event& evt){
-  // create output
+    // create output
     unique_ptr<TriggerInfo> triginfo(new TriggerInfo);
     ++_nevt;
     bool retval(false); // preset to fail
-// find the collection
+    // find the collection
     auto hsH = evt.getValidHandle<HelixSeedCollection>(_hsTag);
     const HelixSeedCollection* hscol = hsH.product();
-// loop over the collection: if any pass the selection, pass this event
+    float mm2MeV = 3./10./_bz0;
+    // loop over the collection: if any pass the selection, pass this event
     for(auto ihs = hscol->begin();ihs != hscol->end(); ++ihs) {
       auto const& hs = *ihs;
-    // compute the helix momentum.  Note this is in units of mm!!!
-      float hmom = hs.helix().momentum();
+      
+      HelixTool helTool(&hs, 3);
+      // compute the helix momentum.  Note this is in units of mm!!!
+      float hmom       = hs.helix().momentum()*mm2MeV;
+      int   nstrawhits = helTool.nstrawhits();
+      float hpT        = hs.helix().radius()*mm2MeV;
+      float chi2XY     = helTool.chi2dXY();
+      float chi2PhiZ   = helTool.chi2dZPhi();
+      float d0         = hs.helix().rcent() - hs.helix().radius();
+      float lambda     = std::fabs(hs.helix().lambda());
+
       if(_debug > 2){
         cout << *currentContext()->moduleLabel() << "status = " << hs.status() << " nhits = " << hs.hits().size() << " mom = " << hmom << endl;
       }
       if( hs.status().hasAllProperties(_goodh) &&
           (!_hascc || hs.caloCluster().isNonnull()) &&
           hs.hits().size() >= _minnhits &&
+	  nstrawhits >= _minnstrawhits &&
+          hpT >= _minpT &&
+	  chi2XY <= _maxchi2XY &&
+	  chi2PhiZ <= _maxchi2PhiZ &&
+	  d0 <= _maxd0 &&
+	  d0 >= _mind0 &&
+	  lambda <= _maxlambda &&
+	  lambda >= _minlambda &&
           hmom > _minmom && hmom < _maxmom) {
         retval = true;
         ++_npass;
