6b47dafa850b88683e56e765964422a589322225
Nam Tran <namtran@bu.edu>
2018-12-11 10:42:53 -0600

    implement a cut off energy for Cry event generator

--- EventGenerator/src/CRYEventGenerator_module.cc
+++ EventGenerator/src/CRYEventGenerator_module.cc
@@ -4,6 +4,8 @@
 #include "MCDataProducts/inc/GenId.hh"
 #include "MCDataProducts/inc/GenParticleCollection.hh"
 #include "MCDataProducts/inc/G4BeamlineInfoCollection.hh"
+#include "GlobalConstantsService/inc/GlobalConstantsHandle.hh"
+#include "GlobalConstantsService/inc/ParticleDataTable.hh"
 
 // Particular generators that this code knows about.
 #include "SeedService/inc/SeedService.hh"
@@ -38,6 +40,9 @@ namespace mu2e {
     private:
       std::unique_ptr<CosmicCRY> cryGen;
       std::string inputfile;
+      unsigned int nRejected_;
+      unsigned int nTotal_;
+      double totalECutoff_;
       int seed_;
       art::RandomNumberGenerator::base_engine_t&     engine_;
   };
@@ -45,9 +50,11 @@ namespace mu2e {
   CryEventGenerator::CryEventGenerator(fhicl::ParameterSet const& pSet) :
     inputfile(pSet.get<std::string>("inputFile",
           "CRYEventGenerator/config/defaultCRYconfig.txt")),
+    nRejected_(0), nTotal_(0), totalECutoff_(pSet.get<double>("totalECutoff", 1E6)),
     seed_( art::ServiceHandle<SeedService>()->getSeed() ),
     engine_(createEngine(seed_))
   {
+    mf::LogInfo("CRYEventGenerator") << "Cutoff energy: " << totalECutoff_ << " MeV.";
     produces<GenParticleCollection>();
   }
 
@@ -57,12 +64,35 @@ namespace mu2e {
 
   void CryEventGenerator::produce(art::Event& evt) {
     std::unique_ptr<GenParticleCollection> genParticles(new GenParticleCollection);
-    cryGen->generate(*genParticles);
+    bool belowECutoff = false;
+    // keep clear + generate partiles untill everything is below cut off
+    while (!belowECutoff){
+      belowECutoff = true;
+      nTotal_ ++;
+      genParticles->clear();
+      cryGen->generate(*genParticles);
+
+      for (unsigned int i = 0; i < genParticles->size(); ++i) {
+        // mf::LogInfo("CRYEventGenerator") << "nRejected_ " << nRejected_ << ", particle: " << genParticles->at(i);
+        if (genParticles->at(i).momentum().e() > totalECutoff_) {
+          belowECutoff = false;
+          break;
+        }
+      }
+
+      if (!belowECutoff) 
+        nRejected_ ++;
+    }
+
     evt.put(std::move(genParticles));
   }
 
   void CryEventGenerator::endRun(art::Run&){
-    mf::LogInfo("CRYEventGenerator") << "Total live time simulated by CRY in this run: " << cryGen->getLiveTime();
+    std::ostringstream oss;
+    oss << "Total live time simulated in this run: " << cryGen->getLiveTime() << "\n";
+    oss << "Total number of events: " << nTotal_ << "\n";
+    oss << "Number of events rejected due to total energy cutoff (" << totalECutoff_ << " MeV): " << nRejected_;
+    mf::LogInfo("CRYEventGenerator") << oss.str();
   }
 
 }
