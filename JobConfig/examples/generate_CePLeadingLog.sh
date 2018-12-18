390f362deacd19dad8e750772b67edfe4702052f
David Nathan Brown <dave_brown@lbl.gov>
2018-12-18 15:33:02 -0600

    Add scripts for CeP production

---
+++ JobConfig/examples/generate_CePLeadingLog.sh
@@ -0,0 +1,3 @@
+generate_fcl --description CePLeadingLog --dsconf MDC2018e --dsowner mu2e --include JobConfig/primary/CePLeadingLog.fcl --run-number 1002 --events-per-job 10000 --njobs 200
+rm -rf CePLeadingLog
+mv 000 CePLeadingLog
