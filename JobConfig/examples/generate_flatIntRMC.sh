86021f122985401cd953d7f840bf8b7c1cf996ab
David Nathan Brown <dave_brown@lbl.gov>
2018-12-19 10:42:28 -0600

    rename examples

--- JobConfig/examples/generate_flatIntRMC.sh
+++
@@ -1,3 +0,0 @@
-generate_fcl --description flatIntRMC --dsconf MDC2018a --dsowner mu2e --include JobConfig/primary/flatIntRMC.fcl --run-number 1002 --events-per-job 60000 --njobs 500
-rm -rf flatIntRMC
-mv 000 flatIntRMC
--- JobConfig/examples/generate_flatIntRPC.sh
+++
@@ -1,3 +0,0 @@
-generate_fcl --description flatIntRPC --dsconf MDC2018a --dsowner mu2e --include JobConfig/primary/flatIntRPC.fcl --run-number 1002 --events-per-job 60000 --njobs 500
-rm -rf flatIntRPC
-mv 000 flatIntRPC
