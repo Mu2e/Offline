fab1df4825b12e45f5f7817584c88948369557b9
gianipez <gianipez@fnal.gov>
2019-03-11 11:22:58 -0500

    removed use of Station class from CalPatRec modules + updates in the Trigger templates file

--- CalPatRec/src/DeltaFinder2_module.cc
+++ CalPatRec/src/DeltaFinder2_module.cc
@@ -232,47 +232,54 @@ namespace mu2e {
       disk_z[i] = tpos.z();
     }
 
-    for (int ist=0; ist<_tracker->nStations(); ist++) {
-      const Station* st = &_tracker->getStation(ist);
+    int       nPlanesPerStation(2);
+    double    station_z(0);
+
+    for (int planeId=0; planeId<_tracker->nPlanes(); planeId++) {
+      const Plane* pln = &_tracker->getPlane(planeId);
+      int  ist = planeId/nPlanesPerStation;
+      int  ipl = planeId % nPlanesPerStation;
 
       //calculate the time-of-flight between the station and each calorimeter disk
       //for a typical Conversion Electron
-      for (int iDisk=0; iDisk<nDisks; ++iDisk){
-	_stationToCaloTOF[iDisk][ist] = (disk_z[iDisk] - st->midZ())/sin(_pitchAngle)/CLHEP::c_light;
+      if (ipl == 0) {
+	station_z = pln->origin().z();
+      }else {
+	station_z = (station_z + pln->origin().z())/2.;
+	for (int iDisk=0; iDisk<nDisks; ++iDisk){
+	  _stationToCaloTOF[iDisk][ist] = (disk_z[iDisk] - station_z)/sin(_pitchAngle)/CLHEP::c_light;
+	}
       }
 
-      for (int ipl=0; ipl<st->nPlanes(); ipl++) {
-	const Plane* pln = &st->getPlane(ipl);
-	for (int ipn=0; ipn<pln->nPanels(); ipn++) {
-	  const Panel* panel = &pln->getPanel(ipn);
-	  int face;
-	  if (panel->id().getPanel() % 2 == 0) face = 0;
-	  else                                 face = 1;
-	  for (int il=0; il<panel->nLayers(); ++il) {
-	    cx.Station = ist;
-	    cx.Plane   = ipl;
-	    cx.Face    = face;
-	    cx.Panel   = ipn;
-	    cx.Layer   = il;
-	    orderID (&cx, &co);
-	    int os = co.Station; 
-	    int of = co.Face;
-	    int op = co.Panel;
-	    PanelZ_t* pz = &_data.oTracker[os][of][op];
-	    pz->fPanel = panel;
-//-----------------------------------------------------------------------------
-// panel caches phi of its center and the z
-//-----------------------------------------------------------------------------
-	    pz->wx  = panel->straw0Direction().x();
-	    pz->wy  = panel->straw0Direction().y();
-	    pz->phi = panel->straw0MidPoint().phi();
-	    pz->z   = (panel->getStraw(0).getMidPoint().z()+panel->getStraw(1).getMidPoint().z())/2.;
-	  }
-	}	
-      }
+      for (int ipn=0; ipn<pln->nPanels(); ipn++) {
+	const Panel* panel = &pln->getPanel(ipn);
+	int face;
+	if (panel->id().getPanel() % 2 == 0) face = 0;
+	else                                 face = 1;
+	for (int il=0; il<panel->nLayers(); ++il) {
+	  cx.Station = ist;
+	  cx.Plane   = ipl;
+	  cx.Face    = face;
+	  cx.Panel   = ipn;
+	  cx.Layer   = il;
+	  orderID (&cx, &co);
+	  int os = co.Station; 
+	  int of = co.Face;
+	  int op = co.Panel;
+	  PanelZ_t* pz = &_data.oTracker[os][of][op];
+	  pz->fPanel = panel;
+	  //-----------------------------------------------------------------------------
+	  // panel caches phi of its center and the z
+	  //-----------------------------------------------------------------------------
+	  pz->wx  = panel->straw0Direction().x();
+	  pz->wy  = panel->straw0Direction().y();
+	  pz->phi = panel->straw0MidPoint().phi();
+	  pz->z   = (panel->getStraw(0).getMidPoint().z()+panel->getStraw(1).getMidPoint().z())/2.;
+	}
+      }	
 
-      if ((ist == 6) || (ist == 13))  _data.stationUsed[ist] = 0;
-      else                            _data.stationUsed[ist] = 1;
+      // if ((ist == 6) || (ist == 13))  _data.stationUsed[ist] = 0;
+      // else                            _data.stationUsed[ist] = 1;
     }
 //-----------------------------------------------------------------------------
 // it is enough to print that once
