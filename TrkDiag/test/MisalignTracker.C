c0e8337f1d22240688c7a9d9c5b2765d448278cc
David Nathan Brown <dave_brown@lbl.gov>
2019-09-23 15:53:18 -0700

    update diagnostics

--- TrkDiag/test/MisalignTracker.C
+++ TrkDiag/test/MisalignTracker.C
@@ -11,34 +11,51 @@
 using namespace std;
 void MisalignTracker(double twist, double skew, double squeeze, double sigalpha, double sigpos, const char* outfile) {
   unsigned NPlanes(36);
-  double planezgap(57.0); // FIXME!!
-  
+  double planezgap(83.0);
+  double planer(700.0);
+  double trackerlen = NPlanes*planezgap; // approximate
+
   TRandom3 myrand(3499803);
+  double rtwist = myrand.Uniform(-twist,twist);
+  double xskew = myrand.Uniform(-skew,skew);
+  double yskew = myrand.Uniform(-skew,skew);
+  double rsqueeze = myrand.Uniform(-squeeze,squeeze);
+  double alphaxskew = xskew/(2*planer);
+  double alphayskew = yskew/(2*planer);
+  double dtwist = twist/(6.28*planer*trackerlen);
 
   filebuf fb;
   fb.open (outfile,ios::out);
   ostream os(&fb);
   // write header
   os << "# Tracker Misalignments from MisalignTracker.C, parameters: "
-  << " twist = "<< twist
-  << " skew = "<< skew
-  << " sigalpha = "<< sigalpha
-  << " sigpos = "<< sigpos
+  << " twist = "<< rtwist << " mm" 
+  << " dtwist = " << dtwist << " rad/mm"
+  << " xskew = "<< xskew << " mm" 
+  << " yskew = "<< yskew << " mm" 
+  << " squeeze = "<< rsqueeze << " mm" 
+  << " sigalpha = "<< sigalpha << " rad" 
+  << " sigpos = "<< sigpos << " mm"
   << endl;
+  
   for(unsigned iplane=0;iplane < NPlanes; ++iplane){
-    double planez = iplane*planezgap;
+    double planez = (iplane-float(NPlanes)/2.0)*planezgap;
     double dalphax = myrand.Gaus(0.0,sigalpha);
-    double dalphay = myrand.Gaus(skew,sigalpha);
-    double dalphaz = myrand.Gaus(twist*planez,sigalpha);
+    double dalphay = myrand.Gaus(0.0,sigalpha);
+    double dalphaz = myrand.Gaus(dtwist*planez,sigalpha);
+    double dx = myrand.Gaus(planez*xskew/trackerlen,sigpos);
+    double dy = myrand.Gaus(planez*yskew/trackerlen,sigpos);
+    double dz = myrand.Gaus(planez*rsqueeze/trackerlen,sigpos);
+
     os << "Plane " << iplane 
       << " dalpha : [ "
       << dalphax << " , "
       << dalphay << " , "
       << dalphaz << " ] "
       << " dpos : [ "
-      << myrand.Gaus(0.0,sigpos) << " , "
-      << myrand.Gaus(0.0,sigpos) << " , "
-      << myrand.Gaus(0.0,sigpos) << " ] "
+      << dx << " , "
+      << dy << " , "
+      << dz << " ] "
       << endl;
   }
 
