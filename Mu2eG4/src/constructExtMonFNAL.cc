74553604a51e3d0fa2beb7a9164e1226ec4ee7ed
Michael T Hedges <hedges7@hubble.physics.purdue.edu>
2019-09-25 12:00:27 -0400

    Changed ExtMonStackMother* volumes to G4Boxes

--- Mu2eG4/src/constructExtMonFNAL.cc
+++ Mu2eG4/src/constructExtMonFNAL.cc
@@ -82,30 +82,61 @@ namespace mu2e {
         
     double px = stack.motherTransverseHalfSize()[0];
     double py = stack.motherTransverseHalfSize()[1];
-    std::vector<G4TwoVector> polygon;
-    polygon.push_back({+px,+py});
-    polygon.push_back({-px,+py});
-    polygon.push_back({-px,-py});
-    polygon.push_back({+px,-py});
 
-    std::vector<G4ExtrudedSolid::ZSection> zsections;
-    zsections.emplace_back(stack.motherStartZ(), G4TwoVector(), 1.);
-    zsections.emplace_back(stack.motherEndZ(), G4TwoVector(), 1.);
-    VolumeInfo mother = nestExtrudedSolid("ExtMonStackMother"+volNameSuffix,
-                                          polygon,
-                                          zsections,
-                                          findMaterialOrThrow("G4_AIR"),
-                                          stackRotationInRoomInv,
-                                          stackRefPointInRoom,
-                                          parent,
-                                          0,
-                                          isStackMotherVisible,
-                                          G4Colour::Magenta(),
-                                          isStackMotherSolid,
-                                          forceAuxEdgeVisible,
-                                          placePV,
-                                          doSurfaceCheck
-                                          );
+    /* Old method: Using G4ExtrudedSolid (bug in Geant4 10.4.2) */
+    //std::vector<G4TwoVector> polygon;
+    //polygon.push_back({+px,+py});
+    //polygon.push_back({-px,+py});
+    //polygon.push_back({-px,-py});
+    //polygon.push_back({+px,-py});
+
+    //std::vector<G4ExtrudedSolid::ZSection> zsections;
+    //zsections.emplace_back(stack.motherStartZ(), G4TwoVector(), 1.);
+    //zsections.emplace_back(stack.motherEndZ(), G4TwoVector(), 1.);
+
+    //VolumeInfo mother = nestExtrudedSolid("ExtMonStackMother"+volNameSuffix,
+    //                                      polygon,
+    //                                      zsections,
+    //                                      findMaterialOrThrow("G4_AIR"),
+    //                                      stackRotationInRoomInv,
+    //                                      stackRefPointInRoom,
+    //                                      parent,
+    //                                      0,
+    //                                      isStackMotherVisible,
+    //                                      G4Colour::Magenta(),
+    //                                      isStackMotherSolid,
+    //                                      forceAuxEdgeVisible,
+    //                                      placePV,
+    //                                      doSurfaceCheck
+    //                                      );
+
+    //if (volNameSuffix == "Dn") std::cout << "volNameSuffix is Dn" << std::endl;
+    //else if (volNameSuffix == "Up") std::cout << "volNameSuffix is Up" << std::endl;
+
+    // Construct ExtMonStackMother* as nestedBox
+    double stackMotherZCoord = (stack.motherStartZ() + stack.motherEndZ())/2.0;
+    CLHEP::Hep3Vector stackMotherZVec (0, 0, stackMotherZCoord);
+    CLHEP::Hep3Vector stackMotherOffset = stackRefPointInRoom
+      + stackRotationInRoom * stackMotherZVec;
+
+    double pz = abs( stack.motherStartZ() - stack.motherEndZ() )/2.0;
+    double const halfDims[3] = {px, py, pz};
+
+    VolumeInfo mother = nestBox("ExtMonStackMother"+volNameSuffix,
+                                halfDims,
+                                findMaterialOrThrow("G4_AIR"),
+                                stackRotationInRoomInv,
+                                stackMotherOffset,
+                                parent,
+                                0,
+                                isStackMotherVisible,
+                                G4Colour::Magenta(),
+                                isStackMotherSolid,
+                                forceAuxEdgeVisible,
+                                placePV,
+                                doSurfaceCheck
+                                );
+
     constructExtMonFNALPlanes(mother,
                               module,
                               stack,
@@ -164,7 +195,6 @@ namespace mu2e {
                                   )
                                 );
 
-
           VolumeInfo vdInfo = nestBox(VirtualDetector::volumeName(vdId),
                                       hlen,
                                       vacuumMaterial,
@@ -216,9 +246,7 @@ namespace mu2e {
       std::ostringstream osp;
       osp<<"EMFPlane"<<volNameSuffix<<iplane;
       
-      AGDEBUG("Constucting "<<osp.str()<<", plane number "<<iplane + stack.planeNumberOffset());
-      G4ThreeVector offset = {stack.plane_xoffset()[iplane], stack.plane_yoffset()[iplane], stack.plane_zoffset()[iplane]};
-      
+
       // nest individual planes
       VolumeInfo vplane = nestBox(osp.str(),
                                   plane.halfSize(),
