49fb4e2e13de12ef139a56366e80b33c38fcf35b
gianipez <gianipez@fnal.gov>
2019-09-29 23:31:39 -0500

    fixed constructor of the TriggerResultsNavigator class

--- Mu2eUtilities/inc/TriggerResultsNavigator.hh
+++ Mu2eUtilities/inc/TriggerResultsNavigator.hh
@@ -1,17 +1,10 @@
 #ifndef Mu2eUtilities_TriggerResultsNavigator_hh
 #define Mu2eUtilities_TriggerResultsNavigator_hh
 //
-//
-// $Id: $
-// $Author: $
-// $Date: $
-//
 // Original author G. Pezzullo
 //
 
 #include "canvas/Persistency/Common/TriggerResults.h"
-#include "fhiclcpp/ParameterSet.h"
-#include "fhiclcpp/ParameterSetRegistry.h"
 
 #include <map>
 #include <string>
@@ -23,19 +16,7 @@ namespace mu2e {
   class TriggerResultsNavigator{
 
   public:
-    TriggerResultsNavigator(const art::TriggerResults* trigResults) :
-      _trigResults(trigResults){
-      auto const  id   = trigResults->parameterSetID();
-      auto const& pset = fhicl::ParameterSetRegistry::get(id);
-      //set the vector<string> with the names of the tirgger_paths
-      _trigPathsNames  = pset.get<std::vector<std::string>>("trigger_paths");
-      
-      //loop over trigResults to fill the map <string, unsigned int) 
-      for (unsigned int i=0; i< _trigPathsNames.size(); ++i){
-    	_trigMap.insert(std::pair<std::string, unsigned int>(_trigPathsNames[i], i));
-      }
-    }
-
+    TriggerResultsNavigator(const art::TriggerResults* trigResults);
     
     // Trigger path information for the current process
     size_t  size() const
