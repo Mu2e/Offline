060a644663d3bf4ce6a4e1309a4499feb300e7e4
gianipez <gianipez@fnal.gov>
2019-10-24 10:20:44 -0500

    code clean up

--- TrkFilters/inc/PhiPrescalingParams.hh
+++ TrkFilters/inc/PhiPrescalingParams.hh
@@ -1,21 +1,22 @@
 #ifndef TrkFilters_PhiPrescalingParams_hh
 #define TrkFilters_PhiPrescalingParams_hh
 
-struct PhiPrescalingParams {
-  float    _amplitude;
-  float    _frequency;
-  float    _phase;
+namespace mu2e {
+  struct PhiPrescalingParams {
+    float    _amplitude;
+    float    _frequency;
+    float    _phase;
 
-  PhiPrescalingParams():
-    _amplitude(0),
-    _frequency(0),
-    _phase(0) {}
+    PhiPrescalingParams():
+      _amplitude(0),
+      _frequency(0),
+      _phase(0) {}
   
-  PhiPrescalingParams(float Amplitude, float Frequency, float Phase):
-    _amplitude(Amplitude),
-    _frequency(Frequency),
-    _phase(Phase) {}
-};
+    PhiPrescalingParams(float Amplitude, float Frequency, float Phase):
+      _amplitude(Amplitude),
+      _frequency(Frequency),
+      _phase(Phase) {}
+  };
 
-
+}
 #endif /* TrkFilters_PhiPrescalingParams_hh */
--- TrkFilters/src/TrkFiltersHelpers.cc
+++ TrkFilters/src/TrkFiltersHelpers.cc
@@ -1,26 +1,26 @@
-// Andrei Gaponenko, 2012
+// Gianantonio Pezzullo, 2019
 
+#include "GeneralUtilities/inc/ParameterSetHelpers.hh"
 #include "TrkFilters/inc/TrkFiltersHelpers.hh"
 
-#include <iostream>
-#include <sstream>
-#include <stdexcept>
+//#include <iostream>
+//#include <sstream>
+//#include <stdexcept>
 
-#include <vector>
+//#include <vector>
 
 #include "TrkFilters/inc/PhiPrescalingParams.hh"
-#include "GeneralUtilities/inc/ParameterSetHelpers.hh"
 
 #include "canvas/Utilities/InputTag.h"
 
 template<>
-bool fhicl::ParameterSet::get_if_present<PhiPrescalingParams>(std::string const & key, PhiPrescalingParams& value) const {
+bool fhicl::ParameterSet::get_if_present<mu2e::PhiPrescalingParams>(std::string const & key, mu2e::PhiPrescalingParams& value) const {
   fhicl::ParameterSet pset;
   const bool present = get_if_present<fhicl::ParameterSet>(key,pset);
   float amplitude = pset.get<float>("amplitude");
   float frequency = pset.get<float>("frequency");
   float phase     = pset.get<float>("phase"    );
 
-  value = PhiPrescalingParams(amplitude, frequency, phase);
+  value = mu2e::PhiPrescalingParams(amplitude, frequency, phase);
   return present;
 }
