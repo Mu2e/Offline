da44a775b91026c57ae93ca59afbfe8b92cfbe71
Ray Culbertson <rlc@fnal.gov>
2019-11-11 14:19:50 -0600

    update for e19 compiler

--- setup.sh
+++ setup.sh
@@ -102,7 +102,7 @@ build=$($MU2E_BASE_RELEASE/buildopts --build)
 # products that need qualifiers.  Note it includes the '+' character
 # and is therefore different from the value shown in
 # SETUP_<productname> environment vars, or by the "ups active" command.
-export MU2E_UPS_QUALIFIERS=+e17:+${build}
+export MU2E_UPS_QUALIFIERS=+e19:+${build}
 export MU2E_ART_SQUALIFIER=s89
 
 MU2E_G4_GRAPHICS_QUALIFIER=''
@@ -132,10 +132,10 @@ fi
 setup -B mu2e_artdaq_core v1_02_30 -q${MU2E_UPS_QUALIFIERS}:+${MU2E_ART_SQUALIFIER}:offline
 
 # Other libraries we need.
-setup -B pcie_linux_kernel_module v2_02_03 -q${MU2E_UPS_QUALIFIERS}
+setup -B pcie_linux_kernel_module v2_02_07a -q${MU2E_UPS_QUALIFIERS}:+${MU2E_ART_SQUALIFIER}
 
 setup -B heppdt   v3_04_01j -q${MU2E_UPS_QUALIFIERS}
-setup -B BTrk   v1_02_18  -q${MU2E_UPS_QUALIFIERS}
+setup -B BTrk   v1_02_18c  -q${MU2E_UPS_QUALIFIERS}
 setup -B cry   v1_7m  -q${MU2E_UPS_QUALIFIERS}
 setup -B gsl v2_5  -q${build}
 setup curl v7_64_1
