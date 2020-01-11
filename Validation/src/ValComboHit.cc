cf6da9490b8586002de531cbc9c1a79278af475a
David Nathan Brown <dave_brown@lbl.gov>
2020-01-11 09:43:05 -0600

    update version number, improve range + titles

--- Validation/src/ValComboHit.cc
+++ Validation/src/ValComboHit.cc
@@ -7,12 +7,12 @@ int mu2e::ValComboHit::declare(art::TFileDirectory tfs) {
   _hN2 = tfs.make<TH1D>( "NHit2", "N Combo Hits", 100, -0.5, 9999.5);
   _hNstr = tfs.make<TH1D>( "Nstr", "N Parent Straws", 11, -0.5, 10.5);
   _hWD = tfs.make<TH1D>( "WD", "WireDistance;Distance from mid (mm)", 100, -800.0, 800.0);
-  _hDE = tfs.make<TH1D>( "DE", "TOT Drift Time Estimate", 100, 0.0, 80.0);
-  _ht = tfs.make<TH1D>( "t", "time", 100, 0.0, 2000.0);
-  _hE = tfs.make<TH1D>( "E", "Energy",50, 0.0, 0.02);
+  _hDE = tfs.make<TH1D>( "DE", "TOT Drift Time Estimate;Drift Time (ns)", 100, 0.0, 80.0);
+  _ht = tfs.make<TH1D>( "t", "time;Hit Time (ns)", 100, 0.0, 2000.0);
+  _hE = tfs.make<TH1D>( "E", "Energy;Hit Energy (KeV)",50, 0.0, 5.0);
   _hqual = tfs.make<TH1D>( "qual", "Qual",50, 0.001, 3.0);
-  _hwres = tfs.make<TH1D>( "wres", "Wire resolution",50, 0.0, 100.0);
-  _htres = tfs.make<TH1D>( "tres", "Transvrse resolution",50, 0.0, 10.0);
+  _hwres = tfs.make<TH1D>( "wres", "Wire resolution;resolution (mm)",50, 0.0, 100.0);
+  _htres = tfs.make<TH1D>( "tres", "Transverse resolution;resolution (mm)",50, 0.0, 10.0);
   _hPanel = tfs.make<TH1D>( "Panel", "Tracker Unique Panel",216, -0.5, 215.5);
   _hStraw = tfs.make<TH1D>( "Straw", "Tracker Straw",96, -0.5, 95.5);
 
@@ -24,7 +24,7 @@ int mu2e::ValComboHit::fill(const mu2e::ComboHitCollection & coll,
 
   // increment this by 1 any time the defnitions of the histograms or the 
   // histogram contents change, and will not match previous versions
-  _hVer->Fill(0.0);
+  _hVer->Fill(1.0);
 
   _hN->Fill(coll.size()); 
   _hN2->Fill(coll.size());
@@ -33,7 +33,7 @@ int mu2e::ValComboHit::fill(const mu2e::ComboHitCollection & coll,
     _hWD->Fill(co.wireDist());
     _hDE->Fill(co.driftTime());
     _ht->Fill(co.time());
-    _hE->Fill(co.energyDep());
+    _hE->Fill(1000*co.energyDep());
     _hqual->Fill(co.qual());
     _hwres->Fill(co.wireRes());
     _htres->Fill(co.transRes());
