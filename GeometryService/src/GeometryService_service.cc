720e0d5e24bbf37e3cc6da687501532915ec3b82
Robert Bernstein <rhbob@fnal.gov>
2020-01-13 13:55:06 -0600

    hayman2 code review

--- GeometryService/src/GeometryService_service.cc
+++ GeometryService/src/GeometryService_service.cc
@@ -207,11 +207,11 @@ namespace mu2e {
     //addDetector(PSShieldMaker::make(*_config, ps.psEndRefPoint(), prodTarget.position()));
 
    if (_config->getString("targetPS_model") == "MDC2018"){ 
-      std::cout << "adding Tier1 in GeometryService" << std::endl;
+     //      std::cout << "adding Tier1 in GeometryService" << std::endl;
       addDetector(PSShieldMaker::make(*_config, ps.psEndRefPoint(), prodTarget.position()));
 	} else 
       if (_config->getString("targetPS_model") == "Hayman_v_2_0"){ 
-	std::cout << " adding Hayman in GeometryService" << std::endl;
+	//	std::cout << " adding Hayman in GeometryService" << std::endl;
 	addDetector(PSShieldMaker::make(*_config, ps.psEndRefPoint(), prodTarget.haymanProdTargetPosition()));
 	  } else 
 	{throw cet::exception("GEOM") << " " << __func__ << " illegal production target version specified in GeometryService_service = " << _config->getString("targetPS_model")  << std::endl;}
