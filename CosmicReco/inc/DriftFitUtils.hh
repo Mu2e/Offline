4a259715e5eecfce20922e9fd346d21c43b00085
sophiemiddleton <sophie@fnal.gov>
2020-01-15 12:12:36 -0600

    changed XYZVec to XYZVec const&

--- CosmicReco/inc/DriftFitUtils.hh
+++ CosmicReco/inc/DriftFitUtils.hh
@@ -34,9 +34,9 @@ using namespace mu2e;
 
 namespace DriftFitUtils{
   	TrackEquation ConvertFitToDetectorFrame(TrackAxes axes, XYZVec Position, XYZVec Direction);
-	double GetTestDOCA(ComboHit const& chit,XYZVec pos, XYZVec dir,  const Tracker* tracker);
+	double GetTestDOCA(ComboHit const& chit,XYZVec const& pos, XYZVec const& dir,  const Tracker* tracker);
 	double GetTestDOCA(ComboHit const& chit,double a0, double a1, double b0, double b1, const Tracker* tracker);
-        int GetAmbig(ComboHit const& chit, XYZVec pos, XYZVec dir,  const Tracker* tracker);
+        int GetAmbig(ComboHit const& chit, XYZVec const& pos, XYZVec const& dir,  const Tracker* tracker);
         int GetAmbig(ComboHit const& chit, double a0, double a1, double b0, double b1,  const Tracker* tracker);
   	double GetPropVelocity(StrawResponse const& srep, ComboHit const& chit); 
 	double GetPropTime(ComboHit const& chit, double vprop, const Tracker* tracker);
