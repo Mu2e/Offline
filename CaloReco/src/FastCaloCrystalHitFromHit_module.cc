55e922a962962347b2508680988b13d295f03626
sophiemiddleton <sophie@fnal.gov>
2020-01-24 12:23:25 -0600

    some edits

--- CaloReco/src/FastCaloCrystalHitFromHit_module.cc
+++ CaloReco/src/FastCaloCrystalHitFromHit_module.cc
@@ -21,145 +21,135 @@
 #include <cmath>
 
 namespace mu2e {
+	class FastCaloCrystalHitFromHit : public art::EDProducer {
+  		public:
 
+			explicit FastCaloCrystalHitFromHit(fhicl::ParameterSet const& pset) :
+			art::EDProducer{pset},
+			caloDigisToken_{consumes<CaloRecoDigiCollection>(pset.get<std::string>("caloDigisModuleLabel"))},
+			time4Merge_          (pset.get<double>     ("time4Merge")),
+			diagLevel_           (pset.get<int>        ("diagLevel",0))
+			{
+				produces<CaloCrystalHitCollection>();
+			}
 
-  class FastCaloCrystalHitFromHit : public art::EDProducer {
-  public:
+	    		void produce(art::Event& e) override;
 
-    explicit FastCaloCrystalHitFromHit(fhicl::ParameterSet const& pset) :
-      art::EDProducer{pset},
-      caloDigisToken_{consumes<CaloRecoDigiCollection>(pset.get<std::string>("caloDigisModuleLabel"))},
-      time4Merge_          (pset.get<double>     ("time4Merge")),
-      diagLevel_           (pset.get<int>        ("diagLevel",0))
-    {
-      produces<CaloCrystalHitCollection>();
-    }
+  		private:
 
-    void produce(art::Event& e) override;
+			typedef art::Ptr<CaloRecoDigi> CaloRecoDigiPtr;
 
-  private:
-
-    typedef art::Ptr<CaloRecoDigi> CaloRecoDigiPtr;
-
-    art::ProductToken<CaloRecoDigiCollection> const caloDigisToken_;
-    double      time4Merge_;
-    int         diagLevel_;
-
-    std::vector<std::vector<const CaloRecoDigi*>> hitMap_;  
-
-
-    void MakeCaloCrystalHits(CaloCrystalHitCollection& CaloCrystalHits, const art::ValidHandle<CaloRecoDigiCollection>& recoCaloDigisHandle);
-
-    void FillBuffer(int crystalId, int nRoid, double time, double timeErr, double eDep, double eDepErr,
+			art::ProductToken<CaloRecoDigiCollection> const caloDigisToken_;
+			double      time4Merge_;
+			int         diagLevel_;
+			std::vector<std::vector<const CaloRecoDigi*>> hitMap_;  
+			void MakeCaloCrystalHits(CaloCrystalHitCollection& CaloCrystalHits, const art::ValidHandle<CaloRecoDigiCollection>& recoCaloDigisHandle);
+			void FillBuffer(int crystalId, int nRoid, double time, double timeErr, double eDep, double eDepErr,
                     std::vector<CaloRecoDigiPtr>& buffer, CaloCrystalHitCollection& CaloCrystalHits);
-  };
+	};
 
-  //------------------------------------------------------------
-  void FastCaloCrystalHitFromHit::produce(art::Event& event)
-  {
-    auto const& recoCaloDigisHandle = event.getValidHandle(caloDigisToken_);
-    auto CaloCrystalHits = std::make_unique<CaloCrystalHitCollection>();
-    MakeCaloCrystalHits(*CaloCrystalHits, recoCaloDigisHandle);
-    //store the crystal hit collection:
-    event.put(std::move(CaloCrystalHits));
-  }
+	void FastCaloCrystalHitFromHit::produce(art::Event& event)
+  	{
+		auto const& recoCaloDigisHandle = event.getValidHandle(caloDigisToken_);
+		auto CaloCrystalHits = std::make_unique<CaloCrystalHitCollection>();
+		MakeCaloCrystalHits(*CaloCrystalHits, recoCaloDigisHandle);
+		//store the crystal hit collection:
+		event.put(std::move(CaloCrystalHits));
+ 	 }
 
+	void FastCaloCrystalHitFromHit::MakeCaloCrystalHits(CaloCrystalHitCollection& CaloCrystalHits, art::ValidHandle<CaloRecoDigiCollection> const& recoCaloDigisHandle)
+  	{
+		Calorimeter const &cal = *(GeomHandle<Calorimeter>());
+		auto const& recoCaloDigis = *recoCaloDigisHandle;
+		CaloRecoDigi const* base = &recoCaloDigis.front(); // TODO What if recoCaloDigis is empty?
+		if (cal.nRO() > int(hitMap_.size())) continue;
+		
+		for (int i = hitMap_.size(); i<= cal.nRO(); ++i) hitMap_.push_back(std::vector<const CaloRecoDigi*>());
+		for (size_t i=0; i<hitMap_.size(); ++i) hitMap_[i].clear();
 
-  //--------------------------------------------------------------------------------------------------------------
-  void FastCaloCrystalHitFromHit::MakeCaloCrystalHits(CaloCrystalHitCollection& CaloCrystalHits, art::ValidHandle<CaloRecoDigiCollection> const& recoCaloDigisHandle)
-  {
-    Calorimeter const &cal = *(GeomHandle<Calorimeter>());
-    auto const& recoCaloDigis = *recoCaloDigisHandle;
-    CaloRecoDigi const* base = &recoCaloDigis.front(); // TODO What if recoCaloDigis is empty?
-
-
-    if (cal.nRO() > int(hitMap_.size()))
-      for (int i = hitMap_.size(); i<= cal.nRO(); ++i) hitMap_.push_back(std::vector<const CaloRecoDigi*>());
-    for (size_t i=0; i<hitMap_.size(); ++i) hitMap_[i].clear();
-
-    for (unsigned int i=0; i< recoCaloDigis.size(); ++i)
-      {
-        int crystalId = cal.caloInfo().crystalByRO(recoCaloDigis[i].ROid());
-        hitMap_[crystalId].push_back(&recoCaloDigis[i]);
-      }
+		for (unsigned int i=0; i< recoCaloDigis.size(); ++i)
+		{
+			int crystalId = cal.caloInfo().crystalByRO(recoCaloDigis[i].ROid());
+			hitMap_[crystalId].push_back(&recoCaloDigis[i]);
+		}
 
 
-    for (unsigned int crystalId=0;crystalId<hitMap_.size();++crystalId)
-      {
-        std::vector<const CaloRecoDigi*> &hits = hitMap_[crystalId];
+		for (unsigned int crystalId=0;crystalId<hitMap_.size();++crystalId)
+		{
+			std::vector<const CaloRecoDigi*> &hits = hitMap_[crystalId];
         
-        //check if empty:
-        if (hits.empty()) continue;
+			//check if empty:
+			if (hits.empty()) continue;
 
-        //sort hits in terms of time:
-        std::sort(hits.begin(),hits.end(),[](const auto a, const auto b){return a->time() < b->time();});
+			//sort hits in terms of time:
+			std::sort(hits.begin(),hits.end(),[](const auto a, const auto b){return a->time() < b->time();});
 
-        //find hit:
-        auto startHit = hits.begin();
-        auto endHit   = hits.begin();
+			//find hit:
+			auto startHit = hits.begin();
+			auto endHit   = hits.begin();
 
-        //create a buffer 
-        std::vector<CaloRecoDigiPtr> buffer;
-        double timeW(0);
-        double eDepTot(0),eDepTotErr(0);
-        int nRoid(0);
+			//create a buffer 
+			std::vector<CaloRecoDigiPtr> buffer;
+			double timeW(0);
+			double eDepTot(0),eDepTotErr(0);
+			int nRoid(0);
 
-        //loop through hits:
-        while (endHit != hits.end())
-          {
-            //time:
-            double deltaTime = (*endHit)->time()-(*startHit)->time();
-        
-            //if > than set merge time:
-            if (deltaTime > time4Merge_) 
-              {
-                double time = timeW/nRoid;
-                double timeErr = 0;
-                //fill that buffer: //TODO -->peakpos, flags etc.
-                FillBuffer(crystalId, nRoid, time, timeErr, eDepTot/nRoid, eDepTotErr/nRoid, buffer, CaloCrystalHits);
-                //clear:
-                buffer.clear();
-                timeW      = 0.0;
-                eDepTot    = 0.0;
-                eDepTotErr = 0.0;
-                nRoid      = 0;
-                startHit   = endHit;
-              }
-            else //if not then:
-              {
-                //add up times
-                timeW      += (*endHit)->time();
-                //add up energies
-                eDepTot    += (*endHit)->energyDep();
-                eDepTotErr += (*endHit)->energyDepErr() * (*endHit)->energyDepErr();
-                //increment no. ROs
-                ++nRoid;
-                //get index:
-                size_t index = *endHit - base;
-                //add to end of hit buffer:
-                buffer.push_back(art::Ptr<CaloRecoDigi>(recoCaloDigisHandle, index));
-                //increment:
-                ++endHit;
-              }
+			//loop through hits:
+			while (endHit != hits.end())
+          		{
+				//time:
+				double deltaTime = (*endHit)->time()-(*startHit)->time();
 
-          }
-        //get time and error
-        double time = timeW/nRoid;
-        double timeErr = 0;
-        //Finally fill buffer:
-        FillBuffer(crystalId, nRoid, time, timeErr, eDepTot/nRoid, eDepTotErr/nRoid, buffer, CaloCrystalHits);
-      }
+				//if > than set merge time:
+				if (deltaTime > time4Merge_) 
+				{
+					double time = timeW/nRoid;
+					double timeErr = 0;
+					//fill that buffer: //TODO -->peakpos, flags etc.
+					FillBuffer(crystalId, nRoid, time, timeErr, eDepTot/nRoid, eDepTotErr/nRoid, buffer, CaloCrystalHits);
+					//clear:
+					buffer.clear();
+					timeW      = 0.0;
+					eDepTot    = 0.0;
+					eDepTotErr = 0.0;
+					nRoid      = 0;
+					startHit   = endHit;
+              			}
+            			else //if not then:
+              			{
+					//add up times
+					timeW      += (*endHit)->time();
+					//add up energies
+					eDepTot    += (*endHit)->energyDep();
+					eDepTotErr += (*endHit)->energyDepErr() * (*endHit)->energyDepErr();
+					//increment no. ROs
+					++nRoid;
+					//get index:
+					size_t index = *endHit - base;
+					//add to end of hit buffer:
+					buffer.push_back(art::Ptr<CaloRecoDigi>(recoCaloDigisHandle, index));
+					//increment:
+					++endHit;
+              			}
+
+          		}
+			//get time and error
+			double time = timeW/nRoid;
+			double timeErr = 0;
+			//Finally fill buffer:
+			FillBuffer(crystalId, nRoid, time, timeErr, eDepTot/nRoid, eDepTotErr/nRoid, buffer, CaloCrystalHits);
+		}
 
 
-    if ( diagLevel_ > 0 )
-      {
-        printf("[FastCaloCrystalHitFromHit::produce] produced RecoCrystalHits ");
-        printf(": CaloCrystalHits.size()  = %i \n", int(CaloCrystalHits.size()));
-      }
+		if ( diagLevel_ > 0 )
+		{
+			printf("[FastCaloCrystalHitFromHit::produce] produced RecoCrystalHits ");
+			printf(": CaloCrystalHits.size()  = %i \n", int(CaloCrystalHits.size()));
+		}
 
-  }
+	}
 
-  void FastCaloCrystalHitFromHit::FillBuffer(int const crystalId,
+	void FastCaloCrystalHitFromHit::FillBuffer(int const crystalId,
                                          int const nRoid,
                                          double const time,
                                          double const timeErr,
@@ -167,17 +157,17 @@ namespace mu2e {
                                          double const eDepErr,
                                          std::vector<CaloRecoDigiPtr>& buffer,
                                          CaloCrystalHitCollection& CaloCrystalHits)
-  {
+  	{
  
-    CaloCrystalHits.emplace_back(CaloCrystalHit(crystalId, nRoid, time, timeErr, eDep, eDepErr, buffer));
+	    CaloCrystalHits.emplace_back(CaloCrystalHit(crystalId, nRoid, time, timeErr, eDep, eDepErr, buffer));
 
-    if (diagLevel_ > 1)
-      {
-        std::cout<<"[FastCaloCrystalHitFromHit] created hit in crystal id="<<crystalId<<"\t with time="<<time<<"\t eDep="<<eDep<<"\t  from "<<nRoid<<" RO"<<std::endl;
+	    if (diagLevel_ > 1)
+	      {
+			std::cout<<"[FastCaloCrystalHitFromHit] created hit in crystal id="<<crystalId<<"\t with time="<<time<<"\t eDep="<<eDep<<"\t  from "<<nRoid<<" RO"<<std::endl;
 
-        
-      }
-  }
+		
+	      }
+  	}	
 
 
 }
