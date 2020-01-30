312ef3131e9ec4a8714db13cf5c03b165e63ab80
sophiemiddleton <sophie@fnal.gov>
2020-01-29 22:06:37 -0600

    add in hists to analysis and fixed minEnergy fcl param

--- CaloCluster/src/CaloClusterCompare_module.cc
+++ CaloCluster/src/CaloClusterCompare_module.cc
@@ -1,6 +1,7 @@
 //Author: S Middleton 
 //Date: Jan 2020
-//Purpose: To help ana;yse out put of different clustering modules
+//Purpose: To help ana;yse out put of different clustering modules -> run scripts thru: 
+//	"valCompare -w validationCalo/ce.html reference.root new_file.root"
 
 //DataProducts:
 #include "RecoDataProducts/inc/CaloClusterCollection.hh"
@@ -48,6 +49,13 @@ namespace mu2e
 		const CaloClusterCollection* _clustercol;
 	      	TH1F* _Energy;
 		TH1F* _Time;
+		TH1F* _DiskId;
+           	TH1F* _EnergyErr;
+		TH1F* _TimeErr;
+	   	TH1F* _Angle;
+	        TH1F* _PosX;  
+	    	TH1F* _PosY;
+		TH1F* _PosZ; 
 		bool findData(const art::Event& evt);
  	};
 
@@ -61,9 +69,23 @@ namespace mu2e
 	
 	void CaloClusterCompare::beginJob() {
       		art::ServiceHandle<art::TFileService> tfs;
-		_Energy= tfs->make<TH1F>("Energy Dep [MeV]","EDep " ,50,0, 110);
+		_Energy= tfs->make<TH1F>("Energy Dep [MeV]","EDep" ,50,0, 110);
 		_Energy->GetXaxis()->SetTitle("EDep");
-		_Energy->SetStats();
+		_Time= tfs->make<TH1F>("Time","Time" ,50,500,1700);
+		_Time->GetXaxis()->SetTitle("Time");
+		_EnergyErr= tfs->make<TH1F>("Energy Dep Err[MeV]","EDep Err" ,50,0, 110);
+		_EnergyErr->GetXaxis()->SetTitle("EDepErr");
+		_TimeErr= tfs->make<TH1F>("TimeErr","TimeErr" ,50,500,1700);
+		_TimeErr->GetXaxis()->SetTitle("TimeErr");
+		_Angle= tfs->make<TH1F>("Angle","Angle " ,50,-3.1415, 3.1415);
+		_Angle->GetXaxis()->SetTitle("Angle");
+		_PosX= tfs->make<TH1F>("PosX","Pos X" ,100,1000,1000);
+		_PosX->GetXaxis()->SetTitle("X Pos");
+		_PosY= tfs->make<TH1F>("PosY","PosY " ,100,1000,1000);
+		_PosY->GetXaxis()->SetTitle("Y Pos");
+		_PosZ= tfs->make<TH1F>("PosZ","Pos Z" ,100,1000,1000);
+		_PosZ->GetXaxis()->SetTitle("Z Pos");
+		
 	}
 
 
@@ -76,6 +98,13 @@ namespace mu2e
 		for(size_t ist = 0;ist < _clustercol->size(); ++ist){
 		CaloCluster sts =(*_clustercol)[ist];
 		_Energy->Fill(sts.energyDep());
+		_Time->Fill(sts.time());
+		_TimeErr->Fill(sts.timeErr());
+		_EnergyErr->Fill(sts.energyDepErr());
+		_Angle->Fill(sts.angle());
+		_PosX->Fill(sts.cog3Vector().x());
+		_PosY->Fill(sts.cog3Vector().y());
+		_PosZ->Fill(sts.cog3Vector().z());
 		}
 	}
 	bool CaloClusterCompare::findData(const art::Event& evt){
