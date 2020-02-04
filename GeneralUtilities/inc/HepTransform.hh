9ce9ce407799ccd3eaadec9b2bd289d2fa582307
Ryunosuke O'Neil <roneil@fnal.gov>
2020-02-04 00:15:29 -0600

    revert HepTransform.hh and AlignedTrackerMaker, reapply my fixes

--- GeneralUtilities/inc/HepTransform.hh
+++ GeneralUtilities/inc/HepTransform.hh
@@ -9,70 +9,44 @@
 
 #include <ostream>
 
-namespace mu2e
-{
+namespace mu2e {
 
-    class HepTransform
-    {
-    public:
-        HepTransform() : _displacement(0, 0, 0),
-                         _rotation(CLHEP::HepRotation::IDENTITY) {}
+  class HepTransform {
+  public:
+    HepTransform():_displacement(0,0,0),
+		   _rotation(CLHEP::HepRotation::IDENTITY) {}
+    HepTransform( CLHEP::Hep3Vector & disp, CLHEP::HepRotation & rot ) :
+      _displacement(disp),
+      _rotation(rot) {}
+    HepTransform( double dx, double dy, double dz,  
+		  double rx, double ry, double rz):
+      _displacement(dx,dy,dz) {
+      _rotation.rotateX(rx);
+      _rotation.rotateY(ry);
+      _rotation.rotateZ(rz);
+    }
 
+    CLHEP::Hep3Vector  displacement() const { return _displacement;}
+    CLHEP::HepRotation rotation()     const { return _rotation;    }
 
-        HepTransform(CLHEP::Hep3Vector const& disp, CLHEP::HepRotation const& rot) :
-                _displacement(disp),
-                _rotation(rot) {}
+    void  setDisplacement ( CLHEP::Hep3Vector & aDisp ) { 
+                                   _displacement = aDisp;}
+    void  setRotation ( CLHEP::HepRotation & aRot ) { _rotation = aRot; }
 
-        HepTransform(double dx, double dy, double dz,
-                     double rx, double ry, double rz) :
-                _displacement(dx, dy, dz)
-        {
-            _rotation.rotateX(rx);
-            _rotation.rotateY(ry);
-            _rotation.rotateZ(rz);
-        }
+    // combine HepTransform
+    HepTransform& operator*=(HepTransform const& b);
+    friend HepTransform operator*(HepTransform const& a, HepTransform const& b);
 
-        HepTransform(HepTransform const& other) :
-            _displacement(other._displacement), _rotation(other._rotation) { }
+    // apply to a vector
+    friend CLHEP::Hep3Vector operator*(HepTransform const& a, 
+				CLHEP::Hep3Vector const& v);
 
+  private:
+    CLHEP::Hep3Vector  _displacement;
+    CLHEP::HepRotation _rotation;
+  };
 
-        CLHEP::Hep3Vector displacement() const { return _displacement; }
-
-        CLHEP::HepRotation rotation() const { return _rotation; }
-
-        void setDisplacement(CLHEP::Hep3Vector &aDisp)
-        {
-            _displacement = aDisp;
-        }
-
-        void setRotation(CLHEP::HepRotation &aRot) { _rotation = aRot; }
-
-        HepTransform &operator=(HepTransform const &other)
-        {
-            if(&other == this)
-                return *this;
-
-            _displacement = other._displacement;
-            _rotation = other._rotation;
-
-            return *this;
-        }
-
-        // combine HepTransform
-        HepTransform &operator*=(HepTransform const &b);
-
-        friend HepTransform operator*(HepTransform const &a, HepTransform const &b);
-
-        // apply to a vector
-        friend CLHEP::Hep3Vector operator*(HepTransform const &a,
-                                           CLHEP::Hep3Vector const &v);
-
-    private:
-        CLHEP::Hep3Vector _displacement;
-        CLHEP::HepRotation _rotation;
-    };
-
-    std::ostream &operator<<(std::ostream &os, const HepTransform &rhs);
+  std::ostream& operator<<(std::ostream& os, const HepTransform& rhs );
 
 } // end of namespace mu2e
 
