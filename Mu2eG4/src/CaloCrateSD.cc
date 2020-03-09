cc14fc964281ae6d68456b1e98c48f3838313cef
Krzysztof L Genser <genser@fnal.gov>
2020-03-09 16:35:05 -0500

    whitespace etc change

--- Mu2eG4/src/CaloCrateSD.cc
+++ Mu2eG4/src/CaloCrateSD.cc
@@ -1,10 +1,6 @@
 //
 // Define a sensitive detector for calorimetric readout
 //
-// $Id: CaloCrateSD.cc,v 1.22 2014/08/01 20:57:45 echenard Exp $
-// $Author: echenard $
-// $Date: 2014/08/01 20:57:45 $
-//
 // Original author Ivan Logashenko
 //
 
@@ -28,52 +24,49 @@
 #include "G4Step.hh"
 #include "G4ios.hh"
 
-
-
 namespace mu2e {
 
-    CaloCrateSD::CaloCrateSD(G4String name, SimpleConfig const & config ):
-      Mu2eSensitiveDetector(name,config)
-    {}
+  CaloCrateSD::CaloCrateSD(G4String name, SimpleConfig const & config ):
+    Mu2eSensitiveDetector(name,config)
+  {}
+
+  G4bool CaloCrateSD::ProcessHits(G4Step* aStep,G4TouchableHistory*)
+  {
+
+    //if( aStep->GetTotalEnergyDeposit() < 1e-6 ) return false;
+
+    _currentSize += 1;
+    if( _sizeLimit>0 && _currentSize>_sizeLimit && (_currentSize - _sizeLimit)==1)
+      {
+        mf::LogWarning("G4") << "Maximum number of particles reached in "
+                             << SensitiveDetectorName<< ": "<< _currentSize <<std::endl;
+        return false;
+      }
 
 
+    ProcessCode endCode(_processInfo->findAndCount(Mu2eG4UserHelpers::findStepStoppingProcessName(aStep)));
 
-    G4bool CaloCrateSD::ProcessHits(G4Step* aStep,G4TouchableHistory*)
-    {
+    const G4TouchableHandle & touchableHandle = aStep->GetPreStepPoint()->GetTouchableHandle();
+    int idro = touchableHandle->GetCopyNumber(1);
 
-         //if( aStep->GetTotalEnergyDeposit() < 1e-6 ) return false;
+    //uncomment for diagnosis purposes only when playing with the geometry
+    //for (int i=0;i<=touchableHandle->GetHistoryDepth();++i) std::cout<<"Calo Crate Transform level "<<i<<"   "<<touchableHandle->GetCopyNumber(i)
+    //<<"  "<<touchableHandle->GetSolid(i)->GetName()<<"   "<<touchableHandle->GetVolume(i)->GetName()<<std::endl;
 
-	 _currentSize += 1;
-	 if( _sizeLimit>0 && _currentSize>_sizeLimit && (_currentSize - _sizeLimit)==1) 
-	 {
-             mf::LogWarning("G4") << "Maximum number of particles reached in " << SensitiveDetectorName<< ": "<< _currentSize <<std::endl;
-             return false;
-	 }
+    _collection->push_back(StepPointMC(_spHelper->particlePtr(aStep->GetTrack()),
+                                       idro,
+                                       aStep->GetTotalEnergyDeposit(),
+                                       aStep->GetNonIonizingEnergyDeposit(),
+                                       aStep->GetPreStepPoint()->GetGlobalTime(),
+                                       aStep->GetPreStepPoint()->GetProperTime(),
+                                       aStep->GetPreStepPoint()->GetPosition() - _mu2eOrigin,
+                                       aStep->GetPreStepPoint()->GetMomentum(),
+                                       aStep->GetStepLength(),
+                                       endCode
+                                       ) );
 
-
-	 ProcessCode endCode(_processInfo->findAndCount(Mu2eG4UserHelpers::findStepStoppingProcessName(aStep)));
-
-	 const G4TouchableHandle & touchableHandle = aStep->GetPreStepPoint()->GetTouchableHandle();
-	 int idro = touchableHandle->GetCopyNumber(1);  
-
-	 //uncomment for diagnosis purposes only when playing with the geometry
- 	 //for (int i=0;i<=touchableHandle->GetHistoryDepth();++i) std::cout<<"Calo Crate Transform level "<<i<<"   "<<touchableHandle->GetCopyNumber(i)
-	 //<<"  "<<touchableHandle->GetSolid(i)->GetName()<<"   "<<touchableHandle->GetVolume(i)->GetName()<<std::endl;
-
-	 _collection->push_back(StepPointMC(_spHelper->particlePtr(aStep->GetTrack()),
-                                	    idro,
-                                	    aStep->GetTotalEnergyDeposit(),
-                                	    aStep->GetNonIonizingEnergyDeposit(),
-                                	    aStep->GetPreStepPoint()->GetGlobalTime(),
-                                	    aStep->GetPreStepPoint()->GetProperTime(),
-                                	    aStep->GetPreStepPoint()->GetPosition() - _mu2eOrigin,
-                                	    aStep->GetPreStepPoint()->GetMomentum(),
-                                	    aStep->GetStepLength(),
-                                	    endCode
-                                	    ) );
-
-	 return true;
-    }
+    return true;
+  }
 
 
-} 
+}
--- Mu2eG4/src/CaloCrystalSD.cc
+++ Mu2eG4/src/CaloCrystalSD.cc
@@ -1,10 +1,6 @@
 //
 // Define a sensitive detector for CaloCrystal Detectors
 //
-// $Id: CaloCrystalSD.cc,v 1.25 2013/08/28 05:58:17 gandr Exp $
-// $Author: gandr $
-// $Date: 2013/08/28 05:58:17 $
-//
 // Original author Ivan Logashenko
 //
 
@@ -29,55 +25,55 @@
 
 namespace mu2e {
 
-    CaloCrystalSD::CaloCrystalSD(G4String name, SimpleConfig const & config ): 
-      Mu2eSensitiveDetector(name,config)
-    { }
+  CaloCrystalSD::CaloCrystalSD(G4String name, SimpleConfig const & config ):
+    Mu2eSensitiveDetector(name,config)
+  { }
 
-    G4bool CaloCrystalSD::ProcessHits(G4Step* aStep,G4TouchableHistory*)
-    {
-        G4double edep = aStep->GetTotalEnergyDeposit();
-	if (edep < 1e-6) return false;
-
-
-	_currentSize += 1;
-	if( _sizeLimit>0 && _currentSize>_sizeLimit && (_currentSize - _sizeLimit)==1) 
-	{
-            mf::LogWarning("G4") << "Maximum number of particles reached in " << SensitiveDetectorName<< ": "<< _currentSize <<std::endl;
-            return false;
-	}
+  G4bool CaloCrystalSD::ProcessHits(G4Step* aStep,G4TouchableHistory*)
+  {
+    G4double edep = aStep->GetTotalEnergyDeposit();
+    if (edep < 1e-6) return false;
 
 
-	ProcessCode endCode(_processInfo->findAndCount(Mu2eG4UserHelpers::findStepStoppingProcessName(aStep)));
-
-	const G4TouchableHandle & touchableHandle = aStep->GetPreStepPoint()->GetTouchableHandle();
-
-	G4int copyNo = touchableHandle->GetCopyNumber(1);  // Make sure to get the right copy number level here
-
-	G4AffineTransform const& toLocal = touchableHandle->GetHistory()->GetTopTransform();
-	G4ThreeVector posWorld           = aStep->GetPreStepPoint()->GetPosition();
-	G4ThreeVector posLocal           = toLocal.TransformPoint(posWorld);
+    _currentSize += 1;
+    if( _sizeLimit>0 && _currentSize>_sizeLimit && (_currentSize - _sizeLimit)==1)
+      {
+        mf::LogWarning("G4") << "Maximum number of particles reached in "
+                             << SensitiveDetectorName<< ": "<< _currentSize <<std::endl;
+        return false;
+      }
 
 
-	// diagnosis purposes only when playing with the geometry, uncomment next two line
-	//for (int i=0;i<=touchableHandle->GetHistoryDepth();++i) std::cout<<"Cry Transform level "<<i<<"   "<<touchableHandle->GetCopyNumber(i)
-        //<<"   "<<touchableHandle->GetHistory()->GetTransform(touchableHandle->GetHistoryDepth()-i).TransformPoint(posWorld)
-	//<<"  "<<touchableHandle->GetSolid(i)->GetName()<<"   "<<touchableHandle->GetVolume(i)->GetName()<<std::endl;
+    ProcessCode endCode(_processInfo->findAndCount(Mu2eG4UserHelpers::findStepStoppingProcessName(aStep)));
+
+    const G4TouchableHandle & touchableHandle = aStep->GetPreStepPoint()->GetTouchableHandle();
+
+    G4int copyNo = touchableHandle->GetCopyNumber(1);  // Make sure to get the right copy number level here
+
+    G4AffineTransform const& toLocal = touchableHandle->GetHistory()->GetTopTransform();
+    G4ThreeVector posWorld           = aStep->GetPreStepPoint()->GetPosition();
+    G4ThreeVector posLocal           = toLocal.TransformPoint(posWorld);
 
 
-	_collection->push_back(StepPointMC(_spHelper->particlePtr(aStep->GetTrack()),
-                        		   copyNo,
-                        		   edep,
-                        		   aStep->GetNonIonizingEnergyDeposit(),
-                        		   aStep->GetPreStepPoint()->GetGlobalTime(),
-                        		   aStep->GetPreStepPoint()->GetProperTime(),
-                        		   aStep->GetPreStepPoint()->GetPosition() - _mu2eOrigin,
-                        		   aStep->GetPreStepPoint()->GetMomentum(),
-                        		   aStep->GetStepLength(),
-                        		   endCode
-                        		   ));
+    // diagnosis purposes only when playing with the geometry, uncomment next two line
+    //for (int i=0;i<=touchableHandle->GetHistoryDepth();++i) std::cout<<"Cry Transform level "<<i<<"   "<<touchableHandle->GetCopyNumber(i)
+    //<<"   "<<touchableHandle->GetHistory()->GetTransform(touchableHandle->GetHistoryDepth()-i).TransformPoint(posWorld)
+    //<<"  "<<touchableHandle->GetSolid(i)->GetName()<<"   "<<touchableHandle->GetVolume(i)->GetName()<<std::endl;
 
-	return true;
+    _collection->push_back(StepPointMC(_spHelper->particlePtr(aStep->GetTrack()),
+                                       copyNo,
+                                       edep,
+                                       aStep->GetNonIonizingEnergyDeposit(),
+                                       aStep->GetPreStepPoint()->GetGlobalTime(),
+                                       aStep->GetPreStepPoint()->GetProperTime(),
+                                       aStep->GetPreStepPoint()->GetPosition() - _mu2eOrigin,
+                                       aStep->GetPreStepPoint()->GetMomentum(),
+                                       aStep->GetStepLength(),
+                                       endCode
+                                       ));
 
-    }
+    return true;
 
-} 
+  }
+
+}
