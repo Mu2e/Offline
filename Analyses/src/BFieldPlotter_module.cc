89f37fa63a81fbb17b389fb01d0be9581f4dc34b
michaelmackenzie <michaelmackenzie@u.northwestern.edu>
2020-03-10 14:59:21 -0500

    Adding a module to plot the magnetic field maps

---
+++ Analyses/src/BFieldPlotter_module.cc
@@ -0,0 +1,173 @@
+//
+// Create a two dimensional histogram of the magnetic field
+//
+// Original author Michael MacKenzie (March 2020)
+//
+
+// Mu2e includes
+#include "BFieldGeom/inc/BFieldManager.hh"
+#include "GeometryService/inc/GeomHandle.hh"
+
+// art includes
+#include "art/Framework/Core/EDAnalyzer.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art_root_io/TFileService.h"
+
+// ROOT includes
+#include "TFile.h"
+#include "TH1F.h"
+#include "TH2.h"
+
+// cetlib includes
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+namespace mu2e {
+
+  class BFieldPlotter : public art::EDAnalyzer {
+  public:
+    struct Config {
+      using Name=fhicl::Name;
+      using Comment=fhicl::Comment;
+      fhicl::Atom<std::string> plane     {Name("plane"     ), Comment("Axis plane is defined by (x, y, or z)")};
+      fhicl::Atom<double>      planeValue{Name("planeValue"), Comment("Value on the axis the plane intersects (mm)")};
+      fhicl::Atom<double>      axisOneMin{Name("axisOneMin"), Comment("Axis one lower edge for plotting (mm) (plane = x/y/z --> axis one = y/x/x)")};
+      fhicl::Atom<double>      axisOneMax{Name("axisOneMax"), Comment("Axis one upper edge for plotting (mm) (plane = x/y/z --> axis one = y/x/x)")};
+      fhicl::Atom<double>      axisTwoMin{Name("axisTwoMin"), Comment("Axis two lower edge for plotting (mm) (plane = x/y/z --> axis two = z/z/y)")};
+      fhicl::Atom<double>      axisTwoMax{Name("axisTwoMax"), Comment("Axis two upper edge for plotting (mm) (plane = x/y/z --> axis two = z/z/y)")};
+      fhicl::Atom<double>      mapBinSize{Name("mapBinSize"), Comment("Map bin size (mm)"), 10.};
+    };
+    typedef art::EDAnalyzer::Table<Config> Parameters;
+
+    explicit BFieldPlotter(const Parameters& pset);
+    virtual ~BFieldPlotter() { }
+
+    void analyze(const art::Event& e);
+
+    void fillHistogram(std::shared_ptr<BFMap> map, art::ServiceHandle<art::TFileService> tfs);
+
+    void endJob();
+
+  private:
+
+    std::string _plane     ; // x, y, or z map plane
+    double      _planeValue; // value to set the plane at (mm)
+    double      _axisOneMin; // sampling axis values (mm)
+    double      _axisOneMax; // sampling axis values (mm)
+    double      _axisTwoMin; // sampling axis values (mm)
+    double      _axisTwoMax; // sampling axis values (mm)
+    double      _mapBinSize; // histogram bin size (mm)
+
+    std::map<std::string,TH2F*> _hMap; //histogram of the map
+    
+  };
+
+  BFieldPlotter::BFieldPlotter(const Parameters& pset) :
+    art::EDAnalyzer(pset)
+    , _plane     (pset().plane     ())
+    , _planeValue(pset().planeValue())
+    , _axisOneMin(pset().axisOneMin())
+    , _axisOneMax(pset().axisOneMax())
+    , _axisTwoMin(pset().axisTwoMin())
+    , _axisTwoMax(pset().axisTwoMax())
+    , _mapBinSize(pset().mapBinSize())
+  {
+    if(_axisOneMin >= _axisOneMax || _axisTwoMin >= _axisTwoMax) {
+      throw cet::exception("BADCONFIG") << "BField mapping plane ill defined: "
+					<< _axisOneMin << " < axisOneValues < " << _axisOneMax << ", "
+					<< _axisTwoMin << " < axisTwoValues < " << _axisTwoMax;
+    }
+    if(_mapBinSize <= 0.) {
+      throw cet::exception("BADCONFIG") << "BField map binning should be >= 0 but given as " << _mapBinSize;
+    }
+
+    if(!(_plane == "x" || _plane == "y" || _plane == "z")) {
+      throw cet::exception("BADCONFIG") << "BField map plane not recognized! Options are x, y, or z but given " 
+					<< _plane.c_str();
+    }
+
+  } //end constructor
+
+  void BFieldPlotter::analyze(const art::Event& event) {
+    art::ServiceHandle<art::TFileService> tfs;
+    // Get the magnetic field manager and the map lists
+    GeomHandle<BFieldManager> bf;
+    const BFieldManager::MapContainerType innerMaps = bf->getInnerMaps();
+    const BFieldManager::MapContainerType outerMaps = bf->getOuterMaps();
+ 
+    //plot inner maps
+    for(std::shared_ptr<BFMap> map : innerMaps) { 
+      fillHistogram(map, tfs);
+    } 
+    
+    //plot outer maps
+    for(std::shared_ptr<BFMap> map : outerMaps) { 
+      fillHistogram(map, tfs);
+    } 
+
+    //plot the default field returned from the manager
+    fillHistogram(NULL, tfs);
+  } // end analyze
+
+
+  //fill a histogram with the magnetic field
+  void BFieldPlotter::fillHistogram(std::shared_ptr<BFMap> map, art::ServiceHandle<art::TFileService> tfs) {
+    //define histogram binning
+    int nbinsOne = (_axisOneMax - _axisOneMin)/_mapBinSize + 1;
+    int nbinsTwo = (_axisTwoMax - _axisTwoMin)/_mapBinSize + 1;
+    
+    //if a null map, plot the default field from the manager
+    const std::string name = (map) ? map->getKey() : "default";
+
+    if(_hMap[name]) return; //already filled the maps in a previous event
+
+    //make a directory for the map
+    art::TFileDirectory tfdir = tfs->mkdir( ("BFieldMapper_"+name).c_str() );
+
+    //define a histogram
+    _hMap[name] = tfdir.make<TH2F>(("hMap"+name).c_str()  , (name + " Magnetic field map").c_str(), 
+				   // add offsets so all values are bin centers and fit edge into the map
+				   nbinsOne, _axisOneMin - _mapBinSize/2.,_axisOneMax + _mapBinSize/2.,
+				   nbinsTwo, _axisTwoMin - _mapBinSize/2.,_axisTwoMax + _mapBinSize/2.);
+    
+    GeomHandle<BFieldManager> bf; //only needed in null map case
+    //Loop through the points in the magnetic field
+    double axisOne = _axisOneMin;
+    while(axisOne <= _axisOneMax) {
+      double axisTwo = _axisTwoMin;
+      while(axisTwo <= _axisTwoMax) {
+	CLHEP::Hep3Vector point;
+	CLHEP::Hep3Vector field;
+	if(_plane == "x") 
+	  point = CLHEP::Hep3Vector(_planeValue, axisOne, axisTwo);
+	else if(_plane == "y") 
+	  point = CLHEP::Hep3Vector(axisOne, _planeValue, axisTwo);
+	else if(_plane == "z") 
+	  point = CLHEP::Hep3Vector(axisOne, axisTwo, _planeValue);
+	if(map)
+	  map->getBFieldWithStatus(point,field);
+	else //if null, get the manager and plot the field returned by it
+	  bf->getBFieldWithStatus(point,field);
+
+	_hMap[name]->Fill(axisOne, axisTwo, field.mag()); //fill with weight of the field magnitude
+	axisTwo += _mapBinSize;
+      } //end axis two loop
+      axisOne += _mapBinSize;
+    } //end axis one loop
+  } //end fillHistorgram
+
+  void BFieldPlotter::endJob() {
+
+  } // end endJob
+
+
+}  // end namespace mu2e
+
+using mu2e::BFieldPlotter;
+DEFINE_ART_MODULE(BFieldPlotter);
