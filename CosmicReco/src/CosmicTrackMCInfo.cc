03b54b88c3b77b28f692003c5f7eac5d1d1e87db
sophiemiddleton <sophie@fnal.gov>
2020-03-14 14:13:17 -0500

    changed initializing to header for a MCInfo

--- CosmicReco/src/CosmicTrackMCInfo.cc
+++ CosmicReco/src/CosmicTrackMCInfo.cc
@@ -1,22 +1,13 @@
 #include "CosmicReco/inc/CosmicTrackMCInfo.hh"
 
 CosmicTrackMCInfo::CosmicTrackMCInfo(){
-     TrueTheta =0;
-     TruePhi =0;
      
-     TruePhiSIM = 0;
-     TrueThetaSIM = 0;
-     TrueMomentum = 0;
-
      TrueFitEquation.Pos.SetXYZ(0,0,0);
      TrueFitEquation.Dir.SetXYZ(0,0,0);
      TrueTrackCoordSystem._XDoublePrime.SetXYZ(0,0,0);
      TrueTrackCoordSystem._YDoublePrime.SetXYZ(0,0,0);
      TrueTrackCoordSystem._ZPrime.SetXYZ(0,0,0);
-     RawTrueParams.A0 = 0.;
-     RawTrueParams.A1 = 0.;
-     RawTrueParams.B0 =0;
-     RawTrueParams.B1 = 0.;
+     
 	
 } 
 
