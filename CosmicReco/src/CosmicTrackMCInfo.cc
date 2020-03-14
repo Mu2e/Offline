fd6a9d97804188e7f7256de81aa0dfdd43d28ba5
sophiemiddleton <sophie@fnal.gov>
2020-03-14 14:17:41 -0500

    accidentally deleted init

--- CosmicReco/src/CosmicTrackMCInfo.cc
+++ CosmicReco/src/CosmicTrackMCInfo.cc
@@ -7,7 +7,10 @@ CosmicTrackMCInfo::CosmicTrackMCInfo(){
      TrueTrackCoordSystem._XDoublePrime.SetXYZ(0,0,0);
      TrueTrackCoordSystem._YDoublePrime.SetXYZ(0,0,0);
      TrueTrackCoordSystem._ZPrime.SetXYZ(0,0,0);
-     
+     RawTrueParams.A0 = 0.;
+     RawTrueParams.A1 = 0.;
+     RawTrueParams.B0 =0;
+     RawTrueParams.B1 = 0.;
 	
 } 
 
