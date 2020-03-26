b109159d07615764ca40a0d233d5bb1ad77e77ad
Andrew Edmonds <aedmonds@lbl.gov>
2020-03-26 16:20:42 -0500

    This now compiles and runs

--- TrkDiag/src/TrackQuality_module.cc
+++ TrkDiag/src/TrackQuality_module.cc
@@ -61,9 +61,9 @@ namespace mu2e
 
     //    mu2e::DbHandle<mu2e::TrkQualDb> _trkQualDb;
     mu2e::ProditionsHandle<mu2e::TrkQualCatalog> _trkQualCatalogH;
-    std::string _currentXmlFile;
-    MVATools* _trkqualmva;
-    MVAMask _mvamask;
+    //    std::string _currentXmlFile;
+    //    MVATools* _trkqualmva;
+    //    MVAMask _mvamask;
     std::map<Float_t, Float_t> _effCalib;
 
     InfoStructHelper _infoStructHelper;
@@ -79,32 +79,6 @@ namespace mu2e
     produces<RecoQualCollection>();
   }
 
-  void TrackQuality::initializeMVA(std::string xmlfilename) {
-    // Initialize MVA
-    _trkqualmva = new MVATools(xmlfilename);
-    _trkqualmva->initMVA();
-
-    // create the MVA mask in case we have removed variables
-    const auto& labels = _trkqualmva->labels();
-    _mvamask = 0;
-    for (int i_var = 0; i_var < TrkQual::n_vars; ++i_var) {
-      for (const auto& i_label : labels) {
-	std::string i_varName = TrkQual::varName(static_cast<TrkQual::MVA_varindex>(i_var));
-	if (i_label.find(i_varName) != std::string::npos) {
-	  _mvamask ^= (1 << i_var);
-	  break;
-	}
-      }
-    }
-
-    // print some extra info, if requested
-    if(_printMVA) {
-      _trkqualmva->showMVA();
-    }
-
-    _currentXmlFile = xmlfilename; // store which file we've just initialized
-  }
-
   void TrackQuality::produce(art::Event& event ) {
     // create output
     unique_ptr<TrkQualCollection> tqcol(new TrkQualCollection());
@@ -118,108 +92,112 @@ namespace mu2e
     TrkQualCatalog const& trkQualCatalog = _trkQualCatalogH.get(event.id());
     for (const auto& i_trkQualEntry : trkQualCatalog.entries()) {
       if (i_trkQualEntry._trainName == _trainName) {
-	_trkqualmva = i_trkQualEntry._mvaTool;
-      }
-    }
 
-    // // get the XML filename for this TrkQual training
-    // auto const& trkQualTable = _trkQualDb.get(event.id());
-    // for (const auto& i_row : trkQualTable.rows()) {
-    //   if (i_row.mvaname() == _trainName) { // check the training name
-    // 	std::string xmlfilename = i_row.xmlfilename();
-    // 	if (xmlfilename != _currentXmlFile) { // only reinitialize if the XML file has changed
-    // 	  initializeMVA(xmlfilename);
-
-    // 	  if (i_row.calibrated() == 1) {
-    // 	    _effCalib = trkQualTable.getCalib(i_row.idx()); // get the calibration if it exists
-    // 	  }
-    // 	}
-    // 	break;
-    //   }
-    // }
-    // if(!_trkqualmva) {
-    //   throw cet::exception("TrackQuality") << "_trkqualmva not initialized properly" << std::endl;
-    // }
-
-    // Go through the tracks and calculate their track qualities
-    for (const auto& i_kalSeed : kalSeeds) {
-      TrkQual trkqual;
-
-      static TrkFitFlag goodfit(TrkFitFlag::kalmanOK);
-      if (i_kalSeed.status().hasAllProperties(goodfit)) {
-
-	// fill the hit count variables
-	TrkInfo tinfo;
-	_infoStructHelper.fillTrkInfoHits(i_kalSeed, tinfo);
-	_infoStructHelper.fillTrkInfoStraws(i_kalSeed, tinfo);
-	trkqual[TrkQual::nactive] = tinfo._nactive;
-	trkqual[TrkQual::factive] = (double) tinfo._nactive / tinfo._nhits;
-	trkqual[TrkQual::fdouble] = (double) tinfo._ndactive / tinfo._nactive;
-	trkqual[TrkQual::fnullambig] = (double) tinfo._nnullambig / tinfo._nactive;
-	trkqual[TrkQual::fstraws] = (double)tinfo._nmatactive / tinfo._nactive;
-
-	// fill fit consistency and t0 variables
-	if (i_kalSeed.fitConsistency() > FLT_MIN) {
-	  trkqual[TrkQual::log10fitcon] = log10(i_kalSeed.fitConsistency());
+	if(_printMVA) {
+	  i_trkQualEntry._mvaTool->showMVA();
 	}
-	else {
-	  trkqual[TrkQual::log10fitcon] = -50.0;
-	}
-	trkqual[TrkQual::t0err] = i_kalSeed.t0().t0Err();
+
+
+	// // get the XML filename for this TrkQual training
+	// auto const& trkQualTable = _trkQualDb.get(event.id());
+	// for (const auto& i_row : trkQualTable.rows()) {
+	//   if (i_row.mvaname() == _trainName) { // check the training name
+	// 	std::string xmlfilename = i_row.xmlfilename();
+	// 	if (xmlfilename != _currentXmlFile) { // only reinitialize if the XML file has changed
+	// 	  initializeMVA(xmlfilename);
+
+	// 	  if (i_row.calibrated() == 1) {
+	// 	    _effCalib = trkQualTable.getCalib(i_row.idx()); // get the calibration if it exists
+	// 	  }
+	// 	}
+	// 	break;
+	//   }
+	// }
+	// if(!i_trkQualEntry._mvaTool) {
+	//   throw cet::exception("TrackQuality") << "i_trkQualEntry._mvaTool not initialized properly" << std::endl;
+	// }
+
+	// Go through the tracks and calculate their track qualities
+	for (const auto& i_kalSeed : kalSeeds) {
+	  TrkQual trkqual;
+
+	  static TrkFitFlag goodfit(TrkFitFlag::kalmanOK);
+	  if (i_kalSeed.status().hasAllProperties(goodfit)) {
+
+	    // fill the hit count variables
+	    TrkInfo tinfo;
+	    _infoStructHelper.fillTrkInfoHits(i_kalSeed, tinfo);
+	    _infoStructHelper.fillTrkInfoStraws(i_kalSeed, tinfo);
+	    trkqual[TrkQual::nactive] = tinfo._nactive;
+	    trkqual[TrkQual::factive] = (double) tinfo._nactive / tinfo._nhits;
+	    trkqual[TrkQual::fdouble] = (double) tinfo._ndactive / tinfo._nactive;
+	    trkqual[TrkQual::fnullambig] = (double) tinfo._nnullambig / tinfo._nactive;
+	    trkqual[TrkQual::fstraws] = (double)tinfo._nmatactive / tinfo._nactive;
+
+	    // fill fit consistency and t0 variables
+	    if (i_kalSeed.fitConsistency() > FLT_MIN) {
+	      trkqual[TrkQual::log10fitcon] = log10(i_kalSeed.fitConsistency());
+	    }
+	    else {
+	      trkqual[TrkQual::log10fitcon] = -50.0;
+	    }
+	    trkqual[TrkQual::t0err] = i_kalSeed.t0().t0Err();
 	
-	// find the best KalSegment and fill variables relating to it
-	KalSegment kseg;
-	std::vector<KalSegment> const& ksegs = i_kalSeed.segments();
-	auto bestkseg = ksegs.begin();
-	double zpos = -1631.11;
-	for(auto ikseg = ksegs.begin(); ikseg != ksegs.end(); ++ikseg){
-	  HelixVal const& hel = ikseg->helix();
-	  // check for a segment whose range includes zpos.  There should be a better way of doing this, FIXME
-	  double sind = hel.tanDip()/sqrt(1.0+hel.tanDip()*hel.tanDip());
-	  if(hel.z0()+sind*ikseg->fmin() < zpos && hel.z0()+sind*ikseg->fmax() > zpos){
-	    bestkseg = ikseg;
-	    break;
-	  }
-	}
-	kseg = *bestkseg;
-	if (bestkseg != ksegs.end()) {
-	  double charge = i_kalSeed.particle().charge();
+	    // find the best KalSegment and fill variables relating to it
+	    KalSegment kseg;
+	    std::vector<KalSegment> const& ksegs = i_kalSeed.segments();
+	    auto bestkseg = ksegs.begin();
+	    double zpos = -1631.11;
+	    for(auto ikseg = ksegs.begin(); ikseg != ksegs.end(); ++ikseg){
+	      HelixVal const& hel = ikseg->helix();
+	      // check for a segment whose range includes zpos.  There should be a better way of doing this, FIXME
+	      double sind = hel.tanDip()/sqrt(1.0+hel.tanDip()*hel.tanDip());
+	      if(hel.z0()+sind*ikseg->fmin() < zpos && hel.z0()+sind*ikseg->fmax() > zpos){
+		bestkseg = ikseg;
+		break;
+	      }
+	    }
+	    kseg = *bestkseg;
+	    if (bestkseg != ksegs.end()) {
+	      double charge = i_kalSeed.particle().charge();
 	  
-	  trkqual[TrkQual::momerr] = bestkseg->momerr();
-	  trkqual[TrkQual::d0] = -1*charge*bestkseg->helix().d0();
-	  trkqual[TrkQual::rmax] = -1*charge*(bestkseg->helix().d0() + 2.0/bestkseg->helix().omega());
+	      trkqual[TrkQual::momerr] = bestkseg->momerr();
+	      trkqual[TrkQual::d0] = -1*charge*bestkseg->helix().d0();
+	      trkqual[TrkQual::rmax] = -1*charge*(bestkseg->helix().d0() + 2.0/bestkseg->helix().omega());
 	  
-	  trkqual.setMVAStatus(MVAStatus::calculated);
-	  trkqual.setMVAValue(_trkqualmva->evalMVA(trkqual.values(), _mvamask));
+	      trkqual.setMVAStatus(MVAStatus::calculated);
+	      trkqual.setMVAValue(i_trkQualEntry._mvaTool->evalMVA(trkqual.values(), i_trkQualEntry._mvaMask));
 
+	    }
+	    else {
+	      trkqual.setMVAStatus(MVAStatus::filled);
+	    }
+	  }
+	  tqcol->push_back(trkqual);
+
+	  // Get the efficiency cut that this track passes
+	  Float_t passEff = -1;
+	  if (_effCalib.size() > 0) {
+	    for (const auto& i_pair : _effCalib) {
+	      if (trkqual.MVAValue() >= i_pair.second) {
+		passEff = i_pair.first;
+		break;
+	      }
+	    }
+	  }
+	  rqcol->push_back(RecoQual(trkqual.status(),trkqual.MVAValue(), passEff));
 	}
-	else {
-	  trkqual.setMVAStatus(MVAStatus::filled);
+
+	if ( (tqcol->size() != rqcol->size()) || (tqcol->size() != kalSeeds.size()) ) {
+	  throw cet::exception("TrackQuality") << "KalSeed, TrkQual and RecoQual sizes are inconsistent (" << kalSeeds.size() << ", " << tqcol->size() << ", " << rqcol->size() << " respectively)";
 	}
       }
-      tqcol->push_back(trkqual);
-
-      // Get the efficiency cut that this track passes
-      Float_t passEff = -1;
-      if (_effCalib.size() > 0) {
-	for (const auto& i_pair : _effCalib) {
-	  if (trkqual.MVAValue() >= i_pair.second) {
-	    passEff = i_pair.first;
-	    break;
-	  }
-	}
-      }
-      rqcol->push_back(RecoQual(trkqual.status(),trkqual.MVAValue(), passEff));
     }
-
-    if ( (tqcol->size() != rqcol->size()) || (tqcol->size() != kalSeeds.size()) ) {
-	throw cet::exception("TrackQuality") << "KalSeed, TrkQual and RecoQual sizes are inconsistent (" << kalSeeds.size() << ", " << tqcol->size() << ", " << rqcol->size() << " respectively)";
-      }
 
     // put the output products into the event
     event.put(move(tqcol));
     event.put(move(rqcol));
-  }  
+  }
 }// mu2e
 
 DEFINE_ART_MODULE(mu2e::TrackQuality);
--- TrkDiag/src/TrkQualCatalogMaker.cc
+++ TrkDiag/src/TrkQualCatalogMaker.cc
@@ -3,29 +3,53 @@
 
 namespace mu2e {
 
-  TrkQualCatalog::ptr_t TrkQualCatalogMaker::fromFcl() {
+  TrkQualCatalog::ptr_t TrkQualCatalogMaker::fillEntries() {
     TrkQualEntries trkQualEntries;
     for (const auto& i_entryConf : _config.trkQualConfigs()) {
       trkQualEntries.push_back(TrkQualEntry(i_entryConf.trainName(), i_entryConf.xmlFileName()));
     }
 
     auto ptr = std::make_shared<TrkQualCatalog>(trkQualEntries);//_config.deadTimeAnalog(), 
+
+    return ptr;
+  }
+
+  void TrkQualCatalogMaker::initializeMVAs(TrkQualCatalog::ptr_t ptr) {
+    // Now initialize the MVAs
+    for (auto& i_trkQualEntry : ptr->modifiableEntries()) {
+      i_trkQualEntry.initializeMVA();
+    }
+
+    if (_config.verbose() > 0) {
+      ptr->print(std::cout);
+    }
+  }
+
+  TrkQualCatalog::ptr_t TrkQualCatalogMaker::fromFcl() {
+
+    auto ptr = fillEntries();
+    initializeMVAs(ptr);
     return ptr;
   }
 
   TrkQualCatalog::ptr_t TrkQualCatalogMaker::fromDb(TrkQualDb::cptr_t tqDb) {
-    // initially fill from fcl to get all the constants
-    auto ptr = fromFcl();
 
-    // now overwrite with db values
-    for (auto& i_trkQualEntry : ptr->modifiableEntries()) {
-      for (const auto& i_row : tqDb->rows()) {
-	if (i_row.mvaname() == i_trkQualEntry._trainName) { // check the training name
-	  i_trkQualEntry._mvaTool = new MVATools(i_row.xmlfilename());
+    // fill the TrkQualCatalog with initial values
+    auto ptr = fillEntries();
+
+    // Change or add entries to the configuration before we create all the MVATools pointers
+    for (const auto& i_row : tqDb->rows()) {
+      for (auto& i_trkQualEntry : ptr->modifiableEntries()) {
+	if (i_row.mvaname() == i_trkQualEntry._trainName) {
+	  i_trkQualEntry._xmlFileName = i_row.xmlfilename();
+	  break; // break from inner for loop since this entry alread exists
 	}
       }
+      // if we get here, then we need to add a new entry
+      ptr->modifiableEntries().push_back(TrkQualEntry(i_row.mvaname(), i_row.xmlfilename()));
     }
 
+    initializeMVAs(ptr);
     return ptr;
   }
 
