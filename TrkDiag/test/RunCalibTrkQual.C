663aae9cdb2f7c08b4b0f2379aefb5296286aa68
Andrew Edmonds <aedmonds@lbl.gov>
2020-03-30 14:44:12 -0500

    Have the signal definition for the TrkQual calibration be a parameter that users can easily define

--- TrkDiag/test/RunCalibTrkQual.C
+++ TrkDiag/test/RunCalibTrkQual.C
@@ -12,7 +12,14 @@ void RunCalibTrkQual(){
   std::string train_names[n_trkquals] = {"TrkQual", "TrkQualNew"};
   for (int i_trkqual = 0; i_trkqual < n_trkquals; ++i_trkqual) {
     std::string train_name = train_names[i_trkqual];
-    CalibTrkQual(tree, train_name);
+
+    TCut time_cut("de.t0>700 && de.t0<1695");
+    TCut tandip_cut("deent.td > 0.577350 && deent.td < 1.000");
+    TCut d0_cut("deent.d0>-80 && de.d0<105");
+    TCut max_radius_cut("detrkqual.MaxRadius>450 && detrkqual.MaxRadius<680");
+    TCut signal_cut = time_cut + tandip_cut + d0_cut + max_radius_cut;
+
+    CalibTrkQual(tree, train_name, signal_cut);
   }
 }
 
