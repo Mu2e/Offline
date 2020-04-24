562b4df346bdb3770a09ceda08fdc14a2d5438f7
soleti <roberto@lbl.gov>
2020-04-24 13:59:19 -0500

    Removed unused declaration

--- Sources/src/FromCorsikaBinary_source.cc
+++ Sources/src/FromCorsikaBinary_source.cc
@@ -67,7 +67,6 @@ namespace mu2e {
                               art::RunPrincipal*&    outR,
                               art::SubRunPrincipal*& outSR,
                               art::EventPrincipal*&  outE);
-      unsigned getSubRunNumber(const std::string& filename) const;
 
       unsigned currentEventNumber_;
 
