3cb8e4b6f0c03586ba60b0bb7c11a78498020725
Ryunosuke O'Neil <roneil@fnal.gov>
2020-05-27 14:11:15 +0100

    fixed regression caused by function name change

--- CosmicReco/src/PDFFit.cc
+++ CosmicReco/src/PDFFit.cc
@@ -389,7 +389,7 @@ double GaussianDriftFit::TimeResidual(ComboHit const& sh, const std::vector<doub
 
   double predictedTime = srep.driftDistanceToTime(sh.strawId(), pca.dca(), 0);
   predictedTime = (pca.s2() > 0 ? predictedTime : -predictedTime);
-  double hit_t0 = sh.propTime() + traj_time + t0 + srep.driftDistanceOffset(sh.strawId(), 0, 0, pca.dca());
+  double hit_t0 = sh.propTime() + traj_time + t0 + srep.driftTimeOffset(sh.strawId(), 0, 0, pca.dca());
   double measuredTime = sh.time() - hit_t0;
 
   return predictedTime - measuredTime;
