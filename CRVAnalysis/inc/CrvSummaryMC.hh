e41de5651d2146a52e440f56e18305f05104a655
Yuri Oksuzian <oksuzian@mu2egpvm05.fnal.gov>
2020-07-18 15:39:36 -0500

    Addition of CRV reco pulses to TrkAna

--- CRVAnalysis/inc/CrvSummaryMC.hh
+++ CRVAnalysis/inc/CrvSummaryMC.hh
@@ -8,18 +8,22 @@ namespace mu2e
   struct CrvSummaryMC
   {
     Double_t            _totalEnergyDeposited;
-    Int_t               _nHitCounters; 
-    CrvSummaryMC(int totalEnergyDeposited, int nHitCounters) :
+    Double_t            _minPathLayer;
+    Double_t            _maxPathLayer;
+    Int_t               _nHitCounters;
+    CrvSummaryMC(double totalEnergyDeposited, double minPathLayer,  double maxPathLayer, int nHitCounters) :
               _totalEnergyDeposited(totalEnergyDeposited),
+              _minPathLayer(minPathLayer),
+              _maxPathLayer(maxPathLayer),
               _nHitCounters(nHitCounters)
               {}
     CrvSummaryMC() :
               _totalEnergyDeposited(0),
+	      _minPathLayer(0),
+	      _maxPathLayer(0),
               _nHitCounters(0)
               {}
   };
 
 }
 #endif
-
-
--- CRVAnalysis/src/CRVAnalysis.cc
+++ CRVAnalysis/src/CRVAnalysis.cc
@@ -59,12 +59,6 @@ namespace mu2e
       recoInfo.emplace_back(cluster.GetCrvSectorType(), cluster.GetAvgCounterPos(),
                             cluster.GetStartTime(), cluster.GetEndTime(),
                             cluster.GetPEs(), cluster.GetCrvRecoPulses().size());
-// test Ptrs
-//      for(auto const& pulse : cluster.GetCrvRecoPulses())
-//	if(pulse.isNull())
-//	  std::cout << "Found invalid CrvRecoPulsePtr" << std::endl;
-//	else
-//	  std::cout << "CrvRecoPulse has " << pulse->GetWaveformIndices().size()  << " Digis" << std::endl;
     }
 
     if(!crvRecoPulseCollection.isValid()) return;
@@ -80,10 +74,6 @@ namespace mu2e
 
 
     //fill the MC collection
-
-    //    std::cout << crvCoincidenceClusterMCCollection
-    //	      << std::endl;
-
     if(crvCoincidenceClusterMCCollection.isValid())
     {
       size_t nClustersMC=crvCoincidenceClusterMCCollection->size();
@@ -116,9 +106,6 @@ namespace mu2e
       MCSummary._totalEnergyDeposited=0;
       std::set<CRSScintillatorBarIndex> counters;
       double totalStep[] = {0, 0, 0, 0};
-      static GlobalConstantsHandle<ParticleDataTable> pdt;
-
-
       for(size_t i=0; i<nStepPoints; i++)
       {
         MCSummary._totalEnergyDeposited+=crvStepPointMCCollection->at(i).totalEDep();
@@ -126,13 +113,8 @@ namespace mu2e
         const CRSScintillatorBarId &CRVCounterId = CRS->getBar(crvStepPointMCCollection->at(i).barIndex()).id();
         int layer = CRVCounterId.getLayerNumber();
         int pdgId = crvStepPointMCCollection->at(i).simParticle()->pdgId();
-        //        ParticleDataTable::maybe_ref info = pdt->particle(pdgId);
         if(abs(pdgId)==13)
           totalStep[layer] = totalStep[layer] + crvStepPointMCCollection->at(i).stepLength();
-        // std::cout<< event.id() << " " << layer << " " << crvStepPointMCCollection->at(i).stepLength()
-        //          << " " << crvStepPointMCCollection->at(i).volumeId()
-        //          << " " << crvStepPointMCCollection->at(i).simParticle()->pdgId()
-        //          << std::endl;
       }
       MCSummary._nHitCounters=counters.size();
       MCSummary._minPathLayer=*std::min_element(totalStep,totalStep+4);
@@ -228,7 +210,6 @@ namespace mu2e
         std::map<art::Ptr<mu2e::SimParticle>,mu2e::MCTrajectory>::const_iterator trajectoryIter;
         for(trajectoryIter=mcTrajectoryCollection->begin(); trajectoryIter!=mcTrajectoryCollection->end(); trajectoryIter++)
         {
-//          if(trajectoryIter->first->id().asInt()==0)  //sim particle with ID 0, but doesn't work, because it's the second stage
           if(trajectoryIter->second.simid()==_trajectorySimParticleId) //seems to point to the primary
           {
             const std::vector<MCTrajectoryPoint> &points = trajectoryIter->second.points();
@@ -269,118 +250,91 @@ namespace mu2e
                                                   const SimParticleTimeOffset &timeOffsets,
                                                   const art::Event& event, CrvPulseInfoRecoCollection &recoInfo, CrvHitInfoMCCollection &MCInfo, CrvWaveformInfoCollection &waveformInfo){
 
-    // mu2e::ConditionsHandle<mu2e::CrvParams> crvPar("ignored");
-    // double _digitizationPeriod  = crvPar->digitizationPeriod;
-    // double _recoPulsePedestal  = crvPar->pedestal;
 
-    art::Handle<CrvRecoPulseCollection> crvRecoPulseCollection;
-    event.getByLabel(crvRecoPulseCollectionModuleLabel,"",crvRecoPulseCollection);
-        if(!crvRecoPulseCollection.isValid()) return;
-    art::Handle<CrvDigiMCCollection> crvDigiMCCollection;
-    if(crvWaveformsModuleLabel!="") event.getByLabel(crvWaveformsModuleLabel,"",crvDigiMCCollection); //this is an optional part for MC information
+     art::Handle<CrvRecoPulseCollection> crvRecoPulseCollection;
+     event.getByLabel(crvRecoPulseCollectionModuleLabel,"",crvRecoPulseCollection);
+     if(!crvRecoPulseCollection.isValid()) return;
 
-    GeomHandle<CosmicRayShield> CRS;
+     mu2e::ConditionsHandle<mu2e::CrvParams> crvPar("ignored");
+     double _digitizationPeriod  = crvPar->digitizationPeriod;
+     double _recoPulsePedestal  = crvPar->pedestal;
+     GeomHandle<CosmicRayShield> CRS;
 
-    // Create SiPM map to extract sequantial SiPM IDs
-    const std::vector<std::shared_ptr<CRSScintillatorBar> > &counters = CRS->getAllCRSScintillatorBars();
-    std::vector<std::shared_ptr<CRSScintillatorBar> >::const_iterator iter;
-    int iSiPM = 0;
-    std::map<int,int> sipm_map;
-    for(iter=counters.begin(); iter!=counters.end(); iter++){
-      const CRSScintillatorBarIndex &barIndex = (*iter)->index();
-      for(int SiPM=0; SiPM<4; SiPM++){
-        if(!(*iter)->getBarDetail().hasCMB(SiPM%2)) continue;
-        sipm_map[barIndex.asInt()*4 + SiPM] = iSiPM;
-        iSiPM++;
-      }
-    }
+     // Create SiPM map to extract sequantial SiPM IDs
+     const std::vector<std::shared_ptr<CRSScintillatorBar> > &counters = CRS->getAllCRSScintillatorBars();
+     std::vector<std::shared_ptr<CRSScintillatorBar> >::const_iterator iter;
+     int iSiPM = 0;
+     std::map<int,int> sipm_map;
+     for(iter=counters.begin(); iter!=counters.end(); iter++)
+       {
+       const CRSScintillatorBarIndex &barIndex = (*iter)->index();
+       for(int SiPM=0; SiPM<4; SiPM++)
+         {
+         if(!(*iter)->getBarDetail().hasCMB(SiPM%2)) continue;
+         sipm_map[barIndex.asInt()*4 + SiPM] = iSiPM;
+         iSiPM++;
+         }
+       }
 
-    // Loop through all reco pulses
-    for(size_t recoPulseIndex=0; recoPulseIndex<crvRecoPulseCollection->size(); recoPulseIndex++){
-      const art::Ptr<CrvRecoPulse> crvRecoPulse(crvRecoPulseCollection, recoPulseIndex);
-      //get information about the counter
-      const CRSScintillatorBarIndex &barIndex = crvRecoPulse->GetScintillatorBarIndex();
-      int sectorNumber  = -1;
-      int moduleNumber  = -1;
-      int layerNumber   = -1;
-      int counterNumber = -1;
-      CrvHelper::GetCrvCounterInfo(CRS, barIndex,
-                                   sectorNumber, moduleNumber, layerNumber, counterNumber);
+     // Loop through all reco pulses
+     for(size_t recoPulseIndex=0; recoPulseIndex<crvRecoPulseCollection->size(); recoPulseIndex++)
+       {
+         const art::Ptr<CrvRecoPulse> crvRecoPulse(crvRecoPulseCollection, recoPulseIndex);
+         //get information about the counter
+         const CRSScintillatorBarIndex &barIndex = crvRecoPulse->GetScintillatorBarIndex();
+         int sectorNumber  = -1;
+         int moduleNumber  = -1;
+         int layerNumber   = -1;
+         int counterNumber = -1;
+         CrvHelper::GetCrvCounterInfo(CRS, barIndex, sectorNumber, moduleNumber, layerNumber, counterNumber);
 
-      //Reco pulses information
-      int SiPM = crvRecoPulse->GetSiPMNumber();
-      int SiPMId = sipm_map.find(barIndex.asInt()*4 + SiPM)->second;
-      CLHEP::Hep3Vector HitPos = CrvHelper::GetCrvCounterPos(CRS, barIndex);
-      double time=crvRecoPulse->GetPulseTime();
-      int    PEs =crvRecoPulse->GetPEs();
-      CrvPulseInfoReco s(HitPos, barIndex.asInt(), sectorNumber, SiPMId);
-      if(recoInfo.find(SiPMId) == recoInfo.end()){
-        std::cout << "Not found it" << std::endl;
-        recoInfo.emplace(SiPMId, s);
-        std::cout << recoInfo[SiPMId]._sectorId << std::endl;
-      }
-      recoInfo[SiPMId]._recoPulses.emplace_back(PEs, time);
+         //Reco pulses information
+         int SiPM = crvRecoPulse->GetSiPMNumber();
+         int SiPMId = sipm_map.find(barIndex.asInt()*4 + SiPM)->second;
+         CLHEP::Hep3Vector HitPos = CrvHelper::GetCrvCounterPos(CRS, barIndex);
+         recoInfo.emplace_back(HitPos, barIndex.asInt(), sectorNumber, SiPMId,
+                               crvRecoPulse->GetPEs(), crvRecoPulse->GetPEsPulseHeight(), crvRecoPulse->GetPulseHeight()+_recoPulsePedestal,
+                               crvRecoPulse->GetPulseBeta(), crvRecoPulse->GetPulseFitChi2(), crvRecoPulse->GetPulseTime());
 
-      std::vector<mu2e::CrvPulseInfoReco::myrecopulse> testReso = recoInfo[SiPMId]._recoPulses;
-      std::cout << testReso[0]._PEs << std::endl;
+         //MCtruth pulses information
+         art::Handle<CrvDigiMCCollection> crvDigiMCCollection;
+         if(crvWaveformsModuleLabel!="") event.getByLabel(crvWaveformsModuleLabel,"",crvDigiMCCollection); //this is an optional part for MC information
+         double totalEnergyDeposited    = 0;
+         double ionizingEnergyDeposited = 0;
+         double earliestHitTime         = NAN;
+         CLHEP::Hep3Vector earliestHitPos;
+         art::Ptr<SimParticle> mostLikelySimParticle;
+         //for this reco pulse
+         CrvHelper::GetInfoFromCrvRecoPulse(crvRecoPulse, crvDigiMCCollection, timeOffsets,
+                                            totalEnergyDeposited, ionizingEnergyDeposited,
+                                            earliestHitTime, earliestHitPos, mostLikelySimParticle);
 
-     // Fill reco struct
-      //      recoInfo.emplace_back(HitPos, time, barIndex.asInt(), sectorNumber, SiPMId, PEs);
+         bool hasMCInfo = (mostLikelySimParticle.isNonnull()?true:false); //MC
+         if(hasMCInfo)
+           {
+             const art::Ptr<SimParticle> &primaryParticle = FindPrimaryParticle(mostLikelySimParticle);
+             const art::Ptr<SimParticle> &parentParticle = FindParentParticle(mostLikelySimParticle);
+             const art::Ptr<SimParticle> &gparentParticle = FindGParentParticle(mostLikelySimParticle);
+             MCInfo.emplace_back(true, mostLikelySimParticle->pdgId(),
+                                 primaryParticle->pdgId(), primaryParticle->startMomentum().e() - primaryParticle->startMomentum().m(), primaryParticle->startPosition(),
+                                 parentParticle->pdgId(),  parentParticle->startMomentum().e()  - parentParticle->startMomentum().m(),  parentParticle->startPosition(),
+                                 gparentParticle->pdgId(), gparentParticle->startMomentum().e() - gparentParticle->startMomentum().m(), gparentParticle->startPosition(),
+                                 earliestHitPos, earliestHitTime, ionizingEnergyDeposited);
+           }
+         else
+           MCInfo.emplace_back();
+       }
 
-      // Fill waveform struct
-      // std::vector<art::Handle<mu2e::CrvDigiCollection> > crvDigisVector;
-      // if(crvDigiModuleLabel!="") event.getByLabel(crvDigiModuleLabel,"",crvDigisVector);
-      // for(size_t i=0; i<crvDigisVector.size(); i++)
-      //   {
-      //     const art::Handle<mu2e::CrvDigiCollection> &crvDigis = crvDigisVector[i];
-      //     // std::string moduleLabel = crvDigis.provenance()->moduleLabel();
-      //     for(size_t j=0; j<crvDigis->size(); j++)
-      //       {
-      //         mu2e::CrvDigi const& digi(crvDigis->at(j));
-      //         int _SiPMId = sipm_map.find(digi.GetScintillatorBarIndex().asInt()*4 + digi.GetSiPMNumber())->second;
-      //         for(size_t k=0; k<mu2e::CrvDigi::NSamples; k++)
-      //           waveformInfo.emplace_back(digi.GetADCs()[k], (digi.GetStartTDC()+k)*_digitizationPeriod, _SiPMId);
-      //       }
-      //   }
-
-
-      // const std::vector<size_t> &waveformIndices = crvRecoPulse->GetWaveformIndices();
-      // for(size_t i=0; i<waveformIndices.size(); i++)
-      //   {
-      //     size_t waveformIndex = waveformIndices[i];
-      //     const CrvDigiMC &digi = crvDigiMCCollection->at(waveformIndex);
-      //     const std::array<double,CrvDigiMC::NSamples> &voltages = digi.GetVoltages();
-      //     const double startTime = digi.GetStartTime();
-      //     for(size_t i=0; i<voltages.size(); i++){
-      //       waveformInfo.emplace_back(voltages[i], startTime + i*_digitizationPeriod, SiPMId);
-      //     }
-      //   }
-
-      //MCtruth pulses information
-      double totalEnergyDeposited    = 0;
-      double ionizingEnergyDeposited = 0;
-      double earliestHitTime         = NAN;
-      CLHEP::Hep3Vector earliestHitPos;
-      art::Ptr<SimParticle> mostLikelySimParticle;
-      //for this reco pulse
-      CrvHelper::GetInfoFromCrvRecoPulse(crvRecoPulse, crvDigiMCCollection, timeOffsets,
-                                         totalEnergyDeposited, ionizingEnergyDeposited,
-                                         earliestHitTime, earliestHitPos, mostLikelySimParticle);
-
-      bool hasMCInfo = (mostLikelySimParticle.isNonnull()?true:false); //MC
-      if(hasMCInfo){
-        const art::Ptr<SimParticle> &primaryParticle = FindPrimaryParticle(mostLikelySimParticle);
-        const art::Ptr<SimParticle> &parentParticle = FindParentParticle(mostLikelySimParticle);
-        const art::Ptr<SimParticle> &gparentParticle = FindGParentParticle(mostLikelySimParticle);
-        MCInfo.emplace_back(true, mostLikelySimParticle->pdgId(),
-                            primaryParticle->pdgId(), primaryParticle->startMomentum().e() - primaryParticle->startMomentum().m(), primaryParticle->startPosition(),
-                            parentParticle->pdgId(),  parentParticle->startMomentum().e()  - parentParticle->startMomentum().m(),  parentParticle->startPosition(),
-                            gparentParticle->pdgId(), gparentParticle->startMomentum().e() - gparentParticle->startMomentum().m(), gparentParticle->startPosition(),
-                            earliestHitPos, earliestHitTime, ionizingEnergyDeposited);
-      }
-      else
-        MCInfo.emplace_back();
-    }
+     //    Fill waveforms struct
+     art::Handle<mu2e::CrvDigiCollection> crvDigis;
+     if(crvDigiModuleLabel!="") event.getByLabel(crvDigiModuleLabel,"",crvDigis);
+     for(size_t j=0; j<crvDigis->size(); j++)
+       {
+         mu2e::CrvDigi const& digi(crvDigis->at(j));
+         int _SiPMId = sipm_map.find(digi.GetScintillatorBarIndex().asInt()*4 + digi.GetSiPMNumber())->second;
+         for(size_t k=0; k<mu2e::CrvDigi::NSamples; k++)
+           waveformInfo.emplace_back(digi.GetADCs()[k], (digi.GetStartTDC()+k)*_digitizationPeriod, _SiPMId);
+       }
    } //FillCrvPulseInfoCollections
 
 }
