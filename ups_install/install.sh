5c4485966c1ea68ed9e506ed9810708105d4af33
Gianantonio Pezzullo <gianipez@fnal.gov>
2020-09-17 15:59:48 -0500

    updated version

--- ups_install/install.sh
+++ ups_install/install.sh
@@ -10,7 +10,7 @@ export COMPILER_CODE=e19
 export DEBUG_LEVEL=prof
 export PACKAGE_NAME=offline
 export PACKAGE_SOURCE=${MU2E_BASE_RELEASE}
-export PACKAGE_VERSION=v09_03_00
+export PACKAGE_VERSION=v09_03_01
 
 # Check that the installation directoy has been defined.
 if [ "${PRODUCTS_INSTALL}" = '' ];then
