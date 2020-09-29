ded66e577af545c2588595bc14a85f84df3d0654
sophiemiddleton <sophie@fnal.gov>
2020-09-29 13:49:03 -0500

    fixed formatting in .hh

--- Mu2eUtilities/inc/MueXSpectrum.hh
+++ Mu2eUtilities/inc/MueXSpectrum.hh
@@ -13,43 +13,43 @@ namespace mu2e {
 
   class MueXSpectrum {
 
-  public:
+    public:
+      
+      struct Params_t {
+        double eMax;
+        double mmu;
+        double Emu;
+        double BR;
+        double Gamma;
+        double me;
+        double mN;
+        double a0;
+        double a1;
+        double a2;
+        double a3;
+        double a4;
+        double a5;
+      } _par;
+      
+      MueXSpectrum(double maxEnergy, double bin, int RadCorrected = 0);
+      
+      ~MueXSpectrum(){}
+   
+      double getWeight(double E) const;
+      double getCorrectedMueXSpectrum(double e) const ;
+      double evalIntegral(double de);
+      static double  f(double E, void *p);
+      void   setSpectrum   (int SpectrumType) { _spectrumType = SpectrumType; }
     
-    struct Params_t {
-      double eMax;
-      double mmu;
-      double Emu;
-      double BR;
-      double Gamma;
-      double me;
-      double mN;
-      double a0;
-      double a1;
-      double a2;
-      double a3;
-      double a4;
-      double a5;
-    } _par;
-    
-    MueXSpectrum(double maxEnergy, double bin, int RadCorrected = 0);
-    
-    ~MueXSpectrum(){}
- 
-    double getWeight                     (double E) const;
-    double getCorrectedMueXSpectrum(double e) const ;
-    double evalIntegral                  (double de);
-    static double  f                  (double E, void *p);
-    void   setSpectrum   (int SpectrumType) { _spectrumType = SpectrumType; }
-  
-  private:
+    private:
 
-    double             _nbins;
-    double             _bin;
-    int                _spectrumType;   // 0:delta function ; 1: rad corrected
-    double             _eMax;           
-    double             _me;		// electron mass
-    double             _integral;     // over n-1 bins...
-  };
+      double             _nbins;
+      double             _bin;
+      int                _spectrumType;// 0:delta function ; 1: rad corrected
+      double             _eMax;           
+      double             _me;// electron mass
+      double             _integral;// over n-1 bins...
+    };
 
 } // end of namespace mu2e
 
