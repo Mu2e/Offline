0af0e124005667f064867805d5357d0cd1551652
David Nathan Brown <dave_brown@lbl.gov>
2020-10-02 00:26:59 -0500

    Add reco ProtonBunchTime, with module to extract from early straw hits and diag

---
+++ TrkHitReco/src/ProtonBunchTimeFromStrawDigis_module.cc
@@ -0,0 +1,128 @@
+
+//
+// This module looks for early tracker digis (before the flash blanking)
+// and uses them to estimate the proton bunch time of this event WRT
+// the DAQ clock
+//
+// Original author David Brown, 10/1/2020 LBNL
+//
+// framework
+#include "art/Framework/Principal/Event.h"
+#include "art_root_io/TFileService.h"
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "ProditionsService/inc/ProditionsHandle.hh"
+#include "RecoDataProducts/inc/StrawDigi.hh"
+#include "RecoDataProducts/inc/ProtonBunchTime.hh"
+#include "TrackerConditions/inc/StrawResponse.hh"
+#include "TH1F.h"
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/variance.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
+#include <boost/accumulators/statistics/mean.hpp>
+
+#include <iostream>
+#include <limits>
+
+namespace mu2e {
+  using namespace TrkTypes;
+
+  class ProtonBunchTimeFromStrawDigis : public art::EDProducer 
+  {
+    public:
+      using Name=fhicl::Name;
+      using Comment=fhicl::Comment;
+
+      struct Config {
+	fhicl::Atom<int> debug{ Name("debugLevel"), Comment("Debug level"), 0};
+	fhicl::Atom<int> diag{ Name("diagLevel"), Comment("Diag level"), 0};
+	fhicl::Atom<int> print{ Name("printLevel"), Comment("Print level"), 0};
+	fhicl::Atom<unsigned> nbins{ Name("nBins"), Comment("Number of bins in time histogram"), 100};
+	fhicl::Atom<unsigned> minnhits{ Name("minNHits"), Comment("Minimum number of hits needed to use result"), 1};
+	fhicl::Atom<float> maxtime { Name("MaxDigiTime"), Comment("Maximum time to accept the digi as 'early' (nsec)"),300.0 };
+	fhicl::Atom<float> meantime { Name("MeanTimeOffset"), Comment("Mean time offset of 'early' hits (nsec)"),103.0}; // this should come from a database FIXME!
+	fhicl::Atom<art::InputTag> strawDigiTag{ Name("StrawDigiTag"), Comment("StrawDigi producer"),"makeSD" };
+      };
+      using Parameters = art::EDProducer::Table<Config>;
+      explicit ProtonBunchTimeFromStrawDigis(Parameters const& config);
+      virtual ~ProtonBunchTimeFromStrawDigis();
+      void produce( art::Event& e) override;
+      void beginJob ( ) override;
+    private:
+      int debug_, diag_, print_;
+      unsigned nbins_, minnhits_;
+      float maxtime_, meantime_;
+      art::InputTag ewmtag_, sdtag_;
+      ProditionsHandle<StrawResponse> _strawResponse_h;
+  };
+
+  ProtonBunchTimeFromStrawDigis::ProtonBunchTimeFromStrawDigis(Parameters const& config) :
+    EDProducer(config),
+    debug_(config().debug()),
+    diag_(config().diag()),
+    print_(config().print()),
+    nbins_(config().nbins()),
+    minnhits_(config().minnhits()),
+    maxtime_(config().maxtime()),
+    meantime_(config().meantime()),
+    sdtag_(config().strawDigiTag())
+  {
+    consumes<StrawDigiCollection>(sdtag_);
+    produces<ProtonBunchTime>();
+  }
+
+  ProtonBunchTimeFromStrawDigis::~ProtonBunchTimeFromStrawDigis() {}
+
+  void ProtonBunchTimeFromStrawDigis::beginJob(){ 
+ }
+
+  void ProtonBunchTimeFromStrawDigis::produce(art::Event& event) {        
+    using namespace boost::accumulators;
+    auto const& srep = _strawResponse_h.get(event.id());
+    auto sdH = event.getValidHandle<StrawDigiCollection>(sdtag_);
+    auto const& sdcol(*sdH);
+    std::unique_ptr<ProtonBunchTime> pbto(new ProtonBunchTime());
+// statistics accumulators
+//
+    accumulator_set<float, stats<tag::mean, tag::variance >> meanacc;
+
+    TH1F* timeplot(0);
+    if(debug_ > 1 && event.id().event() < 1000) {
+      art::ServiceHandle<art::TFileService> tfs;
+      char hname[100];
+      snprintf(hname,100,"PBTO%i",event.id().event());
+      timeplot = tfs->make<TH1F>(hname,"time spectrum;nsec",nbins_,0.0,maxtime_);
+    }
+
+    for (size_t isd=0;isd<sdcol.size();++isd) {
+      const StrawDigi& digi = sdcol[isd];
+    // correct to physical time
+      TDCTimes times;
+      srep.calibrateTimes(digi.TDC(),times,digi.strawId());
+      // take the early time
+      float mintime = std::min(times[StrawEnd::hv],times[StrawEnd::cal]);
+      if(mintime < maxtime_){
+      // This can be improved by using TOT to approximately correct for the propagation time, and maybe peak/ped to correct
+      // for time slewing TODO!
+	meanacc(mintime);
+// early hit: accumulate the histogram
+	if(print_ > 1) std::cout << "Early hit time " << mintime << " SID " << digi.strawId() << std::endl;
+	if(timeplot)timeplot->Fill(mintime);
+      }
+    }
+    // if there aren't enought hits, set to 0
+    pbto->nhits_ = extract_result<tag::count>(meanacc);
+    if(pbto->nhits_ < minnhits_){
+      pbto->pbtime_ = - meantime_;
+      pbto->variance_ = 0.0;
+    } else {
+      pbto->pbtime_ = extract_result<tag::mean>(meanacc) - meantime_;
+      pbto->variance_ = extract_result<tag::variance>(meanacc);
+    }
+    event.put(std::move(pbto));
+  }
+}
+
+using mu2e::ProtonBunchTimeFromStrawDigis;
+DEFINE_ART_MODULE(ProtonBunchTimeFromStrawDigis);
+
--- TrkHitReco/src/StrawHitReco_module.cc
+++ TrkHitReco/src/StrawHitReco_module.cc
@@ -30,7 +30,7 @@
 #include "TrkHitReco/inc/PeakFitFunction.hh"
 #include "TrkHitReco/inc/ComboPeakFitRoot.hh"
 
-#include "DataProducts/inc/EventWindowMarker.hh"
+#include "RecoDataProducts/inc/ProtonBunchTime.hh"
 #include "DataProducts/inc/StrawEnd.hh"
 #include "RecoDataProducts/inc/CaloClusterCollection.hh"
 #include "RecoDataProducts/inc/StrawDigi.hh"
@@ -80,7 +80,7 @@ namespace mu2e {
        art::ProductToken<StrawDigiCollection> const _sdtoken;
        art::ProductToken<StrawDigiADCWaveformCollection> const _sdadctoken;
        art::ProductToken<CaloClusterCollection> const _cctoken;
-       art::InputTag _ewMarkerTag; // name of the module that makes eventwindowmarkers
+       art::InputTag _pbtoTag; // name of the module that makes eventwindowmarkers
        fhicl::ParameterSet _peakfit;  // peak fit (charge reconstruction) parameters
        std::unique_ptr<TrkHitReco::PeakFit> _pfit; // peak fitting algorithm
        // diagnostic
@@ -118,10 +118,10 @@ namespace mu2e {
       _sdtoken{consumes<StrawDigiCollection>(pset.get<art::InputTag>("StrawDigiCollection","makeSD"))},
       _sdadctoken{mayConsume<StrawDigiADCWaveformCollection>(pset.get<art::InputTag>("StrawDigiADCWaveformCollection","makeSD"))},
       _cctoken{mayConsume<CaloClusterCollection>(pset.get<art::InputTag>("caloClusterModuleLabel","CaloClusterFast"))},
-      _ewMarkerTag(pset.get<art::InputTag>("EventWindowMarkerLabel")),
+      _pbtoTag(pset.get<art::InputTag>("ProtonBunchTimeLabel")),
       _peakfit(pset.get<fhicl::ParameterSet>("PeakFitter", {}))
   {
-      consumes<EventWindowMarker>(_ewMarkerTag);
+      consumes<ProtonBunchTime>(_pbtoTag);
       produces<ComboHitCollection>();
       if(_writesh)produces<StrawHitCollection>();
       if (_printLevel > 0) std::cout << "In StrawHitReco constructor " << std::endl;
@@ -181,11 +181,11 @@ namespace mu2e {
         caloClusters = ccH.product();
       }
 
-      double ewmOffset = 0;
-      art::Handle<EventWindowMarker> ewMarkerHandle;
-      if (event.getByLabel(_ewMarkerTag, ewMarkerHandle)){
-        const EventWindowMarker& ewMarker(*ewMarkerHandle);
-        ewmOffset = ewMarker.timeOffset();
+      double pbtoOffset = 0;
+      art::Handle<ProtonBunchTime> pbtoHandle;
+      if (event.getByLabel(_pbtoTag, pbtoHandle)){
+        const ProtonBunchTime& pbto(*pbtoHandle);
+        pbtoOffset = pbto.pbtime_;
       }
 
       std::unique_ptr<StrawHitCollection> shCol;
@@ -219,7 +219,7 @@ namespace mu2e {
 	if(times[StrawEnd::hv] < times[StrawEnd::cal])
 	  eend = StrawEnd(StrawEnd::hv);
 	// take the earliest of the 2 end times
-	float time = times[eend.end()] + ewmOffset;
+	float time = times[eend.end()] - pbtoOffset;
 	if (time < _minT || time > _maxT ){
 	  if(_filter)continue;
 	} else
