3d3bd44229f823f37fa52e40bb9b8e25869230a8
Eric Flumerfelt <eflumerf@fnal.gov>
2020-10-09 14:37:18 -0500

    Updates for revised raw data structure that allows for more flexibility as raw data formats evolve.

--- DataProducts/inc/TrkTypes.hh
+++ DataProducts/inc/TrkTypes.hh
@@ -13,14 +13,14 @@ namespace mu2e {
     static constexpr size_t NADC = 15; // number of ADC samples including presamples
     static constexpr size_t NENDS = 2; // number of straw ends
     // typedefs used for tracker data structures 
-    typedef uint16_t TDCValue;
+    typedef uint32_t TDCValue;
     typedef uint16_t TOTValue;
     typedef uint16_t ADCValue;
     typedef std::array<TDCValue,NENDS> TDCValues;
     typedef std::array<TOTValue,NENDS> TOTValues;
     typedef std::array<float,NENDS> TDCTimes;
     typedef std::array<float,NENDS> TOTTimes;
-    typedef std::array<ADCValue,NADC> ADCWaveform;
+    typedef std::vector<ADCValue> ADCWaveform;
 // transient types used in simulation
     typedef std::vector<float> ADCVoltages;
     typedef std::vector<float> ADCTimes;
