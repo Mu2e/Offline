3a39ab2e6df9b65e4ad8f883a7210ba8776a7591
Andrei Gaponenko <gandr@fnal.gov>
2020-11-26 18:54:24 -0600

    Whitespace fixes.

--- Mu2eHallGeom/src/Mu2eHall.cc
+++ Mu2eHallGeom/src/Mu2eHall.cc
@@ -1,5 +1,5 @@
 // Implementation file for an accessor function for Mu2eHall.
-// Allows users to know inner and outer wall placement for 
+// Allows users to know inner and outer wall placement for
 // reference.
 // Original author: David No. Brown, Louisville.  August 2015
 
@@ -23,30 +23,30 @@
 namespace mu2e {
 
   double Mu2eHall::getWallExtentz( const std::string& bldgSolName, const int side) const {
-    
+
     const ExtrudedSolid& solid = bldgSolids_.find( bldgSolName )->second;
 
     const std::vector<CLHEP::Hep2Vector> verts = solid.getVertices();
 
-    double posHigh = -99999.0, pos2nd = -99999.0, negHigh = 99999.0, 
+    double posHigh = -99999.0, pos2nd = -99999.0, negHigh = 99999.0,
       neg2nd = 99999.0;
 
     for ( unsigned int i = 0; i < verts.size(); i++ ) {
       double z = verts[i].x();// x of vertices get rotated into z
       if ( z > posHigh ) {
-	pos2nd = posHigh;
-	posHigh = z;
+        pos2nd = posHigh;
+        posHigh = z;
       }
       if ( z > pos2nd && z < posHigh ) {
-	pos2nd = z;
+        pos2nd = z;
       }
 
       if ( z < negHigh ) {
-	neg2nd = negHigh;
-	negHigh = z;
+        neg2nd = negHigh;
+        negHigh = z;
       }
       if ( z < neg2nd && z > negHigh ) {
-	neg2nd = z;
+        neg2nd = z;
       }
     }
     if ( side == 2 ) return posHigh  + solid.getOffsetFromMu2eOrigin().z();
