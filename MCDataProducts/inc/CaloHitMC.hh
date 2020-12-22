e4fdea4e7ef1d779cdbd86133e922d0d96b42fae
echenard <echenard@hep.caltech.edu>
2020-12-21 19:48:56 -0800

    Forgot to add the new CaloHitMC files

---
+++ MCDataProducts/inc/CaloHitMC.hh
@@ -0,0 +1,38 @@
+//
+// This calss contains CaloMCEdep for each CaloDigi, matching deposited energy at MC level to reco digi hits
+// Notes:
+// CaloEdepMC: the list of CaloEDepMC is ordered by energy - most energetic first. 
+// time:       taken as the time of the most energetic contribution.
+//
+//
+#ifndef MCDataProducts_CaloHitMC_hh
+#define MCDataProducts_CaloHitMC_hh
+
+#include "MCDataProducts/inc/CaloEDepMC.hh"
+#include <vector>
+
+namespace mu2e
+{   
+   
+   class CaloHitMC
+   {
+       public:                    
+          CaloHitMC()                                     : edeps_()      {};
+          CaloHitMC(const std::vector<CaloEDepMC>& edeps) : edeps_(edeps) {};
+                
+          const std::vector<CaloEDepMC>& energyDeposits  ()           const {return edeps_;       }
+          const CaloEDepMC&              energyDeposit   (unsigned i) const {return edeps_.at(i); } 
+          unsigned                       nParticles      ()           const {return edeps_.size();}
+          float                          time            ()           const {return edeps_.at(0).time();}
+          float                          totalEnergyDep  ()           const;
+          float                          totalEnergyDepG4()           const;
+
+       private:
+          std::vector<CaloEDepMC> edeps_; 
+   };  
+   
+   using  CaloHitMCCollection = std::vector<mu2e::CaloHitMC>;
+} 
+
+#endif
+
---
+++ MCDataProducts/src/CaloHitMC.cc
@@ -0,0 +1,32 @@
+#include "MCDataProducts/inc/CaloHitMC.hh"
+#include <numeric>
+
+namespace mu2e {
+
+   /*
+   bool CaloHitMC::isConversion() const 
+   {
+      for (auto& edep : edeps_)
+      {
+         auto parent(edep.sim());
+         while (parent->hasParent()) parent = parent->parent();                     
+	 if (parent->genParticle() && parent->genParticle()->generatorId().isConversion() ) return true;
+      }    		
+      return false;
+   }
+   */
+
+   float CaloHitMC::totalEnergyDep() const 
+   {
+      auto sumEdep = [](float sum, const CaloEDepMC& edep){return sum +=edep.energyDep();};
+      return std::accumulate(edeps_.begin(),edeps_.end(),0.0f,sumEdep);
+   }
+
+
+   float CaloHitMC::totalEnergyDepG4() const 
+   {
+      auto sumEdepG4 = [](float sum, const CaloEDepMC& edep){return sum +=edep.energyDepG4();};
+      return std::accumulate(edeps_.begin(),edeps_.end(),0.0f,sumEdepG4);
+   }
+
+}
