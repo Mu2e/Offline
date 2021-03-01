d4bad2f59b5e2d641aaa8ff7e2547a7435960655
Stefano Miscetti <stefanomiscetti@gmail.com>
2021-03-01 08:43:17 -0600

    comments from Giani, Rob and Dave implemented

--- CaloFilters/src/CaloCosmicCalib_module.cc
+++ CaloFilters/src/CaloCosmicCalib_module.cc
@@ -5,7 +5,6 @@
 //  from original version of D.Brown
 // -------------------------------------------------
 // framework
-//#include "art/Framework/Core/EDProducer.h"
 #include "art/Framework/Core/EDFilter.h"
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Principal/Event.h"
@@ -41,16 +40,20 @@ namespace mu2e
       using Name    = fhicl::Name;
       using Comment = fhicl::Comment;
       fhicl::Atom<std::string>  cltag            { Name("CaloClusterModuleLabel"), Comment("Calo cluster collection name ") }; 
-      fhicl::Atom<int>          minncrystalhits  { Name("MinNCrystalHits"),        Comment("") }; 
-      fhicl::Atom<double>       minenergy        { Name("MinEnergy"),              Comment("") }; 
-      fhicl::Atom<double>       maxenergy        { Name("MaxEnergy"),              Comment("") }; 
-      fhicl::Atom<std::string>  trigPath         { Name("triggerPath"),            Comment("") }; 
-      fhicl::Atom<int>          debug            { Name("debugLevel"),             Comment("") }; 
-      fhicl::Atom<int>          mincelinout      { Name("MinNumCelinout"),         Comment("") }; 
-      fhicl::Atom<int>          minceldiag       { Name("MinNumCeldiagver"),       Comment("") }; 
-      fhicl::Atom<float>        mincelcut        { Name("MinCelEneCut"),           Comment("") }; 
-      fhicl::Atom<float>        outradius        { Name("OutRadius"),              Comment("") }; 
-      fhicl::Atom<float>        innradius        { Name("InnerRadius"),            Comment("") }; 
+      fhicl::Atom<int>          minncrystalhits  { Name("MinNCrystalHits"),        Comment("Minimum Number of crystal hits") }; 
+      fhicl::Atom<double>       minenergy        { Name("MinEnergy"),              Comment("Minimum Energy for the clusters") }; 
+      fhicl::Atom<double>       maxenergy        { Name("MaxEnergy"),              Comment("Maximum Energy for the clusters") }; 
+      fhicl::Atom<std::string>  trigPath         { Name("triggerPath"),            Comment("Name of the trigger path") }; 
+      fhicl::Atom<int>          debug            { Name("debugLevel"),             Comment("Debug Level") }; 
+      fhicl::Atom<int>          mincelinout      { Name("MinNumCelinout"),         Comment("Minimum Number of crystals for IN-OUT algo") }; 
+      fhicl::Atom<int>          minceldiag       { Name("MinNumCeldiagver"),       Comment("Minimum Number of crystals for DIAG-VER algo") }; 
+      fhicl::Atom<float>        mincelcut        { Name("MinCelEneCut"),           Comment("Minimum Energy for the crystals") }; 
+      fhicl::Atom<float>        outradius        { Name("OutRadius"),              Comment("Value of the Outer radius considered") }; 
+      fhicl::Atom<float>        innradius        { Name("InnerRadius"),            Comment("Value of the Inner radius considered") };
+      fhicl::Atom<float>        crystalsize      { Name("crystalsize"),            Comment("Crystal size") }; 
+      fhicl::Atom<float>        diagminval       { Name("diagminval"),             Comment("Min slope val for DIAG algo") }; 
+      fhicl::Atom<float>        diagmaxval       { Name("diagmaxval"),             Comment("Max slope value for DIAG algo") };
+      fhicl::Atom<float>        dyminval         { Name("dyminval"),               Comment("Min value of Y-depths for OUT-OUT algo   ") };     
     };
 
     explicit CaloCosmicCalib(const art::EDFilter::Table<Config>& config);
@@ -69,6 +72,10 @@ namespace mu2e
     float         _mincelcut;
     float         _outradius;
     float         _innradius;
+    float         _crystalsize;
+    float         _diagminval;
+    float         _diagmaxval;
+    float         _dyminval;
     // counters
     unsigned _nevt, _npass;
     const Calorimeter* _calogeom;
@@ -87,6 +94,10 @@ namespace mu2e
     _mincelcut      (config().mincelcut()),
     _outradius      (config().outradius()),
     _innradius      (config().innradius()),
+    _crystalsize    (config().crystalsize()),
+    _diagminval     (config().diagminval()),
+    _diagmaxval     (config().diagmaxval()),
+    _dyminval       (config().dyminval()),     
     _nevt(0), _npass(0)
   {
     produces<TriggerInfo>();
@@ -120,9 +131,9 @@ namespace mu2e
     // ----------------------------------------------------------
     // These are the cuts that should be handled by the FCL file
     // ---- default for initialization --------------------------
-    float EMIPcut =  _mincelcut; // cut to select MIP
-    float RMINcut = _innradius; // inner radius cut
-    float RMAXcut = _outradius; // outer radius cut
+    //float EMIPcut =  _mincelcut; // cut to select MIP
+    //float RMINcut = _innradius; // inner radius cut
+    //float _outradius = _outradius; // outer radius cut
     //int   MinNumMIPcel = _mincelinout;    // Min Number of MIP cells
     //int   MinNumMIPdia = _minceldiagver;
     
@@ -167,11 +178,11 @@ namespace mu2e
 	  float time  = hitcalo->time();	  
 	  int _cryid = hitcalo->crystalID();	  
 	  int DiskId = _calogeom->crystal(_cryid).diskID();	
- 
+	  
 	  CLHEP::Hep3Vector crystalpos =
 	    _calogeom->geomUtil().mu2eToDiskFF(DiskId,_calogeom->crystal(_cryid).position());
 	  
-	  if( ene> EMIPcut){         // Use cells with energy above a MIP-like threshold
+	  if( ene> _mincelcut){         // Use cells with energy above a MIP-like threshold
 	    xval = crystalpos.x();
 	    yval = crystalpos.y();
 	    rval = sqrt(pow(xval,2)+ pow(yval,2));
@@ -186,8 +197,8 @@ namespace mu2e
 	      RMin = rval; }
 	    // calculate Outer/Inner Ring Energies
 	    
-	    if( rval > RMAXcut ) Eouter = Eouter+ene; // Oouter ring 
-	    if( rval < RMINcut ) Einner = Einner+ene; // Inner  ring 
+	    if( rval > _outradius ) Eouter = Eouter+ene; // Oouter ring 
+	    if( rval < _innradius ) Einner = Einner+ene; // Inner  ring 
 	    NumMipCel++; // Increment number of MIP cells
 	    
 	  }
@@ -199,17 +210,17 @@ namespace mu2e
 	//====================================
 	// Now fill up the selection flag  ...
 	//====================================
-	if( Eouter>EMIPcut && Einner>EMIPcut && NumMipCel>_mincelinout ) selflag=selflag+1;   // Out-IN or IN-Out
+	if( Eouter>_mincelcut && Einner>_mincelcut && NumMipCel>_mincelinout ) selflag=selflag+1;   // Out-IN or IN-Out
 	
-	if( NumMipCel>_minceldiag && YoutMax>-999. && YoutMin<999. ){	  
+	if( NumMipCel>_minceldiag ){ // 	  
 	  DXmax = abs(XMax-XMin);
 	  DYmax = abs(YoutMax-YoutMin);
 	  float DiagVal=0.;
 	  if( DXmax> 0 ) DiagVal = DYmax/DXmax;	  
-	  if( DXmax  < 35.0  ) selflag = selflag+100;  // Vertical selection
-	  if( DiagVal > 1.5 && DiagVal < 2.5 )
+	  if( DXmax  < _crystalsize  ) selflag = selflag+100;  // Vertical selection, cell size+1mm
+	  if( DiagVal > _diagminval && DiagVal < _diagmaxval )     // vertical slope boundaries (1.5, 2.5)
 	    selflag = selflag+200;                     // Diagonal selection
-	  if( DYmax > 140 && RMax > RMAXcut && RMin > RMAXcut )
+	  if( DYmax > _dyminval && RMax > _outradius && RMin > _outradius ) // at least 4 crystals vertical depth
 	    selflag=selflag+10;                        // Outer-Outer
 	}
 	
@@ -226,12 +237,11 @@ namespace mu2e
 	    triginfo->_triggerBits.merge(TriggerFlag::caloCalib);
 	    triginfo->_triggerPath = _trigPath;
 	  }
-	  // associate to the caloCluster which triggers.
-	  // Note there may be other caloClusters which also pass the filter
-	  // but filtering is by event!
+	  //
+	  // associate to the caloClusters which trigger
+	  //
 	  size_t index = std::distance(clcol->begin(),icl);
-	  triginfo->_caloClusters.push_back(art::Ptr<CaloCluster>(clH,index)); // Modificato
-	  //triginfo->_caloCluster = art::Ptr<CaloCluster>(clH,index);	  
+	  triginfo->_caloClusters.push_back(art::Ptr<CaloCluster>(clH,index));   
 	  ++trig_ind;
 	  
 	  if(_debug > 1){
@@ -245,7 +255,7 @@ namespace mu2e
     evt.put(std::move(triginfo));
     return retval;
   }
-
+  
   bool CaloCosmicCalib::endRun( art::Run& run ) {
     if(_debug > 0 && _nevt > 0){
       std::cout << moduleDescription().moduleLabel() << " passed " <<  _npass << " events out of " << _nevt << " for a ratio of " << float(_npass)/float(_nevt) << std::endl;
