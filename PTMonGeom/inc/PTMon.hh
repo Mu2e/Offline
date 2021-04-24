23b38a9672631bfbc19a02ddb030d5c1d95816bf
Helenka Casler <hcasler@gradcenter.cuny.edu>
2021-04-23 20:02:19 -0400

    beginning of GeometryService approach to PTargetMon. Filled out class for PWC

---
+++ PTMonGeom/inc/PTMon.hh
@@ -0,0 +1,13 @@
+#ifndef PTMonGeom_PTMon_hh
+#define PTMonGeom_PTMon_hh
+
+// ProductionTarget Monitor (PTMon) Object
+//
+// Author: Helenka Casler
+//
+
+namespace mu2e {
+
+} // namespace mu2e
+
+#endif
---
+++ PTMonGeom/src/PTMonPWC.cc
@@ -0,0 +1,69 @@
+#include "PTMonGeom/inc/PTMonPWC.hh"
+#include "GeomPrimitives/inc/Box.hh"
+
+namespace mu2e {
+
+  PTMonPWC::PTMonPWC(std::string nameSuffix,
+                     double frameHeight, 
+                     double frameWidth, 
+                     double frameThick, 
+                     double outerPlateThick,
+                     std::string frameMaterialName, 
+                     double windowHeight,
+                     double windowWidth,
+                     double windowThick,
+                     std::string windowMaterialName,
+                     std::string gasMaterialName,
+                     int numVertWires,
+                     int numHorizWires,
+                     CLHEP::Hep3Vector const & originInParent = CLHEP::Hep3Vector(),
+                     CLHEP::HepRotation const & rotationInParent = CLHEP::HepRotation(),
+                     int wireNumStart = 0) : 
+    _nameSuffix(nameSuffix),
+    _originInParent(originInParent),
+    _rotationInParent(rotationInParent),
+    _frameHeight(frameHeight),
+    _frameWidth(frameWidth),
+    _frameThick(frameThick),
+    _outerPlateThick(outerPlateThick),
+    _frameMaterialName(frameMaterialName),
+    _windowMaterialName(windowMaterialName),
+    _gasMaterialName(gasMaterialName),
+    _numVertWires(numVertWires),
+    _numHorizWires(numHorizWires),
+    _wireNumStart(wireNumStart)
+  {
+    _totalThick = (13.*frameThick) + (2.*outerPlateThick);
+    _ground1Z = -5.5*frameThick;
+    _hv1Z = -3.5*frameThick;
+    _hv2Z = 0.5*frameThick;
+    _hv3Z = 4.5*frameThick;
+    _ground2Z = 6.5*frameThick;
+    _gasInZ = 0.5*(_hv1Z + _ground1Z);
+    _vertWireZ = 0.5*(_hv2Z + _hv1Z);
+    _horizWireZ = 0.5*(_hv3Z + _hv2Z);
+    _gasOutZ = 0.5*(_ground2Z + _hv3Z);
+
+    _pwcWindow.reset(new Box(0.5*windowWidth, 0.5*windowHeight, 0.5*windowThick));
+    double vertWireHalfHeight = 0.5*windowHeight / numVertWires;
+    double vertWireHalfThick = 0.5*(_hv2Z - _hv1Z - windowThick);
+    _vertWireGasSection.reset(new Box(0.5*windowWidth, vertWireHalfHeight, vertWireHalfThick));
+    double horizWireHalfWidth = 0.5*windowWidth / numHorizWires;
+    double horizWireHalfThick = 0.5*(_hv3Z - _hv2Z - windowThick);
+    _horizWireGasSection.reset(new Box(horizWireHalfWidth, 0.5*windowHeight, horizWireHalfThick));
+    double gasInHalfThick = 0.5*(_hv1Z - _ground1Z - windowThick);
+    _gasSection1.reset(new Box(windowWidth, windowHeight, gasInHalfThick));
+    double gasOutHalfThick = 0.5*(_ground2Z - _hv3Z - windowThick);
+    _gasSection4.reset(new Box(windowWidth, windowHeight, gasOutHalfThick));
+
+    for (int i=0; i<numVertWires; i++) {
+      double gasY2 = (-0.5*windowHeight) + ((i+0.5)*2.*vertWireHalfHeight);
+      _vertWireYpos.push_back(gasY2);
+    }
+    for (int i=0; i<numHorizWires; i++) {
+      double gasX3 = (0.5*windowWidth) - ((i+0.5)*2.*horizWireHalfWidth);
+      _horizWireXpos.push_back(gasX3);
+    }
+  } // ctor
+
+} // namespace mu2e
