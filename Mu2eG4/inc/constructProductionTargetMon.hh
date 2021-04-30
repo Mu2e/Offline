371c35028a08f4ed47a8bffe9bbb46d5209f6952
Helenka Casler <hcasler@gradcenter.cuny.edu>
2021-04-29 20:34:58 -0400

    cleanup of constructProductionTargetMon

--- Mu2eG4/inc/constructProductionTargetMon.hh
+++ Mu2eG4/inc/constructProductionTargetMon.hh
@@ -9,48 +9,56 @@
 
 //Mu2e includes
 #include "Mu2eG4Helper/inc/VolumeInfo.hh"
+#include "G4LogicalVolume.hh"
 
 namespace mu2e {
 
-    class VolumeInfo;
     class SimpleConfig;
-    class G4LogicalVolume;
     class PTMonPWC;
 
     void constructProductionTargetMon(VolumeInfo const& parent, SimpleConfig const& _config);
 
     // helper methods
-    
-    void constructTargetHallPWC(VolumeInfo const& motherVolume, PTMonPWC* pwc, bool const doSurfaceCheck, int const verbosity);
+
+    void constructTargetHallPWC(VolumeInfo const& motherVolume, 
+                              const PTMonPWC* pwc, 
+                              SimpleConfig const& _config, 
+                              bool const doSurfaceCheck, 
+                              int const verbosity);
 
     void insertOuterFrame(VolumeInfo const& container, 
-                        PTMonPWC* pwc, 
+                        const PTMonPWC* pwc, 
+                        SimpleConfig const& _config,
                         bool const doSurfaceCheck,
                         int const verbosity);
 
     void insertWindows(G4LogicalVolume* windowLogical, 
                      VolumeInfo const& container, 
-                     PTMonPWC* pwc, 
+                     const PTMonPWC* pwc, 
+                     SimpleConfig const& _config,
                      bool const doSurfaceCheck,
                      int const verbosity);
 
     void insertOuterGasBlocks(VolumeInfo const& container, 
-                            PTMonPWC* pwc, 
+                            const PTMonPWC* pwc, 
                             G4Material* gasMaterial, 
+                            SimpleConfig const& _config,
                             bool const doSurfaceCheck,
                             int const verbosity);
 
     void insertVerticalProfileWires(VolumeInfo const& container, 
-                                  PTMonPWC* pwc, 
+                                  const PTMonPWC* pwc, 
                                   G4Material* gasMaterial, 
                                   std::string const& wireNameSuffix, 
+                                  SimpleConfig const& _config,
                                   bool const doSurfaceCheck,
                                   int const verbosity);
 
     void insertHorizontalProfileWires(VolumeInfo const& container, 
-                                    PTMonPWC* pwc, 
+                                    const PTMonPWC* pwc, 
                                     G4Material* gasMaterial, 
                                     std::string const& wireNameSuffix, 
+                                    SimpleConfig const& _config,
                                     bool const doSurfaceCheck,
                                     int const verbosity);
 
