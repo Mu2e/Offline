15f429d5ee1c1278068fb444efd07560df54971a
macndev <mndevilbiss@gmail.com>
2021-05-18 14:37:39 -0500

    KinKal tag change and last deprecation fix

--- setup.sh
+++ setup.sh
@@ -136,7 +136,7 @@ setup -B mu2e_artdaq_core v1_05_09_01 -q${MU2E_UPS_QUALIFIERS}:+${MU2E_ART_SQUAL
 
 setup -B heppdt   v03_04_02 -q${MU2E_UPS_QUALIFIERS}
 setup cetpkgsupport
-setup -B KinKal   v00_01_05a  -q${MU2E_UPS_QUALIFIERS}:p392
+setup -B KinKal   v00_01_06a  -q${MU2E_UPS_QUALIFIERS}:p392
 setup -B BTrk   v1_02_31  -q${MU2E_UPS_QUALIFIERS}:p392
 setup -B cry   v1_7n  -q${MU2E_UPS_QUALIFIERS}
 setup -B gsl v2_6a
