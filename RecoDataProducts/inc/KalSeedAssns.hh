bde8b55ae9a917ecf624b6c76fce6db917426620
sophieMu2e <sophie@fnal.gov>
2021-06-04 15:52:14 -0500

    builds with new code, awaiting Richies fix

--- RecoDataProducts/inc/KalSeedAssns.hh
+++ RecoDataProducts/inc/KalSeedAssns.hh
@@ -1,7 +1,9 @@
 #include "RecoDataProducts/inc/KalSeed.hh"
 #include "RecoDataProducts/inc/HelixSeed.hh"
+#include "RecoDataProducts/inc/CosmicTrackSeed.hh"
 #include "canvas/Persistency/Common/Assns.h"
 namespace mu2e {
   // Assns between a KalSeed and the HelixSeed it was created from
   typedef art::Assns<KalSeed,HelixSeed> KalHelixAssns;
+  typedef art::Assns<KalSeed,CosmicTrackSeed> KalLineAssns;
 }
