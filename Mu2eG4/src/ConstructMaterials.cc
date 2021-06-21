a7b93dcdf680b7ea0af866a660654de968269707
David Nathan Brown <dave_brown@lbl.gov>
2021-06-21 18:32:52 -0500

    Add CosmicLivetime and clean out unused parts.  Allow wildcarding the process codes when selecting particles

--- Mu2eG4/src/ConstructMaterials.cc
+++ Mu2eG4/src/ConstructMaterials.cc
@@ -258,9 +258,9 @@ namespace mu2e {
 
     mat = uniqueMaterialOrThrow( "IPAPolyethylene");
     {
-      G4Material* IPAPolyethylene = new G4Material( mat.name, 0.90*CLHEP::g/CLHEP::cm3, 2);
-      IPAPolyethylene->AddMaterial( findMaterialOrThrow("G4_H"), 0.14);
-      IPAPolyethylene->AddMaterial( findMaterialOrThrow("G4_C"), 0.86);
+      G4Material* IPAPolyethylene = new G4Material( mat.name, 0.96*CLHEP::g/CLHEP::cm3, 2);
+      IPAPolyethylene->AddMaterial( findMaterialOrThrow("G4_H"), 0.12);
+      IPAPolyethylene->AddMaterial( findMaterialOrThrow("G4_C"), 0.88);
     }
 
     mat = uniqueMaterialOrThrow( "Polyethylene096");
