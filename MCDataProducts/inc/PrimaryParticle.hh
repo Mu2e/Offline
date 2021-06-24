67f5bcf8b3404a634e3fa0c2b5b9aaae86a21995
David Nathan Brown <dave_brown@lbl.gov>
2021-06-24 18:54:17 -0500

    Move tests to module.  Remove unnecessary data cache to support backwards-compatibility

--- MCDataProducts/inc/PrimaryParticle.hh
+++ MCDataProducts/inc/PrimaryParticle.hh
@@ -11,23 +11,12 @@ namespace mu2e {
     public:
       typedef std::vector<art::Ptr<SimParticle> > SPPV;
       PrimaryParticle() {}
-      PrimaryParticle(PrimaryParticle const& other ) : _simps(other._simps)
-      { // check these all have the same creation code
-	auto isimp = _simps.begin()++;
-	while(isimp != _simps.end()){
-	  if((*isimp)->creationCode() != _simps.front()->creationCode())
-	    throw cet::exception("Simulation")<<"PrimaryParticle: creation codes don't match" << std::endl;
-	  ++isimp;
-	}
-	_pcode = _simps.front()->creationCode().id();
-      }
       PrimaryParticle(SPPV const& simps) : _simps(simps) {}
       SPPV const& primarySimParticles() const { return _simps; }
-      ProcessCode const& primaryProcess() const { return _pcode; }
+      ProcessCode primaryProcess() const { return _simps.front()->creationCode(); }
       SPPV& modifySimParticles() { return _simps; } // needed for compression
     private:
       SPPV _simps; // associated SimParticles (can be >1).  All must have the same creation code
-      ProcessCode _pcode; // process code of this primary
   };
 
   inline std::ostream& operator<<(std::ostream& ost, const PrimaryParticle& pp ){
