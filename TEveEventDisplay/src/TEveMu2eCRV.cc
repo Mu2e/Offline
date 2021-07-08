a60f95da04a2a896fd88d9e804cd5619482150bc
NamithaChitrazee <namitha@pi.infn.it>
2021-07-08 11:07:46 +0200

    Update TEveMu2eCRV.cc

--- TEveEventDisplay/src/TEveMu2eCRV.cc
+++ TEveEventDisplay/src/TEveMu2eCRV.cc
@@ -10,7 +10,55 @@ namespace mu2e{
     TGeoMedium *Si = new TGeoMedium("Silicon",2, matSi);
     std::vector<double> halflen;
     CLHEP::Hep3Vector position;
-    CosmicRayShield const &CRS = *(GeomHandle<CosmicRayShield>());
+    CosmicRayShield const &CRS = *(GeomHandle<CosmicRayShield>());  
+	 
+	   std::vector<mu2e::CRSScintillatorShield> const& shields = CRS.getCRSScintillatorShields();
+    for(std::vector<mu2e::CRSScintillatorShield>::const_iterator ishield=shields.begin(); ishield!=shields.end(); ++ishield)
+    {
+      CRSScintillatorShield const& shield = *ishield;
+      std::string const& shieldName = shield.getName();
+
+      CRSScintillatorBarDetail const& barDetail = shield.getCRSScintillatorBarDetail();
+      double dx=barDetail.getHalfLengths()[0];
+      double dy=barDetail.getHalfLengths()[1];
+      double dz=barDetail.getHalfLengths()[2];
+      std::cout<<"sci bar details :"<<dx<<" "<<dy<<" "<<dz<<std::endl;
+      int nModules = shield.nModules();
+      for (int im = 0; im < nModules; ++im)
+      {
+        CRSScintillatorModule const & module = shield.getModule(im);
+
+        int nLayers = module.nLayers();
+        for (int il = 0; il < nLayers; ++il)
+        {
+          CRSScintillatorLayer const & layer = module.getLayer(il);
+
+          int nBars = layer.nBars();
+          for (int ib = 0; ib < nBars; ++ib)
+          {
+            CRSScintillatorBar const & bar = layer.getBar(ib);
+            int index = bar.index().asInt();
+            CLHEP::Hep3Vector barOffset = bar.getPosition(); //- _detSysOrigin;
+            double x=barOffset.x();
+            double y=barOffset.y();
+            double z=barOffset.z();
+
+            //boost::shared_ptr<ComponentInfo> info(new ComponentInfo());
+            std::string c=Form("CRV Scintillator %s  module %i  layer %i  bar %i  (index %i)",shieldName.c_str(),im,il,ib, index);
+            //info->setName(c.c_str());
+            //info->setText(0,c.c_str());
+            //info->setText(1,Form("Dimension x: %.f mm, y: %.f mm, z: %.f mm",2.0*dx/CLHEP::mm,2.0*dy/CLHEP::mm,2.0*dz/CLHEP::mm));
+            //info->setText(2,Form("Center at x: %.f mm, y: %.f mm, z: %.f mm",x/CLHEP::mm,y/CLHEP::mm,z/CLHEP::mm));
+            std::cout<<" c = "<<c<<std::endl;
+		  
+            //boost::shared_ptr<Cube> shape(new Cube(x,y,z,  dx,dy,dz,  0, 0, 0, NAN, _geometrymanager, _topvolume, _mainframe, info, true));
+            //_components.push_back(shape);
+            //_crvscintillatorbars[index]=(shape);
+          }
+        }
+      }
+    }
+   
     const std::string TopSectorNames[] = {"T1", "T2", "T3", "T4"};
     for (unsigned int i=0; i<4; i++){
 	    Double_t panelpos[3];
@@ -35,8 +83,8 @@ namespace mu2e{
       SimpleConfig Config(filename);
       std::vector<double> Center;
       if(i==0)  {Config.getVectorDouble("crs.firstCounterT1", Center); orthodetT1->AddElement(sectorshape);}
-      if(i==1)  {Config.getVectorDouble ("crs.firstCounterT2",Center); orthodetT2->AddElement(sectorshape);}
-      if(i==2)  Config.getVectorDouble("crs.firstCounterT3", Center);
+      if(i==1)  Config.getVectorDouble ("crs.firstCounterT2",Center); 
+      if(i==2)  {Config.getVectorDouble("crs.firstCounterT3", Center);orthodetT2->AddElement(sectorshape);}
       if(i==3)  Config.getVectorDouble("crs.firstCounterT4", Center) ;  
       std::cout<<"CRV "<<std::endl;
       topvol->AddNode(crv0, 1, new TGeoTranslation(pointmmTocm(Center[0]),pointmmTocm(Center[1]),pointmmTocm(Center[2]/10)));
