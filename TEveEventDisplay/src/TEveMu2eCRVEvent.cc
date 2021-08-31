569d215c60b0440e0bfc64dd3e09f74073aba70c
NamithaChitrazee <namitha@pi.infn.it>
2021-08-31 20:04:36 +0200

    Update TEveMu2eCRVEvent.cc

--- TEveEventDisplay/src/TEveMu2eCRVEvent.cc
+++ TEveEventDisplay/src/TEveMu2eCRVEvent.cc
@@ -45,7 +45,7 @@ namespace mu2e{
             const CrvRecoPulse &crvRecoPulse = crvcoincol->at(i);
            const CRSScintillatorBarIndex &crvBarIndex = crvRecoPulse.GetScintillatorBarIndex();
            if(index== crvBarIndex)
-           {
+           {std::cout<<"index = "<<index<<std::endl;
             CLHEP::Hep3Vector barOffset = bar.getPosition() - _detSysOrigin;
             double sibarpos[0]=barOffset.x();
             double sibarpos[1]=barOffset.y()+1000.0;
@@ -55,16 +55,31 @@ namespace mu2e{
             sposi.set(sibarpos[0]-dx, sibarpos[1]-dy, sibarpos[2]-dz);
             sposf.set(sibarpos[0]+dx, sibarpos[1]+dy, sibarpos[2]+dz);
            }
-	   return {sposi, sposf, strawtitle, colorid};
+	   
            }
 	 }
        }  
       }
       }
+	  return {sposi, sposf, strawtitle, colorid};
   }
   /*------------Function to 3D draw hits:-------------*/
   void TEveMu2eCRVEvent::DrawHit3D(const std::string &pstr, Int_t n, CLHEP::Hep3Vector pointInMu2e, TEveElementList *HitList)
   {
+	  auto [sposi, sposf, title, colorid] = DrawSciBar();
+    if(sposi.x()!=0){
+      GeomHandle<DetectorSystem> det;
+      CLHEP::Hep3Vector sposin = det->toMu2e(sposi);
+      CLHEP::Hep3Vector sposfn = det->toMu2e(sposf);
+      TEveMu2eCustomHelix *line = new TEveMu2eCustomHelix();
+      line->SetLineWidth(1);
+      line->SetPoint(0,sposin.x(),sposin.y(),sposin.z());
+      line->SetNextPoint(sposfn.x(),sposfn.y(),sposfn.z());
+      line->SetLineColor(colorid);
+      line->SetTitle(Form(title.c_str()));
+      HitList->AddElement(line);
+    }
+	    
     this->SetTitle((DataTitle(pstr, n)).c_str());
     this->SetNextPoint(pointInMu2e.x(), pointInMu2e.y(), pointInMu2e.z()); 
     this->SetMarkerColor(mColor_);
@@ -76,7 +91,26 @@ namespace mu2e{
   /*------------Function to 2D draw hits:-------------*/
   void TEveMu2eCRVEvent::DrawHit2D(const std::string &pstr, Int_t n, CLHEP::Hep3Vector pointInMu2e, TEveElementList *HitList)
   {
-    this->SetTitle((DataTitle(pstr, n)).c_str());
+    auto [sposi, sposf, title, colorid] = DrawSciBar();
+    if(sposi.x()!=0){
+      TEveMu2eCustomHelix *line_twoDstrawXY = new TEveMu2eCustomHelix();
+      line_twoDstrawXY->SetLineWidth(1);
+      line_twoDstrawXY->SetPoint(0,pointmmTocm(sposi.x()),pointmmTocm(sposi.y()),pointmmTocm(sposi.z()));
+      line_twoDstrawXY->SetNextPoint(pointmmTocm(sposf.x()),pointmmTocm(sposf.y()),pointmmTocm(sposf.z()));
+      line_twoDstrawXY->SetLineColor(colorid);
+      line_twoDstrawXY->SetTitle(Form(title.c_str()));
+      HitList->AddElement(line_twoDstrawXY);
+      
+      TEveMu2eCustomHelix *line_twoDstrawXZ = new TEveMu2eCustomHelix();
+      line_twoDstrawXZ->SetLineWidth(1);
+      line_twoDstrawXZ->SetPoint(0,pointmmTocm(sposi.x()),pointmmTocm(sposi.y())+ 1000,pointmmTocm(sposi.z()));
+      line_twoDstrawXZ->SetNextPoint(pointmmTocm(sposf.x()),pointmmTocm(sposf.y())+ 1000,pointmmTocm(sposf.z()));
+      line_twoDstrawXZ->SetLineColor(colorid);
+      line_twoDstrawXZ->SetTitle(Form(title.c_str()));
+      HitList->AddElement(line_twoDstrawXZ);
+    }
+	  
+	  this->SetTitle((DataTitle(pstr, n)).c_str());
     hep3vectorTocm(pointInMu2e);
     this->SetNextPoint(pointInMu2e.x(), pointInMu2e.y(), pointInMu2e.z()); 
     this->SetMarkerColor(mColor_);
