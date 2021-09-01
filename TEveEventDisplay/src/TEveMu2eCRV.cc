90e0b5ac41ea4152a3da136f75fc3c2f66f6aacb
NamithaChitrazee <namitha@pi.infn.it>
2021-09-01 21:01:58 +0200

    Update TEveMu2eCRV.cc

--- TEveEventDisplay/src/TEveMu2eCRV.cc
+++ TEveEventDisplay/src/TEveMu2eCRV.cc
@@ -8,7 +8,7 @@ namespace mu2e{
   void TEveMu2eCRV::DrawCRVDetector(art::Run const& run, TGeoVolume* topvol, TEveElementList *orthodetT1, TEveElementList *orthodetT2){
     TGeoMaterial *matSi = new TGeoMaterial("Si", 28.085,14,2.33);
     TGeoMedium *Si = new TGeoMedium("Silicon",2, matSi);
-    CLHEP::Hep3Vector _detSysOrigin = mu2e::GeomHandle<mu2e::DetectorSystem>()->getOrigin();
+   // CLHEP::Hep3Vector _detSysOrigin = mu2e::GeomHandle<mu2e::DetectorSystem>()->getOrigin();
     std::vector<double> halflen;
     CLHEP::Hep3Vector position;
     CosmicRayShield const &CRS = *(GeomHandle<CosmicRayShield>());
@@ -36,13 +36,13 @@ namespace mu2e{
           int nBars = layer.nBars();
           for (int ib = 0; ib < nBars; ++ib)
           { Double_t sibarpos[3];
-            CRSScintillatorBar const & bar = layer.getBar(ib);
+ CRSScintillatorBar const & bar = layer.getBar(ib);
             int index = bar.index().asInt();
-            CLHEP::Hep3Vector barOffset = bar.getPosition() - _detSysOrigin;
+            CLHEP::Hep3Vector barOffset = bar.getPosition();// - _detSysOrigin;
             sibarpos[0]=barOffset.x();
             sibarpos[1]=barOffset.y();// +1000.0;
             sibarpos[2]=barOffset.z();
-
+      
             //boost::shared_ptr<ComponentInfo> info(new ComponentInfo());
             std::string c=Form("CRV Scintillator %s  module %i  layer %i  bar %i  (index %i)",shieldName.c_str(),im,il,ib, index);
             TEveGeoShape *sibar = new TEveGeoShape();
@@ -50,7 +50,7 @@ namespace mu2e{
         sibar->SetMainTransparency(100);
         
         // if(strcmp(shieldName.c_str(), shieldside.c_str()) > 0) {std::cout<<shieldName<<std::endl;}
-         if(shieldName.compare(4,1, shieldside, 4,1) == 0){ 
+         if(shieldName.compare(4,1, shieldside, 4,1) == 0){
           orthodetT2->AddElement(sibar);
           }
           }
@@ -67,7 +67,7 @@ namespace mu2e{
         panelpos [0] = position.x();
         panelpos [1] = position.y();
         panelpos [2] = position.z();
-            
+      
       TEveGeoShape *sectorshape = new TEveGeoShape();
       sectorshape->SetShape(new TGeoBBox("sectorshape",pointmmTocm(2*halflen[0]), pointmmTocm(2*halflen[2]), pointmmTocm(2*halflen[1]),panelpos));
       sectorshape->SetMainTransparency(100);
@@ -78,7 +78,7 @@ namespace mu2e{
       std::string filename("Mu2eG4/geom/crv_counters_v07.txt");
       SimpleConfig Config(filename);
       std::vector<double> Center;
-      if(i==0)  Config.getVectorDouble("crs.firstCounterT1", Center); 
+      if(i==0)  Config.getVectorDouble("crs.firstCounterT1", Center);
       if(i==1)  Config.getVectorDouble ("crs.firstCounterT2",Center);
       if(i==2)  Config.getVectorDouble("crs.firstCounterT3", Center);
       if(i==3)  Config.getVectorDouble("crs.firstCounterT4", Center) ;
@@ -86,8 +86,5 @@ namespace mu2e{
        topvol->AddNode(crv0, 1, new TGeoTranslation(pointmmTocm(Center[0]),pointmmTocm(Center[1]),pointmmTocm(Center[2]/10)));
     }
   }
-}           
+}
       
-      
-
-
