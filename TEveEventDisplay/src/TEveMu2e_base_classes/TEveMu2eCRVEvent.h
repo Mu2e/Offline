2365228d3e60c6b6a22c8c37efbe3df4b1a4e156
NamithaChitrazee <namitha@pi.infn.it>
2021-09-02 15:04:57 +0200

    Update TEveMu2eCRVEvent.h

--- TEveEventDisplay/src/TEveMu2e_base_classes/TEveMu2eCRVEvent.h
+++ TEveEventDisplay/src/TEveMu2e_base_classes/TEveMu2eCRVEvent.h
@@ -1,4 +1,4 @@
-#ifndef TEveMu2eCRVEvent_h
+ifndef TEveMu2eCRVEvent_h
 #define TEveMu2eCRVEvent_h
 
 #include <TObject.h>
@@ -11,7 +11,7 @@
 #include "Offline/GeometryService/inc/GeomHandle.hh"
 #include "Offline/RecoDataProducts/inc/CrvRecoPulse.hh"
 #include "Offline/TEveEventDisplay/src/TEveMu2e_base_classes/TEveMu2eCustomHelix.h"
- 
+
 namespace mu2e {
   class TEveMu2eCRVEvent : public TEvePointSet {
     public:
@@ -20,15 +20,15 @@ namespace mu2e {
       TEveMu2eCRVEvent(CrvRecoPulse chit) : fCrvRecoPulse_(chit){};
       virtual ~TEveMu2eCRVEvent(){};
 
-      CrvCoincidenceCluster fCrvCoincidenceCluster_; 
+      CrvCoincidenceCluster fCrvCoincidenceCluster_;
       CrvRecoPulse fCrvRecoPulse_;
 
       Int_t mColor_ = kBlue;
-      Int_t mSize_ = 1; 
+      Int_t mSize_ = 1;
       bool AddErrorBar_ = true;
       std::tuple<CLHEP::Hep3Vector, CLHEP::Hep3Vector, std::string, int> DrawSciBar();
-      void DrawHit2D(const std::string &pstr, Int_t b,CLHEP::Hep3Vector HitPos, TEveElementList *list); 
-      void DrawHit3D(const std::string &pstr, Int_t b,CLHEP::Hep3Vector HitPos, TEveElementList *list); 
+      void DrawHit2D(const std::string &pstr, Int_t b,CLHEP::Hep3Vector HitPos, TEveElementList *list2DXY, TEveElementList *list2DXZ);
+      void DrawHit3D(const std::string &pstr, Int_t b,CLHEP::Hep3Vector HitPos, TEveElementList *list3D);
       std::string DataTitle(const std::string &pstr, int n);
       #endif
       ClassDef(TEveMu2eCRVEvent, 0);
@@ -36,3 +36,5 @@ namespace mu2e {
   typedef std::vector<mu2e::TEveMu2eCRVEvent> TEveMu2eCRVEventCollection;
 }
 #endif
+
+
