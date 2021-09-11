96740cb942bfb8fc5d8660be9d3397474f6bfaae
NamithaChitrazee <namitha@pi.infn.it>
2021-09-11 16:29:23 +0200

    Update TEveMu2eCRVEvent.h

--- TEveEventDisplay/src/TEveMu2e_base_classes/TEveMu2eCRVEvent.h
+++ TEveEventDisplay/src/TEveMu2e_base_classes/TEveMu2eCRVEvent.h
@@ -5,12 +5,16 @@
 
 #include <TEvePointSet.h>
 #include <TEveLine.h>
+#include <TEveGeoShape.h>
+#include <TGeoShape.h>
+#include <TGeoBBox.h>
 #include "Offline/RecoDataProducts/inc/CrvCoincidenceCluster.hh"
 #include "Offline/CosmicRayShieldGeom/inc/CosmicRayShield.hh"
 #include "Offline/DataProducts/inc/CRSScintillatorBarIndex.hh"
 #include "Offline/GeometryService/inc/GeomHandle.hh"
 #include "Offline/RecoDataProducts/inc/CrvRecoPulse.hh"
 #include "Offline/TEveEventDisplay/src/TEveMu2e_base_classes/TEveMu2eCustomHelix.h"
+#include "Offline/TEveEventDisplay/src/TEveMu2e_base_classes/TEveMu2e2DProjection.h"
 
 namespace mu2e {
   class TEveMu2eCRVEvent : public TEvePointSet {
@@ -26,8 +30,8 @@ namespace mu2e {
       Int_t mColor_ = kBlue;
       Int_t mSize_ = 1;
       bool AddErrorBar_ = true;
-      std::tuple<CLHEP::Hep3Vector, CLHEP::Hep3Vector, std::string, int> DrawSciBar();
-      void DrawHit2D(const std::string &pstr, Int_t b,CLHEP::Hep3Vector HitPos, TEveElementList *list2DXY, TEveElementList *list2DXZ);
+      std::tuple<std::array<double, 3>, std::array<double, 3>> DrawSciBar();
+      void DrawHit2D(const std::string &pstr, Int_t b,CLHEP::Hep3Vector HitPos, TEveElementList *list2DXY, TEveElementList *list2DYZ);
       void DrawHit3D(const std::string &pstr, Int_t b,CLHEP::Hep3Vector HitPos, TEveElementList *list3D);
       std::string DataTitle(const std::string &pstr, int n);
       #endif
