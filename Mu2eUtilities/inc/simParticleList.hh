4132c305b84401b33eb320a5e0f9c3177e3c85e4
sophieMu2e <sophie@fnal.gov>
2021-10-10 16:01:18 -0500

    added pion lists

--- Mu2eUtilities/inc/simParticleList.hh
+++ Mu2eUtilities/inc/simParticleList.hh
@@ -32,6 +32,11 @@ namespace mu2e {
     // G4 sets this end code for both decay and capture cases
     return simParticleList(simh, PDGCode::mu_minus, ProcessCode::muMinusCaptureAtRest);
   }
+  
+  inline std::vector<art::Ptr<SimParticle> > stoppedPiMinusonList(art::ValidHandle<SimParticleCollection> simh) {
+    // G4 sets this end code for both decay and capture cases
+    return simParticleList(simh, PDGCode::pi_minus, ProcessCode::mu2ePionCaptureAtRest);
+  }
 
 }
 
