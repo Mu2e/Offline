4a0b92c8c872977855bf66c809d6df8644cc1709
NamithaChitrazee <namitha@pi.infn.it>
2021-11-03 22:07:06 +0100

    Update TEveEventDisplay_module.cc

--- TEveEventDisplay/src/TEveEventDisplay_module.cc
+++ TEveEventDisplay/src/TEveEventDisplay_module.cc
@@ -101,7 +101,7 @@ namespace mu2e
     } 
     //construct GUI:
     std::cout<<"CRV coming in "<<_filler.addCrvHits_<<std::endl;
-    DrawOptions DrawOpts(_filler.addCrvHits_, _filler.addCosmicSeedFit_, _filler.addTracks_, _filler.addClusters_, _filler.addHits_, false, _filler.addMCTraj_); 
+    DrawOptions DrawOpts(_filler.addCrvHits_, _filler.addCosmicSeedFit_, _filler.addTracks_, _filler.addClusters_, _filler.addHits_, _filler.addTrkHits, false, _filler.addMCTraj_); 
     std::cout<<"CRV coming in "<<DrawOpts.addCRVInfo<<std::endl;
     _frame = new TEveMu2eMainWindow(gClient->GetRoot(), 1000,600, _pset, DrawOpts);
     //build 2D geometries (now optional):
