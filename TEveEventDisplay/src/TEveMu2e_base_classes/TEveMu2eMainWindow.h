3ad5161ce1067a4384b2632a233d0d5b28d76b82
NamithaChitrazee <namitha@pi.infn.it>
2021-11-14 20:36:48 +0100

    Update TEveMu2eMainWindow.h

--- TEveEventDisplay/src/TEveMu2e_base_classes/TEveMu2eMainWindow.h
+++ TEveEventDisplay/src/TEveMu2e_base_classes/TEveMu2eMainWindow.h
@@ -84,8 +84,8 @@ namespace mu2e{
     bool addCryHits = false;
     bool addMCTraj = false;
     DrawOptions(){};
-    DrawOptions(bool crv, bool cosmictracks, bool tracks, bool clusters, bool combohits, bool trkhits, bool cryhits, bool mctraj) 
-    : addCRVInfo(crv), addCosmicTracks(cosmictracks), addTracks(tracks), addClusters(clusters), addComboHits(combohits), addTrkHits(trkhits), addCryHits(cryhits), addMCTraj(mctraj) {};
+    DrawOptions(bool crv, bool cosmictracks, bool tracks, bool clusters, bool combohits, bool trkhits, bool timeclusters, bool cryhits, bool mctraj) 
+    : addCRVInfo(crv), addCosmicTracks(cosmictracks), addTracks(tracks), addClusters(clusters), addComboHits(combohits), addTrkHits(trkhits), addTimeClusters(timeclusters), addCryHits(cryhits), addMCTraj(mctraj) {};
    };
    
 	class TEveMu2eMainWindow : public TGMainFrame {
