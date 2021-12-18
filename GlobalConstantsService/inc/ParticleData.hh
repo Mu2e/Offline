9b549c58a8958252c7fb64f21f44529039ff732b
Ray Culbertson <rlc@fnal.gov>
2021-12-17 20:33:26 -0600

    start alternate particle list service

---
+++ GlobalConstantsService/inc/ParticleData.hh
@@ -0,0 +1,63 @@
+#ifndef GlobalConstantsService_ParticleData_hh
+#define GlobalConstantsService_ParticleData_hh
+//
+// Partcile data loaded from ParticleDataList
+//
+// the content is
+//  - PDG ID
+//  - displayname (includes special chars like +-*)
+//  - progamming name (includes only alphanumeric and _)
+//        this should be the same as  DataProducts/inc/PDGCode.hh
+//  - charge (units of electron charge)
+//  - mass (MeV)
+//  - lifetime (ns)
+//
+
+#include <iostream>
+#include <iomanip>
+
+#include "Offline/GlobalConstantsService/inc/ParticleData.hh"
+
+namespace mu2e {
+
+
+  class ParticleData {
+
+  public:
+
+    ParticleData(int id, const std::string& name, const std::string& codeName,
+                 float charge, float mass, float lifetime): 
+      _id(id),_name(name),_codeName(codeName),_charge(charge),
+      _mass(mass),_lifetime(lifetime) {}
+
+    int id() const { return _id; }
+    const std::string& name() const { return _name; }
+    const std::string& displayName() const { return _name; }
+    const std::string& codeName() const { return _codeName; }
+    float charge() const { return _charge; }
+    float mass() const { return _mass; }
+    float lifetime() const { return _lifetime; }
+
+    void print( std::ostream& ostr=std::cout) const {
+      ostr << std::setw(11) << _id;
+      ostr << std::setw(22) << _name;
+      ostr << std::setw(35) << _codeName;
+      ostr << std::setw(10) << _charge;
+      ostr << std::setw(12) << _mass;
+      ostr << std::setw(12) << _lifetime;
+      ostr << std::endl;
+    }
+
+  private:
+    int _id;
+    std::string _name;
+    std::string _codeName;
+    float _charge;
+    float _mass;
+    float _lifetime;
+
+  };  // ParticleData
+
+} //end namespace mu2e
+
+#endif /* GlobalConstantsService_ParticleData_hh */
