b992420be31251f4874950f5b152f47b6e9a2ef8
David Nathan Brown <dave_brown@lbl.gov>
2022-02-06 11:22:17 -0600

    Updates for KinKal changes.  Remove updater miter-iter index

--- Mu2eKinKal/src/KKFitSettings.cc
+++ Mu2eKinKal/src/KKFitSettings.cc
@@ -21,23 +21,22 @@ namespace mu2e {
       config.bfcorr_ = fitconfig.bfieldCorr();
       config.tol_ = fitconfig.btol();
       // set the schedule for the meta-iterations
-      unsigned nmiter(0);
       for(auto const& misetting : fitconfig.miConfig()) {
         MetaIterConfig mconfig;
-        mconfig.miter_ = nmiter++;
         mconfig.temp_ = std::get<0>(misetting);
         config.schedule_.push_back(mconfig);
       }
-      // create the updaters requested
+      // create the updaters requested; these don't have to exist, but if they
+      // do, their dimension must match the meta-iterations
+      if(fitconfig.shuConfig().size() >0 && fitconfig.shuConfig().size() != config.schedule_.size())
+        throw cet::exception("RECO")<<"mu2e::KKFitSettings: KKStrawHitUpdaters don't match meta-iterations" << std::endl;
+      size_t imeta(0);
       for(auto const& shusetting : fitconfig.shuConfig()) {
-        size_t imeta = std::get<3>(shusetting);
-        if(imeta >= config.schedule_.size())
-          throw cet::exception("RECO")<<"mu2e::KKFitSettings: Invalid Meta-iteration for KKStrawHitUpdater" << std::endl;
         double mindoca = std::get<0>(shusetting);
         double maxdoca= std::get<1>(shusetting);
         double minprob = std::get<2>(shusetting);
         KKStrawHitUpdater shupdater(mindoca,maxdoca,minprob);
-        config.schedule_[imeta].updaters_.push_back(shupdater);
+        config.schedule_[imeta++].updaters_.push_back(shupdater);
       }
       return config;
     }
