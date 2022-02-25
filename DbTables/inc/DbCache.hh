6ec02bbd4f23ae9f391dcbf95a378a21d7d91ff8
Ray Culbertson <rlc@fnal.gov>
2022-02-25 17:37:04 -0600

    improve cache, add real purging

--- DbTables/inc/DbCache.hh
+++ DbTables/inc/DbCache.hh
@@ -1,18 +1,26 @@
 #ifndef DbTables_DbCache_hh
 #define DbTables_DbCache_hh
 
-#include <iostream>
-#include <iomanip>
-#include <map>
+// A cache of DbTables. As tables are read form the database, they are held
+// here for a while in case they are needed, for example, by different threads.
+// Eventually a tbale may be purged.  If a needed table is purged, 
+// it can always be re-read.
+
 #include "Offline/DbTables/inc/DbTable.hh"
+#include <map>
+#include <queue>
 
 namespace mu2e {
 
   class DbCache {
   public:
 
-    typedef std::map<int,mu2e::DbTable::cptr_t> table_map;
 
+    DbCache():_limitSize(200000000),  // 200 MB
+              _hwmSize(0),_size(0),_nPurges(0),_nPurged(0),
+              _nAdded(0),_nCall(0),_nHit(0) {}
+
+    void setLimitSize(int64_t limitSize) { _limitSize = limitSize; }
     void add(int cid, mu2e::DbTable::cptr_t const& ptr);
 
     bool hasTable(int cid) { return _tables.find(cid)!=_tables.end(); }
@@ -20,13 +28,28 @@ namespace mu2e {
     mu2e::DbTable::cptr_t get(int cid);
 
     void clear() { _tables.clear(); }
-    int purge(const size_t target=200000000);
-    size_t size();
+    int64_t size() const { return _size;}
     void print() const;
+    void printStats() const;
 
   private:
+
+    typedef std::map<int,mu2e::DbTable::cptr_t> table_map;
+
+    void purge();
+
     table_map _tables;
 
+    // items needed for monitoring and purging
+    int64_t _limitSize;
+    int64_t _hwmSize;
+    int64_t _size;
+    std::queue<int> _cidFifo;
+    int _nPurges;
+    int _nPurged;
+    int _nAdded;
+    int _nCall;
+    int _nHit;
   };
 
 }
