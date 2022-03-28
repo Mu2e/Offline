fb386e519bad9c1f58ec4fe2c6dd15fd5c6ffd67
sophieMu2e <sophie@fnal.gov>
2022-03-28 13:13:37 -0500

    name change

--- DbTables/inc/CalSourceCalibTable.hh
+++ DbTables/inc/CalSourceCalibTable.hh
@@ -1,5 +1,5 @@
-#ifndef DbTables_CaloSourceCalibTable_hh
-#define DbTables_CaloSourceCalibTable_hh
+#ifndef DbTables_CalSourceCalibTable_hh
+#define DbTables_CalSourceCalibTable_hh
 
 
 #include <string>
@@ -10,29 +10,29 @@
 
 namespace mu2e {
 
-  class CaloSourceCalibTable : public DbTable {
+  class CalSourceCalibTable : public DbTable {
   public:
 
     class Row {
     public:
-      Row(int roid, double Peak, double Esource, double chisq):
-	_roid(roid),_Peak(Peak),_Esource(Esource), _chisq(chisq) {}
-      int       roid() const { return _roid;} //TODO: does this want to be Offline ID?
-      float     Peak() const { return _Peak; }
-      float     Esource() const { return _Esource; }//TODO do we want Esource...its always the same should we remove it for simplicity
+      Row(int roid, double EPeak, double Esource, double chisq):
+	_roid(roid),_EPeak(EPeak),_Esource(Esource), _chisq(chisq) {}
+      int       roid() const { return _roid;} //this will be Offline ID?
+      float     EPeak() const { return _EPeak; }
+      float     Esource() const { return _Esource; }
       float     chisq() const { return _chisq; }
 
     private:
       int   _roid; // assume Offline ID (otherwise needs to be Long_64)
-      float _Peak;
+      float _EPeak;
       float _Esource;
       float _chisq;
     };
 
-    constexpr static const char* cxname = "CaloSourceCalibTable";
+    constexpr static const char* cxname = "CalSourceCalibTable";
 
-    CaloSourceCalibTable():DbTable(cxname,"calosourcecalib",
-			"roid,Peak,Esource,chisq") {}
+    CalSourceCalibTable():DbTable(cxname,"cal.sourcecalib",
+			"roid,EPeak,Esource,chisq") {}
 
     const Row& rowAt(const std::size_t index) const { return _rows.at(index);}
     const Row& row(const int roid) const { 
@@ -42,11 +42,11 @@ namespace mu2e {
     size_t size() const override { return baseSize() + nrow()*nrow()/2 + nrow()*sizeof(Row); };
 
     void addRow(const std::vector<std::string>& columns) override {
-      int roid = std::stoi(columns[0]); //ROid?
+      int roid = std::stoi(columns[0]);
       // TODO - do we need this:
       if(roid!=int(_rows.size())) {
 	      throw cet::exception("CALOSOURCECALIB_BAD_INDEX") 
-	        << "CaloSourceCalibTable::addRow found index out of order: " 
+	        << "CalSourceCalibTable::addRow found index out of order: " 
 	        <<roid << " != " << _rows.back().roid()+1 <<"\n";
       }
        _rows.emplace_back(roid,
@@ -61,7 +61,7 @@ namespace mu2e {
       Row const& r = _rows.at(irow);
       sstream << std::fixed << std::setprecision(5);
       sstream << r.roid()<<",";
-      sstream << r.Peak()<<",";
+      sstream << r.EPeak()<<",";
       sstream << r.Esource()<<",";
       sstream << r.chisq();
     }
