3509c81eead4ff006eece15ff78088ef7def3f85
Gianantonio Pezzullo <giani.pezzu@gmail.com>
2022-03-30 19:42:08 -0500

    update in the phiPrescaler class

--- GeneralUtilities/inc/PhiPrescalingParams.hh
+++ GeneralUtilities/inc/PhiPrescalingParams.hh
@@ -1,21 +1,34 @@
 #ifndef GeneralUtilities_PhiPrescalingParams_hh
 #define GeneralUtilities_PhiPrescalingParams_hh
 
+#include "fhiclcpp/types/Atom.h"
+
 namespace mu2e {
   struct PhiPrescalingParams {
     float    _amplitude;
     float    _frequency;
     float    _phase;
 
+    struct Config {
+      fhicl::Atom<int> amplitude{fhicl::Name("amplitude"), fhicl::Comment("amplitude")};
+      fhicl::Atom<int> frequency{fhicl::Name("frequency"), fhicl::Comment("frequency")};
+      fhicl::Atom<int> phase    {fhicl::Name("phase"),     fhicl::Comment("phase")};
+    };
+    
     PhiPrescalingParams():
       _amplitude(0),
       _frequency(0),
       _phase(0) {}
-  
+    
     PhiPrescalingParams(float Amplitude, float Frequency, float Phase):
       _amplitude(Amplitude),
       _frequency(Frequency),
-      _phase(Phase) {}
+      _phase    (Phase) {}
+    
+    PhiPrescalingParams(const Config& conf): 
+      _amplitude(conf.amplitude()),
+      _frequency(conf.frequency()),
+      _phase    (conf.phase()) {}
   };
 
 }
