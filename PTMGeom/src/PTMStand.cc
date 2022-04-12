4f63c9579ba30be546d830cd2c87e8915ba58438
Helenka Casler <hcasler@gradcenter.cuny.edu>
2022-04-12 18:10:08 -0400

    added internals for PTMStand class for the top wedge and support column

--- PTMGeom/src/PTMStand.cc
+++ PTMGeom/src/PTMStand.cc
@@ -6,11 +6,24 @@
 //
 
 namespace mu2e{
-  PTMStand::PTMStand(CLHEP::Hep3Vector const& originInMu2e, 
-          CLHEP::HepRotation const& rotationInMu2e)
-          : _originInMu2e(originInMu2e),
-            _rotationInMu2e(rotationInMu2e) {
-    // TODO: fill in the other stuff
+  PTMStand::PTMStand(std::shared_ptr<ExtrudedSolid> topWedge,
+          std::shared_ptr<Box> wedgeCutout,
+          CLHEP::Hep3Vector wedgeCutoutRelPosition,
+          CLHEP::HepRotation wedgeCutoutRelRotation,
+          std::shared_ptr<Box> columnExtrusion,
+          std::vector<CLHEP::Hep3Vector> columnOriginsInMu2e, 
+          std::vector<CLHEP::HepRotation> columnRotationsInMu2e,
+          std::string columnMateriaName,
+          std::string wedgeMaterialName)
+          : _topWedge(topWedge),
+            _wedgeCutout(wedgeCutout),
+            _wedgeCutoutRelPosition(wedgeCutoutRelPosition),
+            _wedgeCutoutRelRotation(wedgeCutoutRelRotation),
+            _columnExtrusion(columnExtrusion),
+            _columnOriginsInMu2e(columnOriginsInMu2e), 
+            _columnRotationsInMu2e(columnRotationsInMu2e),
+            _columnMateriaName(columnMateriaName),
+            _wedgeMaterialName(wedgeMaterialName) {
 
   }
 
