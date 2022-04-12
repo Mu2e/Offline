8da331d6ed2ba02bf07a2de8ea223268f7acb32f
Helenka Casler <hcasler@gradcenter.cuny.edu>
2022-04-12 18:10:08 -0400

    added internals for PTMStand class for the top wedge and support column

--- PTMGeom/inc/PTMStand.hh
+++ PTMGeom/inc/PTMStand.hh
@@ -6,6 +6,9 @@
 #include "CLHEP/Vector/Rotation.h"
 #include "CLHEP/Vector/ThreeVector.h"
 
+#include "Offline/GeomPrimitives/inc/ExtrudedSolid.hh"
+#include "Offline/GeomPrimitives/inc/Box.hh"
+
 // ProductionTarget Monitor Stand (PTM) 
 //
 // Author: Helenka Casler
@@ -22,19 +25,40 @@ namespace mu2e {
   class PTMStand {
 
   public:
-    PTMStand(CLHEP::Hep3Vector const& originInMu2e, 
-          CLHEP::HepRotation const& rotationInMu2e);
+    PTMStand(std::shared_ptr<ExtrudedSolid> topWedge,
+          std::shared_ptr<Box> wedgeCutout,
+          CLHEP::Hep3Vector wedgeCutoutRelPosition,
+          CLHEP::HepRotation wedgeCutoutRelRotation,
+          std::shared_ptr<Box> columnExtrusion,
+          std::vector<CLHEP::Hep3Vector> columnOriginsInMu2e, 
+          std::vector<CLHEP::HepRotation> columnRotationsInMu2e,
+          std::string columnMateriaName,
+          std::string wedgeMaterialName);
     PTMStand() {}
 
-    CLHEP::Hep3Vector const &  originInMu2e()   const { return _originInMu2e; }
-    CLHEP::HepRotation const & rotationInMu2e() const { return _rotationInMu2e; }
+    const GenericTrap* topWedge() const { return _topWedge.get(); }
+    const Box* wedgeCutout() const { return _wedgeCutout.get(); }
+    CLHEP::Hep3Vector const & wedgeCutoutRelPosition() const { return _wedgeCutoutRelPosition; }
+    CLHEP::HepRotation const & wedgeCutoutRelRotation() const { return _wedgeCutoutRelRotation; }
+    const Box* columnExtrusion() const {return _columnExtrusion.get(); }
+    const std::vector<CLHEP::Hep3Vector> columnOriginsInMu2e()   const { return _columnOriginsInMu2e; }
+    const std::vector<CLHEP::HepRotation> columnRotationsInMu2e() const { return _columnOriginsInMu2e; }
+    const std::string columnMateriaName() const { return _columnMaterialName; }
+    const std::string wedgeMaterialName() const { return _wedgeMaterialName; }
 
 
 
 
   private:
-    CLHEP::Hep3Vector _originInMu2e;
-    CLHEP::HepRotation _rotationInMu2e;
+    std::shared_ptr<ExtrudedSolid> _topWedge; // contains its own origin in Mu2e and material name
+    std::shared_ptr<Box> _wedgeCutout;
+    CLHEP::Hep3Vector _wedgeCutoutRelPosition;
+    CLHEP::HepRotation _wedgeCutoutRelRotation;
+    std::shared_ptr<Box> _columnExtrusion;
+    std::vector<CLHEP::Hep3Vector> _columnOriginsInMu2e;
+    std::vector<CLHEP::HepRotation> _columnRotationsInMu2e;
+    std::string _columnMaterialName;
+    std::string _wedgeMaterialName;
 
 
 
