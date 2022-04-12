582f781e47395dad1b5f19f9265534f75648efe2
Helenka Casler <hcasler@gradcenter.cuny.edu>
2022-04-12 18:11:08 -0400

    new low-density aluminum material for support extrusion, small updates to PTM geom file

--- Mu2eG4/src/ConstructMaterials.cc
+++ Mu2eG4/src/ConstructMaterials.cc
@@ -570,6 +570,24 @@ namespace mu2e {
       A6105SmallExtrusion->AddMaterial(findMaterialOrThrow("G4_Zn"), 0.001);
     }
 
+    // 6105 Aluminum, "large" extrusion density
+    // https://en.wikipedia.org/wiki/6105_aluminium_alloy
+    // https://www.matweb.com/search/datasheet.aspx?matguid=9d1c81ac4e2b4e5590e5781f842b4446&ckck=1
+    // This has reduced density to make up for the fact that the Al extrusion is not a solid block
+    mat = uniqueMaterialOrThrow("A6105LargeExtrusion");
+    {
+      G4Material* A6105SmallExtrusion = new G4Material(mat.name, 2.69*0.39*CLHEP::g/CLHEP::cm3, 9);
+      A6105SmallExtrusion->AddMaterial(findMaterialOrThrow("G4_Al"), 0.973);
+      A6105SmallExtrusion->AddMaterial(findMaterialOrThrow("G4_Cr"), 0.001);
+      A6105SmallExtrusion->AddMaterial(findMaterialOrThrow("G4_Cu"), 0.001);
+      A6105SmallExtrusion->AddMaterial(findMaterialOrThrow("G4_Fe"), 0.0035);
+      A6105SmallExtrusion->AddMaterial(findMaterialOrThrow("G4_Mg"), 0.008);
+      A6105SmallExtrusion->AddMaterial(findMaterialOrThrow("G4_Mn"), 0.0015);
+      A6105SmallExtrusion->AddMaterial(findMaterialOrThrow("G4_Si"), 0.01);
+      A6105SmallExtrusion->AddMaterial(findMaterialOrThrow("G4_Ti"), 0.001);
+      A6105SmallExtrusion->AddMaterial(findMaterialOrThrow("G4_Zn"), 0.001);
+    }
+
     // NbTi
     mat = uniqueMaterialOrThrow( "NbTi"); // FIXME verify it
     {
