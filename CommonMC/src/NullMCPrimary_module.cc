2ba1a6ecd5cb9cfeacfddf4213bb0381e5be7b1e
David Nathan Brown <dave_brown@lbl.gov>
2022-04-13 13:18:59 -0500

    Add all products needed for NoPrimary to be a 'normal' primary

--- CommonMC/src/NullMCPrimary_module.cc
+++ CommonMC/src/NullMCPrimary_module.cc
@@ -1,5 +1,5 @@
 //
-//  Trivial module to insert an empty PrimaryParticle object in the event, used for NoPrimary production
+//  Trivial module to insert an empty PrimaryParticle object and related empty DetStep collections in the event, used for NoPrimary production
 //
 #include "art/Framework/Core/EDProducer.h"
 #include "art/Framework/Principal/Event.h"
@@ -8,6 +8,14 @@
 #include "Offline/MCDataProducts/inc/PrimaryParticle.hh"
 #include "Offline/MCDataProducts/inc/StepPointMC.hh"
 #include "Offline/MCDataProducts/inc/MCTrajectoryCollection.hh"
+#include "Offline/MCDataProducts/inc/GenParticle.hh"
+#include "Offline/MCDataProducts/inc/CaloShowerStep.hh"
+#include "Offline/MCDataProducts/inc/CrvStep.hh"
+#include "Offline/MCDataProducts/inc/StrawGasStep.hh"
+#include "Offline/MCDataProducts/inc/SimParticle.hh"
+#include "Offline/MCDataProducts/inc/MCTrajectory.hh"
+#include "Offline/MCDataProducts/inc/StepPointMC.hh"
+#include "Offline/MCDataProducts/inc/StatusG4.hh"
 #include <vector>
 #include <string>
 namespace mu2e {
@@ -28,9 +36,15 @@ namespace mu2e {
     extrasteps_(config().extraSteps())
     {
       produces <PrimaryParticle>();
+      produces <StatusG4>();
+      produces <GenParticleCollection>();
+      produces <SimParticleCollection>();
       produces <MCTrajectoryCollection>();
       for(auto extrastep : extrasteps_)
         produces <StepPointMCCollection>(extrastep);
+      produces <StrawGasStepCollection>();
+      produces <CrvStepCollection>();
+      produces <CaloShowerStepCollection>();
     }
 
   void NullMCPrimary::produce(art::Event& event) {
@@ -38,10 +52,16 @@ namespace mu2e {
     PrimaryParticle pp;
     MCTrajectoryCollection mctc;
     event.put(std::make_unique<PrimaryParticle>(pp));
+    event.put(std::make_unique<StatusG4>());
+    event.put(std::make_unique<GenParticleCollection>());
+    event.put(std::make_unique<SimParticleCollection>());
     event.put(std::make_unique<MCTrajectoryCollection>(mctc));
     StepPointMCCollection empty;
     for(auto extrastep : extrasteps_)
       event.put(std::make_unique<StepPointMCCollection>(empty),extrastep);
+    event.put(std::make_unique<StrawGasStepCollection>());
+    event.put(std::make_unique<CrvStepCollection>());
+    event.put(std::make_unique<CaloShowerStepCollection>());
   }
 }
 DEFINE_ART_MODULE(mu2e::NullMCPrimary)
