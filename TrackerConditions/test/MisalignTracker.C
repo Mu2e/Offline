26ae15bfbc12f649f163b6b2a4f210838e84b2a0
David Nathan Brown <dave_brown@lbl.gov>
2022-04-18 13:19:42 -0500

    Update MisalignTracker

--- TrackerConditions/test/MisalignTracker.C
+++ TrackerConditions/test/MisalignTracker.C
@@ -1,3 +1,7 @@
+//
+// generate parameters to misalign the tracker to include an overall twist (dphi/zdz), skew in x and y, and random mis-positions and angles
+// David Brown (LBNL)
+//
 #include "TRandom3.h"
 #include "TMath.h"
 #include <iostream>
@@ -5,58 +9,84 @@
 #include <vector>
 #include <algorithm>
 #include <string>
-// generate parameters to misalign the tracker to include an overall twist (dphi/zdz), skew in x and y, and random mis-positions and angles
-// David Brown (LBNL)
-//
 
 using namespace std;
-void MisalignTracker(double twist, double skew, double squeeze, double sigalpha, double sigpos, const char* outfile) {
+void MisalignTracker(int seed, double weakfactor, double trackerangle, double trackerpos,
+    double planeangle, double planepos,
+    double panelangle, double panelpos,
+    double wiredV, double wiredW,
+    const char* outfile="MisalignTracker.txt") {
   unsigned NPlanes(36);
+  unsigned NPanels(6);
+  unsigned NStraws(96);
   double planezgap(83.0); // approximate number
   double planer(700.0); // approximate number
   double trackerlen = NPlanes*planezgap; // approximate
 
-  TRandom3 myrand(3499803);
-  double rtwist = myrand.Uniform(-twist,twist);
-  double xskew = myrand.Uniform(-skew,skew);
-  double yskew = myrand.Uniform(-skew,skew);
-  double rsqueeze = myrand.Uniform(-squeeze,squeeze);
-  double dtwist = twist/(TMath::Pi()*2.0*planer*trackerlen);
+  TRandom3 myrand(seed);
+  // define weak modes from tracker parameters
+  double weakangle = trackerangle*weakfactor;
+  double weakpos = trackerpos*weakfactor;
+  double xskew = myrand.Uniform(-weakangle,weakangle);
+  double yskew = myrand.Uniform(-weakangle,weakangle);
+  double twist = myrand.Uniform(-weakangle,weakangle);
+  double zsqueeze = myrand.Uniform(-weakpos,weakpos);
+//  double rsqueeze = myrand.Uniform(-weakpos,weakpos);
 
   filebuf fb;
   fb.open (outfile,ios::out);
   ostream os(&fb);
   // write header
-  os << "# Tracker Misalignments from MisalignTracker.C, parameters: "
-  << " twist = "<< twist << " mm" 
-  << " skew = "<< skew << " mm" 
-  << " squeeze = "<< squeeze << " mm" 
-  << " rtwist = "<< rtwist << " mm" 
-  << " dtwist = " << dtwist << " rad/mm"
-  << " xskew = "<< xskew << " mm" 
-  << " yskew = "<< yskew << " mm" 
-  << " rsqueeze = "<< rsqueeze << " mm" 
-  << " sigalpha = "<< sigalpha << " rad" 
-  << " sigpos = "<< sigpos << " mm"
-  << endl;
+  os << "# Tracker Misalignments from MisalignTracker.C, parameters: " << endl
+    << "# weakfactor " << weakfactor <<  endl
+    << "# trackerangle " << trackerangle << " rad" << endl
+    << "# trackerpos " << trackerpos <<" mm" << endl
+    << "# planeangle = "<< planeangle << " rad" << endl
+    << "# planepos = "<< planepos << " mm" << endl
+    << "# panelangle = "<< panelangle << " rad" << endl
+    << "# panelpos = "<< panelpos << " mm" << endl
+    << "# wiredV = "<< wiredV << " mm" << endl
+    << "# wiredW = "<< wiredW << " mm" << endl
+    << "# twist = " << twist << " rad" << endl
+    << "# xskew = "<< xskew << " rad" << endl
+    << "# yskew = "<< yskew << " rad" << endl
+    << "# zsqueeze = "<< zsqueeze << " mm" << endl
+    // << " rsqueeze = "<< rsqueeze << " mm"  << endl// FIXME
+    << endl;
   for(unsigned ibuf=0;ibuf<3;ibuf++)
     os << "#" << endl;
 
-  // global track: dummy for now
-  os << " TABLE TrkAlignTracker " << endl;
-  os << "0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0" << endl;
+  // global tracker
+  os << "TABLE TrkAlignTracker " << endl;
+  os << "#row, strawid, dx, dy, dz, rx, ry, rz" << endl;
+  double dalphax = myrand.Gaus(0.0,trackerangle);
+  double dalphay = myrand.Gaus(0.0,trackerangle);
+  double dalphaz = myrand.Gaus(0.0,trackerangle);
+  double dx = myrand.Gaus(0.0,trackerpos);
+  double dy = myrand.Gaus(0.0,trackerpos);
+  double dz = myrand.Gaus(0.0,trackerpos);
+  os << "0, 0_0_0, "
+    << dx << ", "
+    << dy << ", "
+    << dz << ", "
+    << dalphax << ", "
+    << dalphay << ", "
+    << dalphaz << endl;
+
   for(unsigned ibuf=0;ibuf<3;ibuf++)
     os << "#" << endl;
- 
+
   os << "TABLE TrkAlignPlane" << endl;
+  os << "#row, strawid, dx, dy, dz, rx, ry, rz" << endl;
   for(unsigned iplane=0;iplane < NPlanes; ++iplane){
     double planez = (iplane-float(NPlanes)/2.0)*planezgap;
-    double dalphax = myrand.Gaus(0.0,sigalpha);
-    double dalphay = myrand.Gaus(0.0,sigalpha);
-    double dalphaz = myrand.Gaus(dtwist*planez,sigalpha);
-    double dx = myrand.Gaus(planez*xskew/trackerlen,sigpos);
-    double dy = myrand.Gaus(planez*yskew/trackerlen,sigpos);
-    double dz = myrand.Gaus(planez*rsqueeze/trackerlen,sigpos);
+    double zfactor = planez/trackerlen; // scale by z position
+    double dalphax = myrand.Gaus(xskew,planeangle);
+    double dalphay = myrand.Gaus(yskew,planeangle);
+    double dalphaz = myrand.Gaus(twist*zfactor,planeangle);
+    double dx = myrand.Gaus(xskew*zfactor,planepos);
+    double dy = myrand.Gaus(yskew*zfactor,planepos);
+    double dz = myrand.Gaus(zsqueeze*zfactor,planepos);
 
     os << iplane << ", " << iplane << "_0_0, "
       << dx << ", "
@@ -66,5 +96,53 @@ void MisalignTracker(double twist, double skew, double squeeze, double sigalpha,
       << dalphay << ", "
       << dalphaz << endl;
   }
+  for(unsigned ibuf=0;ibuf<3;ibuf++)
+    os << "#" << endl;
 
+  os << "TABLE TrkAlignPanel" << endl;
+  os << "#row, strawid, dU, dV, dW, rU, rV, rW" << endl;
+  int index(0);
+  for(unsigned iplane=0;iplane < NPlanes; ++iplane){
+    for(unsigned ipanel=0;ipanel < NPanels; ++ipanel){
+      double dalphax = myrand.Gaus(0,panelangle);
+      double dalphay = myrand.Gaus(0,panelangle);
+      double dalphaz = myrand.Gaus(0,panelangle);
+      // radial squeeze needs to know the panel phi, which i don't FIXME
+      double dx = myrand.Gaus(0.0,panelpos);
+      double dy = myrand.Gaus(0.0,panelpos);
+      double dz = myrand.Gaus(0.0,panelpos);
+
+      os << index << ", " << iplane << "_" << ipanel << "_0, "
+        << dx << ", "
+        << dy << ", "
+        << dz << ", "
+        << dalphax << ", "
+        << dalphay << ", "
+        << dalphaz << endl;
+       index++;
+   }
+  }
+  for(unsigned ibuf=0;ibuf<3;ibuf++)
+    os << "#" << endl;
+  os << "TABLE TrkAlignStraw" << endl;
+  os << "#index,StrawId,wire_cal_dV,wire_cal_dW,wire_hv_dV,wire_hv_dW,straw_cal_dV,straw_cal_dW,straw_hv_dV,straw_hv_dW" << endl;
+  index = 0;
+  for(unsigned iplane=0;iplane < NPlanes; ++iplane){
+    for(unsigned ipanel=0;ipanel<NPanels; ++ipanel){
+      for(unsigned istraw=0;istraw<NStraws; ++istraw){
+        double wire_cal_dV = myrand.Gaus(0.0,wiredV);
+        double wire_cal_dW= myrand.Gaus(0.0,wiredW);
+        double wire_hv_dV = myrand.Gaus(0.0,wiredV);
+        double wire_hv_dW= myrand.Gaus(0.0,wiredW);
+        double straw_cal_dV = myrand.Gaus(0.0,wiredV);
+        double straw_cal_dW= myrand.Gaus(0.0,wiredW);
+        double straw_hv_dV = myrand.Gaus(0.0,wiredV);
+        double straw_hv_dW= myrand.Gaus(0.0,wiredW);
+        os << index << ", " << iplane << "_" << ipanel << "_" << istraw << ", "
+          << wire_cal_dV << ", " << wire_cal_dW << ", " << wire_hv_dV << ", " << wire_hv_dW << ", "
+          << straw_cal_dV << ", " << straw_cal_dW << ", " << straw_hv_dV << ", " << straw_hv_dW << endl;
+        index++;
+      }
+    }
+  }
 }
