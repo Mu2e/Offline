e2ffe213fcc991fb0cd0a7c60c914491fc63707f
Gianantonio Pezzullo <giani.pezzu@gmail.com>
2022-04-23 17:49:45 -0500

    added the intensityInfo data-product

--- CaloReco/src/CaloHitMakerFast_module.cc
+++ CaloReco/src/CaloHitMakerFast_module.cc
@@ -11,18 +11,19 @@
 #include "Offline/RecoDataProducts/inc/CaloDigi.hh"
 #include "Offline/RecoDataProducts/inc/CaloHit.hh"
 #include "Offline/RecoDataProducts/inc/ProtonBunchTime.hh"
+#include "Offline/RecoDataProducts/inc/IntensityInfoCalo.hh"
 
 
 namespace
 {
-    struct HitInfo {
-       HitInfo(float time, float eDep) : nSiPM_(1), time_(time), eDep_(eDep) {}
+  struct HitInfo {
+    HitInfo(float time, float eDep) : nSiPM_(1), time_(time), eDep_(eDep) {}
 
-       void add(float time, float eDep) {time_ = (nSiPM_*time_+time)/(nSiPM_+1); eDep_=(nSiPM_*eDep_+eDep)/(nSiPM_+1); ++nSiPM_;}
+    void add(float time, float eDep) {time_ = (nSiPM_*time_+time)/(nSiPM_+1); eDep_=(nSiPM_*eDep_+eDep)/(nSiPM_+1); ++nSiPM_;}
 
-       unsigned nSiPM_;
-       float    time_, eDep_;
-    };
+    unsigned nSiPM_;
+    float    time_, eDep_;
+  };
 }
 
 
@@ -31,148 +32,153 @@ namespace mu2e {
 
   class CaloHitMakerFast : public art::EDProducer {
 
-    public:
-        struct Config 
-        {
-            using Name    = fhicl::Name;
-            using Comment = fhicl::Comment;
-            fhicl::Atom<art::InputTag> caloDigiCollection { Name("caloDigiCollection"), Comment("CaloDigi collection name") }; 
-	    fhicl::Atom<art::InputTag> pbttoken           { Name("ProtonBunchTimeTag"), Comment("ProtonBunchTime producer")};
-            fhicl::Atom<double>        digiSampling       { Name("digiSampling"),       Comment("Digitization time sampling") }; 
-            fhicl::Atom<double>        deltaTPulses       { Name("deltaTPulses"),       Comment("Maximum time difference between two signals") }; 
-            fhicl::Atom<double>        nPEperMeV          { Name("nPEperMeV"),          Comment("number of photo-electrons per MeV") }; 
-            fhicl::Atom<double>        noiseLevelMeV      { Name("noiseLevelMeV"),      Comment("Noise level in MeV") }; 
-            fhicl::Atom<double>        nSigmaNoise        { Name("nSigmaNoise"),        Comment("Maxnumber of sigma Noise to combine digi") }; 
-            fhicl::Sequence<int>       caphriCrystalID    { Name("caphriCrystalID"),    Comment("List of caphri crystal ID") }; 
-            fhicl::Atom<int>           diagLevel          { Name("diagLevel"),          Comment("Diag Level"),0 };
-        };
-
-        explicit CaloHitMakerFast(const art::EDProducer::Table<Config>& config) :
-           EDProducer{config},
-           caloDigisToken_    {consumes<CaloDigiCollection>(config().caloDigiCollection())},
-           pbttoken_          {consumes<ProtonBunchTime>(config().pbttoken())},
-           digiSampling_      (config().digiSampling()),
-           deltaTPulses_      (config().deltaTPulses()),
-           nPEperMeV_         (config().nPEperMeV()),
-           noise2_            (config().noiseLevelMeV()*config().noiseLevelMeV()),
-           nSigmaNoise_       (config().nSigmaNoise()),
-           caphriCrystalID_   (config().caphriCrystalID()),
-           diagLevel_         (config().diagLevel())
-        {
-            produces<CaloHitCollection>("calo");
-            produces<CaloHitCollection>("caphri");
-        }
-
-        void produce(art::Event& e) override;
-
-
-    private:
-        using pulseMapType = std::unordered_map<unsigned, std::vector<HitInfo>>;
-
-        void extractHits(const CaloDigiCollection& caloDigis, CaloHitCollection& caloHitsColl, CaloHitCollection& caphriHitsColl, double pbtOffset);
-        void addPulse(pulseMapType& pulseMap, unsigned crystalID, float time, float eDep);
-
-        art::ProductToken<CaloDigiCollection> caloDigisToken_;
-        const  art::ProductToken<ProtonBunchTime>    pbttoken_;
-        double              digiSampling_;
-        double              deltaTPulses_;
-        double              nPEperMeV_;
-        double              noise2_;
-        double              nSigmaNoise_;
-        std::vector<int>    caphriCrystalID_;
-        int                 diagLevel_;
+  public:
+    struct Config 
+    {
+      using Name    = fhicl::Name;
+      using Comment = fhicl::Comment;
+      fhicl::Atom<art::InputTag> caloDigiCollection { Name("caloDigiCollection"), Comment("CaloDigi collection name") }; 
+      fhicl::Atom<art::InputTag> pbttoken           { Name("ProtonBunchTimeTag"), Comment("ProtonBunchTime producer")};
+      fhicl::Atom<double>        digiSampling       { Name("digiSampling"),       Comment("Digitization time sampling") }; 
+      fhicl::Atom<double>        deltaTPulses       { Name("deltaTPulses"),       Comment("Maximum time difference between two signals") }; 
+      fhicl::Atom<double>        nPEperMeV          { Name("nPEperMeV"),          Comment("number of photo-electrons per MeV") }; 
+      fhicl::Atom<double>        noiseLevelMeV      { Name("noiseLevelMeV"),      Comment("Noise level in MeV") }; 
+      fhicl::Atom<double>        nSigmaNoise        { Name("nSigmaNoise"),        Comment("Maxnumber of sigma Noise to combine digi") }; 
+      fhicl::Sequence<int>       caphriCrystalID    { Name("caphriCrystalID"),    Comment("List of caphri crystal ID") }; 
+      fhicl::Atom<int>           diagLevel          { Name("diagLevel"),          Comment("Diag Level"),0 };
     };
 
-
-
-    //--------------------------------------------------------------------------------------------------------------
-    void CaloHitMakerFast::produce(art::Event& event)
+    explicit CaloHitMakerFast(const art::EDProducer::Table<Config>& config) :
+      EDProducer{config},
+      caloDigisToken_    {consumes<CaloDigiCollection>(config().caloDigiCollection())},
+      pbttoken_          {consumes<ProtonBunchTime>(config().pbttoken())},
+      digiSampling_      (config().digiSampling()),
+      deltaTPulses_      (config().deltaTPulses()),
+      nPEperMeV_         (config().nPEperMeV()),
+      noise2_            (config().noiseLevelMeV()*config().noiseLevelMeV()),
+      nSigmaNoise_       (config().nSigmaNoise()),
+      caphriCrystalID_   (config().caphriCrystalID()),
+      diagLevel_         (config().diagLevel())
     {
-        if (diagLevel_ > 0) std::cout<<"[FastRecoDigiFromDigi] begin"<<std::endl;
-
-        auto pbtH = event.getValidHandle(pbttoken_);
-        const ProtonBunchTime& pbt(*pbtH);
-        double pbtOffset = pbt.pbtime_;
-
-        const auto& caloDigisHandle = event.getValidHandle(caloDigisToken_);
-        const auto& caloDigis(*caloDigisHandle);
-
-        auto caloHitsColl   = std::make_unique<CaloHitCollection>();
-        auto caphriHitsColl = std::make_unique<CaloHitCollection>();
-        extractHits(caloDigis,*caloHitsColl,*caphriHitsColl,pbtOffset);
-
-        event.put(std::move(caloHitsColl),  "calo");
-        event.put(std::move(caphriHitsColl),"caphri");
-
-        if (diagLevel_ > 0) std::cout<<"[FastRecoDigiFromDigi] end"<<std::endl;
-        return;
+      produces<CaloHitCollection>("calo");
+      produces<CaloHitCollection>("caphri");
+      produces<IntensityInfoCalo>();
     }
 
-
-   //--------------------------------------------------------------------------------------------------------------
-   void CaloHitMakerFast::extractHits(const CaloDigiCollection& caloDigis, CaloHitCollection& caloHitsColl, CaloHitCollection& caphriHitsColl, double pbtOffset)
-   {
-       const Calorimeter& cal = *(GeomHandle<Calorimeter>());
-       ConditionsHandle<CalorimeterCalibrations> calorimeterCalibrations("ignored");
-
-       pulseMapType pulseMap;
-       for (const auto& caloDigi : caloDigis)
-       {
-           int crystalID   = cal.caloIDMapper().crystalIDFromSiPMID(caloDigi.SiPMID());
-
-	   size_t nSamPed  = caloDigi.peakpos() > 3 ? 4 : std::max(caloDigi.peakpos()-1, 1);
-           double baseline(0);
-	   for (size_t i=0; i<nSamPed; ++i){ baseline += caloDigi.waveform().at(i);}
-	   baseline /= nSamPed;
-           double eDep     = (caloDigi.waveform().at(caloDigi.peakpos())-baseline)*calorimeterCalibrations->ADC2MeV(caloDigi.SiPMID());//FIXME! we should use the function ::Peak2MeV, I also think that we should: (i) discard the hit if eDep is <0 (noise/stange pulse), (ii) require a minimum pulse length. gianipez 
-           double time     = caloDigi.t0() + caloDigi.peakpos()*digiSampling_ - pbtOffset;                      //Giani's definition
-           //double time     = caloDigi.t0() + (caloDigi.peakpos()+0.5)*digiSampling_ - shiftTime_; //Bertrand's definition
-
-           addPulse(pulseMap, crystalID, time, eDep);
-           if (diagLevel_ > 2) std::cout<<"[FastRecoDigiFromDigi] extracted Digi with crystalID="<<crystalID<<" eDep="<<eDep<<"\t time=" <<time<<std::endl;
-       }
-
-       for (auto& crystal : pulseMap)
-       {
-	   int crID = crystal.first;
-           bool isCaphri = std::find(caphriCrystalID_.begin(), caphriCrystalID_.end(), crID) != caphriCrystalID_.end();
-	   for (auto& info : crystal.second)
-           {
-               if (diagLevel_ > 1) std::cout<<"[FastRecoDigiFromDigi] extracted Hit with crystalID="<<crID<<" eDep="<<info.eDep_<<"\t time=" <<info.time_<<"\t nSiPM= "<<info.nSiPM_<<std::endl;
-               if (isCaphri) caphriHitsColl.emplace_back(CaloHit(crID, info.nSiPM_, info.time_,info.eDep_));
-               else          caloHitsColl.emplace_back(  CaloHit(crID, info.nSiPM_, info.time_,info.eDep_)); 
-	   }
-       }
-
-       if ( diagLevel_ > 0 ) std::cout<<"[CaloHitMakerFast] extracted "<<caloHitsColl.size()<<" CaloDigis"<<std::endl;
-       if ( diagLevel_ > 0 ) std::cout<<"[CaloHitMakerFast] extracted "<<caphriHitsColl.size()<<" CapriDigis"<<std::endl;
-
-   }
+    void produce(art::Event& e) override;
 
 
+  private:
+    using pulseMapType = std::unordered_map<unsigned, std::vector<HitInfo>>;
+    
+    void extractHits(const CaloDigiCollection& caloDigis, CaloHitCollection& caloHitsColl, CaloHitCollection& caphriHitsColl, , IntensityInfoCalo&, double pbtOffset);
+    void addPulse(pulseMapType& pulseMap, unsigned crystalID, float time, float eDep);
 
-   //--------------------------------------------------------------------------------------------------------------
-   void CaloHitMakerFast::addPulse(pulseMapType& pulseMap, unsigned crystalID, float time, float eDep)
-   {
+    art::ProductToken<CaloDigiCollection> caloDigisToken_;
+    const  art::ProductToken<ProtonBunchTime>    pbttoken_;
+    double              digiSampling_;
+    double              deltaTPulses_;
+    double              nPEperMeV_;
+    double              noise2_;
+    double              nSigmaNoise_;
+    std::vector<int>    caphriCrystalID_;
+    int                 diagLevel_;
+  };
 
-       bool addNewHit(true);
-       for (auto& pulse : pulseMap[crystalID])
-       {
-	   if (std::fabs(pulse.time_ - time) > deltaTPulses_) continue;
+ 
 
-	   float ratio  = (eDep-pulse.eDep_)/(eDep+pulse.eDep_);
-	   float eMean  = (eDep+pulse.eDep_)/2.0;
-           float sigmaR = 0.707*sqrt(1.0/eMean/nPEperMeV_ + noise2_/eMean/eMean);
+  //--------------------------------------------------------------------------------------------------------------
+  void CaloHitMakerFast::produce(art::Event& event)
+  {
+    if (diagLevel_ > 0) std::cout<<"[FastRecoDigiFromDigi] begin"<<std::endl;
 
-           if (abs(ratio) > nSigmaNoise_*sigmaR) continue;
+    auto pbtH = event.getValidHandle(pbttoken_);
+    const ProtonBunchTime& pbt(*pbtH);
+    double pbtOffset = pbt.pbtime_;
 
-           pulse.add(time,eDep); 
-           addNewHit = false;
-           break;
-       }
+    const auto& caloDigisHandle = event.getValidHandle(caloDigisToken_);
+    const auto& caloDigis(*caloDigisHandle);
 
-       if (addNewHit) pulseMap[crystalID].push_back(HitInfo(time, eDep));  
-   }
+    auto caloHitsColl   = std::make_unique<CaloHitCollection>();
+    auto caphriHitsColl = std::make_unique<CaloHitCollection>();
+    auto intInfo        = std::make_unique<IntensityInfoCalo>();
+    extractHits(caloDigis,*caloHitsColl,*caphriHitsColl,*intInfo,pbtOffset);
+
+    event.put(std::move(caloHitsColl),  "calo");
+    event.put(std::move(caphriHitsColl),"caphri");
+    event.put(std::move(intInfo));
+
+    if (diagLevel_ > 0) std::cout<<"[FastRecoDigiFromDigi] end"<<std::endl;
+    return;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------
+  void CaloHitMakerFast::addPulse(pulseMapType& pulseMap, unsigned crystalID, float time, float eDep)
+  {
+
+    bool addNewHit(true);
+    for (auto& pulse : pulseMap[crystalID])
+      {
+	if (std::fabs(pulse.time_ - time) > deltaTPulses_) continue;
+
+	float ratio  = (eDep-pulse.eDep_)/(eDep+pulse.eDep_);
+	float eMean  = (eDep+pulse.eDep_)/2.0;
+	float sigmaR = 0.707*sqrt(1.0/eMean/nPEperMeV_ + noise2_/eMean/eMean);
+
+	if (abs(ratio) > nSigmaNoise_*sigmaR) continue;
+
+	pulse.add(time,eDep); 
+	addNewHit = false;
+	break;
+      }
+    if (addNewHit) pulseMap[crystalID].push_back(HitInfo(time, eDep));  
+  }
+
+
+
+  //--------------------------------------------------------------------------------------------------------------
+  void CaloHitMakerFast::extractHits(const CaloDigiCollection& caloDigis, CaloHitCollection& caloHitsColl, CaloHitCollection& caphriHitsColl, IntensityInfoCalo& intInfo, double pbtOffset)
+  {
+    const Calorimeter& cal = *(GeomHandle<Calorimeter>());
+    ConditionsHandle<CalorimeterCalibrations> calorimeterCalibrations("ignored");
+
+    pulseMapType pulseMap;
+    for (const auto& caloDigi : caloDigis)
+      {
+	int crystalID   = cal.caloIDMapper().crystalIDFromSiPMID(caloDigi.SiPMID());
+	size_t nSamPed  = caloDigi.peakpos() > 3 ? 4 : std::max(caloDigi.peakpos()-1, 1);
+	double baseline(0);
+	for (size_t i=0; i<nSamPed; ++i){ baseline += caloDigi.waveform().at(i);}
+	baseline /= nSamPed;
+	double eDep     = (caloDigi.waveform().at(caloDigi.peakpos())-baseline)*calorimeterCalibrations->ADC2MeV(caloDigi.SiPMID());//FIXME! we should use the function ::Peak2MeV, I also think that we should: (i) discard the hit if eDep is <0 (noise/stange pulse), (ii) require a minimum pulse length. gianipez 
+	double time     = caloDigi.t0() + caloDigi.peakpos()*digiSampling_ - pbtOffset;                      //Giani's definition
+	//double time     = caloDigi.t0() + (caloDigi.peakpos()+0.5)*digiSampling_ - shiftTime_; //Bertrand's definition
+
+	addPulse(pulseMap, crystalID, time, eDep);
+	if (diagLevel_ > 2) std::cout<<"[CaloHitMakerFast] extracted Digi with crystalID="<<crystalID<<" eDep="<<eDep<<"\t time=" <<time<<std::endl;
+      }
+
+    unsigned short evtEnergy(0);
+    for (auto& crystal : pulseMap)
+      {
+	int  crID     = crystal.first;
+	bool isCaphri = std::find(caphriCrystalID_.begin(), caphriCrystalID_.end(), crID) != caphriCrystalID_.end();
+	for (auto& info : crystal.second)
+	  {
+	    if (diagLevel_ > 1) std::cout<<"[CaloHitMakerFast] extracted Hit with crystalID="<<crID<<" eDep="<<info.eDep_<<"\t time=" <<info.time_<<"\t nSiPM= "<<info.nSiPM_<<std::endl;
+	    if (isCaphri) caphriHitsColl.emplace_back(CaloHit(crID, info.nSiPM_, info.time_,info.eDep_));
+	    else          caloHitsColl.emplace_back(  CaloHit(crID, info.nSiPM_, info.time_,info.eDep_)); 
+	    evtEnergy += info.eDep_;
+	  }
+      }
+
+    intInfo.setCaloEnergy(evtEnergy);
+    intInfo.setNCaloHits(pulseMap.size());
+    
+    if ( diagLevel_ > 0 ) std::cout<<"[CaloHitMakerFast] extracted "<<caloHitsColl.size()<<" CaloDigis"<<std::endl;
+    if ( diagLevel_ > 0 ) std::cout<<"[CaloHitMakerFast] extracted "<<caphriHitsColl.size()<<" CapriDigis"<<std::endl;
+  }
+
 }
 
 DEFINE_ART_MODULE(mu2e::CaloHitMakerFast);
--- RecoDataProducts/inc/IntensityInfo.hh
+++ RecoDataProducts/inc/IntensityInfo.hh
@@ -9,38 +9,38 @@
 
 namespace mu2e {
 
-    class IntensityInfo
-    {
-        public:
-            IntensityInfo() {}
-            IntensityInfo(unsigned short nTrackerHits, unsigned short nCaloHits,
-                    unsigned short nProtonTCs, unsigned short caloEnergy,
-                    unsigned short testVariable):
-                nTrackerHits_(nTrackerHits), nCaloHits_(nCaloHits),
-                nProtonTCs_(nProtonTCs), caloEnergy_(caloEnergy),
-                testVariable_(testVariable)
-            {}
+  class IntensityInfo
+  {
+  public:
+    IntensityInfo() {}
+    IntensityInfo(unsigned short nTrackerHits, unsigned short nCaloHits,
+		  unsigned short nProtonTCs, unsigned short caloEnergy,
+		  unsigned short testVariable):
+      nTrackerHits_(nTrackerHits), nCaloHits_(nCaloHits),
+      nProtonTCs_(nProtonTCs), caloEnergy_(caloEnergy),
+      testVariable_(testVariable)
+    {}
 
 	   
-            void setNTrackerHits   (unsigned short tmp) {nTrackerHits_ = tmp;}
-            void setNCaloHits      (unsigned short tmp) {nCaloHits_ = tmp;}
-            void setNProtonTCs     (unsigned short tmp) {nProtonTCs_ = tmp;}
-            void setCaloEnergy     (unsigned short tmp) {caloEnergy_ = tmp;}
-            void setTestVariable   (unsigned short tmp) {testVariable_ = tmp;} 
+    void setNTrackerHits   (unsigned short tmp) {nTrackerHits_ = tmp;}
+    void setNCaloHits      (unsigned short tmp) {nCaloHits_    = tmp;}
+    void setNProtonTCs     (unsigned short tmp) {nProtonTCs_   = tmp;}
+    void setCaloEnergy     (unsigned short tmp) {caloEnergy_   = tmp;}
+    void setTestVariable   (unsigned short tmp) {testVariable_ = tmp;} 
 
-            unsigned short nTrackerHits () const { return nTrackerHits_; }
-            unsigned short nCaloHits    () const { return nCaloHits_   ; }
-            unsigned short nProtonTCs   () const { return nProtonTCs_  ; }
-            unsigned short caloEnergy   () const { return caloEnergy_  ; }
-            unsigned short testVariable () const { return testVariable_; }
+    unsigned short nTrackerHits () const { return nTrackerHits_; }
+    unsigned short nCaloHits    () const { return nCaloHits_   ; }
+    unsigned short nProtonTCs   () const { return nProtonTCs_  ; }
+    unsigned short caloEnergy   () const { return caloEnergy_  ; }
+    unsigned short testVariable () const { return testVariable_; }
 
-        private:
-            unsigned short  nTrackerHits_ = 0;
-            unsigned short  nCaloHits_    = 0;            
-            unsigned short  nProtonTCs_   = 0;        
-            unsigned short  caloEnergy_   = 0;        
-            unsigned short  testVariable_ = 0;            
-    };
+  private:
+    unsigned short  nTrackerHits_ = 0;
+    unsigned short  nCaloHits_    = 0;            
+    unsigned short  nProtonTCs_   = 0;        
+    unsigned short  caloEnergy_   = 0;        
+    unsigned short  testVariable_ = 0;            
+  };
 }
 
 #endif
