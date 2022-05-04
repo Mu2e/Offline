797e41e4774fc3a2134a0ea759aaa85bf68259b1
Andrew Edmonds <aedmonds@lbl.gov>
2022-05-04 08:10:46 -0500

    Combined STM trigger mode, channel, and data type into one 16-bit struct

--- DataProducts/inc/STMTypes.hh
+++ DataProducts/inc/STMTypes.hh
@@ -5,8 +5,22 @@
 #define DataProducts_STMTypes_hh_
 
 namespace mu2e {
-  enum TriggerMode { kInternal=0, kExternal=1 };
+  enum STMTriggerMode { kInternal=0, kExternal=1 };
   enum STMChannel { kHPGe=0, kLaBr=1 };
+  enum STMDataType { kUnsuppressed=0, kZeroSuppressed=1, kMWD=2};
+
+  struct STMTrigType {
+  public:
+    STMTrigType(uint16_t mode, uint16_t channel, uint16_t type) {
+      _data = (mode << 12) | (channel << 10) | type;
+    }
+
+    STMTriggerMode mode() const { return STMTriggerMode((_data & 0xF000) >> 12); }
+    STMChannel channel() const { return STMChannel((_data & 0xC00) >> 10); }
+    STMDataType type() const { return STMDataType((_data & 0x3FF)); }
+  private:
+    uint16_t _data;
+  };
 }
 
 #endif
--- RecoDataProducts/inc/STMDigi.hh
+++ RecoDataProducts/inc/STMDigi.hh
@@ -10,22 +10,23 @@
 #include <array>
 #include <Rtypes.h>
 
+#include "Offline/DataProducts/inc/STMTypes.hh"
+
 namespace mu2e {
 
   class STMDigi {
   public:
-    STMDigi() : _trigNum(0), _trigMode(0), _channel(0), _trigTime(0), _trigTimeOffset(0), _baselineMean(0), _baselineRMS(0), _nDrop(0), _adcs(std::vector<int16_t>()){};
+    STMDigi() : _trigNum(0), _trigType(STMTrigType(0, 0, 0)), _trigTime(0), _trigTimeOffset(0), _baselineMean(0), _baselineRMS(0), _nDrop(0), _adcs(std::vector<int16_t>()){};
 
-    STMDigi(uint32_t trigNum, uint16_t trigMode, uint16_t channel, uint64_t trigTime, uint32_t trigTimeOffset, uint16_t baselineMean, uint16_t baselineRMS, uint16_t nDrop, std::vector<int16_t> adcs) : _trigNum(trigNum), _trigMode(trigMode), _channel(channel), _trigTime(trigTime), _trigTimeOffset(trigTimeOffset), _baselineMean(baselineMean), _baselineRMS(baselineRMS), _nDrop(nDrop), _adcs(adcs) {};
+    STMDigi(uint32_t trigNum, STMTrigType trigType, uint64_t trigTime, uint32_t trigTimeOffset, uint16_t baselineMean, uint16_t baselineRMS, uint16_t nDrop, std::vector<int16_t> adcs) : _trigNum(trigNum), _trigType(trigType), _trigTime(trigTime), _trigTimeOffset(trigTimeOffset), _baselineMean(baselineMean), _baselineRMS(baselineRMS), _nDrop(nDrop), _adcs(adcs) {};
 
     // Simpler constructor for the simulation
-    STMDigi(int tdc, int adc) : _trigTime(tdc), _adcs(std::vector<int16_t>()) {
+    STMDigi(int channel, int tdc, int adc) : _trigType(STMTrigType(0, channel, 0)), _trigTime(tdc), _adcs(std::vector<int16_t>()) {
       _adcs.push_back(adc);
     }
 
     uint32_t trigNum() const { return _trigNum; }
-    uint16_t trigMode() const { return _trigMode; }
-    uint16_t channel() const { return _channel; }
+    STMTrigType trigType() const { return _trigType; }
     uint64_t trigTime() const { return _trigTime; }
     uint32_t trigTimeOffset() const { return _trigTimeOffset; }
     uint16_t baselineMean() const { return _baselineMean; }
@@ -35,8 +36,9 @@ namespace mu2e {
 
   private:
     uint32_t _trigNum;  // trigger number
-    uint16_t _trigMode; // external (beam) or internal (source)
-    uint16_t _channel;  // HPGe or LaBr
+    STMTrigType _trigType; // combination of: trigger mode (external (beam) or internal (source)),
+                           // channel (HPGe or LaBr), and
+                           // data type (unsuppressed, zero-suppressed, MWD etc.
     uint64_t _trigTime; // trigger time [ct]
     uint32_t _trigTimeOffset; // time offset from trigger to first ADC value [ct]
     uint16_t _baselineMean;   // mean baseline (calculated by MWD algorithm)
