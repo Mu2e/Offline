2bf331e9153df27a7cb42e38e5c18d1e7f635ad0
Ray Culbertson <rlc@fnal.gov>
2022-05-18 15:08:17 -0500

    max exceptions, simpler EpicsVar, fix int8

--- DbService/inc/EpicsTool.hh
+++ DbService/inc/EpicsTool.hh
@@ -2,10 +2,11 @@
 #define DbService_EpicsTool_hh
 
 // This class takes a user request, queries the EPICS offline replica database
-// and returns info about epics variables
+// and returns epics variables names and samples content
 
 #include "Offline/DbService/inc/DbReader.hh"
 #include "Offline/DbService/inc/EpicsVar.hh"
+#include "Offline/GeneralUtilities/inc/strVec.hh"
 #include <ctime>
 #include <string>
 #include <vector>
@@ -14,12 +15,11 @@ namespace mu2e {
 
 class EpicsTool {
  public:
-  typedef std::vector<std::string> StringVec;
 
   EpicsTool();
 
   // fill argument with the names of all channels
-  int names(StringVec& names);
+  int names(strVec& names);
   // return a vector of EPICS samples of channel "name"
   // and consistent with time or daysAgo. Time can be a single time
   // 2022-05-05T12:45:02-05:00
--- DbService/inc/EpicsVar.hh
+++ DbService/inc/EpicsVar.hh
@@ -16,33 +16,33 @@ class EpicsVar {
   typedef std::vector<EpicsVar> EpicsVec;
   typedef std::variant<int,double,std::string> variVar;
 
-  EpicsVar() {}
-  EpicsVar(std::string const& csv);
+  EpicsVar(std::string const& csv, int64_t channel_id,
+           std::string const& smpl_time_s, std::time_t smpl_time_t,
+           int64_t nanosecs, int64_t severity_id,
+           int64_t status_id, variVar const& value) :
+    _csv(csv),_channel_id(channel_id),_smpl_time_s(smpl_time_s),
+    _smpl_time_t(smpl_time_t),_nanosecs(nanosecs),_severity_id(severity_id),
+    _status_id(status_id),_value(value) {}
 
   // the channel number internal to EPICS
-  int8_t channel_id() const { return _channel_id; }
+  int64_t channel_id() const { return _channel_id; }
   // the time of the sample as a string, to us
   std::string const& stime() const { return _smpl_time_s; }
   // the time of the sample in epoch time
   std::time_t ttime() const { return _smpl_time_t; }
-  // the nanosec past the intergral seconds
-  int8_t nanosecs() const { return _nanosecs; }
-  int8_t severity_id() const { return _severity_id; }
-  int8_t status_id() const { return _status_id; }
+  // the nanosec past the integral seconds
+  int64_t nanosecs() const { return _nanosecs; }
+  int64_t severity_id() const { return _severity_id; }
+  int64_t status_id() const { return _status_id; }
   // PV value held, with its type, as std::variant
   variVar value() const { return _value; }
-
-  // Not all of the following will be valid - depends on how PV was declared
-  // value() will contain the non-null PV type as a std::variant
-
-  // PV value, if an int
-  int num_val() const { return _num_val; }
-  // PV value, if a float or double
-  double float_val() const { return _float_val; }
-  // PV value, if a string
-  std::string const& str_val() const { return _str_val; }
-  // this char says what the PV type is
-  char datatype() const { return _datatype; }
+  // for convenience, testing and querying variant
+  bool isInt() const { return std::holds_alternative<int>(_value); }
+  int getInt() const { return std::get<int>(_value); }
+  bool isDouble() const { return std::holds_alternative<double>(_value); }
+  double getDouble() const { return std::get<double>(_value); }
+  bool isString() const { return std::holds_alternative<std::string>(_value); }
+  std::string getString() const { return std::get<std::string>(_value); }
 
   // the database row in csv
   std::string const& csv() const { return _csv; };
@@ -50,16 +50,12 @@ class EpicsVar {
  private:
   std::string _csv;
 
-  int8_t _channel_id;
+  int64_t _channel_id;
   std::string _smpl_time_s;
   std::time_t _smpl_time_t;
-  int8_t _nanosecs;
-  int8_t _severity_id;
-  int8_t _status_id;
-  int _num_val;
-  double _float_val;
-  std::string _str_val;
-  char _datatype;
+  int64_t _nanosecs;
+  int64_t _severity_id;
+  int64_t _status_id;
   variVar _value;
 };
 
