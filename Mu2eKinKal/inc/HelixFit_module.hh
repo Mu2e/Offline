db042f8d16f7d843d9f55510079e112554b0e8cc
David Nathan Brown <dave_brown@lbl.gov>
2022-06-18 23:44:08 -0500

    Fixes.  Split seed creation from HelixModule

--- Mu2eKinKal/inc/HelixFit_module.hh
+++ Mu2eKinKal/inc/HelixFit_module.hh
@@ -133,7 +133,7 @@ namespace mu2e {
     private:
       TrkFitFlag fitflag_;
       // utility functions
-      KTRAJ makeSeedTraj(HelixSeed const& hseed) const;
+      virtual KTRAJ makeSeedTraj(HelixSeed const& hseed) const = 0;
       bool goodFit(KKTRK const& ktrk) const;
       void fillSaveTimes(KKTRK const& ktrk,std::set<double>& savetimes) const;
       // data payload
@@ -280,41 +280,6 @@ namespace mu2e {
     event.put(move(kktrkcol));
     event.put(move(kkseedcol));
     event.put(move(kkseedassns));
-  }
-
-  KTRAJ HelixFit::makeSeedTraj(HelixSeed const& hseed) const {
-    // compute the magnetic field at the helix center.  We only want the z compontent, as the helix fit assumes B points along Z
-    auto const& shelix = hseed.helix();
-    double zmin = std::numeric_limits<float>::max();
-    double zmax = std::numeric_limits<float>::min();
-    double tmin = std::numeric_limits<float>::max();
-    double tmax = std::numeric_limits<float>::min();
-    auto const& hits = hseed.hits();
-    for( auto const& hit : hits) {
-      double zpos = hit.pos().z();
-      zmin = std::min(zmin,zpos);
-      zmax = std::max(zmax,zpos);
-      tmin = std::min(tmin,(double)hit.correctedTime());
-      tmax = std::max(tmax,(double)hit.correctedTime());
-    }
-    float zcent = 0.5*(zmin+zmax);
-    VEC3 center(shelix.centerx(), shelix.centery(),zcent);
-    auto bcent = kkbf_->fieldVect(center);
-    VEC3 bnom(0.0,0.0,bcent.Z());
-    // create a PKTRAJ from the helix fit result, to seed the KinKal fit.  First, translate the parameters
-    // Note the sign adjustments; RobustHelix is a purely geometric helix, with slightly different conventions
-    DVEC pars;
-    double psign = copysign(1.0,-charge_*bcent.Z());
-    pars[KTRAJ::rad_] = shelix.radius()*psign;
-    pars[KTRAJ::lam_] = shelix.lambda()*kkfit_.fitDirection().dzdt();
-    pars[KTRAJ::cx_] = shelix.centerx();
-    pars[KTRAJ::cy_] = shelix.centery();
-    pars[KTRAJ::phi0_] = shelix.fz0()+psign*M_PI_2;
-    pars[KTRAJ::t0_] = hseed.t0().t0();
-    // create the initial trajectory
-    Parameters kkpars(pars,seedcov_);
-    //  construct the seed trajectory (infinite initial time range)
-    return KTRAJ(kkpars, mass_, charge_, bnom, TimeRange(tmin,tmax));
   }
 
   bool HelixFit::goodFit(KKTRK const& ktrk) const {
--- Mu2eKinKal/src/LoopHelixFit_module.cc
+++ Mu2eKinKal/src/LoopHelixFit_module.cc
@@ -11,7 +11,43 @@ namespace mu2e {
     public:
       explicit LoopHelixFit(const GlobalSettings& settings) :
         HelixFit(settings,TrkFitFlag::KKLoopHelix) {}
+      KTRAJ makeSeedTraj(HelixSeed const& hseed) const override;
       virtual ~LoopHelixFit() {}
   };
+
+  KTRAJ LoopHelixFit::makeSeedTraj(HelixSeed const& hseed) const {
+    // compute the magnetic field at the helix center.  We only want the z compontent, as the helix fit assumes B points along Z
+    auto const& shelix = hseed.helix();
+    double zmin = std::numeric_limits<float>::max();
+    double zmax = std::numeric_limits<float>::min();
+    double tmin = std::numeric_limits<float>::max();
+    double tmax = std::numeric_limits<float>::min();
+    auto const& hits = hseed.hits();
+    for( auto const& hit : hits) {
+      double zpos = hit.pos().z();
+      zmin = std::min(zmin,zpos);
+      zmax = std::max(zmax,zpos);
+      tmin = std::min(tmin,(double)hit.correctedTime());
+      tmax = std::max(tmax,(double)hit.correctedTime());
+    }
+    float zcent = 0.5*(zmin+zmax);
+    VEC3 center(shelix.centerx(), shelix.centery(),zcent);
+    auto bcent = kkbf_->fieldVect(center);
+    VEC3 bnom(0.0,0.0,bcent.Z());
+    // create a KTRAJ from the helix fit result, to seed the KinKal fit.  First, translate the parameters
+    // Note the sign adjustments; RobustHelix is a purely geometric helix, with slightly different conventions
+    DVEC pars;
+    double psign = copysign(1.0,-charge_*bcent.Z());
+    pars[KTRAJ::rad_] = shelix.radius()*psign;
+    pars[KTRAJ::lam_] = shelix.lambda()*kkfit_.fitDirection().dzdt();
+    pars[KTRAJ::cx_] = shelix.centerx();
+    pars[KTRAJ::cy_] = shelix.centery();
+    pars[KTRAJ::phi0_] = shelix.fz0()+psign*M_PI_2;
+    pars[KTRAJ::t0_] = hseed.t0().t0();
+    // create the initial trajectory
+    Parameters kkpars(pars,seedcov_);
+    //  construct the seed trajectory (infinite initial time range)
+    return KTRAJ(kkpars, mass_, charge_, bnom, TimeRange(tmin,tmax));
+  }
 }
 DEFINE_ART_MODULE(mu2e::LoopHelixFit);
