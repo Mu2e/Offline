76564582ec905a668c263a4d05ab2021097721b2
Ray Culbertson <rlc@fnal.gov>
2022-08-08 17:53:11 -0500

    start new proditions STMEnergyCalib

---
+++ STMConfig/inc/STMEnergyCalibConfig.hh
@@ -0,0 +1,22 @@
+#ifndef STMConfig_STMEnergyCalibConfig_hh
+#define STMConfig_STMEnergyCalibConfig_hh
+//
+// Fcl stanza for STM energy prodition
+//
+#include "fhiclcpp/types/Atom.h"
+#include "fhiclcpp/types/Sequence.h"
+#include <string>
+
+namespace mu2e {
+
+struct STMEnergyCalibConfig {
+  using Name = fhicl::Name;
+  using Comment = fhicl::Comment;
+
+  fhicl::Atom<int> verbose{Name("verbose"), Comment("verbosity: 0 or 1")};
+  fhicl::Atom<bool> useDb{Name("useDb"), Comment("use database or fcl")};
+};
+
+}  // namespace mu2e
+
+#endif
