13a4b05d8e0161a841f0f2ab7b2f7b207ccbbda6
Andrew Edmonds <aedmonds@lbl.gov>
2022-08-08 10:10:38 -0500

    Fix isValid since numeric value of unknown has changed. Also add tests for isValid

--- STMReco/test/testSTMChannel.C
+++ STMReco/test/testSTMChannel.C
@@ -10,6 +10,16 @@ void testSTMChannel() {
     std::cout << "FAILED" << std::endl;
     return -1;
   }
+  std::cout << "and isValid() should be false: isValid() = " << std::boolalpha << ch_default.isValid() << " : ";
+  if (ch_default.isValid() == false) {
+    std::cout << "OK" << std::endl;
+  }
+  else {
+    std::cout << "FAILED" << std::endl;
+    return -1;
+  }
+
+  std::cout << std::endl;
 
   mu2e::STMChannel ch_HPGe(mu2e::STMChannel::HPGe);
   std::cout << "This should be \"HPGe\": " << ch_HPGe << " : ";
@@ -20,6 +30,16 @@ void testSTMChannel() {
     std::cout << "FAILED" << std::endl;
     return -1;
   }
+  std::cout << "and isValid() should be true: isValid() = " << std::boolalpha << ch_HPGe.isValid() << " : ";
+  if (ch_HPGe.isValid() == true) {
+    std::cout << "OK" << std::endl;
+  }
+  else {
+    std::cout << "FAILED" << std::endl;
+    return -1;
+  }
+
+  std::cout << std::endl;
 
   mu2e::STMChannel ch_LaBr(mu2e::STMChannel::LaBr);
   std::cout << "This should be \"LaBr\": " << ch_LaBr << " : ";
@@ -30,6 +50,16 @@ void testSTMChannel() {
     std::cout << "FAILED" << std::endl;
     return -1;
   }
+  std::cout << "and isValid() should be true: isValid() = " << std::boolalpha << ch_LaBr.isValid() << " : ";
+  if (ch_LaBr.isValid() == true) {
+    std::cout << "OK" << std::endl;
+  }
+  else {
+    std::cout << "FAILED" << std::endl;
+    return -1;
+  }
+
+  std::cout << std::endl;
 
   mu2e::STMChannel ch_unknown(mu2e::STMChannel::unknown);
   std::cout << "This should be \"unknown\": " << ch_unknown << " : ";
@@ -40,4 +70,16 @@ void testSTMChannel() {
     std::cout << "FAILED" << std::endl;
     return -1;
   }
+  std::cout << "and isValid() should be false: isValid() = " << std::boolalpha << ch_unknown.isValid() << " : ";
+  if (ch_unknown.isValid() == false) {
+    std::cout << "OK" << std::endl;
+  }
+  else {
+    std::cout << "FAILED" << std::endl;
+    return -1;
+  }
+
+  std::cout << std::endl;
+
+  std::cout << "All tests passed" << std::endl;
 }
