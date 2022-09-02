8f074f754901e5291b67ed2842dea63ee0e77838
Ray Culbertson <rlc@fnal.gov>
2022-09-02 16:30:53 -0500

    add anti_K0

--- Analyses/src/CRYGenPlots_module.cc
+++ Analyses/src/CRYGenPlots_module.cc
@@ -216,6 +216,8 @@ void mu2e::CRYGenPlots::analyze(art::Event const & e)
         _hPtypeKE->Fill(mom4.e(), 6); break;
       case PDGCode::K0: // k0
         _hPtypeKE->Fill(mom4.e(), 6); break;
+      case PDGCode::anti_K0: // anti k0
+        _hPtypeKE->Fill(mom4.e(), 6); break;
       case PDGCode::K_plus: // k+
         _hPtypeKE->Fill(mom4.e(), 6); break;
       case PDGCode::K_minus: // k-
