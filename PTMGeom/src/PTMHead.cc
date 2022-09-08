6f746c7f6b719eec0c2e2646fd6f071753e7ae33
Helenka Casler <hcasler@gradcenter.cuny.edu>
2022-09-08 19:01:36 -0400

    tidying, removing useless comments and commented-out code

--- PTMGeom/src/PTMHead.cc
+++ PTMGeom/src/PTMHead.cc
@@ -6,9 +6,9 @@
 //
 
 namespace mu2e{
-  PTMHead::PTMHead(CLHEP::Hep3Vector const& originInMu2e, 
-          CLHEP::HepRotation const& rotationInMu2e, 
-          std::shared_ptr<PTMPWC> nearPWC, 
+  PTMHead::PTMHead(CLHEP::Hep3Vector const& originInMu2e,
+          CLHEP::HepRotation const& rotationInMu2e,
+          std::shared_ptr<PTMPWC> nearPWC,
           std::shared_ptr<PTMPWC> farPWC,
           double pwcSeparation,
           std::shared_ptr<Box> holderExtrusionLong,
@@ -18,12 +18,12 @@ namespace mu2e{
           double holderExtrusionShortPos,
           std::shared_ptr<Box> handleBase,
           std::string handleMaterialName,
-          double handleHoleSemiMajor, 
+          double handleHoleSemiMajor,
           double handleHoleSemiMinor,
           CLHEP::Hep3Vector handleHoleCenter,
           double handleCornerCutSide,
           double motherMargin,
-          CLHEP::Hep3Vector const& parentOriginInMu2e, 
+          CLHEP::Hep3Vector const& parentOriginInMu2e,
           CLHEP::HepRotation const& parentRotationInMu2e)
           : _originInMu2e(originInMu2e),
             _rotationInMu2e(rotationInMu2e),
@@ -36,25 +36,14 @@ namespace mu2e{
             _holderExtrusionShortPos(holderExtrusionShortPos),
             _handleBase(handleBase),
             _handleMaterialName(handleMaterialName),
-            _handleHoleSemiMajor(handleHoleSemiMajor), 
+            _handleHoleSemiMajor(handleHoleSemiMajor),
             _handleHoleSemiMinor(handleHoleSemiMinor),
             _handleHoleCenter(handleHoleCenter),
             _handleCornerCutSide(handleCornerCutSide) {
-    // set the "overall" dimensions based on the PWC dimensions and positions.
-    // Not assuming the two PWCs are the same size.
-    //if (nearPWC->totalHeight() >= farPWC->totalHeight()) {
-    //  _totalHeight = nearPWC->totalHeight()+(2*handleBase->getYhalfLength())+motherMargin;
-    //} else {
-    //  _totalHeight = farPWC->totalHeight()+(2*handleBase->getYhalfLength())+motherMargin;
-    //}
-    //_totalHeight = holderExtrusionLongSep + (2*holderExtrusionLong->getYhalfLength()) + (2*handleBase->getYhalfLength()) + motherMargin;
-    _totalHeight = holderExtrusionLongSep + (2*handleBase->getYhalfLength()) + motherMargin;// + containerMargin;
+
+    _totalHeight = holderExtrusionLongSep + (2*handleBase->getYhalfLength()) + motherMargin;
     _totalWidth = holderExtrusionLongSep + (2*holderExtrusionLong->getYhalfLength()) + motherMargin;
-    //if (nearPWC->totalWidth() >= farPWC->totalWidth()) {
-    //  _totalWidth = nearPWC->totalWidth()+motherMargin;
-    //} else {
-    //  _totalWidth = farPWC->totalWidth()+motherMargin;
-    //}
+
     _totalLength = pwcSeparation + 0.5*nearPWC->totalThick() + 0.5*farPWC->totalThick() + motherMargin;
 
     _originInParent = originInMu2e - parentOriginInMu2e;
