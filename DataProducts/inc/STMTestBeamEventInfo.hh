6d6f8737b755f0dd807949d6134810cac7d5199a
Andrew Edmonds <awje@bu.edu>
2022-09-23 14:09:36 -0500

    We can know what the STMTestBeamEventInfo contents will be when we create it

--- DataProducts/inc/STMTestBeamEventInfo.hh
+++ DataProducts/inc/STMTestBeamEventInfo.hh
@@ -5,10 +5,9 @@ namespace mu2e {
   class STMTestBeamEventInfo {
   public:
     enum TriggerType { unknown=-1, internal=0, external=1 };
-    STMTestBeamEventInfo() { }
 
-    void triggerType(uint16_t type) { _triggerType = TriggerType(type);}
-    void triggerTime(uint64_t time) { _triggerTime = time;}
+    STMTestBeamEventInfo() { }
+    STMTestBeamEventInfo(uint16_t type, uint64_t time) : _triggerType(TriggerType(type)), _triggerTime(time) { }
 
     TriggerType triggerType() const { return _triggerType;}
     uint64_t triggerTime() const { return _triggerTime;}
