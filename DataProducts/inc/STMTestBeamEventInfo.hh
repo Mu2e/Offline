f78aa57c904b967ffdffe16e60f9e18a1bd1d813
Andrew Edmonds <awje@bu.edu>
2022-09-23 14:01:15 -0500

    Initialize member variables here instead of initializer list

--- DataProducts/inc/STMTestBeamEventInfo.hh
+++ DataProducts/inc/STMTestBeamEventInfo.hh
@@ -5,7 +5,7 @@ namespace mu2e {
   class STMTestBeamEventInfo {
   public:
     enum TriggerType { unknown=-1, internal=0, external=1 };
-    STMTestBeamEventInfo() : _triggerType(TriggerType::unknown), _triggerTime(0) { }
+    STMTestBeamEventInfo() { }
 
     void triggerType(uint16_t type) { _triggerType = TriggerType(type);}
     void triggerTime(uint64_t time) { _triggerTime = time;}
@@ -14,8 +14,8 @@ namespace mu2e {
     uint64_t triggerTime() const { return _triggerTime;}
 
   private:
-    TriggerType _triggerType;
-    uint64_t _triggerTime;
+    TriggerType _triggerType = TriggerType::unknown;
+    uint64_t _triggerTime = 0;
   };
 }
 
