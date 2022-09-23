0318cc87584235a131ba30cae877d79be75032b0
Andrew Edmonds <awje@bu.edu>
2022-09-23 13:57:12 -0500

    Put STMTestBeamHeaders streamers into separate .cc file

--- DataProducts/inc/STMTestBeamHeaders.hh
+++ DataProducts/inc/STMTestBeamHeaders.hh
@@ -59,19 +59,6 @@ namespace mu2e {
       bool checkFixedHeader() const {
         return (this->getFixedHeader() == (uint32_t) 0xDEADBEEF);
       }
-
-
-      friend std::ostream& operator<<(std::ostream& os, const TriggerHeader& header) {
-        os << "Correct fixed header? " << std::boolalpha << header.checkFixedHeader() << std::endl;
-        os << "Data Size: " << header.getDataSize() << " bytes" << std::endl;
-        os << "No. of Slices: " << header.getNSlices() << std::endl;
-        os << "Trigger #" << header.getTriggerNumber() << std::endl;
-        os << "\tMode: " << header.getTriggerMode() << std::endl;
-        os << "\tTime: " << header.getTriggerTime() << " ct" << std::endl;
-        os << "\tTime Offset: " << header.getTriggerOffset() << " ct" << std::endl;
-        os << "\tNo. of Dropped Packets: " << header.getNDroppedPackets() << std::endl;
-        return os;
-      }
     };
 
     struct SliceHeader {
@@ -94,15 +81,10 @@ namespace mu2e {
       unsigned long int getNADC() const {
         return (this->getSliceSize())/ sizeof(int16_t);
       }
-
-      friend std::ostream& operator<<(std::ostream& os, const SliceHeader& header) {
-        os << "Slice #" << header.getSliceNumber() << std::endl;
-        os << "\tSize: " << header.getSliceSize() << " bytes = " << header.getNADC() << " samples" << std::endl;
-        os << "\tTime: " << header.getSliceTime() << " ct" << std::endl;
-
-        return os;
-      }
     };
   };
+
+  std::ostream& operator<<(std::ostream& os, const STMTestBeam::TriggerHeader& header);
+  std::ostream& operator<<(std::ostream& os, const STMTestBeam::SliceHeader& header);
 }
 #endif
