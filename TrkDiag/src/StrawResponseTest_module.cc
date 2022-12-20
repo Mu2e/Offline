df99adc3ceac6abe02137d173e265368e37a4843
David Nathan Brown <dave_brown@lbl.gov>
2022-12-20 16:52:22 -0600

    Update StrawResponseTest.  Improve null hit error assignment

--- TrkDiag/src/StrawResponseTest_module.cc
+++ TrkDiag/src/StrawResponseTest_module.cc
@@ -26,7 +26,8 @@ namespace mu2e {
         fhicl::Atom<int> printLevel{ Name("printLevel"), Comment("Print level" ),0 };
         fhicl::Atom<int> diagLevel{ Name("diagLevel"), Comment("Diagnostic level" ),0 };
         fhicl::Atom<int> nbins{ Name("nBins"), Comment("Number of bins for TGraph objects" ),200 };
-        fhicl::Atom<double> rmax{ Name("rmax"), Comment("Maximum radius to test" ),3.0 };
+        fhicl::Atom<double> tmin{ Name("tmin"), Comment("Minimum time to test" ),-5.0 };
+        fhicl::Atom<double> tmax{ Name("tmax"), Comment("Maximum time to test" ),45.0 };
       };
       using Parameters = art::EDAnalyzer::Table<Config>;
       explicit StrawResponseTest(Parameters const& config);
@@ -36,23 +37,24 @@ namespace mu2e {
     private:
       ProditionsHandle<Tracker> _alignedTracker_h;
       TTree*  srtest_;
-      float dist_, time_, tvar_, vinv_, vavg_;
-      float derr_, rdrift_;
-      TGraph* d2t_, *d2tvar_, *d2v_;
+      float dtime_, vinst_;
+      float rderr_, rdrift_;
+      TGraph* t2d_, *t2derr_, *t2v_;
       int plane_, panel_, straw_;
       int print_, diag_;
       int nbins_;
-      double rmax_;
+      double tmin_, tmax_;
       bool first_;
       ProditionsHandle<StrawResponse> strawResponse_h_;
   };
 
   StrawResponseTest::StrawResponseTest(Parameters const& config) :     art::EDAnalyzer(config),
-  srtest_(0),d2t_(0), d2tvar_(0), d2v_(0),
+  srtest_(0),t2d_(0), t2derr_(0), t2v_(0),
   print_(config().printLevel()),
   diag_(config().diagLevel()),
   nbins_(config().nbins()),
-  rmax_(config().rmax()),
+  tmin_(config().tmin()),
+  tmax_(config().tmax()),
   first_(true) {}
 
   StrawResponseTest::~StrawResponseTest() {}
@@ -64,26 +66,22 @@ namespace mu2e {
 //      srtest_->Branch("plane",&splane_,"plane/I");  Eventually add straw dependence TODO
 //      srtest_->Branch("panel",&spanel_,"panel/I");
 //      srtest_->Branch("straw",&straw_,"straw/I");
-      srtest_->Branch("dist",&dist_,"dist/F");
-//      srtest_->Branch("phi",&phi_,"phi/F"); // add phi dependence TODO
-      srtest_->Branch("time",&time_,"time/F");
-      srtest_->Branch("tvar",&tvar_,"tvar/F");
-      srtest_->Branch("vinv",&vinv_,"vinv/F");
-      srtest_->Branch("vavg",&vavg_,"vavg/F");
       srtest_->Branch("rdrift",&rdrift_,"rdrift/F");
-      srtest_->Branch("derr",&derr_,"derr/F");
-      d2t_ = tfs->make<TGraph>(nbins_);
-      d2t_->SetName("D2T");
-      d2t_->SetTitle("Distance to Time;Distance to Wire (mm);Mean Drift Time (ns)");
-      gDirectory->Append(d2t_);
-      d2tvar_ = tfs->make<TGraph>(nbins_);
-      d2tvar_->SetName("D2TVar");
-      d2tvar_->SetTitle("Drift Time Variance;Distance to Wire (mm);Drift Time Variance (ns^2)");
-      gDirectory->Append(d2tvar_);
-      d2v_ = tfs->make<TGraph>(nbins_);
-      d2v_->SetName("D2V");
-      d2v_->SetTitle("Inverse Instantaneous Drift Velocity;Distance to Wire (mm);Invers Drift Velocity (ns/mm)");
-      gDirectory->Append(d2v_);
+      srtest_->Branch("dtime",&dtime_,"dtime/F");
+      srtest_->Branch("vinst",&vinst_,"vinst/F");
+      srtest_->Branch("rderr",&rderr_,"rderr/F");
+      t2d_ = tfs->make<TGraph>(nbins_);
+      t2d_->SetName("T2D");
+      t2d_->SetTitle("Time to Distance;Drift Time (ns);Distance to Wire (mm)");
+      gDirectory->Append(t2d_);
+      t2derr_ = tfs->make<TGraph>(nbins_);
+      t2derr_->SetName("T2DErr");
+      t2derr_->SetTitle("Drift Distance Error;Drift Time (ns);Drift Distance Error (mm)");
+      gDirectory->Append(t2derr_);
+      t2v_ = tfs->make<TGraph>(nbins_);
+      t2v_->SetName("D2V");
+      t2v_->SetTitle("Instantaneous Drift Velocity;Drift Time (ns);Instantaneous Drift Velocity (ns/mm)");
+      gDirectory->Append(t2v_);
     }
   }
 
@@ -94,20 +92,17 @@ namespace mu2e {
     if(first_){
       // fetch StrawResposne
       auto const& sresponse = strawResponse_h_.getPtr(event.id());
-      vavg_ = sresponse->driftConstantSpeed();
-      double dstep = rmax_/(nbins_-1);
+      double tstep = (tmax_ - tmin_)/(nbins_-1);
       for (int ibin=0;ibin<nbins_; ++ibin){
-        double dist = dstep*ibin;
-        double d2t = sresponse->driftDistanceToTime(sid,dist,0.0);
-//        double t2d = sresponse->driftTimeToDistance(sid,d2t,0.0);
-        auto dinfo = sresponse->driftInfoAtDistance(sid,dist,d2t,0.0);
-        dist_ = dinfo.distance; time_ = d2t; vinv_ = 1.0/dinfo.speed; tvar_ = dinfo.variance;
-        derr_ = sresponse->driftDistanceError(sid,dist,0.0);
-        rdrift_ = sresponse->driftTimeToDistance(sid,time_,0.0);
+        dtime_ = tmin_ + tstep*ibin;
+        rdrift_ = sresponse->driftTimeToDistance(sid,dtime_,0.0);
+        rderr_ = sresponse->driftDistanceError(sid,rdrift_,0.0);
+        vinst_ = sresponse->driftInstantSpeed(sid,rdrift_,0.0,false);
+
         srtest_->Fill();
-        d2t_->SetPoint(ibin,dist,d2t);
-        d2tvar_->SetPoint(ibin,dist,dinfo.variance);
-        d2v_->SetPoint(ibin,dist,1.0/dinfo.speed);
+        t2d_->SetPoint(ibin,dtime_,rdrift_);
+        t2derr_->SetPoint(ibin,dtime_,rderr_);
+        t2v_->SetPoint(ibin,dtime_,vinst_);
       }
     }
   }
