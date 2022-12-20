a8da3e4b398ac376ce26251fa7e0671411d4ab16
David Nathan Brown <dave_brown@lbl.gov>
2022-12-20 16:52:22 -0600

    Update StrawResponseTest.  Improve null hit error assignment

--- Mu2eKinKal/inc/DriftInfo.hh
+++ Mu2eKinKal/inc/DriftInfo.hh
@@ -1,5 +1,6 @@
 #ifndef Mu2eKinKal_DriftInfo_hh
 #define Mu2eKinKal_DriftInfo_hh
+#include <algorithm>
 
 namespace mu2e {
   // struct describing drift information
@@ -8,9 +9,10 @@ namespace mu2e {
     double driftDistance_; // drift distance, calculated from drift time
     double driftDistanceError_; // estimated variance on drift distance
     double driftVelocity_; // instantaneous drift velocity
+    static double maxdvar_; // maximum distance variance, given by straw radius
     double driftDistanceVar() const { return driftDistanceError_*driftDistanceError_; }
-    double nullDistanceVar() const { return
-      std::max(driftDistance_*driftDistance_,driftDistanceVar()); }
+    double nullDistanceVar() const {
+      return std::min(maxdvar_, driftDistance_*driftDistance_ + driftDistanceVar()); }
   };
 }
 #endif
---
+++ Mu2eKinKal/src/DriftInfo.cc
@@ -0,0 +1,4 @@
+#include "Offline/Mu2eKinKal/inc/DriftInfo.hh"
+namespace mu2e {
+  double DriftInfo::maxdvar_(2.5*2.5/3.0); // Should come from geometry service TODO
+}
