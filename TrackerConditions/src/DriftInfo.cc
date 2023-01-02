bb1c7d9bbf948a84a99b425b88acece5fda68f14
David Nathan Brown <dave_brown@lbl.gov>
2023-01-02 14:39:38 -0600

    Fix bug in assigning null variance. Calibration drift RMS including sign errors

--- TrackerConditions/src/DriftInfo.cc
+++ TrackerConditions/src/DriftInfo.cc
@@ -1,5 +1,9 @@
 #include "Offline/TrackerConditions/inc/DriftInfo.hh"
 namespace mu2e {
-  double DriftInfo::maxdvar_(2.4*2.4/3.0); // effective maximum drift distance variance: should come from StrawResponse TODO:w
+  double DriftInfo::maxdvar_(2.4*2.4/3.0); // effective maximum drift distance variance: should come from StrawResponse TODO
 
+  double DriftInfo::nullDistanceVar() const {
+    double rdrift = std::max(0.0,driftDistance_); // don't count negative drift distances
+    return std::min(maxdvar_, rdrift*rdrift + driftDistanceVar());
+  }
 }
