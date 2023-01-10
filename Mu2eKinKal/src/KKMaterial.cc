752abdf216278836d1839efeb655487af783fd5e
David Nathan Brown <dave_brown@lbl.gov>
2023-01-10 13:00:24 -0600

    Correct material configuration

--- Mu2eKinKal/src/KKMaterial.cc
+++ Mu2eKinKal/src/KKMaterial.cc
@@ -1,6 +1,7 @@
 #include "Offline/Mu2eKinKal/inc/KKMaterial.hh"
 #include "Offline/GeometryService/inc/GeomHandle.hh"
 #include "Offline/TrackerGeom/inc/Tracker.hh"
+#include "KinKal/MatEnv/DetMaterial.hh"
 #include "Offline/GeometryService/inc/GeometryService.hh"
 
 namespace mu2e {
@@ -12,11 +13,16 @@ namespace mu2e {
     gasmatname_(matconfig.strawGasMaterialName()),
     wirematname_(matconfig.strawWireMaterialName()),
     eloss_((MatEnv::DetMaterial::energylossmode)matconfig.eloss()),
-    matdbinfo_(0) {}
+    matdbinfo_(new MatDBInfo(filefinder_,eloss_)) {
+      std::cout << "Setting KinKal scattering Dahl-Lynch fraction to " << matconfig.dahlLynchScatteringFraction() << std::endl;
+      // MatEnv forces const-cast to change scattering parameter
+      const_cast<MatEnv::DetMaterial*>(matdbinfo_->findDetMaterial(wallmatname_))->setScatterFraction(matconfig.dahlLynchScatteringFraction());
+      const_cast<MatEnv::DetMaterial*>(matdbinfo_->findDetMaterial(gasmatname_))->setScatterFraction(matconfig.dahlLynchScatteringFraction());
+      const_cast<MatEnv::DetMaterial*>(matdbinfo_->findDetMaterial(wirematname_))->setScatterFraction(matconfig.dahlLynchScatteringFraction());
+    }
 
   KKStrawMaterial const& KKMaterial::strawMaterial() const {
-    if(matdbinfo_ == 0){
-      matdbinfo_ = new MatDBInfo(filefinder_,eloss_);
+    if(smat_ == 0){
       Tracker const & tracker = *(GeomHandle<Tracker>());
       auto const& sprop = tracker.strawProperties();
       smat_ = std::make_unique<KKStrawMaterial>(
