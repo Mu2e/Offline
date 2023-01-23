6e99e27fe18e16afec561f8162a1b76687880e56
Gianantonio Pezzullo <giani.pezzu@gmail.com>
2023-01-23 15:36:10 -0600

    now using OptionalAtom

--- GeneralUtilities/inc/PhiPrescalingParams.hh
+++ GeneralUtilities/inc/PhiPrescalingParams.hh
@@ -1,7 +1,7 @@
 #ifndef GeneralUtilities_PhiPrescalingParams_hh
 #define GeneralUtilities_PhiPrescalingParams_hh
 
-#include "fhiclcpp/types/Atom.h"
+#include "fhiclcpp/types/OptionalAtom.h"
 
 namespace mu2e {
   struct PhiPrescalingParams {
@@ -10,9 +10,9 @@ namespace mu2e {
     float    _phase;
 
     struct Config {
-      fhicl::Atom<float> amplitude{fhicl::Name("amplitude"), fhicl::Comment("amplitude"), 0.};
-      fhicl::Atom<float> frequency{fhicl::Name("frequency"), fhicl::Comment("frequency"), 0.};
-      fhicl::Atom<float> phase    {fhicl::Name("phase"),     fhicl::Comment("phase"), 0.};
+      fhicl::OptionalAtom<float> amplitude{fhicl::Name("amplitude"), fhicl::Comment("amplitude")};
+      fhicl::OptionalAtom<float> frequency{fhicl::Name("frequency"), fhicl::Comment("frequency")};
+      fhicl::OptionalAtom<float> phase    {fhicl::Name("phase"),     fhicl::Comment("phase")};
     };
 
     PhiPrescalingParams():
@@ -25,10 +25,12 @@ namespace mu2e {
       _frequency(Frequency),
       _phase    (Phase) {}
 
-    PhiPrescalingParams(const Config& conf):
-      _amplitude(conf.amplitude()),
-      _frequency(conf.frequency()),
-      _phase    (conf.phase()) {}
+    PhiPrescalingParams(const Config& conf){
+      float x;
+      if (conf.amplitude(x)) _amplitude = x;
+      if (conf.frequency(x)) _frequency = x;
+      if (conf.phase(x))     _phase     = x;
+    }
   };
 
 }
