639b419a0fa1667eec91b1771e7d823dee787983
David Nathan Brown <dave_brown@lbl.gov>
2023-01-30 14:03:18 -0600

    Add state for using delta-T constraint in drift residual.  Create end sign function

--- Mu2eKinKal/inc/WireHitState.hh
+++ Mu2eKinKal/inc/WireHitState.hh
@@ -8,22 +8,21 @@
 namespace mu2e {
   // struct describing wire hit internal state
   struct WireHitState {
-    enum State { unusable=-3, inactive=-2, left=-1, null=0, right=1};  // drift state
+    enum State { unusable=-3, inactive=-2, left=-1, null=0, right=1, leftdt=2,rightdt=3};  // drift state
     enum TOTUse { unused=0, nullonly=1, driftonly=2, all=3}; // how to use TOT time constraint
     enum NullDistVar { rstraw=0, rdrift=1}; // how to assign null variance
     enum QualityIndex { bkg=0, sign=1, drift=2, chi2=3};// quality info index
     State state_ = null;
     StrawHitUpdaters::algorithm algo_ = StrawHitUpdaters::unknown; // algorithm used to set this state
     NullDistVar nulldvar_ = rstraw; // distance variance for null hits
-    double dVar_; // drift distance variance value
-    double dDdT_; // drift distance time derivative, crudely the drift velocity
     bool frozen_ = false; // if set, state not allowed to change during update
     TOTUse totuse_ = all;
     std::array<double,4> quality_ = {-1.0,-1.0,-1.0,-1.0}; // algorithm-dependent, dimensionless quality of this state assignment
 // convenience functions
     bool frozen() const { return frozen_; }
     bool wireConstraint() const { return state_ == null; }
-    bool driftConstraint() const { return state_ == left || state_ == right; }
+    bool driftConstraint() const { return state_ == left || state_ == right || state_ == leftdt || state_ == rightdt; }
+    bool driftDTConstraint() const { return state_ == leftdt || state_ == rightdt; }
     bool isInactive() const { return state_ == inactive; }
     bool active() const { return state_ > inactive; }
     bool usable() const { return state_ > unusable; }
@@ -39,9 +38,9 @@ namespace mu2e {
     bool operator != (WireHitState::State state) const { return state_ != state; }
     double lrSign() const {
       switch (state_) {
-        case left:
+        case left: case leftdt:
           return -1.0;
-        case right:
+        case right: case rightdt:
           return 1.0;
         default:
           return 0.0;
