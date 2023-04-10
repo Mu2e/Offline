07ed1517cedc0cb6d10c3b5f00d04ad385c2ec39
David Nathan Brown <dave_brown@lbl.gov>
2023-04-09 19:02:44 -0500

    Move to lazy evaluation.  Make internal structs double precision

--- GeneralUtilities/src/CombineTwoDPoints.cc
+++ GeneralUtilities/src/CombineTwoDPoints.cc
@@ -2,8 +2,8 @@
 #include "TMath.h"
 #include <stdexcept>
 namespace mu2e {
-  using SVEC = ROOT::Math::SVector<float,2>;
-  using SMAT = ROOT::Math::SMatrix<float,2,2,ROOT::Math::MatRepSym<float,2>>;
+  using SVEC = ROOT::Math::SVector<double,2>;
+  using SMAT = ROOT::Math::SMatrix<double,2,2,ROOT::Math::MatRepSym<double,2>>;
   TwoDWeight::TwoDWeight(TwoDPoint const& pos,float ivar) {
     wt_ = pos.weight(ivar);
     wtpos_ = wt_*pos.pos();
@@ -22,45 +22,71 @@ namespace mu2e {
     return *this;
   }
 
-  CombinedTwoDPoints::CombinedTwoDPoints(TwoDPoint const& point, float ivar) : ivar_(ivar), point_(point), wt_(point), chisq_(0.0) {
-    wts_.push_back(wt_);
-    chi0_ = ROOT::Math::Similarity(point.pos(),wts_.back().wt());
+  void TwoDWeight::print(std::ostream& os) const {
+    os << *this << std::endl;
   }
 
-  CombinedTwoDPoints::CombinedTwoDPoints(std::vector<TwoDPoint> points, float ivar) : ivar_(ivar) {
+  CombinedTwoDPoints::CombinedTwoDPoints(TwoDPoint const& point, float ivar) : ivar_(ivar), ptcalc_(true), chicalc_(true), point_(point), wt_(point),  chisq_(0.0) {
+    wts_.push_back(wt_);
+    chi0_ = ROOT::Math::Similarity(point.pos(),wts_.back().weight());
+  }
+
+  CombinedTwoDPoints::CombinedTwoDPoints(std::vector<TwoDPoint> points, float ivar) : ivar_(ivar), ptcalc_(false), chicalc_(false), chi0_(0.0), chisq_(0.0) {
     wts_.reserve(points.size());
     for (auto const& point : points ){
       wts_.push_back(TwoDWeight(point));
-      chi0_ += ROOT::Math::Similarity(point.pos(),wts_.back().wt());
+      chi0_ += ROOT::Math::Similarity(point.pos(),wts_.back().weight());
+      wt_ += wts_.back();
     }
-    for(auto const& wt : wts_){
-      wt_ += wt;
+  }
+
+  TwoDPoint const& CombinedTwoDPoints::point() const {
+    if(!ptcalc_){
+      ptcalc_ = true;
+      point_ = wt_.point();
     }
-    point_ = wt_.point();
-    chisq_ = chi0_ - ROOT::Math::Similarity(wt_.wtPos(),point_.cov());
+    return point_;
   }
 
   void CombinedTwoDPoints::addPoint(TwoDPoint const& point) {
     wts_.push_back(TwoDWeight(point,ivar_));
-    chi0_ += ROOT::Math::Similarity(point.pos(),wts_.back().wt());
     wt_ += wts_.back();
-    point_ = wt_.point();
-    chisq_ += - ROOT::Math::Similarity(wt_.wtPos(),point_.cov());
+    chi0_ += ROOT::Math::Similarity(point.pos(),wts_.back().weight());
+    chicalc_ = false;
+    ptcalc_ = false;
   }
 
-  float CombinedTwoDPoints::dChi2(TwoDPoint const& point) const {
-    auto dpt = point_.pos() - point.pos();
-    SMAT cov = point_.cov() + point.cov();
+  double CombinedTwoDPoints::dChi2(TwoDPoint const& pt) const {
+    auto dpt = point().pos() - pt.pos();
+    SMAT cov = point().cov() + pt.cov();
     TwoDPoint dpoint(dpt,cov);
     TwoDWeight wt(dpoint,ivar_);
     return ROOT::Math::Similarity(dpoint.pos(),wt.wt());
   }
 
-  double CombinedTwoDPoints::probability() const{
+  double CombinedTwoDPoints::chisquared() const {
+    if(!chicalc_){
+      chicalc_ = true;
+      chisq_ = chi0_ - ROOT::Math::Similarity(wt_.wtPos(),point().cov());
+    }
+    return chisq_;
+  }
+
+  double CombinedTwoDPoints::consistency() const{
     auto ndof = nDOF();
-    if( ndof > 0 && chisq_ > 0.0)
-      return TMath::Prob(chisq_,ndof);
+    if( ndof > 0 && chisquared() > 0.0)
+      return TMath::Prob(chisquared(),ndof);
     else
       return -1.0;
   }
+
+  void CombinedTwoDPoints::print(std::ostream& os) const {
+    os << "CombinedTwoDPoints with " << nPoints() << " points, " << point() << " chisquared " << chisquared() << std::endl;
+  }
+}
+
+std::ostream& operator << (std::ostream& ost, mu2e::TwoDWeight const& wt) {
+  ost << "TwoDWeight position (" << wt.weightPosition()[0] << "," << wt.weightPosition()[1] << "), upper weight matrix ("
+    << wt.weight()(0,0) << "," <<  wt.weight()(0,1) << "," << wt.weight()(1,1) << ") ";
+  return ost;
 }
--- GeneralUtilities/src/TwoDPoint.cc
+++ GeneralUtilities/src/TwoDPoint.cc
@@ -2,29 +2,38 @@
 #include <stdexcept>
 #include <limits>
 namespace mu2e {
-  using SMAT = ROOT::Math::SMatrix<float,2,2,ROOT::Math::MatRepSym<float,2>>;
+  using SMAT = ROOT::Math::SMatrix<double,2,2,ROOT::Math::MatRepSym<double,2>>;
   TwoDPoint::TwoDPoint(VEC2 const& pos,float ucos, float uvar, float vvar) : pos_(pos.X(),pos.Y()) {
     if(fabs(ucos)>1.0) throw std::invalid_argument( "Unphysical covariance orientation" );
     if( uvar < std::numeric_limits<float>::min() || vvar < std::numeric_limits<float>::min())
       throw std::invalid_argument( "Unphysical variance" );
-    float ucos2 = ucos*ucos;
-    float usin2 = 1.0-ucos2;
-    float usin = sqrt(usin2);
-    float cov[3] = {ucos2*uvar + usin2*vvar, ucos*usin*(uvar -vvar),usin2*uvar + ucos2*vvar};
+    double ucos2 = ucos*ucos;
+    double usin2 = 1.0-ucos2;
+    double usin = sqrt(usin2);
+    double cov[3] = {ucos2*uvar + usin2*vvar, ucos*usin*(uvar -vvar),usin2*uvar + ucos2*vvar};
     cov_ = SMAT(cov,cov+3);
   }
 
   TwoDPoint::TwoDPoint(VEC3 const& pos,VEC3 const& udir, float uvar, float vvar) : TwoDPoint(VEC2(pos.X(),pos.Y()),udir.Dot(VEC3(1.0,0.0,0.0)),uvar,vvar){}
 
-  SMAT TwoDPoint::weight(float ivar) const {
+  SMAT TwoDPoint::weight(float ires) const {
     SMAT cov(cov_);
-    if(ivar > 0.0){
-      cov(0,0) += ivar;
-      cov(1,1) += ivar;
-    }
+    double ivar = ires*ires;
+    cov(0,0) += ivar;
+    cov(1,1) += ivar;
     int ifail(0);
     SMAT retval = cov.Inverse(ifail);
     if(ifail != 0)throw std::invalid_argument( "Inversion Failure" );
     return retval;
   }
+
+  void TwoDPoint::print(std::ostream& os) const {
+    os << *this << std::endl;
+  }
+}
+
+std::ostream& operator << (std::ostream& ost, mu2e::TwoDPoint const& pt) {
+  ost << "TwoDPoint position (" << pt.pos()[0] << "," << pt.pos()[1] << "), upper covariance ("
+    << pt.cov()(0,0) << "," << pt.cov()(0,1) << "," << pt.cov()(1,1) << ") ";
+  return ost;
 }
