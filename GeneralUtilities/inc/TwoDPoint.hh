cddc7e62b1e17a82a330b13461ee82ecc6afbd1e
David Nathan Brown <dave_brown@lbl.gov>
2023-04-09 19:02:44 -0500

    Move to lazy evaluation.  Make internal structs double precision

--- GeneralUtilities/inc/TwoDPoint.hh
+++ GeneralUtilities/inc/TwoDPoint.hh
@@ -9,13 +9,14 @@
 #include "Math/Vector2D.h"
 #include "Math/SVector.h"
 #include "Math/SMatrix.h"
+#include <iostream>
 namespace mu2e {
   class TwoDPoint {
     public:
       using VEC3 = ROOT::Math::XYZVectorF; // spatial only 3D vector
       using VEC2 = ROOT::Math::XYVectorF; // spatial only 2D vector
-      using SVEC = ROOT::Math::SVector<float,2>; // 2D algebraic vector
-      using SMAT = ROOT::Math::SMatrix<float,2,2,ROOT::Math::MatRepSym<float,2>>; // 2D symmetric matrix
+      using SVEC = ROOT::Math::SVector<double,2>; // 2D algebraic vector
+      using SMAT = ROOT::Math::SMatrix<double,2,2,ROOT::Math::MatRepSym<double,2>>; // 2D symmetric matrix
       TwoDPoint() {}
       TwoDPoint(SVEC const& pos, SMAT const& cov) : pos_(pos), cov_(cov) {} // explicit construct
       TwoDPoint(VEC2 const& pos,float ucos, float uvar, float vvar); // construct from a point, semi-major direction cosine to the X axis, and semi-major and minor variances
@@ -26,9 +27,11 @@ namespace mu2e {
       SMAT weight(float intrinsicvar=0.0) const; // invert the covariance to create the weight matrix.  Optionaly add a constant variance
       VEC2 pos2() const { return VEC2(pos_[0],pos_[1]); } // physical vector
       VEC3 pos3() const { return VEC3(pos_[0],pos_[1],0.0); } // physical vector; Z coordinate is meaningless
+      void print(std::ostream& os) const;
     private:
       SVEC pos_; // position in XY cartesian coordinates represented as an algebraic vector
       SMAT cov_; // covariance matrix in XY cartesian coordinates
   };
 }
+std::ostream& operator << (std::ostream& ost, mu2e::TwoDPoint const& pt);
 #endif
