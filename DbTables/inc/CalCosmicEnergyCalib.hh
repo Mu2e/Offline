04c16573ca390f5353773542aefa4d3607a9ef09
sophieMu2e <sophie@fnal.gov>
2023-05-14 14:46:02 -0500

    added cosmic table

--- DbTables/inc/CalCosmicEnergyCalib.hh
+++ DbTables/inc/CalCosmicEnergyCalib.hh
@@ -1,5 +1,5 @@
-#ifndef DbTables_CaloCosmicsEneTable_hh
-#define DbTables_CaloCosmicsEneTable_hh
+#ifndef DbTables_CalCosmicEnergyCalib_hh
+#define DbTables_CalCosmicEnergyCalib_hh
 
 
 #include <string>
@@ -10,53 +10,48 @@
 
 namespace mu2e {
 
-  class CaloCosmicsEneTable : public DbTable {
-    public:
+  class CalCosmicEnergyCalib : public DbTable {
+  public:
 
     class Row {
-      public:
-      Row(int roid, double Peak, double ErrPeak, double Width, double ErrWidth, double Chi2):
-      _roid(roid),_Peak(Peak),_ErrPeak(ErrPeak),_Width(Width),_ErrWidth(ErrWidth),_Chi2(Chi2) {}
-      int       roid()     const { return _roid;} // Offline ID
-      float     Peak()     const { return _Peak; }
-      float     ErrPeak()  const { return _ErrPeak; }
-      float     Width()    const { return _Width; }
-      float     ErrWidth() const { return _ErrWidth; }
-      float     Chi2()     const { return _Chi2; }
+    public:
+      Row(uint16_t roid, float EPeak, float ErrEPeak, float Width, float ErrWidth, float chisq):_roid(roid),_EPeak(EPeak), _ErrEPeak(ErrEPeak), _Width(Width), _ErrWidth(ErrWidth), _chisq(chisq){}
+      uint16_t  roid() const { return _roid;}
+      float EPeak() const { return _EPeak; }
+      float ErrEPeak() const { return _ErrEPeak; }
+      float Width() const { return _Width; }
+      float ErrWidth() const { return _ErrWidth; }
+      float chisq() const { return _chisq; }
 
-      private:
-      int   _roid;
-      float _Peak;
-      float _ErrPeak;
+    private:
+      uint16_t  _roid;
+      float _EPeak;
+      float _ErrEPeak;
       float _Width;
       float _ErrWidth;
-      float _Chi2;
+      float _chisq;
     };
 
-    constexpr static const char* cxname = "CaloCosmicsEneTable";
+    constexpr static const char* cxname = "CalCosmicEnergyCalib";
 
-    CaloCosmicsEneTable():DbTable(cxname,"calocosmicsenecalib",
-    "roid,Peak,ErrPeak,Width,ErrWidth,Chi2") {}
+    CalCosmicEnergyCalib():DbTable(cxname,"cal.sourcecalib","roid,EPeak,ErrEPeak,Width,ErrWidth,chisq"){}
 
     const Row& rowAt(const std::size_t index) const { return _rows.at(index);}
-    const Row& row(const int roid) const { return _rows.at(_chanIndex.at(roid));}
+    const Row& row(const int roid) const { return _rows.at(roid); }
     std::vector<Row> const& rows() const {return _rows;}
     std::size_t nrow() const override { return _rows.size(); };
     size_t size() const override { return baseSize() + nrow()*nrow()/2 + nrow()*sizeof(Row); };
 
     void addRow(const std::vector<std::string>& columns) override {
       int roid = std::stoi(columns[0]);
+      // enforce a strict sequential order - optional
       if(roid!=int(_rows.size())) {
-        throw cet::exception("CaloCosmicsEnergyCalib_BAD_INDEX")
-        << "CaloCosmicsEneTable::addRow found index out of order: "
+        throw cet::exception("CALOSOURCECALIB_BAD_INDEX")
+        << "CalCosmicEnergyCalib::addRow found index out of order: "
         <<roid << " != " << _rows.back().roid()+1 <<"\n";
       }
-      _rows.emplace_back(roid,
-      std::stoi(columns[1]),
-      std::stof(columns[2]),
-      std::stoi(columns[3]),
-      std::stof(columns[4]),
-      std::stof(columns[5]) );
+       _rows.emplace_back(roid,std::stoi(columns[1]),std::stof(columns[2]),std::stof(columns[3]),std::stof(columns[4]),std::stof(columns[5]));
+      // add this channel to the map index - optional
       //_chanIndex[_rows.back().roid()] = _rows.size()-1;
     }
 
@@ -64,19 +59,18 @@ namespace mu2e {
       Row const& r = _rows.at(irow);
       sstream << std::fixed << std::setprecision(5);
       sstream << r.roid()<<",";
-      sstream << r.Peak()<<",";
-      sstream << r.ErrPeak()<<",";
+      sstream << r.EPeak()<<",";
+      sstream << r.ErrEPeak()<<",";
       sstream << r.Width()<<",";
       sstream << r.ErrWidth()<<",";
-      sstream << r.Chi2();
+      sstream << r.chisq();
     }
 
     virtual void clear() override { baseClear(); _rows.clear();}
 
-    private:
+  private:
     std::vector<Row> _rows;
-    std::map<int,std::size_t> _chanIndex;
+    //std::map<int,std::size_t> _chanIndex;
   };
-
 };
 #endif
