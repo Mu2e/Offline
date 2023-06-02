5ae5f55186a62267e375763d718383e31a993369
Mete Yucel <myucel@fnal.gov>
2023-06-02 13:45:43 -0500

    added Keras Quality parameters

--- RecoDataProducts/inc/BkgCluster.hh
+++ RecoDataProducts/inc/BkgCluster.hh
@@ -14,24 +14,27 @@ namespace mu2e
    struct BkgCluster
    {
        //Default hit count chosen for compuational efficiency
-       BkgCluster()                              : _pos(),    _time(0.0),  _hits(), _flag(BkgClusterFlag::update)  {_hits.reserve(16);}
-       BkgCluster(XYZVectorF const& pos, float time) : _pos(pos), _time(time), _hits(), _flag(BkgClusterFlag::update)  {_hits.reserve(16);}
+       BkgCluster()                              : _pos(),    _time(0.0),  _hits(), _flag(BkgClusterFlag::update) , _kerasQ(0.) {_hits.reserve(16);}
+       BkgCluster(XYZVectorF const& pos, float time) : _pos(pos), _time(time), _hits(), _flag(BkgClusterFlag::update) , _kerasQ(0.)  {_hits.reserve(16);}
 
        BkgClusterFlag const&        flag() const {return _flag; }
-       XYZVectorF const&                pos()  const {return _pos;  }
+       XYZVectorF const&            pos()  const {return _pos;  }
        float                        time() const {return _time; }
        std::vector<unsigned> const& hits() const {return _hits; }
        std::vector<unsigned>&       hits()       {return _hits; }
+       float                        getKerasQ() const {return _kerasQ; }
 
        void pos(XYZVectorF const& pos)               {_pos = pos;}
        void time(float time)                     {_time = time;}
        void addHit(unsigned val)                 {_hits.emplace_back(val);}
        void clearHits()                          {_hits.clear();}
+       void setKerasQ(float kerasQ)                  {_kerasQ = kerasQ;}
 
        XYZVectorF                _pos;  // ideally should be a 2d vec - FIXME
        float                 _time;
        std::vector<unsigned> _hits;
        BkgClusterFlag        _flag;
+       float                _kerasQ; //result of keras result for the cluster
    };
 
    typedef std::vector<mu2e::BkgCluster> BkgClusterCollection;
