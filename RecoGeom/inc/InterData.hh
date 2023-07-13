5310e35f6336b54b78e1803aceb8aa9c33b8910a
David Nathan Brown <dave_brown@lbl.gov>
2023-07-12 20:23:13 -0500

    Define structure for Intersection and add test

--- RecoGeom/inc/InterData.hh
+++ RecoGeom/inc/InterData.hh
@@ -2,18 +2,19 @@
 //  Data payload for the intersection point of a PiecewiseTrajectory with a surface
 //  original author: David Brown (LBNL) 2023
 //
-#ifndef Mu2eKinKal_InterData_hh
-#define Mu2eKinKal_InterData_hh
+#ifndef RecoGeom_InterData_hh
+#define RecoGeom_InterData_hh
 #include "Offline/DataProducts/inc/GenVector.hh"
+#include "Offline/RecoGeom/inc/IntersectFlag.hh"
+
 namespace mu2e {
-  namespace Mu2eKinKal {
+  namespace RecoGeom {
     struct InterData {
-      enum Status{unknown=-1,nointersection=0,good,outsidetime,outsideboundary};
-      Status status_ = unknown;
-      XYZVectorD inter_; // intersection point
+      RecoGeom::IntersectFlag flag_; // intersection status
+      XYZVectorD pos_; // intersection position
       XYZVectorD norm_; // surface normal at intersection
       XYZVectorD pdir_; // particle direction at intersection
-      double time_; // particle time at intersection
+      double time_; // time at intersection (from particle)
     };
   }
 }
---
+++ RecoGeom/src/IntersectionTest_main.cc
@@ -0,0 +1,90 @@
+//
+// Test intersections with KinKal objects
+//
+#include "KinKal/Trajectory/ParticleTrajectory.hh"
+#include "KinKal/General/ParticleState.hh"
+#include "KinKal/General/TimeRange.hh"
+#include "Offline/RecoGeom/inc/Intersection.hh"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <getopt.h>
+
+using VEC3 = ROOT::Math::XYZVectorD;
+using mu2e::RecoGeom::Ray;
+using mu2e::RecoGeom::Cylinder;
+using mu2e::RecoGeom::Annulus;
+using mu2e::RecoGeom::Rectangle;
+using mu2e::RecoGeom::IntersectFlag;
+using KTRAJ = KinKal::LoopHelix;
+using PKTRAJ = KinKal::ParticleTrajectory<KTRAJ>;
+using KinKal::ParticleState;
+using KinKal::TimeRange;
+static struct option long_options[] = {
+  {"ccost",     required_argument, 0, 'c'  },
+  {"cphi",        required_argument, 0, 'p'  },
+  {"crad",        required_argument, 0, 'r'  },
+  {"clen",        required_argument, 0, 'l'  },
+  {"pcost",     required_argument, 0, 'C'  },
+  {"pphi",        required_argument, 0, 'P'  },
+  {"pmom",        required_argument, 0, 'M'  },
+  {NULL, 0,0,0}
+};
+
+void print_usage() {
+  printf("Usage: IntersectionTest  --crad --clen --ccost --cphi (cylinder radius, half length, axis direction) --pmom, --pcost, --pphi  (particle momentum in MeV/c, direction) ");
+}
+
+int main(int argc, char** argv) {
+
+  int opt;
+  int long_index =0;
+  VEC3 point(0.0,0.0,0.0);
+  double ccost(0.5), cphi(0.0), crad(400), clen(1000);
+  double pcost(0.5), pphi(0.0), pmom(400);
+  while ((opt = getopt_long_only(argc, argv,"",
+          long_options, &long_index )) != -1) {
+    switch (opt) {
+      case 'c' : ccost = atof(optarg);
+                 break;
+      case 'p' : cphi = atof(optarg);
+                 break;
+      case 'r' : crad = atof(optarg);
+                 break;
+      case 'l' : clen = atof(optarg);
+                 break;
+      case 'C' : pcost = atof(optarg);
+                 break;
+      case 'P' : pphi = atof(optarg);
+                 break;
+      case 'M' : pmom = atof(optarg);
+                 break;
+      default: print_usage();
+               exit(EXIT_FAILURE);
+    }
+  }
+  VEC3 origin(0.0,0.0,0.0);
+
+  double csint = sqrt(1.0-ccost*ccost);
+  VEC3 axis(csint*cos(cphi), csint*sin(cphi), ccost);
+  Cylinder cyl(axis,origin,crad,clen);
+  std::cout << "Test " << cyl << std::endl;
+
+  double psint = sqrt(1.0-pcost*pcost);
+  VEC3 momvec(psint*cos(pphi), psint*sin(pphi), pcost);
+  momvec *= pmom;
+  ParticleState pstate(origin,momvec,0.0,0.5,-1.0);
+//  std::cout << "Test " << pstate << std::endl;
+  std::cout << "Test particle momentum " << pstate.momentum3() << std::endl;
+  double speed = pstate.speed();
+  double tmax = 2*sqrt(crad*crad + clen*clen)/speed;
+
+  VEC3 bnom(0.0,0.0,1.0);
+  TimeRange trange(0.0,tmax);
+  KTRAJ ktraj(pstate,bnom,trange);
+  mu2e::RecoGeom::Intersection inter(ktraj,cyl, 0.0);
+  auto const& idata = inter.interData();
+  std::cout << "Intersection status " << idata.flag_ << " position " << idata.pos_ << " time " << idata.time_ << std::endl;
+
+  return 0;
+}
