bb816a837df58a5e7393ab9f9c8d637102d6d5c3
Andrew Edmonds <awje@bu.edu>
2023-07-14 15:15:17 -0500

    Put VD SpotSizeCollUpStr in correct mother volume

--- Mu2eG4/src/constructVirtualDetectors.cc
+++ Mu2eG4/src/constructVirtualDetectors.cc
@@ -1519,7 +1519,7 @@ namespace mu2e {
     if ( vdg->exist(vdId) ) {
 
       //const VolumeInfo& parent = _helper->locateVolInfo("MSTMMother");
-      const VolumeInfo& parent = _helper->locateVolInfo("HallAir");
+      const VolumeInfo& parent = _helper->locateVolInfo("stmDownstreamEnvelope");
       const double vdRIn  = 0.0;
       const double vdROut = _config.getDouble("vd.STMSSCollUpStr.r");
       const TubsParams vdParams(vdRIn, vdROut, vdg->getHalfLength());
