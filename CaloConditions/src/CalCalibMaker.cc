7f57bd7650baf1302b8ad2d74cac4a48c61abff3
sophieMu2e <sophie@fnal.gov>
2023-07-24 10:40:04 -0500

    added cal calib maker

--- CaloConditions/src/CalCalibMaker.cc
+++ CaloConditions/src/CalCalibMaker.cc
@@ -7,16 +7,33 @@ Written by S. Middleton
 namespace mu2e {
   typedef std::shared_ptr<CalCalib> ptr_t;
 
+/*******************************************************************/
   ptr_t CalCalibMaker::fromFcl() {
+  
+  if (_config.verbose()) {
+    cout << "CRalCalibMaker::fromFcl making nominal CalCalib\n";
+  }
   CalCalibPar nominal(_config.ADC2MeV(), _config.timeoffset(), _config.algID());
 
   size_t nChan = CaloConst::_nChannel;
 
+  if (_config.verbose()) {
+    cout << "CalCalibMaker::fromFcl filling " << nChan << " channels\n";
+    cout << "CalCalibMaker::fromFcl nominal " << fixed << setprecision(3)
+         << setw(10) << nominal.ADC2MeV() << setprecision(3) << setw(10)
+         << nominal.timeoffset() << setprecision(3) << setw(10)
+         << nominal.algID() << setprecision(3) << setw(10) << "\n";
+  }
+  
   CalCalib::CalibVec cvec(nChan, nominal);
   auto ptr = std::make_shared<CalCalib>(cvec);
   return ptr;
 
   } // end fromFcl
+/*******************************************************************/
+
+
+/*******************************************************************/
 
   ptr_t CalCalibMaker::fromDb(CalEnergyCalib::cptr_t) {
     // initially fill from fcl to get all the constants
@@ -25,5 +42,5 @@ namespace mu2e {
     return ptr;
 
   } // end fromDb
-
+/*******************************************************************/
 }
