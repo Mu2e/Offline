3446bc49fabdbbe802b1e743691b3d48599cca01
David Nathan Brown <dave_brown@lbl.gov>
2023-07-24 14:22:35 -0500

    Move code to KinKal

--- RecoGeom/src/Ray.cc
+++
@@ -1,5 +0,0 @@
-#include "Offline/RecoGeom/inc/Ray.hh"
-std::ostream& operator <<(std::ostream& ost, mu2e::RecoGeom::Ray const& ray){
-  ost << "Ray starting " << ray.start_ << " direction " << ray.dir_;
-  return ost;
-}
