9ed81d1c52808b77bafd4125dd28455b0c6772db
David Nathan Brown <dave_brown@lbl.gov>
2023-08-15 17:25:31 -0700

    Small tweaks

--- KinKalGeom/src/Tracker.cc
+++ KinKalGeom/src/Tracker.cc
@@ -9,11 +9,11 @@ namespace mu2e {
     Tracker::Tracker() :
       // cylinders are defined by TT_outer (_inner) virtual detectors
       // Disks are defined to match TT_front (mid, back) virtual detectors
-      outer_ { std::make_shared<Cylinder>(VEC3(0.0,0.0,1.0),VEC3(0.0,0.0,4.0),850.1,1635.11)},
+      outer_ { std::make_shared<Cylinder>(VEC3(0.0,0.0,1.0),VEC3(0.0,0.0,4.0),850.11,1635.11)},
       inner_{ std::make_shared<Cylinder>(VEC3(0.0,0.0,1.0),VEC3(0.0,0.0,4.0),376.9,1635.11)},
-      front_{ std::make_shared<Disk>(VEC3(0.0,0.0,1.0),VEC3(1.0,0.0,0.0),VEC3(0.0,0.0,-1631.11),850.1)},
-      mid_{ std::make_shared<Disk>(VEC3(0.0,0.0,1.0),VEC3(1.0,0.0,0.0),VEC3(0.0,0.0,10.1),850.1)},
-      back_{ std::make_shared<Disk>(VEC3(0.0,0.0,1.0),VEC3(1.0,0.0,0.0),VEC3(0.0,0.0,1639.11),850.1)}
+      front_{ std::make_shared<Disk>(VEC3(0.0,0.0,1.0),VEC3(1.0,0.0,0.0),VEC3(0.0,0.0,-1631.12),850.11)},
+      mid_{ std::make_shared<Disk>(VEC3(0.0,0.0,1.0),VEC3(1.0,0.0,0.0),VEC3(0.0,0.0,10.09),850.11)},
+      back_{ std::make_shared<Disk>(VEC3(0.0,0.0,1.0),VEC3(1.0,0.0,0.0),VEC3(0.0,0.0,1639.10),850.11)}
     {}
   }
 }
