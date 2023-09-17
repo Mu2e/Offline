f002592409fe98ac1a0b44f522077da1a649cb95
ehrlich-uva <ehrlich@virginia.edu>
2023-09-17 13:55:24 -0500

    reconstruct CRV hit position based on readout times at both counter ends

--- CRVAnalysis/src/CRVAnalysis.cc
+++ CRVAnalysis/src/CRVAnalysis.cc
@@ -54,7 +54,7 @@ namespace mu2e
       const CrvCoincidenceCluster &cluster = crvCoincidenceClusterCollection->at(i);
 
       //fill the Reco collection
-      recoInfo.emplace_back(cluster.GetCrvSectorType(), cluster.GetAvgCounterPos(),
+      recoInfo.emplace_back(cluster.GetCrvSectorType(), cluster.GetAvgHitPos(),
                             cluster.GetStartTime(), cluster.GetEndTime(),
                             cluster.GetPEs(), cluster.GetCrvRecoPulses().size(),
                             cluster.GetLayers().size(), cluster.GetSlope());
@@ -91,8 +91,8 @@ namespace mu2e
                               primaryParticle->pdgId(), primaryParticle->startMomentum().e() - primaryParticle->startMomentum().m(), primaryParticle->startPosition(),
                               parentParticle->pdgId(),  parentParticle->startMomentum().e()  - parentParticle->startMomentum().m(),  parentParticle->startPosition(),
                               gparentParticle->pdgId(), gparentParticle->startMomentum().e() - gparentParticle->startMomentum().m(), gparentParticle->startPosition(),
-                              clusterMC.GetEarliestHitPos(),
-                              clusterMC.GetEarliestHitTime(),
+                              clusterMC.GetAvgHitPos(),
+                              clusterMC.GetAvgHitTime(),
                               clusterMC.GetTotalEnergyDeposited());
         }
         else MCInfo.emplace_back();
@@ -234,12 +234,14 @@ namespace mu2e
          art::Handle<CrvDigiMCCollection> crvDigiMCCollection;
          if(crvWaveformsModuleLabel!="") event.getByLabel(crvWaveformsModuleLabel,"",crvDigiMCCollection); //this is an optional part for MC information
          double visibleEnergyDeposited  = 0;
-         double earliestHitTime         = NAN;
+         double earliestHitTime         = 0;
+         double avgHitTime              = 0;
          CLHEP::Hep3Vector earliestHitPos;
+         CLHEP::Hep3Vector avgHitPos;
          art::Ptr<SimParticle> mostLikelySimParticle;
          //for this reco pulse
          CrvMCHelper::GetInfoFromCrvRecoPulse(crvRecoPulse, crvDigiMCCollection, visibleEnergyDeposited,
-                                            earliestHitTime, earliestHitPos, mostLikelySimParticle);
+                                            earliestHitTime, earliestHitPos, avgHitTime, avgHitPos, mostLikelySimParticle);
 
          bool hasMCInfo = (mostLikelySimParticle.isNonnull()?true:false); //MC
          if(hasMCInfo)
@@ -251,7 +253,7 @@ namespace mu2e
                                  primaryParticle->pdgId(), primaryParticle->startMomentum().e() - primaryParticle->startMomentum().m(), primaryParticle->startPosition(),
                                  parentParticle->pdgId(),  parentParticle->startMomentum().e()  - parentParticle->startMomentum().m(),  parentParticle->startPosition(),
                                  gparentParticle->pdgId(), gparentParticle->startMomentum().e() - gparentParticle->startMomentum().m(), gparentParticle->startPosition(),
-                                 earliestHitPos, earliestHitTime, visibleEnergyDeposited);
+                                 avgHitPos, avgHitTime, visibleEnergyDeposited);
            }
          else
            MCInfo.emplace_back();
