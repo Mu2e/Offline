080a46d72eb421925a9114f0585eb603232e8f2d
Rob Kutschke <kutschke@fnal.gov>
2024-01-09 20:31:02 -0600

    Updates motivated by the comments in the review (PR 1169)

--- Mu2eUtilities/inc/ReSeedByEventID.hh
+++ Mu2eUtilities/inc/ReSeedByEventID.hh
@@ -3,30 +3,33 @@
 //
 // Reseed a random engine using the event id and a user supplied salt.
 //
+// Does not work with all CLHEP::RandomEngine concrete types.
+//   - see description in .cc file
+//
 
 #include "Offline/SeedService/inc/SeedService.hh"
 
 #include "canvas/Persistency/Provenance/EventID.h"
 #include "art/Framework/Services/Optional/RandomNumberGenerator.h"
-#include "CLHEP/Random/MixMaxRng.h"
+#include "CLHEP/Random/RandomEngine.h"
 
 namespace mu2e {
 
   class ReSeedByEventID{
 
   public:
-    ReSeedByEventID( CLHEP::HepRandomEngine& engine, SeedService::seed_t salt);
+    ReSeedByEventID( CLHEP::HepRandomEngine& engine, SeedService::seed_t salt, int verbosity=0 );
 
     void reseed ( art::EventID const& id ) const;
 
   private:
 
-    CLHEP::MixMaxRng *  _engine; // the engine to be reseeded
-    SeedService::seed_t _salt;   // user supplied salt.
+    CLHEP::HepRandomEngine& engine_;     // The engine to be reseeded
+    SeedService::seed_t     salt_;       // User supplied salt.
+    int                     verbosity_;  // Control amount of printout; 0=none.
 
   };
 
 } // namespace mu2e
-
 
 #endif
