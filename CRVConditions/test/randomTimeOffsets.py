f1c2fb81f4a0806cb978bb51aacc1a1fa8951d7c
ehrlich-uva <ehrlich@virginia.edu>
2024-02-25 02:03:06 -0600

    added CRV time offsets in MC and reco

--- CRVConditions/test/randomTimeOffsets.py
+++ CRVConditions/test/randomTimeOffsets.py
@@ -1,14 +1,23 @@
 import random
 import sys
+import csv
 
 if len(sys.argv) != 2:
-  print("usage \"randomTimeOffsets nChannels\"")
-  print("nChannels=22016 when using the full CRV\"")
+  print("usage \"randomTimeOffsets path_to_channelMapFile\"")
   sys.exit()
 
-nChannels=int(sys.argv[1])
+with open(sys.argv[1], "r") as channelMapFile:
+    fileReader = csv.reader(channelMapFile, delimiter="\t")
 
-print("TABLE CRVTime")
-print("#channel,timeOffset")
-for channel in range(nChannels):
-  print("{},{:.3f}".format(channel,random.gauss(0, 1)))
+    # Skip the first row, which is the header
+    next(fileReader)
+
+    print("TABLE CRVTime")
+    print("#channel,timeOffset")
+
+    timeOffsets = {}
+    for row in fileReader:
+        (channel, ROC, FEB, FEBchannel) = row
+        if not FEB in timeOffsets:
+           timeOffsets[FEB] = random.gauss(0, 1)
+        print("{},{:.3f}".format(channel,timeOffsets[FEB]))
