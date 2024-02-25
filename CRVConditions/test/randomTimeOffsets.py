af3e4ad8002a98bbc227b0c8f05cbee52f21ea0e
ehrlich-uva <ehrlich@virginia.edu>
2024-02-25 02:30:01 -0600

    added CrvSiPM part to random calib database file

--- CRVConditions/test/randomTimeOffsets.py
+++ CRVConditions/test/randomTimeOffsets.py
@@ -15,9 +15,16 @@ with open(sys.argv[1], "r") as channelMapFile:
     print("TABLE CRVTime")
     print("#channel,timeOffset")
 
+    channels = []
     timeOffsets = {}
     for row in fileReader:
         (channel, ROC, FEB, FEBchannel) = row
+        channels.append(channel)
         if not FEB in timeOffsets:
            timeOffsets[FEB] = random.gauss(0, 1)
         print("{},{:.3f}".format(channel,timeOffsets[FEB]))
+
+    print("TABLE CRVSiPM")
+    print("#channel,pedestal,calibPulseHeight,calibPulseArea")
+    for channel in channels:
+        print("{},{:.1f},{:.1f},{:.1f}".format(channel,100.0,11.4,394.6))
