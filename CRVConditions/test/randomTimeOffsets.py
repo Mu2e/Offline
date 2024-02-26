09e0f29eb18c63713cdd36f80f0a6d3f8bfc81b7
ehrlich-uva <ehrlich@virginia.edu>
2024-02-25 19:27:47 -0600

    more changes to CRV random time offset script

--- CRVConditions/test/randomTimeOffsets.py
+++ CRVConditions/test/randomTimeOffsets.py
@@ -2,29 +2,35 @@ import random
 import sys
 import csv
 
-if len(sys.argv) != 2:
-  print("usage \"randomTimeOffsets path_to_channelMapFile\"")
+if len(sys.argv) != 3:
+  print("usage \"randomTimeOffsets nChannels path_to_channelMapFile\"")
+  print("nChannels=22016 when using the full CRV\"")
   sys.exit()
 
-with open(sys.argv[1], "r") as channelMapFile:
+nChannels=int(sys.argv[1])
+
+with open(sys.argv[2], "r") as channelMapFile:
     fileReader = csv.reader(channelMapFile, delimiter="\t")
 
     # Skip the first row, which is the header
     next(fileReader)
 
+    timeOffsetsFEB = {}
+    timeOffsetsChannel = {}
+    for row in fileReader:
+        (channelStr, ROC, FEBStr, FEBchannel) = row
+        channel=int(channelStr)
+        FEB=int(FEBStr)
+        if not FEB in timeOffsetsFEB:
+           timeOffsetsFEB[FEB] = random.gauss(0, 1)
+        timeOffsetsChannel[channel] = timeOffsetsFEB[FEB]
+
     print("TABLE CRVTime")
     print("#channel,timeOffset")
-
-    channels = []
-    timeOffsets = {}
-    for row in fileReader:
-        (channel, ROC, FEB, FEBchannel) = row
-        channels.append(channel)
-        if not FEB in timeOffsets:
-           timeOffsets[FEB] = random.gauss(0, 1)
-        print("{},{:.3f}".format(channel,timeOffsets[FEB]))
+    for channel in range(nChannels):
+        print("{},{:.3f}".format(channel,timeOffsetsChannel.get(channel,0.0)))
 
     print("TABLE CRVSiPM")
     print("#channel,pedestal,calibPulseHeight,calibPulseArea")
-    for channel in channels:
+    for channel in range(nChannels):
         print("{},{:.1f},{:.1f},{:.1f}".format(channel,100.0,11.4,394.6))
