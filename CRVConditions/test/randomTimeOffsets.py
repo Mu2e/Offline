108b1253f0ad99a316dd107b3a22b121f2236623
ehrlich-uva <ehrlich@virginia.edu>
2024-04-04 12:51:32 -0500

    added CRV DB tables for perfect conditions, other CRV DB changes

--- CRVConditions/test/randomTimeOffsets.py
+++ CRVConditions/test/randomTimeOffsets.py
@@ -2,13 +2,16 @@ import random
 import sys
 import csv
 
-if len(sys.argv) != 3:
-  print("usage \"randomTimeOffsets nChannels path_to_channelMapFile\"")
+if len(sys.argv) != 6:
+  print("usage \"randomTimeOffsets nChannels path_to_channelMapFile sigma limitlow limithigh\"")
   print("nChannels=22016 when using the full CRV\"")
   print("nChannels=2048 when using the extracted position\"")
   sys.exit()
 
 nChannels=int(sys.argv[1])
+sigma=float(sys.argv[3])
+limitlow=float(sys.argv[4])
+limithigh=float(sys.argv[5])
 
 with open(sys.argv[2], "r") as channelMapFile:
     fileReader = csv.reader(channelMapFile, delimiter="\t")
@@ -23,10 +26,15 @@ with open(sys.argv[2], "r") as channelMapFile:
         channel=int(channelStr)
         FEB=int(FEBStr)
         if not FEB in timeOffsetsFEB:
-           timeOffsetsFEB[FEB] = random.gauss(0, 1)
+           while True:
+             r = random.gauss(0, sigma)
+             if r>limitlow and r<limithigh:
+               timeOffsetsFEB[FEB] = r
+               break
         timeOffsetsChannel[channel] = timeOffsetsFEB[FEB]
 
     print("TABLE CRVTime")
+    print("#{} channels, sigma: {}, limitlow: {}, limithigh: {}".format(nChannels,sigma,limitlow,limithigh))
     print("#channel,timeOffset")
     for channel in range(nChannels):
         print("{},{:.3f}".format(channel,timeOffsetsChannel.get(channel,0.0)))
