b21906338c404e61503bd155d5250ee067ee8073
David Nathan Brown <dave_brown@lbl.gov>
2024-04-12 14:02:08 -0500

    Add module to merge multiple KalSeed collections into a single Ptr collection

---
+++ TrkReco/src/MergeKalSeeds_module.cc
@@ -0,0 +1,64 @@
+//
+//  Merge KalSeeds from different reconstruction paths.
+//  This module produces an indirect (Ptr) collection, not a deep copy, to keep from break provenance or MC truth associations
+//  Note that, when persisting merged collections created by this module, the KalSeed object collections merged here MUST ALSO BE PERSISTED in
+//  order for the references to not be broken.
+//  Note too that the MC truth association of these KalSeeds ARE NOT MERGED by this module.  This is to avoid making reco code depend on MC.
+//  There is a separate module which merges the Assns between the KalSeeds referenced by the output of this module and the associated KalSeedMCs.
+//  original author: D. Brown (LBNL)
+//
+#include "fhiclcpp/types/Atom.h"
+#include "fhiclcpp/types/Sequence.h"
+#include "canvas/Utilities/InputTag.h"
+#include "canvas/Persistency/Common/Ptr.h"
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+// mu2e data products
+#include "Offline/RecoDataProducts/inc/KalSeed.hh"
+#include "Offline/Mu2eUtilities/inc/LsqSums4.hh"
+// C++
+#include <vector>
+
+namespace mu2e {
+  class MergeKalSeeds : public art::EDProducer {
+    public:
+      using Name=fhicl::Name;
+      using Comment=fhicl::Comment;
+      struct Config {
+        fhicl::Atom<int> debug{ Name("debugLevel"), Comment("Debug Level"), 0};
+        fhicl::Sequence<art::InputTag> seedCollTags {Name("KalSeedCollections"), Comment("KalSeed collections tomerge") };
+      };
+      using Parameters = art::EDProducer::Table<Config>;
+      explicit MergeKalSeeds(const Parameters& conf);
+      void produce(art::Event& evt) override;
+    private:
+      int debug_;
+      std::vector<art::InputTag> seedcolltags_;
+  };
+
+  MergeKalSeeds::MergeKalSeeds(const Parameters& config) : art::EDProducer{config}, debug_(config().debug())
+    {
+      for(auto const& seedcolltag :config().seedCollTags()){
+        consumes<KalSeedCollection>    (seedcolltag);
+        seedcolltags_.push_back(seedcolltag);
+        if(debug_ > 0) std::cout << "Merging KalSeeds from " << seedcolltag << std::endl;
+      }
+      produces<KalSeedPtrCollection>    ();
+    }
+
+  void MergeKalSeeds::produce(art::Event& event) {
+    // create output
+    std::unique_ptr<KalSeedPtrCollection> mkseeds(new KalSeedPtrCollection);
+    // loop over input KalSeed collections
+    for(auto const& seedcolltag : seedcolltags_) {
+      auto const& ksch = event.getValidHandle<KalSeedCollection>(seedcolltag);
+      auto const& ksc = *ksch;
+      if(debug_ > 2) std::cout << seedcolltag << " Has " << ksc.size() << " KalSeeds" << std::endl;
+      for(size_t ikseed = 0; ikseed <ksc.size(); ++ikseed){ mkseeds->emplace_back(ksch,ikseed); }
+    }
+    if(debug_ > 1) std::cout << "Merged " << mkseeds->size() << " KalSeeds" << std::endl;
+    event.put(std::move(mkseeds));
+  }
+}
+DEFINE_ART_MODULE(mu2e::MergeKalSeeds)
