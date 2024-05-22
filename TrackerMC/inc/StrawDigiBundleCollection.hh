013de2e08dfea9b3a9f1cd29e8af5c2beb336c1c
edcallaghan <ejc3@berkeley.edu>
2024-05-22 14:17:25 -0700

    change StrawDigiBundleCollection insertion signature to accept const refs, as opposed to templated-pointers

--- TrackerMC/inc/StrawDigiBundleCollection.hh
+++ TrackerMC/inc/StrawDigiBundleCollection.hh
@@ -39,13 +39,11 @@ namespace mu2e{
 
       // convenience methods for accepting new StrawDigiBundles
       // from different source situations
-      template<template<typename> class Pointer>
-      void Append(const Pointer<StrawDigiCollection>& digis,
-                  const Pointer<StrawDigiADCWaveformCollection>& adcss,
-                  const Pointer<StrawDigiMCCollection>& mcs);
-      template<template<typename> class Pointer>
-      void Append(const Pointer<StrawDigiCollection>& digis,
-                  const Pointer<StrawDigiADCWaveformCollection>& adcss);
+      void Append(const StrawDigiCollection& digis,
+                  const StrawDigiADCWaveformCollection& adcss,
+                  const StrawDigiMCCollection& mcs);
+      void Append(const StrawDigiCollection& digis,
+                  const StrawDigiADCWaveformCollection& adcss);
       void Append(const StrawDigiBundle bundle);
 
       // utility functions
@@ -73,67 +71,6 @@ namespace mu2e{
     private:
       /**/
   };
-
-  // forward size query to underlying container
-  size_t StrawDigiBundleCollection::size() const{
-    auto rv = this->bundles.size();
-    return rv;
-  }
-
-  // forward iterator access to underlying bundles
-  SDBC_iterator StrawDigiBundleCollection::begin(){
-    auto rv = this->bundles.begin();
-    return rv;
-  }
-
-  SDBC_const_iterator StrawDigiBundleCollection::begin() const{
-    auto rv = this->bundles.begin();
-    return rv;
-  }
-
-  SDBC_iterator StrawDigiBundleCollection::end(){
-    auto rv = this->bundles.end();
-    return rv;
-  }
-
-  SDBC_const_iterator StrawDigiBundleCollection::end() const{
-    auto rv = this->bundles.end();
-    return rv;
-  }
-
-  // convenience methods for accepting new StrawDigiBundles
-  // from different source situations
-  template<template<typename> class Pointer>
-  void StrawDigiBundleCollection::Append(const Pointer<StrawDigiCollection>& digis,
-                                         const Pointer<StrawDigiADCWaveformCollection>& adcss,
-                                         const Pointer<StrawDigiMCCollection>& mcs){
-      if ((digis->size() != adcss->size()) || (digis->size() != mcs->size())){
-        std::string msg = "Attempting to append unsynchronized StrawDigi triplets: lengths = " + std::to_string(digis->size()) + ", " + std::to_string(adcss->size()) + ", " + std::to_string(mcs->size());
-        throw cet::exception("TRIPLET SIZE MISMATCH") << msg << std::endl;
-      }
-      for (size_t i = 0 ; i < digis->size() ; i++){
-        auto digi = digis->at(i);
-        auto adcs = adcss->at(i);
-        auto mc   = mcs->at(i);
-        this->bundles.emplace_back(digi, adcs, mc);
-      }
-  }
-
-  template<template<typename> class Pointer>
-  void StrawDigiBundleCollection::Append(const Pointer<StrawDigiCollection>& digis,
-                                         const Pointer<StrawDigiADCWaveformCollection>& adcss){
-      if (digis->size() != adcss->size()){
-        std::string msg = "Attempting to append unsynchronized StrawDigi doublets: lengths = " + std::to_string(digis->size()) + ", " + std::to_string(adcss->size());
-        throw cet::exception("DOUBLET SIZE MISMATCH") << msg << std::endl;
-      }
-      for (size_t i = 0 ; i < digis->size() ; i++){
-        const auto& digi = digis->at(i);
-        const auto& adcs = adcss->at(i);
-        StrawDigiBundle bundle(digi, adcs);
-        this->bundles.emplace_back(digi, adcs);
-        continue;
-      }
-  }
 }
 
 #endif
