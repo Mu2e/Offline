f07126a61b08d0da54dde4c52506e1315cb755e5
edcallaghan <ejc3@berkeley.edu>
2024-06-26 18:37:20 -0500

    consolidate StrawDigi-/TrkStrawHit- provenance classes into single DigiProvenance class

---
+++ MCDataProducts/inc/DigiProvenance.hh
@@ -0,0 +1,26 @@
+// Flag to designate a digi as having been produced from simulation,
+// read from preexisting data, or a hybridization fo the two
+// Ed Callaghan, 2024
+
+#ifndef MCDataProducts_DigiProvenance_hh
+#define MCDataProducts_DigiProvenance_hh
+
+// stl
+#include <map>
+#include <string>
+
+// mu2e
+#include <Offline/GeneralUtilities/inc/EnumToStringSparse.hh>
+
+namespace mu2e{
+  // enum equipped with std::string descriptions
+  class DigiProvenanceDetail{
+    public:
+      enum enum_type {unknown=0, Simulation, Mixed, External};
+      static std::string const& typeName();
+      static std::map<enum_type, std::string> const& names();
+  };
+  using DigiProvenance = EnumToStringSparse<DigiProvenanceDetail>;
+}
+
+#endif
