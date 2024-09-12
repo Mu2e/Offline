a8c35c62b27f6d27d0d6afcfd6b1e14ddf01abce
Gianantonio Pezzullo <giani.pezzu@gmail.com>
2024-09-12 12:40:51 -0500

    fixed the TriggerInfoMerger module

--- Trigger/src/MergeTriggerInfo_module.cc
+++ Trigger/src/MergeTriggerInfo_module.cc
@@ -11,6 +11,9 @@
 #include "art/Framework/Principal/Handle.h"
 // mu2e data products
 #include "Offline/RecoDataProducts/inc/TriggerInfo.hh"
+#include "Offline/RecoDataProducts/inc/KalSeed.hh"
+#include "Offline/RecoDataProducts/inc/HelixSeed.hh"
+#include "Offline/RecoDataProducts/inc/TimeCluster.hh"
 // C++
 #include <vector>
 #include <memory>
@@ -25,8 +28,8 @@ namespace mu2e {
     using  Name    = fhicl::Name;
     using  Comment = fhicl::Comment;
     struct Config {
-      fhicl::Atom<int> debug{ Name("debugLevel"),
-          Comment("Debug Level"), 0};
+      fhicl::Atom<int> debug   { Name("debugLevel"), Comment("Debug Level"), 0};
+      fhicl::Atom<int> ntupMode{ Name("ntupMode")  , Comment("Ntupleing mode"), 0};
     };
 
     using        Parameters = art::EDProducer::Table<Config>;
@@ -34,20 +37,29 @@ namespace mu2e {
     void         produce(art::Event& evt) override;
   private:
     int          _debug;
+    int          _ntupMode;
   };
 
   MergeTriggerInfo::MergeTriggerInfo(const Parameters& config) :
     art::EDProducer{config},
-    _debug   (config().debug())
+    _debug   (config().debug()),
+    _ntupMode(config().ntupMode())
   {
     produces<TriggerInfoCollection>    ();
+    if (_ntupMode == 1){
+      produces<KalSeedCollection>();
+      produces<HelixSeedCollection>();
+      produces<TimeClusterCollection>();
+    }
   }
 
   void MergeTriggerInfo::produce(art::Event& event) {
-    std::unique_ptr<TriggerInfoCollection> tiCol(new TriggerInfoCollection);
+    std::unique_ptr<TriggerInfoCollection>   tiCol(new TriggerInfoCollection);
+    std::unique_ptr<KalSeedCollection>       ksCol(new KalSeedCollection);
+    std::unique_ptr<HelixSeedCollection>     hsCol(new HelixSeedCollection);
+    std::unique_ptr<TimeClusterCollection>   tcCol(new TimeClusterCollection);
 
-    std::vector<art::Handle<TriggerInfo> > list_of_triggerInfo;
-    event.getMany<TriggerInfo>();
+    std::vector<art::Handle<TriggerInfo> > list_of_triggerInfo = event.getMany<TriggerInfo>();
 
     if(_debug > 0){
       std::cout << "["<<moduleDescription().moduleLabel() << "] number of TriggerInfo found in the is: "<< list_of_triggerInfo.size() << std::endl;
@@ -55,10 +67,32 @@ namespace mu2e {
 
     for (auto & trigInfoH: list_of_triggerInfo){
       TriggerInfo trigInfo(*trigInfoH.product());
+      if(_debug > 0){
+        std::cout << "["<<moduleDescription().moduleLabel() << "] helices, tracks: "<< trigInfo.tracks().size() << " " << trigInfo.helixes().size() << std::endl;
+      }
+      if (_ntupMode == 1){
+        for (auto trkPtr: trigInfo.tracks()){
+          KalSeed trk(*trkPtr.get());
+          ksCol->push_back(trk);
+        }
+        for (auto hsPtr : trigInfo.helixes()){
+          HelixSeed hs(*hsPtr.get());
+          hsCol->push_back(hs);
+        }
+        for (auto tcPtr : trigInfo.hitClusters()){
+          TimeCluster tc(*tcPtr.get());
+          tcCol->push_back(tc);
+        }
+      }
       tiCol->push_back(trigInfo);
     }
 
     event.put(std::move(tiCol));
+    if (_ntupMode == 1){
+      event.put(std::move(ksCol));
+      event.put(std::move(hsCol));
+      event.put(std::move(tcCol));
+    }
   }
 }
 DEFINE_ART_MODULE(mu2e::MergeTriggerInfo)
