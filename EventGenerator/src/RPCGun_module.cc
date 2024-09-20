2a4281ab9c6393a095aefb6afc9431b0e4c6839e
sophieMu2e <sophie@fnal.gov>
2024-09-20 12:53:54 -0500

    fixes

--- EventGenerator/src/RPCGun_module.cc
+++ EventGenerator/src/RPCGun_module.cc
@@ -156,7 +156,7 @@ namespace mu2e {
       double tau = part->endProperTime() / gc.getParticleLifetime(part->pdgId());
       while(part->parent().isNonnull()) { //while particle has a parent which is not null
         part = part->parent();
-        if ( abs(part->pdgId()) == 211 ) { //if pion
+        if ( abs(part->pdgId()) == PDGCode::pi_plus ) { //if pion
             tau += part->endProperTime() / gc.getParticleLifetime(part->pdgId());
           }
       }
