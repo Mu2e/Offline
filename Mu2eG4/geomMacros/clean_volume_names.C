4adcb16d9151594d427a3bcd8da6c2fc528fa7a9
Andrew Edmonds <aedmonds@york.cuny.edu>
2024-10-07 09:42:02 -0500

    Don't assume 9 chars and add a separate clean_path function

--- Mu2eG4/geomMacros/clean_volume_names.C
+++ Mu2eG4/geomMacros/clean_volume_names.C
@@ -4,15 +4,24 @@
 //
 
 std::string clean_volume_names(std::string name) {
-  bool all_found = false;
-  while (!all_found) {
-    auto pos_0x = name.find("0x");
-    if (pos_0x == std::string::npos) {
-      all_found = true;
-    }
-    else {
-      name = name.erase(pos_0x, 9); // always 9 chars
-    }
+  auto pos_0x = name.find("0x");
+  if (pos_0x != std::string::npos) {
+    name = name.erase(pos_0x, name.size()-pos_0x);
   }
+
   return name;
+}
+
+std::string clean_path_names(std::string name) {
+
+  std::string clean_path;
+
+  std::vector<std::string> tokens;
+  std::stringstream ssname(name);
+  std::string temp;
+  while (std::getline(ssname, temp, '/')) {
+    clean_path += clean_volume_names(temp) + "/";
+  }
+
+  return clean_path;
 }
