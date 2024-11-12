e87ff4a0fab8a3cc00c93072c7d75a2eb795b42a
David Nathan Brown <dave_brown@lbl.gov>
2024-11-11 21:41:24 -0800

    Add option to save trajectory inside the tracker volume

--- Mu2eKinKal/inc/KKFitSettings.hh
+++ Mu2eKinKal/inc/KKFitSettings.hh
@@ -86,7 +86,7 @@ namespace mu2e {
       fhicl::Atom<float> maxStrawDOCA { Name("MaxStrawDOCA"), Comment("Max DOCA to add straw material (mm)") };
       fhicl::Atom<float> maxStrawDOCAConsistency { Name("MaxStrawDOCAConsistency"), Comment("Max DOCA chi-consistency to add straw material") };
       // extension and sampling
-      fhicl::Atom<std::string> saveTraj { Name("SaveTrajectory"), Comment("How to save the trajectory in the KalSeed: None, Full, or T0 (just the t0 segment)") };
+      fhicl::Atom<std::string> saveTraj { Name("SaveTrajectory"), Comment("How to save the trajectory in the KalSeed: None, Full, TrackerVolume, or T0 (1 segment containing t0)") };
     };
     // struct for configuring a KinKal fit module
     struct KKModuleConfig {
