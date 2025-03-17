7fb995a49bb43c46c2c06678ab3406135afd8dc9
Ray Culbertson <rlc@fnal.gov>
2025-03-17 14:48:44 -0500

    reduce constructors

--- DbService/inc/GrlHeader.hh
+++ DbService/inc/GrlHeader.hh
@@ -13,9 +13,10 @@ namespace mu2e {
 class GrlHeader {
  public:
   typedef std::vector<GrlHeader> HeaderVec;
-  GrlHeader(const std::string& name, int locked = 0, int lid = -1,
-            std::string const& create_time = "",
-            std::string const& create_user = "") :
+  GrlHeader(const std::string& name) :
+      _name(name), _locked(0), _lid(-1), _create_time(""), _create_user("") {}
+  GrlHeader(const std::string& name, int locked, int lid,
+            std::string const& create_time, std::string const& create_user) :
       _name(name),
       _locked(locked), _lid(lid), _create_time(create_time),
       _create_user(create_user) {}
