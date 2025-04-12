0f8f3e5a271f40a91355ec94c5cd9b3426b217b3
Natalie Harrison <natalieharrison.2718@gmail.com>
2025-04-12 09:08:49 -0500

    Updated CRV.cc by consolidating rectangles

--- KinKalGeom/inc/CRV.hh
+++ KinKalGeom/inc/CRV.hh
@@ -16,54 +16,33 @@ namespace mu2e {
       CRV();
       // accessors
       // return by reference
-      auto const& rightSector1() const { return *rightSector1_; }
-      auto const& rightSector2() const { return *rightSector2_; }
-      auto const& rightSector3() const { return *rightSector3_; }
-      auto const& rightSector4() const { return *rightSector4_; }
-      auto const& rightSector5() const { return *rightSector5_; }
-      auto const& rightSector6() const { return *rightSector6_; }
-      auto const& leftSector1() const { return *leftSector1_; }
-      auto const& leftSector2() const { return *leftSector2_; }
-      auto const& leftSector3() const { return *leftSector3_; }
-      auto const& topSector1() const { return *topSector1_; }
-      auto const& topSector2() const { return *topSector2_; }
-      auto const& topSector3() const { return *topSector3_; }
-      auto const& topSector4() const { return *topSector4_; }
-      auto const& topSector5() const { return *topSector5_; }
-      auto const& extSector1() const { return *extSector1_; }
-      auto const& extSector2() const { return *extSector2_; }
-      auto const& upstreamSector1() const { return *upstreamSector1_; }
-      auto const& downstreamSector1() const { return *downstreamSector1_; }
-      auto const& downstreamSector2() const { return *downstreamSector2_; }
-      auto const& downstreamSector3() const { return *downstreamSector3_; }
-      auto const& downstreamSector4() const { return *downstreamSector4_; }
+      auto const& rightSectorBlock() const { return *rightSectorBlock_; }
+      auto const& rightSectorHole() const { return *rightSectorHole_; }
+      auto const& cryoSectorHole() const { return *cryoSectorHole_; }
+      auto const& leftSectorBlock() const { return *leftSectorBlock_; }
+      auto const& leftSectorHole() const { return *leftSectorHole_; }
+      auto const& topSectorBlock() const { return *topSectorBlock_; }
+      auto const& extSectorBlock() const { return *extSectorBlock_; }
+      auto const& upstreamSector1() const { return *upstreamSectorBlock_; }
+      auto const& downstreamSectorBlock() const { return *downstreamSectorBlock_; }
+      auto const& downstreamSectorHole() const { return *downstreamSectorHole_; }
       auto const& cryoSector1() const { return *cryoSector1_; }
       auto const& cryoSector2() const { return *cryoSector2_; }
-      auto const& rightSector1Ptr() const { return rightSector1_; }
-      auto const& rightSector2Ptr() const { return rightSector2_; }
-      auto const& rightSector3Ptr() const { return rightSector3_; }
-      auto const& rightSector4Ptr() const { return rightSector4_; }
-      auto const& rightSector5Ptr() const { return rightSector5_; }
-      auto const& rightSector6Ptr() const { return rightSector6_; }
-      auto const& leftSector1Ptr() const { return leftSector1_; }
-      auto const& leftSector2Ptr() const { return leftSector2_; }
-      auto const& leftSector3Ptr() const { return leftSector3_; }
-      auto const& topSector1Ptr() const { return topSector1_; }
-      auto const& topSector2Ptr() const { return topSector2_; }
-      auto const& topSector3Ptr() const { return topSector3_; }
-      auto const& topSector4Ptr() const { return topSector4_; }
-      auto const& topSector5Ptr() const { return topSector5_; }
-      auto const& extSector1Ptr() const { return extSector1_; }
-      auto const& extSector2Ptr() const { return extSector2_; }
-      auto const& upstreamSector1Ptr() const { return upstreamSector1_; }
-      auto const& downstreamSector1Ptr() const { return downstreamSector1_; }
-      auto const& downstreamSector2Ptr() const { return downstreamSector2_; }
-      auto const& downstreamSector3Ptr() const { return downstreamSector3_; }
-      auto const& downstreamSector4Ptr() const { return downstreamSector4_; }
+      //
+      auto const& rightSectorBlockPtr() const { return rightSectorBlock_; }
+      auto const& rightSectorHolePtr() const { return rightSectorHole_; }
+      auto const& cryoSectorHolePtr() const { return cryoSectorHole_; }
+      auto const& leftSectorBlockPtr() const { return leftSectorBlock_; }
+      auto const& leftSectorHolePtr() const { return leftSectorHole_; }
+      auto const& topSectorBlockPtr() const { return topSectorBlock_; }
+      auto const& extSectorBlockPtr() const { return extSectorBlock_; }
+      auto const& upstreamSectorBlockPtr() const { return upstreamSectorBlock_; }
+      auto const& downstreamSectorBlockPtr() const { return downstreamSectorBlock_; }
+      auto const& downstreamSectorHolePtr() const { return downstreamSectorHole_; }
       auto const& cryoSector1Ptr() const { return cryoSector1_; }
       auto const& cryoSector2Ptr() const { return cryoSector2_; }
       private:
-      RecPtr rightSector1_, rightSector2_, rightSector3_, rightSector4_, rightSector5_, rightSector6_, leftSector1_, leftSector2_, leftSector3_, topSector1_, topSector2_, topSector3_, topSector4_, topSector5_, extSector1_, extSector2_, upstreamSector1_, downstreamSector1_, downstreamSector2_, downstreamSector3_, downstreamSector4_, cryoSector1_, cryoSector2_;
+      RecPtr rightSectorBlock_, rightSectorHole_, cryoSectorHole_, leftSectorBlock_, leftSectorHole_, topSectorBlock_, extSectorBlock_, upstreamSectorBlock_, downstreamSectorBlock_, downstreamSectorHole_, cryoSector1_, cryoSector2_;
     };
   }
 }
--- KinKalGeom/src/CRV.cc
+++ KinKalGeom/src/CRV.cc
@@ -4,6 +4,7 @@
   // 04/04/25 : made with newest geometry. Rectangles are set within CRV modules so that they are at the depth of the center of the first layer. In offline they are treated as volumes.
   // norm here = layerDirection in offline
   // udir = gapDirection in offline
+  // simplified all sectors into single rectangle with additional rectangles representing holes.
 */
 
 namespace mu2e {
@@ -12,29 +13,18 @@ namespace mu2e {
     using KinKal::Rectangle;
     // currently use hard-coded geometry
     CRV::CRV() :
-      rightSector1_{ std::make_shared<Rectangle>(VEC3(-1.0,0.0,0.0),VEC3(0.0,0.0,1.0), VEC3(-2537.19,275.3,-10273.77),2066.25,2275.)},
-      rightSector2_{ std::make_shared<Rectangle>(VEC3(-1.0,0.0,0.0),VEC3(0.0,0.0,1.0), VEC3(-2537.19,275.3,-2835.27),5372.25,2275.)},
-      rightSector3_{ std::make_shared<Rectangle>(VEC3(-1.0,0.0,0.0),VEC3(0.0,0.0,1.0), VEC3(-2537.19,2058.03,2950.23),413.25,522.5)},
-      rightSector4_{ std::make_shared<Rectangle>(VEC3(-1.0,0.0,0.0),VEC3(0.0,0.0,1.0), VEC3(-2537.19,-471.97,2950.23),413.25,1520.)},
-      rightSector5_{ std::make_shared<Rectangle>(VEC3(-1.0,0.0,0.0),VEC3(0.0,0.0,1.0), VEC3(-2537.19,275.3,4189.98),826.5,2275.)},
-      rightSector6_{ std::make_shared<Rectangle>(VEC3(-1.0,0.0,0.0),VEC3(0.0,0.0,1.0), VEC3(-2537.19,950.53,6669.48),1653.,1600.)},
-      leftSector1_{ std::make_shared<Rectangle>(VEC3(1.0,0.0,0.0),VEC3(0.0,0.0,1.0), VEC3(2537.19,275.53,-5728.02),1653.,2275.)},
-      leftSector2_{ std::make_shared<Rectangle>(VEC3(1.0,0.0,0.0),VEC3(0.0,0.0,1.0), VEC3(2537.19,275.53,470.73),4545.75,2275.)},
-      leftSector3_{ std::make_shared<Rectangle>(VEC3(1.0,0.0,0.0),VEC3(0.0,0.0,1.0), VEC3(2537.19,950.53,6669.48),1653.,1600.)},
-      topSector1_{ std::make_shared<Rectangle>(VEC3(0.0,1.0,0.0),VEC3(0.0,0.0,1.0), VEC3(0.,2663.12,-11100.27),1239.5,3000.)},
-      topSector2_{ std::make_shared<Rectangle>(VEC3(0.0,1.0,0.0),VEC3(0.0,0.0,1.0), VEC3(0.,2663.12,-9034.02),826.5,3000.)},
-      topSector3_{ std::make_shared<Rectangle>(VEC3(0.0,1.0,0.0),VEC3(0.0,0.0,1.0), VEC3(0.,2663.12,-6554.52),1653.,3000.)},
-      topSector4_{ std::make_shared<Rectangle>(VEC3(0.0,1.0,0.0),VEC3(0.0,0.0,1.0), VEC3(0.,2663.12,470.73),5372.25,3000.)},
-      topSector5_{ std::make_shared<Rectangle>(VEC3(0.0,1.0,0.0),VEC3(0.0,0.0,1.0), VEC3(0.,2663.12,7082.73),1239.75,3000.)},
-      extSector1_{ std::make_shared<Rectangle>(VEC3(0.0,1.0,0.0),VEC3(-1.0,0.0,0.0), VEC3(4034.17,2917.12,-9985.2),413.25,2500.)},
-      extSector2_{ std::make_shared<Rectangle>(VEC3(0.0,1.0,0.0),VEC3(-1.0,0.0,0.0), VEC3(3207.67,2917.12,-9985.2),413.25,2500.)},
-      upstreamSector1_{ std::make_shared<Rectangle>(VEC3(0.0,0.0,1.0),VEC3(0.0,-1.0,0.0), VEC3(650.,1597.12,-12599.76),1653.,3450.)},
-      downstreamSector1_{ std::make_shared<Rectangle>(VEC3(0.0,0.0,-1.0),VEC3(0.0,-1.0,0.0), VEC3(0.,1759.67,8472.26),1239.75,2850.)},
-      downstreamSector2_{ std::make_shared<Rectangle>(VEC3(0.0,0.0,-1.0),VEC3(0.0,-1.0,0.0), VEC3(-1665.,106.67,8472.26),413.25,1185.)},
-      downstreamSector3_{ std::make_shared<Rectangle>(VEC3(0.0,0.0,-1.0),VEC3(0.0,-1.0,0.0), VEC3(1665.,106.67,8472.26),413.25,1185.)},
-      downstreamSector4_{ std::make_shared<Rectangle>(VEC3(0.0,0.0,-1.0),VEC3(0.0,-1.0,0.0), VEC3(0.,-719.83,8472.26),413.25,2850.)},
-      cryoSector1_{ std::make_shared<Rectangle>(VEC3(-1.0,0.0,0.0),VEC3(0.0,-1.0,0.0), VEC3(-7130.34,2006.65,3340.7),516.5625,850.)},
-      cryoSector2_{ std::make_shared<Rectangle>(VEC3(-1.0,0.0,0.0),VEC3(0.0,-1.0,0.0), VEC3(-7130.34,1079.,3618.2),103.3125,572.5)}
+      rightSectorBlock_{ std::make_shared<Rectangle>(VEC3(-1.0,0.0,0.0),VEC3(0.0,0.0,1.0), VEC3(-2537.19,275.3,-2008.77),10331.25,2275.)},
+      rightSectorHole_{ std::make_shared<Rectangle>(VEC3(-1.0,0.0,0.0),VEC3(0.0,0.0,1.0), VEC3(-2537.19,-1324.47,6669.48),1653.,675.)},
+      cryoSectorHole_{ std::make_shared<Rectangle>(VEC3(-1.0,0.0,0.0),VEC3(0.0,0.0,1.0), VEC3(-2537.19,1273.03,2950.23),413.25,232.5)},
+      leftSectorBlock_{ std::make_shared<Rectangle>(VEC3(1.0,0.0,0.0),VEC3(0.0,0.0,1.0), VEC3(2537.19,275.53,470.73),7851.75,2275.)},
+      leftSectorHole_{ std::make_shared<Rectangle>(VEC3(1.0,0.0,0.0),VEC3(0.0,0.0,1.0), VEC3(2537.19,-1324.47,6669.48),1653.,675.)},
+      topSectorBlock_{ std::make_shared<Rectangle>(VEC3(0.0,1.0,0.0),VEC3(0.0,0.0,1.0), VEC3(0.,2663.12,-2008.77),10331.25,3000.)},
+      extSectorBlock_{ std::make_shared<Rectangle>(VEC3(0.0,1.0,0.0),VEC3(-1.0,0.0,0.0), VEC3(3595.25,2917.12,-9985.2),826.5,2500.)},
+      upstreamSectorBlock_{ std::make_shared<Rectangle>(VEC3(0.0,0.0,1.0),VEC3(0.0,-1.0,0.0), VEC3(650.,1597.92,-12599.76),1653.,3450.)},
+      downstreamSectorBlock_{ std::make_shared<Rectangle>(VEC3(0.0,0.0,-1.0),VEC3(0.0,-1.0,0.0), VEC3(0.,933.17,8472.26),2066.25,2850.)},
+      downstreamSectorHole_{ std::make_shared<Rectangle>(VEC3(0.0,0.0,-1.0),VEC3(0.0,-1.0,0.0), VEC3(0.,106.67,8472.26),413.25,480.)},
+      cryoSector1_{ std::make_shared<Rectangle>(VEC3(-1.0,0.0,0.0),VEC3(0.0,-1.0,0.0), VEC3(-3226.34,2006.65,3340.7),516.5625,850.)},
+      cryoSector2_{ std::make_shared<Rectangle>(VEC3(-1.0,0.0,0.0),VEC3(0.0,-1.0,0.0), VEC3(-3226.34,1079.,3618.2),103.3125,572.5)}
     {}
   }
 }
