376e0d70e0d601defdee50494b1d31ae36da652d
michaelmackenzie <michaelmackenzie@u.northwestern.edu>
2025-07-01 15:52:11 -0500

    Move pion tau evaluation, add pion count filtering

--- EventGenerator/src/PionFilter_module.cc
+++ EventGenerator/src/PionFilter_module.cc
@@ -7,6 +7,7 @@
 #include "art_root_io/TFileService.h"
 #include "art/Framework/Principal/Handle.h"
 #include "art_root_io/TFileService.h"
+#include "fhiclcpp/types/OptionalAtom.h"
 
 // Mu2e includes.
 #include "Offline/MCDataProducts/inc/StageParticle.hh"
@@ -15,6 +16,7 @@
 #include "Offline/GlobalConstantsService/inc/GlobalConstantsHandle.hh"
 #include "Offline/GlobalConstantsService/inc/PhysicsParams.hh"
 #include "Offline/GlobalConstantsService/inc/ParticleDataList.hh"
+#include "Offline/Mu2eUtilities/inc/SimParticleGetTau.hh"
 #include "Offline/TrackerGeom/inc/Tracker.hh"
 #include <iostream>
 #include <string>
@@ -28,11 +30,12 @@ namespace mu2e {
       struct Config {
         using Name=fhicl::Name;
         using Comment=fhicl::Comment;
-        fhicl::Atom<int> diagLevel{Name("diagLevel")};
-        fhicl::Atom<double> tmin{Name("tmin")};
-        fhicl::Atom<double> tmax{Name("tmax")};
-        fhicl::Atom<int> processCode{Name("processCode")};
-        fhicl::Atom<bool> isNull{Name("isNull")};
+        fhicl::Atom<int> diagLevel{Name("diagLevel"), Comment("Diagnostic print level"), 0};
+        fhicl::Atom<double> tmin{Name("tmin"), Comment("Selected pion minimum end time")};
+        fhicl::Atom<double> tmax{Name("tmax"), Comment("Selected pion maximum end time")};
+        fhicl::OptionalAtom<int> maxPions{Name("maxPions"), Comment("Maximum number of pion stops")};
+        fhicl::Atom<int> processCode{Name("processCode"), Comment("Pion end process code to select")};
+        fhicl::Atom<bool> isNull{Name("isNull"), Comment("Skip filtering is turned on"), false};
       };
       explicit PionFilter(const art::EDFilter::Table<Config>& config);
       virtual bool filter(art::Event& event) override;
@@ -44,6 +47,7 @@ namespace mu2e {
       int diagLevel_;
       double tmin_;
       double tmax_;
+      int maxPions_;
       int processCode_;
       bool isNull_;
       float _totalweight = 0;
@@ -60,36 +64,49 @@ namespace mu2e {
     , processCode_{config().processCode()}
     , isNull_{config().isNull()}
   {
+    if(!config().maxPions(maxPions_)) maxPions_ = -1;
   }
 
   void PionFilter::beginJob(){
       art::ServiceHandle<art::TFileService> tfs;
-
-}
+  }
 
   bool PionFilter::filter(art::Event& evt) {
       if(isNull_) return true;
       bool passed = false;
       std::vector<art::Handle<SimParticleCollection>> vah = evt.getMany<SimParticleCollection>();
+      const PhysicsParams& gc = *GlobalConstantsHandle<PhysicsParams>();
+      const std::vector<int> decayOffCodes = {PDGCode::pi_plus, PDGCode::pi_minus};
+      int npions(0);
       for (auto const& ah : vah) { //always one collection
         for(const auto& aParticle : *ah){
           art::Ptr<SimParticle> pp(ah, aParticle.first.asUint());
 
-          float _endglobaltime = pp->endGlobalTime();
+          // check if this is a pion of interest
           if( pp->stoppingCode() == processCode_ and std::abs(pp->pdgId()) == PDGCode::pi_plus){
-            const PhysicsParams& gc = *GlobalConstantsHandle<PhysicsParams>();
-            _totalweight += exp(-1*pp->endProperTime() / gc.getParticleLifetime(pp->pdgId()));
-            _ntot += 1;
-          }
-          if( pp->stoppingCode() == processCode_ and std::abs(pp->pdgId()) == PDGCode::pi_plus and _endglobaltime > tmin_ and _endglobaltime < tmax_ ){
-            passed = true;
-            const PhysicsParams& gc = *GlobalConstantsHandle<PhysicsParams>();
-            _selectedweight += exp(-1*pp->endProperTime() / gc.getParticleLifetime(pp->pdgId()));
-            _nsel += 1;
+            const float globalTime = pp->endGlobalTime();
+            const float weight = SimParticleGetTau::calculate(pp, decayOffCodes, gc);
+
+            // count found pions
+            _totalweight += weight;
+            ++_ntot;
+            ++npions;
+
+            // check additional filters
+            if(globalTime > tmin_ and globalTime < tmax_ ){
+              passed = true;
+              _selectedweight += weight;
+              ++_nsel;
+            }
           }
         }
       }
-    return passed;
+
+      // check global filters
+      passed &= maxPions_ < 0 || npions <= maxPions_;
+
+      // return the result
+      return passed;
   }
 
   void PionFilter::endJob(){
