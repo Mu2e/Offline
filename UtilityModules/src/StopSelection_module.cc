32ef709f9c83014ac067e595197a2bf3ccac7943
michaelmackenzie <michaelmackenzie@u.northwestern.edu>
2025-07-05 11:24:31 -0500

    Add selection logic to stopped particle sampling

--- UtilityModules/src/StopSelection_module.cc
+++ UtilityModules/src/StopSelection_module.cc
@@ -34,6 +34,15 @@ namespace mu2e {
 
   class StopSelection : public art::EDProducer {
   public:
+    // for applying selections to stops
+    struct StopConfig {
+      using Name=fhicl::Name;
+      using Comment=fhicl::Comment;
+      fhicl::OptionalAtom<double> tmin{Name("tmin"), Comment("Minimum stop time")};
+      fhicl::OptionalAtom<double> tmax{Name("tmax"), Comment("Maximum stop time")};
+    };
+
+    // top-level configuration
     struct Config {
       using Name=fhicl::Name;
       using Comment=fhicl::Comment;
@@ -41,8 +50,29 @@ namespace mu2e {
       fhicl::Atom<int> pdgId{Name("pdgId"),Comment("Particle PDG code to select")};
       fhicl::Atom<int> processCode{Name("processCode"), Comment("Particle end process code to select")};
       fhicl::OptionalSequence<int> decayOffPdgs{Name("decayOffPdgs"), Comment("List of PDG IDs that decay was turned off, for event weights")};
+      fhicl::OptionalTable<StopConfig> cuts { Name("cuts"), Comment("Stop selection table") };
       fhicl::Atom<int> diagLevel{Name("diagLevel"), Comment("Diagnostic print level"), 0};
     };
+
+    // Cut selection struct
+    struct StopCuts {
+      using var=std::pair<bool, double>;
+      var tmin_;
+      var tmax_;
+      StopCuts() : tmin_(var(false, 0.)), tmax_(var(false, 0.)) {}
+      StopCuts(StopConfig config) : StopCuts(){
+        tmin_.first = config.tmin(tmin_.second);
+        tmax_.first = config.tmax(tmax_.second);
+      }
+      bool apply_cuts(art::Ptr<SimParticle> sim) {
+        if(sim.isNull()) return false;
+        const double t = sim->endGlobalTime();
+        if(tmin_.first && t < tmin_.second) return false;
+        if(tmax_.first && t > tmax_.second) return false;
+        return true;
+      }
+    };
+
     explicit StopSelection(const art::EDProducer::Table<Config>& config);
     virtual void produce(art::Event& event) override;
     virtual void beginSubRun(art::SubRun& sr) override;
@@ -51,36 +81,55 @@ namespace mu2e {
     virtual void endJob() override;
 
   private:
+    std::vector<art::Ptr<SimParticle>> pruneStops(const std::vector<art::Ptr<SimParticle>>& sims);
+
     art::ProductToken<SimParticleCollection> const simsToken_;
     PDGCode pdgId_;
     ProcessCode::enum_type processCode_;
     std::vector<int> decayOffPdgs_;
     int diagLevel_;
+    StopCuts cuts_;
     SumOfWeights total_;
+    int nempty_;
 
     art::RandomNumberGenerator::base_engine_t& eng_;
     CLHEP::RandFlat randomFlat_;
   };
 
+  //--------------------------------------------------------------------------------------------------------------
   StopSelection::StopSelection(const art::EDProducer::Table<Config>& config) :
     EDProducer{config}
     , simsToken_{consumes<SimParticleCollection>(config().simCollTag())}
     , pdgId_{config().pdgId()}
     , processCode_{static_cast<ProcessCode::enum_type>(config().processCode())}
     , diagLevel_{config().diagLevel()}
+    , nempty_(0)
     , eng_{createEngine(art::ServiceHandle<SeedService>()->getSeed())}
     , randomFlat_{eng_}
   {
     produces<mu2e::SimParticleCollection>();
     if(!config().decayOffPdgs(decayOffPdgs_)) decayOffPdgs_ = {};
+    if(config().cuts()) cuts_ = StopCuts(*config().cuts());
     // if some decays were turned off, produce an event weight
     if(!decayOffPdgs_.empty()) produces<mu2e::EventWeight>();
     produces<SumOfWeights, art::InSubRun>();
   }
 
+  //--------------------------------------------------------------------------------------------------------------
   void StopSelection::beginJob(){
   }
 
+  //--------------------------------------------------------------------------------------------------------------
+  // apply selections to the stop collection
+  std::vector<art::Ptr<SimParticle>> StopSelection::pruneStops(const std::vector<art::Ptr<SimParticle>>& sims) {
+    std::vector<art::Ptr<SimParticle>> outlist;
+    for(auto sim : sims) {
+      if(cuts_.apply_cuts(sim)) outlist.push_back(sim);
+    }
+    return outlist;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------
   void StopSelection::produce(art::Event& event) {
     auto output{std::make_unique<SimParticleCollection>()};
     const bool make_weight = !decayOffPdgs_.empty();
@@ -89,14 +138,15 @@ namespace mu2e {
     // get the sim collection and the corresponding stops
     const auto simh = event.getValidHandle<SimParticleCollection>(simsToken_);
     const auto stops = simParticleList(simh, pdgId_, processCode_);
+    const auto selected = pruneStops(stops);
 
     if(diagLevel_ > 1) {
-      printf("StopSelection::%s:: Printing input sim collections:\n", __func__);
+      printf("StopSelection::%s:: From %zu stops selected %zu candidates\n", __func__, stops.size(), selected.size());
     }
     // select a random stop if available
-    if(!stops.empty()) {
-      const int index = randomFlat_.fireInt(stops.size());
-      art::Ptr<SimParticle> sim = stops[index];
+    if(!selected.empty()) {
+      const int index = randomFlat_.fireInt(selected.size());
+      art::Ptr<SimParticle> sim = selected[index];
 
       // get the selected stop's time weight if requested
       if(make_weight) {
@@ -111,9 +161,11 @@ namespace mu2e {
         output->insert(std::make_pair(sim->id(), *sim));
         sim = sim->parent();
       }
+      total_.add(weight);
+    } else {
+      ++nempty_;
     }
 
-    total_.add(weight);
 
     // produce the data products
     event.put(std::move(output));
@@ -123,17 +175,21 @@ namespace mu2e {
     }
   }
 
+  //--------------------------------------------------------------------------------------------------------------
   void StopSelection::beginSubRun(art::SubRun&) {
     total_.reset();
   }
 
+  //--------------------------------------------------------------------------------------------------------------
   void StopSelection::endSubRun(art::SubRun& sr) {
     sr.put(std::unique_ptr<SumOfWeights>(new SumOfWeights(total_.sum(), total_.count())), art::fullSubRun());
     if(diagLevel_ > 0) {
-      printf("[StopSelection::%s] Selected %lu stops with a sum of weights of %.5g\n", __func__, total_.count(), total_.sum());
+      printf("[StopSelection::%s] Selected %lu stops with a sum of weights of %.5g, %i empty events\n", __func__,
+             total_.count(), total_.sum(), nempty_);
     }
   }
 
+  //--------------------------------------------------------------------------------------------------------------
   void StopSelection::endJob(){
   }
 }
