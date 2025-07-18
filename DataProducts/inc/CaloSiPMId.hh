c37a5f72f52da13cf9d51ff3cd01361949f1d3cc
giro94 <giro9414@gmail.com>
2025-07-18 09:56:28 -0500

    adjusted wrong sipmid checks and added detector type

--- DataProducts/inc/CaloSiPMId.hh
+++ DataProducts/inc/CaloSiPMId.hh
@@ -8,6 +8,7 @@
 #include "Offline/DataProducts/inc/CaloConst.hh"
 #include "Offline/DataProducts/inc/CrystalId.hh"
 #include <iosfwd>
+#include <bitset>
 
 namespace mu2e {
 
@@ -26,9 +27,15 @@ namespace mu2e {
     { return CrystalId(CrystalId::value_type(_id/CaloConst::_nSiPMPerCrystal)); }
     value_type disk() const;
     bool isValid() const { return _id < CaloConst::_nChannel; }
-    bool isCrystal() const { return _id < CaloConst::_nCrystal; }
-    bool isPINDiode() const { return _id >= CaloConst::_nCrystal; }
-    value_type detType() const;
+    bool isCrystal() const { return _id < CaloConst::_nCrystalChannel; }
+    bool isPINDiode() const { return _id >= CaloConst::_nCrystalChannel; }
+    CaloConst::detType detType() const;
+
+    std::bitset<4> pinDiodeCode() const { return std::bitset<4>(_id-CaloConst::_nCrystalChannel); }
+    int pinDiodeDisk() const { return pinDiodeCode()[0]; }
+    int pinDiodePhi() const { return pinDiodeCode()[1]; }
+    int pinDiodeSphere() const { return pinDiodeCode()[2]; }
+    int pinDiodePin() const { return pinDiodeCode()[3]; }
 
   private:
 
