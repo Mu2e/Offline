571086da5e0544c3c530b08ebf4fb92dcd4b3cd7
echenard <echenard@caltech.edu>
2025-07-31 20:43:26 -0500

    Recycling and more generic scorer geom

--- MCDataProducts/inc/ProcessCode.hh
+++ MCDataProducts/inc/ProcessCode.hh
@@ -87,10 +87,10 @@ namespace mu2e {
       mu2eCeMinusLeadingLog,   mu2eCePlusEndpoint,  mu2eDIOLeadingLog, mu2eInternalRMC,  // 171
       mu2eExternalRMC,         mu2eFlateMinus,      mu2eFlatePlus, mu2eFlatPhoton, // 175
       mu2eCePlusLeadingLog, mu2ePionCaptureAtRest, mu2eExternalRPC, mu2eInternalRPC, // 179
-      mu2eCaloCalib, mu2ePienu, mu2eunused7, mu2eunused8, // 183
+      mu2eCaloCalib, mu2ePienu, CoupledTransportation, mu2eunused8, // 183
       uninitialized, NoProcess, GammaGeneralProc, mu2eGammaConversion, // 187
       Radioactivation, nCaptureHP, nFissionHP, mu2eAntiproton, // 191
-      mu2eFlatMuMinus, mu2eFlatMuPlus, CoupledTransportation, // 194
+      mu2eFlatMuMinus, mu2eFlatMuPlus, // 193
       lastEnum,
       // An alias for backward compatibility
       mu2eHallAir = mu2eKillerVolume
@@ -145,10 +145,10 @@ namespace mu2e {
     "mu2eCeMinusLeadingLog", "mu2eCePlusEndpoint",  "mu2eDIOLeadingLog", "mu2eInternalRMC", \
     "mu2eExternalRMC",  "mu2eFlateMinus",      "mu2eFlatePlus", "mu2eFlatPhoton", \
   "mu2eCePlusLeadingLog", "mu2ePionCaptureAtRest", "mu2eExternalRPC", "mu2eInternalRPC", \
-    "mu2eCaloCalib", "mu2ePienu", "mu2eunused7", "mu2eunused8", \
+    "mu2eCaloCalib", "mu2ePienu", "CoupledTransportation", "mu2eunused8", \
       "uninitialized", "NoProcess", "GammaGeneralProc", \
       "mu2eGammaConversion","Radioactivation", "nCaptureHP", "nFissionHP", "mu2eAntiproton", \
-      "mu2eFlatMuMinus", "mu2eFlatMuPlus", "CoupledTransportation"
+      "mu2eFlatMuMinus", "mu2eFlatMuPlus"
 #endif
 
   public:
