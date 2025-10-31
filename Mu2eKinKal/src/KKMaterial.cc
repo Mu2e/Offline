006d50eb58fe3f9448759849bfd33f4686af07c2
David Nathan Brown <dave_brown@lbl.gov>
2025-10-31 16:02:15 -0700

    Follow changes to DetMaterial configuration

--- Mu2eKinKal/src/KKMaterial.cc
+++ Mu2eKinKal/src/KKMaterial.cc
@@ -14,11 +14,17 @@ namespace mu2e {
     gasmatname_(matconfig.strawGasMaterialName()),
     wirematname_(matconfig.strawWireMaterialName()),
     ipamatname_(matconfig.IPAMaterialName()),
-    stmatname_(matconfig.STMaterialName()),
-    eloss_((DetMaterial::energylossmode)matconfig.eloss()),
-    matdbinfo_(std::make_unique<MatDBInfo>(filefinder_,eloss_)) {}
+    stmatname_(matconfig.STMaterialName()) {
+      MatEnv::DetMaterialConfig dmconf;
+      dmconf.elossmode_ = (DetMaterial::energylossmode)matconfig.elossMode();
+      dmconf.scatterfrac_solid_ = matconfig.solidScatter();
+      dmconf.scatterfrac_gas_ = matconfig.gasScatter();
+      dmconf.ebrehmsfrac_ = matconfig.eBrehms();
+      matdbinfo_ = std::make_unique<MatDBInfo>(filefinder_,dmconf);
+    }
 
   KKStrawMaterial const& KKMaterial::strawMaterial() const {
+    // deferred construction as this object depends on the tracker, which is created at beginJob
     if(smat_ == nullptr){
       Tracker const & tracker = *(GeomHandle<Tracker>());
       auto const& sprop = tracker.strawProperties();
