6f64ff2158e3ff96112423dd08c309698fa77d99
Jason <jasonzguo2016@gmail.com>
2025-11-16 18:42:16 -0800

    removed btrk from particleid

--- ParticleID/src/ParticleIDRead_module.cc
+++ ParticleID/src/ParticleIDRead_module.cc
@@ -21,7 +21,7 @@
 
 #include "Offline/RecoDataProducts/inc/PIDProduct.hh"
 
-#include "BTrk/TrkBase/TrkParticle.hh"
+#include "BTrkLegacy/inc/TrkParticle.hh"
 // mu2e tracking
 #include "Offline/RecoDataProducts/inc/TrkFitDirection.hh"
 
