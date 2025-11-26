8d1d78b2e32159e9595671507699705440393965
Iuri Oksuzian <oksuzian@gmail.com>
2025-11-26 09:39:24 -0600

    Convert grlTool from boost::program_options to ParseCLI
    - Replace boost::program_options with ParseCLI for command-line parsing
    - Register global options (--help/-h, --verbose/-v) in empty subcommand
    - Register all 12 command subcommands (define-word, print-words, etc.)
    - Use ParseCLI::setArgs() for parsing and getter methods for values
    - Remove Boost::program_options dependency from CMakeLists.txt
    - Fix typo: 'orgument' -> 'argument'

--- DbService/src/grlTool_main.cc
+++ DbService/src/grlTool_main.cc
@@ -5,117 +5,125 @@
 
 #include "Offline/DbService/inc/GrlTool.hh"
 #include "Offline/DbService/inc/GrlList.hh"
-#include <boost/program_options.hpp>
+#include "Offline/GeneralUtilities/inc/ParseCLI.hh"
 #include <iomanip>
 #include <iostream>
 
 using namespace mu2e;
-namespace po = boost::program_options;
 
 int main(int argc, char** argv) {
-  // std::vector<std::string> words;
+  // Set up ParseCLI with help string
+  std::string helpstr = R"(
+A program to read and write the goodrun database
+  WORDNAMEs are category/subcategory/word and define an integer which
+           may have different values for each run
+  BITNAMEs are bits within a WORD, like "noise"=0x1
 
-  // bool flags = false, conditions = false, longp = false, transitions = false;
-  // std::string run, time, type;
-  // int last = 0, days = 0;
-  int verbose = 0;
+**** bits ****
 
-  po::options_description desc("Allowed options");
-  desc.add_options()("help,h", "describe arguments")
-    ("verbose,v", po::value<int>(&verbose), "set verbose level (0-3)");
-    //  ("verbose,v", po::value<std::vector<int>(), "Verbosity level (can be repeated)");
-  // ("verbose,v",  "more verbose");
+define-word WORDNAME ["description"]
+    ex.: define-word shift/dqm/trk
+print-words
 
-  //(
-  //    "run,r", po::value(&run), "run range")(
-  //    "last,n", po::value(&last), "print last N runs")(
-  //    "type,y", po::value(&type), "comma-seperated list of run types")(
-  //    "time,t", po::value(&time), "Restrict times for print, see help")(
-  //    "days,d", po::value(&days), "restrict time to N days ago to now")(
-  //    "long,l", "print longer report")(
-  //    "conditions,c","also print conditions")(
-  //    "transitions,a","also print transitions");
+define-bit WORDNAME BITNAME ["description"]
+    define a new bit associated to the given word
+    ex.: define-bit shift/dqm/trk noise
+print-bits [WORDNAME]
 
-  po::variables_map vm;
-  po::parsed_options parsed = po::parse_command_line(argc, argv, desc);
-  po::store(parsed, vm);
-  po::notify(vm);
+set-word WORDNAME RUNRANGE BITNAME [BITNAME]
+    set a word with bits to indicate problems
+    later entries can override earlier ones
+    for run range text syntax see
+       https://mu2ewiki.fnal.gov/wiki/ConditionsData#Intervals_of_validity
+    # set this word to 0 (nothing bad), for one run, or run range
+    ex.: set-bits shift/dqm/trk 10500
+    ex.: set-bits shift/dqm/trk 10500-105010
+    # set this word, overriding previous word, only for these subruns
+    ex.: set-bits shift/dqm/trk 10500:50-10500:52 noise trips baddqm
 
-  std::vector<std::string> unregistered =
-      po::collect_unrecognized(parsed.options, po::include_positional);
+print-entries [WORDNAME] [RUNRANGE]
 
-  if (vm.count("help") && unregistered.empty()) {
-    std::cout <<
-        R"(
+gen-list COMMANDFILE [RUNTYPE]
+    apply list of bits selections in command file to runtype runs
+    and generate a goodrun list
 
-  grTool [GLOBAL OPTIONS] command [OPTIONS]
+**** lists ****
 
-  A program to read and write the goodrun database
-    WORDNAMEs are category/subcategory/word and define an integer which
-             may have different values for each run
-    BITNAMEs are bits within a WORD, like "noise"=0x1
+list-lists
+    list all defined GRL lists
+create-list LISTNAME [FILENAME]
+    define a list, if FILENAME is provided, then add those IoV's into the list
+extend-list LISTNAME IOV
+    add an IoV to a list. IoV in standard text format
+lock-list LISTNAME
+    prevent any new IoV's being added to the GRL
+print-list LISTNAME
+    print the IoV's in the list
+)";
 
-  -h, --help   print help
+  ParseCLI cli(helpstr, true, false);
 
-  **** bits ****
+  // Register global options (empty subcommand "")
+  cli.addSubcommand("", "Global options");
+  cli.addSwitch("", "verbose", "v", "verbose", true, "set verbose level (0-3)", "0");
 
-  define-word WORDNAME ["description"]
-      ex.: define-word shift/dqm/trk
-  print-words
+  // Register all command subcommands
+  // Note: Each subcommand needs at least one switch registered for ParseCLI validation
+  // We register a dummy switch with an unlikely name that won't conflict, just to satisfy validation
+  cli.addSubcommand("define-word", "define a word");
+  cli.addSwitch("define-word", "_dummy", "", "_dummy", false, "", "", false, false);
+  cli.addSubcommand("print-words", "print all words");
+  cli.addSwitch("print-words", "_dummy", "", "_dummy", false, "", "", false, false);
+  cli.addSubcommand("define-bit", "define a bit");
+  cli.addSwitch("define-bit", "_dummy", "", "_dummy", false, "", "", false, false);
+  cli.addSubcommand("print-bits", "print bits");
+  cli.addSwitch("print-bits", "_dummy", "", "_dummy", false, "", "", false, false);
+  cli.addSubcommand("set-word", "set a word with bits");
+  cli.addSwitch("set-word", "_dummy", "", "_dummy", false, "", "", false, false);
+  cli.addSubcommand("print-entries", "print entries");
+  cli.addSwitch("print-entries", "_dummy", "", "_dummy", false, "", "", false, false);
+  cli.addSubcommand("gen-list", "generate a goodrun list");
+  cli.addSwitch("gen-list", "_dummy", "", "_dummy", false, "", "", false, false);
+  cli.addSubcommand("list-lists", "list all defined GRL lists");
+  cli.addSwitch("list-lists", "_dummy", "", "_dummy", false, "", "", false, false);
+  cli.addSubcommand("create-list", "create a list");
+  cli.addSwitch("create-list", "_dummy", "", "_dummy", false, "", "", false, false);
+  cli.addSubcommand("extend-list", "extend a list");
+  cli.addSwitch("extend-list", "_dummy", "", "_dummy", false, "", "", false, false);
+  cli.addSubcommand("lock-list", "lock a list");
+  cli.addSwitch("lock-list", "_dummy", "", "_dummy", false, "", "", false, false);
+  cli.addSubcommand("print-list", "print a list");
+  cli.addSwitch("print-list", "_dummy", "", "_dummy", false, "", "", false, false);
 
-  define-bit WORDNAME BITNAME ["description"]
-      define a new bit associated to the given word
-      ex.: define-bit shift/dqm/trk noise
-  print-bits [WORDNAME]
-
-  set-word WORDNAME RUNRANGE BITNAME [BITNAME]
-      set a word with bits to indicate problems
-      later entries can override earlier ones
-      for run range text syntax see
-         https://mu2ewiki.fnal.gov/wiki/ConditionsData#Intervals_of_validity
-      # set this word to 0 (nothing bad), for one run, or run range
-      ex.: set-bits shift/dqm/trk 10500
-      ex.: set-bits shift/dqm/trk 10500-105010
-      # set this word, overriding previous word, only for these subruns
-      ex.: set-bits shift/dqm/trk 10500:50-10500:52 noise trips baddqm
-
-  print-entries [WORDNAME] [RUNRANGE]
-
-  gen-list COMMANDFILE [RUNTYPE]
-      apply list of bits selections in command file to runtype runs
-      and generate a goodrun list
-
-  **** lists ****
-
-  list-lists
-      list all defined GRL lists
-  create-list LISTNAME [FILENAME]
-      define a list, if FILENAME is provided, then add those IoV's into the list
-  extend-list LISTNAME IOV
-      add an IoV to a list. IoV in standard text format
-  lock-list LISTNAME
-      prevent any new IoV's being added to the GRL
-  print-list LISTNAME
-      print the IoV's in the list
-
-)" << std::endl;
-
-    return 1;
+  // Parse command line arguments
+  int rc = cli.setArgs(argc, argv);
+  if (rc != 0) {
+    if (rc == 999) {
+      // Help was printed by autohelp
+      return 1;
+    }
+    return rc;
   }
 
-  if (unregistered.empty()) {
+  // Get verbose level
+  int verbose = cli.getInt("", "verbose");
+
+  // Get subcommand (command name)
+  std::string command = cli.subcommand();
+  if (command.empty()) {
     std::cout << "Please specify a command\n";
     return 1;
   }
 
+  // Get positional arguments (command arguments)
+  const StringVec& positionals = cli.positionals();
+  std::string arg1, arg2, arg3;
+  if (positionals.size() > 0) arg1 = positionals[0];
+  if (positionals.size() > 1) arg2 = positionals[1];
+  if (positionals.size() > 2) arg3 = positionals[2];
+
   GrlTool tool;
   tool.setVerbose(verbose);
-
-  std::string command = unregistered[0];
-  std::string arg1,arg2,arg3;
-  if (unregistered.size()>1) arg1 = unregistered[1];
-  if (unregistered.size()>2) arg2 = unregistered[2];
-  if (unregistered.size()>3) arg3 = unregistered[3];
 
   if( command == "define-word" ) {
     tool.defineWord(arg1,arg2);
@@ -128,8 +136,8 @@ int main(int argc, char** argv) {
   } else if( command == "set-word" ) {
     // any number of bit name, concatenate with commas
     StringVec bitnames;
-    for (size_t i=3; i<unregistered.size(); i++) {
-      bitnames.push_back(unregistered[i]);
+    for (size_t i=2; i<positionals.size(); i++) {
+      bitnames.push_back(positionals[i]);
     }
     tool.setBits(arg1,arg2,bitnames);
   } else if( command == "print-entries" ) {
@@ -153,7 +161,7 @@ int main(int argc, char** argv) {
     tool.lockList(arg1); // name
   } else if( command == "print-list" ) {
     if (arg1.empty()) {
-      std::cout << "print- list requires a list name as first orgument"<< std::endl;
+      std::cout << "print-list requires a list name as first argument"<< std::endl;
     } else {
       GrlList ll = tool.list(arg1);
       //ll.print(std::cout);
