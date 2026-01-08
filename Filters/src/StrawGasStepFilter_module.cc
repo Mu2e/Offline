1311cb05cf974d2dbe6ebd6c1e90f6934cb89bf4
Richard Bonventre <rbonventre@lbl.gov>
2026-01-08 15:40:54 -0600

    Added StrawGasStepFilter

---
+++ Filters/src/StrawGasStepFilter_module.cc
@@ -0,0 +1,59 @@
+// requires a minimum number of steps in online straws
+
+#include "art/Framework/Core/EDFilter.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "Offline/MCDataProducts/inc/StrawGasStep.hh"
+#include "Offline/ProditionsService/inc/ProditionsHandle.hh"
+#include "Offline/TrackerConditions/inc/TrackerStatus.hh"
+
+#include <string>
+
+using namespace std;
+
+namespace mu2e{
+
+  class StrawGasStepFilter : public art::EDFilter {
+  public:
+    using Name=fhicl::Name;
+    using Comment=fhicl::Comment;
+
+    struct Config {
+      fhicl::Atom<art::InputTag> stepstag { Name("StrawGasStepTag"), Comment("StrawGasStep tag")};
+      fhicl::Atom<unsigned> minsteps { Name("MinSteps"), Comment("Minimum number of SGS")};
+    };
+    using Parameters = art::EDFilter::Table<Config>;
+    explicit StrawGasStepFilter(const Parameters& config);
+    virtual bool filter(art::Event& e);
+
+  private:
+    art::InputTag _stepsTag;
+    size_t        _minSteps;
+
+    ProditionsHandle<TrackerStatus> _trackerStatus_h;
+
+
+  };
+
+  StrawGasStepFilter::StrawGasStepFilter(const Parameters& config) :
+    art::EDFilter{config},
+    _stepsTag(config().stepstag()),
+    _minSteps(config().minsteps())
+    {
+    }
+
+  bool StrawGasStepFilter::filter(art::Event& event)  {
+    auto const& trackerStatus = _trackerStatus_h.getPtr(event.id());
+
+    size_t count = 0;
+    auto steps = event.getValidHandle<StrawGasStepCollection>(_stepsTag);
+    for (size_t i=0;i<steps->size();i++){
+      if (!trackerStatus->noSignal(steps->at(i).strawId()))
+        count += 1;
+    }
+    return count >= _minSteps;
+  }
+}
+
+DEFINE_ART_MODULE(mu2e::StrawGasStepFilter)
