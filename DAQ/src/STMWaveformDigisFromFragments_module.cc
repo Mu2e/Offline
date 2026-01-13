1386b4364eae7f2292d320674d5a5bca834f13c8
Andrew Edmonds <aedmonds@york.cuny.edu>
2026-01-13 13:20:00 -0600

    Fix whitespace

--- DAQ/src/STMWaveformDigisFromFragments_module.cc
+++ DAQ/src/STMWaveformDigisFromFragments_module.cc
@@ -25,7 +25,7 @@
 #include <numeric>
 #include <random>
 #include <vector>
- 
+
 // ROOT includes
 #include <TROOT.h>
 #include <TApplication.h>
@@ -68,7 +68,7 @@ class art::STMWaveformDigisFromFragments : public EDProducer
   private:
 
   art::InputTag _stmFragmentsTag;
-  
+
   // Fhicl params
   double _width_guess;
   double _rise_time_guess;
@@ -82,19 +82,19 @@ class art::STMWaveformDigisFromFragments : public EDProducer
   double avg_num = 0;
   // Pulse time average den
   double avg_den = 0;
-  
+
   // Params to store end of last event
   int lastEventLength = 0;
   uint64_t lastEWT = 0;
   std::vector<double> prevEvent;
-  
+
   int eventCounter = 0;
-  
+
   static double rising_edge(double*t, double*p);
   static double osc_rising_edge(double*t, double*p);
   double fit_rising_edge(std::vector<double> x, std::vector<double> y, double pulse_time);
   std::vector<double> linspace(double start_in, double end_in, int num_in);
-  
+
 }; // STMWaveformDigisFromFragments
 
 // ======================================================================
@@ -118,12 +118,12 @@ double STMWaveformDigisFromFragments::fit_rising_edge(std::vector<double> x, std
   double amp_guess = *std::max_element(y.begin(),y.end()); // amplitude
   double center_guess = pulse_time; // pulse centre
   double t0_guess = center_guess - _width_guess / 2; // rising edge t0
-  
+
   // Find the middle of the pulse (same as middle of data)
   int middle = (x.size() / 2);
-  // x values to middle 
+  // x values to middle
   std::vector<double> fit_x(x.begin(), x.begin()+middle);
-  // y values to middle 
+  // y values to middle
   std::vector<double> fit_y(y.begin(), y.begin()+middle);
   // Get start point
   double start = fit_x.front();
@@ -138,7 +138,7 @@ double STMWaveformDigisFromFragments::fit_rising_edge(std::vector<double> x, std
   fn->SetParameters(offset_guess, amp_guess, _rise_time_guess, t0_guess);
   fn->SetParNames("Offset","A","#tau","t_{0}");
   fn->SetLineWidth(3);
-  
+
   // Draw and fit graph
   TGraph *g = new TGraph(vsize, &fit_x[0], &fit_y[0]);
   g->Draw("A*");
@@ -161,7 +161,7 @@ std::vector<double> STMWaveformDigisFromFragments::linspace(double start_in, dou
   double num = num_in;
 
   if (num == 0) { return linspaced; }
-  if (num == 1) 
+  if (num == 1)
     {
       linspaced.push_back(start);
       return linspaced;
@@ -187,7 +187,7 @@ STMWaveformDigisFromFragments::STMWaveformDigisFromFragments(const art::EDProduc
   ,_samp_freq(config().samp_freq())
   ,_pulse_region(config().pulse_region())
   ,_pulses_per_event(config().pulses_per_event())
-  
+
 {
   // Set the size of the vector
   prevEvent.resize(_pulse_region);
@@ -201,7 +201,7 @@ void STMWaveformDigisFromFragments::produce(Event& event)
 {
   eventCounter++;
   std::unique_ptr<mu2e::STMWaveformDigiCollection> stm_waveform_digis(new mu2e::STMWaveformDigiCollection);
-  
+
   //auto STMFragments = event.getValidHandle<artdaq::Fragments>("daq:STM");
   art::Handle<artdaq::Fragments> STMFragmentsH;
   if(!event.getByLabel(_stmFragmentsTag, STMFragmentsH)) {
@@ -215,7 +215,7 @@ void STMWaveformDigisFromFragments::produce(Event& event)
     const auto& stm_frag = static_cast<mu2e::STMFragment>(frag);
 
     uint64_t EWT = *(stm_frag.EvNum());
-    int16_t event_len = *(stm_frag.EvLen());    
+    int16_t event_len = *(stm_frag.EvLen());
     detID = *(stm_frag.detID()) & 0xFF;
     //std::cout << "DetID: " << *(stm_frag.detID()) << "  |   " << detID << "\n";
     //std::cout << "In event: " << event_number << " | Length of event: " << event_len << "\n";
@@ -234,7 +234,7 @@ void STMWaveformDigisFromFragments::produce(Event& event)
     uint64_t ADC;
     memcpy(&ADC, adc, sizeof(ADC));
     //std::cout << "ADC time = " << ADC << " = " << (ADC/75e6) << "\n";
-    
+
     std::vector<int16_t> adcs;
     adcs.reserve(event_len);
     for (int16_t i_adc_sample = 0; i_adc_sample < event_len; ++i_adc_sample) {
@@ -247,79 +247,79 @@ void STMWaveformDigisFromFragments::produce(Event& event)
     // Initialise vectors for pulse locations and fit vals
     std::vector<double> pulse_locs;
     std::vector<double> fit_vals;
-    
+
     // Get and fit the pulses
     for(size_t i=0; i < adcs.size(); i++){
       // If the data is above the pulse threshold
       if (adcs[i] > _threshold) {
-	// If the pulse started in the previous event
-	if (avg_den > 0 && i == 0) {
-	  // Reset the average number...
-	  avg_num = 0;
-	  // Sum negative value from the previous event
-	  for (int k = -avg_den; k < 0; ++k) {
-	    avg_num += k;
-	  }
-	} // End if pulse in previous event
-  	  // Average pulse time numerator
-	avg_num += i;
-	// Average pulse time denonimator
-	avg_den += 1;
+        // If the pulse started in the previous event
+        if (avg_den > 0 && i == 0) {
+          // Reset the average number...
+          avg_num = 0;
+          // Sum negative value from the previous event
+          for (int k = -avg_den; k < 0; ++k) {
+            avg_num += k;
+          }
+        } // End if pulse in previous event
+          // Average pulse time numerator
+        avg_num += i;
+        // Average pulse time denonimator
+        avg_den += 1;
       } else {
-	// Else if not in a pulse region
-	if (avg_num != 0) {	    
-	  // Calculate the pulse time average
-	  int avg_time = static_cast<int>(avg_num / avg_den);
-	  // Store current pulse location
-	  double pulse_loc = avg_time;
-	  // Minimum pulse fit region
-	  int pulse_min = static_cast<int>(pulse_loc - (_pulse_region - 1) / 2);
-	  // Maximum pulse fit region
-	  int pulse_max = static_cast<int>(pulse_loc + (_pulse_region - 1) / 2);
-	  // Get diff
-	  int pulse_diff = (pulse_max+1) - pulse_min;
-	  // x values to fit
-	  double p_min = pulse_min / _samp_freq;
-	  double p_max = pulse_max / _samp_freq;
-	  std::vector<double> x(_pulse_region);	  
-	  // Find linspace in pulse region
-	  x = linspace(p_min, p_max, _pulse_region); 
-	  // y values to fit
-	  std::vector<double> y;
-	  for(int k=0; k < pulse_diff; k++){
-	    y.push_back(adcs[pulse_min + k]);
-	  }
-	  // If the minimum pulse fit region is in the previous event
-	  if (pulse_min < 0) {
-	    // get start value in last event to copy
-	    int del = pulse_min + _pulse_region; 
-	    // Store the y data that is in the previous event
-	    std::vector<double> prev_event(prevEvent.begin() + del, prevEvent.end());
-	    // Store the y data that is in this event
-	    std::vector<double> this_event(adcs.begin(), adcs.begin() + pulse_max + 1);
-	    // New y values to fit
-	    y.insert(y.begin(), prev_event.begin(), prev_event.end());
-	    y.insert(y.end(), this_event.begin(), this_event.end());
-	  }
-	  // Fit rising edge
-	  double rising_edge_fit = fit_rising_edge(x, y, pulse_loc / _samp_freq);	  
-	  if(rising_edge_fit > 0){
-	    // rising edge is not in last event
-	    // Save pulse time and fit vals in this event
-	    pulse_locs.push_back(pulse_loc);
-	    fit_vals.push_back(rising_edge_fit);
-	  }
-	  // Save the length of the event	    
-	  lastEventLength = adcs.size();
-	  // Save last pulse region of event
-	  int copyloc = adcs.size() - _pulse_region;
-	  prevEvent.insert(prevEvent.begin(), adcs.begin() + copyloc, adcs.end());	  
-	  // Increment number of pulses
-	  pulse_num++;
-	}
-	// Reset average variables
-	avg_num = 0;
-	avg_den = 0;
+        // Else if not in a pulse region
+        if (avg_num != 0) {
+          // Calculate the pulse time average
+          int avg_time = static_cast<int>(avg_num / avg_den);
+          // Store current pulse location
+          double pulse_loc = avg_time;
+          // Minimum pulse fit region
+          int pulse_min = static_cast<int>(pulse_loc - (_pulse_region - 1) / 2);
+          // Maximum pulse fit region
+          int pulse_max = static_cast<int>(pulse_loc + (_pulse_region - 1) / 2);
+          // Get diff
+          int pulse_diff = (pulse_max+1) - pulse_min;
+          // x values to fit
+          double p_min = pulse_min / _samp_freq;
+          double p_max = pulse_max / _samp_freq;
+          std::vector<double> x(_pulse_region);
+          // Find linspace in pulse region
+          x = linspace(p_min, p_max, _pulse_region);
+          // y values to fit
+          std::vector<double> y;
+          for(int k=0; k < pulse_diff; k++){
+            y.push_back(adcs[pulse_min + k]);
+          }
+          // If the minimum pulse fit region is in the previous event
+          if (pulse_min < 0) {
+            // get start value in last event to copy
+            int del = pulse_min + _pulse_region;
+            // Store the y data that is in the previous event
+            std::vector<double> prev_event(prevEvent.begin() + del, prevEvent.end());
+            // Store the y data that is in this event
+            std::vector<double> this_event(adcs.begin(), adcs.begin() + pulse_max + 1);
+            // New y values to fit
+            y.insert(y.begin(), prev_event.begin(), prev_event.end());
+            y.insert(y.end(), this_event.begin(), this_event.end());
+          }
+          // Fit rising edge
+          double rising_edge_fit = fit_rising_edge(x, y, pulse_loc / _samp_freq);
+          if(rising_edge_fit > 0){
+            // rising edge is not in last event
+            // Save pulse time and fit vals in this event
+            pulse_locs.push_back(pulse_loc);
+            fit_vals.push_back(rising_edge_fit);
+          }
+          // Save the length of the event
+          lastEventLength = adcs.size();
+          // Save last pulse region of event
+          int copyloc = adcs.size() - _pulse_region;
+          prevEvent.insert(prevEvent.begin(), adcs.begin() + copyloc, adcs.end());
+          // Increment number of pulses
+          pulse_num++;
+        }
+        // Reset average variables
+        avg_num = 0;
+        avg_den = 0;
       }
     }
     // Make sure variables are reset if events are not consecutive
@@ -331,17 +331,17 @@ void STMWaveformDigisFromFragments::produce(Event& event)
     double peak_fitTime1 = 0;
     double peak_fitTime2 = 0;
     double peak_sep = 0;
-    // Find separation of first two consecutive pulses    
+    // Find separation of first two consecutive pulses
     if(pulse_locs.size()>2){
       peak_fitTime1 = fit_vals[0]*1e9;
       peak_fitTime2 = fit_vals[1]*1e9;
       peak_sep = (peak_fitTime2 - peak_fitTime1);
     }
     std::cout << "pulse 0 = " << peak_fitTime1 << "  |  pulse 1 = " << peak_fitTime2 << "  |  pulse sep = " << peak_sep << "\n";
-  
+
     // Store EWT
     lastEWT = EWT;
-    
+
     // Create the STMWaveformDigi and put it in the event
     uint32_t trig_time_offset = 0;
     mu2e::STMWaveformDigi stm_waveform(detID, EWT, DTC, ADC, trig_time_offset, peak_fitTime1, peak_fitTime2, peak_sep, adcs);
