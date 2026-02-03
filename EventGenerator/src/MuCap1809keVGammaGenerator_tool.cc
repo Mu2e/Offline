6f296817894f0361399ef493b23e6753c4a35f90
michaelmackenzie <michaelmackenzie@u.northwestern.edu>
2026-02-03 14:11:13 -0600

    Allow angle restriction and force photon generation for CAPHRI studies

--- EventGenerator/src/MuCap1809keVGammaGenerator_tool.cc
+++ EventGenerator/src/MuCap1809keVGammaGenerator_tool.cc
@@ -18,13 +18,21 @@ namespace mu2e {
     struct PhysConfig {
       using Name=fhicl::Name;
       using Comment=fhicl::Comment;
+      fhicl::Atom<double> czMin  {Name("czmin")  , Comment("Restrict cos(theta_z) minimum"), -1.};
+      fhicl::Atom<double> czMax  {Name("czmax")  , Comment("Restrict cos(theta_z) maximum"),  1.};
+      fhicl::Atom<bool>   fireAll{Name("fireAll"), Comment("Add a photon to all events, otherwise use the branching fraction"),  false};
     };
     typedef art::ToolConfigTable<PhysConfig> Parameters;
 
     explicit MuCap1809keVGammaGenerator(Parameters const& conf) :
+      _czMin(conf().czMin()),
+      _czMax(conf().czMax()),
+      _fireAll(conf().fireAll()),
       _pdgId(PDGCode::gamma),
       _mass(GlobalConstantsHandle<ParticleDataList>()->particle(_pdgId).mass())
-    {}
+    {
+      if(_czMin < -1. || _czMax > 1. || _czMin > _czMax) throw cet::exception("BADCONFIG") << "Cos(theta_z) range unphysical: " << _czMin << " - " << _czMax;
+    }
 
     std::vector<ParticleGeneratorTool::Kinematic> generate() override;
     void generate(std::unique_ptr<GenParticleCollection>& out, const IO::StoppedParticleF& stop) override;
@@ -32,11 +40,14 @@ namespace mu2e {
     void finishInitialization(art::RandomNumberGenerator::base_engine_t& eng, const std::string& material) override {
       _energy = GlobalConstantsHandle<PhysicsParams>()->get1809keVGammaEnergy(material);
       _intensity = GlobalConstantsHandle<PhysicsParams>()->get1809keVGammaIntensity(material);
-      _randomUnitSphere = new RandomUnitSphere(eng);
+      _randomUnitSphere = new RandomUnitSphere(eng, _czMin, _czMax);
       _randFlat = new CLHEP::RandFlat(eng);
     }
 
   private:
+    double _czMin;
+    double _czMax;
+    bool   _fireAll;
     PDGCode::type _pdgId;
     double _mass;
     double _energy = 0.;
@@ -49,8 +60,8 @@ namespace mu2e {
   std::vector<ParticleGeneratorTool::Kinematic> MuCap1809keVGammaGenerator::generate() {
     std::vector<ParticleGeneratorTool::Kinematic>  res;
 
-    double rand = _randFlat->fire();
-    if (rand < _intensity) {
+    const bool fire = (_fireAll) ? true : _randFlat->fire() < _intensity;
+    if (fire) {
       const double momentum = _energy * sqrt(1 - std::pow(_mass/_energy,2));
       CLHEP::Hep3Vector p3 = _randomUnitSphere->fire(momentum);
       CLHEP::HepLorentzVector fourmom(p3, _energy);
