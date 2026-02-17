8d5e23d93d6bcdcfecf02df79122d6497e74f213
Paolo Girotti <giro94@libero.it>
2026-02-17 09:08:31 -0600

    file name change

--- CaloVisualizer/inc/THMu2eCaloDisk.hh
+++ CaloVisualizer/inc/THMu2eCaloDisk.hh
@@ -158,7 +158,7 @@ public:
   void SetBinCombineMode(Int_t bin, Int_t mode);
   void SetCombineMode(Int_t mode);
   void SetCombineMode(const char* formula);
-  bool LoadMapFile(std::map<int, std::map<int, mu2e::channelInfo>>& output, const char* filename = "Offline/CaloConditions/data/caloDMAP_20250827.dat");
+  bool LoadMapFile(std::map<int, std::map<int, mu2e::channelInfo>>& output, const char* filename = "Offline/CaloConditions/data/caloDMAP_nominal.dat");
   bool LoadMapDB(std::map<int, std::map<int, mu2e::channelInfo>>& output);
 
   Int_t FillOffline(int SiPMId, Double_t w);
