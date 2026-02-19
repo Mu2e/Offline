bfd39c70091fc0c470b426474c1375914f2183cc
David Nathan Brown <dave_brown@lbl.gov>
2026-02-19 12:37:30 -0800

    Cleanup

--- CosmicReco/inc/ComboHitInfoMC.hh
+++
@@ -1,8 +0,0 @@
-#ifndef CosmicReco_ComboHitInfoMC_HH
-namespace mu2e {
-  struct ComboHitInfoMC {
-    Int_t _rel; // relation to the 1st hit
-    XYZVectorF _mcpos; // position of this hit
-  };
-}
-#endif
