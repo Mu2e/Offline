0ed747822647459ea5e4fcd3948a58a1c47ae0b9
Weilong (Sam) Zhou <wzhou2@caltech.edu>
2026-03-03 22:47:36 -0800

    Simplify CSV handling for combined calib info tables

--- DbTables/inc/CalCombinedEnergyCalib.hh
+++ DbTables/inc/CalCombinedEnergyCalib.hh
@@ -75,15 +75,9 @@ class CalCombinedEnergyCalib : public DbTable {
           << index
           << " != " << _rows.size() << "\n";
     }
-    std::string statusMessage = columns[4];
-    for (std::size_t i = 5; i < columns.size(); ++i) {
-      statusMessage += ",";
-      statusMessage += columns[i];
-    }
-
     _rows.emplace_back(CaloSiPMId(index), std::stof(columns[1]),
                        std::stof(columns[2]), std::stoi(columns[3]),
-                       fromCsvText(statusMessage));
+                       columns[4]);
   }
 
   void rowToCsv(std::ostringstream& sstream, std::size_t irow) const override {
@@ -93,7 +87,7 @@ class CalCombinedEnergyCalib : public DbTable {
     sstream << r.ADC2MeV() << ",";
     sstream << r.ADC2MeV_err() << ",";
     sstream << r.status_code() << ",";
-    sstream << toCsvText(r.status_message());
+    sstream << "\"" << r.status_message() << "\"";
   }
 
   virtual void clear() override {
@@ -102,39 +96,6 @@ class CalCombinedEnergyCalib : public DbTable {
   }
 
  private:
-  static std::string toCsvText(const std::string& text) {
-    if (text.find_first_of(",\"") == std::string::npos) return text;
-    std::string out;
-    out.reserve(text.size() + 2);
-    out.push_back('"');
-    for (char c : text) {
-      if (c == '"') out += "\"\"";
-      else out.push_back(c);
-    }
-    out.push_back('"');
-    return out;
-  }
-
-  static std::string fromCsvText(const std::string& text) {
-    if (text.size() < 2 || text.front() != '"' || text.back() != '"') {
-      return text;
-    }
-    std::string out;
-    out.reserve(text.size() - 2);
-    for (std::size_t i = 1; i + 1 < text.size(); ++i) {
-      if (text[i] == '"' && i + 2 < text.size() && text[i + 1] == '"') {
-        out.push_back('"');
-        ++i;
-      } else if (text[i] == '\\' && i + 2 < text.size() && text[i + 1] == '"') {
-        out.push_back('"');
-        ++i;
-      } else {
-        out.push_back(text[i]);
-      }
-    }
-    return out;
-  }
-
   std::vector<Row> _rows;
 };
 
--- DbTables/inc/CalEnergyCalibInfo.hh
+++ DbTables/inc/CalEnergyCalibInfo.hh
@@ -51,20 +51,15 @@ class CalEnergyCalibInfo : public DbTable {
           << "CalEnergyCalibInfo::addRow expected at least 3 columns, got "
           << columns.size() << "\n";
     }
-    std::string comment = columns[2];
-    for (std::size_t i = 3; i < columns.size(); ++i) {
-      comment += ",";
-      comment += columns[i];
-    }
     _rows.emplace_back(std::stoi(columns[0]), std::stoi(columns[1]),
-                       fromCsvText(comment));
+                       columns[2]);
   }
 
   void rowToCsv(std::ostringstream& sstream, std::size_t irow) const override {
     Row const& r = _rows.at(irow);
     sstream << r.energyCalibCid() << ",";
     sstream << r.combinedEnergyCalibCid() << ",";
-    sstream << toCsvText(r.comment());
+    sstream << "\"" << r.comment() << "\"";
   }
 
   virtual void clear() override {
@@ -73,40 +68,6 @@ class CalEnergyCalibInfo : public DbTable {
   }
 
  private:
-  static std::string toCsvText(const std::string& text) {
-    if (text.find_first_of(",\"") == std::string::npos) return text;
-    std::string out;
-    out.reserve(text.size() + 2);
-    out.push_back('"');
-    for (char c : text) {
-      if (c == '"') out += "\"\"";
-      else out.push_back(c);
-    }
-    out.push_back('"');
-    return out;
-  }
-
-  static std::string fromCsvText(const std::string& text) {
-    if (text.size() < 2 || text.front() != '"' || text.back() != '"') {
-      return text;
-    }
-    std::string out;
-    out.reserve(text.size() - 2);
-    for (std::size_t i = 1; i + 1 < text.size(); ++i) {
-      if (text[i] == '"' && i + 2 < text.size() && text[i + 1] == '"') {
-        out.push_back('"');
-        ++i;
-      } else if (text[i] == '\\' && i + 2 < text.size() &&
-                 text[i + 1] == '"') {
-        out.push_back('"');
-        ++i;
-      } else {
-        out.push_back(text[i]);
-      }
-    }
-    return out;
-  }
-
   std::vector<Row> _rows;
 };
 
