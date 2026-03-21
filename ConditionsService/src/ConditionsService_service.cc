b6db42296a8cb7014fb4545a0115448945e4fa31
Ray Culbertson <rlc@fnal.gov>
2026-03-21 17:47:27 -0500

    final removal of ConditionsService

--- ConditionsService/src/ConditionsService_service.cc
+++
@@ -1,8 +0,0 @@
-//
-// ConditionsService pulgin
-//
-
-#include "art/Framework/Services/Registry/ServiceDefinitionMacros.h"
-#include "Offline/ConditionsService/inc/ConditionsService.hh"
-
-DEFINE_ART_SERVICE(mu2e::ConditionsService)
