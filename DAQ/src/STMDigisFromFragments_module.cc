7ef4a54db5ad14ecdc87ed2bb8b2280985b6360c
Bryan Gonzalez <bgonzale@mu2ebuild02.fnal.gov>
2026-04-06 11:47:35 -0500

    latest changes to unpacking - bryan

--- DAQ/src/STMDigisFromFragments_module.cc
+++ DAQ/src/STMDigisFromFragments_module.cc
@@ -45,7 +45,6 @@ namespace art
 }
 
 using art::STMDigisFromFragments;
-// ======================================================================
 
 class art::STMDigisFromFragments : public EDProducer
 {
@@ -81,6 +80,14 @@ private:
   size_t _totalRaw{0};
   size_t _totalZS{0};
   size_t _totalPH{0};
+
+  //Additional metrics
+  size_t _totalZeroRaw{0};
+  size_t _totalZeroZS{0};
+  size_t _totalZeroPH{0};
+  size_t _totalEmptyRaw{0};
+  size_t _totalEmptyZS{0};
+  size_t _totalEmptyPH{0};
 }; // STMDigisFromFragments
 
 // ======================================================================
@@ -92,13 +99,10 @@ STMDigisFromFragments::STMDigisFromFragments(const art::EDProducer::Table<Config
 
 {
   // Set the size of the vector
-  produces<mu2e::STMWaveformDigiCollection>("raw");//Waveform
+  produces<mu2e::STMWaveformDigiCollection>("raw");//Waveforms
   produces<mu2e::STMWaveformDigiCollection>("zs");
-  //produces<mu2e::STMMWDDigiCollection>("mwd");//MWD digi series
-
-  //Georges's suggestions
-  produces<mu2e::STMMWDDigiCollection>("ph");
   produces<mu2e::STMWaveformDigiCollection>("rawWithHeader");
+  produces<mu2e::STMMWDDigiCollection>("ph"); // digi series
 
   //turns file into binary
   _rawOut.open(config().rawFile(), std::ios::binary);
@@ -111,6 +115,7 @@ STMDigisFromFragments::STMDigisFromFragments(const art::EDProducer::Table<Config
   if(!_rawOut || !_zsOut || !_phOut || !_rawHeaderOut || !_eventOut){
     throw cet::exception("FILEOPEN")<< "Failed to open one or more output files\n";
   }
+  
 }
 
 STMDigisFromFragments::~STMDigisFromFragments(){
@@ -123,23 +128,21 @@ STMDigisFromFragments::~STMDigisFromFragments(){
   if( _rawHeaderOut.is_open() )
     _rawHeaderOut.close();
   if( _eventOut.is_open() )
-    _eventOut.close(); //Closing files    
-}
+    _eventOut.close();    
+}//Closing files
 
 // ----------------------------------------------------------------------
 
 void STMDigisFromFragments::produce(Event& event)
 {
   
-  ++_totalEvents; //Increment Event Counter
+  ++_totalEvents; //Increment Total Event Counter
   
   std::unique_ptr<mu2e::STMWaveformDigiCollection> raw_waveform_digis(new mu2e::STMWaveformDigiCollection);
   std::unique_ptr<mu2e::STMWaveformDigiCollection> zs_waveform_digis(new mu2e::STMWaveformDigiCollection);
   std::unique_ptr<mu2e::STMMWDDigiCollection> ph_digis(new mu2e::STMMWDDigiCollection);
-
-  // std::unique_ptr<mu2e::STMWaveformDigiCollection> ph_waveform_digis (new mu2e::STMWaveformDigiCollection);
   std::unique_ptr<mu2e::STMWaveformDigiCollection> raw_header_waveform_digis(new mu2e::STMWaveformDigiCollection);
-  // std::unique_ptr<mu2e::STMWaveformDigiCollection> mwd_waveform_digis(new mu2e::STMWaveformDigiCollection);//Original
+  //std::unique_ptr<mu2e::STMWaveformDigiCollection> ph_waveform_digis(new mu2e::STMWaveformDigiCollection);//Original
 
   art::Handle<artdaq::Fragments> STMFragmentsH;
   event.getByLabel(_stmFragmentsTag, STMFragmentsH);
@@ -151,19 +154,21 @@ void STMDigisFromFragments::produce(Event& event)
     out.write(reinterpret_cast<const char*>(data),//Writing to file stream in binary
               words * sizeof(int16_t));
   };
-  
-    //Variables to keep track of all zero element arrays& empty arrays
-    size_t zeroRaw_frags = 0;
-    size_t zeroZS_frags = 0;
-    size_t emptyRaw_frags = 0;
-    size_t emptyZS_frags = 0;
-    size_t zeroPH_frags = 0;//Rename MWD ->ph to avoid confusion, MWD is the algorithm which capture pulse heights
-    size_t emptyPH_digis = 0;
 
+  //Event Metrics
+  size_t localRaw_frags{0};
+  size_t localZS_frags{0};
+  size_t localPH_frags{0};
+  size_t zeroRaw_frags{0}; 
+  size_t zeroZS_frags{0};
+  size_t zeroPH_frags{0};
+  size_t emptyRaw_frags{0};
+  size_t emptyZS_frags{0};
+  size_t emptyPH_frags{0};
 
   //loop over frags
   for (const auto& frag : *STMFragments) {
-    ++_totalFragments; //Increment Frag counter
+    ++_totalFragments; //Increment Total Frag counter
     
     //Check if this is a container fragment
     if (frag.type() == artdaq::Fragment::ContainerFragmentType){
@@ -174,26 +179,29 @@ void STMDigisFromFragments::produce(Event& event)
       _totalInner += blocks;
 
       //loop over container
+      // i index corresponds to inner frag 
       for (size_t i = 0; i < cont_frag.block_count(); ++i){
 
         auto inner_frag = cont_frag.at(i);
         mu2e::STMFragment stm_frag(*inner_frag);
         mu2e::STMWaveformDigi stm_waveform;
 
-        auto ptr = stm_frag.payloadBegin();//Outer variable
-        auto words = stm_frag.payloadWords();
+        //auto ptr = stm_frag.payloadBegin();//Outer variable
+        //auto words = stm_frag.payloadWords();
 
         if (stm_frag.isRaw()) {
-          ++_totalRaw; //Increment raw counter
-
+          ++_totalRaw; //Increment job counter
+	  ++localRaw_frags;//Increment event counter
+	    
           auto payloadPtr = stm_frag.payloadBegin();
           auto payloadWords = stm_frag.payloadWords();
-          bool allZeros = true;
-
-          //Chekcs if payloadwords reads empty frag
+          bool allZeros = true;//Assume raw frag is zero filled
+	  
+          //Checks for empty frag
           if (payloadWords == 0) {
-            std::cout<< "Found an empty Raw frag at i = " << i <<"\n";
-            ++emptyRaw_frags;// Increment Empty Raw counter
+            std::cout<< "\nFound an empty frag, i = " << i <<" @Raw\n";
+	    ++_totalEmptyRaw;//Job counter
+	    ++emptyRaw_frags;//Event counter
             continue;//stops this loop check and goes to next frag
             }
 
@@ -207,50 +215,55 @@ void STMDigisFromFragments::produce(Event& event)
 
           //Chekcs if zero filled
           if (allZeros){
-            std::cout<< "Found a zero filled Raw frag at i = " << i << "\n";
-            ++zeroRaw_frags;//Increment Zero Raw Counter
+            std::cout << "\nFound a zero filled frag, i = " << i << " @Raw\n";
+	    ++_totalZeroRaw;//Increment counters
+	    ++zeroRaw_frags;//Increment Zero Raw Counter
             continue;
           }
 
           //Print first 20 values 
-          std::cout<< "First 20 adcs: ";
+          std::cout << "\nFirst 20 adcs: ";
           for (size_t kk = 0; kk < std::min<size_t>(payloadWords,20); ++kk){
-            std::cout<< payloadPtr[kk] <<" ";
+            std::cout << payloadPtr[kk] << " ,";
           }
-          std::cout<< "Raw i = " << i <<"\n";
+          std::cout << "\nRaw header : ZS Length = " << stm_frag.zsLength()
+		    << " , ZS Regions  = " << stm_frag.zsRegions()
+		    << " , i = " << i << " @Raw\n";
 
           //Ideally only good frags get up to here
           //------full data (header + payload)
           { 
             //Waveform with data creation
-            auto ptr = stm_frag.dataBegin();//Inner variable
-            auto words = stm_frag.dataWords();
-            stm_waveform.set_data(words, ptr);
+            auto dataPtr = stm_frag.dataBegin();//Inner variable
+            auto dataWords = stm_frag.dataWords();
+            stm_waveform.set_data(dataWords, dataPtr);
             raw_header_waveform_digis->emplace_back(stm_waveform);
-            writePayload(_rawHeaderOut,ptr,words); 
+            writePayload( _rawHeaderOut , dataPtr, dataWords); 
           }
 
           //----- payload-only
           {
-	    auto ptr = stm_frag.payloadBegin();
-            auto words = stm_frag.payloadWords();
-            stm_waveform.set_data(words, ptr);
+	    //auto payloadPtr = stm_frag.payloadBegin();
+            //auto payloadWords = stm_frag.payloadWords();
+            stm_waveform.set_data(payloadWords, payloadPtr);
             raw_waveform_digis->emplace_back(stm_waveform);
-            writePayload(_rawOut, ptr, words);
+            writePayload(_rawOut, payloadPtr, payloadWords);
           }
 
-                  
         }//End of isRaw
+	
         else if (stm_frag.isZS()){
           ++_totalZS; //Incremenet ZS counter
-
+	  ++localZS_frags;
+	  
 	  auto payloadPtr = stm_frag.payloadBegin();
 	  auto payloadWords = stm_frag.payloadWords();
 	  bool allZeros = true; //assumes all adcs are zero
 
 	  //Check if payload is empty
 	  if (payloadWords == 0) {
-	    std::cout<< "Found an empty ZS frag at i = " << i<< "\n";
+	    std::cout << "\nFound an empty frag, i = " << i<< " @ZS\n";
+	    ++_totalEmptyZS;
 	    ++emptyZS_frags; //Increment empty ZS counter
 	    continue;
 	  }
@@ -263,73 +276,73 @@ void STMDigisFromFragments::produce(Event& event)
 	  }
 	  //Check if zero filled
 	  if (allZeros) {
-	    std::cout << "Found a zero filled ZS frag at i = "<< i<< "\n";
+	    std::cout << "\nFound a zero filled frag, i = "<< i<< " @ZS\n";
+	    ++_totalZeroZS;
 	    ++zeroZS_frags;
 	    continue;
 	  }
 	  //Print first 20 payload adcs
-	  std::cout<< "First 20 adcs: ";
+	  std::cout << "\nFirst 20 adcs: ";
 	  for (size_t kk = 0 ; kk < std::min<size_t>(payloadWords,20) ; ++kk){
-	    std::cout << payloadPtr[kk] << " ";
+	    std::cout << payloadPtr[kk] << " , ";
 	  }
-	  std::cout << "ZS i = " << i << "\n";
+	  std::cout << "i = " << i << " @ZS\n";
 
 	  //Defintions for payload references
-	  auto payloadEnd = payloadPtr + payloadWords;
+	  auto dataPtr = stm_frag.dataBegin();
+	  auto dataWords = stm_frag.dataWords();
+	  auto dataEnd = dataPtr + dataWords;
 	  size_t seg = 0;
 	  size_t totalLen = 0;
 	  uint16_t lastZSindex = 0; //keeps track of last recorded index from header -> with respect to what?
 	  uint16_t lastLen = 0; //keeps track of last recoded length from header
+
+	  writePayload(_zsOut, payloadPtr, payloadWords);
 	  
-	  while (payloadPtr + 2 <= payloadEnd){
-	    uint16_t current_zs_location = static_cast<uint16_t>(payloadPtr[0]);
-	    uint16_t current_zs_size = static_cast<uint16_t>(payloadPtr[1]);
-	    auto adc = payloadPtr + 2;
-	    if (adc + current_zs_size > payloadEnd)
+	  while (dataPtr + 2 <= dataEnd){
+	    uint16_t current_zs_location = static_cast<uint16_t>(dataPtr[0]);
+	    uint16_t current_zs_size = static_cast<uint16_t>(dataPtr[1]);
+	    auto adc = dataPtr + 2;
+	    if (adc + current_zs_size > dataEnd)
 	      break;
 
 	    uint32_t trigTimeOffset = current_zs_location;
 	    std::vector<int16_t> segADCS(adc, adc +  current_zs_size); //1D array, contains adcs to this_zs_Size - 1
 	    mu2e::STMWaveformDigi stm_waveform(trigTimeOffset, segADCS); //New constructore use
 	    zs_waveform_digis->emplace_back(stm_waveform); //emplace
-	    writePayload(_zsOut, payloadPtr, payloadWords);
 	    
 	    //A print check per segment
-	    std::cout << "i = " << i << ", seg = "<< seg << ", zs_location = " << current_zs_location << ", current_ZS_size = " << current_zs_size
-		      <<", trigTimeOffset = " << trigTimeOffset << "\n";
+	    std::cout << "Region = " << seg << " , zs_location = " << current_zs_location << " , current_ZS_size = " << current_zs_size
+		      << " , trigTimeOffset = " << trigTimeOffset << "\n" ;
 	    
 	    lastZSindex = current_zs_location;
 	    lastLen = current_zs_size;
 	    totalLen += lastLen;
 	    ++seg;
 
-	    payloadPtr = adc + current_zs_size;
+	    dataPtr = adc + current_zs_size;
 	  }
      	  //Summary
-	  std::cout << "ZS i = " << i
-		    << "segments = " << seg
-		    << "lastZSindex = " << lastZSindex
-		    << " lastZSLen = " << lastLen << "with a total calculated length of " << totalLen << "\n";
-	  
-	  
-	  
-	  //----- payload-only
-	  //{ 
-          //stm_waveform.set_data(words,ptr);
-	  //zs_waveform_digis->emplace_back(stm_waveform);
-          //writePayload(_zsOut, ptr, words);
-          //}
+	  std::cout << "ZS Regions = " << seg
+		    << " , lastZSindex = " << lastZSindex
+		    << " , lastZSLen = " << lastLen
+		    << " , ZS total length = " << totalLen
+		    << " , i =  " << i <<  " @ZS\n";
 	  
         }//End of isZS
+	
         else if (stm_frag.isMWD()){
 	  ++_totalPH;
+	  ++localPH_frags;
 	  //Check if zero filled
 	  auto payloadPtr = stm_frag.payloadBegin();
 	  auto payloadWords = stm_frag.payloadWords();
 	  bool allZeros = true;
 
 	  if (payloadWords ==0){
-	    ++emptyPH_digis;
+	    ++_totalEmptyPH;
+	    ++emptyPH_frags;
+	    std::cout << "\nFound an empty frag, i = " << i<< " @PH\n";
 	    continue;
 	  }
 
@@ -342,21 +355,23 @@ void STMDigisFromFragments::produce(Event& event)
 	  }
 	  //count if zero filled
 	  if (allZeros){
+	    ++_totalZeroPH;
 	    ++zeroPH_frags;
+	    std::cout<< "\nFound a zero filled frag, i = " << i<< " @PH\n";
 	    continue;
 	  }
+
+	  std::cout << "\nFound a good frag, i = " << i <<" @PH\n";
+	  size_t digiWords = stm_frag.payloadWords();	  
+          auto const* digiPtr = stm_frag.payloadBegin();
+
+	  writePayload(_phOut,digiPtr, digiWords);
 	  
-	  int n_PH_digis = stm_frag.payloadWords();
-	  for (int i_PH = 0; i_PH < n_PH_digis; ++i_PH){
-	    auto const* ptr = stm_frag.payloadBegin();
-	    int16_t i_ptr = ptr[i_PH];
-	    mu2e::STMMWDDigi PH_digi(0, i_ptr);
+	  for (size_t i_PH = 0; i_PH < digiWords ; ++i_PH){
+	    int16_t PH = digiPtr[i_PH];
+	    mu2e::STMMWDDigi PH_digi(0, PH);
 	    ph_digis->emplace_back(PH_digi);
 	  }
-      
-    	  //stm_waveform.set_data(words, ptr);
-      //ph_waveform_digis->emplace_back(stm_waveform);
-	  //writePayload(_phOut, ptr, words);
 
       }//End of isMWD and is checks
 
@@ -398,28 +413,31 @@ void STMDigisFromFragments::produce(Event& event)
   } //End of frags loop---George suggestions
 
   //Event Summary -> tells us what happens per event
-  std::cout<<"---------------------------------------------------------"<<"\n";
-  std::cout << "raw_waveform_digis total size with data = "<<raw_waveform_digis->size()<<"\n";
-  std::cout << "zs_waveform_digis total size with data = "<<zs_waveform_digis->size()<<"\n";
-  std::cout << "mwd_digis total size with data = " << ph_digis->size()<<"\n";
+  std::cout << "\n========== STM EVENT SUMMARY ==========\n";
+  std::cout << "Extracted Raw waveforms     : "<< raw_waveform_digis->size() <<"\n";
+  std::cout << "Extracted ZS waveforms      : "<< zs_waveform_digis->size() <<"\n";
+  std::cout << "Extracted PH digis          : " << ph_digis->size() <<"\n";
 
-  std::cout<< "---------------------------------------------------------"<<"\n";
-  std::cout<< "Number of Zero arrays for Raw = " << zeroRaw_frags<< "\n";
-  std::cout<< "Number of Zero arrays for ZS = " << zeroZS_frags<< "\n";
-  std::cout<< "Number of Zero digis for MWD = " << zeroPH_frags<< "\n";
+  std::cout << "\n--- Frags Read ---\n";
+  std::cout << "Raw frags   : " << localRaw_frags << "\n";
+  std::cout << "ZS frags    : " << localZS_frags << "\n";
+  std::cout << "PH frags    : " << localPH_frags << "\n";
+  
+  std::cout << "\n--- Filter results ---\n";
+  std::cout << "Zero Raw frags   : " << zeroRaw_frags << "\n";
+  std::cout << "Zero ZS frags    : " << zeroZS_frags << "\n";
+  std::cout << "Zero PH frags    : " << zeroPH_frags << "\n";
 
-  std::cout<< "---------------------------------------------------------"<<"\n";
-  std::cout<< "Number of empty arrays for Raw = " <<emptyRaw_frags<<"\n";
-  std::cout<< "Number of empty arrays for ZS = " <<emptyZS_frags<< "\n";
-  std::cout<< "Number of empty digis for MWD = " << emptyPH_digis << "\n";
-  std::cout<< "---------------------------------------------------------\n"<<"\n";
+  std::cout << "Empty Raw frags  : " << emptyRaw_frags <<"\n";
+  std::cout << "Empty ZS frags   : " << emptyZS_frags << "\n";
+  std::cout << "Empty PH frags   : " << emptyPH_frags << "\n";
+  
+  std::cout << "=================================\n";
 
-
+  //Final move
   event.put(std::move(raw_waveform_digis), "raw");
   event.put(std::move(zs_waveform_digis), "zs");
   event.put(std::move(ph_digis), "ph");
-  //New parameters to consider
-  //event.put(std::move(ph_waveform_digis), "ph");
   event.put(std::move(raw_header_waveform_digis), "rawWithHeader");
     
 } // produce()
@@ -430,17 +448,26 @@ void STMDigisFromFragments::produce(Event& event)
 void STMDigisFromFragments::endJob() {
 
   //Tells us what happened at the very end
-  std::cout << "\n========== STM SUMMARY ==========\n";
+  std::cout << "\n========== STM JOB SUMMARY ==========\n";
 
   std::cout << "Total events       : " << _totalEvents << "\n";
   std::cout << "Total fragments    : " << _totalFragments << "\n";
   std::cout << "Container frags    : " << _totalContainers << "\n";
   std::cout << "Inner fragments    : " << _totalInner << "\n";
 
-  std::cout << "\n--- Data types ---\n";
+  std::cout << "\n--- Data types read ---\n";
   std::cout << "RAW               : " << _totalRaw << "\n";
   std::cout << "ZS                : " << _totalZS << "\n";
-  std::cout << "PH               : " << _totalPH << "\n";
+  std::cout << "PH                : " << _totalPH << "\n";
+
+  std::cout << "\n-- -Data types filtered ---\n";
+  std::cout << "Zero RAW frags    : " << _totalZeroRaw << "\n";
+  std::cout << "Zero ZS frags     : " << _totalZeroZS << "\n";
+  std::cout << "Zero PH frags     : " << _totalZeroPH << "\n";
+  std::cout << "Empty Raw frags   : " << _totalEmptyRaw << "\n";
+  std::cout << "Empty ZS frags    : " << _totalEmptyZS << "\n";
+  std::cout << "Empty PH frags    : " << _totalEmptyPH << "\n";
+
 
   std::cout << "=================================\n";
 }
