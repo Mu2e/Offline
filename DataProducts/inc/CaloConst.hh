96d3cbbc425a3c504761073f007fc62c4de689d2
giro94 <giro9414@gmail.com>
2026-04-22 10:45:17 -0500

    Removed exception for invalid offlineID; Added functions to print sipm type name

--- DataProducts/inc/CaloConst.hh
+++ DataProducts/inc/CaloConst.hh
@@ -6,41 +6,45 @@
 //
 #include <array>
 #include <cstdint>
+#include <string>
 
 namespace mu2e {
 
-  class CaloConst{
+class CaloConst {
 
-    // define the bit field shifts and masks
-  public:
-    constexpr static uint16_t _nCrystalPerDisk = 674;
-    constexpr static uint16_t _nSiPMPerCrystal = 2;
-    constexpr static uint16_t _nDisk           = 2;
-    // PIN diodes are each one SiPM channel
-    constexpr static uint16_t _nPINDiodPerDisk = 8;
-    constexpr static uint16_t _nPINDiodeLaserBox = 4;
-    constexpr static uint16_t _nChannelSpares = 4;
-    constexpr static uint16_t _nCrystal        = _nCrystalPerDisk*_nDisk;
-    constexpr static uint16_t _nCrystalChannel = _nCrystal*_nSiPMPerCrystal;
-    constexpr static uint16_t _nCaphriCrystal  = 4;
-    // crystal numbers, not SiPM channels. Only in disk 0.
-    constexpr static std::array<uint16_t,_nCaphriCrystal> _caphriId = {582,609,610,637};
+  // define the bit field shifts and masks
+public:
+  constexpr static uint16_t _nCrystalPerDisk = 674;
+  constexpr static uint16_t _nSiPMPerCrystal = 2;
+  constexpr static uint16_t _nDisk = 2;
+  // PIN diodes are each one SiPM channel
+  constexpr static uint16_t _nPINDiodPerDisk = 8;
+  constexpr static uint16_t _nPINDiodeLaserBox = 4;
+  constexpr static uint16_t _nChannelSpares = 4;
+  constexpr static uint16_t _nCrystal = _nCrystalPerDisk * _nDisk;
+  constexpr static uint16_t _nCrystalChannel = _nCrystal * _nSiPMPerCrystal;
+  constexpr static uint16_t _nCaphriCrystal = 4;
+  // crystal numbers, not SiPM channels. Only in disk 0.
+  constexpr static std::array<uint16_t, _nCaphriCrystal> _caphriId = {582, 609, 610, 637};
 
-    constexpr static uint16_t _nChannel        = _nCrystalChannel + _nPINDiodPerDisk*_nDisk + _nPINDiodeLaserBox;
-    constexpr static uint16_t _nChannelDB      = _nChannel + _nChannelSpares;
+  constexpr static uint16_t _nChannel =
+      _nCrystalChannel + _nPINDiodPerDisk * _nDisk + _nPINDiodeLaserBox;
+  constexpr static uint16_t _nChannelDB = _nChannel + _nChannelSpares;
 
-    constexpr static uint16_t _nDIRAC       = 161;
-    constexpr static uint16_t _nChPerDIRAC  = 20;
-    constexpr static uint16_t _nRawChannel  = _nChPerDIRAC*_nDIRAC;
+  constexpr static uint16_t _nDIRAC = 161;
+  constexpr static uint16_t _nChPerDIRAC = 20;
+  constexpr static uint16_t _nRawChannel = _nChPerDIRAC * _nDIRAC;
 
-    constexpr static uint16_t _invalid      = 9999;
+  constexpr static uint16_t _invalid = 9999;
 
-    using CaloSiPMId_type = std::uint16_t;
+  using CaloSiPMId_type = std::uint16_t;
 
-    enum SiPMn {SiPM0=0,SiPM1=1};
-    enum detType {CsI=0,CAPHRI=1,PINDiode=2,Invalid=3};
+  enum SiPMn { SiPM0 = 0, SiPM1 = 1 };
+  enum detType { CsI = 0, CAPHRI = 1, PINDiode = 2, Invalid = 3 };
 
-  };
+  static const std::string detTypeName(detType type);
+};
 
-}
+std::ostream& operator<<(std::ostream& ost, const CaloConst::detType& type);
+} // namespace mu2e
 #endif /* DataProducts_CaloConst_hh */
---
+++ DataProducts/src/CaloConst.cc
@@ -0,0 +1,26 @@
+#include "Offline/DataProducts/inc/CaloConst.hh"
+#include <iostream>
+
+namespace mu2e {
+
+const std::string CaloConst::detTypeName(detType type) {
+  switch (type) {
+  case CaloConst::detType::CsI:
+    return "CsI";
+  case CaloConst::detType::PINDiode:
+    return "PINDiode";
+  case CaloConst::detType::CAPHRI:
+    return "CAPHRI";
+  case CaloConst::detType::Invalid:
+    return "Invalid";
+  default:
+    return "UNKNOWN";
+  }
+}
+
+std::ostream& operator<<(std::ostream& ost, const CaloConst::detType& type) {
+  ost << CaloConst::detTypeName(type);
+  return ost;
+}
+
+} // end namespace mu2e
