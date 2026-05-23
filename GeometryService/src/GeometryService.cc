caf81c181d3655edd680ab3e29f23e54f2e28d92
David Nathan Brown <dave_brown@lbl.gov>
2026-05-23 13:03:05 -0700

    Change accessor

--- GeometryService/src/GeometryService.cc
+++ GeometryService/src/GeometryService.cc
@@ -356,10 +356,13 @@ namespace mu2e {
       KinKalGeomMaker kkgm;
       addDetector( std::move(kkgm.makeKKG()) );
       // directly build KKMaterial; it's constructor does everything
-      DetMap::iterator itrk = _detectors.find("Tracker");
-      Tracker const& tracker = static_cast<Tracker const&>(*itrk->second);
-      addDetector( std::make_unique<KKMaterial>(_kkMat,tracker));
-      return;
+      auto trkptr =  getElement<Tracker>();
+      if(trkptr){
+        Tracker const& tracker = *trkptr;
+        addDetector( std::make_unique<KKMaterial>(_kkMat,tracker));
+        return;
+      } else
+        throw cet::exception("GEOM") << "No Tracker found! " << "\n";
 
     } // preBeginRun()
 
