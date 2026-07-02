787018383baa92611e8fcfecc0024396acc9e19e
David Nathan Brown <dave_brown@lbl.gov>
2026-07-02 16:55:15 -0700

    Add diagnostics. Update config to save everything needed for reflection constraint

--- TrkReco/src/SelectReflections_module.cc
+++ TrkReco/src/SelectReflections_module.cc
@@ -34,31 +34,36 @@ namespace mu2e {
 
   class SelectReflections : public art::EDFilter {
     public:
-      enum bestpair {mom=0, deltat,deltap}; // selection options for defining the best pair
+      enum bestpair {mom=0, deltat,deltap, nactive}; // selection options for defining the best pair
       using Name=fhicl::Name;
       using Comment=fhicl::Comment;
       struct Config {
         fhicl::Atom<int> debug{ Name("debugLevel"), Comment("Debug Level"), 0};
         fhicl::Atom<art::InputTag> upstreamTag {Name("UpstreamKalSeedCollection"), Comment("Upstream KalSeed collection") };
         fhicl::Atom<art::InputTag> downstreamTag {Name("DownstreamKalSeedCollection"), Comment("Downstream KalSeed collection") };
-        fhicl::Atom<double> maxdt{ Name("MaxDeltaT"), Comment("Maximum time difference at tracker entrance")};
-        fhicl::Atom<double> maxdp{ Name("MaxDeltaP"), Comment("Maximum scalar momentum difference at tracker entrance")};
+        fhicl::Atom<std::string> surface{ Name("Surface"), Comment("Surface to compare fits at")};
+        fhicl::Atom<double> maxdt{ Name("MaxDeltaT"), Comment("Maximum time difference at comparison surface")};
+        fhicl::Atom<double> maxdp{ Name("MaxDeltaP"), Comment("Maximum scalar momentum difference at comparison surface")};
         fhicl::DelegatedParameter selector{ Name("Selector"), Comment("Selector parameters")};
         fhicl::Atom<int> selectbest{ Name("SelectBestPair"), Comment("Method to select the best pair if multiple pairs are found ")};
       };
       using Parameters = art::EDFilter::Table<Config>;
       explicit SelectReflections(const Parameters& config);
       bool filter(art::Event& evt) override;
+      void endJob() override;
     private:
       int debug_;
+      SurfaceId sid_;
       double maxdt_, maxdp_;
       art::ProductToken<KalSeedCollection> upstreamtoken_, downstreamtoken_;
       std::unique_ptr<KalSeedSelector> selector_;
       bestpair selbest_;
+      unsigned nevts_=0, nref_=0, nmultref_=0;
   };
 
   SelectReflections::SelectReflections(const Parameters& config) : art::EDFilter{config},
     debug_(config().debug()),
+    sid_(config().surface()),
     maxdt_(config().maxdt()),
     maxdp_(config().maxdp()),
     upstreamtoken_ { consumes<KalSeedCollection> (config().upstreamTag()) },
@@ -69,7 +74,7 @@ namespace mu2e {
     }
 
   bool SelectReflections::filter(art::Event& event) {
-    static const SurfaceId trkfront (SurfaceIdDetail::TT_Front);
+    ++nevts_;
     // create output
     std::unique_ptr<KalSeedPtrCollection> mkseeds(new KalSeedPtrCollection);
     auto const& upksch = event.getValidHandle<KalSeedCollection>(upstreamtoken_);
@@ -79,7 +84,7 @@ namespace mu2e {
     if(debug_ > 1) std::cout << "Upstream " << upksc.size() << " , Downstream " << downksc.size() << " KalSeeds" << std::endl;
     bool keep(false);
     if(upksc.size() > 0 && downksc.size() > 0){
-      std::vector<std::tuple<size_t, size_t,double, double, double>> matches; // matched up and downstream track, with (downstream) mom, dt and dmom
+      std::vector<std::tuple<size_t, size_t,double, double, double, unsigned>> matches; // matched up and downstream track, with (downstream) mom, dt and dmom
       for(size_t iup = 0; iup <upksc.size(); ++iup){
         auto const& upks = upksc[iup];
         if(selector_->select(upks)){
@@ -88,7 +93,7 @@ namespace mu2e {
           auto uptrkiinter = upks.intersections().end();
           for(auto upiinter = upks.intersections().begin(); upiinter != upks.intersections().end(); ++upiinter){
             auto const& upinter = *upiinter;
-            if(upinter.surfaceId() == trkfront && upinter.momentum3().Z() > 0.0){ // correct surface and direction
+            if(upinter.surfaceId() == sid_ && upinter.momentum3().Z() > 0.0){ // correct surface and direction
               if(debug_ > 1) std::cout << "Found upstream intersection mom " << upinter.momentum3() << " time " << upinter.time() << std::endl;
               uptrkiinter = upiinter;
               break;
@@ -105,7 +110,7 @@ namespace mu2e {
               auto downtrkiinter = downks.intersections().end();
               for(auto downiinter = downks.intersections().begin(); downiinter != downks.intersections().end(); ++downiinter){
                 auto const& downinter = *downiinter;
-                if(downinter.surfaceId() == trkfront && downinter.momentum3().Z() > 0.0){ // correct surface and direction
+                if(downinter.surfaceId() == sid_ && downinter.momentum3().Z() > 0.0){ // correct surface and direction
                   if(debug_ > 1) std::cout << "Found downstream intersection mom " << downinter.momentum3() << " time " << downinter.time() << std::endl;
                   downtrkiinter = downiinter;
                   break;
@@ -116,8 +121,8 @@ namespace mu2e {
                 double dt = fabs(uptrkiinter->time() - downtrkiinter->time());
                 double dmom = fabs(uptrkiinter->mom() - downtrkiinter->mom());
                 if( dt < maxdt_ && dmom < maxdp_){
-                  if(debug_ > 1) std::cout << "Found matching track pair " << std::endl;
-                  matches.emplace_back(iup,idown,downtrkiinter->mom(),dt,dmom);
+                  if(debug_ > 1) std::cout << "Found matching track pair, dt " << dt << " dmom " << dmom << std::endl;
+                  matches.emplace_back(iup,idown,downtrkiinter->mom(),dt,dmom,upks.nHits()+downks.nHits());
                 }
               }
             }
@@ -127,21 +132,27 @@ namespace mu2e {
       // if there are >1 matches select the best
       int ibest = -1;
       if(matches.size() >0 ){
+        ++nref_;
         ibest = 0;
         if(matches.size()>1){
-          if(debug_ > 0) std::cout << "Selecting best reflection pair from " << matches.size() << " candidates " << std::endl;
+          ++nmultref_;
+          if(debug_ > 0) std::cout << "Selecting best reflection pair from " << matches.size() << " candidates " << " according to " << selbest_ << " event " << event.id() << std::endl;
           double value = std::numeric_limits<double>::max();
           for (size_t imatch = 0; imatch < matches.size(); ++imatch) {
             auto const& match = matches[imatch];
-            if(selbest_ == mom && -std::get<2>(match) < value){
+            if(debug_ > 1)std::cout << "Match " << imatch << " has downstream momentum " << std::get<2>(match) << " dt " << std::get<3>(match) << " dp " << std::get<4>(match) << " nactive " << std::get<5>(match) << std::endl;
+            if(selbest_ == mom && std::get<2>(match) > value){
               ibest = imatch;
-              value = -std::get<2>(match); // sign to make highest momentum best
+              value = std::get<2>(match);
             } else if(selbest_ == deltat && std::get<3>(match) < value){
               ibest = imatch;
               value = std::get<3>(match);
             } else if(selbest_ == deltap && std::get<4>(match) < value){
               ibest = imatch;
               value = std::get<4>(match);
+            } else if(selbest_ == nactive && std::get<5>(match) > value){
+              ibest = imatch;
+              value = std::get<5>(match);
             }
           }
         }
@@ -157,6 +168,10 @@ namespace mu2e {
     }
     event.put(std::move(mkseeds));
     return keep;
+  }
+
+  void SelectReflections::endJob() {
+    std::cout << moduleDescription().moduleLabel() << " processed " << nevts_ << " events and found " << nref_ << " reflections with " << nmultref_ << " multiple reflections" << std::endl;
   }
 }
 DEFINE_ART_MODULE(mu2e::SelectReflections)
